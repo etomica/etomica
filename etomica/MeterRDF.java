cf7c2a3221ba406aac34f8fff2e50e05dc06e5c6
David Kofke <kofke@buffalo.edu>
2004-09-22 14:16:45 +0000

    Completing fixes to MeterRDF

--- etomica/MeterRDF.java
+++ etomica/MeterRDF.java
@@ -2,23 +2,22 @@ package etomica;
 import etomica.units.Dimension;
 
 /**
- * Meter for tabulation of the atomic radial distribution function (RDF)
- * Should work ok in ensembles with fluctuating volume and particle numbers
- * Not suited for tabulation of RDFs of mixtures or heteroatomic molecules.
+ * Meter for tabulation of the atomic radial distribution function (RDF).
  *
  * @author David Kofke
  */
 public class MeterRDF extends MeterFunction implements EtomicaElement {
-    private AtomsetIteratorPhaseDependent iterator;
-    private final IteratorDirective iteratorDirective = new IteratorDirective();
-    private double[] vShell;
-    private final Space.CoordinatePair cPair;
-    protected double delr;
-    
+	
+	/**
+	 * Creates meter with default to compute pair correlation for all
+	 * leaf atoms in a phase.
+	 * @param parent
+	 */
     public MeterRDF(SimulationElement parent) {
 	    super(parent, new DataSourceUniform());
-	    ((DataSourceUniform)xDataSource).setDimension(Dimension.LENGTH);
-	    ((DataSourceUniform)xDataSource).setLabel("r");
+	    xDataSourceUniform = (DataSourceUniform)xDataSource;
+	    xDataSourceUniform.setDimension(Dimension.LENGTH);
+	    xDataSourceUniform.setLabel("r");
 	    setLabel("rdf");
 	    iterator = new ApiLeafAtoms();
 	    cPair = parent.space.makeCoordinatePair();
@@ -29,31 +28,45 @@ public class MeterRDF extends MeterFunction implements EtomicaElement {
         return info;
     }
     
+    /**
+     * Mutator method for the iterator that generates the atom pairs used to tabulate
+     * the radial distribution function.  By setting this iterator the meter can be
+     * configured to compute pair distribution for any set of atom pairs.  At construction
+     * the default is an instance of ApiLeafAtoms, which generates pairs from all leaf
+     * atoms in the phase.
+     * @param iter
+     */
     public void setIterator(AtomsetIteratorPhaseDependent iter) {iterator = iter;}
+    
+    /**
+     * Accessor method for the iterator that generates the atom pairs used to
+     * tabulate the radial distribution function.
+     * @return
+     */
     public AtomsetIteratorPhaseDependent getIterator() {return iterator;}
     
-    public IteratorDirective getDirective() {return iteratorDirective;}
-
+    /**
+     * Returns Dimension.NULL, indicating that the measured quantity is dimensionless.
+     */
     public Dimension getDimension() {return Dimension.NULL;}
 
 	/**
-	 * Computes RDF for the current configuration
-	 *    For future development: It may be possible to extend to particular atom pairs by changing iterator and using a different normalization
+	 * Computes RDF for the current configuration of the given phase.
 	 */
 	public double[] getDataAsArray(Phase phase) {
-		iterator.setPhase(phase);
         cPair.setBoundary(phase.boundary());
 	    for(int i=0; i<nDataPerPhase; i++) {phaseData[i] = 0.0;}  //zero histogram
-	    double xMax = ((DataSourceUniform)xDataSource).getXMax();
+	    double xMax = xDataSourceUniform.getXMax();
 	    double xMaxSquared = xMax*xMax;
 	    int count = 0;
+		iterator.setPhase(phase);
 	    iterator.reset();
 	    while(iterator.hasNext()) {                 //iterate over all pairs
 	    	Atom[] pair = iterator.next();
 	    	cPair.reset(pair[0].coord, pair[1].coord);
 	    	double r2 = cPair.r2();       //compute pair separation
 	        if(r2 < xMaxSquared) {
-	            int index = ((DataSourceUniform)xDataSource).getIndex(Math.sqrt(r2));  //determine histogram index
+	            int index = xDataSourceUniform.getIndex(Math.sqrt(r2));  //determine histogram index
 	            phaseData[index]++;                        //add once for each atom
 	        }
 	        //TODO consider if this (count) is being used correctly
@@ -61,16 +74,19 @@ public class MeterRDF extends MeterFunction implements EtomicaElement {
 	    }
 //	    int n = phase.atomCount();             //compute normalization: divide by
 	    double norm = count/phase.volume();    //n, and density*(volume of shell)
-	    double[] x = xDataSource.getData();
+	    double[] x = xDataSourceUniform.getData();
 	    Space space = phase.simulation().space();
-	    double xMin = ((DataSourceUniform)xDataSource).getXMin();
-	    double dx2 = 0.5*(xMax - xMin)/(double)nDataPerPhase;
+	    double dx2 = 0.5*(xMax - xDataSourceUniform.getXMin())/(double)nDataPerPhase;
 	    for(int i=0; i<nDataPerPhase; i++) {
 	        double vShell = space.sphereVolume(x[i]+dx2)-space.sphereVolume(x[i]-dx2);
 	    	phaseData[i] /= (norm*vShell);
 	    }
 	    return phaseData;
 	}
+	
+    private AtomsetIteratorPhaseDependent iterator;
+    private final Space.CoordinatePair cPair;
+    private final DataSourceUniform xDataSourceUniform;//local copy of xDataSource to avoid repeated casts
 	
 	/**
 	 * main method to demonstrate and test use of class.
