8ef82076792ac219a8e06763cfc19bdee283c17d
David Kofke <kofke@buffalo.edu>
2004-10-26 22:17:02 +0000

    Gamma function in SpecialFunctions

--- etomica/utility/SpecialFunctions.java
+++ etomica/utility/SpecialFunctions.java
@@ -41,5 +41,83 @@ public final class SpecialFunctions {
 //		return factorial;	
 //	}
 	
-   
+    /**
+     * Returns the ln(gamma), the natural logarithm of the gamma function.
+     */
+    //method is not tested
+    public static double lnGamma(double x) {
+    	double tmp = x+5.5;
+    	double y = x;
+    	double stp = 2.5066282746310005;
+    	tmp = (x+0.5)*Math.log(tmp) - tmp;
+    	double ser = 1.000000000190015;
+    	for(int i=0; i<5; i++) {
+    		y += 1.0;
+    		ser += lnGammaCoeff[i]/y;
+    	}
+    	return tmp + Math.log(stp*ser/x);
+    }
+    private static final double[] lnGammaCoeff = new double[] {
+    		+76.18009172947146,
+			-86.50532032941677,
+			+24.01409824083091,
+			-1.231739572450155,
+			+0.1208650973866179e-02,
+			-0.5395239384953e-05};
+    
+    /**
+     * Normalized incomplete gamma function, equal to
+     * Integrate[Exp[-t] t^(a-1),{t,x,Infinity}]/Gamma[a]
+     */
+    //method is not tested
+    public static double gammaQ(double a, double x) {
+    	if(x < 0.0 || a < 0.0) throw new IllegalArgumentException();
+    	if(a == 0.0) return 1.0;
+    	if(x < a+1.0) {
+    		return 1.0 - gser(a, x);
+    	} else {
+    		return gcf(a, x);
+    	}
+    }
+    
+    private static double gser(double a, double x) {
+    	int nmax = 500;
+    	double epsilon = 3.0e-12;
+    	double ap = a;
+    	double sum = 1.0/a;
+    	double del = sum;
+    	for(int n=1; n<=nmax; n++) {
+    		ap++;
+    		del *= x/ap;
+    		sum += del;
+    		if(Math.abs(del) < Math.abs(sum)*epsilon) break;
+    	}
+    	return sum * Math.exp(-x + a*Math.log(x) - lnGamma(a));
+    }
+    
+    private static double gcf(double a, double x) {
+    	int imax = 500;
+    	double epsilon = 3.0e-12;
+    	double b = x + 1.0 - a;
+    	double c = 1.0/Double.MIN_VALUE;
+    	double d = 1.0/b;
+		double h = d;
+    	for(int i=0; i<=imax; i++) {
+    		double an = -i*(i-a);
+    		b += 2.0;
+    		d = an*d + b;
+    		if(Math.abs(d) < Double.MIN_VALUE) d = Double.MIN_VALUE;
+    		c = b + an/c;
+    		if(Math.abs(c) < Double.MIN_VALUE) c = Double.MIN_VALUE;
+    		d = 1.0/d;
+    		double del = d*c;
+    		h *= del;
+    		if(Math.abs(del-1.0) < epsilon) break;
+    	}
+    	return Math.exp(-x + a*Math.log(x) - lnGamma(a))*h;
+    }
+    
+    public static void main(String[] args) {
+    	System.out.println(gammaQ(20.0,200.0));
+    }
 }
