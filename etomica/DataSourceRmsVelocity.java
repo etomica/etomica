21c8e833a3bf7685a81927fa0027186af7329195
David Kofke <kofke@buffalo.edu>
2004-10-28 21:08:59 +0000

    Updating DataSource and Meters

---
+++ etomica/DataSourceRmsVelocity.java
@@ -0,0 +1,60 @@
+package etomica;
+
+/**
+ * Meter for the root-mean-square velocity of a set of atoms. Useful to obtain
+ * histograms of atom speeds.
+ * 
+ * @author David Kofke
+ */
+
+public class DataSourceRmsVelocity extends DataSourceAdapter {
+
+	//TODO define a dimension for this property
+	public DataSourceRmsVelocity() {
+		super(etomica.units.Dimension.UNDEFINED);
+	}
+
+	/**
+	 * Returns the rms velocity of the atoms given by the iterator. Value is
+	 * given in the first element of the array, which is always of dimension 1.
+	 */
+	public double[] getData() {
+		iterator.reset();
+		int count = 0;
+		value[0] = 0.0;
+		while (iterator.hasNext()) {
+			Atom atom = iterator.nextAtom();
+			value[0] = atom.coord.rm()
+					* Math.sqrt(atom.coord.momentum().squared());
+			count++;
+		}
+		value[0] /= (double) count;
+		return value;
+	}
+
+	/**
+	 * Sets the iterator defining the atoms for which the RMS velocity is
+	 * calculated.
+	 * 
+	 * @param iterator
+	 */
+	public void setIterator(AtomIterator iterator) {
+		if (iterator == null)
+			this.iterator = AtomIterator.NULL;
+		else
+			this.iterator = iterator;
+	}
+
+	/**
+	 * @return The iterator defining the atoms used for the RMS velocity
+	 *         calculation
+	 */
+	public AtomIterator getIterator() {
+		return iterator;
+	}
+
+	private AtomIterator iterator = AtomIterator.NULL;
+
+	private final double[] value = new double[1];
+
+}//end of DataSourceVelocityRms
--- etomica/MeterTemperature.java
+++ etomica/MeterTemperature.java
@@ -1,49 +1,47 @@
 package etomica;
+
 import etomica.units.Dimension;
 
 /**
- * Meter for measurement of the temperature based on kinetic-energy equipartition
+ * Meter for measurement of the temperature based on kinetic-energy
+ * equipartition
  */
- 
- /* History of changes
-  * 7/03/02 (DAK) Changes to tie in with function of kinetic-energy meter.
-  */
 
-public final class MeterTemperature extends MeterScalar implements EtomicaElement, MeterAtomic {
-    
-    private final MeterKineticEnergy meterKE;
-    
-    public MeterTemperature() {
-        this(Simulation.instance);
-    }
-    public MeterTemperature(SimulationElement parent) {
-        super(parent);
-        setLabel("Temperature");
-        meterKE = new MeterKineticEnergy(this);
-    }
- 
-    public static EtomicaInfo getEtomicaInfo() {
-        EtomicaInfo info = new EtomicaInfo("Records temperature as given via kinetic energy");
-        return info;
-    }
-    
-    public void setPhase(Phase[] phase) {
-        super.setPhase(phase);
-        meterKE.setPhase(phase);
-    }
-        
-    public double getDataAsScalar(Phase phase) {
-        return (2./(double)(phase.atomCount()*phase.simulation().space().D()))*meterKE.getDataAsScalar(phase);
-    }
-    
-	public Dimension getDimension() {return Dimension.TEMPERATURE;}
-
-/**
- * Class method to compute the temperature of a phase from its total kinetic energy using equipartition
+/*
+ * History of changes 7/03/02 (DAK) Changes to tie in with function of
+ * kinetic-energy meter.
  */
- //commented out because MeterKineticEnergy static method is disabled 
- //iteration of atoms requires contstruction of iterator; not sure if want to do that
-//    public static double currentValue(Phase phase) {
-//        return (2./(double)(phase.atomCount()*phase.parentSimulation().space().D()))*MeterKineticEnergy.currentValue(phase);
-//    }    
+
+public final class MeterTemperature extends MeterScalar implements
+		EtomicaElement {
+
+	public MeterTemperature() {
+		this(Simulation.instance);
+	}
+
+	public MeterTemperature(SimulationElement parent) {
+		super(parent);
+		setLabel("Temperature");
+		meterKE = new MeterKineticEnergy(this);
+		D = parent.space.D();
+	}
+
+	public static EtomicaInfo getEtomicaInfo() {
+		EtomicaInfo info = new EtomicaInfo(
+				"Records temperature as given via kinetic energy");
+		return info;
+	}
+
+	public double getDataAsScalar(Phase phase) {
+		return (2. / (double) (phase.atomCount() * D))
+				* meterKE.getDataAsScalar(phase);
+	}
+
+	public Dimension getDimension() {
+		return Dimension.TEMPERATURE;
+	}
+
+	private final MeterKineticEnergy meterKE;
+	private final int D;
+
 }
