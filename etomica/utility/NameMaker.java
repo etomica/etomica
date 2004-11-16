284d271e9ff5fe0dfd7a512a481e2ba1f72bfa7b
David Kofke <kofke@buffalo.edu>
2004-11-16 04:47:29 +0000

    Added comments

--- etomica/utility/NameMaker.java
+++ etomica/utility/NameMaker.java
@@ -10,33 +10,45 @@ import etomica.Integrator;
 import etomica.Phase;
 
 /**
- * @author kofke
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Utility class with a method that generates a unique string that can be used
+ * to name an instance of a class. The name is formed from the class name given
+ * to the method, concatenated by a number equal to the number of times the
+ * method has been invoked previously with the given class.
  */
 public class NameMaker {
 
     /**
-     * 
+     * Private constructor prevents instantiation.
      */
-    private NameMaker() { }
+    private NameMaker() {
+    }
 
+    /**
+     * Generates the name String as from the given Class instance. The name is
+     * formed from the class name given to the method, concatenated by a number
+     * equal to the number of times the method has been invoked previously for
+     * the given class.
+     */
     public static String makeName(Class className) {
-        ClassCount thisCount = (ClassCount)classList.get(className);
+        ClassCount thisCount = (ClassCount) classList.get(className);
         if (thisCount == null) {
             thisCount = new ClassCount();
-            classList.put(className,thisCount);
+            classList.put(className, thisCount);
         }
-        return className.getName().substring(8)+thisCount.count++;
+        return className.getName().substring(8) + thisCount.count++;
     }
-    
+
     private static HashMap classList = new HashMap();
-    
+
+    //A mutable Integer class used to keep an instance count for each Class.
     private static class ClassCount {
         int count;
     }
-    
+
+    /**
+     * Simple main method to test the function of the class. Should print three
+     * strings to the console: "Phase0", "Integrator0", and "Phase1".
+     */
     public static void main(String[] argv) {
         System.out.println(NameMaker.makeName(Phase.class));
         System.out.println(NameMaker.makeName(Integrator.class));
