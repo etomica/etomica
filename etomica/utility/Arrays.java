53f949d23af9d53e6f1dd6a04f675f0f0c45bf47
David Kofke <kofke@buffalo.edu>
2004-11-16 04:39:14 +0000

    Added comments

--- etomica/utility/Arrays.java
+++ etomica/utility/Arrays.java
@@ -1,6 +1,6 @@
 /*
  * History
- * Created on Oct 28, 2004 by kofke
+ * Created on Oct 28, 2004 by kofke, schultz
  */
 package etomica.utility;
 
@@ -16,36 +16,65 @@ public final class Arrays {
 	 */
 	private Arrays() {	}
 
-	public static Object[] resizeArray(Object[] array, int newSize) {
-		Object[] newArray = (Object[])Array.newInstance(array.getClass().getComponentType(),newSize);
-		int minSize = Math.min(array.length,newSize);
-		System.arraycopy(array,0,newArray,0,minSize);
+    /**
+     * Returns a new array holding the objects in the given array, but adjusted
+     * to the given size.  If the new size is greater than the given array, all
+     * elements of the array are copied to the new array, starting from index 0
+     * (with unfilled elements of new array left as null);
+     * if the new size is smaller than the given array, elements of the old array
+     * (starting from 0) will be copied to fill the new array.  New array is
+     * of the same type as the old one (returned array must be cast before
+     * assigning to a field of the same type as the old array).
+     * @param oldArray array with elements to be copied to the new array
+     * @param newSize size of the new array
+     * @return the new array
+     */
+	public static Object[] resizeArray(Object[] oldArray, int newSize) {
+		Object[] newArray = (Object[])Array.newInstance(oldArray.getClass().getComponentType(),newSize);
+		int minSize = Math.min(oldArray.length,newSize);
+		System.arraycopy(oldArray,0,newArray,0,minSize);
 		return newArray;
 	}
 	
 
+    /**
+     * Returns an array formed from adding the newObject to the elements
+     * in the given array.  New array is one element larger than given array,
+     * and is of same type as given array.
+     * @param objects array with objects to be put in new array
+     * @param newObject object placed at end of new array
+     * @return new array 
+     */
 	public static Object[] addObject(Object[] objects, Object newObject) {
 		objects = resizeArray(objects,objects.length+1);
 		objects[objects.length-1] = newObject;
 		return objects;
 	}
-	
-	public static Object[] removeObject(Object[] objects, Object object) {
-		int length = objects.length;
+
+    /**
+     * Returns an array formed by removing the given object from the given array.  
+     * New array is one element smaller than given array,
+     * and is of same type as given array.
+     * @param array array with objects to be put in new array
+     * @param newObject object placed at end of new array
+     * @return new array 
+     */
+	public static Object[] removeObject(Object[] array, Object object) {
+		int length = array.length;
 		for (int i=0; i<length; i++) {
-			if (objects[i] == object) {
-				Object lastObject = objects[length-1];
-				objects = resizeArray(objects,length-1);
-				if (i < length-2) {
-					System.arraycopy(objects,i+1,objects,i,length-i-2);
+			if (array[i] == object) {//look for object in array
+				Object lastObject = array[length-1];//save last object, which is about to be dropped
+				array = resizeArray(array,length-1);//shorten array, dropping last object
+				if (i < length-2) {//overwrite target object
+					System.arraycopy(array,i+1,array,i,length-i-2);
 				}
-				if (i < length-1) {
-					objects[length-2] = lastObject;
+				if (i < length-1) {//recover last object
+					array[length-2] = lastObject;
 				}
 				break;
 			}
 		}
-		return objects;
+		return array;
 	}
 
 }
