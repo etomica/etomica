a6a3ea74ec8fb7d6ccdf98a6c97ba845e13533dd
David Kofke <kofke@buffalo.edu>
2004-11-17 06:01:29 +0000

    Removed inappropriate methods that had been copied from MeterScalar

--- etomica/MeterTensor.java
+++ etomica/MeterTensor.java
@@ -21,25 +21,7 @@ public abstract class MeterTensor extends MeterAbstract {
         super(space.D()*space.D());
         translator = new DataTranslatorTensor(space);
     }
-    
-    /**
-     * Returns a tensor value as the measurement for the given phase.
-     * Subclasses define this method to specify the measurement they make.
-     */
-    public abstract double getDataAsScalar(Phase phase);
-    
-    /**
-     * Causes the single getDataAsScalar(Phase) value to be computed and
-     * returned for the given phase. In response to a getData() call,
-     * MeterAbstract superclass will loop over all phases previously specified
-     * via setPhase and collect these values into a vector and return them in
-     * response to a getData() call.
-     */
-    public final double[] getData(Phase phase) {
-        phaseData[0] = getDataAsScalar(phase);
-        return phaseData;
-    }
-    
+        
     public DataTranslator getTranslator() {return translator;}
     
     private final DataTranslatorTensor translator;
