9e026e2b33a713a17152d0e3c0b6e3c69c7129bd
Andrew Schultz <ajs42@buffalo.edu>
2004-12-07 22:30:54 +0000

    add DataSource fronts for potential and kinetic energies from the Integrator

---
+++ etomica/IntegratorKineticEnergy.java
@@ -0,0 +1,55 @@
+package etomica;
+
+import etomica.Integrator.IntervalEvent;
+import etomica.units.Dimension;
+
+/**
+ * Acts as a DataSource to retrieve the energy from the integrator 
+ */
+public class IntegratorKineticEnergy implements DataSource, Integrator.IntervalListener {
+
+    public IntegratorKineticEnergy(IntegratorMD aIntegrator) {
+        integrator = aIntegrator;
+        integrator.addIntervalListener(this);
+        meterKE = new MeterKineticEnergy();
+    }
+    
+    public double[] getData() {
+        return integrator.getKineticEnergy();
+    }
+
+    public int getPriority() {return 200;}
+    
+    public void intervalAction(IntervalEvent evt) {
+        if (evt.type() == Integrator.IntervalEvent.DONE) {
+            double[] currentKE = integrator.getKineticEnergy();
+            Phase[] phase = integrator.getPhase();
+            meterKE.setPhase(phase);
+            double[] KE = meterKE.getData();
+            for (int i=0; i<phase.length; i++) {
+                if (Math.abs(KE[i] - currentKE[i]) > 1.e-9) {
+                    System.out.println("final kinetic energy ("+currentKE[i]+") for "+phase[i]+" doesn't match actual energy ("+KE[i]+")");
+                }
+            }
+        }
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    public void setLabel(String string) {
+        label = string;
+    }
+
+    public Dimension getDimension() {
+        return Dimension.ENERGY;
+    }
+
+    public DataTranslator getTranslator() {
+        return DataTranslator.IDENTITY;
+    }
+
+    private IntegratorMD integrator;
+    private String label = "Kinetic Energy";
+    private MeterKineticEnergy meterKE;
+}
---
+++ etomica/IntegratorPotentialEnergy.java
@@ -0,0 +1,54 @@
+package etomica;
+
+import etomica.Integrator.IntervalEvent;
+import etomica.units.Dimension;
+
+/**
+ * Acts as a DataSource to retrieve the energy from the integrator 
+ */
+public class IntegratorPotentialEnergy implements DataSource, Integrator.IntervalListener {
+
+    public IntegratorPotentialEnergy(Integrator aIntegrator) {
+        integrator = aIntegrator;
+        integrator.addIntervalListener(this);
+    }
+    
+    public double[] getData() {
+        return integrator.getPotentialEnergy();
+    }
+
+    public int getPriority() {return 200;}
+    
+    public void intervalAction(IntervalEvent evt) {
+        if (evt.type() == Integrator.IntervalEvent.DONE) {
+            double[] currentPE = integrator.getPotentialEnergy();
+            Phase[] phase = integrator.getPhase();
+            MeterPotentialEnergy meterPE = new MeterPotentialEnergy(integrator.potential);
+            meterPE.setPhase(phase);
+            double[] PE = meterPE.getData();
+            for (int i=0; i<phase.length; i++) {
+                if (PE[i] != currentPE[i]) {
+                    System.out.println("final potential energy ("+currentPE[i]+") for "+phase[i]+" doesn't match actual energy ("+PE[i]+")");
+                }
+            }
+        }
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    public void setLabel(String string) {
+        label = string;
+    }
+
+    public Dimension getDimension() {
+        return Dimension.ENERGY;
+    }
+
+    public DataTranslator getTranslator() {
+        return DataTranslator.IDENTITY;
+    }
+
+    private Integrator integrator;
+    private String label = "Potential Energy";
+}
