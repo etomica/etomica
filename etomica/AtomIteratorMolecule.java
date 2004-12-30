f476d83630949196223508afce0e9d337d824bba
David Kofke <kofke@buffalo.edu>
2004-12-30 20:43:26 +0000

    New formulation of AtomIteratorMolecule, for restructuring of PotentialMaster. Class previously named AtomIteratorMolecule is now AtomIteratorAllMolecules, and is not species dependent (now gives all molecules in phase)

--- etomica/AtomIteratorMolecule.java
+++ etomica/AtomIteratorMolecule.java
@@ -1,100 +1,79 @@
+/*
+ * History
+ * Created on Dec 30, 2004 by kofke
+ */
 package etomica;
 
+import etomica.IteratorDirective.Direction;
+
 /**
- * Iterator for the molecules in a phase. Loops over those atoms that lie just
- * below the species agents in the atom tree hierarchy. Can be set to loop over
- * all molecules in a phase, or only those for a given species.
- * 
- * @author David Kofke
- * @since 02.02.16
+ * Iterator for the molecules of a single species in a phase.  Can be targeted to
+ * give the molecule containing a specific atom (if consistent with the species),
+ * or all molecules of the species in a phase.<br>
+ * This class is used by PotentialMaster to iterate over molecules for single-body
+ * potentials.
  */
+public class AtomIteratorMolecule extends AtomIteratorAdapter implements
+        AtomsetIteratorMolecule {
 
-public final class AtomIteratorMolecule extends AtomIteratorAdapter implements
-		AtomsetIteratorSpeciesDependent, AtomIteratorSpeciesDependent {
+    /**
+     * @param species species for which molecules are returned as iterates. Only
+     * species[0] is relevant, and must not be null.
+     */
+    public AtomIteratorMolecule(Species[] species) {
+        super(new AtomIteratorListSimple());
+        listIterator = (AtomIteratorListSimple)iterator;
+        this.species = species[0];
+        index = this.species.getIndex();
+    }
 
-	public AtomIteratorMolecule() {
-		super(new AtomIteratorTree(2));
-		treeIterator = (AtomIteratorTree) iterator;
-		setPhase(null);
-	}
+    /**
+     * Sets the phase containing the molecules for iteration.
+     */
+    public void setPhase(Phase phase) {
+        speciesAgentNode = (AtomTreeNodeGroup)phase.getAgent(species).node;
+        setList();
+    }
 
-	/**
-	 * Returns a new iterator ready to iterate over the molecules of the given
-	 * phase.
-	 */
-	public AtomIteratorMolecule(Phase phase) {
-		this();
-		setPhase(phase);
-	}
+    /**
+     * Specifies molecule returned by this iterator, as the one containing
+     * the given target atom.  Only the first element of the array is relevant.
+     * If argument is null, of zero length, or if targetAtoms[0] is null, then
+     * no target atom is specified and all molecules of the species in the
+     * phase will be given on iteration.
+     */
+    public void setTarget(Atom[] targetAtoms) {
+        if(targetAtoms == null || targetAtoms.length == 0) targetAtom = null;
+        else targetAtom = targetAtoms[0];
+        setList();
+    }
 
-	/**
-	 * Returns a new iterator ready to iterate over the molecules of the given
-	 * species in the given phase.
-	 */
-	public AtomIteratorMolecule(Phase phase, Species species) {
-		this();
-		set(phase, species);
-	}
+    /** 
+     * Has no effect, but is included as part of the AtomsetIteratorMolecule interface.
+     */
+    public void setDirection(Direction direction) {
+        //ignore
+    }
 
-	public void set(Phase phase, Species species) {
-		this.species = species;
-		setPhase(phase);
-	}
+    /**
+     * Configures the list iterator with a list appropriate to the specified
+     * phase and target.
+     */
+    private void setList() {
+        if(targetAtom == null) {
+            listIterator.setList(speciesAgentNode.childList);
+        } else {
+            AtomTreeNode moleculeNode = targetAtom.node.childWhereDescendedFrom(speciesAgentNode);
+            littleList.clear();
+            if(moleculeNode != null) littleList.add(moleculeNode.atom);
+            listIterator.setList(littleList);
+        }
+    }
 
-	public void setPhase(Phase phase) {
-		if (phase == null)
-			treeIterator.setRoot(null);
-		else if (species == null) {
-			treeIterator.setIterationDepth(2);
-			treeIterator.setRoot(phase.speciesMaster);
-		} else {
-			treeIterator.setIterationDepth(1);
-			treeIterator.setRoot(phase.getAgent(species));
-		}
-	}
-
-	public void setSpecies(Species[] species) {
-		this.species = species[0];
-		setPhase(phase);
-	}
-
-	private final AtomIteratorTree treeIterator;
-
-	private Species species;
-
-	private Phase phase;
-
-	/**
-	 * main method to test and demonstrate use of this class.
-	 */
-	public static void main(String args[]) {
-
-		Simulation sim = new Simulation();
-		Simulation.instance = sim;
-		Species species2 = new SpeciesSpheresMono(sim);
-		Species species1 = new SpeciesSpheres(sim.space, sim.potentialMaster.sequencerFactory(), 3, 3);
-		Species species0 = new SpeciesSpheres(sim.space, sim.potentialMaster.sequencerFactory(), 3, 2);
-		species0.setNMolecules(3);
-		species1.setNMolecules(2);
-		species2.setNMolecules(3);
-		Phase phase = new Phase(sim.space);
-//		sim.elementCoordinator.go();
-
-		AtomIteratorMolecule iterator = new AtomIteratorMolecule();
-		System.out.println(iterator.hasNext());
-
-		iterator.setPhase(phase);
-		System.out.println(iterator.hasNext());
-		iterator.reset();
-		while (iterator.hasNext())
-			System.out.println(iterator.next().toString());
-		System.out.println();
-
-		iterator.set(phase, species2);
-		iterator.reset();
-		while (iterator.hasNext())
-			System.out.println(iterator.next().toString());
-		System.out.println();
-	}//end of main
-
-}//end of AtomIteratorMolecule
+    private final int index;
+    private final AtomIteratorListSimple listIterator;
+    private final Species species;
+    private final AtomList littleList = new AtomList();
+    private AtomTreeNodeGroup speciesAgentNode;
+    private Atom targetAtom;
+}
