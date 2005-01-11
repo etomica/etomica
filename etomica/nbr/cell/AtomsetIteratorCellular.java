fb54659dce68cec710a490be1641e910aadb4fbb
David Kofke <kofke@buffalo.edu>
2005-01-11 14:43:16 +0000

    Minor correction to comment

--- etomica/nbr/cell/AtomsetIteratorCellular.java
+++ etomica/nbr/cell/AtomsetIteratorCellular.java
@@ -8,7 +8,7 @@ import etomica.lattice.CellLattice;
 /**
  * Interface for atomset iterators that use cell-based neighbor
  * lists for iteration.  Defines a method that returns the iterator
- * the loops over the cells neighboring a given cell.  Access to
+ * that loops over the cells neighboring a given cell.  Access to
  * the cell iterator is needed to adjust the neighbor range defining
  * the neighbor cells.
  */
