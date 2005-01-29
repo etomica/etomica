5f687d88d0183f6bac95c540586b195ba5522867
Andrew Schultz <ajs42@buffalo.edu>
2005-01-29 02:00:20 +0000

    remove 3D method

--- etomica/space/BoundaryPeriodicSquare.java
+++ etomica/space/BoundaryPeriodicSquare.java
@@ -51,35 +51,6 @@ public class BoundaryPeriodicSquare extends Boundary {
         dr.ME(dimensionsHalf);
     }
 
-    public void nearestImage(Vector3D dr) {
-        //      dr.x -= dimensions.x*((dr.x > 0.0) ? Math.floor(dr.x/dimensions.x +
-        // 0.5) : Math.ceil(dr.x/dimensions.x - 0.5));
-        //      dr.y -= dimensions.y*((dr.y > 0.0) ? Math.floor(dr.y/dimensions.y +
-        // 0.5) : Math.ceil(dr.y/dimensions.y - 0.5));
-        //      dr.z -= dimensions.z*((dr.z > 0.0) ? Math.floor(dr.z/dimensions.z +
-        // 0.5) : Math.ceil(dr.z/dimensions.z - 0.5));
-        //      final double dimxHalf = 0.5*dimensions.x;
-        //      final double dimyHalf = 0.5*dimensions.y;
-        //      final double dimzHalf = 0.5*dimensions.z;
-        /*
-         * while(dr.x > +dimensionsHalf.x) dr.x -= dimensions.x; while(dr.x <
-         * -dimensionsHalf.x) dr.x += dimensions.x; while(dr.y >
-         * +dimensionsHalf.y) dr.y -= dimensions.y; while(dr.y <
-         * -dimensionsHalf.y) dr.y += dimensions.y; while(dr.z >
-         * +dimensionsHalf.z) dr.z -= dimensions.z; while(dr.z <
-         * -dimensionsHalf.z) dr.z += dimensions.z;
-         */
-        dr.PE(dimensionsHalf);
-        dr.mod(dimensions);
-        dr.ME(dimensionsHalf);
-        //System.out.println("dimesions = "+dimensions);
-        //    System.out.print(dr.x+" ");dr.x %= dimensionsHalf.x;
-        // System.out.println(dr.x);
-        //    dr.x = ((dr.x + dimensions.x) % dimensions.x) - dimensionsHalf.x;
-        //    dr.y = ((dr.y + dimensions.y) % dimensions.y) - dimensionsHalf.y;
-        //    dr.z = ((dr.z + dimensions.z) % dimensions.z) - dimensionsHalf.z;
-    }
-
     //Converts dr to its nearest-image value and returns in shift
     //the difference between the new and old values of dr
     public void nearestImage(Vector3D dr, Vector3D shift) {
