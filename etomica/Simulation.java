9cf25647bcb6fa199744b134f26ff0be2d21e58c
Andrew Schultz <ajs42@buffalo.edu>
2005-01-31 03:36:46 +0000

    rerererandomize

--- etomica/Simulation.java
+++ etomica/Simulation.java
@@ -236,8 +236,8 @@ public class Simulation {
     
     private static int instanceCount = 0;
     
-//    public static final java.util.Random random = new java.util.Random();
-    public static final java.util.Random random = new java.util.Random(1);
+    public static final java.util.Random random = new java.util.Random();
+//    public static final java.util.Random random = new java.util.Random(1);
         
      /**
       * Returns the mediator that coordinates the elements of the simulation.
