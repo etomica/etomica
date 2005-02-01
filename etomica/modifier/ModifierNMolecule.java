011ac4eeb8a45f2616fc7afb30f9e0b54df27f87
David Kofke <kofke@buffalo.edu>
2005-02-01 04:28:32 +0000

    Fix ModifierNMolecule class

--- etomica/modifier/ModifierNMolecule.java
+++ etomica/modifier/ModifierNMolecule.java
@@ -1,37 +1,30 @@
 package etomica.modifier;
 
 import etomica.Modifier;
+import etomica.SpeciesAgent;
 import etomica.units.Dimension;
 
-
-
 /*
- * History
- * Created on Jan 31, 2005 by kofke
+ * History Created on Jan 31, 2005 by kofke
  */
-class ModifierNMolecule extends Modifier {
-        
-        public ModifierNMolecule() {
-            super(Dimension.QUANTITY);
-        }
-        
-        public void setValue(double d) {
- //           if(initializing) return;
-            if(d < 0) d = 0;
-            boolean isPaused = this.selector.integrator.isPaused();
-   //         if(!isPaused) {
-                try {
-                    this.selector.restartAction.actionPerformed();
-                } catch (NullPointerException ex) {return;}
-   //         }
-            try {
-                 this.selector.speciesAgent.setNMolecules((int)d);
-            } catch(NullPointerException ex) {}
-                try {
-                    this.selector.restartAction.actionPerformed();
-                } catch (NullPointerException ex) {return;}
-            if(this.selector.display != null) this.selector.display.repaint();
-            this.selector.integrator.reset();
-        }
-        public double getValue() {return (this.selector.speciesAgent!=null)?(double)this.selector.speciesAgent.moleculeCount():0;}
+public class ModifierNMolecule extends Modifier {
+
+    public ModifierNMolecule(SpeciesAgent speciesAgent) {
+        super(Dimension.QUANTITY);
+        this.speciesAgent = speciesAgent;
     }
+
+    public void setValue(double d) {
+        if (d < 0) d = 0;
+        speciesAgent.setNMolecules((int) d);
+//        if (this.selector.display != null)
+//            this.selector.display.repaint();
+//        this.selector.integrator.reset();
+    }
+
+    public double getValue() {
+        return (speciesAgent != null) ? (double)speciesAgent.moleculeCount() : 0;
+    }
+    
+    private final SpeciesAgent speciesAgent;
+}
