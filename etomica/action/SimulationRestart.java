73c876a561b2e34dceb863f5073b308f64dd4716
David Kofke <kofke@buffalo.edu>
2005-02-04 04:07:24 +0000

    Make restart re-initialize the configuration too

--- etomica/action/SimulationRestart.java
+++ etomica/action/SimulationRestart.java
@@ -27,6 +27,11 @@ public final class SimulationRestart extends SimulationActionAdapter {
      * this method is broken
      */
     public void actionPerformed() {
+
+        for(Iterator iter=simulation.getPhaseList().iterator(); iter.hasNext(); ) {
+            Phase phase = (Phase)iter.next();
+            phase.reset();
+        }
         
         for(Iterator iter=simulation.getIntegratorList().iterator(); iter.hasNext(); ) {
             Integrator integrator = (Integrator)iter.next();
