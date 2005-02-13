1d2bc0712e8580de285672e2cc96e9672c2ac3f5
Andrew Schultz <ajs42@buffalo.edu>
2005-02-13 23:22:47 +0000

    add cluster suitable for use in umbrella sampling

---
+++ etomica.apps/etomica/virial/ClusterWeightUmbrella.java
@@ -0,0 +1,49 @@
+package etomica.virial;
+
+
+/**
+ * Umbrella cluster weight wrapper function.  The umbrella cluster's
+ * value will be positive when any of the sub clusters have non-zero values.
+ * This differs from a ClusterSum mostly because the sub cluster values are
+ * not actually summed.
+ */
+public class ClusterWeightUmbrella implements ClusterWeight {
+	
+	/**
+	 * Contructs an umbrella cluster from the given clusters.
+	 */
+	public ClusterWeightUmbrella(ClusterAbstract[] allClusters) {
+		clusterArray = allClusters;
+		weightRatio = new double[allClusters.length];
+		for (int i=0; i<weightRatio.length; i++) {
+			weightRatio[i] = 1.0/weightRatio.length;
+		}
+	}
+	
+	public int pointCount() {
+		// can they be different?
+		return clusterArray[0].pointCount();
+	}
+	
+	public double value(CoordinatePairSet cPairSet, double beta) {
+		double sum = 0.0;
+		for (int i=0; i<clusterArray.length; i++) {
+			double v = clusterArray[i].value(cPairSet,beta);
+			sum += v*v*weightRatio[i];
+		}
+		return Math.sqrt(sum);
+	}
+	
+	public void setWeightRatio(double[] aWeightRatio) {
+		for (int i=weightRatio.length-1; i>=0; i--) {
+			weightRatio[i] = aWeightRatio[i]/aWeightRatio[0];
+		}
+	}
+	
+	public double[] getWeightRatio() {
+		return weightRatio;
+	}
+    
+    private final ClusterAbstract[] clusterArray;
+    private final double[] weightRatio;
+}
