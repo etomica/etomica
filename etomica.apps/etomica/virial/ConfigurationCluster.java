7159ffdb23ece000d848479a44c09235a0454108
Andrew Schultz <ajs42@buffalo.edu>
2005-02-13 01:36:53 +0000

    update ConfigurationCluster.  cluster is obtained from PhaseCluster

--- etomica.apps/etomica/virial/ConfigurationCluster.java
+++ etomica.apps/etomica/virial/ConfigurationCluster.java
@@ -1,69 +1,50 @@
 package etomica.virial;
 
-import etomica.Atom;
 import etomica.AtomIterator;
 import etomica.Configuration;
-import etomica.Default;
-import etomica.Simulation;
 import etomica.Space;
 import etomica.space.Vector;
 
 /**
  * @author kofke
  *
- * Generates a configuration such that the value of a given cluster will have a
- * specific sign (positive or negative).
+ * Generates a configuration such that the value of the phase's
+ * sampling cluster is positive at beta = 1.
  */
 public class ConfigurationCluster extends Configuration {
 
 	/**
 	 * Constructor for ConfigurationCluster.
-	 * @param space
 	 */
 	public ConfigurationCluster(Space space) {
 		super(space);
 	}
 
 	/**
-	 * Constructor for ConfigurationCluster.
-	 * @param sim
-	 */
-	public ConfigurationCluster(Simulation sim) {
-		super(sim);
-	}
-
-	/**
 	 * @see etomica.Configuration#initializePositions(etomica.AtomIterator)
 	 */
 	public void initializePositions(AtomIterator[] iterator) {
-		Vector dimensions = phase.space.makeVector();
-		dimensions.E(Default.BOX_SIZE);
-		Vector center = phase.space.makeVector();
-		center.Ea1Tv1(0.5, dimensions);
+		Vector translationVector = phase.space().makeVector();
+        Vector dimVector = Space.makeVector(dimensions);
+		Vector center = phase.space().makeVector();
 		AtomIterator iter = iterator[0];
 		iter.reset();
-		while(iter.hasNext()) iter.next().coord.translateTo(center);//put all at center of box
-		double value = cluster.value(phase.getPairSet().resetPairs(), 1.0);
-		while( value == 0 || (signPositive != (value>0.0))) { //if center is not ok, keep trying random positions until ok
-			iter.reset();
-			iter.next().coord.translateTo(center);
+		while(iter.hasNext()) iter.nextAtom().coord.position().E(center);//put all at center of box
+		double value = phase.getSampleCluster().value(phase.getCPairSet(), 1.0);
+        if (value == 0) {
+            System.out.println("initial cluster value bad... trying to fix it.  don't hold your breath.");
+        }
+		while( value == 0 ) { //if center is not ok, keep trying random positions until ok
+		    // if we make it in here we probably won't make it out!
+            iter.reset();
+			iter.nextAtom();
 			while(iter.hasNext()) {
-				Atom a = iter.next();
-				a.coord.translateToRandom(phase);	
+                translationVector.setRandomCube();
+                translationVector.TE(dimVector);
+                iter.nextAtom().coord.position().PE(translationVector);
 			}
-			value = cluster.value(phase.getPairSet().resetPairs(),1.0);
-		}//end while
-	}
-
-	boolean signPositive;
-	Cluster cluster;
-	PhaseCluster phase;
-	/**
-	 * Returns the cluster.
-	 * @return Cluster
-	 */
-	public Cluster getCluster() {
-		return cluster;
+			value = phase.getSampleCluster().value(phase.getCPairSet(),1.0);
+		}
 	}
 
 	/**
@@ -74,23 +55,7 @@ public class ConfigurationCluster extends Configuration {
 		return phase;
 	}
 
-	/**
-	 * Returns the signPositive.
-	 * @return boolean
-	 */
-	public boolean isSignPositive() {
-		return signPositive;
-	}
-
-	/**
-	 * Sets the cluster.
-	 * @param cluster The cluster to set
-	 */
-	public void setCluster(Cluster cluster) {
-		this.cluster = cluster;
-	}
-
-	/**
+    /**
 	 * Sets the phase.
 	 * @param phase The phase to set
 	 */
@@ -98,12 +63,5 @@ public class ConfigurationCluster extends Configuration {
 		this.phase = phase;
 	}
 
-	/**
-	 * Sets the signPositive.
-	 * @param signPositive The signPositive to set
-	 */
-	public void setSignPositive(boolean signPositive) {
-		this.signPositive = signPositive;
-	}
-
+    private PhaseCluster phase;
 }
