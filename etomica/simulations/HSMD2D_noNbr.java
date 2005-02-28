3ea2e253d7e37fbc9729db9229040d02c9fc09f5
David Kofke <kofke@buffalo.edu>
2005-02-28 18:38:16 +0000

    add some more demonstration features

--- etomica/simulations/HSMD2D_noNbr.java
+++ etomica/simulations/HSMD2D_noNbr.java
@@ -5,8 +5,18 @@ import etomica.Species;
 import etomica.SpeciesSpheresMono;
 import etomica.action.PhaseImposePbc;
 import etomica.action.activity.ActivityIntegrate;
+import etomica.data.AccumulatorAverage;
+import etomica.data.AccumulatorHistory;
+import etomica.data.DataPump;
+import etomica.data.meter.MeterPressureHard;
+import etomica.data.meter.MeterTemperature;
+import etomica.graphics.DeviceNSelector;
+import etomica.graphics.DeviceThermoSelector;
+import etomica.graphics.DisplayBoxesCAE;
+import etomica.graphics.DisplayPlot;
 import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntegratorHard;
+import etomica.integrator.IntervalActionAdapter;
 import etomica.potential.P2HardSphere;
 import etomica.space2d.Space2D;
 
@@ -19,6 +29,13 @@ import etomica.space2d.Space2D;
 public class HSMD2D_noNbr extends Simulation {
     
     public ActivityIntegrate activityIntegrate;
+    public AccumulatorAverage pressureAverage;
+    public AccumulatorHistory pressureHistory;
+    public AccumulatorAverage temperatureAverage;
+    public AccumulatorHistory temperatureHistory;
+    public Phase phase;
+    public SpeciesSpheresMono species;
+    public IntegratorHard integrator;
 
     public HSMD2D_noNbr() {
     	this(new Space2D());
@@ -27,28 +44,71 @@ public class HSMD2D_noNbr extends Simulation {
     public HSMD2D_noNbr(Space2D space) {
         super(space);
 
-        IntegratorHard integrator = new IntegratorHard(potentialMaster);
+        integrator = new IntegratorHard(potentialMaster);
         integrator.setIsothermal(false);
         activityIntegrate = new ActivityIntegrate(integrator);
         getController().addAction(activityIntegrate);
-        SpeciesSpheresMono species = new SpeciesSpheresMono(this);
+        species = new SpeciesSpheresMono(this);
         species.setNMolecules(64);
-	    Phase phase = new Phase(space);
+	    phase = new Phase(space);
 	    P2HardSphere potential = new P2HardSphere(space);
 	    potentialMaster.setSpecies(potential,new Species[]{species,species});
         
         integrator.addIntervalListener(new PhaseImposePbc(phase));
         phase.speciesMaster.addSpecies(species);
         integrator.addPhase(phase);
-    }
+        integrator.setIsothermal(true);
+        
+        MeterPressureHard meterPressure = new MeterPressureHard(integrator);
+        meterPressure.setPhase(phase);
+        pressureAverage = new AccumulatorAverage();
+        DataPump pressurePump = new DataPump(meterPressure, pressureAverage);
+        IntervalActionAdapter pressureAction = new IntervalActionAdapter(pressurePump, integrator);
+
+        pressureHistory = new AccumulatorHistory();
+        pressureAverage.makeDataPusher(
+          new AccumulatorAverage.Type[] {AccumulatorAverage.AVERAGE}).
+                                      addDataSink(pressureHistory);
+        
+        MeterTemperature meterTemperature = new MeterTemperature();
+        meterTemperature.setPhase(phase);
+        temperatureAverage = new AccumulatorAverage();
+        DataPump temperaturePump = new DataPump(meterTemperature, temperatureAverage);
+        IntervalActionAdapter temperatureAction = new IntervalActionAdapter(temperaturePump, integrator);
+
+        pressureHistory = new AccumulatorHistory();
+        pressureAverage.makeDataPusher(
+          new AccumulatorAverage.Type[] {AccumulatorAverage.AVERAGE}).
+                                      addDataSink(pressureHistory);
+        temperatureHistory = new AccumulatorHistory();
+        temperatureAverage.makeDataPusher(
+            new AccumulatorAverage.Type[] {AccumulatorAverage.AVERAGE}).
+                                        addDataSink(temperatureHistory);
+}
     
     /**
      * Demonstrates how this class is implemented.
      */
     public static void main(String[] args) {
         HSMD2D_noNbr sim = new HSMD2D_noNbr();
-        SimulationGraphic graphic = new SimulationGraphic(sim);
+        final SimulationGraphic graphic = new SimulationGraphic(sim);
         sim.activityIntegrate.setDoSleep(true);
+        DisplayBoxesCAE pressureDisplay = new DisplayBoxesCAE();
+        pressureDisplay.setAccumulator(sim.pressureAverage);
+        DisplayPlot pressurePlot = new DisplayPlot();
+        sim.pressureHistory.addDataSink(pressurePlot.makeDataSink());
+        DisplayBoxesCAE temperatureDisplay = new DisplayBoxesCAE();
+        temperatureDisplay.setAccumulator(sim.temperatureAverage);
+        DisplayPlot temperaturePlot = new DisplayPlot();
+        sim.temperatureHistory.addDataSink(temperaturePlot.makeDataSink());
+        DeviceNSelector nSelector = new DeviceNSelector(sim, sim.phase.getAgent(sim.species));
+        DeviceThermoSelector thermo = new DeviceThermoSelector(sim.getController(), sim.integrator);
+        graphic.add(nSelector);
+        graphic.add(thermo);
+        graphic.add(pressureDisplay);
+        graphic.add(pressurePlot);
+        graphic.add(temperatureDisplay);
+        graphic.add(temperaturePlot);
 		graphic.makeAndDisplayFrame();
     }//end of main
     
