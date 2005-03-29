bf60aadc3a23c896d18e97456fb9377fd05b31e5
David Kofke <kofke@buffalo.edu>
2005-03-29 13:38:09 +0000

    allow access to the wrapped array

--- etomica/atom/AtomsetArray.java
+++ etomica/atom/AtomsetArray.java
@@ -5,8 +5,7 @@ import etomica.AtomSet;
 
 
 /**
- * AtomSet formed by wrapping an Atom array
- *
+ * AtomSet formed by wrapping an Atom array.
  */
 
 /*
@@ -16,33 +15,53 @@ import etomica.AtomSet;
 public class AtomsetArray implements AtomSet {
 
     /**
-     * 
+     * Wraps a new atom array of the given length.
      */
     public AtomsetArray(int nAtoms) {
         atoms = new Atom[nAtoms];
     }
+    
+    /**
+     * Wraps the given atom array.  Subsequent call to getArray
+     * will return the array instance given here.
+     */
+    public AtomsetArray(Atom[] atoms) {
+        this.atoms = atoms;
+    }
 
-    /* (non-Javadoc)
-     * @see etomica.AtomSet#getAtom(int)
+    /**
+     * Part of implementation of AtomSet interface.
      */
     public Atom getAtom(int i) {
         return atoms[i];
     }
     
+    /**
+     * @return the wrapped array of atoms, which is declared final in the class.
+     */
+    public Atom[] getArray() {
+        return atoms;
+    }
+    
+    /**
+     * Copies the atoms in the given array to the wrapped, final array of atoms.
+     */
     public void setAtoms(Atom[] newAtoms) {
         if(newAtoms.length != atoms.length) throw new IllegalArgumentException("Wrong size array");
         System.arraycopy(newAtoms, 0, this.atoms, 0, atoms.length);
     }
 
-    /* (non-Javadoc)
-     * @see etomica.AtomSet#count()
+    /**
+     * Returns the length of the wrapped array.
      */
     public int count() {
         return atoms.length;
     }
 
-    /* (non-Javadoc)
-     * @see etomica.AtomSet#equals(etomica.AtomSet)
+    /**
+     * Returns true if elements of the wrapped array are equal
+     * to those in the given atom set, with the comparison
+     * performed element-by-element.
      */
     public boolean equals(AtomSet atoms) {
         if(atoms.count() != count()) return false;
