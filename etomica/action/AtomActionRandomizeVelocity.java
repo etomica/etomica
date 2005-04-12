1c000eb8d0270e213d06fd7f9031127d0e5ba81f
David Kofke <kofke@buffalo.edu>
2005-04-12 21:55:04 +0000

    assign zero velocity if mass is infinite; initialize with default temperature

--- etomica/action/AtomActionRandomizeVelocity.java
+++ etomica/action/AtomActionRandomizeVelocity.java
@@ -1,6 +1,7 @@
 package etomica.action;
 
 import etomica.Atom;
+import etomica.Default;
 import etomica.space.ICoordinateKinetic;
 import etomica.space.Vector;
 import etomica.statmech.MaxwellBoltzmann;
@@ -22,14 +23,21 @@ public class AtomActionRandomizeVelocity extends AtomActionAdapter {
 
     public AtomActionRandomizeVelocity() {
         setLabel("Randomize velocity");
+        setTemperature(Default.TEMPERATURE);
     }
 
-    /* (non-Javadoc)
-     * @see etomica.action.AtomAction#actionPerformed(etomica.Atom)
+    /**
+     * Assigns velocity to atom with components selected from a Maxwell-Boltzmann
+     * distribution with temperature most recently set for the action.  If atom 
+     * mass is infinite, assigns a zero velocity.
      */
     public void actionPerformed(Atom a) {
         Vector velocity = ((ICoordinateKinetic)a.coord).velocity();
         double mass = a.type.getMass();
+        if(Double.isInfinite(mass)) {
+            velocity.E(0.0);
+            return;
+        }
         int D = velocity.D();
         for(int i=0; i<D; i++) {
             velocity.setX(i,MaxwellBoltzmann.randomMomentumComponent(temperature,mass)/mass);
@@ -38,7 +46,7 @@ public class AtomActionRandomizeVelocity extends AtomActionAdapter {
     
     /**
      * @return Returns the Boltzmann-distribution temperature used to sample
-     * the new velocity.
+     * the new velocity.  Default value is that from Default class.
      */
     public double getTemperature() {
         return temperature;
@@ -49,6 +57,7 @@ public class AtomActionRandomizeVelocity extends AtomActionAdapter {
     public void setTemperature(double temperature) {
         this.temperature = temperature;
     }
+    
     private double temperature;
 
 }
