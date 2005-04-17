ff591223e56c74e609ff067454496a0e4c60a925
David Kofke <kofke@buffalo.edu>
2005-04-17 22:16:42 +0000

    add comments

--- etomica/nbratom/CriterionAdapter.java
+++ etomica/nbratom/CriterionAdapter.java
@@ -1,71 +1,87 @@
 /*
  * Created on Mar 2, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package etomica.nbratom;
 
 import etomica.Atom;
+import etomica.AtomPair;
 import etomica.Phase;
 import etomica.nbr.NeighborCriterion;
 
 /**
+ * Wraps another criterion while adding additional criteria to the acceptance
+ * decision. Used to introduce criteria related to the species, molecule, and
+ * other identifiers of the pair. Does not normally consider atom positions, but
+ * defers this to the wrapped iterator. Consequently the methods needUpdate,
+ * unsafe, and others that related to the atom configurations are simply passed
+ * on to the wrapped criterion.
+ * 
  * @author andrew
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public abstract class CriterionAdapter extends NeighborCriterion {
 
+    /**
+     * Constructs criterion that wraps the given criterion.
+     * 
+     * @param criterion
+     */
     public CriterionAdapter(NeighborCriterion criterion) {
         subCriterion = criterion;
     }
-    
+
     /**
-     * Indicates whether the neighbor list for the given atom should
-     * be updated, according to this criterion.  
+     * Implementation of this method should introduce new criterion and return
+     * true if pair meets this criterion and that of the wrapped
+     * NeighborCriterion.
+     */
+    public abstract boolean accept(AtomPair pair);
+
+    /**
+     * Indicates whether the neighbor list for the given atom should be updated,
+     * according to the wrapped criterion.
+     * 
      * @return true if the atom's list should be updated.
      */
     public boolean needUpdate(Atom atom) {
         return subCriterion.needUpdate(atom);
     }
-    
+
     /**
-     * Specifies the phase where the criterion is being applied.  Sometimes
-     * needed if the criterion depends on features of the phase, such as the
-     * volume.
+     * Specifies to the wrapped criterion the phase where the criterion is being
+     * applied. Sometimes needed if the criterion depends on features of the
+     * phase, such as the volume.
      */
     public void setPhase(Phase phase) {
         subCriterion.setPhase(phase);
     }
-    
+
     /**
-     * Indicates whether the atom has changed (e.g. moved) by an amount 
-     * that might have caused its neighbor list to be invalid.  If this
-     * method returns true, a neighbor list failure may have introduced
+     * Indicates whether the atom has changed (e.g. moved) by an amount that
+     * might have caused its wrapped-criterion neighbor list to be invalid. If
+     * this method returns true, a neighbor list failure may have introduced
      * errors in the calculation.
      */
     public boolean unsafe() {
         return subCriterion.unsafe();
     }
-    
+
     /**
-     * Indicates to criterion that given atom's neighbor list has just been 
-     * updated, and that properties (e.g., record of atom's position) 
-     * used by needUpdate and unsafe() methods should be reset.  
+     * Indicates to wrapped criterion that given atom's neighbor list has just
+     * been updated, and that properties (e.g., record of atom's position) used
+     * by needUpdate and unsafe() methods should be reset.
      */
     public void reset(Atom atom) {
         subCriterion.reset(atom);
     }
-    
+
     /**
      * Returns the nominal distance within which two atoms are considered
-     * neighbors.
+     * neighbors, according to the wrapped criterion.
      */
     public double getNeighborRange() {
-        return subCriterion.getNeighborRange(); 
+        return subCriterion.getNeighborRange();
     }
 
-    protected NeighborCriterion subCriterion;
+    protected final NeighborCriterion subCriterion;
 }
