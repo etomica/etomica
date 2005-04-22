7960ca44ee496d32570ef32680faa9276ed65e6d
David Kofke <kofke@buffalo.edu>
2005-04-22 22:37:09 +0000

    interface instead of a class

--- etomica/modifier/ModifierBoolean.java
+++ etomica/modifier/ModifierBoolean.java
@@ -4,9 +4,9 @@ package etomica.modifier;
  * A ModifierBoolean object permits changes to be made to a boolean property of another object.
  */
 
-public abstract class ModifierBoolean implements java.io.Serializable {
+public interface ModifierBoolean {
     
-    public abstract void setBoolean(boolean b);
-    public abstract boolean getBoolean();
+    public void setBoolean(boolean b);
+    public boolean getBoolean();
 
 }
