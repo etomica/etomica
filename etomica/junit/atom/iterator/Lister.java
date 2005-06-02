a7807e149afa541a806eb8c72257ee429750ce3d
David Kofke <kofke@buffalo.edu>
2005-06-02 04:27:08 +0000

    ctrl-shift-F reformat new convenience methods to add multiple atoms to lister list

--- etomica/junit/atom/iterator/Lister.java
+++ etomica/junit/atom/iterator/Lister.java
@@ -7,14 +7,17 @@ import etomica.Atom;
 import etomica.AtomSet;
 import etomica.action.AtomAction;
 import etomica.action.AtomsetActionAdapter;
+import etomica.atom.AtomList;
+import etomica.atom.iterator.AtomIteratorListSimple;
 
 /**
+ * Class used to test iterators by collecting a list of strings
+ * given by the toString method of the atoms (or atom sets) produced
+ * by the iterator.
+ * 
  * @author aawalker
- *
  */
 class Lister extends AtomsetActionAdapter implements AtomAction {
-// Below is original line, from kmb2 project as of 12/22/04 work
-//class Lister implements AtomsetActive, AtomActive {	
 
 	public final LinkedList list;
 	public AtomSet atoms;
@@ -22,63 +25,51 @@ class Lister extends AtomsetActionAdapter implements AtomAction {
 	public Lister() {
 		list = new LinkedList();
 	}
+    
+    public void clear() {
+        list.clear();
+    }
 
-	/**
-	 * @see etomica.AtomActive#actionPerformed(etomica.Atom)
-	 */
 	public void actionPerformed(Atom atom) {
 		list.add(atom.toString());
 	}
 
-	/**
-	 * @see etomica.AtomActive#actionPerformed(etomica.Atom)
-	 */
-/*	public void actionPerformed(Atom[] atom) {
-//		for(int i=0; i<atom.length; i++) actionPerformed(atom[i]);
-//		String newString = "[";
-		String newString = "";
-		System.out.println("atom.length is: "+atom.length); // added 1/7/05 to debug Atom
-		for(int i=0; i<atom.length-1; i++) newString += atom[i].toString()+",";
-		if(atom.length > 0) newString += atom[atom.length-1].toString();
-//		newString += "]";
-		list.add(newString);
-	}
-*/
-	/**
-	 * Gives an array of listers each with their own list.
-	 * 
-	 * @param n the number of listers in the array
-	 * @return AtomLister[]
-	 */	
-	public static Lister[] listerArray(int n) {
-		Lister[] lister = new Lister[n];
-		for (int i=0;i<n;i++) {
-			lister[i] = new Lister();
-		}
-		return lister;
-	}
-
 	public void setAtom(Atom atom) {
-//		setAtoms(new Atom[] {atom}); // could use this structure to wrap the atom during actionperformed; use with arrays only, not single atom in IteratorTest?
 		setAtoms(atoms); //AtomSet={atoms});
-		}
+	}
 
 	public Atom getAtom() {
 		return (atoms == null || atoms.count() < 1) ? null : atoms.getAtom(0);
 	}
 
-	/* New actionPerformed method based on AtomSet rather than Atom[] as argument.
+	/**
+     * Adds atomSet.toString() to list.
 	 * kmb 4/27/05
 	 */
-	public void actionPerformed(AtomSet a) {
-//		String newString = "";
-//		if(UnitTest.VERBOSE) System.out.println("atom.length is: " + a.count());
-//		for(int i=0; i<a.count()-1; i++) newString += a.getAtom(i).toString()+",";
-//		if(a.count() > 0) newString += a.getAtom(a.count()-1).toString();
-//		list.add(newString);
-        list.add(a.toString());
+	public void actionPerformed(AtomSet atomSet) {
+        list.add(atomSet.toString());
 	}
+    
+    /**
+     * Performs action on each element of array.
+     */
+    public void addEachToList(AtomSet[] a) {
+        for(int i=0; i<a.length; i++) {
+            actionPerformed(a[i]);
+        }
+    }
 
+    /**
+     * Performs action of each element of array.
+     */
+    public void addEachToList(AtomList list) {
+        AtomIteratorListSimple iterator = new AtomIteratorListSimple(list);
+        iterator.reset();
+        while(iterator.hasNext()) {
+            actionPerformed(iterator.nextAtom());
+        }
+    }
+    
 	public String toString() {
 	    Iterator iter = list.iterator();
         String string = "{";
@@ -89,5 +80,20 @@ class Lister extends AtomsetActionAdapter implements AtomAction {
         string += "}";
         return string;
     }
+
+    /**
+     * Gives an array of listers each with their own list.
+     * 
+     * @param n the number of listers in the array
+     * @return AtomLister[]
+     */ 
+    public static Lister[] listerArray(int n) {
+        Lister[] lister = new Lister[n];
+        for (int i=0;i<n;i++) {
+            lister[i] = new Lister();
+        }
+        return lister;
+    }
+
 
 }
