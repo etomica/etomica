9049ab6f6cd965749e15c4b50500edffc2b22cc0
Andrew Schultz <ajs42@buffalo.edu>
2005-06-22 04:55:51 +0000

    update for Data changes

--- etomica/log/DataLogger.java
+++ etomica/log/DataLogger.java
@@ -3,7 +3,11 @@ package etomica.log;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import etomica.Data;
 import etomica.DataSink;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataGroup;
 import etomica.units.Dimension;
 
 
@@ -26,16 +30,49 @@ public class DataLogger implements DataSink {
         }
 	}
 
-	public void putData(double[] values) {
+	public void putData(Data data) {
         try { 
             FileWriter fileWriter = new FileWriter(fileName,true);
-    		for(int j=0; j<values.length; j++) {fileWriter.write(values[j]+" ");}
+            if (data instanceof DataGroup) {
+                for(int i=0; i<((DataGroup)data).getNData(); i++) {
+                    putData(fileWriter,((DataGroup)data).getData(i));
+                }
+            }
+            if (data instanceof DataDoubleArray) {
+                double[] values = ((DataDoubleArray)data).getData();
+                for(int j=0; j<values.length; j++) {fileWriter.write(values[j]+" ");}
+            }
+            else if (data instanceof DataDouble) {
+                fileWriter.write(Double.toString(((DataDouble)data).x));
+            }
     		fileWriter.write("\n");
             fileWriter.close();
         }catch(IOException e) {
             System.err.println("Cannot writing to "+fileName+", caught IOException: " + e.getMessage());
         }
 	}
+    
+    /**
+     * @exception IOException
+     * @param fileWriter
+     * @param data
+     */
+    private void putData(FileWriter fileWriter, Data data) throws IOException {
+        if (data instanceof DataGroup) {
+            for(int i=0; i<((DataGroup)data).getNData(); i++) {
+                putData(fileWriter,((DataGroup)data).getData(i));
+            }
+        }
+        if (data instanceof DataDoubleArray) {
+            double[] values = ((DataDoubleArray)data).getData();
+            for(int j=0; j<values.length; j++) {fileWriter.write(values[j]+" ");}
+        }
+        else if (data instanceof DataDouble) {
+            fileWriter.write(Double.toString(((DataDouble)data).x));
+        }
+        fileWriter.write("\n");
+        fileWriter.close();
+    }        
     
     public void setDimension(Dimension dimension) {
         this.dimension = dimension;
