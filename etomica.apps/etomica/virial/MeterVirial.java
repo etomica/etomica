4a6fe7ca9966ee32d6760f08cdef0f546020e340
Andrew Schultz <ajs42@buffalo.edu>
2005-06-22 01:06:52 +0000

    update for Data changes

--- etomica.apps/etomica/virial/MeterVirial.java
+++ etomica.apps/etomica/virial/MeterVirial.java
@@ -1,15 +1,20 @@
 package etomica.virial;
 
+import etomica.Data;
+import etomica.DataInfo;
+import etomica.DataSource;
+import etomica.Meter;
 import etomica.Phase;
-import etomica.data.meter.MeterArray;
+import etomica.data.types.DataDoubleArray;
 import etomica.units.Dimension;
+import etomica.utility.NameMaker;
 
 /**
  * Measures value of clusters in a phase and returns the values
  * divided by the sampling bias of the integrator.
  */
 
-public class MeterVirial extends MeterArray {
+public class MeterVirial implements DataSource, Meter {
 
 	protected final ClusterAbstract clusters[];
 	double beta;
@@ -20,36 +25,37 @@ public class MeterVirial extends MeterArray {
 	 * Constructor for MeterVirial.
 	 */
 	public MeterVirial(ClusterAbstract[] aClusters, IntegratorClusterMC aIntegrator, double temperature) {
+        setName(NameMaker.makeName(this.getClass()));
 		integrator = aIntegrator;
 		clusters = aClusters;
 		setTemperature(temperature);
-		setNDataPerPhase(clusters.length);
+        data = new DataDoubleArray(new DataInfo("Cluster Value",Dimension.NULL));
+		data.setLength(clusters.length);
         weightFactor = 1.0;
 	}
 
-    public void setPhase(Phase[] p) {
-        if (p.length != 1) throw new IllegalArgumentException("MeterVirial can only handle 1 phase");
-        super.setPhase(p);
-        setSampleCluster(((PhaseCluster)p[0]).getSampleCluster());
+	public DataInfo getDataInfo() {
+        return data.getDataInfo();
     }
-
+    
     /**
      * Sets the cluster used by the integrator to sample phase space.
      */
     public void setSampleCluster(ClusterWeight sampleCluster) {
-        CoordinatePairSet cPairSet = ((PhaseCluster)phase[0]).getCPairSet();
-        AtomPairSet aPairSet = ((PhaseCluster)phase[0]).getAPairSet();
+        CoordinatePairSet cPairSet = phase.getCPairSet();
+        AtomPairSet aPairSet = phase.getAPairSet();
         weightFactor = sampleCluster.value(cPairSet,aPairSet,beta) / integrator.getWeight();
     }
     
-	public double[] getDataAsArray(Phase p) {
-		CoordinatePairSet cPairSet = ((PhaseCluster)p).getCPairSet();
-        AtomPairSet aPairSet = ((PhaseCluster)p).getAPairSet();
+	public Data getData() {
+		CoordinatePairSet cPairSet = phase.getCPairSet();
+        AtomPairSet aPairSet = phase.getAPairSet();
 		double w = weightFactor * integrator.getWeight();
+        double[] x = data.getData();
 		for (int i=0; i<clusters.length; i++) {
-			phaseData[i] = clusters[i].value(cPairSet,aPairSet,beta)/w;
+			x[i] = clusters[i].value(cPairSet,aPairSet,beta)/w;
 		}
-		return phaseData;
+		return data;
 	}
 
 	public Dimension getDimension() {
@@ -64,4 +70,28 @@ public class MeterVirial extends MeterArray {
 		beta = 1.0/temperature;
 	}
 
+    /**
+     * @return Returns the phase.
+     */
+    public Phase getPhase() {
+        return phase;
+    }
+    /**
+     * @param phase The phase to set.
+     */
+    public void setPhase(Phase phase) {
+        setSampleCluster(((PhaseCluster)phase).getSampleCluster());
+        this.phase = (PhaseCluster)phase;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+    private PhaseCluster phase;
+	private final DataDoubleArray data;
 }
