782ef1a3f95eada90f2197e93c11d83e728abf26
David Kofke <kofke@buffalo.edu>
2005-07-03 23:49:56 +0000

    correct comment

--- etomica/tests/TestSWChain.java
+++ etomica/tests/TestSWChain.java
@@ -35,7 +35,7 @@ import etomica.space3d.Space3D;
 
 /**
  * Simple square-well chain simulation.
- * Initial configurations at http://gordon.eng.buffalo.edu/etomica/tests/
+ * Initial configurations at http://rheneas.eng.buffalo.edu/etomica/tests/
  */
  
 public class TestSWChain extends Simulation {
