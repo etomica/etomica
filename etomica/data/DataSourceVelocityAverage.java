27b627ebf7baeac3e1303414521dda1832baeb8d
David Kofke <kofke@buffalo.edu>
2005-07-05 19:22:35 +0000

    re-define so that it calculates the mass-average velocity; fix comments, cleanup

--- etomica/data/DataSourceVelocityAverage.java
+++ etomica/data/DataSourceVelocityAverage.java
@@ -1,6 +1,7 @@
 package etomica.data;
 
 import etomica.Atom;
+import etomica.AtomTypeLeaf;
 import etomica.Data;
 import etomica.DataInfo;
 import etomica.DataSource;
@@ -13,15 +14,12 @@ import etomica.space.Vector;
 import etomica.units.Dimension;
 
 /**
- * Calculates the center of mass (COM) over a set of atoms. The mass and
- * position of all atoms passed to the actionPerformed are accumulated and used
- * to compute their center of mass. Calculated COM is obtained via the getData
- * method, which returns an array with the elements of the calculated COM
- * vector, or via the getCOM method, which returns a vector with the calculated
- * COM. Calculation is zeroed via the reset method. <br>
- * A typical use of this class would have it passed to the allAtoms method of an
- * iterator, or wrapped in an AtomGroupAction to calculate the COM of the atoms
- * in an atom group.
+ * Calculates the mass average velocity over a set of atoms. The velocity
+ * and mass of all atoms passed to the actionPerformed are accumulated and used
+ * to compute the mass-average velocity (total momentum divided by the total mass).
+ * Calculated average is obtained via the getData
+ * method, which returns a DataVector with the average.
+ * Calculation is zeroed via the reset method. <br>
  * 
  * @author David Kofke
  */
@@ -32,7 +30,6 @@ import etomica.units.Dimension;
 public class DataSourceVelocityAverage extends AtomActionAdapter implements DataSource {
 
     public DataSourceVelocityAverage(Space space) {
-        this.space = space;
         data = new DataVector(space, new DataInfo("Average Velocity", Dimension.UNDEFINED));
         vectorSum = space.makeVector();
         groupWrapper = new AtomGroupAction(new MyAction());
@@ -53,16 +50,16 @@ public class DataSourceVelocityAverage extends AtomActionAdapter implements Data
 
     /**
      * Returns the center of mass (calculated so far) as an array.
-     * Does not reset COM sums.  Implementation of DataSource interface.
+     * Does not reset sums.  Implementation of DataSource interface.
      */
     public Data getData() {
-        data.x.Ea1Tv1(1.0 / numAtoms, vectorSum);
+        data.x.Ea1Tv1(1.0 / massSum, vectorSum);
         return data;
     }
     
     /**
-     * Returns the center of mass (calculated so far) as a vector.
-     * Does not reset COM sums.
+     * Returns the average velocity (calculated so far) as a vector.
+     * Does not reset sums.
      */
     public Vector getVelocityAverage() {
         getData();
@@ -75,20 +72,18 @@ public class DataSourceVelocityAverage extends AtomActionAdapter implements Data
      */
     public void reset() {
         vectorSum.E(0.0);
-        numAtoms = 0;
+        massSum = 0;
     }
 
     private class MyAction extends AtomActionAdapter {
         public void actionPerformed(Atom a) {
             vectorSum.PE(((ICoordinateKinetic)a.coord).velocity());
-            numAtoms++;
+            massSum += ((AtomTypeLeaf)a.type).getMass();
         }
     }
 
-    private final Space space;
     private final Vector vectorSum;
     private final DataVector data;
     private double massSum;
-    private int numAtoms;
     private AtomGroupAction groupWrapper;
 }
