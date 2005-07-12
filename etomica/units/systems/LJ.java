eb900efffc232b25df9167621558e1507b44926a
David Kofke <kofke@buffalo.edu>
2005-07-12 15:53:24 +0000

    make inner classes static

--- etomica/units/systems/LJ.java
+++ etomica/units/systems/LJ.java
@@ -27,17 +27,17 @@ public class LJ extends UnitSystem implements java.io.Serializable {
     private double epsilon = Default.POTENTIAL_WELL;
     private double mass = Default.ATOM_MASS;
     
-    private Mass massUnit = new Mass();
-    private Length lengthUnit = new Length();
-    private Time timeUnit = new Time();
-    private Charge chargeUnit = new Charge();
-    private Dipole dipoleUnit = new Dipole();
-    private Energy energyUnit = new Energy();
-    private Temperature temperatureUnit = new Temperature();
-    private Pressure2D pressure2DUnit = new Pressure2D();
-    private Pressure pressureUnit = new Pressure();
-    private Volume2D volume2DUnit = new Volume2D();
-    private Volume volumeUnit = new Volume();
+    private Mass massUnit = new Mass(sigma, epsilon, mass);
+    private Length lengthUnit = new Length(sigma, epsilon, mass);
+    private Time timeUnit = new Time(sigma, epsilon, mass);
+    private Charge chargeUnit = new Charge(sigma, epsilon, mass);
+    private Dipole dipoleUnit = new Dipole(sigma, epsilon, mass);
+    private Energy energyUnit = new Energy(sigma, epsilon, mass);
+    private Temperature temperatureUnit = new Temperature(sigma, epsilon, mass);
+    private Pressure2D pressure2DUnit = new Pressure2D(sigma, epsilon, mass);
+    private Pressure pressureUnit = new Pressure(sigma, epsilon, mass);
+    private Volume2D volume2DUnit = new Volume2D(sigma, epsilon, mass);
+    private Volume volumeUnit = new Volume(sigma, epsilon, mass);
         
     public double getSigma() {return sigma;}
     public void setSigma(double s) {sigma = s; update();}
@@ -61,129 +61,129 @@ public class LJ extends UnitSystem implements java.io.Serializable {
 
         
     private void update() {
-        massUnit.updateConversions();
-        lengthUnit.updateConversions();
-        timeUnit.updateConversions();
-        chargeUnit.updateConversions();
-        dipoleUnit.updateConversions();
-        energyUnit.updateConversions();
-        temperatureUnit.updateConversions();
-        pressureUnit.updateConversions();
-        pressure2DUnit.updateConversions();
-        volumeUnit.updateConversions();
-        volume2DUnit.updateConversions();
+        massUnit.updateConversions(sigma, epsilon, mass);
+        lengthUnit.updateConversions(sigma, epsilon, mass);
+        timeUnit.updateConversions(sigma, epsilon, mass);
+        chargeUnit.updateConversions(sigma, epsilon, mass);
+        dipoleUnit.updateConversions(sigma, epsilon, mass);
+        energyUnit.updateConversions(sigma, epsilon, mass);
+        temperatureUnit.updateConversions(sigma, epsilon, mass);
+        pressureUnit.updateConversions(sigma, epsilon, mass);
+        pressure2DUnit.updateConversions(sigma, epsilon, mass);
+        volumeUnit.updateConversions(sigma, epsilon, mass);
+        volume2DUnit.updateConversions(sigma, epsilon, mass);
     }
     
     //  \u03B5 is unicode for epsilon
     //  \u03C3 is unicode for sigma
     //  \u00BD is unicode for "1/2"
     
-    public final class Mass extends BaseUnit.Mass {
-        private Mass() {
+    public static final class Mass extends BaseUnit.Mass {
+        private Mass(double sigma, double epsilon, double mass) {
         	super(1.0, "LJ units", "m", Prefix.NOT_ALLOWED);
-        	updateConversions();
+        	updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
             setToSimConversionFactor(mass); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Length extends BaseUnit.Length {
-        private Length() {
+    public static final class Length extends BaseUnit.Length {
+        private Length(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03C3", Prefix.NOT_ALLOWED);
-			updateConversions();
+			updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(sigma); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Time extends BaseUnit.Time {
-        private Time() {
+    public static final class Time extends BaseUnit.Time {
+        private Time(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "(\u03B5m\u03C3^2)^\u00BD", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(1.0/Math.sqrt(epsilon * mass * sigma*sigma)); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Charge extends BaseUnit.Charge {
-        private Charge() {
+    public static final class Charge extends BaseUnit.Charge {
+        private Charge(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "(\u03B5\u03C3)^(-\u00BD)", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(Math.sqrt(epsilon * sigma)); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Dipole extends BaseUnit.Dipole {
-        private Dipole() {
+    public static final class Dipole extends BaseUnit.Dipole {
+        private Dipole(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "(\u03B5\u03C3^3)^(-\u00BD)", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(Math.sqrt(epsilon * sigma*sigma*sigma)); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Energy extends BaseUnit.Energy {
-        private Energy() {
+    public static final class Energy extends BaseUnit.Energy {
+        private Energy(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03B5", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(epsilon); //conversion from LJ unit to simulation unit
         }
     }
 
 
-    public final class Temperature extends BaseUnit.Temperature {
-        private Temperature() {
+    public static final class Temperature extends BaseUnit.Temperature {
+        private Temperature(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03B5", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(epsilon); //conversion from LJ unit to simulation unit
         }
     }
-    public final class Pressure extends BaseUnit.Pressure {
-        private Pressure() {
+    public static final class Pressure extends BaseUnit.Pressure {
+        private Pressure(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03B5/\u03C3^3", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(epsilon/(sigma*sigma*sigma)); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Pressure2D extends BaseUnit.Pressure2D {
-        private Pressure2D() {
+    public static final class Pressure2D extends BaseUnit.Pressure2D {
+        private Pressure2D(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03B5/\u03C3^2", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(epsilon/(sigma*sigma)); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Volume extends BaseUnit.Volume {
-        private Volume() {
+    public static final class Volume extends BaseUnit.Volume {
+        private Volume(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03C3^3", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(sigma*sigma*sigma); //conversion from LJ unit to simulation unit
         }
     }
 
-    public final class Volume2D extends BaseUnit.Volume2D {
-        private Volume2D() {
+    public static final class Volume2D extends BaseUnit.Volume2D {
+        private Volume2D(double sigma, double epsilon, double mass) {
 			super(1.0, "LJ units", "\u03C3^2", Prefix.NOT_ALLOWED);
-            updateConversions();
+            updateConversions(sigma, epsilon, mass);
         }
-        private void updateConversions() {
+        private void updateConversions(double sigma, double epsilon, double mass) {
 			setToSimConversionFactor(sigma*sigma); //conversion from LJ unit to simulation unit
         }
     }
