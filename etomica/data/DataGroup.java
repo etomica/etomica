28bf8c449c76097683992e6d5ea79545bf3bd76d
David Kofke <kofke@buffalo.edu>
2005-07-12 16:26:51 +0000

    complete comments

--- etomica/data/DataGroup.java
+++ etomica/data/DataGroup.java
@@ -8,8 +8,22 @@ import etomica.units.Dimension;
 
 
 /**
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Gathers one or more Data instances into a single Data object.  This has
+ * several uses
+ * <ul>
+ * <li>enables a data handler to take a single data stream and emit multiple
+ * different streams from it.  An example is given by the accumulators, which
+ * take data an compile one or more new sets of data from it.
+ * <li>enables data that are naturally related to be handled as a single data
+ * object.  For example, data that describes x-y pair values for a function y(x)
+ * can be moved around together.
+ * </ul>
+ * Data sinks typically need to manipulate data groups to extract the individual
+ * data elements in them.
+ * <p>
+ * The DataInfo instance for the DataGroup has a label that describes the collection,
+ * and a dimension that is the dimension of all of the data objects it holds (if they
+ * are all the same) or UNDEFINED (if they are not).
  *
  * @author David Kofke
  *
@@ -21,6 +35,14 @@ import etomica.units.Dimension;
  */
 public class DataGroup extends Data {
 
+    /**
+     * Forms a data group from the given array of data objects.  Given data
+     * array is cloned, but the data instances it holds are kept by this DataGroup.
+     * 
+     * @param label a descriptive label for the data collection
+     * @param dimension physical dimensions of the data, or UNDEFINED if they are not of the same physical dimensions
+     * @param data array of data to be encapsulated in this group
+     */
     public DataGroup(String label, Dimension dimension, Data[] data) {
         super(new DataInfo(label, dimension, getFactory(data)));
         this.data = (Data[])data.clone();
@@ -51,20 +73,42 @@ public class DataGroup extends Data {
     }
 
 
+    /**
+     * Applies the E method to all data elements held, in a one-to-one
+     * correspondence with the elements in the given data group. 
+     * 
+     * @throws ClassCastException if the given data is not an instance of DataGroup.
+     * @throws IllegalArgumentException if the given DataGroup has a different number of data elements than this DataGroup.
+     * 
+     */
     public void E(Data data) {
+        if(((DataGroup)data).data.length != this.data.length) {
+            throw new IllegalArgumentException("Attempt to copy data groups of different length: (this.length, argument's length): ("+this.data.length+", "+((DataGroup)data).data.length+")");
+        }
         for (int i=0; i<this.data.length; i++) {
             this.data[i].E(((DataGroup)data).getData(i));
         }
     }
 
+    /**
+     * Returns the i-th data element in the group, counting from 0.
+     *
+     * @throws ArrayIndexOutOfBounds exception if the given value does not reference a legitimate element
+     */
     public Data getData(int i) {
         return data[i];
     }
     
+    /**
+     * Returns the number of data objects encapsulated by this DataGroup.
+     */
     public int getNData() {
         return data.length;
     }
-    
+
+    /**
+     * Returns a string formed from the encapsulated data objects.
+     */
     public String toString() {
         StringBuffer string = new StringBuffer(dataInfo.getLabel());
         for(int i=0; i<data.length; i++) {
@@ -76,6 +120,13 @@ public class DataGroup extends Data {
     
     final Data[] data;
     
+    /**
+     * Returns a factory for a data group holding copies of the
+     * given data objects.  Each new instance made by the factory
+     * will be a DataGroup having its owns copy of the given data.
+     * When the factory is made, the given Data array is cloned, 
+     * but the data instances it holds are not.
+     */
     public static DataFactory getFactory(Data[] data) {
         return new Factory(data);
     }
