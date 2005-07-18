86578dcc53d2cb350ad69c4dea7411de876fec11
Andrew Schultz <ajs42@buffalo.edu>
2005-07-18 22:12:36 +0000

    remove agentList from Species (Phase handles it now) actually use Default.MOLECULE_COUNT

--- etomica/SpeciesSpheres.java
+++ etomica/SpeciesSpheres.java
@@ -1,5 +1,5 @@
 package etomica;
-import etomica.action.AtomActionAdapter;
+
 import etomica.atom.AtomFactoryHomo;
 import etomica.atom.AtomFactoryMono;
 import etomica.atom.AtomSequencerFactory;
@@ -89,9 +89,6 @@ public class SpeciesSpheres extends Species implements EtomicaElement {
     public void setAtomsPerMolecule(final int n) {
         if(n == getAtomsPerMolecule()) return;
         ((AtomFactoryHomo)factory).setAtomsPerGroup(n);
-        allAgents(new AtomActionAdapter() {public void actionPerformed(Atom a) {
-            SpeciesAgent agent = (SpeciesAgent)a;
-            agent.setNMolecules(agent.getNMolecules());}});
     }
     /**
      * @return the number of spheres in each molecule made by this species.
@@ -99,51 +96,6 @@ public class SpeciesSpheres extends Species implements EtomicaElement {
     public int getAtomsPerMolecule() {
         return ((AtomFactoryHomo)factory).getAtomsPerGroup();
     }
-
-    /**
-     * Demonstrates how this class is implemented.
-     */
-/*    public static void main(String[] args) {
-	    IntegratorHard integratorHard1 = new IntegratorHard();
-//	    integratorHard1.setTimeStep(0.02);
-	    SpeciesSpheres speciesSpheres1 = new SpeciesSpheres(10,6);//10 molecules, 3 atoms per molecule
-	    SpeciesSpheres speciesSpheres2 = new SpeciesSpheres(3);
-	//    speciesSpheres2.setColor(java.awt.Color.red);
-	    final Phase phase = new Phase();
-	    P2HardSphere potential = new P2HardSphere();
-	    P2HardSphere potential2 = new P2HardSphere();
-	    
-	    //intermolecular potential
-	    Potential2Group potential0 = new Potential2Group();
-	    P2HardSphere p0Inter = new P2HardSphere(potential0);
-	    
-	    //intramolecular potential
-	    Potential1Group potential3 = new Potential1Group();
-	    P2Tether p2Tether = new P2Tether(potential3);
-	    p2Tether.setIterator(new AtomPairIterator(Simulation.instance.space,
-	            new AtomIteratorSequential(false),
-	            new AtomIteratorBonds()));
-	    
-	    Controller controller1 = new Controller();
-	    DisplayPhase displayPhase1 = new DisplayPhase();
-	    IntegratorMD.Timer timer = integratorHard1.new Timer(integratorHard1.chronoMeter());
-	    timer.setUpdateInterval(10);
-	    MeterEnergy meterEnergy = new MeterEnergy();
-	    meterEnergy.setPhase(phase);
-	    DisplayBox displayEnergy = new DisplayBox();
-	    displayEnergy.setMeter(meterEnergy);
-		Simulation.instance.panel().setBackground(java.awt.Color.yellow);
-		Simulation.instance.elementCoordinator.go(); //invoke this method only after all elements are in place
-		                                    //calling it a second time has no effect
-		                                    
-		potential.setSpecies(speciesSpheres1, speciesSpheres2);
-        potential2.setSpecies(speciesSpheres2, speciesSpheres2);
-        potential0.setSpecies(speciesSpheres1, speciesSpheres1); 
-        potential3.setSpecies(speciesSpheres1);
-	//    displayPhase1.setColorScheme(integratorHard1.new HighlightColliders());
-	    Simulation.makeAndDisplayFrame(Simulation.instance);
-	}//end of main
-*/
 }
 
 
