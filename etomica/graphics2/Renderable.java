c9c36d7becf4c12f016d1894f0ea11eb1e7ddf20
Henrique Bucher <hbucher@gmail.com,>
2005-07-29 23:09:22 +0000

    Revamped the whole interface to exploit Java's OO capabilities Much cleaner and intuitive now

--- etomica/graphics2/Renderable.java
+++ etomica/graphics2/Renderable.java
@@ -1,24 +1,30 @@
 package etomica.graphics2;
 
+import etomica.space.Vector;
+
 /** Interface used by SceneManager to communicate with the graphics package. 
  *  Should be implemented by all classes willing to draw the contents of etomica. */
 
 public interface Renderable
 {
-	// SHAPE TYPES
-	public final int ELLIPSE = 1;
+
+	public interface Shape 
+	{
+		public void setPosition( Vector pos );
+		public void setScale( Vector scale );
+		public void setRotation( Vector fromvec, Vector tovec );
+		public void setRotation( float angle, Vector axis );
+		public void setColor( int color_index );
+		public void setColorScheme( ColorScheme scheme );
+	};
+	public interface Sphere extends Shape
+	{
+	};
+	public interface Polyline extends Shape
+	{
+		public void appendLine( Vector pointA, Vector pointB );
+	};
+	public Sphere createSphere();
+	public Polyline createPoly();
 	
-	// PROPERTY IDS
-	public final int ELLIPSE_RADIUS = 1001; // float=>sphere, float[3]=>ellipse
-	
-	public int  createObject( int type );
-	public void setObjectPosition( int index, float x, float y, float z );
-	public void setObjectColor( int index, int color_index );
-	public void setObjectProperty( int index, int prop_type, float value );
-	public void setObjectProperty( int index, int prop_type, float[] value );
-	public void setObjectProperty( int index, int prop_type, Object value );
-	
-	public void setColorScheme( ColorScheme scheme );
-	
-	public float[] getCameraPosition();
 };
