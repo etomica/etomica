cfa11c3b38a42161af457fddfb7f40fd187508c0
David Kofke <kofke@buffalo.edu>
2005-07-30 02:27:53 +0000

    commenting improvements

--- etomica/DataSource.java
+++ etomica/DataSource.java
@@ -1,8 +1,11 @@
 package etomica;
 
 /**
- * Interface for an object that can provide data 
- * (in the form of an array of doubles) for plotting or other analysis.
+ * Interface for an object that can provide Data objects
+ * on request.  Normally a DataSource heads a DataStream that processes
+ * and/or records the data as it passes through different segments.  
+ * Data is pulled from the DataSource by a DataPump and pushed down the
+ * data stream.
  */
  
 public interface DataSource {
@@ -11,7 +14,13 @@ public interface DataSource {
      * @return the data given by this source
      */
 	public Data getData();
-    
+
+    /**
+     * Returns the DataInfo instance that will be held by Data
+     * given by this source.  This information is useful for
+     * setting up the data stream and for providing annotation when
+     * displaying or writing the Data.
+     */
     public DataInfo getDataInfo();
     
-}//end of DataSource
+}
