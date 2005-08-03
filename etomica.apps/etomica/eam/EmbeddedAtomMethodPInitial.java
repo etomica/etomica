becaa6b5703230261170963db5d7cf4329ecd15d
Kate Shaul <schadel2@buffalo.edu>
2005-08-03 15:34:26 +0000

    Potential used to initialize the allatomAgents array for Embedded-Atom Method potential calculation

---
+++ etomica.apps/etomica/eam/EmbeddedAtomMethodPInitial.java
@@ -0,0 +1,45 @@
+package etomica.potential;
+import etomica.Atom;
+import etomica.AtomSet;
+import etomica.Space;
+import etomica.potential.EmbeddedAtomMethodP2.Wrapper;
+import etomica.space.Vector;
+
+/**
+ * This class initializes the allatomAgents[EmbeddedAtomMethodP2.agentIndex] array.
+ * All atoms' energy values and gradients are set to 0 and (0,0,0), respectively.
+ * 
+ * EmbeddedAtomMethodPInitial is a one-body potential, and is used along with 
+ * EmbeddedAtomMethodP2 and EmbeddedAtomMethodPMany in the EAMMd3D simulation class.
+ * 
+ * This class was created by A. Schultz and K.R. Schadel July 2005.
+ */
+
+public final class EmbeddedAtomMethodPInitial extends P1SoftBoundary {
+
+	public EmbeddedAtomMethodPInitial(Space space) {
+		super(space);
+		gradient = space.makeVector();
+		gradient.E(0);
+    }
+	
+	public double energy(AtomSet a) {
+		((Wrapper)((Atom)a).allatomAgents[EmbeddedAtomMethodP2.agentIndex]).x = 0;
+		return 0;
+	}
+
+	public Vector gradient(AtomSet a) {
+		((Wrapper)((Atom)a).allatomAgents[EmbeddedAtomMethodP2.agentIndex]).x = 0;
+		((Wrapper)((Atom)a).allatomAgents[EmbeddedAtomMethodP2.agentIndex]).A.E(0);
+		return gradient;
+	}
+	
+	public double virial(AtomSet atoms) {
+	    return 0.0;
+    }
+   
+	Vector gradient;
+}
+
+    
+
