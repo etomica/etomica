9c6fc664555ec3f8ebfd0b7faaf2f327c8df15ec
Kate Shaul <schadel2@buffalo.edu>
2005-08-03 15:39:13 +0000

    Molecular-Dyamics simulation using the Embedded-Atom Method potential; appropriate for metallic and covalently-bonded solids

---
+++ etomica/simulations/EAMMd3D.java
@@ -0,0 +1,122 @@
+package etomica.simulations;
+import etomica.AtomTypeLeaf;
+import etomica.Configuration;
+import etomica.ConfigurationLattice;
+import etomica.Controller;
+import etomica.DataSink;
+import etomica.Phase;
+import etomica.Simulation;
+import etomica.Species;
+import etomica.SpeciesSpheresMono;
+import etomica.action.PhaseImposePbc;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.atom.AtomTypeSphere;
+import etomica.data.AccumulatorAverage;
+import etomica.data.DataPump;
+import etomica.data.meter.MeterEnergy;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.graphics.ColorSchemeByType;
+import etomica.graphics.DisplayPhase;
+import etomica.graphics.DisplayPlot;
+import etomica.graphics.SimulationGraphic;
+import etomica.integrator.IntegratorVelocityVerlet;
+import etomica.integrator.IntervalActionAdapter;
+import etomica.lattice.Crystal;
+import etomica.lattice.LatticeCrystal;
+import etomica.lattice.crystal.BasisBetaSnA5;
+import etomica.lattice.crystal.PrimitiveTetragonal;
+import etomica.potential.EmbeddedAtomMethodP2;
+import etomica.potential.EmbeddedAtomMethodPInitial;
+import etomica.potential.EmbeddedAtomMethodPMany;
+import etomica.potential.ParameterSetEAM;
+import etomica.space3d.Space3D;
+import etomica.units.ElectronVolt;
+
+/**
+ * Molecular-Dynamics Simulation Using the Embedded-Atom Method (EAM) Potential.  
+ * 
+ * The EAM potential is intended for use with metallic systems.
+ * 
+ * Because the EAM potential consists of a pair-wise term and a many-body term, it 
+ * was necessary to calculate these parts of the potential separately.  The many-body
+ * term (the embedding energy function) consists of pair-wise sums multiplied together.
+ * These pair-wise terms are calculated in the same class as the pair-wise potential.
+ * Both are calculated using the infrastructure already in place for looping through pairs.
+ * An allatomAgents array is used to hold the pair-wise terms for the many-body 
+ * potential so that the many-body potential may be calculated by referring only to 
+ * each atom's information in the array (so that the many-body potential part may be
+ * calculated as a one-body potential).
+ * 
+ * This class was adapted from LjMd3D.java by K.R. Schadel and A. Schultz July 2005.
+ */
+ 
+public class EAMMd3D extends Simulation {
+    
+    public IntegratorVelocityVerlet integrator;
+    public SpeciesSpheresMono species;
+    public Phase phase;
+    public EmbeddedAtomMethodPInitial potential0;
+    public EmbeddedAtomMethodP2 potentialA;
+    public EmbeddedAtomMethodPMany potentialB;
+    public Controller controller;
+    public DisplayPhase display;
+    public DisplayPlot plot;
+    public MeterEnergy energy;
+    public ActivityIntegrate activityIntegrate;
+
+    public static void main(String[] args) {
+    	EAMMd3D sim = new EAMMd3D();
+    	MeterPotentialEnergy energyMeter = new MeterPotentialEnergy(sim.potentialMaster);
+    	energyMeter.setPhase(sim.phase);
+        AccumulatorAverage energyAccumulator = new AccumulatorAverage();
+        DataPump energyManager = new DataPump(energyMeter,new DataSink[]{energyAccumulator});
+        energyAccumulator.setBlockSize(50);
+        IntervalActionAdapter adapter = new IntervalActionAdapter(energyManager, sim.integrator);
+        adapter.setActionInterval(5);
+        SimulationGraphic simgraphic = new SimulationGraphic(sim);
+    	simgraphic.makeAndDisplayFrame();
+    	ColorSchemeByType.setColor(sim.species.getFactory().getType(), java.awt.Color.red);
+    	sim.activityIntegrate.setMaxSteps(100);
+    	sim.getController().run();
+    	double[] data = energyAccumulator.getData();
+        double PE = data[AccumulatorAverage.AVERAGE.index]/sim.species.getAgent(sim.phase).getNMolecules();
+        System.out.println("PE/epsilon="+ElectronVolt.UNIT.fromSim(PE));
+    }
+    
+    public EAMMd3D() {
+        super(Space3D.INSTANCE);
+        integrator = new IntegratorVelocityVerlet(potentialMaster, space);
+        integrator.setTimeStep(0.01);
+        activityIntegrate = new ActivityIntegrate(integrator);
+        activityIntegrate.setSleepPeriod(2);
+        getController().addAction(activityIntegrate);
+        species = new SpeciesSpheresMono(this);
+        species.setNMolecules(216);
+        ((AtomTypeLeaf)species.getFactory().getType()).setMass(118.71);
+        ((AtomTypeSphere)species.getFactory().getType()).setDiameter(3.44); 
+        //The diameter given is really the equilibrium distance between the atoms.
+        int j = EmbeddedAtomMethodP2.agentIndex; 
+        	// This forces the EmbeddedAtomMethodP2 to 
+        	//request an agentIndex from Atom.
+        phase = new Phase(this);
+        PrimitiveTetragonal primitive = new PrimitiveTetragonal(space, 5.8318, 3.1819);
+        LatticeCrystal crystal = new LatticeCrystal(new Crystal(
+        		primitive, new BasisBetaSnA5(primitive)));
+        Configuration config = new ConfigurationLattice(crystal);
+        phase.setConfiguration(config);
+        potential0 = new EmbeddedAtomMethodPInitial(space);
+        potentialA = new EmbeddedAtomMethodP2(space, ParameterSetEAM.Sn);
+        potentialB = new EmbeddedAtomMethodPMany(space, ParameterSetEAM.Sn);
+        this.potentialMaster.setSpecies(potentialB, new Species[]{species});
+        this.potentialMaster.setSpecies(potentialA, new Species[]{species,species});
+        this.potentialMaster.setSpecies(potential0, new Species[]{species});    
+
+        integrator.addPhase(phase);
+        PhaseImposePbc imposepbc = new PhaseImposePbc();
+        imposepbc.setPhase(phase);
+        integrator.addListener(imposepbc);
+		
+		energy = new MeterEnergy(potentialMaster);
+    }
+    
+}
