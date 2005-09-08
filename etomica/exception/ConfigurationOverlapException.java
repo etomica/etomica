7f426bc83ab0fe87249b8e5fc3a239b06ad607dd
David Kofke <kofke@buffalo.edu>
2005-09-08 22:22:21 +0000

    add comments; add package documentation for etomica.exception

--- etomica/exception/ConfigurationOverlapException.java
+++ etomica/exception/ConfigurationOverlapException.java
@@ -2,15 +2,21 @@ package etomica.exception;
 
 import etomica.phase.Phase;
 
+/**
+ * Exception thrown when an overlap is detected in the configuration of
+ * a phase, subject to the status of an ignoreOverlap flag.
+ */
 public class ConfigurationOverlapException extends Exception {
 
     /**
-     * Constructor for MethodNotImplementedException.
+     * Constructor for ConfigurationOverlapException
+     * 
+     * @param phase the Phase in which the overlap is detected
      */
     public ConfigurationOverlapException(Phase phase) {
-	    super("Overlap in configuration "+phase);
-	    this.phase = phase;
+        super("Overlap in configuration "+phase);
+        this.phase = phase;
     }
 
-    public Phase phase;
+    public final Phase phase;
 }
