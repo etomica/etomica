e498ea3fb52b3dc57a451dee80f4b3c47f490045
David Kofke <kofke@buffalo.edu>
2005-09-22 00:52:37 +0000

    tweak some comments; add some package documentation

--- etomica/modifier/ModifierBoolean.java
+++ etomica/modifier/ModifierBoolean.java
@@ -1,12 +1,20 @@
 package etomica.modifier;
 
 /**
- * A ModifierBoolean object permits changes to be made to a boolean property of another object.
+ * Interface that permits changes to be made to a boolean property of another object.
+ * This type of modifier might be plugged into a checkbox or toggle button.
  */
 
 public interface ModifierBoolean {
     
+    /**
+     * Sets the modified value to the given boolean.
+     */
     public void setBoolean(boolean b);
+
+    /**
+     * Returns the current state of the modified value.
+     */
     public boolean getBoolean();
 
 }
