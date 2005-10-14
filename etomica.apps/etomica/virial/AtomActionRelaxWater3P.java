fe97912b41647cb6a5040c30c24ab99ab1370bfb
Andrew Schultz <ajs42@buffalo.edu>
2005-10-14 03:17:16 +0000

    add an action for 3P (SPCE or SPC) water relaxation

---
+++ etomica.apps/etomica/virial/AtomActionRelaxWater3P.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package etomica.virial;
+
+import etomica.action.AtomActionAdapter;
+import etomica.atom.Atom;
+import etomica.models.water.AtomTreeNodeWater3P;
+import etomica.space.Space;
+import etomica.space.Vector;
+
+public class AtomActionRelaxWater3P extends AtomActionAdapter {
+    public AtomActionRelaxWater3P(Space space) {
+        work = space.makeVector();
+        cosAngle = Math.cos(109.5/180.0*Math.PI);
+        sinAngle = Math.sin(109.5/180.0*Math.PI);
+        distance = 1.0;
+    }
+    
+    public void actionPerformed(Atom molecule) {
+        AtomTreeNodeWater3P waterNode = (AtomTreeNodeWater3P)molecule.node;
+        Atom O = waterNode.O;
+        Atom H1 = waterNode.H1;
+        Atom H2 = waterNode.H2;
+        // normalize OH1
+        Vector p1 = H1.coord.position();
+        p1.ME(O.coord.position());
+        p1.TE(1/Math.sqrt(p1.squared()));
+        Vector p2 = H2.coord.position();
+        p2.ME(O.coord.position());
+        p2.TE(1/Math.sqrt(p2.squared()));
+        // move H2 to fix bond angle
+        double d = p1.dot(p2);
+        work.Ev1Pa1Tv2(p2,-d,p1);
+        work.TE(1/Math.sqrt(work.squared()));
+        p2.Ea1Tv1(sinAngle,work);
+        p2.PEa1Tv1(cosAngle,p1);
+        p2.TE(distance/Math.sqrt(p2.squared()));
+        p1.TE(distance);
+        p1.PE(O.coord.position());
+        p2.PE(O.coord.position());
+    }
+
+    private final Vector work;
+    private final double sinAngle, cosAngle, distance;
+}
