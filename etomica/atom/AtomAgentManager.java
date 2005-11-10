789e6dc7076cfede7897e6eec6bbb10ab06f5f1e
Andrew Schultz <ajs42@buffalo.edu>
2005-11-10 02:13:14 +0000

    add AtomAgentManager class to manage agents in all atoms of a phase, contained in an array.

---
+++ etomica/atom/AtomAgentManager.java
@@ -0,0 +1,149 @@
+package etomica.atom;
+
+import java.lang.reflect.Array;
+
+import etomica.atom.Atom.AgentSource;
+import etomica.atom.iterator.AtomIteratorTree;
+import etomica.phase.Phase;
+import etomica.phase.PhaseEvent;
+import etomica.phase.PhaseListener;
+import etomica.util.Arrays;
+
+/**
+ * AtomAgentManager acts on behalf of client classes (an AgentSource) to manage 
+ * agents in every Atom in a phase.  When atoms are added or removed from the 
+ * phase, the agents array (indexed by the atom's global index) is updated.  
+ * The client should call getAgents() at any point where an atom might have 
+ * have been added to the system because the old array would be stale at that
+ * point. 
+ * @author andrew
+ */
+public class AtomAgentManager implements PhaseListener {
+
+    public AtomAgentManager(AgentSource source, Phase phase) {
+        agentSource = source;
+        setPhase(phase);
+        newAtomList = new AtomList();
+    }
+    
+    public Object[] getAgents() {
+        if (!newAtomList.isEmpty()) {
+            // if newAtomList has Atoms, agents must have been full, so extend it now
+            agents = Arrays.resizeArray(agents,newMaxIndex+phase.getSpeciesMaster().getIndexReservoirSize());
+            for (AtomLinker link = newAtomList.header.next; link != newAtomList.header; link = link.next) {
+                addAgent(link.atom);
+            }
+            newAtomList.clear();
+        }
+        newMaxIndex = -1;
+        return agents;
+    }
+    
+    public void setPhase(Phase newPhase) {
+        if (phase != null) {
+            // remove ourselves as a listener to the old phase
+            phase.getSpeciesMaster().removeListener(this);
+        }
+        phase = newPhase;
+        if (phase == null) {
+            // zero-out the array so the agents get GC'd
+            agents = (Object[])Array.newInstance(agentSource.makeAgent(null).getClass(),0);
+        }
+        phase.getSpeciesMaster().addListener(this);
+        // hope the class returns an actual class with a null Atom and use it to construct
+        // the array
+        agents = (Object[])Array.newInstance(agentSource.makeAgent(null).getClass(),
+                phase.getSpeciesMaster().getMaxGlobalIndex()+1);
+        // fill in the array with agents from all the atoms
+        AtomIteratorTree iterator = new AtomIteratorTree(phase.getSpeciesMaster(),Integer.MAX_VALUE,true);
+        iterator.reset();
+        while (iterator.hasNext()) {
+            addAgent(iterator.nextAtom());
+        }
+    }
+    
+    public void actionPerformed(PhaseEvent evt) {
+        if (evt.type() == PhaseEvent.ATOM_ADDED) {
+            Atom a = evt.atom();
+            if (a.type.isLeaf()) {
+                addAgent(a);
+            }
+            else {
+                if (treeIterator == null) {
+                    treeIterator = new AtomIteratorTree(Integer.MAX_VALUE);
+                    treeIterator.setDoAllNodes(true);
+                }
+                // add all atoms below this atom
+                treeIterator.setRoot(a);
+                while (treeIterator.hasNext()) {
+                    addAgent(treeIterator.nextAtom());
+                }
+            }       
+        }
+        else if (evt.type() == PhaseEvent.ATOM_REMOVED) {
+            Atom a = evt.atom();
+            if (a.type.isLeaf()) {
+                int index = a.getGlobalIndex();
+                if (agents[index] != null) {
+                    // Atom used to have an agent.  nuke it.
+                    agents[index] = null;
+                }
+                else if (agents.length < index+1) {
+                    // must have been just added
+                    newAtomList.remove(a);
+                }
+            }
+            else {
+                if (treeIterator == null) {
+                    treeIterator = new AtomIteratorTree(Integer.MAX_VALUE);
+                    treeIterator.setDoAllNodes(true);
+                }
+                // nuke all atoms below this atom
+                treeIterator.setRoot(a);
+                while (treeIterator.hasNext()) {
+                    int index = treeIterator.nextAtom().getGlobalIndex();
+                    if (agents[index] != null) {
+                        // Atom used to have an agent.  nuke it.
+                        agents[index] = null;
+                    }
+                    else if (agents.length < index+1) {
+                        // maybe it was just added
+                        newAtomList.remove(a);
+                    }
+                }
+            }
+        }
+        else if (evt.type() == PhaseEvent.ATOM_CHANGE_INDEX) {
+            // the atom's index changed.  assume it would get the same agent
+            agents[evt.atom().getGlobalIndex()] = agents[evt.getIndex()];
+            agents[evt.getIndex()] = null;
+        }
+        else if (evt.type() == PhaseEvent.GLOBAL_INDEX) {
+            // indices got compacted.  If our array is a lot bigger than it
+            // needs to be, shrink it.
+            SpeciesMaster speciesMaster = (SpeciesMaster)evt.getSource();
+            int reservoirSize = speciesMaster.getIndexReservoirSize();
+            if (agents.length > evt.getIndex()+reservoirSize) {
+                agents = Arrays.resizeArray(agents,evt.getIndex()+reservoirSize);
+            }
+        }
+    }
+    
+    protected void addAgent(Atom a) {
+        if (agents.length < a.getGlobalIndex()+1) {
+            // no room in the array.  reallocate the array lazily later.
+            newAtomList.add(a);
+            if (newMaxIndex < a.getGlobalIndex()) {
+                newMaxIndex = a.getGlobalIndex();
+            }
+        }
+        agents[a.getGlobalIndex()] = agentSource.makeAgent(a);
+    }
+    
+    private final AgentSource agentSource;
+    protected Object[] agents;
+    private AtomList newAtomList;
+    private AtomIteratorTree treeIterator;
+    private Phase phase;
+    private int newMaxIndex;
+}
