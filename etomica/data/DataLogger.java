e33337c12602ea135e98021dcf40244dab642b33
Andrew Schultz <ajs42@buffalo.edu>
2005-12-16 00:30:54 +0000

    nuke etomica.log.  A new DataLogger in etomica.data combines the best of LoggerAbstract and the old DataLogger

---
+++ etomica/data/DataLogger.java
@@ -0,0 +1,268 @@
+package etomica.data;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import etomica.integrator.IntegratorIntervalEvent;
+import etomica.integrator.IntegratorNonintervalEvent;
+import etomica.integrator.IntegratorNonintervalListener;
+
+
+/* History
+ * 07/06/04 (DAK) added makeWriteAction method, defined Action class, and set
+ * apart actions in doWrite method
+ */
+
+/**
+ * DataSink that manages a FileWriter and also listens to non-interval 
+ * integrator events and sends appropriate data to a DataWriter.
+ */
+public class DataLogger implements DataPipe, IntegratorNonintervalListener, java.io.Serializable {
+    
+    public DataLogger(){
+        setWriteInterval(100);
+        setPriority(300);
+    }
+    
+    /**
+     * Gives data to DataSink for writing
+     */
+    public void putData(Data data) {
+        if (--count == 0) {
+            count = writeInterval;
+            doWrite(data);
+        }
+    }
+    
+    public void putDataInfo(DataInfo newDataInfo) {
+        dataInfo = newDataInfo;
+        insertTransformerIfNeeded();
+        dataSink.putDataInfo(dataInfo);
+    }
+    
+    protected void insertTransformerIfNeeded() {
+        if(dataWriter == null || dataInfo == null) return;
+        //remove transformer if one was previously inserted
+        dataSink = dataWriter;
+        DataProcessor caster = dataWriter.getDataCaster(dataInfo);
+        if(caster != null) {
+            caster.setDataSink(dataWriter);
+            dataSink = caster;
+        }
+    }
+
+    public DataProcessor getDataCaster(DataInfo dataInfo) {
+        // we don't care about the type although the DataWriter might
+        return null;
+    }
+    
+    /**
+     * Sets the data writer to the given DataSink.  It must be an instance of
+     * DataLogger.DataWriter
+     */
+    public void setDataSink(DataSink dataWriter) {
+        this.dataWriter = (DataWriter)dataWriter;
+    }
+    
+    /**
+     * Close file when integrator is done.
+     */
+    public void nonintervalAction(IntegratorNonintervalEvent evt){
+        if(evt.type() == IntegratorIntervalEvent.DONE) {
+            if (writeOnFinishSource != null) {
+                doWrite(writeOnFinishSource.getData());
+            }
+            closeFile(); //close the file when DONE.
+        }
+    }
+    
+    /**
+     * Performs the open/write/close file actions in accordance with the
+     * settings of the Logger.  Called by intervalAction and by the
+     * actionPerformed method of any Actions made by the writeAction method.
+     */
+    private void doWrite(Data data) {
+        openFile();
+        dataWriter.setFileWriter(fileWriter);
+        if(closeFileEachTime) {
+            closeFile();
+        }
+        dataSink.putData(data);
+    }
+    
+    /**
+     * Define how many number of interval events received between writings to
+     * file.
+     */
+    public void setWriteInterval(int newWriteInterval){
+        writeInterval = newWriteInterval;
+        count = writeInterval;
+    }
+    
+    public int getWriteInterval() {
+        return writeInterval;
+    }
+    
+    
+    private void openFile(){
+        try { 
+            if(sameFileEachTime && fileIsOpen) return;
+            if(fileName == "") fileName = defaultFileName(); //if fileName is not defined yet, use the current date to be the fileName.
+            fileWriter = new FileWriter(fileName + fileNameSuffix, appending);
+            if (!appending) {
+                dataWriter.reset();
+            }
+            fileIsOpen = true;
+        }
+        catch(IOException e) {
+            System.err.println("Cannot open a file, caught IOException: " + e.getMessage());
+        }
+    }
+    
+    protected String defaultFileName() {//now use the current date to be the fileName, subclass could change this.
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMd_H_mm_ss", new Locale("en","US"));
+        Date now = new Date();
+        String currentTime = formatter.format(now);
+        return currentTime;
+    }
+    
+    private void closeFile(){ 
+        try {
+            if(fileIsOpen) { fileWriter.close(); fileIsOpen = false;}
+            if(!sameFileEachTime) fileName = "";
+        }catch(IOException e) {
+            System.err.println("Cannot close a file, caught IOException: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Define the name of the output file.
+     */
+    public void setFileName(String fileName) {//test if the fileName ends with ".txt", cut ".txt" from it.
+        if(fileName.endsWith(fileNameSuffix)) {
+            fileName = fileName.substring(0, fileName.length()-4);
+        }
+        this.fileName = fileName;
+    }
+    
+    public String getFileName() {
+        return (fileName+fileNameSuffix);
+    }
+    
+    /**
+     * Overwrite or attach the data to the file.  Default is to append, not
+     * overwrite.
+     */
+    public void setAppending(boolean appending) {
+        this.appending = appending; 
+    }
+    
+    public boolean isAppending() {
+        return appending;
+    }
+    
+    /**
+     * Write to the same file at each time INTERVAL, and close the file at the
+     * end of the simulation. Default is true.
+     */
+    public void setSameFileEachTime(boolean sameFileEachTime) {
+        this.sameFileEachTime = sameFileEachTime;
+    }
+    
+    public boolean isSameFileEachTime() {
+        return sameFileEachTime;
+    }
+    
+    /**
+     * Close the file and open a new one at each time INTERVAL. The new file will be named by defaultFileName().
+     */
+    public void setCloseFileEachTime(boolean closeFileEachTime) {
+        this.closeFileEachTime = closeFileEachTime;
+    }
+    
+    public boolean isCloseFileEachTime() {
+        return closeFileEachTime;
+    }
+    
+    /**
+     * @return Returns the writeOnFinish.
+     */
+    public DataSource getWriteOnFinishDataSource() {
+        return writeOnFinishSource;
+    }
+
+    /**
+     * @param writeOnFinish The writeOnFinish to set.
+     */
+    public void setWriteOnFinish(DataSource newWriteOnFinishSource) {
+        writeOnFinishSource = newWriteOnFinishSource;
+    }
+
+    /**
+     * @return Returns the writeOnInterval.
+     */
+    public boolean isWriteOnInterval() {
+        return writeOnInterval;
+    }
+
+    /**
+     * @param writeOnInterval The writeOnInterval to set.
+     */
+    public void setWriteOnInterval(boolean writeOnInterval) {
+        this.writeOnInterval = writeOnInterval;
+    }
+
+    /**
+     * @return Returns the interval-listener priority.
+     */
+    public int getPriority() {
+        return priority;
+    }
+    
+    /**
+     * Sets the interval-listener priority.  Default value is 300, which
+     * puts this after central-image enforcement and accumulator updates.
+     * @param priority The priority to set.
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+
+    private int count; //counts number of events received before calling doAction().
+    private int writeInterval; //number of times specified by the user between two actions.
+                                //could be set by setUpdateInterval() method.
+    private String fileName; //output file name. This name should not have a suffix.
+    private boolean appending = true; //whether to overwrite to the existing data 
+                                        //or attach the data. Default is not overwriting.
+    protected FileWriter fileWriter; //this is a handle that subclass can use to write data.
+    protected String fileNameSuffix = ".txt"; //Default suffix is text file, subclass can change this.
+    private boolean sameFileEachTime = true; //whether to write to the same file at each INTERVAL.
+    private boolean closeFileEachTime = false; //whether to close the file and open a new one at each INTERVAL.
+    private boolean fileIsOpen = false; //at the beginning, it is false.
+    private int priority;
+    private DataSource writeOnFinishSource = null;
+    private boolean writeOnInterval = true;
+    private DataWriter dataWriter;
+    private DataSink dataSink;
+    private DataInfo dataInfo;
+    
+    /**
+     * Interface for a DataSink that actually writes data to a file
+     */
+    public interface DataWriter extends DataSink {
+        
+        /**
+         * Sets the FileWriter to be used for writing.  
+         */
+        public void setFileWriter(FileWriter newFileWriter);
+
+        /**
+         * Informs the DataSink that it will be writing the beginning of the 
+         * so printing header information is appropriate.
+         */
+        public void reset();
+    }
+}
---
+++ etomica/data/DataTableWriter.java
@@ -0,0 +1,93 @@
+package etomica.data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import etomica.data.DataLogger.DataWriter;
+import etomica.data.types.CastArrayToDoubleArray;
+import etomica.data.types.CastGroupOfTablesToDataTable;
+import etomica.data.types.CastGroupToDoubleArray;
+import etomica.data.types.CastToTable;
+import etomica.data.types.DataArray;
+import etomica.data.types.DataGroup;
+import etomica.data.types.DataTable;
+
+/**
+ * A DataWriter that writes out data as a table with column headings
+ */
+public class DataTableWriter implements DataWriter, java.io.Serializable {
+
+    public DataTableWriter() {
+        super();
+        firstWrite = true;
+    }
+    
+    public void putDataInfo(DataInfo newDataInfo) {
+        // no DataSink
+    }
+
+    public DataProcessor getDataCaster(DataInfo newDataInfo) {
+        if (newDataInfo.getDataClass() == DataTable.class) {
+            return null;
+        }
+        else if (newDataInfo.getDataClass() == DataGroup.class) {
+            DataInfo[] info = ((DataGroup.Factory)newDataInfo.getDataFactory()).getDataInfoArray(); 
+            Class innerDataClass = info[0].getDataClass();
+            for (int i = 1; i<info.length; i++) {
+                if (info[i].getDataClass() != innerDataClass) {
+                    throw new IllegalArgumentException("DataSinkTable can only handle homogeneous groups");
+                }
+            }
+            if(innerDataClass == DataTable.class) {
+                return new CastGroupOfTablesToDataTable();
+            }
+            return new CastGroupToDoubleArray();
+        }
+        else if(newDataInfo.getDataClass() == DataArray.class) {
+            return new CastArrayToDoubleArray();
+        }
+        return new CastToTable();
+    }
+    
+    public void putData(Data data) {
+        DataTable table = (DataTable)data;
+        try {
+            int nColumns = table.getNColumns();
+            if (nColumns < 1) {
+                return;
+            }
+            if (firstWrite) {
+                // if this is the first write to a file, start with the column headers
+                fileWriter.write(table.getDataInfo().getLabel()+" "+table.getDataInfo().getDimension()+"\n");
+                fileWriter.write(table.getColumn(0).getHeading());
+                for (int i=1; i<nColumns; i++) {
+                    fileWriter.write("  "+table.getColumn(i).getHeading());
+                }
+                fileWriter.write("\n");
+                firstWrite = false;
+            }
+            for (int i=0; i<table.getNRows(); i++) {
+                fileWriter.write(Double.toString(table.getColumn(0).getData()[i]));
+                for (int j=1; j<nColumns; j++) {
+                    fileWriter.write("  "+Double.toString(table.getColumn(j).getData()[i]));
+                }
+                fileWriter.write("\n");
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+    
+    public void setFileWriter(FileWriter newFileWriter) {
+        fileWriter = newFileWriter;
+    }
+    
+    public void reset() {
+        firstWrite = true;
+    }
+
+    private FileWriter fileWriter;
+    private boolean firstWrite;
+}
