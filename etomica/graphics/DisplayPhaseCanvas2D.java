58fda20da36558cb9102457d6fef7f3201168f44
Andrew Schultz <ajs42@buffalo.edu>
2006-01-06 06:33:06 +0000

    bustage fix (perhaps we can live without the certprovider Vertices)

--- etomica/graphics/DisplayPhaseCanvas2D.java
+++ etomica/graphics/DisplayPhaseCanvas2D.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.TextField;
 import java.util.Iterator;
 
-import sun.security.provider.certpath.Vertex;
 import etomica.atom.Atom;
 import etomica.atom.AtomFilter;
 import etomica.atom.AtomTypeOrientedSphere;
