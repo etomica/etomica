6ead0df7b3bde29766dcd7b0ef11550fd13263fb
Nancy Cribbin <cribbin@buffalo.edu>
2006-01-11 20:29:33 +0000

    Added graphic simulation

--- etomica/models/hexane/TestHexane.java
+++ etomica/models/hexane/TestHexane.java
@@ -11,12 +11,23 @@ import etomica.atom.iterator.ApiBuilder;
 import etomica.atom.iterator.ApiIntragroup;
 import etomica.atom.iterator.AtomIteratorArrayList;
 import etomica.atom.iterator.IteratorDirective;
+import etomica.data.AccumulatorAverage;
+import etomica.data.DataPump;
+import etomica.data.meter.MeterPotentialEnergyFromIntegrator;
+import etomica.data.meter.MeterPressureHard;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataGroup;
+import etomica.graphics.ColorSchemeByType;
+import etomica.graphics.DisplayPhase;
+import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntegratorHard;
+import etomica.integrator.IntervalActionAdapter;
 import etomica.nbr.CriterionBondedSimple;
 import etomica.nbr.CriterionMolecular;
 import etomica.nbr.CriterionMolecularNonAdjacent;
 import etomica.nbr.CriterionSimple;
 import etomica.nbr.NeighborCriterion;
+import etomica.nbr.list.NeighborListManager;
 import etomica.nbr.list.PotentialMasterList;
 import etomica.phase.Phase;
 import etomica.potential.P2HardBond;
@@ -53,7 +64,7 @@ public class TestHexane extends Simulation {
 
     public TestHexane(Space space, int numMolecules) {
         //super(space, false, new PotentialMasterNbr(space, 12.0));
-        super(space, false, new PotentialMasterList(space, 12.0));
+        super(space, true, new PotentialMasterList(space, 12.0));
         int chainLength = 6;
         int numAtoms = numMolecules * chainLength;
         ConfigurationHexane config = new ConfigurationHexane(space);
@@ -78,10 +89,21 @@ public class TestHexane extends Simulation {
         //nan this is where the squared, unsquared box stuff comes in.
         //makes the density 0.41657 per Dr. Monson's comment in e-mail.
         def.boxSize = 7.018;
-        integrator = new IntegratorHard(potentialMaster, timeStep, def.temperature);
+        integrator = new IntegratorHard(potentialMaster, timeStep,
+                def.temperature);
         integrator.setTimeStep(timeStep);
         integrator.setIsothermal(true);
-        ActivityIntegrate activityIntegrate = new ActivityIntegrate(this, integrator);
+        ActivityIntegrate activityIntegrate = new ActivityIntegrate(this,
+                integrator);
+        //Added to make it work.
+        NeighborListManager nbrManager = ((PotentialMasterList) potentialMaster)
+                .getNeighborManager();
+        integrator.addListener(nbrManager);
+        nbrManager.setRange(defaults.atomSize * neighborRangeFac);
+        ((PotentialMasterList) potentialMaster).setCellRange(2);
+        ((PotentialMasterList) potentialMaster).setRange(neighborRangeFac
+                * defaults.atomSize);
+
         getController().addAction(activityIntegrate);
         activityIntegrate.setMaxSteps(nSteps);
 
@@ -90,7 +112,8 @@ public class TestHexane extends Simulation {
         //This potential is the intermolecular potential between atoms on
         // different molecules. We use the class "Potential" because we are
         // reusing the instance as we define each potential.
-        Potential potential = new P2HardSphere(space, def.atomSize, def.ignoreOverlap);
+        Potential potential = new P2HardSphere(space, def.atomSize,
+                def.ignoreOverlap);
 
         //here, we add the species to the PotentialMaster, using types.
         //The PotentialMaster generates a group potential and automatically
@@ -116,79 +139,83 @@ public class TestHexane extends Simulation {
         sameMoleculeCrit.setIntraMolecular(false);
 
         //we add the new criterion to the potential.
-        ((Potential2)potential).setCriterion(sameMoleculeCrit);
+        ((Potential2) potential).setCriterion(sameMoleculeCrit);
+        nbrManager.addCriterion(sameMoleculeCrit, new AtomType[] {sphereType,sphereType});
 
         //Add the Potential to the PotentialMaster
         potentialMaster.addPotential(potential, new AtomType[] { sphereType,
                 sphereType });
-
-        //INTRAMOLECULAR POTENTIAL STUFF
+        
+         //INTRAMOLECULAR POTENTIAL STUFF
 
         //This PotentialGroup will hold all the intramolecular potentials.
         //We give 1 as the argument because we are using 1 molecule to iterate
-        // on.  The actual interactions between the atoms on the molecules will
+        // on. The actual interactions between the atoms on the molecules will
         // be calculated by a Potential2, but their summation is the molecule's
-        //effect on itself, which is a Potential1, or a Potential with nBody = 1.
-        PotentialGroup potentialChainIntra = new PotentialGroup(1, space);      
-        
-        			//BONDED INTERACTIONS
-       
+        //effect on itself, which is a Potential1, or a Potential with nBody =
+        // 1.
+        PotentialGroup potentialChainIntra = new PotentialGroup(1, space);
+
+        //BONDED INTERACTIONS
+
         //nan This potential simulates the bonds between atoms in a molecule.
         // It will be superceded by a set of MC moves at some point in the
         // future.
-        //This potential uses hard sphere interactions to model the bonded interactions
+        //This potential uses hard sphere interactions to model the bonded
+        // interactions
         // of the atoms of the molecule.
-        potential = new P2HardBond(space, def.atomSize, neighborRangeFac, def.ignoreOverlap);
-        
+        potential = new P2HardBond(space, def.atomSize, neighborRangeFac,
+                def.ignoreOverlap);
+
         //Only the atoms next to each other interact, so we have two criteria:
         //		The atoms must be on the same molecule- CriterionMolecular
         //		The atoms must be neighbors
         //Both are included in the CriterionBondedSimple class, so we use it.
         NeighborCriterion criterion = new CriterionBondedSimple(nbrCriterion);
-        ((Potential2)potential).setCriterion(criterion);
-       
+        ((Potential2) potential).setCriterion(criterion);
+        
+
         //We will need an atom pair iterator (Api) that runs through the atoms
         // on a single molecule.
-        //The atom pair iterator (Api) runs through the atoms on a single molecule.
+        //The atom pair iterator (Api) runs through the atoms on a single
+        // molecule.
         //  It has an inner loop and an outer loop.
         ApiIntragroup bonded = ApiBuilder.makeAdjacentPairIterator();
- 
-        //We add the Potential and its Iterator to the PotentialGroup, in one fell swoop. Yay us!
+
+        //We add the Potential and its Iterator to the PotentialGroup, in one
+        // fell swoop. Yay us!
         potentialChainIntra.addPotential(potential, bonded);
-        
-        
-        			//NONBONDED INTERACTIONS
+
+        //NONBONDED INTERACTIONS
         //This potential describes the basic hard sphere interactions between
         // 2 atoms of a molecule.
         potential = new P2HardSphere(space, def.atomSize, false);
-        
+
         //Only the atoms next to each other interact, so we have two criteria:
         //		The atoms must be on the same molecule- CriterionMolecular
         //		The atoms must be separated by 3 bonds.
-        //We end up needing to do stuff with the 
-        criterion = new CriterionMolecularNonAdjacent(3,
-                nbrCriterion);
-        ((Potential2)potential).setCriterion(criterion);
-        
+        //We end up needing to do stuff with the
+        criterion = new CriterionMolecularNonAdjacent(3, nbrCriterion);
+        ((Potential2) potential).setCriterion(criterion);
+
         //This iterator runs through the atoms on a single molecule. It skips
         // the specified number of atoms.
-        //The inner iterator runs through 
+        //The inner iterator runs through
         //The outer iterator runs through
         //Similar to ApiBuilder.makeNonAdjacentPairIterator
         AtomIteratorArrayList aiInnerUp = new AtomIteratorArrayList(IteratorDirective.UP, 3);
         AtomIteratorArrayList aiInnerDn = new AtomIteratorArrayList(IteratorDirective.DOWN, 3);
         ApiIntragroup intra = new ApiIntragroup(aiInnerUp, aiInnerDn);
-        
+
         //Now we add this potential to the PotentialGroup.
         potentialChainIntra.addPotential(potential, intra);
-        
-        
+
         //Now we add this PotentialGroup to the PotentialMaster.
-        potentialMaster.addPotential(potentialChainIntra, new AtomType[] {
-                sphereType });
+        potentialMaster.addPotential(potentialChainIntra,
+                new AtomType[] { species.getMoleculeType() });
 
         //nan, um, how do we worry about running through all the molecules?
-        
+
         phase = new Phase(this);
         //nan we need to do more with the boundary
         bdry =  new BoundaryHexane(space);
@@ -198,6 +225,7 @@ public class TestHexane extends Simulation {
 
         //Initialize the positions of the atoms.
         config.initializeCoordinates(phase);
+
     }
 
     public static void main(String[] args) {
@@ -208,6 +236,31 @@ public class TestHexane extends Simulation {
         TestHexane sim = new TestHexane(Space3D.getInstance(), numMolecules);
 
         System.out.println("Happy Goodness!!");
+
+        MeterPressureHard pMeter = new MeterPressureHard(sim.space,
+                sim.integrator);
+        pMeter.setPhase(sim.phase);
+        MeterPotentialEnergyFromIntegrator energyMeter = new MeterPotentialEnergyFromIntegrator(
+                sim.integrator);
+        energyMeter.setPhase(sim.phase);
+        AccumulatorAverage energyAccumulator = new AccumulatorAverage(sim);
+        DataPump energyManager = new DataPump(energyMeter, energyAccumulator);
+        energyAccumulator.setBlockSize(50);
+        pMeter.setPhase(sim.phase);
+        //This is the wire to plug the pump into the electric socket and make
+        // it run.
+        new IntervalActionAdapter(energyManager, sim.integrator);
+
+        //This starts the simulation, sets up its own thread to start the sim,
+        // and then moves on down the list.
+        //sim.getController().actionPerformed();
+
+        double avgPE = ((DataDouble) ((DataGroup) energyAccumulator.getData())
+                .getData(AccumulatorAverage.AVERAGE.index)).x;
+        System.out.println("PE  " + avgPE);
+        
+        SimulationGraphic simGraphic = new SimulationGraphic(sim);
+        simGraphic.makeAndDisplayFrame();
 
     }
 
