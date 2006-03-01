ae0ffd7282e5b9b0ccd9a293803abf956c01c30d
Kate Shaul <schadel2@buffalo.edu>
2006-03-01 17:18:49 +0000

    math corrections

--- etomica.apps/etomica/meam/MEAMPMany.java
+++ etomica.apps/etomica/meam/MEAMPMany.java
@@ -47,7 +47,8 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
 	
     public void setPhase(Phase phase) {
         super.setPhase(phase);
-        agentManager = (AtomAgentManager)phaseAgentManager.getAgents()[phase.getIndex()];
+        AtomAgentManager[] agentManager = (AtomAgentManager[])phaseAgentManager.getAgents();
+        agents = (Wrapper[])agentManager[phase.getIndex()].getAgents();
     }
     
     protected double rhoi0(Wrapper agent) {
@@ -64,25 +65,25 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
     protected double rhoi2(Wrapper agent) {
 		return Math.sqrt(   
 			(agent.sums[Wrapper.rhoj2xxBin]* agent.sums[Wrapper.rhoj2xxBin])
-			+ (2 * agent.sums[Wrapper.rhoj2xyBin] * agent.sums[Wrapper.rhoj2xyBin])
-			+ (2 * agent.sums[Wrapper.rhoj2xzBin] * agent.sums[Wrapper.rhoj2xzBin])
+			+ (2.0 * agent.sums[Wrapper.rhoj2xyBin] * agent.sums[Wrapper.rhoj2xyBin])
+			+ (2.0 * agent.sums[Wrapper.rhoj2xzBin] * agent.sums[Wrapper.rhoj2xzBin])
 			+ (agent.sums[Wrapper.rhoj2yyBin] * agent.sums[Wrapper.rhoj2yyBin])
-			+ (2 * agent.sums[Wrapper.rhoj2yzBin] * agent.sums[Wrapper.rhoj2yzBin])
+			+ (2.0 * agent.sums[Wrapper.rhoj2yzBin] * agent.sums[Wrapper.rhoj2yzBin])
 			+ (agent.sums[Wrapper.rhoj2zzBin] * agent.sums[Wrapper.rhoj2zzBin])
-			- ((1/3) * agent.sums[Wrapper.rhoj2Bin]));
+			- ((1.0/3.0) * agent.sums[Wrapper.rhoj2Bin]));
     }
     
     protected double rhoi3(Wrapper agent) {
 		return Math.sqrt(
 		     (agent.sums[Wrapper.rhoj3xxxBin] * agent.sums[Wrapper.rhoj3xxxBin])
-		+(3 * agent.sums[Wrapper.rhoj3xxyBin] * agent.sums[Wrapper.rhoj3xxyBin])
-		+(3 * agent.sums[Wrapper.rhoj3xxzBin] * agent.sums[Wrapper.rhoj3xxzBin])
-		+(3 * agent.sums[Wrapper.rhoj3xyyBin] * agent.sums[Wrapper.rhoj3xyyBin])
-		+(6 * agent.sums[Wrapper.rhoj3xyzBin] * agent.sums[Wrapper.rhoj3xyzBin])
-		+(3 * agent.sums[Wrapper.rhoj3xzzBin] * agent.sums[Wrapper.rhoj3xzzBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3xxyBin] * agent.sums[Wrapper.rhoj3xxyBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3xxzBin] * agent.sums[Wrapper.rhoj3xxzBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3xyyBin] * agent.sums[Wrapper.rhoj3xyyBin])
+		+(6.0 * agent.sums[Wrapper.rhoj3xyzBin] * agent.sums[Wrapper.rhoj3xyzBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3xzzBin] * agent.sums[Wrapper.rhoj3xzzBin])
 		+    (agent.sums[Wrapper.rhoj3yyyBin] * agent.sums[Wrapper.rhoj3yyyBin])
-		+(3 * agent.sums[Wrapper.rhoj3yyzBin] * agent.sums[Wrapper.rhoj3yyzBin])
-		+(3 * agent.sums[Wrapper.rhoj3yzzBin] * agent.sums[Wrapper.rhoj3yzzBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3yyzBin] * agent.sums[Wrapper.rhoj3yyzBin])
+		+(3.0 * agent.sums[Wrapper.rhoj3yzzBin] * agent.sums[Wrapper.rhoj3yzzBin])
 		+    (agent.sums[Wrapper.rhoj3zzzBin] * agent.sums[Wrapper.rhoj3zzzBin]));
     }
     
@@ -106,17 +107,20 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
 		double tav1 = tav1(agent);
 		double tav2 = tav2(agent);
 		double tav3 = tav3(agent);
-    	return tav1 * (rhoi1/rhoi0) * (rhoi1/rhoi0)
-		+ tav2 * (rhoi2/rhoi0) * (rhoi2/rhoi0)
-		+ tav3 * (rhoi3/rhoi0) * (rhoi3/rhoi0);
+    	return (tav1 * (rhoi1/rhoi0) * (rhoi1/rhoi0))
+		+ (tav2 * (rhoi2/rhoi0) * (rhoi2/rhoi0))
+		+ (tav3 * (rhoi3/rhoi0) * (rhoi3/rhoi0));
     }
     
     protected double rhoi(Wrapper agent) {
     	double rhoi0 = rhoi0(agent);
 		double gamma = gamma(agent);
+		
+		
+		
 		//The following expression for the background electron density of atom i
 		//is appropriate for Sn only.
-		return (2 * rhoi0) / (1 + Math.exp(-gamma));
+		return (2.0 * rhoi0) / (1.0 + Math.exp(-gamma));
     }
     
 	public double energy(AtomSet a) {
@@ -127,7 +131,7 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
 		//The embedding energy of atom i
 		double F = p.A * p.Ec * (rhoi / p.rhoScale) * Math.log( rhoi / p.rhoScale);
 		
-		return F + ((1/2)*agent.sums[Wrapper.phiBin]);
+		return F + ((1.0/2.0)*agent.sums[Wrapper.phiBin]);
 	}
 
 	public Vector gradient(AtomSet a) {
@@ -143,6 +147,7 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
 		double gamma = gamma(agent);
 		double rhoi = rhoi(agent);
 		
+		
 		gradRhoi0.E(agent.gradientSums[Wrapper.rhoj0Bin]);
 		
 		gradRhoi1.Ea1Tv1(agent.sums[Wrapper.rhoj1xBin], 
@@ -151,67 +156,70 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
 				agent.gradientSums[Wrapper.rhoj1xBin]);
 		gradRhoi1.PEa1Tv1(agent.sums[Wrapper.rhoj1zBin], 
 				agent.gradientSums[Wrapper.rhoj1zBin]);
-		gradRhoi1.TE(1/rhoi1);
+		gradRhoi1.TE(1.0/rhoi1);
 		
 		gradRhoi2.Ea1Tv1(agent.sums[Wrapper.rhoj2xxBin], agent.gradientSums[Wrapper.rhoj2xxBin]);
-		gradRhoi2.PEa1Tv1(2 * agent.sums[Wrapper.rhoj2xyBin], agent.gradientSums[Wrapper.rhoj2xyBin]);
-		gradRhoi2.PEa1Tv1(2 * agent.sums[Wrapper.rhoj2xzBin], agent.gradientSums[Wrapper.rhoj2xzBin]);
+		gradRhoi2.PEa1Tv1(2.0 * agent.sums[Wrapper.rhoj2xyBin], agent.gradientSums[Wrapper.rhoj2xyBin]);
+		gradRhoi2.PEa1Tv1(2.0 * agent.sums[Wrapper.rhoj2xzBin], agent.gradientSums[Wrapper.rhoj2xzBin]);
 		gradRhoi2.PEa1Tv1(agent.sums[Wrapper.rhoj2yyBin], agent.gradientSums[Wrapper.rhoj2yyBin]);
-		gradRhoi2.PEa1Tv1(2 * agent.sums[Wrapper.rhoj2yzBin], agent.gradientSums[Wrapper.rhoj2yzBin]);
+		gradRhoi2.PEa1Tv1(2.0 * agent.sums[Wrapper.rhoj2yzBin], agent.gradientSums[Wrapper.rhoj2yzBin]);
 		gradRhoi2.PEa1Tv1(agent.sums[Wrapper.rhoj2zzBin], agent.gradientSums[Wrapper.rhoj2zzBin]);
-		gradRhoi2.PEa1Tv1(-(1/3) * agent.sums[Wrapper.rhoj2Bin], agent.gradientSums[Wrapper.rhoj2Bin]);
-		gradRhoi2.TE(1/rhoi2);
+		gradRhoi2.PEa1Tv1(-(1.0/3.0) * agent.sums[Wrapper.rhoj2Bin], agent.gradientSums[Wrapper.rhoj2Bin]);
+		gradRhoi2.TE(1.0/rhoi2);
 		
 		gradRhoi3.Ea1Tv1(agent.sums[Wrapper.rhoj3xxxBin], 
 						agent.gradientSums[Wrapper.rhoj3xxxBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3xxyBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3xxyBin], 
 					agent.gradientSums[Wrapper.rhoj3xxyBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3xxzBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3xxzBin], 
 					agent.gradientSums[Wrapper.rhoj3xxzBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3xyyBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3xyyBin], 
 					agent.gradientSums[Wrapper.rhoj3xyyBin]);
-		gradRhoi3.PEa1Tv1(6 * agent.sums[Wrapper.rhoj3xyzBin], 
+		gradRhoi3.PEa1Tv1(6.0 * agent.sums[Wrapper.rhoj3xyzBin], 
 					agent.gradientSums[Wrapper.rhoj3xyzBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3xzzBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3xzzBin], 
 					agent.gradientSums[Wrapper.rhoj3xzzBin]);
 		gradRhoi3.PEa1Tv1(agent.sums[Wrapper.rhoj3yyyBin], 
 					agent.gradientSums[Wrapper.rhoj3yyyBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3yyzBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3yyzBin], 
 					agent.gradientSums[Wrapper.rhoj3yyzBin]);
-		gradRhoi3.PEa1Tv1(3 * agent.sums[Wrapper.rhoj3yzzBin], 
+		gradRhoi3.PEa1Tv1(3.0 * agent.sums[Wrapper.rhoj3yzzBin], 
 					agent.gradientSums[Wrapper.rhoj3yzzBin]);
 		gradRhoi3.PEa1Tv1(agent.sums[Wrapper.rhoj3zzzBin], 
 					agent.gradientSums[Wrapper.rhoj3zzzBin]);
-		gradRhoi3.TE(1/rhoi3);
+		gradRhoi3.TE(1.0/rhoi3);
 		
 		gradtav1.Ea1Tv1(-agent.sums[Wrapper.t1Rhoj0Bin]/(rhoi0*rhoi0), agent.gradientSums[Wrapper.rhoj0Bin]);
-		gradtav1.PEa1Tv1(1/rhoi0, agent.gradientSums[Wrapper.t1Rhoj0Bin]);
+		gradtav1.PEa1Tv1(1.0/rhoi0, agent.gradientSums[Wrapper.t1Rhoj0Bin]);
 		
 		gradtav2.Ea1Tv1(-agent.sums[Wrapper.t2Rhoj0Bin]/(rhoi0*rhoi0), agent.gradientSums[Wrapper.rhoj0Bin]);
-		gradtav2.PEa1Tv1(1/rhoi0, agent.gradientSums[Wrapper.t2Rhoj0Bin]);
+		gradtav2.PEa1Tv1(1.0/rhoi0, agent.gradientSums[Wrapper.t2Rhoj0Bin]);
 		
 		gradtav3.Ea1Tv1(-agent.sums[Wrapper.t3Rhoj0Bin]/(rhoi0*rhoi0), agent.gradientSums[Wrapper.rhoj0Bin]);
-		gradtav3.PEa1Tv1(1/rhoi0, agent.gradientSums[Wrapper.t3Rhoj0Bin]);
+		gradtav3.PEa1Tv1(1.0/rhoi0, agent.gradientSums[Wrapper.t3Rhoj0Bin]);
 		
-		gradGamma.Ea1Tv1( (-2/rhoi0)*( (tav1*rhoi1*rhoi1) + (tav2*rhoi2*rhoi2)
+		gradGamma.Ea1Tv1( (-2.0/rhoi0)*( (tav1*rhoi1*rhoi1) + (tav2*rhoi2*rhoi2)
 				+ (tav3*rhoi3*rhoi3) ), gradRhoi0);
-		gradGamma.PEa1Tv1(2*tav1*rhoi1, gradRhoi1);
-		gradGamma.PEa1Tv1(2*tav2*rhoi2, gradRhoi2);
-		gradGamma.PEa1Tv1(2*tav3*rhoi3, gradRhoi3);
+		gradGamma.PEa1Tv1(2.0*tav1*rhoi1, gradRhoi1);
+		gradGamma.PEa1Tv1(2.0*tav2*rhoi2, gradRhoi2);
+		gradGamma.PEa1Tv1(2.0*tav3*rhoi3, gradRhoi3);
 		gradGamma.PEa1Tv1(rhoi1*rhoi1, gradtav1);
 		gradGamma.PEa1Tv1(rhoi2*rhoi2, gradtav2);
 		gradGamma.PEa1Tv1(rhoi3*rhoi3, gradtav3);
-		gradGamma.TE(1/(rhoi0*rhoi0));
+		gradGamma.TE(1.0/(rhoi0*rhoi0));
 		
-		gradRhoi.Ea1Tv1(rhoi0*Math.exp(-gamma)/(1+Math.exp(-gamma)), gradGamma);
+		gradRhoi.Ea1Tv1(rhoi0*Math.exp(-gamma)/(1.0 + Math.exp(-gamma)), gradGamma);
 		gradRhoi.PE(gradRhoi0);
-		gradRhoi.TE(2/(1+Math.exp(-gamma)));
-		
+		gradRhoi.TE(2.0/(1.0+Math.exp(-gamma)));
+	
 		gradF.Ea1Tv1( (p.A*p.Ec/p.rhoScale)*
-				(Math.log(rhoi)-Math.log(p.rhoScale)+1), gradRhoi);
+				(Math.log(rhoi)-Math.log(p.rhoScale)+1.0), gradRhoi);
+		
+		//System.out.println("gradF is " + gradF);
+		//System.exit(0);
 		
 		gradEi.E(gradF);
-		gradEi.PEa1Tv1(1/2, agent.gradientSums[Wrapper.phiBin]);
+		gradEi.PEa1Tv1(1.0/2.0, agent.gradientSums[Wrapper.phiBin]);
 		
 		return gradEi;
 		
@@ -239,5 +247,4 @@ public final class MEAMPMany extends Potential1 implements PotentialSoft {
     private final Vector3D gradEi;
     private final Vector gradient;
 	private PhaseAgentManager phaseAgentManager;
-	private AtomAgentManager agentManager;
 }
