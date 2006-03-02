bb7089cc15d8941015390d1e8154fc9c25ec6140
Andrew Schultz <ajs42@buffalo.edu>
2006-03-02 02:12:01 +0000

    resurrection of LJMD module.  Still needs a bit of work.

---
+++ etomica.modules/etomica/modules/ljmd/Ljmd.java
@@ -0,0 +1,77 @@
+package etomica.modules.ljmd;
+import etomica.action.PhaseImposePbc;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.config.ConfigurationSequential;
+import etomica.integrator.IntegratorVelocityVerlet;
+import etomica.integrator.IntervalActionAdapter;
+import etomica.phase.Phase;
+import etomica.potential.P2LennardJones;
+import etomica.simulation.Simulation;
+import etomica.space.Space;
+import etomica.space2d.Space2D;
+import etomica.space3d.Space3D;
+import etomica.species.Species;
+import etomica.species.SpeciesSpheresMono;
+
+public class Ljmd extends Simulation {
+    
+    public SpeciesSpheresMono species;
+    public Phase phase;
+    public IntegratorVelocityVerlet integrator;
+    public ActivityIntegrate activityIntegrate;
+    
+    public Ljmd(Space space) {
+        super(space);
+        defaults.makeLJDefaults();
+        defaults.boxSize = 14.0;
+        
+        final int N = 182;  //number of atoms
+        
+        
+        //controller and integrator
+	    integrator = new IntegratorVelocityVerlet(this);
+	    integrator.setIsothermal(false);
+        activityIntegrate = new ActivityIntegrate(this, integrator);
+        activityIntegrate.setSleepPeriod(1);
+        getController().addAction(activityIntegrate);
+        integrator.setTimeStep(0.01);
+     //   integrator.setDoSleep(false);
+
+	    //species and potentials
+	//    SpeciesSphereWells disks = new SpeciesSphereWells(this);//index 1
+	    species = new SpeciesSpheresMono(this);//index 1
+	    species.setName("");
+	    species.setNMolecules(N);
+        
+        //instantiate several potentials for selection in combo-box
+	    P2LennardJones potential = new P2LennardJones(this);
+	    potentialMaster.addPotential(potential, new Species[]{species, species});
+	    
+        //construct phase
+	    phase = new Phase(this);
+        new ConfigurationSequential(space).initializeCoordinates(phase);
+        integrator.setPhase(phase);
+		
+        PhaseImposePbc imposePBC = new PhaseImposePbc(phase);
+        integrator.addListener(new IntervalActionAdapter(imposePBC));
+        
+    }//end of constructor    
+    
+    public static void main(String[] args) {
+        Space space = Space2D.getInstance();
+        if(args.length != 0) {
+            try {
+                int D = Integer.parseInt(args[0]);
+                if (D == 3) {
+                    space = Space3D.getInstance();
+                }
+            } catch(NumberFormatException e) {}
+        }
+            
+        Ljmd sim = new Ljmd(space);
+        sim.getController().actionPerformed();
+    }//end of main
+    
+}
+
+
---
+++ etomica.modules/etomica/modules/ljmd/LjmdGraphic.java
@@ -0,0 +1,397 @@
+package etomica.modules.ljmd;
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+import etomica.action.Action;
+import etomica.atom.AtomTypeLeaf;
+import etomica.atom.iterator.AtomIteratorLeafAtoms;
+import etomica.data.AccumulatorAverage;
+import etomica.data.AccumulatorHistogram;
+import etomica.data.AccumulatorHistory;
+import etomica.data.DataFork;
+import etomica.data.DataPump;
+import etomica.data.DataSink;
+import etomica.data.DataSourceFunction;
+import etomica.data.DataSourceRmsVelocity;
+import etomica.data.DataSourceUniform;
+import etomica.data.AccumulatorAverage.StatType;
+import etomica.data.DataSourceUniform.LimitType;
+import etomica.data.meter.MeterDensity;
+import etomica.data.meter.MeterEnergy;
+import etomica.data.meter.MeterKineticEnergy;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.data.meter.MeterPressure;
+import etomica.data.meter.MeterRDF;
+import etomica.data.meter.MeterTemperature;
+import etomica.graphics.ColorSchemeByType;
+import etomica.graphics.ConstantsGraphic;
+import etomica.graphics.DeviceNSelector;
+import etomica.graphics.DeviceThermoSelector;
+import etomica.graphics.DeviceTrioControllerButton;
+import etomica.graphics.DisplayBox;
+import etomica.graphics.DisplayBoxesCAE;
+import etomica.graphics.DisplayPhase;
+import etomica.graphics.DisplayPlot;
+import etomica.graphics.DisplayTable;
+import etomica.graphics.SimulationGraphic;
+import etomica.integrator.IntervalActionAdapter;
+import etomica.space.Space;
+import etomica.space2d.Space2D;
+import etomica.space3d.Space3D;
+import etomica.statmech.MaxwellBoltzmann;
+import etomica.units.Energy;
+import etomica.units.Unit;
+import etomica.units.systems.LJ;
+import etomica.util.DoubleRange;
+import etomica.util.Constants.CompassDirection;
+
+public class LjmdGraphic {
+    
+    public LjmdGraphic(Ljmd sim) {
+        LJ unitSystem = new LJ();
+        Unit tUnit = Energy.DIMENSION.getUnit(unitSystem);
+
+        sim.getDefaults().blockSize = 100;
+        sim.getDefaults().doSleep = true;
+        sim.activityIntegrate.setDoSleep(true);
+        
+        sim.register(sim.integrator);
+        
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        
+        DeviceTrioControllerButton control = new DeviceTrioControllerButton(sim);
+        
+        //temperature selector
+	    DeviceThermoSelector tSelect = new DeviceThermoSelector(sim,sim.integrator);
+//	    tSelect.setTemperatures(new double[] {50.,100.,300.,600.,1000.});
+	    tSelect.setTemperatures(new double[] {0.3,0.5,0.7,1.0,1.3,2.0,3.0});
+	    tSelect.setUnit(tUnit);
+	    tSelect.setSelected(0); //sets adiabatic as selected temperature
+	    tSelect.getLabel().setText("Set value");
+	    
+	    //display of phase, timer
+	    final DisplayPhase displayPhase = new DisplayPhase(sim.phase,sim.getDefaults().pixelUnit);
+        ColorSchemeByType colorScheme = new ColorSchemeByType();
+        colorScheme.setColor(sim.species.getMoleculeType(),java.awt.Color.red);
+        displayPhase.setColorScheme(new ColorSchemeByType());
+        sim.integrator.addListener(new IntervalActionAdapter(new Action() {
+            public void actionPerformed() {displayPhase.repaint();}
+            public String getLabel() {return "Phase";}
+        }));
+        
+   /*     DisplayTimer timer = new DisplayTimer(integrator);
+        timer.setLabelType(DisplayBox.BORDER);
+        timer.setUnit(new Unit(LennardJones.Time.UNIT));
+	*/    
+	    //meters and displays
+        MeterRDF rdfMeter = new MeterRDF(sim.space);
+        rdfMeter.getXDataSource().setXMax(4.0);
+        rdfMeter.setPhase(sim.phase);
+        AccumulatorAverage rdfAverage = new AccumulatorAverage(20);
+        DataPump rdfPump = new DataPump(rdfMeter,rdfAverage);
+        IntervalActionAdapter rdfAdapter =  new IntervalActionAdapter(rdfPump);
+        rdfAdapter.setActionInterval(50);
+        rdfAverage.setPushInterval(20);
+        sim.integrator.addListener(rdfAdapter);
+        sim.register(rdfMeter,rdfPump);
+        
+        DisplayPlot rdfPlot = new DisplayPlot();
+        rdfPlot.setXSource(rdfMeter.getXDataSource());
+        rdfAverage.addDataSink(rdfPlot.getDataTable(),new StatType[]{StatType.AVERAGE});
+        rdfAverage.setPushInterval(1);
+        rdfPlot.getPlot().setTitle("Radial Distribution Function");
+		
+		//add meter and display for current kinetic temperature
+		sim.getDefaults().historyPeriod = 1000;
+
+		
+		//velocity distribution
+        double vMin = 0;
+        double vMax = 4;
+		DataSourceRmsVelocity meterVelocity = new DataSourceRmsVelocity();
+        meterVelocity.setIterator(new AtomIteratorLeafAtoms(sim.phase));
+        AccumulatorHistogram velocityHistogram = new AccumulatorHistogram();
+        DataPump velocityPump = new DataPump(meterVelocity, velocityHistogram);
+        velocityHistogram.getHistograms()[0].setXRange(new DoubleRange(vMin,vMax));
+        velocityHistogram.getHistograms()[0].setAutoScale(false);
+        IntervalActionAdapter velocityAdapter = new IntervalActionAdapter(velocityPump);
+        velocityHistogram.setPushInterval(10);
+        sim.integrator.addListener(velocityAdapter);
+        velocityAdapter.setActionInterval(10);
+        sim.register(meterVelocity,velocityPump);
+        
+        final DisplayPlot vPlot = new DisplayPlot();
+        velocityHistogram.addDataSink(vPlot.getDataTable());
+        vPlot.setDoLegend(false);
+//      vPlot.getPlot().setYRange(0.0,0.8);
+        vPlot.getPlot().setTitle("Velocity distribution");
+		
+		final MaxwellBoltzmann.Distribution mbDistribution = new MaxwellBoltzmann.Distribution(sim.space,sim.integrator.getTemperature(),((AtomTypeLeaf)sim.species.getMoleculeType()).mass);
+		final DataSourceFunction mbSource = new DataSourceFunction(mbDistribution);
+		DataSourceUniform mbX = mbSource.getXSource();
+		mbX.setTypeMax(LimitType.HALF_STEP);
+		mbX.setTypeMin(LimitType.HALF_STEP);
+		mbX.setNValues(velocityHistogram.getNBins());
+		mbX.setXMin(vMin);
+		mbX.setXMax(vMax);
+		mbSource.update();
+        DataPump mbPump = new DataPump(mbSource,vPlot.getDataTable());
+        IntervalActionAdapter mbAdapter = new IntervalActionAdapter(mbPump);
+        mbAdapter.setActionInterval(100);
+        sim.integrator.addListener(mbAdapter);
+
+		//listner to update Maxwell-Boltzmann plot when temperature is changed
+        tSelect.getSelector().addItemListener(new java.awt.event.ItemListener() {
+		    public void itemStateChanged(java.awt.event.ItemEvent event) {
+		        Object item = event.getItem();
+		        if(item instanceof Double) {
+		            mbDistribution.setTemperature(((Double)item).doubleValue());
+		            mbSource.update();
+		            vPlot.doUpdate();
+		            vPlot.repaint();
+		        }
+		    }
+		});//end of addItemListener
+		
+		
+/*		double[] xHist = vHistogram.xValues();
+		double[] xMB = mbSource.xValues();
+		for(int i=0; i<xHist.length; i++) {
+		    System.out.println(xHist[i]+"  "+xMB[i]);
+		}
+*/		
+		
+		MeterTemperature thermometer = new MeterTemperature();
+        thermometer.setPhase(sim.phase);
+        DataFork temperatureFork = new DataFork();
+        DataPump temperaturePump = new DataPump(thermometer,temperatureFork);
+        IntervalActionAdapter temperatureAdapter = new IntervalActionAdapter(temperaturePump);
+        temperatureAdapter.setActionInterval(10);
+        sim.integrator.addListener(temperatureAdapter);
+        sim.register(meterVelocity,velocityPump);
+        AccumulatorHistory temperatureHistory = new AccumulatorHistory();
+		DisplayBox tBox = new DisplayBox();
+		temperatureFork.setDataSinks(new DataSink[]{tBox,temperatureHistory});
+		tBox.setUnit(tUnit);
+		tBox.setLabel("Measured value");
+		tBox.setLabelPosition(CompassDirection.NORTH);
+		
+	    MeterDensity densityMeter = new MeterDensity(sim.space.D());
+        densityMeter.setPhase(sim.phase);
+	    DisplayBox densityBox = new DisplayBox();
+        DataPump densityPump = new DataPump(densityMeter, densityBox);
+        IntervalActionAdapter densityAdapter = new IntervalActionAdapter(densityPump);
+        densityAdapter.setActionInterval(10);
+        sim.integrator.addListener(densityAdapter);
+        sim.register(densityMeter,densityPump);
+	    densityBox.setLabel("Number density");
+	    
+		MeterEnergy eMeter = new MeterEnergy(sim.potentialMaster);
+        eMeter.setPhase(sim.phase);
+        AccumulatorHistory energyHistory = new AccumulatorHistory();
+        DataPump energyPump = new DataPump(eMeter, energyHistory);
+        IntervalActionAdapter energyAdapter = new IntervalActionAdapter(energyPump);
+        energyAdapter.setActionInterval(10);
+        energyHistory.setPushInterval(10);
+        sim.integrator.addListener(energyAdapter);
+        sim.register(eMeter,energyPump);
+		
+		MeterPotentialEnergy peMeter = new MeterPotentialEnergy(sim.potentialMaster);
+        peMeter.setPhase(sim.phase);
+        AccumulatorHistory peHistory = new AccumulatorHistory();
+        AccumulatorAverage peAccumulator = new AccumulatorAverage(sim);
+        DataFork peFork = new DataFork(new DataSink[]{peHistory, peAccumulator});
+        DataPump pePump = new DataPump(peMeter, peFork);
+        IntervalActionAdapter peAdapter = new IntervalActionAdapter(pePump);
+        peAdapter.setActionInterval(10);
+        peHistory.setPushInterval(10);
+        sim.register(peMeter,pePump);
+        sim.integrator.addListener(peAdapter);
+		
+		MeterKineticEnergy keMeter = new MeterKineticEnergy();
+        keMeter.setPhase(sim.phase);
+        AccumulatorHistory keHistory = new AccumulatorHistory();
+        DataPump kePump = new DataPump(keMeter, keHistory);
+        IntervalActionAdapter keAdapter = new IntervalActionAdapter(kePump);
+        keAdapter.setActionInterval(10);
+        keHistory.setPushInterval(10);
+        sim.register(keMeter,kePump);
+        sim.integrator.addListener(keAdapter);
+        
+        DisplayPlot ePlot = new DisplayPlot();
+        energyHistory.setDataSink(ePlot.getDataTable());
+        peHistory.setDataSink(ePlot.getDataTable());
+        keHistory.setDataSink(ePlot.getDataTable());
+		
+		ePlot.setDoLegend(true);
+		
+		MeterPressure pMeter = new MeterPressure(sim.potentialMaster,sim.space);
+        pMeter.setPhase(sim.phase);
+        AccumulatorAverage pAccumulator = new AccumulatorAverage(sim);
+        DataPump pPump = new DataPump(pMeter,pAccumulator);
+        IntervalActionAdapter pAdapter = new IntervalActionAdapter(pPump);
+        pAdapter.setActionInterval(10);
+        sim.register(pMeter,pPump);
+        sim.integrator.addListener(pAdapter);
+        
+        DisplayBoxesCAE pDisplay = new DisplayBoxesCAE();
+        pDisplay.setAccumulator(pAccumulator);
+        DisplayBoxesCAE peDisplay = new DisplayBoxesCAE();
+        peDisplay.setAccumulator(peAccumulator);
+
+        DeviceNSelector nSlider = new DeviceNSelector(sim,sim.species.getAgent(sim.phase));
+        nSlider.setMinimum(1);
+        nSlider.setMaximum(224);
+        nSlider.setLabel("Number of atoms");
+        nSlider.setShowBorder(true);
+
+        //************* Lay out components ****************//
+        
+        //tabbed pane for the big displays
+        javax.swing.JPanel bigPanel = new javax.swing.JPanel();
+        
+    	final javax.swing.JTabbedPane displayPanel = new javax.swing.JTabbedPane();
+    	try {
+        	((javax.swing.JComponent)displayPhase.graphic()).setPreferredSize(new java.awt.Dimension(300,300));
+        } catch(ClassCastException e) {}
+        displayPhase.setScale(0.7);
+        bigPanel.add(displayPhase.graphic());
+        bigPanel.add(displayPanel);
+        
+//    	javax.swing.JPanel displayPhasePanel = new javax.swing.JPanel();//3d
+//    	displayPhasePanel.add(displayPhase.graphic());//3d
+//    	displayPanel.add(displayPhase.getLabel(), displayPhasePanel);//3d
+        javax.swing.JPanel thermoPanel = new javax.swing.JPanel();
+        thermoPanel.add(pDisplay.graphic());
+        thermoPanel.add(peDisplay.graphic());
+        
+        displayPanel.add("Thermo",thermoPanel);
+    	displayPanel.add("RDF", rdfPlot.graphic());
+    	displayPanel.add("Velocity",vPlot.graphic());
+    	
+    	javax.swing.JPanel energyPanel = new javax.swing.JPanel(new java.awt.GridLayout(0,1));
+ /*   	pePlot.getPlot().setSize(300,200);
+    	kePlot.getPlot().setSize(300,200);
+    	pePlot.getPlot().setTopPadding(0);
+    	kePlot.getPlot().setTopPadding(0);
+    	pePlot.getPlot().setTitle("Potential");
+    	kePlot.getPlot().setTitle("Kinetic");
+ */   //	energyPanel.add(pePlot.graphic());
+    //	energyPanel.add(kePlot.graphic());
+        ePlot.getPlot().setTitle("Energy History");
+        energyPanel.add(ePlot.graphic());
+    	displayPanel.add("Energy",energyPanel);
+//        displayPanel.add(plot.getLabel(), plot.graphic());
+        
+        
+        displayPanel.addChangeListener(
+            new javax.swing.event.ChangeListener() {
+                public void stateChanged(javax.swing.event.ChangeEvent event) {
+                    displayPanel.invalidate();
+                    displayPanel.validate();
+                }
+        });
+        
+        //panel for the start buttons
+        JPanel startPanel = (JPanel)control.graphic();
+        
+        //panel for the temperature control/display
+        JPanel temperaturePanel = new JPanel(new java.awt.GridBagLayout());
+//        temperaturePanel.setBorder(new javax.swing.border.TitledBorder("Temperature (K)"));
+        temperaturePanel.setBorder(new javax.swing.border.TitledBorder("Temperature (\u03B5)"));
+        java.awt.GridBagConstraints gbc1 = new java.awt.GridBagConstraints();
+        gbc1.gridx = 0;  gbc1.gridy = 1;
+        gbc1.gridwidth = 1;
+        temperaturePanel.add(tSelect.graphic(null),gbc1);
+        gbc1.gridx = 1;  gbc1.gridy = 1;
+        temperaturePanel.add(tBox.graphic(null),gbc1);
+        
+   //     JPanel timePanel = (JPanel)timer.graphic();
+   //     timePanel.setBorder(new javax.swing.border.TitledBorder("Time (LJ)"));
+  //      timePanel.add(timer.graphic());
+        
+ //   	final javax.swing.JTabbedPane sliderPanel = new javax.swing.JTabbedPane();
+ //       sliderPanel.add("Number of atoms",nSlider.graphic());
+        //panel for all the controls
+//        JPanel controlPanel = new JPanel(new java.awt.GridLayout(0,1));
+        JPanel controlPanel = new JPanel(new java.awt.GridBagLayout());
+        java.awt.GridBagConstraints gbc2 = new java.awt.GridBagConstraints();
+//        gbc2.gridx = 0;
+//        gbc2.gridy = java.awt.GridBagConstraints.RELATIVE;
+        gbc2.gridy = 0;
+        gbc2.gridx = java.awt.GridBagConstraints.RELATIVE;
+        controlPanel.add(startPanel,gbc2);
+    //    controlPanel.add(timer.graphic(),gbc2);
+    //    controlPanel.add(timePanel,gbc2);
+        controlPanel.add(temperaturePanel, gbc2);
+        controlPanel.add(nSlider.graphic(), gbc2);
+ //       controlPanel.add(sliderPanel, gbc2);
+        controlPanel.add(densityBox.graphic(), gbc2);
+ //       controlPanel.add(pressureBox.graphic(), gbc2);
+        
+        panel.add(controlPanel, java.awt.BorderLayout.NORTH);
+        panel.add(bigPanel, java.awt.BorderLayout.EAST);
+//        panel().add(displayPanel, java.awt.BorderLayout.EAST);
+		
+		//***************set all the colors******************
+		java.awt.Color background = ConstantsGraphic.KHAKI.brighter().brighter();
+		java.awt.Color contrast = ConstantsGraphic.DARK_RED;
+		java.awt.Color sliderColor = ConstantsGraphic.TAN;
+	//	java.awt.Color tabColor = java.awt.Color.black;
+	    java.awt.Color tabColor = ConstantsGraphic.DARK_RED;
+		java.awt.Color tabTextColor = background;
+		java.awt.Color buttonColor = tabColor;
+		java.awt.Color buttonTextColor = tabTextColor;
+		java.awt.Color panelColor = ConstantsGraphic.TAN;
+		panel.setBackground(background);
+		bigPanel.setBackground(background);
+
+		controlPanel.setBackground(background);
+
+		startPanel.setBackground(contrast); //border color
+		startPanel.setOpaque(false);
+
+//		timePanel.setBackground(contrast); //border color
+//		timePanel.setOpaque(false);
+		temperaturePanel.setBackground(contrast); //border color
+		temperaturePanel.setOpaque(false);
+		tBox.graphic(null).setBackground(background);
+		tSelect.graphic(null).setBackground(background);
+		densityBox.graphic().setBackground(background);
+		nSlider.graphic().setBackground(background);
+//		pressureBox.graphic().setBackground(background);
+		
+    }//end of constructor    
+    
+    public static void main(String[] args) {
+        Space space = Space2D.getInstance();
+        if(args.length != 0) {
+            try {
+                int D = Integer.parseInt(args[0]);
+                if (D == 3) {
+                    space = Space3D.getInstance();
+                }
+            } catch(NumberFormatException e) {}
+        }
+            
+        Ljmd sim = new Ljmd(space);
+
+        LjmdGraphic ljmdGraphic = new LjmdGraphic(sim);
+		SimulationGraphic.makeAndDisplayFrame(ljmdGraphic.panel);
+    }//end of main
+    
+    public static class Applet extends javax.swing.JApplet {
+
+	    public void init() {
+	        getRootPane().putClientProperty(
+	                        "defeatSystemEventQueueCheck", Boolean.TRUE);
+		    getContentPane().add(new LjmdGraphic(new Ljmd(Space2D.getInstance())).panel);
+	    }
+    }//end of Applet
+    
+    protected JPanel panel;
+}//end of LJMD_Module class
+
+
