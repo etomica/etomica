9c52df59588ef659e18a6db6d5f11370a394c139
Andrew Schultz <ajs42@buffalo.edu>
2006-03-07 19:41:02 +0000

    Change DataSourceRmsVelocity to return a histogram of velocities instead of the average.  Back when this was actually used, histogramming was still being done internally.

--- etomica/data/DataSourceRmsVelocity.java
+++ etomica/data/DataSourceRmsVelocity.java
@@ -1,10 +1,18 @@
 package etomica.data;
 
-import etomica.EtomicaElement;
+import etomica.atom.Atom;
 import etomica.atom.AtomLeaf;
 import etomica.atom.iterator.AtomIterator;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataFunction;
 import etomica.space.ICoordinateKinetic;
-import etomica.units.Undefined;
+import etomica.units.DimensionRatio;
+import etomica.units.Length;
+import etomica.units.Null;
+import etomica.units.Time;
+import etomica.util.DoubleRange;
+import etomica.util.Histogram;
 
 /**
  * Meter for the root-mean-square velocity of a set of atoms. Useful to obtain
@@ -13,34 +21,59 @@ import etomica.units.Undefined;
  * @author David Kofke
  */
 
-public class DataSourceRmsVelocity extends DataSourceScalar implements EtomicaElement {
+public class DataSourceRmsVelocity implements DataSourceAtomic {
 
-	//TODO define a dimension for this property
-	public DataSourceRmsVelocity() {
-		super("RMS Velocity", Undefined.DIMENSION);
-	}
+	public DataSourceRmsVelocity(Histogram histogram) {
+        atomData = new DataDouble("RMS Velocity", new DimensionRatio(Length.DIMENSION, Time.DIMENSION));
+        this.histogramRMS = histogram;
+        setupData();
+    }
+    
+    public DataInfo getDataInfo() {
+        return data.getDataInfo();
+    }
 
 	/**
 	 * Returns the rms velocity of the atoms given by the iterator. Value is
 	 * given in the first element of the array, which is always of dimension 1.
 	 */
-	public double getDataAsScalar() {
+	public Data getData() {
 		iterator.reset();
-		int count = 0;
-		double value = 0.0;
+        histogramRMS.reset();
 		while (iterator.hasNext()) {
 			AtomLeaf atom = (AtomLeaf)iterator.nextAtom();
-			value += Math.sqrt(((ICoordinateKinetic)atom.coord).velocity().squared());
-			count++;
+			histogramRMS.addValue(Math.sqrt(((ICoordinateKinetic)atom.coord).velocity().squared()));
 		}
-		value /= count;
-		return value;
+
+        //covertly invoke getHistogram, which actually calculates the histogram
+        // our DataFunction wraps the histogram array
+        if (data.getYData().getData() != histogramRMS.getHistogram() ||
+                data.getXData(0).getData() != histogramRMS.xValues()) {
+            // we wrap the histogram inner array instances in the DataFunction.
+            // if they change, we need a new instance of the DataFunction
+            setupData();
+        }
+        
+		return data;
 	}
     
-    public int getDataLength() {
-        return 1;
+    /**
+     * Creates the data object (a DataFunction) to be returned by getData().  
+     * data wraps the histogram's double[] so copying is not needed.
+     */
+    protected void setupData() {
+        int nBins = histogramRMS.getNBins();
+        DataDoubleArray xData = new DataDoubleArray(atomData.getDataInfo().getLabel(),atomData.getDataInfo().getDimension(), 
+                                                    new int[]{nBins},histogramRMS.xValues());
+        DataDoubleArray yData = new DataDoubleArray("Histogram",Null.DIMENSION,new int[]{nBins},histogramRMS.getHistogram());
+        data = new DataFunction(new DataDoubleArray[]{xData}, yData);
     }
-
+    
+    public Data getData(Atom a) {
+        atomData.x = Math.sqrt(((ICoordinateKinetic)((AtomLeaf)a).coord).velocity().squared());
+        return atomData;
+    }        
+    
 	/**
 	 * Sets the iterator defining the atoms for which the RMS velocity is
 	 * calculated.
@@ -63,5 +96,8 @@ public class DataSourceRmsVelocity extends DataSourceScalar implements EtomicaEl
 	}
 
 	private AtomIterator iterator = AtomIterator.NULL;
+    private final DataDouble atomData;
+    private final Histogram histogramRMS;
+    private DataFunction data;
 
 }//end of DataSourceVelocityRms
