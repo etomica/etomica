b53e9166c1497bb575d100b2670bd2f642c6971a
Andrew Schultz <ajs42@buffalo.edu>
2006-03-09 22:29:44 +0000

    be vewy quiet

--- etomica/potential/P1Harmonic.java
+++ etomica/potential/P1Harmonic.java
@@ -41,7 +41,6 @@ public class P1Harmonic extends Potential1 implements PotentialSoft {
 
     public void setSpringConstant(double springConstant) {
         w = springConstant;
-        System.out.println("spring constant " + w);
     }
     
     public double getSpringConstant() {
@@ -50,7 +49,6 @@ public class P1Harmonic extends Potential1 implements PotentialSoft {
     
     public void setX0(Vector x0) {
         this.x0.E(x0);
-        System.out.println(x0);
     }
     
     public Vector getX0() {
@@ -70,16 +68,6 @@ public class P1Harmonic extends Potential1 implements PotentialSoft {
 
     public double energy(AtomSet a) {
         return 0.5*w*((AtomLeaf)a).coord.position().Mv1Squared(x0);
-//        r.Mv1Squared(x0);
-//        Vector d = ((Atom)a).node.parentPhase().boundary().dimensions();
-//        double aSum = 0.0;
-//        for(int i=0; i<D; i++) {
-//            double x = r.x(i) - x0;
-//            double dx = d.x(i);
-//            if(x > 0.5*dx) x = dx - x;
-//            aSum += x*x;
-//        }
-//        return 0.5*w*aSum;
     }
     
     //XXX consider whether 1-body potentials should contribute to virial
@@ -89,21 +77,11 @@ public class P1Harmonic extends Potential1 implements PotentialSoft {
 
     public Vector gradient(AtomSet a){
         Vector r = ((AtomLeaf)a).coord.position();
-//        Vector d = ((Atom)a).node.parentPhase().boundary().dimensions();
         force.Ev1Mv2(r,x0);
         force.TE(w);
-//        for(int i=0; i<D; i++) {
-//            double x = r.x(i) - x0;
-//            double dx = d.x(i);
-//            if(x > 0.5*dx) {
-//                x = dx - x;
-//                force.setX(i, -w*x);
-//            }
-//            else force.setX(i, w*x);
-//        }
             
         return force;
-    }//end of gradient
+    }
         
-}//end of P1Harmonic
+}
    
