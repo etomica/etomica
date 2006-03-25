4619d3b64b97a71e7088abff3b83f4648b602676
Andrew Schultz <ajs42@buffalo.edu>
2006-03-25 00:27:16 +0000

    Major cleanup.  Make pivotting work, eliminate helper methods, fix indenting, update matrix appropriately (the parts that got borked before were accidentally unimportant).  Add comments.

--- etomica/math/linearalgebra/GaussJordanElimination.java
+++ etomica/math/linearalgebra/GaussJordanElimination.java
@@ -1,94 +1,122 @@
 package etomica.math.linearalgebra;
 
-//probably wrong.  See sWAP method.
+//maybe wrong
 //who wrote this?
 public class GaussJordanElimination implements java.io.Serializable {
     
-    private int []indxc;
-    private int []indxr;
-    private int []ipiv;
+    private GaussJordanElimination(){}
     
-    private int    icol,irow,j,k,l,ll;
-    private double big,dum,pivinv;
-    
-    public double [][]aa = new double [3][3];
-    public double [][]bb = new double[3][1];
-    
-    public GaussJordanElimination(){}
-    
-    
-    public void gaussj(double [][]a,int n,double [][]b,int m){
+    /**
+     * Performs Gauss-Jordan elimination in place to solve Ax=b.  A is
+     * an nxn matrix, b is an nxm vector.
+     */
+    public static void gaussj(double [][]a,int n,double [][]b,int m){
         
-        indxc = new int [n];
-        indxr = new int [n];
-        ipiv  = new int [n];
+        int[] indxc = new int[n];
+        int[] indxr = new int[n];
+        boolean[] ipiv  = new boolean[n];
         
-        for(j = 0 ;j < n;j++)
-            ipiv[j] = 0;
-        
+        for(int j = 0; j < n; j++)
+            ipiv[j] = false;
         
         for(int i = 0 ; i < n; i++){
-            big = 0.0;
-            for(j = 0;j < n;j++)
-                if(ipiv[j] != 1.0)
-                for(k = 0;k < n;k++){
-                    if(ipiv[k] == 0){
-                        if(Math.abs(a[j][k]) >= big){
-                            big = Math.abs(a[j][k]);
-                            irow = j;
-                            icol = k;
+            double max = -1;
+            int icol = -1;
+            int irow = -1;
+            
+            // loop over rows
+            for(int j = 0; j < n; j++) {
+                // if we haven't already pivotted this row
+                if(!ipiv[j]) {
+                    // loop over cols
+                    for(int k = 0; k < n; k++){
+                        // if we haven't already pivotted the row we'd swap with
+                        // if this was the max
+                        if(!ipiv[k]){
+                            if(Math.abs(a[j][k]) >= max){
+                                max = Math.abs(a[j][k]);
+                                irow = j;
+                                icol = k;
+                            }
                         }
                     }
-                    else{
-                        if(ipiv[k] > 1){System.err.println("gaussj:Singular Matrix-1");}
-                    }
-                
+                }
             }
-        
-                    ++(ipiv[icol]);
-                    
-                    if(irow != icol){
-                        for(l = 0;l < n; l++)
-                            sWAP(a[irow][l],a[icol][l]);
-                        
-                        for(l = 0;l < m; l++)
-                            sWAP(b[irow][l],b[icol][l]);
-                        
+
+            if (max == 0) {
+                // singular matrix.
+                for (int j=0; j<n; j++) {
+                    for (int k=0; k<m; k++) {
+                        b[j][k] = Double.NaN;
                     }
+                }
+                return;
+            }
                     
-                    indxr[i] = irow;
-                    indxc[i] = icol;
-                    if(a[icol][icol] == 0.0){System.err.println("gaussj:Singular Matrix-2");}
-                    pivinv = 1.0/a[icol][icol];
-                    a[icol][icol] = 1.0;
-                    for(l = 0;l < n;l++)a[icol][l]*= pivinv;
-                    for(l = 0;l < m;l++)b[icol][l]*= pivinv;
-                    
-                    for(ll = 0;ll < n;ll++)
-                        if(ll != icol){
-                            dum = a[ll][icol];
-                            a[ll][icol] = 0.0;
-                            for(l = 0;l < n;l++){a[ll][l] -= a[icol][l]*dum;}
-                            for(l = 0;l < m;l++){b[ll][l] -= b[icol][l]*dum;}  
-                        }
+            // mark the column as having been pivotted
+            ipiv[icol] = true;
+
+            if(irow != icol){
+                //swap rows irow and icol
+                for(int l = 0;l < n; l++) {
+                    double temp = a[irow][l];
+                    a[irow][l] = a[icol][l];
+                    a[icol][l] = temp;
+                }
+                
+                for(int l = 0;l < m; l++) {
+                    double temp = b[irow][l];
+                    b[irow][l] = b[icol][l];
+                    b[icol][l] = temp;
+                }
+            }
+
+            // remember pivotting scheme... for the ith step, we swapped
+            // irow and icol!
+            indxr[i] = irow;
+            indxc[i] = icol;
+
+            double pivinv = 1.0/a[icol][icol];
+            // divide the icol row by the pivot value
+            for(int l = 0;l < n;l++) {
+                a[icol][l]*= pivinv;
+            }
+            for(int l = 0;l < m;l++) {
+                b[icol][l]*= pivinv;
+            }
+
+            // subtract the icol row from the other rows (times a constant so 
+            // that the icol element of a becomes 0).
+            for(int ll = 0;ll < n;ll++) {
+                if(ll != icol){
+                    double dum = a[ll][icol];
+                    for(int l = 0;l < n; l++){
+                        a[ll][l] -= a[icol][l]*dum;
                     }
-         
-                    for(l = n-1;l >= 0;l--){
-                        if(indxr[l] != indxc[l])
-                            for(k = 0;k < n;k++)
-                                sWAP(a[k][indxr[l]],a[k][indxc[l]]);
-                            
-                        
-                    }
-       }
-    
-    public void sWAP(double a, double b){
-        double temp = a;
-        a = b;
-        b = temp;
+                    for(int l = 0;l < m; l++){
+                        b[ll][l] -= b[icol][l]*dum;
+                    }  
+                }
+            }
+        }
+ 
+        for(int l = n-1;l >= 0;l--){
+            // unswap the rows back to where they came from
+            if(indxr[l] != indxc[l]) {
+                int irl = indxr[l];
+                int icl = indxc[l];
+                for(int k = 0;k < n;k++) {
+                    double temp = a[k][irl];
+                    a[k][irl] = a[k][icl];
+                    a[k][icl] = temp;
+                }
+            }
+        }
     }
     
-    public void definition(){
+    public static void main(String[] args) {
+        double[][] aa = new double[3][3];
+        double[][] bb = new double[3][1];
         aa[0][0] = 2.0;
         aa[0][1] = 1.0;
         aa[0][2] = 1.0;
@@ -101,19 +129,12 @@ public class GaussJordanElimination implements java.io.Serializable {
         bb[0][0] = 1.0;
         bb[1][0] = -2.0;
         bb[2][0] = 7.0;
-    }
-    
-        public void output(){
-            for(int i = 0;i < 3;i++){
-             System.out.println(bb[i][0]+"\t"+i);
-            }
+        
+        GaussJordanElimination.gaussj(aa, 3, bb, 1);
+
+        for(int i = 0;i < 3;i++){
+            System.out.println(bb[i][0]+"\t"+i);
         }
-    
-    public static void main(String[] args) {
-        GaussJordanElimination gje = new GaussJordanElimination();
-        gje.definition();
-        gje.gaussj(gje.aa,3,gje.bb,1);
-        gje.output();
     }
     
 }
