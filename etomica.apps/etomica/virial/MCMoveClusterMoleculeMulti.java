ec8dae09dcdc88c4e67194c84513d22ec4f65612
Andrew Schultz <ajs42@buffalo.edu>
2006-04-04 19:45:26 +0000

    update getA/B stuff

--- etomica.apps/etomica/virial/MCMoveClusterMoleculeMulti.java
+++ etomica.apps/etomica/virial/MCMoveClusterMoleculeMulti.java
@@ -23,32 +23,32 @@ public class MCMoveClusterMoleculeMulti extends MCMoveMolecule {
     }
     
     /**
-	 * Constructor for MCMoveAtomMulti.
-	 * @param parentIntegrator
-	 * @param nAtoms number of atoms to move in a trial.  Number of atoms in
-	 * phase should be at least one greater than this value (greater
-	 * because first atom is never moved)
-	 */
-	public MCMoveClusterMoleculeMulti(PotentialMaster potentialMaster, 
-			double stepSize, int nAtoms) {
-		super(potentialMaster,stepSize,Double.POSITIVE_INFINITY,false);
-		this.nAtoms = nAtoms;
-		selectedAtoms = new Atom[nAtoms];
+     * Constructor for MCMoveAtomMulti.
+     * @param parentIntegrator
+     * @param nAtoms number of atoms to move in a trial.  Number of atoms in
+     * phase should be at least one greater than this value (greater
+     * because first atom is never moved)
+     */
+    public MCMoveClusterMoleculeMulti(PotentialMaster potentialMaster, 
+            double stepSize, int nAtoms) {
+        super(potentialMaster,stepSize,Double.POSITIVE_INFINITY,false);
+        this.nAtoms = nAtoms;
+        selectedAtoms = new Atom[nAtoms];
         translationVectors = new Vector[nAtoms];
         for (int i=0; i<nAtoms; i++) {
             translationVectors[i] = potential.getSpace().makeVector();
         }
         weightMeter = new MeterClusterWeight(potential);
         setName("MCMoveClusterMolecule");
-	}
+    }
 
     public void setPhase(Phase p) {
         super.setPhase(p);
         weightMeter.setPhase(p);
     }
     
-	//note that total energy is calculated
-	public boolean doTrial() {
+    //note that total energy is calculated
+    public boolean doTrial() {
         if (selectedAtoms[0] == null) selectMolecules();
         uOld = weightMeter.getDataAsScalar();
         for(int i=0; i<selectedAtoms.length; i++) {
@@ -60,10 +60,9 @@ public class MCMoveClusterMoleculeMulti extends MCMoveMolecule {
         ((PhaseCluster)phase).trialNotify(null);
         uNew = Double.NaN;
         return true;
-	}
+    }
 	
-	// inefficient if selecting most of a large set of atoms
-	protected Atom[] selectMolecules() {
+    protected Atom[] selectMolecules() {
         AtomIteratorAllMolecules iterator = new AtomIteratorAllMolecules(phase);
         if (iterator.size()-1 != nAtoms) throw new IllegalStateException("move should work on number of molecules in phase-1");
         iterator.reset();
@@ -72,39 +71,32 @@ public class MCMoveClusterMoleculeMulti extends MCMoveMolecule {
         while (iterator.hasNext()) {
             selectedAtoms[i++] = iterator.nextAtom();
         }
-		return selectedAtoms;
-	}
+        return selectedAtoms;
+    }
 	
-	public void rejectNotify() {
-//        System.out.println("multi rejected");
+    public void rejectNotify() {
         for(int i=0; i<selectedAtoms.length; i++) {
             groupTranslationVector.E(translationVectors[i]);
             groupTranslationVector.TE(-1);
             moveMoleculeAction.actionPerformed(selectedAtoms[i]);
         }
         ((PhaseCluster)phase).rejectNotify();
-	}
+    }
 
     public void acceptNotify() {
-//        System.out.println("multi accepted");
         ((PhaseCluster)phase).acceptNotify();
     }
-    public double trialRatio() {return 1.0;}
     
     public double getB() {
-        return Math.log(probabilityRatio());
+        return 0.0;
     }
     
-    public double probabilityRatio() {
+    public double getA() {
         uNew = weightMeter.getDataAsScalar();
-//        System.out.println("multi uOld "+uOld+" uNew "+uNew);
         return (uOld==0.0) ? Double.POSITIVE_INFINITY : uNew/uOld;
     }
-    
 	
-	
-	private final int nAtoms;
-	private final Atom[] selectedAtoms;
+    private final int nAtoms;
+    private final Atom[] selectedAtoms;
     private final Vector[] translationVectors;
-
 }
