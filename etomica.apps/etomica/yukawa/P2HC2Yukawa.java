a42a41ef8df6c2bb0f90d7f74f8f4d767a1a37ac
Mike Sellers <msellers@buffalo.edu>
2006-06-29 18:13:03 +0000

    Fixed EXTENDS and IMPLEMENTS for Potential2 and PotentialSoftSpherical.  uInt returns zero.

---
+++ etomica.apps/etomica/yukawa/P2HC2Yukawa.java
@@ -0,0 +1,132 @@
+package etomica.yukawa;
+import etomica.EtomicaElement;
+import etomica.EtomicaInfo;
+import etomica.atom.AtomLeaf;
+import etomica.atom.AtomPair;
+import etomica.atom.AtomSet;
+import etomica.phase.Phase;
+import etomica.potential.Potential2;
+import etomica.potential.Potential2Spherical;
+import etomica.simulation.Simulation;
+import etomica.space.NearestImageTransformer;
+import etomica.space.Space;
+import etomica.space.Vector;
+
+/**
+ * Hard-core plus two Yukawa fluid (HC2Yukawa): A Lennard Jones like potential.
+ * 
+ * ============================================================================================================================
+ * 2001. Pini, Stell, and Wilding. "Liquid-gas phase behaviour of an argon-like fluid modelled by the hard-core two-Yukawa potential"
+ * ============================================================================================================================
+ * 	
+ * 			| infinity																		r <= sigma
+ * U(r) =	| 
+ * 			|(A1/r) * exp[-z1 * (r - sigma)] - (A2 * epsilon/r) * exp[-z2 * (r - sigma)]	r > sigma
+ * 
+ * 	LJ behavior parameters:	A1 = 1.6438 * sigma
+ * 							z1 = 14.7 / sigma
+ * 							A2 = 2.03 * sigma
+ * 							z2 = 2.69 / sigma
+ * 
+ * @author msellers
+ */
+
+public final class P2HC2Yukawa extends Potential2 implements Potential2Spherical, EtomicaElement {
+
+	public P2HC2Yukawa(Simulation sim){
+		this(sim.space, sim.getDefaults().atomSize, sim.getDefaults().potentialWell);
+	}
+	
+	public P2HC2Yukawa(Space space, double sigma, double epsilon){
+		super(space);
+		
+		dr = space.makeVector();
+		setSigma(sigma);
+		setEpsilon(epsilon);
+		setParameters(sigma);
+	}
+	
+	public double getRange(){return Double.POSITIVE_INFINITY;}
+	
+	public void setPhase(Phase phase) {
+		nearestImageTransformer = phase.getBoundary();
+	}
+	
+	public static EtomicaInfo getEtomicaInfo() {
+		EtomicaInfo info = new EtomicaInfo("Hard-core plus two Yukawa fluid potential");
+		return info;
+	}
+	
+	/**
+	 * Energy method.  u(double r) form.
+	 */
+	public double u(double r2){
+		double r = Math.sqrt(r2);
+		//hard core repulsion
+		if (r <= sigma){
+			return Double.POSITIVE_INFINITY;
+		}
+		
+		//two-Yukawa tail attraction
+		double expRsigma = Math.exp(r - sigma);
+		double uterm1 = (A1 / r) * expZ1 * expRsigma;
+		double uterm2 = (A2 * epsilon / r) * expZ2 * expRsigma;
+		
+		return (uterm1 - uterm2);
+	}
+		
+    /**
+     * Energy of the pair as given by the u(double) method
+     */
+    public double energy(AtomSet atoms) {
+        AtomPair pair = (AtomPair)atoms;
+        dr.Ev1Mv2(((AtomLeaf)pair.atom1).coord.position(),((AtomLeaf)pair.atom0).coord.position());
+        nearestImageTransformer.nearestImage(dr);
+        return u(dr.squared());
+    }
+	
+    /**
+     * Integral from rC to infinity.
+     */
+	public double uInt(double rC){
+		
+		return 0;
+	}
+
+	/**
+	 * Accessor methods for size and energy parameters.
+	 */
+	public double getSigma() {return sigma;}
+	
+	public double getEpsilon() {return epsilon;}
+	
+	/**
+	 * Mutator methods for size and energy parameters.
+	 */
+	public final void setEpsilon(double eps) {epsilon = eps;}
+	
+	public final void setSigma(double s) {sigma = s;}
+	
+	/**
+	 * Parameter calculation method.
+	 */
+	public final void setParameters(double s){
+		A1 = 1.6438 * s;
+		A2 = 2.03 * s;
+		z1 = 14.7 / s;
+		z2 = 2.69 / s;
+		expZ1 = Math.exp(-z1);
+		expZ2 = Math.exp(-z2);
+	}
+	
+	private double sigma;
+	private double epsilon;
+	private double A1;
+	private double A2;
+	private double z1;
+	private double z2;
+	private double expZ1;
+	private double expZ2; 
+	private final Vector dr;
+	private NearestImageTransformer nearestImageTransformer;
+}	
