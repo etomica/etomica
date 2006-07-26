71d5359c17c97b020131c86a29647c25c19adfef
Nancy Cribbin <cribbin@buffalo.edu>
2006-07-26 15:38:42 +0000

    initial code

---
+++ etomica.apps/etomica/models/hexane/MCMoveReptate.java
@@ -0,0 +1,205 @@
+package etomica.models.hexane;
+
+import etomica.atom.Atom;
+import etomica.atom.AtomArrayList;
+import etomica.atom.AtomLeaf;
+import etomica.atom.AtomSource;
+import etomica.atom.AtomSourceRandomMolecule;
+import etomica.atom.AtomTreeNodeGroup;
+import etomica.atom.iterator.AtomIterator;
+import etomica.atom.iterator.AtomIteratorSinglet;
+import etomica.config.ConformationChain;
+import etomica.config.ConformationChainZigZag;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.exception.ConfigurationOverlapException;
+import etomica.integrator.mcmove.MCMovePhaseStep;
+import etomica.phase.Phase;
+import etomica.potential.PotentialMaster;
+import etomica.simulation.Simulation;
+import etomica.space.Vector;
+
+public class MCMoveReptate extends MCMovePhaseStep {
+    
+    public MCMoveReptate(Simulation sim){
+        this(sim.potentialMaster, sim.getDefaults().atomSize, 
+                sim.getDefaults().boxSize/2, sim.getDefaults().ignoreOverlap /*, con*/);
+    }
+    
+    public MCMoveReptate(PotentialMaster potentialMaster, double stepSize,
+            double stepSizeMax, boolean fixOverlap){
+        super(potentialMaster);
+        atomSource = new AtomSourceRandomMolecule();
+        energyMeter = new MeterPotentialEnergy(potentialMaster);
+//        conf = con;
+//        if(!(conf instanceof ConformationChainZigZag)){
+//            throw new IllegalArgumentException("MCMoveRepate will only work with ConformationZigZag right now.  Sorry about that.");
+//        }
+        
+        setStepSizeMax(stepSizeMax);
+        setStepSizeMin(0.0);
+        setStepSize(stepSize);
+        perParticleFrequency = true;
+        energyMeter.setIncludeLrc(false);
+        setName("MCMoveReptate");
+        this.fixOverlap = fixOverlap;
+
+        
+    }
+    
+    
+    
+    /**
+     * method to perform a trial move.
+     */
+    public boolean doTrial(){
+       atom = atomSource.getAtom();
+       if(atom == null) return false;
+       energyMeter.setTarget(atom);
+       uOld = energyMeter.getDataAsScalar();
+//       if(uOld > 1e10 && !fixOverlap) {
+//           throw new RuntimeException(new ConfigurationOverlapException(atom.node.parentPhase()));
+//       }
+       
+//       //This is the part where we make the change.  We've selected an atom already.
+//       AtomIteratorBasis aim = new AtomIteratorBasis();
+//       aim.setBasis(atom.node.parentMolecule());
+//       aim.reset();
+//       
+//       
+//       //Decide which direction the molecule is moving.
+//       double dir = Simulation.random.nextDouble();
+//       Atom at = new Atom(phase.space());
+//       Vector store = phase.space().makeVector();
+//       
+//       
+//       int leng = aim.size();
+//       
+//       if(dir <= 0.5){
+//           
+//       } else {
+//           //make sure the vectors are pointing in the proper direction (original)
+//           if(conf.isReversed()) {conf.reverse();}
+//           
+//           at = aim.nextAtom();
+//           
+//       }
+       
+       //Pick direction & set up list of atoms to iterate
+       forward = Simulation.random.nextBoolean();
+       AtomArrayList childlist = ((AtomTreeNodeGroup)atom.node).childList;
+       int numChildren = childlist.size();
+       
+       if(forward){
+           Vector position = ((AtomLeaf)childlist.get(numChildren-1)).coord.position();
+           positionOld.E(position);
+           for (int j = numChildren - 1; j > 0; j--) {
+               Vector position2 = ((AtomLeaf)childlist.get(j-1)).coord.position();
+               position.E(position2);
+               position = position2;
+           }
+           tempV.setRandomSphere();
+           tempV.TE(bondLength);
+           ((AtomLeaf)childlist.get(0)).coord.position().PE(tempV);
+       }
+       else {
+           Vector position = ((AtomLeaf)childlist.get(0)).coord.position();
+           positionOld.E(position);
+           for(int j = 0; j < numChildren-1; j++){
+               Vector position2 = ((AtomLeaf)childlist.get(j+1)).coord.position();
+               position.E(position2);
+               position = position2;
+           }
+           tempV.setRandomSphere();
+           tempV.TE(bondLength);
+           ((AtomLeaf)childlist.get(numChildren - 1)).coord.position().PE(tempV);
+           
+       }
+       
+       uNew = energyMeter.getDataAsScalar();
+       return true;
+    }
+
+    public double getA(){
+        return 1.0;
+    }
+    
+    public double getB(){
+        uNew = energyMeter.getDataAsScalar();
+        return -(uNew - uOld);
+    }
+    
+    public void acceptNotify(){
+        //we don't actually need to do anything here.
+    }
+    
+    public void rejectNotify(){
+        AtomArrayList childlist = ((AtomTreeNodeGroup)atom.node).childList;
+        int numChildren = childlist.size();
+        if (!forward) {
+            Vector position = ((AtomLeaf)childlist.get(numChildren-1)).coord.position();
+            for (int j=numChildren-1; j>0; j--) {
+                Vector position2 = ((AtomLeaf)childlist.get(j-1)).coord.position();
+                position.E(position2);
+                position = position2;
+            }
+            ((AtomLeaf)childlist.get(0)).coord.position().E(positionOld);
+        }
+        else {
+            Vector position = ((AtomLeaf)childlist.get(0)).coord.position();
+            for (int j=0; j<numChildren-1; j++) {
+                Vector position2 = ((AtomLeaf)childlist.get(j+1)).coord.position();
+                position.E(position2);
+                position = position2;
+            }
+            ((AtomLeaf)childlist.get(numChildren-1)).coord.position().E(positionOld);
+        }
+        
+        
+    }
+    
+    public AtomIterator affectedAtoms(){
+        return null;
+    }
+    
+    public double energyChange(){return uNew - uOld;}
+     
+    public void setPhase(Phase p) {
+        super.setPhase(p);
+        energyMeter.setPhase(p);
+        atomSource.setPhase(p);
+        tempV = phase.space().makeVector();
+        positionOld = phase.space().makeVector();
+    }
+    
+    /**
+     * @return Returns the atomSource.
+     */
+    public AtomSource getAtomSource() {
+        return atomSource;
+    }
+    /**
+     * @param atomSource The atomSource to set.
+     */
+    public void setAtomSource(AtomSource source) {
+        atomSource = source;
+    }
+    
+    
+    
+    
+    
+    protected final MeterPotentialEnergy energyMeter;
+//    protected final Vector translationVector;
+    private Atom atom;
+    protected double uOld;
+    protected double uNew = Double.NaN;
+    protected AtomSource atomSource;
+    protected boolean fixOverlap;
+    private Vector tempV;
+    private ConformationChain conf;
+    private Vector positionOld;
+    private boolean forward;
+    private double bondLength;
+    
+    
+}
