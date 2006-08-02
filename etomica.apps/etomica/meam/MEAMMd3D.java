d49c4b6bb174d4e2bca99d595742f91ac251cfa0
Mike Sellers <msellers@buffalo.edu>
2006-08-02 21:52:56 +0000

    Included accumulator averages for KE and PE to calculate Heat Capacity.
    KE and PE combined to one graph.
    
    Cv(PE) and Cv(KE) shown in display boxes.

--- etomica.apps/etomica/meam/MEAMMd3D.java
+++ etomica.apps/etomica/meam/MEAMMd3D.java
@@ -6,13 +6,21 @@ import etomica.atom.AtomTypeLeaf;
 import etomica.atom.AtomTypeSphere;
 import etomica.config.Configuration;
 import etomica.config.ConfigurationLattice;
+import etomica.data.AccumulatorAverage;
 import etomica.data.AccumulatorHistory;
+import etomica.data.Data;
 import etomica.data.DataInfo;
+import etomica.data.DataProcessor;
 import etomica.data.DataPump;
+import etomica.data.DataTag;
+import etomica.data.AccumulatorAverage.StatType;
 import etomica.data.meter.MeterEnergy;
 import etomica.data.meter.MeterKineticEnergy;
 import etomica.data.meter.MeterPotentialEnergy;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataDouble.DataInfoDouble;
 import etomica.graphics.ColorSchemeByType;
+import etomica.graphics.DisplayBox;
 import etomica.graphics.DisplayPhase;
 import etomica.graphics.DisplayPlot;
 import etomica.graphics.SimulationGraphic;
@@ -28,7 +36,16 @@ import etomica.space3d.Space3D;
 import etomica.space3d.Vector3D;
 import etomica.species.Species;
 import etomica.species.SpeciesSpheresMono;
+import etomica.units.CompoundDimension;
+import etomica.units.CompoundUnit;
+import etomica.units.Dimension;
+import etomica.units.Energy;
+import etomica.units.Joule;
 import etomica.units.Kelvin;
+import etomica.units.Mole;
+import etomica.units.Quantity;
+import etomica.units.Temperature;
+import etomica.units.Unit;
 import etomica.util.HistoryCollapsingAverage;
 
 /**
@@ -68,28 +85,126 @@ public class MEAMMd3D extends Simulation {
 
     public static void main(String[] args) {
     	MEAMMd3D sim = new MEAMMd3D();
+    	
     	MeterPotentialEnergy energyMeter = new MeterPotentialEnergy(sim.potentialMaster);
-    	energyMeter.setPhase(sim.phase);
-    	AccumulatorHistory energyAccumulator = new AccumulatorHistory(HistoryCollapsingAverage.FACTORY);
-        DisplayPlot plot = new DisplayPlot();
-        energyAccumulator.setDataSink(plot.getDataSet().makeDataSink());
-    	DataPump energyManager = new DataPump(energyMeter,energyAccumulator);
     	MeterKineticEnergy kineticMeter = new MeterKineticEnergy();
+    	
+    	energyMeter.setPhase(sim.phase);
     	kineticMeter.setPhase(sim.phase);
-    	AccumulatorHistory kineticAccumulator = new AccumulatorHistory(HistoryCollapsingAverage.FACTORY);
-     	DisplayPlot plotKE = new DisplayPlot();
-    	kineticAccumulator.setDataSink(plotKE.getDataSet().makeDataSink());
-    	DataPump kineticManager = new DataPump(kineticMeter, kineticAccumulator);
-    	//energyAccumulator.setBlockSize(50);
+        
+        AccumulatorHistory energyAccumulator = new AccumulatorHistory(HistoryCollapsingAverage.FACTORY);
+        AccumulatorHistory kineticAccumulator = new AccumulatorHistory(HistoryCollapsingAverage.FACTORY);
+        
+        AccumulatorAverage accumulatorAveragePE = new AccumulatorAverage(50);
+    	AccumulatorAverage accumulatorAverageKE = new AccumulatorAverage(50);
+    	
+    	DataPump energyManager = new DataPump(energyMeter,accumulatorAveragePE);   	
+    	DataPump kineticManager = new DataPump(kineticMeter, accumulatorAverageKE);
+    	
+    	accumulatorAveragePE.addDataSink(energyAccumulator, new StatType[]{StatType.MOST_RECENT});
+    	accumulatorAverageKE.addDataSink(kineticAccumulator, new StatType[]{StatType.MOST_RECENT});
+    	
+    	DisplayPlot plot = new DisplayPlot();
+        //DisplayPlot plotKE = new DisplayPlot();
+    	
+        energyAccumulator.setDataSink(plot.getDataSet().makeDataSink());
+        kineticAccumulator.setDataSink(plot.getDataSet().makeDataSink());
+        //energyAccumulator.setBlockSize(50);
+        
+    	accumulatorAveragePE.setPushInterval(1);
+    	accumulatorAverageKE.setPushInterval(1);
+    	
+    	sim.register(energyMeter, energyManager);
+    	sim.register(kineticMeter, kineticManager);
+    	
+    	//Heat Capacity (PE)
+    	DataProcessor dataProcessorPE = new DataProcessor(){
+    		MEAMMd3D sim = new MEAMMd3D();
+    		DataDouble data;
+    		DataInfoDouble dataInfo;
+    		DataTag tag = new DataTag();
+    		
+    		public Data processData(Data inputData){
+    			data.x = ((DataDouble)inputData).x;
+    			double systemTemp = sim.integrator.getTemperature();
+    			data.x /= systemTemp;
+    			data.x *= data.x/sim.phase.moleculeCount();
+    			return data;
+    		}
+    		
+    		public DataProcessor getDataCaster(DataInfo inputDataInfo){
+    			return null;
+    		}
+    		
+    		public DataInfo processDataInfo(DataInfo inputDataInfo){
+    			Dimension cvDimension = new CompoundDimension(new Dimension[]{Energy.DIMENSION, Temperature.DIMENSION, Quantity.DIMENSION}, new double[]{1,-1,-1});
+    			data = new DataDouble();
+    			dataInfo = new DataInfoDouble("Heat Capacity", cvDimension);
+    			return dataInfo;
+    		}
+    		
+    		public DataTag getTag(){
+    			return tag;
+    		}
+    	};
+    	
+//    	Heat Capacity (KE)
+    	DataProcessor dataProcessorKE = new DataProcessor(){
+    		MEAMMd3D sim = new MEAMMd3D();
+    		DataDouble data;
+    		DataInfoDouble dataInfo;
+    		DataTag tag = new DataTag();
+    		
+    		public Data processData(Data inputData){
+    			data.x = ((DataDouble)inputData).x;
+    			double systemTemp = sim.integrator.getTemperature();
+    			data.x /= systemTemp;
+    			data.x *= data.x/sim.phase.moleculeCount();
+    			return data;
+    		}
+    		
+    		public DataProcessor getDataCaster(DataInfo inputDataInfo){
+    			return null;
+    		}
+    		
+    		public DataInfo processDataInfo(DataInfo inputDataInfo){
+    			Dimension cvDimension = new CompoundDimension(new Dimension[]{Energy.DIMENSION, Temperature.DIMENSION, Quantity.DIMENSION}, new double[]{1,-1,-1});
+    			data = new DataDouble();
+    			dataInfo = new DataInfoDouble("Heat Capacity", cvDimension);
+    			return dataInfo;
+    		}
+    		
+    		public DataTag getTag(){
+    			return tag;
+    		}
+    	};
+    	
+    	accumulatorAveragePE.addDataSink(dataProcessorPE, new StatType[]{StatType.STANDARD_DEVIATION});
+    	accumulatorAverageKE.addDataSink(dataProcessorKE, new StatType[]{StatType.STANDARD_DEVIATION});
+    	  	
         IntervalActionAdapter adapter = new IntervalActionAdapter(energyManager, sim.integrator);
         adapter.setActionInterval(1);
         IntervalActionAdapter kineticAdapter = new IntervalActionAdapter(kineticManager, sim.integrator);
-        kineticAdapter.setActionInterval(1);
+        kineticAdapter.setActionInterval(1);    
+
         SimulationGraphic simgraphic = new SimulationGraphic(sim);
+        
+    	DisplayBox cvBoxPE = new DisplayBox();
+    	dataProcessorPE.setDataSink(cvBoxPE);
+    	cvBoxPE.setUnit(new CompoundUnit(new Unit[]{Joule.UNIT, Kelvin.UNIT, Mole.UNIT}, new double []{1,-1,-1}));
+    	cvBoxPE.setLabel("PE Cv contrib.");
+    	DisplayBox cvBoxKE = new DisplayBox();
+    	dataProcessorKE.setDataSink(cvBoxKE);
+    	cvBoxKE.setUnit(new CompoundUnit(new Unit[]{Joule.UNIT, Kelvin.UNIT, Mole.UNIT}, new double []{1,-1,-1}));
+    	cvBoxKE.setLabel("KE Cv contrib.");
+    	
     	simgraphic.makeAndDisplayFrame();
     	simgraphic.panel().add(plot.graphic());
-    	simgraphic.panel().add(plotKE.graphic());
-        ColorSchemeByType colorScheme = ((ColorSchemeByType)((DisplayPhase)simgraphic.displayList().getFirst()).getColorScheme());
+    	//simgraphic.panel().add(plotKE.graphic());
+    	simgraphic.panel().add(cvBoxPE.graphic());
+    	simgraphic.panel().add(cvBoxKE.graphic());
+    	
+    	ColorSchemeByType colorScheme = ((ColorSchemeByType)((DisplayPhase)simgraphic.displayList().getFirst()).getColorScheme());
     	colorScheme.setColor(sim.sn.getMoleculeType(),java.awt.Color.blue);
     	colorScheme.setColor(sim.ag.getMoleculeType(),java.awt.Color.gray);
     	colorScheme.setColor(sim.cu.getMoleculeType(),java.awt.Color.orange);
@@ -105,7 +220,7 @@ public class MEAMMd3D extends Simulation {
     public MEAMMd3D() {
         super(Space3D.getInstance()); //INSTANCE); kmb change 8/3/05
         integrator = new IntegratorVelocityVerlet(this);
-        integrator.setTimeStep(0.005);
+        integrator.setTimeStep(0.001);
         integrator.setTemperature(Kelvin.UNIT.toSim(298));
         integrator.setThermostatInterval(100);
         integrator.setIsothermal(true);
@@ -147,14 +262,15 @@ public class MEAMMd3D extends Simulation {
         //PrimitiveTetragonal primitive = new PrimitiveTetragonal(space, 5.92, 3.23);
         LatticeCrystal crystal = new LatticeCrystal(new Crystal(
         		primitive, new BasisBetaSnA5(primitive)));
-
+		
+		
         //FCC Cu
-        /**
+		/**
 	    phase.setDimensions(new Vector3D(3.6148*3, 3.6148*3, 3.6148*6));
 	    PrimitiveCubic primitive = new PrimitiveCubic(space, 3.6148);
 	    LatticeCrystal crystal = new LatticeCrystal(new Crystal(
 		        primitive, new BasisCubicFcc(primitive)));
-	    **/
+	    */
         
         //FCC Ag
         /**
@@ -162,7 +278,7 @@ public class MEAMMd3D extends Simulation {
 	    PrimitiveCubic primitive = new PrimitiveCubic(space, 4.0863);
 	    LatticeCrystal crystal = new LatticeCrystal(new Crystal(
 		        primitive, new BasisCubicFcc(primitive)));
-	    **/
+	    */
            
 		Configuration config = new ConfigurationLattice(crystal);
 		config.initializeCoordinates(phase);
