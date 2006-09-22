41e004942e23ec7b54679cb4eeff29b0c44b34a7
Andrew Schultz <ajs42@buffalo.edu>
2006-09-22 21:30:51 +0000

    Bug 41: Simple 3-body Atom iterator and bond angle potential

---
+++ etomica/atom/iterator/TripletInnerSequential.java
@@ -0,0 +1,182 @@
+package etomica.atom.iterator;
+import java.io.Serializable;
+
+import etomica.action.AtomsetAction;
+import etomica.action.AtomsetCount;
+import etomica.action.AtomsetDetect;
+import etomica.atom.Atom;
+import etomica.atom.AtomArrayList;
+import etomica.atom.AtomSet;
+import etomica.atom.AtomTreeNodeGroup;
+import etomica.atom.AtomsetArray;
+import etomica.atom.iterator.IteratorDirective.Direction;
+
+public class TripletInnerSequential implements AtomsetIteratorBasisDependent, 
+        AtomsetIteratorDirectable, Serializable  {
+
+    public TripletInnerSequential() {
+        super();
+        next = new AtomsetArray(3);
+        atomArray = next.getArray();
+    }
+
+    public void setDirection(Direction direction) {
+        doGoDown = true;
+        doGoUp = true;
+        if (direction == IteratorDirective.Direction.UP) {
+            doGoDown = false;
+        }
+        else if (direction == IteratorDirective.Direction.DOWN) {
+            doGoUp = false;
+        }
+    }
+
+    public void setBasis(AtomSet atoms) {
+        childList = ((AtomTreeNodeGroup)((Atom)atoms).node).childList;
+    }
+
+    public int basisSize() {
+        return 1;
+    }
+
+    public boolean haveTarget(Atom target) {
+        return childList.contains(targetAtom);
+    }
+
+    public void setTarget(Atom newTargetAtom) {
+        targetAtom = newTargetAtom;
+    }
+
+    public boolean contains(AtomSet atomsd) {
+        if(atomsd == null || atomsd.count() != 2) return false;
+        if(detector == null) {
+            detector = new AtomsetDetect(atomsd);
+        } else {
+            detector.setAtoms(atomsd);
+            detector.reset();
+        }
+        allAtoms(detector);
+        return detector.detectedAtom();
+    }
+
+    public boolean hasNext() {
+        if (stateUpDown == 0) {
+            // if we're not going to go down, we're done when we reach 2 from the end
+            if (cursor < childList.size() - 2) {
+                return true;
+            }
+            return false;
+        }
+        if (stateUpDown == 1) {
+            if (cursor < childList.size() - 1 && cursor > 0) {
+                return true;
+            }
+            return false;
+        }
+        if (stateUpDown == 2 && cursor > 1 && cursor < childList.size()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void reset() {
+        if (targetAtom != null) {
+            cursor = childList.indexOf(targetAtom);
+            if (doGoUp) {
+                if (cursor < childList.size() - 2) {
+                    stateUpDown = 0;
+                }
+                else if (doGoDown) {
+                    if (cursor > 0 && cursor < childList.size() - 1) {
+                        stateUpDown = 1;
+                    }
+                    else if (cursor > 1 && cursor < childList.size()) {
+                        stateUpDown = 2;
+                    }
+                    else {
+                        stateUpDown = 3;
+                    }
+                }
+            }
+            else {
+                if (cursor > 1 && cursor < childList.size()) {
+                    stateUpDown = 2;
+                }
+                else {
+                    stateUpDown = 3;
+                }
+            }
+        }
+        else {
+            doGoUp = true;
+            doGoDown = false;
+            stateUpDown = 0;
+            cursor = 0;
+        }
+    }
+
+    public void unset() {
+        cursor = childList.size();
+    }
+
+    public AtomSet next() {
+        atomArray[0] = childList.get(cursor-stateUpDown);
+        atomArray[1] = childList.get(cursor-stateUpDown+1);
+        atomArray[2] = childList.get(cursor-stateUpDown+2);
+        if (stateUpDown == 0) {
+            if (targetAtom == null) {
+                cursor++;
+            }
+            else if (doGoDown) {
+                stateUpDown = 1;
+            }
+        }
+        else if (stateUpDown == 1) {
+            stateUpDown = 2;
+        }
+        else if (stateUpDown == 2) {
+            stateUpDown = 3;
+        }
+        return next;
+    }
+
+    public AtomSet peek() {
+        atomArray[0] = childList.get(cursor-stateUpDown);
+        atomArray[1] = childList.get(cursor-stateUpDown+1);
+        atomArray[2] = childList.get(cursor-stateUpDown+2);
+        return next;
+    }
+
+    public void allAtoms(AtomsetAction action) {
+        reset();
+        while (hasNext()) {
+            action.actionPerformed(next());
+        }
+    }
+
+    public int size() {
+        if(counter == null) {
+            counter = new AtomsetCount();
+        } else {
+            counter.reset();
+        }
+        allAtoms(counter);
+        return counter.callCount();
+    }
+
+    public int nBody() {
+        return 3;
+    }
+
+    
+    protected int stateUpDown; // 0 = up, 1 = middle, 2 = down, 3 = all done
+    protected boolean doGoUp, doGoDown;
+    protected AtomArrayList childList;
+    protected Atom targetAtom;
+    protected int cursor;
+    protected final AtomsetArray next;
+    protected final Atom[] atomArray;
+    protected AtomsetCount counter;
+    protected AtomsetDetect detector;
+    private static final long serialVersionUID = 1L;
+}
---
+++ etomica/potential/P3BondAngle.java
@@ -0,0 +1,88 @@
+package etomica.potential;
+
+import etomica.atom.AtomLeaf;
+import etomica.atom.AtomSet;
+import etomica.phase.Phase;
+import etomica.space.NearestImageTransformer;
+import etomica.space.Space;
+import etomica.space.Vector;
+
+/**
+ * Simple 3-body soft bond-angle potential 
+ * @author andrew
+ */
+public class P3BondAngle extends Potential {
+
+    public P3BondAngle(Space space) {
+        super(3, space);
+        dr12 = space.makeVector();
+        dr23 = space.makeVector();
+        setAngle(Math.PI);
+    }
+
+    public void setPhase(Phase phase) {
+        nearestImageTransformer = phase.getBoundary();
+    }
+
+    public double energy(AtomSet atomSet) {
+        AtomLeaf atom0 = (AtomLeaf)atomSet.getAtom(0);
+        AtomLeaf atom1 = (AtomLeaf)atomSet.getAtom(1);
+        AtomLeaf atom2 = (AtomLeaf)atomSet.getAtom(2);
+        dr12.Ev1Mv2(atom1.coord.position(),atom0.coord.position());
+        dr23.Ev1Mv2(atom2.coord.position(),atom1.coord.position());
+        nearestImageTransformer.nearestImage(dr12);
+        nearestImageTransformer.nearestImage(dr23);
+        double costheta = -dr12.dot(dr23)/Math.sqrt(dr12.squared()*dr23.squared());
+        double dtheta;
+        // machine precision can give us numbers with magnitudes slightly greater than 1
+        if (costheta > 1) {
+            dtheta = 0;
+        }
+        else if (costheta < -1) {
+            dtheta = Math.PI;
+        }
+        else {
+            dtheta = Math.acos(costheta);
+        }
+        dtheta -= angle;
+        return epsilon*dtheta*dtheta;
+    }
+
+    /**
+     * Sets the nominal bond angle (in radians)
+     */
+    public void setAngle(double newAngle) {
+        angle = newAngle;
+    }
+    
+    /**
+     * Returns the nominal bond angle (in radians)
+     */
+    public double getAngle() {
+        return angle;
+    }
+
+    /**
+     * Sets the characteristic energy of the potential
+     */
+    public void setEpsilon(double newEpsilon) {
+        epsilon = newEpsilon;
+    }
+    
+    /**
+     * Returns the characteristic energy of the potential
+     */
+    public double getEpsilon() {
+        return epsilon;
+    }
+    
+    public double getRange() {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    protected final Vector dr12, dr23;
+    protected NearestImageTransformer nearestImageTransformer;
+    protected double angle;
+    protected double epsilon;
+    private static final long serialVersionUID = 1L;
+}
