ef04187ad21f99ebb10ad6098302cc01c4d11899
Andrew Schultz <ajs42@buffalo.edu>
2006-11-27 21:18:49 +0000

    add a velocity window

---
+++ etomica/graphics/ActionVelocityWindow.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package etomica.graphics;
+
+import java.awt.Color;
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+
+import etomica.action.Action;
+import etomica.atom.AtomLeaf;
+import etomica.atom.iterator.AtomIteratorLeafAtoms;
+import etomica.phase.Phase;
+import etomica.space.ICoordinateKinetic;
+import etomica.space.Vector;
+
+/**
+ * Action that opens a new window and dumps the velocities into the window.
+ * @author andrew
+ */
+public class ActionVelocityWindow implements Action {
+    private final AtomIteratorLeafAtoms iterator;
+    
+    public ActionVelocityWindow(Phase phase) {
+        iterator = new AtomIteratorLeafAtoms(phase);
+    }
+    
+    public String getLabel() {
+        return "a label";
+    }
+    
+    public void actionPerformed() {
+        JFrame f = new JFrame();
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.setBackground(Color.white);
+        textArea.setForeground(Color.black);
+        iterator.reset();
+        while (iterator.hasNext()) {
+            AtomLeaf atom = (AtomLeaf)iterator.nextAtom();
+            Vector vel = ((ICoordinateKinetic)atom.coord).velocity();
+            String str = Double.toString(vel.x(0));
+            for (int i=1; i<vel.D(); i++) {
+                str += " "+Double.toString(vel.x(i));
+            }
+            textArea.append(str+"\n");
+        }
+        f.add(textArea);
+        f.pack();
+        f.setSize(400,600);
+        f.setVisible(true);
+    }
+}
