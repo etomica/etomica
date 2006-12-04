cb65749f326084f858e565dfb1b08759f96f2da9
Andrew Schultz <ajs42@buffalo.edu>
2006-12-04 04:20:16 +0000

    add sliders for # of balls and # of urns

--- etomica.modules/etomica/modules/entropylottery/EntropyLotteryGraphic.java
+++ etomica.modules/etomica/modules/entropylottery/EntropyLotteryGraphic.java
@@ -4,16 +4,22 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 
 import etomica.action.Action;
+import etomica.action.ActionGroup;
+import etomica.action.ActionGroupSeries;
+import etomica.action.SimulationRestart;
 import etomica.data.AccumulatorHistory;
 import etomica.data.DataPump;
 import etomica.data.DataSourceCountSteps;
 import etomica.data.DataTag;
+import etomica.graphics.DeviceNSelector;
+import etomica.graphics.DeviceSlider;
 import etomica.graphics.DeviceTrioControllerButton;
 import etomica.graphics.DisplayPhase;
 import etomica.graphics.DisplayPlot;
 import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntervalActionAdapter;
 import etomica.space1d.Space1D;
+import etomica.units.Pixel;
 import etomica.util.HistoryCollapsing;
 
 public class EntropyLotteryGraphic {
@@ -30,9 +36,12 @@ public class EntropyLotteryGraphic {
         panel.setLayout(new BorderLayout());
         
         DeviceTrioControllerButton control = new DeviceTrioControllerButton(sim);
+        SimulationRestart restartAction = (SimulationRestart)control.getRestartButton().getAction();
+        restartAction.setConfiguration(new ConfigurationZero(sim.space));
         
 	    //display of phase, timer
 	    final DisplayPhase displayPhase = new DisplayPhase(sim.phase,sim.getDefaults().pixelUnit);
+        displayPhase.setPixelUnit(new Pixel(300/sim.phase.getBoundary().getDimensions().x(0)));
         displayPhase.setDrawingHeight(300);
         DisplayPhaseCanvas1DBins canvas = new DisplayPhaseCanvas1DBins(displayPhase);
         displayPhase.setPhaseCanvas(canvas);
@@ -64,6 +73,7 @@ public class EntropyLotteryGraphic {
         IntervalActionAdapter iaa = new IntervalActionAdapter(pump);
         iaa.setActionInterval(20);
         sim.integrator.addListener(iaa);
+        sim.register(meterEntropy, pump);
         
         DataSourceProbabilityDensity probabilityDensity = new DataSourceProbabilityDensity();
         sim.integrator.addListener(probabilityDensity);
@@ -77,6 +87,7 @@ public class EntropyLotteryGraphic {
         sim.integrator.addListener(stepCounter);
         probabilityEntropyHistory.setTimeDataSource(stepCounter);
         entropyProcessor.setDataSink(probabilityEntropyHistory);
+        sim.register(probabilityDensity, pump);
         
         DisplayPlot entropyPlot = new DisplayPlot();
         entropyHistory.setDataSink(entropyPlot.getDataSet().makeDataSink());
@@ -85,19 +96,47 @@ public class EntropyLotteryGraphic {
         entropyPlot.setLegend(new DataTag[]{probabilityDensity.getTag()}, "predicted");
         displayPanel.add(entropyPlot.getPlot());
         
+        DeviceNSelector nSelector = new DeviceNSelector(sim.getController());
+        nSelector.setSpeciesAgent(sim.phase.getAgent(sim.species));
+        nSelector.setLabel("Number of Balls");
+        nSelector.setShowBorder(true);
+        
+        final DeviceSlider nUrnSelector = new DeviceSlider(sim.getController(), new ModifierDimensions(sim.phase));
+        nUrnSelector.setMinimum(3);
+        nUrnSelector.setMaximum(30);
+        nUrnSelector.setLabel("Number of Urns");
+        nUrnSelector.setShowBorder(true);
+        Action resetDisplay = new Action() {
+            public void actionPerformed() {
+                double nUrn = nUrnSelector.getValue();
+                double a2p = 300.0/nUrn;
+                displayPhase.setPixelUnit(new Pixel(a2p));
+                ((DisplayPhaseCanvas1DBins)displayPhase.canvas).setYScale(nUrn*nUrn/(6*sim.phase.getAgent(sim.species).getNMolecules()));
+                displayPhase.repaint();
+            }
+            public String getLabel() {
+                // because you deserve it
+                return null;
+            }
+        };
+        nUrnSelector.setPostAction(new ActionGroupSeries(new Action[]{resetDisplay, restartAction}));
+        nUrnSelector.doUpdate();
+
+        nSelector.setResetAction(new ActionGroupSeries(new Action[]{resetDisplay, restartAction}));
+        nSelector.doUpdate();
+
+        resetDisplay.actionPerformed();
         
         bigPanel.add(displayPhase.graphic());
         bigPanel.add(displayPanel);
-        
-        //panel for the start buttons
-        JPanel startPanel = (JPanel)control.graphic();
-        
         
         JPanel controlPanel = new JPanel(new java.awt.GridBagLayout());
         java.awt.GridBagConstraints gbc2 = new java.awt.GridBagConstraints();
         gbc2.gridy = 0;
         gbc2.gridx = java.awt.GridBagConstraints.RELATIVE;
-        controlPanel.add(startPanel,gbc2);
+        controlPanel.add(control.graphic(),gbc2);
+        controlPanel.add(nSelector.graphic(),gbc2);
+        controlPanel.add(nUrnSelector.graphic(),gbc2);
         panel.add(controlPanel, java.awt.BorderLayout.NORTH);
         panel.add(bigPanel, java.awt.BorderLayout.EAST);
 
---
+++ etomica.modules/etomica/modules/entropylottery/ModifierDimensions.java
@@ -0,0 +1,37 @@
+package etomica.modules.entropylottery;
+
+import etomica.modifier.Modifier;
+import etomica.phase.Phase;
+import etomica.space.Vector;
+import etomica.units.Dimension;
+import etomica.units.Length;
+
+public class ModifierDimensions implements Modifier {
+
+    public ModifierDimensions(Phase phase) {
+        this.phase = phase;
+    }
+
+    public Dimension getDimension() {
+        return Length.DIMENSION;
+    }
+
+    public String getLabel() {
+        return "Dimensions";
+    }
+
+    public double getValue() {
+        return phase.getBoundary().getDimensions().x(0);
+    }
+
+    public void setValue(double newValue) {
+        if (newValue <= 0 || newValue > 1000) {
+            throw new IllegalArgumentException("Bogus value for dimension");
+        }
+        Vector dim = phase.getBoundary().getDimensions();
+        dim.setX(0, newValue);
+        phase.setDimensions(dim);
+    }
+
+    private final Phase phase;
+}
