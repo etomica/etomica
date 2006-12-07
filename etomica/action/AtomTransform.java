19ddec5a896d163b737214c114e9f4487a39e14c
Andrew Schultz <ajs42@buffalo.edu>
2006-12-07 19:30:46 +0000

    keep it simple

--- etomica/action/AtomTransform.java
+++ etomica/action/AtomTransform.java
@@ -10,7 +10,10 @@ public class AtomTransform {
         while(iterator.hasNext()) {
             AtomLeaf a = (AtomLeaf)iterator.nextAtom();
             Vector r = a.coord.position();
-            r.transform(a.getNode().parentPhase().getBoundary(),r0, transformMatrix);
+            r.ME(r0);
+            a.getNode().parentPhase().getBoundary().nearestImage(r);
+            r.transform(transformMatrix);
+            r.PE(r0);
         }
     }
     
