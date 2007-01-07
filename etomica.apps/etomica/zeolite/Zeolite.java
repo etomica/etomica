0a3ba73ee28e2d4edb69565d4441169870ffbfc1
Andrew Schultz <ajs42@buffalo.edu>
2007-01-07 02:19:59 +0000

    remove ConfigurationSequential.  Use ConfigurationLattice instead.

--- etomica.apps/etomica/zeolite/Zeolite.java
+++ etomica.apps/etomica/zeolite/Zeolite.java
@@ -1,41 +1,5 @@
 package etomica.zeolite;
-import etomica.action.PhaseImposePbc;
-import etomica.action.activity.ActivityIntegrate;
-import etomica.atom.Atom;
-import etomica.atom.AtomArrayList;
-import etomica.atom.AtomLeaf;
-import etomica.atom.AtomTreeNodeGroup;
-import etomica.atom.AtomType;
-import etomica.atom.AtomTypeLeaf;
-import etomica.atom.iterator.AtomIteratorTree;
-import etomica.config.ConfigurationLattice;
-import etomica.config.ConfigurationSequential;
-import etomica.graphics.ColorSchemeByType;
-import etomica.graphics.DeviceNSelector;
-import etomica.graphics.DisplayPhase;
-import etomica.graphics.SimulationGraphic;
-import etomica.integrator.IntegratorHard;
-import etomica.integrator.IntegratorVelocityVerlet;
-import etomica.lattice.LatticeCubicFcc;
-import etomica.nbr.CriterionSimple;
-import etomica.nbr.NeighborCriterion;
-import etomica.nbr.list.NeighborListManager;
-import etomica.nbr.list.PotentialMasterList;
-import etomica.phase.Phase;
-import etomica.potential.P2HardSphere;
-import etomica.potential.P1HardBoundary;
-import etomica.simulation.Simulation;
-import etomica.space.Space;
 import etomica.space3d.Space3D;
-import etomica.species.Species;
-import etomica.species.SpeciesSpheresMono;
-import etomica.util.Default;
-import etomica.config.ConfigurationFile;
-import etomica.potential.P2LennardJones;
-import etomica.units.Kelvin;
-import etomica.units.Pixel;
-import etomica.space.BoundaryRectangularSlit;
-import etomica.space.BoundaryRectangularPeriodic;
 
 
 public class Zeolite {
