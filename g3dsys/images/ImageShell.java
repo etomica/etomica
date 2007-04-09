8f8234590c0b3fb60924f8b6d67cde2521a168b2
Collin Tedlock <ctedlock@buffalo.edu>
2007-04-09 21:25:06 +0000

    DRAW_LARGE_BOX operational; just scales the boundary lines while not drawing the originals.

--- g3dsys/images/ImageShell.java
+++ g3dsys/images/ImageShell.java
@@ -108,23 +108,6 @@ public class ImageShell extends Figure {
     //still bound to axis directions; need to use vector orientation instead
     float dx = 0, dy = 0, dz = 0; //for linear combinations
     Figure[] figs = _gsys.getFigs();
-
-    //draw large box if necessary
-    if(drawBoundaryType == DRAW_LARGE_BOX) {
-      for(int i=0; i<figs.length; i++) {
-        if(figs[i] instanceof Line) {
-          p.set(((Line)figs[i]).getStart().x * (2*numLayers+1),
-              ((Line)figs[i]).getStart().y * (2*numLayers+1),
-              ((Line)figs[i]).getStart().z * (2*numLayers+1));
-          q.set(((Line)figs[i]).getEnd().x * (2*numLayers+1),
-              ((Line)figs[i]).getEnd().y * (2*numLayers+1),
-              ((Line)figs[i]).getEnd().z * (2*numLayers+1));
-          _gsys.screenSpace(p, s);
-          _gsys.screenSpace(q, t);
-          g3d.drawDashedLine(figs[i].getColor(),0,0,s.x,s.y,s.z,t.x,t.y,t.z);
-        }
-      }
-    }
     
     //IndexIteratorSequential iter = new IndexIteratorSequential(D,numLayers);
     int[] sizes = new int[D];
@@ -205,6 +188,23 @@ public class ImageShell extends Figure {
       dx = 0;
       dy = 0;
       dz = 0;
+    }
+
+    //draw large box
+    if(drawBoundaryType == DRAW_LARGE_BOX) {
+      for(int i=0; i<figs.length; i++) {
+        if(figs[i] instanceof Line) {
+          p.set(((Line)figs[i]).getStart().x * (2*numLayers+1),
+              ((Line)figs[i]).getStart().y * (2*numLayers+1),
+              ((Line)figs[i]).getStart().z * (2*numLayers+1));
+          q.set(((Line)figs[i]).getEnd().x * (2*numLayers+1),
+              ((Line)figs[i]).getEnd().y * (2*numLayers+1),
+              ((Line)figs[i]).getEnd().z * (2*numLayers+1));
+          _gsys.screenSpace(p, s);
+          _gsys.screenSpace(q, t);
+          g3d.drawDashedLine(figs[i].getColor(),0,0,s.x,s.y,s.z,t.x,t.y,t.z);
+        }
+      }
     }
     
   }
