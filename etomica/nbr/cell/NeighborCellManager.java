996b0259a38b2325f459283d4e531da414d83167
Andrew Schultz <ajs42@buffalo.edu>
2007-04-29 21:38:37 +0000

    update comments.  Don't call removeFromCell (just remove the atom from the cell's list).

--- etomica/nbr/cell/NeighborCellManager.java
+++ etomica/nbr/cell/NeighborCellManager.java
@@ -159,9 +159,14 @@ public class NeighborCellManager implements PhaseCellManager, AgentSource, Phase
         atomIterator.reset();
         for (IAtom atom = atomIterator.nextAtom(); atom != null;
              atom = atomIterator.nextAtom()) {
-            agentManager.setAgent(atom, null);
             if (atom.getType().isInteracting()) {
                 assignCell(atom);
+            }
+            else {
+                // the atom is probably not in a cell, but if it is, this is
+                // the only way it will get purged.  This is probably as cheap
+                // or cheaper than checking first.
+                agentManager.setAgent(atom, null);
             }
         }
     }
@@ -169,18 +174,11 @@ public class NeighborCellManager implements PhaseCellManager, AgentSource, Phase
     public Cell getCell(IAtom atom) {
         return (Cell)agentManager.getAgent(atom);
     }
-    
-    protected void removeFromCell(IAtom atom) {
-        Cell cell = (Cell)agentManager.getAgent(atom);
-        if (cell != null) {
-            cell.removeAtom(atom);
-            agentManager.setAgent(atom, null);
-        }
-    }
-    
+
     /**
-     * Assigns the cell for the given atom.
-     * @param atom
+     * Assigns the cell for the given atom.  The atom will be listed in the
+     * cell's atom list and the cell with be associated with the atom via
+     * agentManager.
      */
     public void assignCell(IAtom atom) {
         IVector position = (positionDefinition != null) ?
@@ -198,7 +196,11 @@ public class NeighborCellManager implements PhaseCellManager, AgentSource, Phase
     public Class getAgentClass() {
         return Cell.class;
     }
-    
+
+    /**
+     * Returns the cell containing the given atom.  The atom is added to the
+     * cell's atom list.
+     */
     public Object makeAgent(IAtom atom) {
         if (atom.getType().isInteracting()) {
             IVector position = (positionDefinition != null) ?
@@ -214,8 +216,11 @@ public class NeighborCellManager implements PhaseCellManager, AgentSource, Phase
         return null;
     }
 
-    public void releaseAgent(Object agent, IAtom atom) {
-        removeFromCell(atom);
+    /**
+     * Removes the given atom from the cell.
+     */
+    public void releaseAgent(Object cell, IAtom atom) {
+        ((Cell)cell).removeAtom(atom);
     }
     
     public void actionPerformed(PhaseEvent event) {
@@ -258,7 +263,11 @@ public class NeighborCellManager implements PhaseCellManager, AgentSource, Phase
         private void updateCell(IAtom atom) {
             if (atom.getType().isInteracting()) {
                 Boundary boundary = phase.getBoundary();
-                neighborCellManager.removeFromCell(atom);
+                Cell cell = neighborCellManager.getCell(atom);
+                // we only need to remove the atom from the cell's list and
+                // not de-associate the atom from the cell.  assignCell below
+                // will do that.
+                cell.removeAtom(atom);
                 if (atom instanceof IAtomGroup) {
                     IVector shift = boundary.centralImage(moleculePosition.position(atom));
                     if (!shift.isZero()) {
