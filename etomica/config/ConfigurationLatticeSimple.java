540d8da50c85e03c0225045bb0f7cd2c1d80c57a
Andrew Schultz <ajs42@buffalo.edu>
2007-05-05 03:39:15 +0000

    Add a simple version of ConfigurationLattice

---
+++ etomica/config/ConfigurationLatticeSimple.java
@@ -0,0 +1,185 @@
+package etomica.config;
+
+import etomica.action.AtomActionTranslateTo;
+import etomica.atom.IAtom;
+import etomica.atom.IAtomGroup;
+import etomica.atom.iterator.AtomIteratorAllMolecules;
+import etomica.integrator.IntegratorHard;
+import etomica.lattice.BravaisLatticeCrystal;
+import etomica.lattice.IndexIteratorRectangular;
+import etomica.lattice.IndexIteratorSizable;
+import etomica.lattice.LatticeCubicFcc;
+import etomica.lattice.SpaceLattice;
+import etomica.phase.Phase;
+import etomica.simulation.Simulation;
+import etomica.space.IVector;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresMono;
+
+/**
+ * Constructs configuration that has the molecules placed on the sites of a
+ * lattice. Molecules are placed sequentially on the sites of the lattice in an
+ * order specified by an index iterator set at construction.
+ * <p>
+ * Iteration over the indexes yields integer arrays, and with each iteration the
+ * array is passed to the site method of the lattice which returns the position
+ * for placement of the next molecule. Each array index is iterated to a maximum
+ * value determined by the number of molecules to be placed, the dimensions of
+ * the phase in which they are placed, and the lattice constants of the lattice.
+ * <p>
+ * An instance of this class may be configured to place atoms such that they
+ * uniformly fill the volume of the phase. It will attempt this by scaling the
+ * lattice constants of the configuration in an appropriate way. Success in
+ * getting a good spatial distribution may vary.
+ * <p>
+ * An instance can also be configured to remember the indices used to get
+ * lattice position for each molecule that is placed. This can be useful if it
+ * is desired to associate each molecule with a lattice site.
+ */
+public class ConfigurationLatticeSimple extends Configuration {
+
+    /**
+     * Constructs class using instance of IndexIteratorRectangular as the default
+     * index iterator.
+     */
+    public ConfigurationLatticeSimple(SpaceLattice lattice) {
+        this(lattice, new IndexIteratorRectangular(lattice.D()));
+    }
+
+    /**
+     * Construct class that will place atoms on sites of the given lattice,
+     * proceeding in the order resulting from iteration through the given index
+     * iterator.
+     */
+    public ConfigurationLatticeSimple(SpaceLattice lattice,
+            IndexIteratorSizable indexIterator) {
+        if(indexIterator.getD() != lattice.D()) {
+            throw new IllegalArgumentException("Dimension of index iterator and lattice are incompatible");
+        }
+        this.lattice = lattice;
+        this.indexIterator = indexIterator;
+        atomActionTranslateTo = new AtomActionTranslateTo(lattice.getSpace());
+    }
+
+    /**
+     * Places the molecules in the given phase on the positions of the
+     * lattice.  
+     */
+    public void initializeCoordinates(Phase phase) {
+        AtomIteratorAllMolecules atomIterator = new AtomIteratorAllMolecules(phase);
+        int sumOfMolecules = atomIterator.size();
+        if (sumOfMolecules == 0) {
+            return;
+        }
+        int basisSize = 1;
+        if (lattice instanceof BravaisLatticeCrystal) {
+            basisSize = ((BravaisLatticeCrystal)lattice).getBasis().getScaledCoordinates().length;
+        }
+        int nCells = (int) Math.ceil((double) sumOfMolecules
+                / (double) basisSize);
+
+        // determine scaled shape of simulation volume
+        IVector shape = phase.getSpace().makeVector();
+        shape.E(phase.getBoundary().getDimensions());
+        IVector latticeConstantV = Space.makeVector(lattice.getLatticeConstants());
+        shape.DE(latticeConstantV);
+
+        // determine number of cells in each direction
+        int[] latticeDimensions = calculateLatticeDimensions(nCells, shape);
+        if (indexIterator.getD() > latticeDimensions.length) {
+            int[] iteratorDimensions = new int[latticeDimensions.length+1];
+            System.arraycopy(latticeDimensions, 0, iteratorDimensions, 0,
+                    latticeDimensions.length);
+            iteratorDimensions[latticeDimensions.length] = basisSize;
+            indexIterator.setSize(iteratorDimensions);
+        }
+        else {
+            indexIterator.setSize(latticeDimensions);
+        }
+
+        // Place molecules
+        atomIterator.reset();
+        indexIterator.reset();
+        for (IAtom a = atomIterator.nextAtom(); a != null;
+             a = atomIterator.nextAtom()) {
+            if (a instanceof IAtomGroup) {
+                // initialize coordinates of child atoms
+                Conformation config = a.getType().creator().getConformation();
+                config.initializePositions(((IAtomGroup)a).getChildList());
+            }
+
+            atomActionTranslateTo.setDestination((IVector)lattice.site(indexIterator.next()));
+            atomActionTranslateTo.actionPerformed(a);
+        }
+    }
+
+    protected int[] calculateLatticeDimensions(int nCells, IVector shape) {
+        int dimLeft = shape.getD();
+        int nCellsLeft = nCells;
+        int[] latticeDimensions = new int[shape.getD()];
+        while (dimLeft > 0) {
+            double smin = Double.POSITIVE_INFINITY;
+            int dmin = 0;
+            double product = 1.0;
+            for (int idim = 0; idim < shape.getD(); idim++) {
+                if (latticeDimensions[idim] > 0)
+                    continue;
+                if (shape.x(idim) < smin) {
+                    smin = shape.x(idim);
+                    dmin = idim;
+                }
+                product *= shape.x(idim);
+            }
+            // round off except for last dimension (then round up)
+            if (dimLeft > 1) {
+                latticeDimensions[dmin] = (int) Math.round(shape.x(dmin)
+                        * Math.pow((nCellsLeft / product), 1.0 / dimLeft));
+                if (latticeDimensions[dmin] == 0){
+                	latticeDimensions[dmin] = 1;
+                }
+            } else {
+                latticeDimensions[dmin] = (int) Math.ceil(shape.x(dmin)
+                        * nCellsLeft / product);
+            }
+            nCellsLeft = (nCellsLeft + latticeDimensions[dmin] - 1)
+                    / latticeDimensions[dmin];
+            dimLeft--;
+        }
+        return latticeDimensions;
+    }
+
+    protected final SpaceLattice lattice;
+    protected final IndexIteratorSizable indexIterator;
+    protected final AtomActionTranslateTo atomActionTranslateTo;
+    private static final long serialVersionUID = 2L;
+
+    public static void main(String[] args) {
+        Simulation sim = new Simulation(Space3D.getInstance());
+        sim.getDefaults().atomSize = 5.0;
+        Phase phase = new Phase(sim);
+        sim.addPhase(phase);
+        SpeciesSpheresMono species = new SpeciesSpheresMono(sim);
+        sim.getSpeciesManager().addSpecies(species);
+        int k = 4;
+        phase.getAgent(species).setNMolecules(4 * k * k * k);
+        IntegratorHard integrator = new IntegratorHard(sim);
+        integrator.setPhase(phase);
+//        ColorSchemeByType colorScheme = new ColorSchemeByType();
+        // CubicLattice lattice = new LatticeCubicBcc();
+        BravaisLatticeCrystal lattice = new LatticeCubicFcc();
+        // CubicLattice lattice = new LatticeCubicSimple();
+        ConfigurationLatticeSimple configuration = new ConfigurationLatticeSimple(lattice);
+        // phase.boundary().setDimensions(new Space3D.Vector(15.,30.,60.5));
+        configuration.initializeCoordinates(phase);
+        // etomica.graphics.DisplayPhase display = new
+        // etomica.graphics.DisplayPhase(phase);
+
+        etomica.graphics.SimulationGraphic simGraphic = new etomica.graphics.SimulationGraphic(
+                sim);
+//        ((ColorSchemeByType) ((DisplayPhase) simGraphic.displayList()
+//                .getFirst()).getColorScheme()).setColor(species
+//                .getMoleculeType(), java.awt.Color.red);
+        simGraphic.makeAndDisplayFrame();
+    }
+}
