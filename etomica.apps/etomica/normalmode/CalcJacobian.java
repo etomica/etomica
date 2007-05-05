5137c4447eceff81eedb4712a585071aeb9e39d0
Andrew Schultz <ajs42@buffalo.edu>
2007-05-05 01:53:00 +0000

    Add Jacobian class and "Simulation"

---
+++ etomica.apps/etomica/normalmode/CalcJacobian.java
@@ -0,0 +1,164 @@
+package etomica.normalmode;
+
+import etomica.atom.AtomAgentManager;
+import etomica.atom.IAtom;
+import etomica.atom.iterator.AtomIteratorAllMolecules;
+import etomica.normalmode.CoordinateDefinition.SiteSource;
+import etomica.phase.Phase;
+import etomica.space.IVector;
+
+/**
+ * Class that calculates the dq/dx Jacobian.
+ * @author Andrew Schultz
+ */
+public class CalcJacobian {
+
+    public CalcJacobian(int coordinateDim) {
+        this.coordinateDim = coordinateDim;
+        iterator = new AtomIteratorAllMolecules();
+    }
+    
+    public double[][] getJacobian() {
+        int l = coordinateDim*iterator.size();
+        double[][] tempJacobian;
+        boolean doFull = false;
+        if (doFull) {
+            tempJacobian = new double[l][l];
+        }
+        else {
+            tempJacobian = new double[l-coordinateDim][l];
+        }
+        
+        int vectorPos = 0;
+        if (doFull) {
+            for (int j=0; j<coordinateDim; j++) {
+                for (int i=0; i<l/coordinateDim; i++) {
+                    tempJacobian[j][i*coordinateDim+j] = 1;
+                }
+            }
+            vectorPos = 1;
+        }
+        for (int iVector = 0; iVector < waveVectors.length; iVector++) {
+            iterator.reset();
+            // sum T over atoms
+            int atomCount = 0;
+            boolean realIsZero = false;
+            boolean imaginaryIsZero = false;
+            for (IAtom atom = iterator.nextAtom(); atom != null;
+                 atom = iterator.nextAtom()) {
+                IVector latticePosition = (IVector)siteManager.getAgent(atom);
+                double kR = waveVectors[iVector].dot(latticePosition);
+                double coskR = Math.cos(kR);
+                double sinkR = Math.sin(kR);
+                for (int iDim = 0; iDim < coordinateDim; iDim++) {
+                    if (waveVectorCoefficients[iVector] == 1) {
+                        tempJacobian[vectorPos*coordinateDim+iDim][atomCount*coordinateDim+iDim] = coskR;
+                        tempJacobian[(vectorPos+1)*coordinateDim+iDim][atomCount*coordinateDim+iDim] = -sinkR;
+                    }
+                    else if (Math.abs(sinkR) < 1.e-14) {
+                        if (realIsZero) {
+                            throw new RuntimeException("oops "+iVector+" "+waveVectors[iVector]);
+                        }
+                        imaginaryIsZero = true;
+                        tempJacobian[vectorPos*coordinateDim+iDim][atomCount*coordinateDim+iDim] = coskR;
+                    }
+                    else if (Math.abs(coskR) < 1.e-14) {
+                        if (imaginaryIsZero) {
+                            throw new RuntimeException("oops "+iVector+" "+waveVectors[iVector]);
+                        }
+                        realIsZero = true;
+                        tempJacobian[vectorPos*coordinateDim+iDim][atomCount*coordinateDim+iDim] = -sinkR;
+                    }
+                    else {
+                        throw new RuntimeException("oops");
+                    }
+                }
+                atomCount++;
+            }
+            for (int iDim=0; iDim<coordinateDim; iDim++) {
+                // zero out the last matrix element if it's statistically 0
+                if (Math.abs(tempJacobian[vectorPos*coordinateDim+iDim][l-coordinateDim+iDim]) < 1.e-14) {
+                    tempJacobian[vectorPos*coordinateDim+iDim][l-coordinateDim+iDim] = 0;
+                }
+                if (waveVectorCoefficients[iVector] == 1) {
+                    if (Math.abs(tempJacobian[(vectorPos+1)*coordinateDim+iDim][l-coordinateDim+iDim]) < 1.e-14) {
+                        tempJacobian[(vectorPos+1)*coordinateDim+iDim][l-coordinateDim+iDim] = 0;
+                    }
+                }
+                for (int j = 0; j < l - coordinateDim; j+=coordinateDim) {
+                    // subtract the element corresonding to the last atom from the other atoms
+                    if (!doFull) {
+                        tempJacobian[vectorPos*coordinateDim+iDim][j+iDim] -= tempJacobian[vectorPos*coordinateDim+iDim][l-coordinateDim+iDim];
+                    }
+                    if (Math.abs(tempJacobian[vectorPos*coordinateDim+iDim][j+iDim]) < 1.e-14) {
+                        // zero out the element if it's statistically 0
+                        tempJacobian[vectorPos*coordinateDim+iDim][j+iDim] = 0;
+                    }
+                    if (waveVectorCoefficients[iVector] == 1) {
+                        // if both real and imaginary are important, handle the imaginary part
+                        if (!doFull) {
+                            tempJacobian[(vectorPos+1)*coordinateDim+iDim][j+iDim] -= tempJacobian[vectorPos*coordinateDim+iDim+1][l-coordinateDim+iDim];
+                        }
+                        if (Math.abs(tempJacobian[(vectorPos+1)*coordinateDim+iDim][j+iDim]) < 1.e-14) {
+                            tempJacobian[(vectorPos+1)*coordinateDim+iDim][j+iDim] = 0;
+                        }
+                    }
+                }
+            }
+            vectorPos += Math.round(waveVectorCoefficients[iVector]*2);
+        }
+        if (doFull) {
+            return tempJacobian;
+        }
+        double[][] jacobian = new double[l-coordinateDim][l-coordinateDim];
+        for (int i=0; i<l-coordinateDim; i++) {
+            for (int j=0; j<l-coordinateDim; j++) {
+                jacobian[i][j] = tempJacobian[i][j];
+            }
+        }
+        return jacobian;
+    }
+
+    /**
+     * Sets the object that defines the normal-coordinate wave vectors.
+     */
+    public void setWaveVectorFactory(WaveVectorFactory newWaveVectorFactory) {
+        waveVectorFactory = newWaveVectorFactory;
+    }
+
+    /**
+     * @return the WaveVectorFactory last given via the set methods.
+     */
+    public WaveVectorFactory getWaveVectorFactory() {
+        return waveVectorFactory;
+    }
+    
+    public void setPhase(Phase newPhase) {
+
+        realT = new double[coordinateDim];
+        imaginaryT = new double[coordinateDim];
+
+        waveVectorFactory.makeWaveVectors(newPhase);
+        waveVectors = waveVectorFactory.getWaveVectors();
+        waveVectorCoefficients = waveVectorFactory.getCoefficients();
+
+        siteManager = new AtomAgentManager(new SiteSource(newPhase.getSpace()), newPhase);
+        iterator.setPhase(newPhase);
+        iterator.reset();
+    }
+    
+    protected void setWaveVectors(IVector[] newWaveVectors, double[] coefficients) {
+        waveVectors = newWaveVectors;
+        waveVectorCoefficients = coefficients;
+    }
+    
+    private static final long serialVersionUID = 1L;
+    protected int coordinateDim;
+    protected double[] realT, imaginaryT;
+    protected IVector[] waveVectors;
+    protected double[] waveVectorCoefficients;
+    protected NormalModes normalModes;
+    private final AtomIteratorAllMolecules iterator;
+    private AtomAgentManager siteManager;
+    private WaveVectorFactory waveVectorFactory;
+}
---
+++ etomica.apps/etomica/normalmode/SimCalcJ.java
@@ -0,0 +1,123 @@
+package etomica.normalmode;
+
+import Jama.Matrix;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.config.ConfigurationLatticeSimple;
+import etomica.integrator.IntegratorMD;
+import etomica.lattice.BravaisLattice;
+import etomica.lattice.LatticeCubicFcc;
+import etomica.lattice.LatticeCubicSimple;
+import etomica.lattice.crystal.Primitive;
+import etomica.lattice.crystal.PrimitiveFcc;
+import etomica.phase.Phase;
+import etomica.potential.PotentialMaster;
+import etomica.simulation.Simulation;
+import etomica.space.BoundaryRectangularPeriodic;
+import etomica.space.IVector;
+import etomica.space.Space;
+import etomica.species.SpeciesSpheresMono;
+
+/**
+ * Simulation class of hard spheres in 1D or 3D that calculates the dq/dx
+ * Jacobian.
+ */
+public class SimCalcJ extends Simulation {
+
+    public SimCalcJ(Space space, int numAtoms) {
+        super(space, true, new PotentialMaster(space));
+
+        defaults.makeLJDefaults();
+        defaults.atomSize = 1.0;
+
+        SpeciesSpheresMono species = new SpeciesSpheresMono(this);
+        getSpeciesManager().addSpecies(species);
+
+        bdry = new BoundaryRectangularPeriodic(space, getRandom(), 1);
+        phase = new Phase(bdry);
+        addPhase(phase);
+        phase.getAgent(species).setNMolecules(numAtoms);
+
+        IVector dimensions = bdry.getDimensions();
+        if (space.D() == 1) {
+            dimensions.E(numAtoms);
+        }
+        else {
+            dimensions.E(Math.round(Math.pow(numAtoms/4, 1.0/3.0)));
+        }
+        bdry.setDimensions(dimensions);
+        phase.setBoundary(bdry);
+
+        if (space.D() == 1) {
+            lattice = new LatticeCubicSimple(1, phase.getBoundary().getDimensions().x(0) / numAtoms);
+        } else {
+            lattice = new LatticeCubicFcc(1);
+        }
+        config = new ConfigurationLatticeSimple(lattice);
+
+        config.initializeCoordinates(phase);
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // defaults
+        int D = 1;
+        int nA = 4;
+
+        // parse arguments
+        if (args.length > 0) {
+            nA = Integer.parseInt(args[0]);
+        }
+
+        System.out.println("Calculating "
+                + (D == 1 ? "1D" : (D == 3 ? "FCC" : "2D hexagonal"))
+                + " hard sphere Jacobian for N="+nA);
+
+        // construct simulation
+        SimCalcJ sim = new SimCalcJ(Space.getInstance(D), nA);
+
+        // set up initial configuration and save nominal positions
+        Primitive primitive = sim.lattice.getPrimitive();// lattice used to position atoms, not scaled to phase volume
+        if (D == 3) {
+            primitive = ((LatticeCubicFcc) sim.lattice).getPrimitiveFcc();
+        }
+
+        // set up normal-mode meter
+        WaveVectorFactory waveVectorFactory;
+        if (D == 1) {
+            waveVectorFactory = new WaveVectorFactory1D();
+        } else if (D == 2) {
+            waveVectorFactory = null;
+        } else {
+            waveVectorFactory = new WaveVectorFactoryFcc((PrimitiveFcc) primitive);
+        }
+        CalcJacobian meterJacobian = new CalcJacobian(D);
+        meterJacobian.setWaveVectorFactory(waveVectorFactory);
+        meterJacobian.setPhase(sim.phase);
+
+        double[][] jac = meterJacobian.getJacobian();
+        
+        for (int i=0; i<jac.length; i++) {
+            for (int j=0; j<jac[0].length; j++) {
+                System.out.print(jac[i][j]+"  ");
+            }
+            System.out.println();
+        }
+        
+        Matrix m = new Matrix(jac);
+        double d = m.det();
+        double d2 = Math.abs(d) / (Math.pow(nA,(D*(nA-1))/2.0));
+        System.out.println("raw det = "+d+" (log2 det = "+Math.log(Math.abs(d))/Math.log(2)+")");
+        System.out.println("dx/dq det = "+1/d2+" (log2 det = "+Math.log(d2)/Math.log(2)+")");
+    }
+
+    private static final long serialVersionUID = 1L;
+    public IntegratorMD integrator;
+    public ActivityIntegrate activityIntegrate;
+    public Phase phase;
+    public BoundaryRectangularPeriodic bdry;
+    public BravaisLattice lattice;
+    public ConfigurationLatticeSimple config;
+}
