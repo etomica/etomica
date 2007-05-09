c6f7432ee5bb5dc05aec7a3b2de9e546b1f48dce
Andrew Schultz <ajs42@buffalo.edu>
2007-05-09 02:37:05 +0000

    Use FCC primitive instead of cubic with FCC basis.

--- etomica.apps/etomica/normalmode/SimCalcJ.java
+++ etomica.apps/etomica/normalmode/SimCalcJ.java
@@ -5,15 +5,15 @@ import etomica.action.activity.ActivityIntegrate;
 import etomica.config.ConfigurationLatticeSimple;
 import etomica.integrator.IntegratorMD;
 import etomica.lattice.BravaisLattice;
-import etomica.lattice.LatticeCubicFcc;
-import etomica.lattice.LatticeCubicSimple;
 import etomica.lattice.crystal.Primitive;
+import etomica.lattice.crystal.PrimitiveCubic;
 import etomica.lattice.crystal.PrimitiveFcc;
 import etomica.phase.Phase;
 import etomica.potential.PotentialMaster;
 import etomica.simulation.Simulation;
+import etomica.space.Boundary;
+import etomica.space.BoundaryDeformableLattice;
 import etomica.space.BoundaryRectangularPeriodic;
-import etomica.space.IVector;
 import etomica.space.Space;
 import etomica.species.SpeciesSpheresMono;
 
@@ -32,26 +32,23 @@ public class SimCalcJ extends Simulation {
         SpeciesSpheresMono species = new SpeciesSpheresMono(this);
         getSpeciesManager().addSpecies(species);
 
-        bdry = new BoundaryRectangularPeriodic(space, getRandom(), 1);
-        phase = new Phase(bdry);
+        phase = new Phase(this);
         addPhase(phase);
         phase.getAgent(species).setNMolecules(numAtoms);
 
-        IVector dimensions = bdry.getDimensions();
+        Primitive primitive;
         if (space.D() == 1) {
-            dimensions.E(numAtoms);
+            primitive = new PrimitiveCubic(space, 1);
+            bdry = new BoundaryRectangularPeriodic(space, getRandom(), numAtoms);
         }
         else {
-            dimensions.E(Math.round(Math.pow(numAtoms/4, 1.0/3.0)));
+            primitive = new PrimitiveFcc(space, 1);
+            int n = (int)Math.round(Math.pow(numAtoms, 1.0/3.0));
+            bdry = new BoundaryDeformableLattice(primitive, getRandom(), new int[]{n,n,n});
         }
-        bdry.setDimensions(dimensions);
         phase.setBoundary(bdry);
 
-        if (space.D() == 1) {
-            lattice = new LatticeCubicSimple(1, phase.getBoundary().getDimensions().x(0) / numAtoms);
-        } else {
-            lattice = new LatticeCubicFcc(1);
-        }
+        lattice = new BravaisLattice(primitive);
         config = new ConfigurationLatticeSimple(lattice);
 
         config.initializeCoordinates(phase);
@@ -78,11 +75,7 @@ public class SimCalcJ extends Simulation {
         // construct simulation
         SimCalcJ sim = new SimCalcJ(Space.getInstance(D), nA);
 
-        // set up initial configuration and save nominal positions
-        Primitive primitive = sim.lattice.getPrimitive();// lattice used to position atoms, not scaled to phase volume
-        if (D == 3) {
-            primitive = ((LatticeCubicFcc) sim.lattice).getPrimitiveFcc();
-        }
+        Primitive primitive = sim.lattice.getPrimitive();
 
         // set up normal-mode meter
         WaveVectorFactory waveVectorFactory;
@@ -91,7 +84,7 @@ public class SimCalcJ extends Simulation {
         } else if (D == 2) {
             waveVectorFactory = null;
         } else {
-            waveVectorFactory = new WaveVectorFactoryFcc((PrimitiveFcc) primitive);
+            waveVectorFactory = new WaveVectorFactorySimple(primitive);
         }
         CalcJacobian meterJacobian = new CalcJacobian(D);
         meterJacobian.setWaveVectorFactory(waveVectorFactory);
@@ -99,25 +92,25 @@ public class SimCalcJ extends Simulation {
 
         double[][] jac = meterJacobian.getJacobian();
         
-        for (int i=0; i<jac.length; i++) {
-            for (int j=0; j<jac[0].length; j++) {
-                System.out.print(jac[i][j]+"  ");
+        if (jac.length < 100) {
+            for (int i=0; i<jac.length; i++) {
+                for (int j=0; j<jac[0].length; j++) {
+                    System.out.print(jac[i][j]+"  ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
         
         Matrix m = new Matrix(jac);
-        double d = m.det();
-        double d2 = Math.abs(d) / (Math.pow(nA,(D*(nA-1))/2.0));
-        System.out.println("raw det = "+d+" (log2 det = "+Math.log(Math.abs(d))/Math.log(2)+")");
-        System.out.println("dx/dq det = "+1/d2+" (log2 det = "+Math.log(1.0/d2)/Math.log(2)+")");
+        double d = Math.abs(m.det());
+        System.out.println("dx/dq det = "+1/d+" (log2 det = "+Math.log(1.0/d*Math.sqrt(nA))/Math.log(2)+")");
     }
 
     private static final long serialVersionUID = 1L;
     public IntegratorMD integrator;
     public ActivityIntegrate activityIntegrate;
     public Phase phase;
-    public BoundaryRectangularPeriodic bdry;
+    public Boundary bdry;
     public BravaisLattice lattice;
     public ConfigurationLatticeSimple config;
 }
