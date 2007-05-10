367fb7fd497e9e47e429c068d91b949989750f2a
Andrew Schultz <ajs42@buffalo.edu>
2007-05-10 01:29:16 +0000

    Use IVector3D

--- etomica/math/geometry/Plane.java
+++ etomica/math/geometry/Plane.java
@@ -2,6 +2,8 @@
 //(maybe should put this in a Space package hierarchy)
 package etomica.math.geometry;
 
+import etomica.space.IVector;
+import etomica.space3d.IVector3D;
 import etomica.space3d.Vector3D;
 
 /**
@@ -11,20 +13,16 @@ import etomica.space3d.Vector3D;
  */
  
  //reference:  http://mathworld.wolfram.com/Plane.html
- 
- /* History of changes
-  * 09/07/02 (DAK) new
-  * 09/21/02 (DAK) added method for computing in-plane vectors
-  */
 
 public class Plane implements java.io.Serializable {
     
+    private static final long serialVersionUID = 1L;
     //coefficients defining plane, ax + by + cz + d = 0
     //such that a^2 + b^c + c^2 = 1
     private double a, b, c, d;
-    private Vector3D[] inPlane; //work vectors used by inPlaneSquare method
-    private Vector3D work0; //work vector used by inPlaneSquare method (no-x0 version)
-    private Vector3D work1; //work vector used by inPlaneSquare method (no-x0 version)
+    private IVector3D[] inPlane; //work vectors used by inPlaneSquare method
+    private IVector3D work0; //work vector used by inPlaneSquare method (no-x0 version)
+    private IVector3D work1; //work vector used by inPlaneSquare method (no-x0 version)
     
     /**
      * Tolerance used to judge if a given point is in the plane.
@@ -62,7 +60,7 @@ public class Plane implements java.io.Serializable {
      * Defines the plane via a normal vector (1st argument) and
      * a point (2nd argument).
      */
-    public void setNormalPoint(Vector3D normal, Vector3D point) {
+    public void setNormalPoint(IVector3D normal, IVector3D point) {
         a = normal.x(0);
         b = normal.x(1);
         c = normal.x(2);
@@ -116,7 +114,7 @@ public class Plane implements java.io.Serializable {
      * Sets the given vector to be a unit normal vector to the plane and returns it.  
      * If argument is null, creates a new 3D Vector to return.
      */
-    public Vector3D getNormalVector(Vector3D normal) {
+    public IVector getNormalVector(IVector3D normal) {
         if(normal == null) return new Vector3D(a, b, c);
         normal.E(a,b,c);
         return normal;
@@ -131,10 +129,10 @@ public class Plane implements java.io.Serializable {
      * second vector is (uniquely) defined as perpendicular to both the normal and the first
      * in-plane vector.
      */
-    public Vector3D[] inPlaneVectors(Vector3D[] p) {
+    public IVector3D[] inPlaneVectors(IVector3D[] p) {
         if(p == null || p.length != 2) p = new Vector3D[] {new Vector3D(), new Vector3D()};
-        Vector3D p1 = p[0];
-        Vector3D p2 = p[1];
+        IVector3D p1 = p[0];
+        IVector3D p2 = p[1];
         if(a == 0 && b == 0) {//parallel to xy plane
             p1.E(1, 0, 0);
             p2.E(0, 1, 0);
@@ -153,7 +151,7 @@ public class Plane implements java.io.Serializable {
     /**
      * Sets the orientation of the plane to be normal to the given vector.
      */
-    public void setNormalVector(Vector3D n) {
+    public void setNormalVector(IVector3D n) {
         if(n.squared() == 0.0) throw new IllegalArgumentException("Error: attempt to set orientation of plane with respect to an ill-defined vector");
         a = n.x(0);
         b = n.x(1);
@@ -201,7 +199,7 @@ public class Plane implements java.io.Serializable {
      * Sets the given vector to be the point in the plane closest to the origin.
      * If vector is null, makes a new Vector and returns it.
      */
-    public Vector3D center(Vector3D v) {
+    public IVector3D center(IVector3D v) {
         if(v == null) return new Vector3D(-d*a, -d*b, -d*c);
         v.E(-d*a, -d*b, -d*c);
         return v;
@@ -212,7 +210,7 @@ public class Plane implements java.io.Serializable {
      * Uses second vector to hold result that is returned; if second vector is 
      * null, creates a new Vector instance.
      */
-    public Vector3D nearestPoint(Vector3D x0, Vector3D point) {
+    public IVector3D nearestPoint(Vector3D x0, Vector3D point) {
         if(point == null) point = new Vector3D();
         double factor = distanceTo(x0);
         point.E(x0.x(0)-factor*a, x0.x(1)-factor*b, x0.x(2)-factor*c);
@@ -237,7 +235,7 @@ public class Plane implements java.io.Serializable {
      * Dihedral angle between this plane and the given plane.
      */
     public double dihedralAngle(Plane p) {
-        Vector3D n = p.getNormalVector(null);
+        IVector n = p.getNormalVector(null);
         return a*n.x(0) + b*n.x(1) + c*n.x(2);
     }
     
@@ -285,11 +283,11 @@ public class Plane implements java.io.Serializable {
      * otherwise makes a new array and returns it.  Square is aligned so that its vertices
      * fall on the lines defined by the inPlaneVectors result.
      */
-    public Vector3D[] inPlaneSquare(Vector3D x0, double d, Vector3D[] s) {
-        if(s == null || s.length != 4) s = new Vector3D[] {new Vector3D(), new Vector3D(), new Vector3D(), new Vector3D()};
+    public IVector3D[] inPlaneSquare(IVector x0, double d, IVector3D[] s) {
+        if(s == null || s.length != 4) s = new IVector3D[] {new Vector3D(), new Vector3D(), new Vector3D(), new Vector3D()};
         inPlane = inPlaneVectors(inPlane);
-        Vector3D p1 = inPlane[0];
-        Vector3D p2 = inPlane[1];
+        IVector p1 = inPlane[0];
+        IVector p2 = inPlane[1];
       //  System.out.println("work: "+x0.toString());
       //  System.out.println("p1 : "+p1.toString());
       //  System.out.println("p2 : "+p2.toString());
@@ -304,7 +302,7 @@ public class Plane implements java.io.Serializable {
     /**
      * inPlaneSquare with square centered on point in plane closest to origin.
      */
-    public Vector3D[] inPlaneSquare(double d, Vector3D[] s) {
+    public IVector3D[] inPlaneSquare(double d, IVector3D[] s) {
         return inPlaneSquare(center(work0), d, s);
     }
     
@@ -321,8 +319,8 @@ public class Plane implements java.io.Serializable {
     public static void main(String[] args) {
         Plane plane = new Plane(2.8, 7.5, -2.1, 293.8);
     //    Plane plane = new Plane(5.0, 0.0, 0.0, 293.8);
-        Vector3D[] p = plane.inPlaneVectors(null);
-        Vector3D n = plane.getNormalVector(null);
+        IVector[] p = plane.inPlaneVectors(null);
+        IVector n = plane.getNormalVector(null);
         System.out.println("Normal: "+n.toString());
         System.out.println("plane1: "+p[0].toString());
         System.out.println("plane2: "+p[1].toString());
