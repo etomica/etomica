cc8002ed27f9a921773e9c9a26bc5ee9cb2f7054
Andrew Schultz <ajs42@buffalo.edu>
2007-05-18 23:32:34 +0000

    make it possible to run interactively, subtract not add jacobian

--- etomica.apps/etomica/normalmode/CalcHarmonicA.java
+++ etomica.apps/etomica/normalmode/CalcHarmonicA.java
@@ -15,13 +15,22 @@ public class CalcHarmonicA {
     public static void main(String[] args) {
         
         //set up simulation parameters
-        String filename = args[0];
-        int D = Integer.parseInt(args[1]);
-        int totalCells = Integer.parseInt(args[2]);
-        double temperature = 1.0;
+        String filename = "normal_modes_LJ_3D_108";
+        int D = 3; 
+        int totalCells = 27;
+        double temperature = 0.01;
         double harmonicFudge = 1.0;
-        int basisSize = 1;
-        
+        int basisSize = 4;
+
+        if (args.length > 0) {
+            filename = args[0];
+        }
+        if (args.length > 1) {
+            D = Integer.parseInt(args[1]);
+        }
+        if (args.length > 2) {
+            totalCells = Integer.parseInt(args[2]);
+        }
         if (args.length > 3) {
             harmonicFudge = Double.parseDouble(args[3]);
         }
@@ -31,7 +40,11 @@ public class CalcHarmonicA {
         if (args.length > 5) {
             basisSize = Integer.parseInt(args[5]);
         }
-
+        
+        doit(filename, D, harmonicFudge, temperature, basisSize, totalCells);
+    }
+    
+    public static void doit(String filename, int D, double harmonicFudge, double temperature, int basisSize, int totalCells) {
         NormalModesFromFile normalModes = new NormalModesFromFile(filename, D);
         normalModes.setHarmonicFudge(harmonicFudge);
         normalModes.setTemperature(temperature);
@@ -47,10 +60,10 @@ public class CalcHarmonicA {
         }
         
         if (totalCells % 2 == 0) {
-            AHarmonic += Math.log(Math.pow(2.0, basisSize*D*(totalCells - Math.pow(2,D))/2.0) / Math.pow(totalCells,0.5*D));
+            AHarmonic -= Math.log(Math.pow(2.0, basisSize*D*(totalCells - Math.pow(2,D))/2.0) / Math.pow(totalCells,0.5*D));
         }
         else {
-            AHarmonic += Math.log(Math.pow(2.0, basisSize*D*(totalCells - 1)/2.0) / Math.pow(totalCells,0.5*D));
+            AHarmonic -= Math.log(Math.pow(2.0, basisSize*D*(totalCells - 1)/2.0) / Math.pow(totalCells,0.5*D));
         }
         
         System.out.println("Harmonic-reference free energy: "+AHarmonic*temperature);
