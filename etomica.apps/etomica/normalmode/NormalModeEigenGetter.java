a25c695cd2311a160e5ed7596beb7b735726853c
Andrew Schultz <ajs42@buffalo.edu>
2007-05-19 02:21:21 +0000

    Explicitly write 0 for the first three eignenvalues (which should correspond to the COM translation).  Write eigenvectors in rows instead of columns.

--- etomica.apps/etomica/normalmode/NormalModeEigenGetter.java
+++ etomica.apps/etomica/normalmode/NormalModeEigenGetter.java
@@ -9,14 +9,19 @@ import Jama.Matrix;
 public class NormalModeEigenGetter {
     
     public static void main (String[] args){
-        String filename = "normal_modes3D_32_130_cubic";
+        String filename = "normal_modes_LJ_3D_32";
         if (args.length > 0) {
             filename = args[0];
         }
+        doit(filename);
+    }
+    
+    public static void doit(String filename) {
         //first index of S indicates the wave vector
         //the remaining two indices describe a square matrix of dimension equal to coordinateDim
         //e.g., coordinateDim = D for monatomic spherical molecules in D dimensions
         double[][][] S = ArrayReader2D.getFromFile(filename+".S");
+        double maxIgnoredVal = 0;
         try {
             FileWriter fileWriterVal = new FileWriter(filename+".val");
             FileWriter fileWriterVec = new FileWriter(filename+".vec");
@@ -26,14 +31,25 @@ public class NormalModeEigenGetter {
     //            double[] e = evaler.getImagEigenvalues();
                 Matrix eigenvectors = evaler.getV();
     
-                for (int j=0; j<eigenvectors.getRowDimension(); j++) {
-                    for (int k=0; k<eigenvectors.getColumnDimension(); k++) {
+                for (int k=0; k<eigenvectors.getColumnDimension(); k++) {
+                    for (int j=0; j<eigenvectors.getRowDimension(); j++) {
                         fileWriterVec.write(eigenvectors.get(j,k)+" ");
                     }
                     fileWriterVec.write("\n");
                 }
                 for (int j=0; j<d.length; j++) {
-                    fileWriterVal.write(d[j]+" ");
+                    if (i==0 && j<3) {
+                        if (Math.abs(d[j]) > maxIgnoredVal) {
+                            maxIgnoredVal = d[j];
+                        }
+                        fileWriterVal.write("0 ");
+                    }
+                    else {
+                        if (maxIgnoredVal > d[j] * 1E-10) {
+                            System.err.println("Ignored "+maxIgnoredVal+" and now I found "+d[j]);
+                        }
+                        fileWriterVal.write(d[j]+" ");
+                    }
                 }
                 fileWriterVal.write("\n");
                 
@@ -50,5 +66,4 @@ public class NormalModeEigenGetter {
             throw new RuntimeException(e);
         }
     }
-    
 }
