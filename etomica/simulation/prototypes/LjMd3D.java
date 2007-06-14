448657cb92b15180053ad3b340c42688246d335c
Andrew Schultz <ajs42@buffalo.edu>
2007-06-14 01:36:54 +0000

    Clean up, take and report something useful

--- etomica/simulation/prototypes/LjMd3D.java
+++ etomica/simulation/prototypes/LjMd3D.java
@@ -2,11 +2,14 @@ package etomica.simulation.prototypes;
 import etomica.action.PhaseImposePbc;
 import etomica.action.activity.ActivityIntegrate;
 import etomica.action.activity.Controller;
-import etomica.data.meter.MeterEnergy;
-import etomica.graphics.DisplayPhase;
-import etomica.graphics.DisplayPlot;
+import etomica.config.ConfigurationLattice;
+import etomica.data.AccumulatorAverage;
+import etomica.data.DataPump;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.graphics.DisplayBoxesCAE;
 import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntegratorVelocityVerlet;
+import etomica.lattice.LatticeCubicFcc;
 import etomica.phase.Phase;
 import etomica.potential.P2LennardJones;
 import etomica.potential.PotentialMaster;
@@ -27,18 +30,21 @@ public class LjMd3D extends Simulation {
     public Phase phase;
     public P2LennardJones potential;
     public Controller controller;
-    public DisplayPhase display;
-    public DisplayPlot plot;
-    public MeterEnergy energy;
+    public MeterPotentialEnergy energy;
+    public AccumulatorAverage avgEnergy;
 
     public static void main(String[] args) {
     	final String APP_NAME = "LjMd3D";
     	final LjMd3D sim = new LjMd3D();
-    	final SimulationGraphic simGraphic = new SimulationGraphic(sim, APP_NAME);
+    	final SimulationGraphic simGraphic = new SimulationGraphic(sim, APP_NAME, 3);
 
         simGraphic.getController().getReinitButton().setPostAction(simGraphic.getDisplayPhasePaintAction(sim.phase));
 
         simGraphic.makeAndDisplayFrame(APP_NAME);
+
+        DisplayBoxesCAE display = new DisplayBoxesCAE();
+        display.setAccumulator(sim.avgEnergy);
+        simGraphic.add(display);
     }
     
     public LjMd3D() {
@@ -47,8 +53,8 @@ public class LjMd3D extends Simulation {
         defaults.makeLJDefaults();
         integrator = new IntegratorVelocityVerlet(this, potentialMaster);
         integrator.setTimeStep(0.01);
-        ActivityIntegrate activityIntegrate = new ActivityIntegrate(this,integrator);
-        activityIntegrate.setSleepPeriod(2);
+        ActivityIntegrate activityIntegrate = new ActivityIntegrate(integrator, true, false);
+        activityIntegrate.setSleepPeriod(1);
         getController().addAction(activityIntegrate);
         species = new SpeciesSpheresMono(this);
         getSpeciesManager().addSpecies(species);
@@ -58,22 +64,21 @@ public class LjMd3D extends Simulation {
         potential = new P2LennardJones(this);
         potentialMaster.addPotential(potential,new Species[]{species,species});
         
-//      elementCoordinator.go();
-        //explicit implementation of elementCoordinator activities
         integrator.setPhase(phase);
         PhaseImposePbc imposepbc = new PhaseImposePbc();
         imposepbc.setPhase(phase);
         integrator.addIntervalAction(imposepbc);
 		
-		energy = new MeterEnergy(potentialMaster);
-//		energy.setHistorying(true);
-//		
-//		energy.getHistory().setHistoryLength(500);
-//		
-//		plot = new DisplayPlot(this);
-//		plot.setLabel("Energy");
-//		plot.setDataSources(energy.getHistory());
-		
+        ConfigurationLattice configuration = new ConfigurationLattice(new LatticeCubicFcc());
+        configuration.initializeCoordinates(phase);
+        energy = new MeterPotentialEnergy(potentialMaster);
+        energy.setPhase(phase);
+        avgEnergy = new AccumulatorAverage(200);
+        avgEnergy.setPushInterval(10);
+        DataPump pump = new DataPump(energy, avgEnergy);
+        integrator.addIntervalAction(pump);
+        integrator.setActionInterval(pump, 10);
+        register(energy, pump);
     }
     
 }
