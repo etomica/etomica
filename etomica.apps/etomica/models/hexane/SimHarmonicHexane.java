cda9bc80a0bf351186975df80c913c820c72c6a5
Nancy Cribbin <cribbin@buffalo.edu>
2007-06-26 21:08:43 +0000

    Starting to code.  Don't worry Andrew, there's no errors in it.  :)

--- etomica.apps/etomica/models/hexane/SimHarmonicHexane.java
+++ etomica.apps/etomica/models/hexane/SimHarmonicHexane.java
@@ -1,12 +1,19 @@
-/*
- * Created on May 24, 2005
- */
 package etomica.models.hexane;
+
+import java.util.ArrayList;
 
 import etomica.action.activity.ActivityIntegrate;
 import etomica.atom.AtomType;
 import etomica.atom.AtomTypeGroup;
 import etomica.atom.AtomTypeSphere;
+import etomica.data.AccumulatorAverage;
+import etomica.data.DataFork;
+import etomica.data.DataPump;
+import etomica.data.AccumulatorAverage.StatType;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataGroup;
+import etomica.graphics.DisplayBoxesCAE;
 import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntegratorMC;
 import etomica.integrator.mcmove.MCMoveMolecule;
@@ -14,30 +21,40 @@ import etomica.integrator.mcmove.MCMoveRotateMolecule3D;
 import etomica.integrator.mcmove.MCMoveStepTracker;
 import etomica.lattice.BravaisLattice;
 import etomica.lattice.crystal.Primitive;
+import etomica.lattice.crystal.PrimitiveCubic;
+import etomica.lattice.crystal.PrimitiveFcc;
+import etomica.nbr.list.PotentialMasterList;
+import etomica.normalmode.BoltzmannProcessor;
 import etomica.normalmode.CoordinateDefinition;
+import etomica.normalmode.CoordinateDefinitionLeaf;
+import etomica.normalmode.MCMoveHarmonic;
 import etomica.normalmode.MCMoveMoleculeCoupled;
+import etomica.normalmode.MeterHarmonicEnergy;
+import etomica.normalmode.MeterNormalMode;
+import etomica.normalmode.NormalModes;
+import etomica.normalmode.NormalModes1DHR;
+import etomica.normalmode.NormalModesFromFile;
+import etomica.normalmode.P2XOrder;
+import etomica.normalmode.SimHarmonic;
+import etomica.normalmode.WaveVectorFactory;
 import etomica.phase.Phase;
 import etomica.potential.P2HardSphere;
 import etomica.potential.Potential;
+import etomica.potential.Potential2;
+import etomica.potential.Potential2HardSpherical;
 import etomica.potential.PotentialMaster;
 import etomica.simulation.Simulation;
+import etomica.space.Boundary;
 import etomica.space.BoundaryDeformableLattice;
 import etomica.space.BoundaryDeformablePeriodic;
+import etomica.space.BoundaryRectangularPeriodic;
 import etomica.space.Space;
 import etomica.space3d.Space3D;
-/**
- * @author nancycribbin
- *  
- */
+import etomica.species.Species;
+import etomica.species.SpeciesSpheresMono;
+import etomica.units.Pixel;
 
-/*
- * We use a PotentialMaster, rather than a PotentialMasterNbr, so that we do not
- * need to deal with cells, which BoundaryDeformablePeriodic cannot deal with at
- * this time.
- * 
- */
-
-public class TestHexane extends Simulation {
+public class SimHarmonicHexane extends Simulation {
 
     public ActivityIntegrate activityIntegrate;
     public IntegratorMC integrator;
@@ -49,9 +66,6 @@ public class TestHexane extends Simulation {
     public CoordinateDefinition coordinateDefinition;
     public Primitive primitive;
     
-//    public MCMoveVolume moveVolume;
-//    public MCMoveCrankshaft crank; 
-//    public MCMoveReptate snake;
     public MCMoveMolecule moveMolecule;
     public CBMCGrowSolidHexane growMolecule;
     public MCMoveRotateMolecule3D rot;
@@ -61,7 +75,7 @@ public class TestHexane extends Simulation {
 //    public PairIndexerMolecule pri;
 
     
-    public TestHexane(Space space, double dens, int xCells, int yCells, int zCells) {
+    public SimHarmonicHexane(Space space, double dens, int xCells, int yCells, int zCells) {
         //super(space, false, new PotentialMasterNbr(space, 12.0));
 //        super(space, true, new PotentialMasterList(space, 12.0));
         super(space, false);
@@ -203,8 +217,8 @@ public class TestHexane extends Simulation {
 //        // 2 atoms of a molecule.
 //        
 //        //Only the atoms next to each other interact, so we have two criteria:
-//        //		The atoms must be on the same molecule- CriterionMolecular
-//        //		The atoms must be separated by 3 bonds, or 2 other atoms.
+//        //        The atoms must be on the same molecule- CriterionMolecular
+//        //        The atoms must be separated by 3 bonds, or 2 other atoms.
 //        ApiIntragroup nonbonded = ApiBuilder.makeNonAdjacentPairIterator(2);
 //        potentialChainIntra.addPotential(potential, nonbonded);
 //        
@@ -389,5 +403,278 @@ public class TestHexane extends Simulation {
 
         System.out.println("Go look at the data!");
     }
-
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    private static final String APP_NAME = "Sim Harmonic";
+//    private static final long serialVersionUID = 1L;
+//    public IntegratorMC integrator;
+//    public ActivityIntegrate activityIntegrate;
+//    public Phase phase;
+//    public Boundary boundary;
+//    public Species species;
+//    public NormalModes normalModes;
+//    public int[] nCells;
+//    public CoordinateDefinition coordinateDefinition;
+//    public PrimitiveHexane primitive;
+//    
+//    private double density;
+//   
+//    
+//    public SimHarmonicHexane(Space space, double dens, int xCells, int yCells, 
+//            int zCells, String filename, double harmonicFudge) {
+//        super(space, true);
+//
+//        int chainLength = 6;
+//        int numAtoms = xCells * yCells * zCells * chainLength;
+//        int D = space.D();
+//        if( D != 3) {
+//            throw new IllegalStateException("SimHarmonicHexane requires a 3D space");
+//        }
+//
+//        species = new SpeciesHexane(this);
+//        getSpeciesManager().addSpecies(species);
+//
+//        phase = new Phase(this);
+//        addPhase(phase);
+//        phase.getAgent(species).setNMolecules(numAtoms);
+//
+//        integrator = new IntegratorMC(this, null);
+//
+//        activityIntegrate = new ActivityIntegrate(integrator);
+//        getController().addAction(activityIntegrate);
+//
+//        MCMoveHarmonic move = new MCMoveHarmonic(getRandom());
+//        integrator.getMoveManager().addMCMove(move);
+//
+//            primitive = new PrimitiveHexane(space);
+//            double v = primitive.unitCell().getVolume();
+//            // close packed density is 0.4165783882178116
+//            // Monson reports data for 0.373773507616 and 0.389566754417
+//            
+//            primitive.scaleSize(Math.pow(v*dens,-1.0/3.0));
+//            int n = (int)Math.round(Math.pow(numAtoms, 1.0/3.0));
+//            nCells = new int[]{xCells, yCells, zCells};
+//            boundary = new BoundaryDeformableLattice(primitive, getRandom(), nCells);
+//
+//            phase.setBoundary(boundary);
+//
+//        coordinateDefinition = new CoordinateDefinitionLeaf(phase, primitive);
+//        coordinateDefinition.initializeCoordinates(nCells);
+//        
+//        if(D == 1) {
+//            normalModes = new NormalModes1DHR();
+//        } else {
+//            normalModes = new NormalModesFromFile(filename, D);
+//        }
+//        normalModes.setHarmonicFudge(harmonicFudge);
+//        
+//        WaveVectorFactory waveVectorFactory = normalModes.getWaveVectorFactory();
+//        waveVectorFactory.makeWaveVectors(phase);
+//        move.setOmegaSquared(normalModes.getOmegaSquared(phase), waveVectorFactory.getCoefficients());
+//        move.setEigenVectors(normalModes.getEigenvectors(phase));
+//        move.setWaveVectors(waveVectorFactory.getWaveVectors());
+//        move.setWaveVectorCoefficients(waveVectorFactory.getCoefficients());
+//        move.setCoordinateDefinition(coordinateDefinition);
+//        
+//        move.setPhase(phase);
+//        
+//        integrator.setPhase(phase);
+//    }
+//
+//    /**
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        
+//        //set up simulation parameters
+//        int D = 3;
+//        int nA = 27;
+//        
+//        double density = 1.3;
+//        double harmonicFudge = 1;
+//        long steps = 800000;
+//        if (D == 1) {
+//            nA = 3;
+//            density = 0.5;
+//        }
+//        boolean graphic = false;
+//        String filename = "normal_modes3D_27_130";
+//        if (args.length > 0) {
+//            filename = args[0];
+//        }
+//        if (args.length > 1) {
+//            density = Double.parseDouble(args[1]);
+//        }
+//        if (args.length > 2) {
+//            steps = Long.parseLong(args[2]);
+//        }
+//        if (args.length > 3) {
+//            nA = Integer.parseInt(args[3]);
+//        }
+//        if (args.length > 4) {
+//            harmonicFudge = Double.parseDouble(args[4]);
+//        }
+//        System.out.println("Running "+(D==1 ? "1D" : (D==3 ? "FCC" : "2D hexagonal")) +" harmonic simulation, measuring hard sphere energy");
+//        System.out.println(nA+" atoms at density "+density);
+//        System.out.println("harmonic fudge: "+harmonicFudge);
+//        System.out.println(steps+" MC steps");
+//        
+//        //construct simulation
+//        SimHarmonic sim = new SimHarmonic(Space.getInstance(D), nA, density, filename, harmonicFudge);
+//        
+//        //add hard potentials for FEP calculations.  With de novo sampling potential is not otherwise used.
+//        Potential2 p2 = new P2HardSphere(sim.getSpace(), 1.0, true);
+//        if (D == 1) {
+//            p2 = new P2XOrder(sim.getSpace(), (Potential2HardSpherical)p2);
+//        }
+//        PotentialMaster potentialMaster = (D == 1 ? new PotentialMasterList(sim) : new PotentialMaster(sim.getSpace()));
+//        potentialMaster.addPotential(p2, new AtomType[]{sim.species.getMoleculeType(),sim.species.getMoleculeType()});
+//
+//        if (potentialMaster instanceof PotentialMasterList) {
+//            double neighborRange;
+//            if (D == 1) {
+//                neighborRange = 1.01 / density;
+//            }
+//            else {
+//                //FCC
+//                double L = Math.pow(0.26*density, 1.0/3.0);
+//                neighborRange = L / Math.sqrt(2.0);
+//            }
+//            ((PotentialMasterList)potentialMaster).setRange(neighborRange);
+//            // find neighbors now.  Don't hook up NeighborListManager (neighbors won't change)
+//            ((PotentialMasterList)potentialMaster).getNeighborManager(sim.phase).reset();
+//        }
+//
+//        //meters for FEP calculations
+//        MeterPotentialEnergy meterPE = new MeterPotentialEnergy(potentialMaster);
+//        meterPE.setPhase(sim.phase);
+//        BoltzmannProcessor bp = new BoltzmannProcessor();
+//        bp.setTemperature(1);
+//        DataPump pump = new DataPump(meterPE,bp);
+//        AccumulatorAverage avgBoltzmann = new AccumulatorAverage(1);
+//        bp.setDataSink(avgBoltzmann);
+//        avgBoltzmann.setPushInterval(5);
+//        sim.integrator.addIntervalAction(pump);
+//
+////         MeterMomentumCOM meterCOM = new MeterMomentumCOM(sim.space);
+////         MeterPositionCOM meterCOM = new MeterPositionCOM(sim.space);
+////         DataSinkConsole console = new DataSinkConsole();
+////         DataProcessorFunction filter = new DataProcessorFunction(new Function.Chop());
+////         DataPump comPump = new DataPump(meterCOM,filter);
+////         filter.setDataSink(console);
+////         IntervalActionAdapter comAdapter = new IntervalActionAdapter(comPump);
+////         sim.integrator.addListener(comAdapter);
+////         meterCOM.setPhase(sim.phase);
+//
+//        //set up things for determining energy of harmonic system
+//        //read and set up wave vectors
+//
+//        if(graphic){
+//            //meter for harmonic system energy, sent to direct and to boltzmann average
+//            MeterHarmonicEnergy harmonicEnergy = new MeterHarmonicEnergy(sim.coordinateDefinition, sim.normalModes);
+//            harmonicEnergy.setPhase(sim.phase);
+//            DataFork harmonicFork = new DataFork();
+//            AccumulatorAverage harmonicAvg = new AccumulatorAverage(5);
+//            DataPump pumpHarmonic = new DataPump(harmonicEnergy, harmonicFork);
+//            harmonicFork.addDataSink(harmonicAvg);
+//            sim.integrator.addIntervalAction(pumpHarmonic);
+//
+//            //histogram energy of individual modes
+////            MeterHarmonicSingleEnergy harmonicSingleEnergy = new MeterHarmonicSingleEnergy(coordinateDefinitionLeaf, sim.normalModes);
+////            harmonicSingleEnergy.setTemperature(1.0);
+////            harmonicSingleEnergy.setPhase(sim.phase);
+////    //        DataProcessorFunction harmonicLog = new DataProcessorFunction(new Function.Log());
+////            AccumulatorAverage harmonicSingleAvg = new AccumulatorAverage(5);
+////            DataHistogram harmonicSingleHistogram = new DataHistogram(new HistogramSimple.Factory(50, new DoubleRange(0, 1)));
+////            pump = new DataPump(harmonicSingleEnergy, harmonicSingleHistogram);
+////    //        harmonicLog.setDataSink(harmonicSingleHistogram);
+////            harmonicSingleHistogram.setDataSink(harmonicSingleAvg);
+////            iaa= new IntervalActionAdapter(pump);
+////            iaa.setActionInterval(1);
+////            sim.integrator.addListener(iaa);
+//            
+//            //set up measurement of S matrix, to check that configurations are generated as expected
+//            MeterNormalMode meterNormalMode = new MeterNormalMode();
+//            meterNormalMode.setCoordinateDefinition(sim.coordinateDefinition);
+//            WaveVectorFactory waveVectorFactory = sim.normalModes.getWaveVectorFactory();
+//            meterNormalMode.setWaveVectorFactory(waveVectorFactory);
+//            meterNormalMode.setPhase(sim.phase);
+//
+//
+//            //graphic simulation -- set up window
+////            sim.getDefaults().pixelUnit = new Pixel(0.05);
+//            SimulationGraphic simG = new SimulationGraphic(sim, APP_NAME);
+//            ArrayList dataStreamPumps = simG.getController().getDataStreamPumps();
+//            dataStreamPumps.add(pump);
+//            dataStreamPumps.add(pumpHarmonic);
+//            
+//            DisplayBoxesCAE boxesPE = new DisplayBoxesCAE();
+//            boxesPE.setAccumulator(avgBoltzmann);
+//            boxesPE.setPrecision(6);
+//            simG.add(boxesPE);
+//
+//            DisplayBoxesCAE harmonicBoxes = new DisplayBoxesCAE();
+//            harmonicBoxes.setAccumulator(harmonicAvg);
+//            simG.add(harmonicBoxes);
+//
+////            DisplayPlot harmonicPlot = new DisplayPlot();
+////            harmonicPlot.setDoLegend(false);
+////            harmonicSingleAvg.addDataSink(harmonicPlot.getDataSet().makeDataSink(), new StatType[]{StatType.AVERAGE});
+////            simG.add(harmonicPlot);
+//
+//            simG.getDisplayPhase(sim.phase).setPixelUnit(new Pixel(10));
+//            simG.makeAndDisplayFrame(APP_NAME);
+//        } else {
+//            //not graphic, so run simulation batch
+//            //S data is written to file
+//            sim.activityIntegrate.setMaxSteps(steps);
+//
+//            sim.getController().actionPerformed();
+//            
+//            DataGroup boltzmannData = (DataGroup)avgBoltzmann.getData();
+//            double pNotOverlap = ((DataDouble)boltzmannData.getData(StatType.AVERAGE.index)).x;
+//            double pError = ((DataDouble)boltzmannData.getData(StatType.ERROR.index)).x;
+//            
+//            System.out.println("avg HS Boltzmann factor "+pNotOverlap+" +/- "+pError);
+//            
+//            System.out.println("free energy contribution "+(-Math.log(pNotOverlap))+" +/- "+(pError/pNotOverlap));
+//            System.out.println("free energy contribution per molecule "+(-Math.log(pNotOverlap)/nA)+" +/- "+(pError/pNotOverlap)/nA);
+//        }
+//
+//    }
+//
+//
+//}
