b411ad9964ec967034e37f11b9322b6619adf0a9
Andrew Schultz <ajs42@buffalo.edu>
2007-07-03 18:54:05 +0000

    bug 160 bustage: fix AtomIteratorSinglet

--- etomica/atom/iterator/AtomIteratorSinglet.java
+++ etomica/atom/iterator/AtomIteratorSinglet.java
@@ -86,6 +86,7 @@ public final class AtomIteratorSinglet implements AtomIteratorAtomDependent, jav
     
     public AtomSet next() {
         atomSetSinglet.atom = nextAtom();
+        if (atomSetSinglet.atom == null) return null;
         return atomSetSinglet;
     }
     
