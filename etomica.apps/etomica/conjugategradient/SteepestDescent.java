0c8d470d8b5204253d725b28b861b62ef2970a88
Mike Sellers <msellers@buffalo.edu>
2007-07-10 17:53:06 +0000

    Working Steepest Descent Algorithm for solving Ax = b.

--- etomica.apps/etomica/conjugategradient/SteepestDescent.java
+++ etomica.apps/etomica/conjugategradient/SteepestDescent.java
@@ -3,77 +3,106 @@ package etomica.conjugategradient;
 import Jama.Matrix;
 
 public class SteepestDescent {
+
 	protected int imax;
 	protected Matrix A; 
 	protected Matrix B, X, R, Q; 
-	protected double delta, delta0;
+
 	
-	public SteepestDescent(int iter, Matrix a, Matrix x, Matrix b){
+	public SteepestDescent(Matrix A, Matrix x, Matrix b, int iter){
 		this.imax = iter;
 		this.B = b;
-		this.A = a;
+		this.A = A;
 		this.X = x;
 	}
 	
+	
+   /**  Steepest Descent solution for
+    * 	Ax = b
+   @param A     Matrix (square, symmetric, and positive-definite/indefinite)
+   @param x		Vector (initial solution guess)
+   @param b		Vector
+   @return x 	Solution (five significant figures)
+   */
 	public Matrix SteepestDescentAlgorithm(){
-		//double [] val = new double []{};
-		int i = 0;
-		
+			
 		/*
 		 * an error tolerance, 0 <= epsilon < 1
-		 * as default, let epsilon equals to 0.5
+		 * as default, let epsilon equal to 0.0001
 		 */
-		double epsilon = 0.5;
+		double epsilon = 0.000001;
+		
+		double delta;
+		double delta0;
+		double alpha;
+		double e2d0;
+		
+		int i = 0;
 		
 		// r = b - A*x
-		R = B;
+		R = B.copy();
 		R.minusEquals(A.times(X));
-		
+			
 		// delta = r^T*r
 		delta = ((R.transpose()).times(R)).trace();
+		
 		delta0 = delta;
+		e2d0 = epsilon*epsilon*delta0;
 		
+		while (i < imax && delta > e2d0 ){
 		
-		while (i<imax && delta > epsilon*epsilon*delta ){
 			Q = A.times(R);
-			double alpha = delta/(((R.transpose()).times(Q)).trace());
 			
-			X.plusEquals(R.timesEquals(alpha));
+			alpha =  delta / ((R.transpose()).times(Q)).trace();
+					
+			X.plusEquals(R.times(alpha));
 			
-			/*
-			 * every square root of number of iterations, exact residual is computed
-			 * As default, set every 50 iterations
-			*/
+			System.out.println("delta = "+delta);
+			System.out.println("e2d0 = "+e2d0);
+			
+			//Check for exact residual computation or fast recursive formula.  As default, exact set every 50 iterations.
 			if (i%50 == 0){
-				R = B;
+				R = B.copy();
 				R.minusEquals(A.times(X));
-			} else {
-				R.minusEquals(Q.timesEquals(alpha));
+			} 
+			
+			else if (delta <= e2d0){
+				R = B.copy();
+				R.minusEquals(A.times(X));
+			}
+			else {
+				R.minusEquals(Q.times(alpha));
 			}
 			
 			delta = ((R.transpose()).times(R)).trace();
 			
 			i++;
+			
 		}
 		
-		System.out.println(X.det());
 		return X;
 	}
 
 	public static void main (String [] args){
 		
-	      double[][] valsA = {{1.,3.},{0.375,1.}};
-	      Matrix aValue = new Matrix(valsA);
+	      double[][] valsA = {{3.,2.},{2.,6.}};
+	      Matrix A = new Matrix(valsA);
 	      
-	      double[][] valsB = {{2.},{1}};
-	      Matrix bValue = new Matrix(valsB);
+	      double[][] valsB = {{2.},{-8.}};
+	      Matrix b = new Matrix(valsB);
 	      
-	      double[][] valsX = {{6.},{-1.}};
-	      Matrix xValue = new Matrix(valsX);
+	      double[][] valsX = {{-8.},{8.}};
+	      Matrix x = new Matrix(valsX);
 	      
-	      SteepestDescent steepestDescent = new SteepestDescent(10000, aValue, xValue, bValue);
-	      steepestDescent.SteepestDescentAlgorithm();
-	    //  System.out.println("xValue: " + xValue);
+	      A.print(10,3);
+	      b.print(10,3);
+	      x.print(10,3);
+	      
+	      SteepestDescent steepestDescent = new SteepestDescent(A, x, b, 150);
+	      
+	      x = steepestDescent.SteepestDescentAlgorithm();
+	      
+	      x.print(10, 4);
 		
 	}
 }
