51a58888a5ba66184522c01f01d537f66119dfc7
Ken Benjamin <kb56@buffalo.edu>
2007-07-27 23:28:35 +0000

    Interface that allows one to retrieve the polarization energy

---
+++ etomica/potential/PotentialPolarizable.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jul 27, 2007
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package etomica.potential;
+
+/**
+ * @author kenbenjamin
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface PotentialPolarizable {
+	
+	public double getPolarizationEnergy();
+
+}
