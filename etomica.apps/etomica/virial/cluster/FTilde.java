fda932e383c08307e6ad4653161134c7b5df7040
Andrew Schultz <ajs42@buffalo.edu>
2007-07-28 00:20:34 +0000

    fix polarizable bustage

--- etomica.apps/etomica/virial/cluster/FTilde.java
+++ etomica.apps/etomica/virial/cluster/FTilde.java
@@ -1,6 +1,7 @@
 package etomica.virial.cluster;
 
 import etomica.atom.AtomSet;
+import etomica.potential.IPotential;
 import etomica.virial.MayerFunction;
 
 
@@ -11,6 +12,9 @@ public class FTilde implements MayerFunction, java.io.Serializable {
 	}
 	public double f(AtomSet aPair, double beta) {
 		return fWrapped.f(aPair,beta) + 1.0;
+	}
+	public IPotential getPotential() {
+	    return fWrapped.getPotential();
 	}
 	public String toString() {return "f~  ";}
 }
