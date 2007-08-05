1f051c4f7a84cc1603db4a469c07722ec57ca365
David Kofke <kofke@buffalo.edu>
2007-08-05 16:54:16 +0000

    Add convolution method.

--- etomica.apps/etomica/virial/cluster/ClusterOperations.java
+++ etomica.apps/etomica/virial/cluster/ClusterOperations.java
@@ -3,13 +3,16 @@ package etomica.virial.cluster;
 import javax.swing.JPanel;
 
 import etomica.graphics.SimulationGraphic;
-//import etomica.virial.junk.MyApplet;
 
 public class ClusterOperations {
 
     private ClusterOperations() {
     }
     
+    /**
+     * Returns a new cluster that is the product of the given clusters.  Input clusters must have the
+     * same number of root points and my not both have bonds between the same root points.
+     */
     public static ClusterDiagram product(ClusterDiagram cluster1, ClusterDiagram cluster2) {
         int numRoot = cluster1.getNumRootPoints();
         if(numRoot != cluster2.getNumRootPoints()) {
@@ -23,8 +26,9 @@ public class ClusterOperations {
         cluster1.copyTo(productCluster);
 
         //copy cluster2 bonds to product
-        for (int node1 = 0; node1 < cluster2.mNumBody; node1++) {
-            int[] connections = cluster2.mConnections[node1];
+        for (int i = 0; i < cluster2.mNumBody; i++) {
+            int[] connections = cluster2.mConnections[i];
+            int node1 = i;
             if(node1 >= numRoot) node1 += offset;
             for(int j = 0; ; j++) {
                 int node2 = connections[j];
@@ -40,15 +44,62 @@ public class ClusterOperations {
         return productCluster; 
     }
     
+    public static ClusterDiagram convolution(ClusterDiagram cluster1, ClusterDiagram cluster2) {
+        int numRoot = cluster1.getNumRootPoints();
+        if(numRoot != 2 || numRoot != cluster2.getNumRootPoints()) {
+            throw new IllegalArgumentException("Convolution requires diagrams with exactly two root points each");
+        }
+        int numBody = cluster1.mNumBody + cluster2.mNumBody - 1;
+        ClusterDiagram convolutionCluster = new ClusterDiagram(numBody, numRoot, new int[][] {});
+        for(int i=0; i<cluster1.mNumBody; i++) {
+            int[] connections = cluster1.mConnections[i];
+            int node1 = i;
+            if(node1 == 0) {
+                node1 = cluster1.mNumBody;
+            }
+            for(int j=0; ; j++) {
+                int node2 = connections[j];
+                if(node2 == -1) break;
+                if(node2 == 0) {
+                    node2 = cluster1.mNumBody;
+                }
+                convolutionCluster.addConnection(node1, node2);
+            }
+        }
+        for(int i=0; i<cluster2.mNumBody; i++) {
+            int[] connections = cluster2.mConnections[i];
+            int node1 = i;
+            if(node1 != 0) {
+                node1 += cluster1.mNumBody - 1;
+            }
+            //if(node1 > 1) node1--;
+            for(int j=0; ; j++) {
+                int node2 = connections[j];
+                if(node2 == -1) break;
+                if(node2 != 0) {
+                    node2 += cluster1.mNumBody - 1;
+                }
+                //if(node2 > 1) node2--;
+                convolutionCluster.addConnection(node1, node2);
+            }
+        }
+        return convolutionCluster;
+    }
+    
     public static void main(String args[]) {
         ClusterDiagram cluster1 = new ClusterDiagram(5, 2, Standard.ring(5));
         cluster1.deleteConnection(0, 1);
         cluster1.deleteConnection(1, 0);
-        ClusterDiagram cluster2 = new ClusterDiagram(5,2);
-        cluster2 = new ClusterDiagram(5, 2, Standard.ring(5));
-        ClusterDiagram product = ClusterOperations.product(cluster1, cluster2);
-//        MyApplet applet = new MyApplet();
+        ClusterDiagram cluster2 = new ClusterDiagram(4,2);
+//        cluster2 = new ClusterDiagram(4, 2, Standard.ring(4));
+//        cluster2.addConnection(0, 2);
+//        cluster2.deleteConnection(0, 1);
+//        cluster2.deleteConnection(1, 0);
+        ClusterDiagram product = ClusterOperations.convolution(cluster2, cluster1);
+//        etomica.virial.junk.MyApplet applet = new etomica.virial.junk.MyApplet();
 //        applet.init();
+//        applet.starter.setDrawNumbersOfBlack(true);
+//        applet.starter.setDrawNumbersOfWhite(true);
 //        applet.starter.addCluster(cluster1);
 //        applet.starter.addCluster(cluster2);
 //        applet.starter.addCluster(product);
