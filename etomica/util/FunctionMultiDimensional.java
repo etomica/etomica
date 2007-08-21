3b3c3ae26f1f7f16eb9853b182713b93b52c670e
Tai Boon Tan <taitan@buffalo.edu>
2007-08-21 20:13:44 +0000

    FunctionMultiDimensional Interface

---
+++ etomica/util/FunctionMultiDimensional.java
@@ -0,0 +1,14 @@
+package etomica.util;
+
+
+/**
+ * Interface for multi-dimensional function
+ * 
+ * @author Tai Boon Tan
+ *
+ */
+public interface FunctionMultiDimensional {
+
+	public double function(double[] variables);
+
+}
