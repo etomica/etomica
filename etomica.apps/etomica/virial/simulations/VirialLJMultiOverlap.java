2327394da3ef32c8c55d4bb177ae4d12136ebf3e
Andrew Schultz <ajs42@buffalo.edu>
2007-09-07 20:11:13 +0000

    Adding main method for LJ mixtures

---
+++ etomica.apps/etomica/virial/simulations/VirialLJMultiOverlap.java
@@ -0,0 +1,190 @@
+package etomica.virial.simulations;
+
+import etomica.action.Action;
+import etomica.data.AccumulatorAverage;
+import etomica.data.AccumulatorRatioAverage;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataGroup;
+import etomica.potential.P2LennardJones;
+import etomica.potential.Potential2Spherical;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.util.ParameterBase;
+import etomica.util.ReadParameters;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerESpherical;
+import etomica.virial.MayerFunction;
+import etomica.virial.MayerGeneralSpherical;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesFactorySpheres;
+import etomica.virial.cluster.Standard;
+import etomica.virial.simulations.SimulationVirialOverlap;
+
+/**
+ * LJ simulation using Mayer sampling to evaluate cluster integrals
+ */
+public class VirialLJMultiOverlap {
+
+
+    public static void main(String[] args) {
+
+        VirialMixParam params = new VirialMixParam();
+        if (args.length > 0) {
+            ReadParameters readParameters = new ReadParameters(args[0], params);
+            readParameters.readParameters();
+        }
+        final int nPoints = params.nPoints;
+        double temperature = params.temperature;
+        long numSubSteps = params.numSubSteps;
+        double sigmaHSRef = params.sigmaHSRef;
+        int mixID = params.mixID;
+        int[] nTypes = params.nTypes;
+
+        // sanity-check nPoints
+        int sum = 0;
+        for (int i=0; i<nTypes.length; i++) {
+            sum += nTypes[i];
+        }
+        if (sum != nPoints) {
+            throw new RuntimeException("Number of each type needs to add up to nPoints");
+        }
+            
+        final double[] HSB = new double[9];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        HSB[8] = Standard.B8HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B2HS: "+HSB[2]);
+        System.out.println("B3HS: "+HSB[3]+" = "+(HSB[3]/(HSB[2]*HSB[2]))+" B2HS^2");
+        System.out.println("B4HS: "+HSB[4]+" = "+(HSB[4]/(HSB[2]*HSB[2]*HSB[2]))+" B2HS^3");
+        System.out.println("B5HS: "+HSB[5]+" = 0.110252 B2HS^4");
+        System.out.println("B6HS: "+HSB[6]+" = 0.03881 B2HS^5");
+        System.out.println("B7HS: "+HSB[7]+" = 0.013046 B2HS^6");
+        System.out.println("B8HS: "+HSB[8]+" = 0.004164 B2HS^7");
+        System.out.println("Lennard Jones overlap sampling B"+nPoints+" at T="+temperature);
+        System.out.println("Mixture type "+mixID);
+        System.out.print("Points of each species:");
+        for (int i=0; i<nTypes.length; i++) {
+            System.out.print(" "+nTypes[i]);
+        }
+        System.out.println();
+		
+        Space space = Space3D.getInstance();
+        
+        MayerHardSphere fRef = new MayerHardSphere(space,sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(space,sigmaHSRef);
+        double sigma11 = 1.0;
+        double sigma22 = 1.0;
+        double sigma12 = 1.0;
+        double epsilon11 = 1.0;
+        double epsilon22 = 1.0;
+        double epsilon12 = 1.0;
+        if (mixID == 1) {
+            epsilon12 = 0.75;
+        }
+        else if (mixID == 2) {
+            sigma12 = 0.885;
+            sigma22 = 0.769;
+        }
+        else if (mixID == 3) {
+            sigma12 = 0.884; // who smoked crack?
+            sigma22 = 0.768; // not me!
+            epsilon12 = 0.773;
+            epsilon22 = 0.597;
+        }
+        else if (mixID != 0) {
+            throw new RuntimeException("Don't know how to do mix "+mixID);
+        }
+        Potential2Spherical p11Target = new P2LennardJones(space, sigma11, epsilon11);
+        MayerGeneralSpherical f11Target = new MayerGeneralSpherical(space,p11Target);
+        Potential2Spherical p12Target = new P2LennardJones(space, sigma12, epsilon12);
+        MayerGeneralSpherical f12Target = new MayerGeneralSpherical(space,p12Target);
+        Potential2Spherical p22Target = new P2LennardJones(space, sigma22, epsilon22);
+        MayerGeneralSpherical f22Target = new MayerGeneralSpherical(space,p22Target);
+        MayerESpherical e11Target = new MayerESpherical(space,p11Target);
+        MayerESpherical e12Target = new MayerESpherical(space,p12Target);
+        MayerESpherical e22Target = new MayerESpherical(space,p22Target);
+        ClusterAbstract targetCluster = Standard.virialClusterMixture(nPoints, new MayerFunction[][]{{f11Target,f12Target},{f12Target,f22Target}},
+                                                                               new MayerFunction[][]{{e11Target,e12Target},{e12Target,e22Target}}, new int[]{nPoints-0,0});
+        targetCluster.setTemperature(temperature);
+        ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
+        refCluster.setTemperature(temperature);
+
+        System.out.println((numSubSteps*1000)+" steps ("+numSubSteps+" blocks of 1000)");
+		
+        final SimulationVirialOverlap sim = new SimulationVirialOverlap(space,new SpeciesFactorySpheres(), temperature, refCluster, targetCluster);
+        sim.integratorOS.setNumSubSteps(1000);
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+//        sim.setRefPref(1.0082398078547523);
+        sim.initRefPref(refFileName, numSubSteps/100);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, numSubSteps/40);
+        
+        System.out.println("equilibration finished");
+
+        Action progressReport = new Action() {
+            public void actionPerformed() {
+                System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                double ratio = sim.dsvo.getDataAsScalar();
+                double error = sim.dsvo.getError();
+                System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+            }
+        };
+        sim.integratorOS.addIntervalAction(progressReport);
+        sim.integratorOS.setActionInterval(progressReport, (int)(numSubSteps/10));
+        
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(numSubSteps);
+        for (int i=0; i<2; i++) {
+            System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize());
+        }
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
+        
+        double ratio = sim.dsvo.getDataAsScalar();
+        double error = sim.dsvo.getError();
+        System.out.println("ratio average: "+ratio+", error: "+error);
+        System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+        DataGroup allYourBase = (DataGroup)sim.accumulators[0].getData(sim.dsvo.minDiffLocation());
+        System.out.println("hard sphere ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("hard sphere   average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("hard sphere overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+        
+        allYourBase = (DataGroup)sim.accumulators[1].getData(sim.accumulators[1].getNBennetPoints()-sim.dsvo.minDiffLocation()-1);
+        System.out.println("lennard jones ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("lennard jones average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("lennard jones overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+    }
+    
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialMixParam extends ParameterBase {
+        public int nPoints = 3;
+        public double temperature = 1.0;
+        public long numSubSteps = 1000000;
+        public double sigmaHSRef = 1.5;
+        public int mixID = 0;
+        public int[] nTypes = new int[]{nPoints};
+    }
+}
+
