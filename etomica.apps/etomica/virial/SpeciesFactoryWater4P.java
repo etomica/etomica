8c5261709ead9248e46e8bfa59d79ab44ff3bf90
Andrew Schultz <ajs42@buffalo.edu>
2007-09-19 18:44:45 +0000

    Require caller to give us a Conformation

--- etomica.apps/etomica/virial/SpeciesFactoryWater4P.java
+++ etomica.apps/etomica/virial/SpeciesFactoryWater4P.java
@@ -1,5 +1,7 @@
 package etomica.virial;
 
+import etomica.atom.AtomTypeGroup;
+import etomica.config.Conformation;
 import etomica.models.water.SpeciesWater4P;
 import etomica.simulation.ISimulation;
 import etomica.species.Species;
@@ -9,7 +11,14 @@ import etomica.species.Species;
  * SpeciesFactory that makes SpeciesWater
  */
 public class SpeciesFactoryWater4P implements SpeciesFactory, java.io.Serializable {
-    public Species makeSpecies(ISimulation sim) {
-        return new SpeciesWater4P(sim);
+    public SpeciesFactoryWater4P(Conformation conformation) {
+        this.conformation = conformation;
     }
+    public Species makeSpecies(ISimulation sim) {
+        SpeciesWater4P species = new SpeciesWater4P(sim);
+        ((AtomTypeGroup)species.getMoleculeType()).setConformation(conformation);
+        return species;
+    }
+    
+    protected Conformation conformation;
 }
