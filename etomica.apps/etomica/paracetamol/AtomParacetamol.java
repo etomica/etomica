c44a689e13e47d01f2250a1d7771202de27cc5c3
Tai Boon Tan <taitan@buffalo.edu>
2007-10-18 07:04:14 +0000

    changed N to N1 changed the numbering of atoms

--- etomica.apps/etomica/paracetamol/AtomParacetamol.java
+++ etomica.apps/etomica/paracetamol/AtomParacetamol.java
@@ -10,7 +10,7 @@ public class AtomParacetamol extends AtomGroup{
 		super(atomType);
 	}
 	
-	public AtomLeaf O1, O2, C1, C2, C3, C4, C5, C6, C7, C8, N;
+	public AtomLeaf O1, O2, C1, C2, C3, C4, C5, C6, C7, C8, N1;
 	public AtomLeaf H1, H2, H3, H4, H5, H6, H7, H8, H9;
 	
 	public final static int indexC1 = 0;
@@ -20,12 +20,12 @@ public class AtomParacetamol extends AtomGroup{
 	public final static int indexH2 = 4;
 	public final static int indexC4 = 5;
 	public final static int indexO1 = 6;	
-	public final static int indexH5 = 7;
-	public final static int indexC5 = 8;
-	public final static int indexH3 = 9;
-	public final static int indexC6 =10;
-	public final static int indexH4 =11;
-	public final static int indexN  =12;
+	public final static int indexC5 = 7;
+	public final static int indexH3 = 8;
+	public final static int indexC6 = 9;
+	public final static int indexH4 =10;
+	public final static int indexH5 =11;
+	public final static int indexN1  =12;
 	public final static int indexH6 =13;
 	public final static int indexC7 =14;
 	public final static int indexO2 =15;
