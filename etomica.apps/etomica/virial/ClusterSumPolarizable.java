af2389aac897d4f79b123e7f76b3fbbe1261c482
Andrew Schultz <ajs42@buffalo.edu>
2007-10-25 04:47:46 +0000

    "45" should be "g45"  !! Apply delta truncation for any delta (not just D) Include mechanism (hack) to return value=0 for clusters with no atoms beyond some distance

--- etomica.apps/etomica/virial/ClusterSumPolarizable.java
+++ etomica.apps/etomica/virial/ClusterSumPolarizable.java
@@ -33,7 +33,7 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
     public ClusterAbstract makeCopy() {
         ClusterSumPolarizable copy = new ClusterSumPolarizable(clusters,clusterWeights,f);
         copy.setTemperature(1/beta);
-        copy.setDeltaDCut(Math.sqrt(deltaDCut2));
+        copy.setDeltaCut(Math.sqrt(deltaCut2));
         return copy;
     }
 
@@ -59,8 +59,19 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
         
         // recalculate all f values for all pairs
         PotentialPolarizable scfPotential = null;
+        // deltaD and deltaE run into precision problems for long distances
+        boolean truncateDelta = false;
+        boolean tooClose = pushR2 > 0;
         for(int i=0; i<nPoints-1; i++) {
             for(int j=i+1; j<nPoints; j++) {
+                double r2 = cPairs.getr2(i,j);
+                if (r2 > pushR2) {
+                    tooClose = false;
+                }
+                else if (r2 > deltaCut2) {
+                    truncateDelta = true;
+                    break;
+                }
                 for(int k=0; k<f.length; k++) {
                     fValues[i][j][k] = f[k].f(aPairs.getAPair(i,j),beta);
                     fValues[j][i][k] = fValues[i][j][k];
@@ -71,6 +82,9 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
         }
 
         value = 0.0;
+        if (tooClose) {
+            return value;
+        }
 
         for(int i=0; i<clusters.length; i++) {
             //System.out.println("clusters.length = " + clusters.length);
@@ -79,6 +93,10 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
             value += clusterWeights[i] * v;
             //System.out.println("value = " + value);
             //System.out.println("clusterWeights["+i+"] = " + clusterWeights[i]);
+        }
+        
+        if (truncateDelta) {
+            return value;
         }
         
         if (nPoints == 3) {
@@ -128,118 +146,107 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
         }
 
         else if (nPoints == 4) {
-            // deltaD runs into precision problems for long distances
-            boolean truncateLong = false;
-            for (int i=0; i<nPoints-1; i++) {
-                for (int j=i+1; j<nPoints; j++) {
-                    if (cPairs.getr2(i,j) > deltaDCut2) {
-                        truncateLong = true;
-                        break;
-                    }
+
+            double f12 = fValues[0][1][0];
+            double f13 = fValues[0][2][0];
+            double f14 = fValues[0][3][0];
+            double f23 = fValues[1][2][0];
+            double f24 = fValues[1][3][0];
+            double f34 = fValues[2][3][0];
+            double g12 = f12+1; //Math.exp(-beta*u12);
+            double g13 = f13+1; //Math.exp(-beta*u13);
+            double g14 = f14+1; //Math.exp(-beta*u14);
+            double g23 = f23+1; //Math.exp(-beta*u23);
+            double g24 = f24+1; //Math.exp(-beta*u24);
+            double g34 = f34+1; //Math.exp(-beta*u34);
+
+            scfAtoms.clear();
+            // we need to properly construct these lists even if we don't use them
+            // (due to overlaps) because the next list is obtained by removing/adding
+            // atoms from this one.
+            scfAtoms.add(box.molecule(0));
+            scfAtoms.add(box.molecule(1));
+            scfAtoms.add(box.molecule(2));
+
+            double deltaD = 0;
+            // if 12 13 or 23 is overlapped, then we can't calculate u123Pol and
+            // couldn't calculate the uijPol.  Fortunately, gij is 0, so the 123
+            // term is 0.
+            if (g12*g13*g23 != 0) {
+                double u123Pol = scfPotential.getPolarizationEnergy(scfAtoms);
+                double deltaU123 = u123Pol - (uijPol[0][1] + uijPol[0][2] + uijPol[1][2]);
+                double beta123 = beta*deltaU123;
+                // for small x, exp(-x)-1 ~= -x
+                // for x < 1E-8, the approximation is value within machine precision
+                // for x < 1E-15, exp(-x) is 1, so the approximation is more accurate
+                //   than simply doing the math.
+                double exp123 = -beta123;
+                if (Math.abs(beta123) > 1E-8) {
+                    exp123 = Math.exp(-beta123) - 1;
                 }
+                // original formula has g14+g24+g34-2 instead of f14+f24+f34+1.
+                // Using f should have better precision since
+                // the g's will all be close to 1 (or even equal to 1) for 
+                // systems with molecules having large separations.
+                deltaD += -exp123*g12*g13*g23*((f14+f24+f34)+1);
             }
 
-            if (!truncateLong) {
-                double f12 = fValues[0][1][0];
-                double f13 = fValues[0][2][0];
-                double f14 = fValues[0][3][0];
-                double f23 = fValues[1][2][0];
-                double f24 = fValues[1][3][0];
-                double f34 = fValues[2][3][0];
-                double g12 = f12+1; //Math.exp(-beta*u12);
-                double g13 = f13+1; //Math.exp(-beta*u13);
-                double g14 = f14+1; //Math.exp(-beta*u14);
-                double g23 = f23+1; //Math.exp(-beta*u23);
-                double g24 = f24+1; //Math.exp(-beta*u24);
-                double g34 = f34+1; //Math.exp(-beta*u34);
-
-                scfAtoms.clear();
-                // we need to properly construct these lists even if we don't use them
-                // (due to overlaps) because the next list is obtained by removing/adding
-                // atoms from this one.
-                scfAtoms.add(box.molecule(0));
-                scfAtoms.add(box.molecule(1));
-                scfAtoms.add(box.molecule(2));
-
-                double deltaD = 0;
-                // if 12 13 or 23 is overlapped, then we can't calculate u123Pol and
-                // couldn't calculate the uijPol.  Fortunately, gij is 0, so the 123
-                // term is 0.
-                if (g12*g13*g23 != 0) {
-                    double u123Pol = scfPotential.getPolarizationEnergy(scfAtoms);
-                    double deltaU123 = u123Pol - (uijPol[0][1] + uijPol[0][2] + uijPol[1][2]);
-                    double beta123 = beta*deltaU123;
-                    // for small x, exp(-x)-1 ~= -x
-                    // for x < 1E-8, the approximation is value within machine precision
-                    // for x < 1E-15, exp(-x) is 1, so the approximation is more accurate
-                    //   than simply doing the math.
-                    double exp123 = -beta123;
-                    if (Math.abs(beta123) > 1E-8) {
-                        exp123 = Math.exp(-beta123) - 1;
-                    }
-                    // original formula has g14+g24+g34-2 instead of f14+f24+f34+1.
-                    // Using f should have better precision since
-                    // the g's will all be close to 1 (or even equal to 1) for 
-                    // systems with molecules having large separations.
-                    deltaD += -exp123*g12*g13*g23*((f14+f24+f34)+1);
+            scfAtoms.remove(2);
+            scfAtoms.add(box.molecule(3));
+            if (g12*g14*g24 != 0) {
+                double u124Pol = scfPotential.getPolarizationEnergy(scfAtoms);
+                double deltaU124 = u124Pol-(uijPol[0][1]+uijPol[0][3]+uijPol[1][3]);
+                double beta124 = beta*deltaU124;
+                double exp124 = -beta124;
+                if (Math.abs(beta124) > 1E-8) {
+                    exp124 = Math.exp(-beta124) - 1;
                 }
+                deltaD +=  -exp124*g12*g14*g24*((f13+f23+f34)+1);
+            }
 
-                scfAtoms.remove(2);
-                scfAtoms.add(box.molecule(3));
-                if (g12*g14*g24 != 0) {
-                    double u124Pol = scfPotential.getPolarizationEnergy(scfAtoms);
-                    double deltaU124 = u124Pol-(uijPol[0][1]+uijPol[0][3]+uijPol[1][3]);
-                    double beta124 = beta*deltaU124;
-                    double exp124 = -beta124;
-                    if (Math.abs(beta124) > 1E-8) {
-                        exp124 = Math.exp(-beta124) - 1;
-                    }
-                    deltaD +=  -exp124*g12*g14*g24*((f13+f23+f34)+1);
+            scfAtoms.remove(1);
+            scfAtoms.add(box.molecule(2));
+            if (g13*g14*g34 != 0) {
+                double u134Pol = scfPotential.getPolarizationEnergy(scfAtoms);
+                double deltaU134 = u134Pol-(uijPol[0][2]+uijPol[0][3]+uijPol[2][3]);
+                double beta134 = beta*deltaU134;
+                double exp134 = -beta134;
+                if (Math.abs(beta134) > 1E-8) {
+                    exp134 = Math.exp(-beta134) - 1;
                 }
+                deltaD += -exp134*g13*g14*g34*((f12+f23+f24)+1);
+            }
 
-                scfAtoms.remove(1);
-                scfAtoms.add(box.molecule(2));
-                if (g13*g14*g34 != 0) {
-                    double u134Pol = scfPotential.getPolarizationEnergy(scfAtoms);
-                    double deltaU134 = u134Pol-(uijPol[0][2]+uijPol[0][3]+uijPol[2][3]);
-                    double beta134 = beta*deltaU134;
-                    double exp134 = -beta134;
-                    if (Math.abs(beta134) > 1E-8) {
-                        exp134 = Math.exp(-beta134) - 1;
-                    }
-                    deltaD += -exp134*g13*g14*g34*((f12+f23+f24)+1);
+            scfAtoms.remove(0);
+            scfAtoms.add(box.molecule(1));
+            if (g23*g24*g34 != 0) {
+                double u234Pol = scfPotential.getPolarizationEnergy(scfAtoms);
+                double deltaU234 = u234Pol-(uijPol[1][2]+uijPol[1][3]+uijPol[2][3]);
+                double beta234 = beta*deltaU234;
+                double exp234 = -beta234;
+                if (Math.abs(beta234) > 1E-8) {
+                    exp234 = Math.exp(-beta234) - 1;
                 }
+                deltaD += -exp234*g23*g24*g34*((f12+f13+f14)+1);
+            }
 
-                scfAtoms.remove(0);
-                scfAtoms.add(box.molecule(1));
-                if (g23*g24*g34 != 0) {
-                    double u234Pol = scfPotential.getPolarizationEnergy(scfAtoms);
-                    double deltaU234 = u234Pol-(uijPol[1][2]+uijPol[1][3]+uijPol[2][3]);
-                    double beta234 = beta*deltaU234;
-                    double exp234 = -beta234;
-                    if (Math.abs(beta234) > 1E-8) {
-                        exp234 = Math.exp(-beta234) - 1;
-                    }
-                    deltaD += -exp234*g23*g24*g34*((f12+f13+f14)+1);
+            scfAtoms.add(box.molecule(0));
+            if (g12*g13*g14*g23*g24*g34 != 0) {
+                double u1234Pol = scfPotential.getPolarizationEnergy(scfAtoms);
+                // deltaU1234 would have deltaUabc subtracted off, but we'd also add it back
+                // in for expU1234, so just don't subtract in the first place 
+                double deltaU1234 = u1234Pol-(uijPol[0][1]+uijPol[0][2]+uijPol[0][3]+uijPol[1][2]+uijPol[1][3]+uijPol[2][3]); //-deltaU123-deltaU124-deltaU134-deltaU234;
+                double beta1234 = beta*deltaU1234; //deltaU123+deltaU124+deltaU134+deltaU234+deltaU1234);
+                double exp1234 = -beta1234;
+                if (Math.abs(beta1234) > 1E-8) {
+                    exp1234 = Math.exp(-beta1234) - 1;
                 }
+                deltaD += exp1234*g12*g13*g14*g23*g24*g34;
+            }
 
-                scfAtoms.add(box.molecule(0));
-                if (g12*g13*g14*g23*g24*g34 != 0) {
-                    double u1234Pol = scfPotential.getPolarizationEnergy(scfAtoms);
-                    // deltaU1234 would have deltaUabc subtracted off, but we'd also add it back
-                    // in for expU1234, so just don't subtract in the first place 
-                    double deltaU1234 = u1234Pol-(uijPol[0][1]+uijPol[0][2]+uijPol[0][3]+uijPol[1][2]+uijPol[1][3]+uijPol[2][3]); //-deltaU123-deltaU124-deltaU134-deltaU234;
-                    double beta1234 = beta*deltaU1234; //deltaU123+deltaU124+deltaU134+deltaU234+deltaU1234);
-                    double exp1234 = -beta1234;
-                    if (Math.abs(beta1234) > 1E-8) {
-                        exp1234 = Math.exp(-beta1234) - 1;
-                    }
-                    deltaD += exp1234*g12*g13*g14*g23*g24*g34;
-                }
+            // Mason and Spurling book deltaD; 5/11/07
 
-                // Mason and Spurling book deltaD; 5/11/07
-
-                // deltaU1234 = u1234-u12-u13-u14-u23-u24-u34;
+            // deltaU1234 = u1234-u12-u13-u14-u23-u24-u34;
 
 //          deltaD = 2*Math.exp(-beta*(u12+u13+u23))*(Math.exp(-beta*deltaU123)-1) + 2*Math.exp(-beta*(u12+u14+u24))*(Math.exp(-beta*deltaU124)-1)
 //                  + 2*Math.exp(-beta*(u13+u14+u34))*(Math.exp(-beta*deltaU134)-1) + 2*Math.exp(-beta*(u23+u24+u34))*(Math.exp(-beta*deltaU234)-1)
@@ -253,14 +260,13 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
 
 
 //                       kmb added this line 8/16/06
-                // deltaD has to be multiplied by weightPrefactor from Standard class, just like deltaC was multiplied by
-                // clusterWeights above to get value; note, for B3 clusterWeights = weightPrefactor
+            // deltaD has to be multiplied by weightPrefactor from Standard class, just like deltaC was multiplied by
+            // clusterWeights above to get value; note, for B3 clusterWeights = weightPrefactor
 
-                // -(1/8) is the B4 prefactor multiplying all diagrams.
-                // coefficient is -(1-4)/4! = -1/8
-                deltaD = -0.125*deltaD;
-                value += deltaD;
-            }
+            // -(1/8) is the B4 prefactor multiplying all diagrams.
+            // coefficient is -(1-4)/4! = -1/8
+            deltaD = -0.125*deltaD;
+            value += deltaD;
         }
         else if (nPoints == 5) {
             final double f12 = fValues[0][1][0];
@@ -554,7 +560,7 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
             scfAtoms.remove(0);
             scfAtoms.add(box.molecule(3));  // 354
             
-            if (g34*g35*45 != 0) {
+            if (g34*g35*g45 != 0) {
                 double u345Pol = scfPotential.getPolarizationEnergy(scfAtoms);
                 double deltaU345 = u345Pol-uijPol[2][3]-uijPol[2][4]-uijPol[3][4];
                 beta345 = deltaU345*beta;
@@ -689,12 +695,12 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
         beta = 1/temperature;
     }
     
-    public void setDeltaDCut(double newDeltaDCut) {
-        deltaDCut2 = newDeltaDCut*newDeltaDCut;
+    public void setDeltaCut(double newDeltaDCut) {
+        deltaCut2 = newDeltaDCut*newDeltaDCut;
     }
     
-    public double getDeltaDCut() {
-        return Math.sqrt(deltaDCut2);
+    public double getDeltaCut() {
+        return Math.sqrt(deltaCut2);
     }
     
     private static final long serialVersionUID = 1L;
@@ -706,6 +712,7 @@ public class ClusterSumPolarizable implements ClusterAbstract, java.io.Serializa
     private double value, lastValue;
     private double beta;
     protected final AtomArrayList scfAtoms;
-    protected double deltaDCut2 = Double.POSITIVE_INFINITY;
+    protected double deltaCut2 = Double.POSITIVE_INFINITY;
     protected final double[][] uijPol;
+    public double pushR2 = 0;
 }
