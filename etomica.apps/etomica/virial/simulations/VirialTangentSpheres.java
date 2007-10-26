4cb0125b9fa52d8cd04c66e87c92a59035b5de06
Andrew Schultz <ajs42@buffalo.edu>
2007-10-26 03:16:47 +0000

    Add virial simulation of tangent spheres

---
+++ etomica.apps/etomica/virial/simulations/VirialTangentSpheres.java
@@ -0,0 +1,144 @@
+package etomica.virial.simulations;
+
+import etomica.atom.iterator.ApiBuilder;
+import etomica.atom.iterator.ApiIntergroup;
+import etomica.config.ConformationLinear;
+import etomica.data.AccumulatorAverage;
+import etomica.data.AccumulatorRatioAverage;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataGroup;
+import etomica.potential.P2HardSphere;
+import etomica.potential.P2SquareWell;
+import etomica.potential.Potential2;
+import etomica.potential.PotentialGroup;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.Species;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.MayerEGeneral;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesFactoryTangentSpheres;
+import etomica.virial.cluster.Standard;
+
+/**
+ * Generic simulation using Mayer sampling to evaluate cluster integrals
+ */
+public class VirialTangentSpheres {
+
+
+    public static void main(String[] args) {
+        int nPoints = 3;
+        int nSpheres = 2;
+        double sigmaHSRef = Math.pow(nSpheres,1.0/3.0);
+        double temperature = 1;
+        long steps = 2000l;
+        if (args.length > 0) nPoints = Integer.parseInt(args[0]);
+        if (args.length > 1) nSpheres = Integer.parseInt(args[1]);
+        if (args.length > 2) temperature = Double.parseDouble(args[2]);
+        if (args.length > 3) steps = Long.parseLong(args[3]);
+            
+        if (temperature > 0) {
+            sigmaHSRef += 0.5;
+        }
+        if (args.length > 4) sigmaHSRef = Double.parseDouble(args[4]);
+        double[] HSB = new double[8];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B2HS: "+HSB[2]);
+        System.out.println("B3HS: "+HSB[3]+" = "+(HSB[3]/(HSB[2]*HSB[2]))+" B2HS^2");
+        System.out.println("B4HS: "+HSB[4]+" = "+(HSB[4]/(HSB[2]*HSB[2]*HSB[2]))+" B2HS^3");
+        System.out.println("B5HS: "+HSB[5]+" = 0.110252 B2HS^4");
+        System.out.println("B6HS: "+HSB[6]+" = 0.03881 B2HS^5");
+        System.out.println("B7HS: "+HSB[7]+" = 0.013046 B2HS^6");
+        System.out.println(nSpheres+"-mer Overlap sampling B"+nPoints+(temperature>0 ? " at "+temperature : " (HS)"));
+		
+        Space space = Space3D.getInstance();
+        
+        MayerHardSphere fRef = new MayerHardSphere(space,sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(space,sigmaHSRef);
+        PotentialGroup pTargetGroup = new PotentialGroup(2, space);
+        Potential2 p2 = null;
+        if (temperature == 0) {
+            p2 = new P2HardSphere(space, 1.0, false);
+            temperature = 1.0;
+        }
+        else {
+            p2 = new P2SquareWell(space, 1.0, 1.5, 1.0, false);
+        }
+        pTargetGroup.addPotential(p2, new ApiIntergroup());
+        MayerGeneral fTarget = new MayerGeneral(pTargetGroup);
+        MayerEGeneral eTarget = new MayerEGeneral(pTargetGroup);
+        ClusterAbstract targetCluster = Standard.virialCluster(nPoints, fTarget, nPoints>3, eTarget, true);
+        targetCluster.setTemperature(temperature);
+        
+        ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
+        refCluster.setTemperature(temperature);
+
+        System.out.println((steps*1000)+" steps ("+steps+" blocks of 1000)");
+        SimulationVirialOverlap sim = new SimulationVirialOverlap(space,new SpeciesFactoryTangentSpheres(nSpheres, new ConformationLinear(space,1.0)), temperature,refCluster,targetCluster);
+        sim.integratorOS.setNumSubSteps(1000);
+        
+        if (nSpheres > 2) {
+            PotentialGroup pIntra = sim.integrators[1].getPotential().makePotentialGroup(1);
+            pIntra.addPotential(p2,ApiBuilder.makeNonAdjacentPairIterator());
+            sim.integrators[1].getPotential().addPotential(pIntra,new Species[]{sim.species});
+        }
+        
+        if (p2 instanceof P2HardSphere) {
+            temperature = 0;
+        }
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+        sim.initRefPref(refFileName, steps/40);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, steps/20);
+        
+        System.out.println("equilibration finished");
+        System.out.println("MC Move step sizes (ref)    "+sim.mcMoveTranslate[0].getStepSize()+" "
+                +sim.mcMoveRotate[0].getStepSize()+" "
+                +(sim.mcMoveWiggle[0]==null ? "" : (""+sim.mcMoveWiggle[0].getStepSize())));
+        System.out.println("MC Move step sizes (target) "+sim.mcMoveTranslate[1].getStepSize()+" "
+                +sim.mcMoveRotate[1].getStepSize()+" "
+                +(sim.mcMoveWiggle[1]==null ? "" : (""+sim.mcMoveWiggle[1].getStepSize())));
+
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(steps);
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
+        
+        double ratio = sim.dsvo.getDataAsScalar();
+        double error = sim.dsvo.getError();
+        System.out.println("ratio average: "+ratio+", error: "+error);
+        System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+        DataGroup allYourBase = (DataGroup)sim.accumulators[0].getData(sim.dsvo.minDiffLocation());
+        System.out.println("hard sphere ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("hard sphere   average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("hard sphere overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+        
+        allYourBase = (DataGroup)sim.accumulators[1].getData(sim.accumulators[1].getNBennetPoints()-sim.dsvo.minDiffLocation()-1);
+        System.out.println("chain ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("chain average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("chain overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+	}
+}
+
