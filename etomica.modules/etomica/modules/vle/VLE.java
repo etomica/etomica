dbf0ac8c15acf09f5518d247fee1a5aadc2defd6
Andrew Schultz <ajs42@buffalo.edu>
2007-11-19 00:26:29 +0000

    Changes to handle CO2.  Add sliders to VLE.

--- etomica.modules/etomica/modules/vle/VLE.java
+++ etomica.modules/etomica/modules/vle/VLE.java
@@ -1,5 +1,6 @@
 package etomica.modules.vle;
 
+import etomica.action.Action;
 import etomica.data.AccumulatorAverageCollapsing;
 import etomica.data.AccumulatorHistory;
 import etomica.data.DataFork;
@@ -9,22 +10,82 @@ import etomica.data.DataTag;
 import etomica.data.meter.MeterDensity;
 import etomica.data.meter.MeterNMolecules;
 import etomica.data.meter.MeterPressure;
+import etomica.graphics.DeviceSlider;
 import etomica.graphics.DisplayPlot;
 import etomica.graphics.DisplayTextBoxesCAE;
 import etomica.graphics.SimulationGraphic;
+import etomica.modifier.ModifierGeneral;
+import etomica.units.Debye;
+import etomica.units.Kelvin;
+import etomica.units.Liter;
+import etomica.units.Mole;
+import etomica.units.Pascal;
 import etomica.units.Pixel;
+import etomica.units.Prefix;
+import etomica.units.PrefixedUnit;
+import etomica.units.UnitRatio;
 import etomica.util.HistoryCollapsingAverage;
 
 public class VLE extends SimulationGraphic {
 
     private final static String APP_NAME = "Virial / VLE";
-    private final static int REPAINT_INTERVAL = 50;
+    private final static int REPAINT_INTERVAL = 200;
 
-    public VLE(VLESim sim) {
+    public VLE(final VLESim sim) {
         super(sim, TABBED_PANE, APP_NAME, REPAINT_INTERVAL);
+
+        getDisplayBox(sim.boxLiquid).setPixelUnit(new Pixel(8));
+        getDisplayBox(sim.boxVapor).setPixelUnit(new Pixel(8));
+
+        DeviceThermoSliderGEMC thermoSlider = new DeviceThermoSliderGEMC(sim.getController());
+        thermoSlider.setUnit(Kelvin.UNIT);
+        thermoSlider.setIntegrators(sim.integratorLiquid, sim.integratorVapor);
+        thermoSlider.setIsothermal();
+        thermoSlider.setMinimum(200);
+        thermoSlider.setMaximum(400);
+        thermoSlider.setIsothermalButtonsVisibility(false);
+        add(thermoSlider);
         
-        getDisplayBox(sim.boxLiquid).setPixelUnit(new Pixel(15));
-        getDisplayBox(sim.boxVapor).setPixelUnit(new Pixel(15));
+        DeviceSlider sigmaSlider = new DeviceSlider(sim.getController(), new ModifierGeneral(sim, "sigma"));
+        sigmaSlider.setPrecision(2);
+        sigmaSlider.setMinimum(2);
+        sigmaSlider.setMaximum(5);
+        sigmaSlider.setShowValues(true);
+        sigmaSlider.setEditValues(true);
+        sigmaSlider.setShowBorder(true);
+        sigmaSlider.setLabel("sigma (A)");
+        add(sigmaSlider);
+        sigmaSlider.doUpdate();
+        sigmaSlider.setPostAction(new Action() {
+            public void actionPerformed() {
+                getPaintAction(sim.boxLiquid).actionPerformed();
+                getPaintAction(sim.boxVapor).actionPerformed();
+            }
+        });
+        
+        DeviceSlider epsilonSlider = new DeviceSlider(sim.getController(), new ModifierGeneral(sim, "epsilon"));
+        epsilonSlider.setUnit(Kelvin.UNIT);
+        epsilonSlider.setPrecision(1);
+        epsilonSlider.setMinimum(100);
+        epsilonSlider.setMaximum(250);
+        epsilonSlider.setShowValues(true);
+        epsilonSlider.setEditValues(true);
+        epsilonSlider.setShowBorder(true);
+        epsilonSlider.setLabel("epsilon (K)");
+        add(epsilonSlider);
+        epsilonSlider.doUpdate();
+
+        DeviceSlider momentSlider = new DeviceSlider(sim.getController(), new ModifierGeneral(sim, "moment"));
+        momentSlider.setUnit(Debye.UNIT);
+        momentSlider.setPrecision(2);
+        momentSlider.setMinimum(3);
+        momentSlider.setMaximum(6);
+        momentSlider.setShowValues(true);
+        momentSlider.setEditValues(true);
+        momentSlider.setShowBorder(true);
+        momentSlider.setLabel("moment (Debye)");
+        momentSlider.doUpdate();
+        add(momentSlider);
 
         MeterDensity meterDensityLiquid = new MeterDensity(sim.getSpace());
         meterDensityLiquid.setBox(sim.boxLiquid);
@@ -64,13 +125,16 @@ public class VLE extends SimulationGraphic {
 //        histogramLiquidDensity.addDataSink(liquidDensityHistogramPlot.getDataSet().makeDataSink());
 //        histogramLiquidDensity.setPushInterval(100);
         DisplayTextBoxesCAE liquidDensityDisplay = new DisplayTextBoxesCAE();
+        liquidDensityDisplay.setUnit(new UnitRatio(Mole.UNIT, Liter.UNIT));
         liquidDensityDisplay.setAccumulator(avgLiquidDensity);
-        liquidDensityDisplay.setLabel("Liquid Density");
+        liquidDensityDisplay.setLabel("Liquid Density (mol/L)");
         add(liquidDensityDisplay);
         DisplayPlot liquidDensityHistoryPlot = new DisplayPlot();
+        liquidDensityHistoryPlot.setUnit(new UnitRatio(Mole.UNIT, Liter.UNIT));
         liquidDensityHistoryPlot.setLabel("Liquid Density");
         add(liquidDensityHistoryPlot);
         historyDensityLiquid.addDataSink(liquidDensityHistoryPlot.getDataSet().makeDataSink());
+        liquidDensityHistoryPlot.setLegend(new DataTag[]{historyDensityLiquid.getTag()}, "Density (mol/L)");
         historyDensityLiquid.setPushInterval(1);
 
 //        DisplayPlot vaporDensityHistogramPlot = new DisplayPlot();
@@ -79,14 +143,23 @@ public class VLE extends SimulationGraphic {
 //        histogramVaporDensity.addDataSink(vaporDensityHistogramPlot.getDataSet().makeDataSink());
 //        histogramVaporDensity.setPushInterval(100);
         DisplayTextBoxesCAE vaporDensityDisplay = new DisplayTextBoxesCAE();
+        vaporDensityDisplay.setUnit(new UnitRatio(Mole.UNIT, Liter.UNIT));
         vaporDensityDisplay.setAccumulator(avgVaporDensity);
-        vaporDensityDisplay.setLabel("Vapor Density");
+        vaporDensityDisplay.setLabel("Vapor Density (mol/L)");
         add(vaporDensityDisplay);
         DisplayPlot vaporDensityHistoryPlot = new DisplayPlot();
+        vaporDensityHistoryPlot.setUnit(new UnitRatio(Mole.UNIT, Liter.UNIT));
+        vaporDensityHistoryPlot.setLegend(new DataTag[]{historyDensityVapor.getTag()}, "Density (mol/L)");
         vaporDensityHistoryPlot.setLabel("Vapor Density");
         add(vaporDensityHistoryPlot);
         historyDensityVapor.addDataSink(vaporDensityHistoryPlot.getDataSet().makeDataSink());
         historyDensityVapor.setPushInterval(1);
+//      historyDensityLiquid.addDataSink(densityHistoryPlot.getDataSet().makeDataSink());
+//      historyDensityLiquid.setPushInterval(1);
+//      historyDensityVapor.addDataSink(densityHistoryPlot.getDataSet().makeDataSink());
+//      historyDensityVapor.setPushInterval(1);
+//      densityHistoryPlot.setLegend(new DataTag[]{meterDensityLiquid.getTag()}, "Liquid");
+//      densityHistoryPlot.setLegend(new DataTag[]{meterDensityVapor.getTag()}, "Vapor");
         
         MeterNMolecules meterNMoleculesLiquid = new MeterNMolecules();
         meterNMoleculesLiquid.setBox(sim.boxLiquid);
@@ -143,22 +216,25 @@ public class VLE extends SimulationGraphic {
         
         DisplayPlot plotHistoryPressure = new DisplayPlot();
         plotHistoryPressure.setLabel("Pressure History");
+        plotHistoryPressure.setUnit(new PrefixedUnit(Prefix.MEGA, Pascal.UNIT));
 
         DisplayTextBoxesCAE displayPressureLiquid = new DisplayTextBoxesCAE();
-        displayPressureLiquid.setLabel("Liquid pressure");
+        displayPressureLiquid.setUnit(new PrefixedUnit(Prefix.MEGA, Pascal.UNIT));
+        displayPressureLiquid.setLabel("Liquid pressure (MPa)");
         displayPressureLiquid.setAccumulator(avgPressureLiquid);
         add(displayPressureLiquid);
         historyPressureLiquid.addDataSink(plotHistoryPressure.getDataSet().makeDataSink());
-        plotHistoryPressure.setLegend(new DataTag[]{meterPressureLiquid.getTag()}, "Liquid");
+        plotHistoryPressure.setLegend(new DataTag[]{meterPressureLiquid.getTag()}, "Liquid (MPa)");
         sim.integratorLiquid.addIntervalAction(pumpPressureLiquid);
         sim.integratorLiquid.setActionInterval(pumpPressureLiquid, 500);
         
         DisplayTextBoxesCAE displayPressureVapor = new DisplayTextBoxesCAE();
-        displayPressureVapor.setLabel("Vapor pressure");
+        displayPressureVapor.setUnit(new PrefixedUnit(Prefix.MEGA, Pascal.UNIT));
+        displayPressureVapor.setLabel("Vapor pressure (MPa)");
         displayPressureVapor.setAccumulator(avgPressureVapor);
         add(displayPressureVapor);
         historyPressureVapor.addDataSink(plotHistoryPressure.getDataSet().makeDataSink());
-        plotHistoryPressure.setLegend(new DataTag[]{meterPressureVapor.getTag()}, "Vapor");
+        plotHistoryPressure.setLegend(new DataTag[]{meterPressureVapor.getTag()}, "Vapor (MPa)");
         add(plotHistoryPressure);
         sim.integratorVapor.addIntervalAction(pumpPressureVapor);
         sim.integratorVapor.setActionInterval(pumpPressureVapor, 500);
--- etomica.modules/etomica/modules/vle/VLESim.java
+++ etomica.modules/etomica/modules/vle/VLESim.java
@@ -1,51 +1,57 @@
 package etomica.modules.vle;
 
+import etomica.action.BoxImposePbc;
 import etomica.action.activity.ActivityIntegrate;
+import etomica.atom.AtomType;
+import etomica.atom.AtomTypeSphere;
 import etomica.box.Box;
 import etomica.config.Configuration;
 import etomica.config.ConfigurationLattice;
 import etomica.integrator.IntegratorMC;
 import etomica.integrator.IntegratorManagerMC;
 import etomica.integrator.mcmove.MCMoveAtom;
-import etomica.integrator.mcmove.MCMoveStepTracker;
+import etomica.integrator.mcmove.MCMoveRotate;
 import etomica.lattice.LatticeCubicFcc;
-import etomica.nbr.cell.PotentialMasterCell;
-import etomica.potential.IPotential;
-import etomica.potential.P2LennardJones;
-import etomica.potential.P2SoftSphericalTruncated;
+import etomica.potential.P2LJQ;
+import etomica.potential.P2SoftTruncated;
+import etomica.potential.PotentialMaster;
 import etomica.simulation.Simulation;
 import etomica.space.BoundaryRectangularPeriodic;
 import etomica.space3d.Space3D;
-import etomica.species.Species;
-import etomica.species.SpeciesSpheresMono;
+import etomica.species.SpeciesSpheresRotating;
+import etomica.units.Debye;
+import etomica.units.Kelvin;
 
 public class VLESim extends Simulation {
 
     public final Box boxLiquid, boxVapor;
-    public final Species species;
+    public final SpeciesSpheresRotating species;
     public final IntegratorMC integratorLiquid, integratorVapor;
     public final IntegratorManagerMC integratorGEMC;
-    public final PotentialMasterCell potentialMaster;
     public final ActivityIntegrate activityIntegrate;
-    public final IPotential potential;
     protected double sigma;
     protected double temperature;
     protected double epsilon;
+    protected double moment;
     protected double density;
+    protected final P2LJQ p2LJQ;
+    protected final P2SoftTruncated p2Truncated;
     
     public VLESim() {
         super(Space3D.getInstance());
-        int initNumMolecules = 1000;
-        sigma = 1.0;
-        temperature = 1.0;
-        epsilon = 1.0;
-        density = 0.3;
-        double cutoff = 4*sigma;
+        int initNumMolecules = 300;
+        sigma = 3.82;
+        temperature = Kelvin.UNIT.toSim(273.15);
+        epsilon = Kelvin.UNIT.toSim(182.9);
+        moment = Debye.UNIT.toSim(5.1);
+        moment *= moment;
+        density = 0.004;
 
         double initBoxSize = Math.pow(initNumMolecules/density, (1.0/3.0));
         
-        species = new SpeciesSpheresMono(this);
+        species = new SpeciesSpheresRotating(this);
         getSpeciesManager().addSpecies(species);
+        ((AtomTypeSphere)species.getLeafType()).setDiameter(sigma);
 
         boxLiquid = new Box(new BoundaryRectangularPeriodic(space, random, initBoxSize));
         addBox(boxLiquid);
@@ -57,22 +63,34 @@ public class VLESim extends Simulation {
         config.initializeCoordinates(boxLiquid);
         config.initializeCoordinates(boxVapor);
         
-        potentialMaster = new PotentialMasterCell(this, cutoff);
-        P2LennardJones p2LJ = new P2LennardJones(space, sigma, epsilon);
-        potential = new P2SoftSphericalTruncated(p2LJ, cutoff);
-        potentialMaster.addPotential(potential, new Species[]{species, species});
-        potentialMaster.setCellRange(3);
+        PotentialMaster potentialMaster = new PotentialMaster(space);
+        p2LJQ = new P2LJQ(space, sigma, epsilon, moment);
+        p2LJQ.setTemperature(temperature);
+        p2Truncated = new P2SoftTruncated(p2LJQ, 4.0*sigma);
+//        ((P2SoftSphericalTruncatedBox)potential).setTruncationFactor(0.35);
+        potentialMaster.addPotential(p2Truncated, new AtomType[]{species.getLeafType(), species.getLeafType()});
         
         integratorLiquid = new IntegratorMC(potentialMaster, random, temperature);
         integratorLiquid.setBox(boxLiquid);
         MCMoveAtom atomMove = new MCMoveAtom(potentialMaster, random, 0.5, 5.0, true);
         integratorLiquid.getMoveManager().addMCMove(atomMove);
-        ((MCMoveStepTracker)atomMove.getTracker()).setNoisyAdjustment(true);
+        MCMoveRotate rotateMove = new MCMoveRotate(potentialMaster, random);
+        integratorLiquid.getMoveManager().addMCMove(rotateMove);
+//        ((MCMoveStepTracker)atomMove.getTracker()).setNoisyAdjustment(true);
+        BoxImposePbc pbc = new BoxImposePbc(boxLiquid);
+        integratorLiquid.addIntervalAction(pbc);
+        integratorLiquid.setActionInterval(pbc, 100);
+        
         integratorVapor = new IntegratorMC(potentialMaster, random, temperature);
         integratorVapor.setBox(boxVapor);
         atomMove = new MCMoveAtom(potentialMaster, random, 0.5, 5.0, true);
         integratorVapor.getMoveManager().addMCMove(atomMove);
-        ((MCMoveStepTracker)atomMove.getTracker()).setNoisyAdjustment(true);
+        rotateMove = new MCMoveRotate(potentialMaster, random);
+        integratorVapor.getMoveManager().addMCMove(rotateMove);
+//        ((MCMoveStepTracker)atomMove.getTracker()).setNoisyAdjustment(true);
+        pbc = new BoxImposePbc(boxVapor);
+        integratorVapor.addIntervalAction(pbc);
+        integratorVapor.setActionInterval(pbc, 100);
         
         integratorGEMC = new IntegratorManagerMC(random);
         integratorGEMC.setGlobalMoveInterval(2);
@@ -80,7 +98,8 @@ public class VLESim extends Simulation {
         integratorGEMC.addIntegrator(integratorVapor);
         MCMoveVolumeExchangeVLE volumeExchange = new MCMoveVolumeExchangeVLE(
                 potentialMaster, random, integratorLiquid,integratorVapor);
-        ((MCMoveStepTracker)volumeExchange.getTracker()).setNoisyAdjustment(true);
+        volumeExchange.setStepSize(0.05);
+//        ((MCMoveStepTracker)volumeExchange.getTracker()).setNoisyAdjustment(true);
         MCMoveMoleculeExchangeVLE moleculeExchange = new MCMoveMoleculeExchangeVLE(
                 potentialMaster, random, integratorLiquid,integratorVapor);
         integratorGEMC.getMoveManager().addMCMove(volumeExchange);
@@ -89,10 +108,33 @@ public class VLESim extends Simulation {
         
         activityIntegrate = new ActivityIntegrate(integratorGEMC);
         getController().addAction(activityIntegrate);
+    }
+    
+    public void setSigma(double newSigma) {
+        sigma = newSigma;
+        System.out.println("setting sigma to "+sigma);
+        p2LJQ.setSigma(sigma);
+        p2Truncated.setTruncationRadius(4.0*sigma);
+        ((AtomTypeSphere)species.getLeafType()).setDiameter(sigma);
+    }
 
-        integratorLiquid.getMoveEventManager().addListener(potentialMaster.getNbrCellManager(boxLiquid).makeMCMoveListener());
-        integratorVapor.getMoveEventManager().addListener(potentialMaster.getNbrCellManager(boxVapor).makeMCMoveListener());
-        potentialMaster.getNbrCellManager(boxLiquid).assignCellAll();
-        potentialMaster.getNbrCellManager(boxVapor).assignCellAll();
+    public double getSigma() {
+        return sigma;
+    }
+    
+    public void setEpsilon(double newEpsilon) {
+        p2LJQ.setEpsilon(newEpsilon);
+    }
+    
+    public double getEpsilon() {
+        return p2LJQ.getEpsilon();
+    }
+    
+    public void setMoment(double newQ) {
+        p2LJQ.setQuadrupolarMomentSquare(newQ*newQ);
+    }
+    
+    public double getMoment() {
+        return Math.sqrt(p2LJQ.getQuadrupolarMomentSquare());
     }
 }
