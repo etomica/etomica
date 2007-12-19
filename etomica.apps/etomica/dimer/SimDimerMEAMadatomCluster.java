8b4eb0798c141d7ec38c112605a037ec9e602429
Mike Sellers <msellers@buffalo.edu>
2007-12-19 22:03:58 +0000

    Simulation for Cluster

---
+++ etomica.apps/etomica/dimer/SimDimerMEAMadatomCluster.java
@@ -0,0 +1,61 @@
+package etomica.dimer;
+
+import etomica.simulation.Simulation;
+
+/**
+ * Simulation using Henkelman's Dimer method to find a saddle point for
+ * an adatom of Sn on a surface, modeled with MEAM.
+ * 
+ * @author msellers
+ *
+ */
+
+public class SimDimerMEAMadatomCluster extends Simulation{
+	
+	 public static void main(String[] args){
+	        
+	        String fileName = args[0];
+	        int mdSteps = Integer.parseInt(args[1]);
+	        
+	    	final String APP_NAME = "SimDimerMEAMadatomCluster";
+	    	
+	    	//Simulation 1 - MD and Dimer search
+	    	final SimDimerMEAMadatom sim1 = new SimDimerMEAMadatom(fileName, false, false, false);
+	    	sim1.activityIntegrateMD.setMaxSteps(mdSteps);
+	    	sim1.activityIntegrateDimer.setMaxSteps(1);
+	    	sim1.activityIntegrateMin.setMaxSteps(0);
+	        sim1.getController().actionPerformed();
+	        
+	        //Simulation 2 - Fine grain Dimer search
+	        final SimDimerMEAMadatom sim2 = new SimDimerMEAMadatom(fileName, true, false, false);
+	        sim2.activityIntegrateMD.setMaxSteps(0);
+	        sim2.activityIntegrateDimer.setMaxSteps(1);
+	    	sim2.activityIntegrateMin.setMaxSteps(0);
+	        sim2.getController().actionPerformed();
+	        
+	        //Simulation 3 - Vibrational normal mode analysis
+	        final SimDimerMEAMadatom sim3 = new SimDimerMEAMadatom(fileName, false, true, false);
+	        sim3.activityIntegrateMD.setMaxSteps(0);
+	        sim3.activityIntegrateDimer.setMaxSteps(0);
+	    	sim3.activityIntegrateMin.setMaxSteps(0);
+	        sim3.getController().actionPerformed();
+	        
+	        //Simulation 4 - Minimum Search
+	        final SimDimerMEAMadatom sim4 = new SimDimerMEAMadatom(fileName, false, false, true);
+	        sim4.activityIntegrateMD.setMaxSteps(0);
+	        sim4.activityIntegrateDimer.setMaxSteps(0);
+	    	sim4.activityIntegrateMin.setMaxSteps(1);
+	        sim4.getController().actionPerformed();
+	        
+		    //Simulation 5 - Vibrational normal mode analysis
+	        final SimDimerMEAMadatom sim5 = new SimDimerMEAMadatom(fileName+"_minimum", false, true, false);
+	        sim5.activityIntegrateMD.setMaxSteps(0);
+	        sim5.activityIntegrateDimer.setMaxSteps(0);
+	    	sim5.activityIntegrateMin.setMaxSteps(1);
+	        sim5.getController().actionPerformed();
+	    }
+
+
+
+
+}
