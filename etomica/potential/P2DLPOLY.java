e037b623feb2beb1c83fbdb2dcdc1c5f9ef7e97f
Tai Boon Tan <taitan@buffalo.edu>
2008-01-10 22:25:32 +0000

    2-body potential using dl_poly package

---
+++ etomica/potential/P2DLPOLY.java
@@ -0,0 +1,69 @@
+package etomica.potential;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import etomica.action.WriteConfigurationP2DLPOLY;
+import etomica.atom.AtomSet;
+import etomica.box.Box;
+import etomica.space.Space;
+
+public class P2DLPOLY extends Potential{
+	
+	public P2DLPOLY(Space space){
+		super(2, space);
+       	configP2DLPOLY = new WriteConfigurationP2DLPOLY();
+    	configP2DLPOLY.setConfName("CONFIG");
+
+    
+	}
+	
+	public double energy(AtomSet atoms) {
+
+		configP2DLPOLY.setMolecule(atoms.getAtom(0), atoms.getAtom(1));
+		
+		
+		
+		configP2DLPOLY.actionPerformed();
+		int typeInteraction = configP2DLPOLY.getTypeInteraction();
+		
+		try{
+			Runtime rt = Runtime.getRuntime();
+			Process proc = rt.exec("DLMULTI");
+			int exitVal = proc.waitFor();
+			FileReader fileReader = new FileReader("ConfigEnergy");			
+			BufferedReader bufReader = new BufferedReader(fileReader);
+			
+			String line = bufReader.readLine();
+			return Double.parseDouble(line);
+			
+			
+		}catch (IOException e){
+			throw new RuntimeException(e);
+		}catch (InterruptedException err){
+			throw new RuntimeException(err);
+		}
+	}
+
+	public double getRange() {
+		return Double.POSITIVE_INFINITY;
+	}
+
+
+	public void setBox(Box box) {
+		configP2DLPOLY.setBox(box);
+	}
+	
+	public WriteConfigurationP2DLPOLY getConfigP2DLPOLY() {
+		return configP2DLPOLY;
+	}
+
+	public void setConfigP2DLPOLY(WriteConfigurationP2DLPOLY configP2DLPOLY) {
+		this.configP2DLPOLY = configP2DLPOLY;
+	}
+
+	private Box box;
+	private WriteConfigurationP2DLPOLY configP2DLPOLY;
+
+}
