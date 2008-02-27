4cf26050a149fb986155f915f38c3f106658ed1c
Andrew Schultz <ajs42@buffalo.edu>
2008-02-27 01:10:20 +0000

    soft water potential with SPC parameters

---
+++ etomica/models/water/P2WaterSPCSoft.java
@@ -0,0 +1,19 @@
+
+package etomica.models.water;
+
+import etomica.space.Space;
+import etomica.units.Electron;
+import etomica.units.Kelvin;
+
+/** 
+ * SPC potential for water.  All the real work is done in P2Water3P.
+ */
+public class P2WaterSPCSoft extends P2Water3PSoft {
+
+    public P2WaterSPCSoft(Space space) {
+        super(space, 3.1670, Kelvin.UNIT.toSim(78.23), 
+                Electron.UNIT.toSim(-0.82), Electron.UNIT.toSim(0.41));
+    }
+
+    private static final long serialVersionUID = 1L;
+}
