08b2135b3128e96eabff097192d67183f6f2d7d7
Andrew Schultz <ajs42@buffalo.edu>
2008-02-27 00:58:34 +0000

    Update conformation.  default orientation has the O to H bisection pointing in the y direction

--- etomica/models/water/ConformationWater3P.java
+++ etomica/models/water/ConformationWater3P.java
@@ -9,8 +9,8 @@ import etomica.space.Space;
  */
 public class ConformationWater3P extends Conformation {
 
-    private double bondLengthOH = 1.0;
-    private double angleHOH = 109.5*Math.PI/180.;
+    protected static final double bondLengthOH = 1.0;
+    protected static final double angleHOH = 109.5*Math.PI/180.;
 
     public ConformationWater3P(Space space) {
         super(space);
@@ -18,18 +18,17 @@ public class ConformationWater3P extends Conformation {
     
     public void initializePositions(AtomSet list){
         
-        double x = 0.0;
-        double y = 0.0;
-        
-        IAtomPositioned o = (IAtomPositioned)list.getAtom(0);
-        o.getPosition().E(new double[] {x, y, 0.0});
-               
-        IAtomPositioned h1 = (IAtomPositioned)list.getAtom(0);
-        h1.getPosition().E(new double[] {x+bondLengthOH, y, 0.0});
-                
-        IAtomPositioned h2 = (IAtomPositioned)list.getAtom(0);
-        h2.getPosition().E(new double[] {x+bondLengthOH*Math.cos(angleHOH), y+bondLengthOH*Math.sin(angleHOH), 0.0});
+        IAtomPositioned o = (IAtomPositioned)list.getAtom(2);
+        o.getPosition().E(new double[] {0, 0, 0.0});
 
+        double x = bondLengthOH*Math.sin(0.5*angleHOH);
+        double y = bondLengthOH*Math.cos(0.5*angleHOH);
+        
+        IAtomPositioned h1 = (IAtomPositioned)list.getAtom(0);
+        h1.getPosition().E(new double[] {-x, y, 0.0});
+                
+        IAtomPositioned h2 = (IAtomPositioned)list.getAtom(1);
+        h2.getPosition().E(new double[] {+x, y, 0.0});
     }
     
     private static final long serialVersionUID = 1L;
