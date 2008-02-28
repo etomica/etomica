850120b9bd19003aa4d074481d7c358f883e78a3
Andrew Schultz <ajs42@buffalo.edu>
2008-02-28 02:53:55 +0000

    Add DipoleSource interface

---
+++ etomica/atom/DipoleSource.java
@@ -0,0 +1,16 @@
+package etomica.atom;
+
+import etomica.space.IVector;
+
+/**
+ * Interface for something that can calculate the dipole of a molecule.
+ */
+public interface DipoleSource {
+    
+    /**
+     * Returns the dipole of the given molecule.
+     * The method is likely to through an exception if a molecule of the wrong
+     * type is passed in.
+     */
+    public IVector getDipole(IMolecule molecule);
+}
