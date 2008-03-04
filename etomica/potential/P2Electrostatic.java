1718bdc7eb2cf36d873fef8b1c8ebb33efdc9b04
Andrew Schultz <ajs42@buffalo.edu>
2008-03-04 03:05:13 +0000

    add electrostatic potential

---
+++ etomica/potential/P2Electrostatic.java
@@ -0,0 +1,64 @@
+package etomica.potential;
+
+import etomica.space.Space;
+
+/**
+ * Simple electrostatic potential class.
+ * @author Andrew Schultz
+ */
+public class P2Electrostatic extends Potential2SoftSpherical {
+
+    public P2Electrostatic(Space space) {
+        super(space);
+    }
+    
+    public double d2u(double r2) {
+        return +2*u(r2);
+    }
+
+    public double du(double r2) {
+        return -u(r2);
+    }
+
+    public double uInt(double rc) {
+        // lie.  Nobody really wants to know it's infinity
+        return 0;
+    }
+
+    public double u(double r2) {
+        return qq/Math.sqrt(r2);
+    }
+
+    /**
+     * Returns the charge on the first type of atom.
+     */
+    public double getCharge1() {
+        return q1;
+    }
+    
+    /**
+     * Sets the charge on the first type of atom.
+     */
+    public void setCharge1(double newCharge1) {
+        q1 = newCharge1;
+        qq = q1*q2;
+    }
+
+    /**
+     * Returns the charge on the second type of atom.
+     */
+    public double getCharge2() {
+        return q2;
+    }
+    
+    /**
+     * Sets the charge on the second type of atom.
+     */
+    public void setCharge2(double newCharge2) {
+        q2 = newCharge2;
+        qq = q1*q2;
+    }
+    
+    private static final long serialVersionUID = 1L;
+    protected double q1, q2, qq;
+}
