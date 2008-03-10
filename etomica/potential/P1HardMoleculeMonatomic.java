51f484529f02274942614fdcd82c358390e64518
Andrew Schultz <ajs42@buffalo.edu>
2008-03-10 02:07:18 +0000

    Let the wrapped potential be mutable.  Add hard versions of MoleculeMonatomic

---
+++ etomica/potential/P1HardMoleculeMonatomic.java
@@ -0,0 +1,44 @@
+package etomica.potential;
+
+import etomica.api.IAtomSet;
+import etomica.api.IMolecule;
+import etomica.api.IPotential;
+import etomica.space.Space;
+import etomica.space.Tensor;
+
+/**
+ * 2-body hard Potential class for use between two monatomic molecules.
+ *
+ * @author Andrew Schultz
+ */
+ public class P1HardMoleculeMonatomic extends P1MoleculeMonatomic implements
+        PotentialHard {
+
+    public P1HardMoleculeMonatomic(Space space, IPotential potential) {
+        super(space, potential);
+    }
+
+    public void bump(IAtomSet atoms, double falseTime) {
+        leafAtomSet.atom = ((IMolecule)atoms.getAtom(0)).getChildList().getAtom(0);
+        ((PotentialHard)wrappedPotential).bump(leafAtomSet, falseTime);
+    }
+
+    public double collisionTime(IAtomSet atoms, double falseTime) {
+        leafAtomSet.atom = ((IMolecule)atoms.getAtom(0)).getChildList().getAtom(0);
+        return ((PotentialHard)wrappedPotential).collisionTime(leafAtomSet, falseTime);
+    }
+
+    public double energyChange() {
+        return ((PotentialHard)wrappedPotential).energyChange();
+    }
+
+    public double lastCollisionVirial() {
+        return ((PotentialHard)wrappedPotential).lastCollisionVirial();
+    }
+
+    public Tensor lastCollisionVirialTensor() {
+        return ((PotentialHard)wrappedPotential).lastCollisionVirialTensor();
+    }
+
+    private static final long serialVersionUID = 1L;
+}
--- etomica/potential/P1MoleculeMonatomic.java
+++ etomica/potential/P1MoleculeMonatomic.java
@@ -5,6 +5,7 @@ import etomica.api.IBox;
 import etomica.api.IMolecule;
 import etomica.api.IPotential;
 import etomica.atom.AtomPair;
+import etomica.atom.AtomSetSinglet;
 import etomica.space.Space;
 
 /**
@@ -14,18 +15,17 @@ import etomica.space.Space;
  *
  * @author Andrew Schultz
  */
-public class P2MoleculeMonatomic extends Potential {
+public class P1MoleculeMonatomic extends Potential {
 
-    public P2MoleculeMonatomic(Space space, IPotential potential) {
-        super(2, space);
+    public P1MoleculeMonatomic(Space space, IPotential potential) {
+        super(1, space);
         wrappedPotential = potential;
-        leafAtoms = new AtomPair();
+        leafAtomSet = new AtomSetSinglet();
     }
     
     public double energy(IAtomSet atoms) {
-        leafAtoms.atom0 = ((IMolecule)atoms.getAtom(0)).getChildList().getAtom(0);
-        leafAtoms.atom1 = ((IMolecule)atoms.getAtom(1)).getChildList().getAtom(0);
-        return wrappedPotential.energy(leafAtoms);
+        leafAtomSet.atom = ((IMolecule)atoms.getAtom(0)).getChildList().getAtom(0);
+        return wrappedPotential.energy(leafAtomSet);
     }
 
     public double getRange() {
@@ -45,6 +45,6 @@ public class P2MoleculeMonatomic extends Potential {
     }
 
     private static final long serialVersionUID = 1L;
-    protected final AtomPair leafAtoms;
+    protected final AtomSetSinglet leafAtomSet;
     protected IPotential wrappedPotential;
 }
