818219cac5ccb8fd8602eca04579dc14efe9deb6
Andrew Schultz <ajs42@buffalo.edu>
2008-03-19 20:16:02 +0000

    update comments

--- etomica/potential/PotentialCalculation.java
+++ etomica/potential/PotentialCalculation.java
@@ -8,21 +8,17 @@ import etomica.api.IPotential;
  * arbitrary potential.  Examples of such actions are summing the energy, 
  * computing forces on atoms, determining collision times, etc.
  * Concrete subclasses define these actions through implementation of the 
- * doCalculation(AtomsetIterator, Potential) method, which should be set up
- * to loop through the iterates provided by the iterator (after resetting it)
- * and perform the defined calculation using the given potential.
+ * doCalculation(IAtomSet, IPotential) method, which should perform the
+ * defined calculation on the atoms using the given potential.
  *
  * @see PotentialMaster
  * @see PotentialGroup
  */
-
 public interface PotentialCalculation {
  	
 	/**
-	 * Method giving the specific calculation performed by this class.  Concrete subclasses
-	 * are expected to invoke the iterator's reset() method before beginning iteration.
-	 * @param iterator Iterator that has been conditioned to return the desired atom-sets
-	 * for which the calculation is performed.
+	 * Method giving the specific calculation performed by this class.
+	 * @param atoms IAtomSet the atom sets for which the calculation is performed.
 	 * @param potential The potential used to apply the action defined by this class.
 	 */
 	public void doCalculation(IAtomSet atoms, IPotential potential);
