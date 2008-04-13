bdca9f47bac24f506cbe7b1b9d038015d1f0379c
Andrew Schultz <ajs42@buffalo.edu>
2008-04-13 21:47:17 +0000

    add back a (non-molecule) potential class that wraps a hard potential

---
+++ etomica/potential/P2HardWrapper.java
@@ -0,0 +1,72 @@
+package etomica.potential;
+
+import etomica.api.IAtomSet;
+import etomica.api.IBox;
+import etomica.space.ISpace;
+import etomica.space.Tensor;
+import etomica.units.Dimension;
+
+/**
+ * Hard potential class that wraps another hard potential.
+ *
+ * @author Andrew Schultz
+ */
+ public class P2HardWrapper implements PotentialHard {
+
+    public P2HardWrapper(ISpace space, PotentialHard potential) {
+        this.space = space;
+        wrappedPotential = potential;
+    }
+
+    public double energy(IAtomSet atoms) {
+        return wrappedPotential.energy(atoms);
+    }
+
+    public Dimension getRangeDimension() {
+        return wrappedPotential.getRangeDimension();
+    }
+
+    public int nBody() {
+        return wrappedPotential.nBody();
+    }
+
+    public void setBox(IBox box) {
+        wrappedPotential.setBox(box);
+    }
+    
+    public PotentialHard getWrappedPotential() {
+        return wrappedPotential;
+    }
+
+    public void setWrappedPotential(PotentialHard newWrappedPotential) {
+        wrappedPotential = newWrappedPotential;
+    }
+
+    public double getRange() {
+        return wrappedPotential.getRange();
+    }
+    
+    public void bump(IAtomSet atoms, double falseTime) {
+        wrappedPotential.bump(atoms, falseTime);
+    }
+
+    public double collisionTime(IAtomSet atoms, double falseTime) {
+        return wrappedPotential.collisionTime(atoms, falseTime);
+    }
+
+    public double energyChange() {
+        return wrappedPotential.energyChange();
+    }
+
+    public double lastCollisionVirial() {
+        return wrappedPotential.lastCollisionVirial();
+    }
+
+    public Tensor lastCollisionVirialTensor() {
+        return wrappedPotential.lastCollisionVirialTensor();
+    }
+
+    private static final long serialVersionUID = 1L;
+    protected final ISpace space;
+    protected PotentialHard wrappedPotential;
+}
