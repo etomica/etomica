db50a700bc0217d1ad50ea95e495e2cf889980e9
Andrew Schultz <ajs42@buffalo.edu>
2008-06-24 23:27:14 +0000

    Fix up to work in any dimension (2D or 3D) as well as handling surfaces that are perpendicular to y or z

--- etomica.modules/etomica/modules/interfacial/DataProcessorInterfacialTension.java
+++ etomica.modules/etomica/modules/interfacial/DataProcessorInterfacialTension.java
@@ -7,15 +7,20 @@ import etomica.data.DataPipe;
 import etomica.data.DataProcessor;
 import etomica.data.IDataInfo;
 import etomica.data.types.DataDouble;
-import etomica.data.types.DataTensor;
 import etomica.data.types.DataDouble.DataInfoDouble;
 import etomica.space.ISpace;
-import etomica.space.Tensor;
 import etomica.units.Area;
 import etomica.units.DimensionRatio;
 import etomica.units.Energy;
 import etomica.units.Length;
 
+/**
+ * Data Processor that takes the virial components as input data and returns
+ * the interfacial tension.  This class must be told which dimension the
+ * surfaces exist in.
+ *
+ * @author Andrew Schultz
+ */
 public class DataProcessorInterfacialTension extends DataProcessor {
 
     public DataProcessorInterfacialTension(ISpace space) {
@@ -31,22 +36,37 @@ public class DataProcessorInterfacialTension extends DataProcessor {
         return box;
     }
 
-    public Data processData() {
-        return data;
+    /**
+     * Sets the dimension of the surfaces.  0 (x) means that the surfaces exist
+     * in the yz plane.
+     */
+    public void setSurfaceDimension(int newDim) {
+        surfaceDim = newDim;
+    }
+
+    /**
+     * Returns the dimension of the surfaces.  0 (x) means that the surfaces
+     * exist in the yz plane.
+     */
+    public int getSurfaceDimension() {
+        return surfaceDim;
     }
 
     protected Data processData(Data inputData) {
         double area = 1;
         IVector dim = box.getBoundary().getDimensions();
-        for (int i=1; i<dim.getD(); i++) {
+        int D = dim.getD();
+        for (int i=0; i<D; i++) {
+            if (i == surfaceDim) continue;
             area *= dim.x(i);
         }
         data.x = 0;
-        double vxx = inputData.getValue(0);
-        for (int i=1; i<inputData.getLength(); i++) {
-            data.x += vxx - inputData.getValue(i);
+        data.x = inputData.getValue(0);
+        for (int i=0; i<D; i++) {
+            if (i == surfaceDim) continue;
+            data.x -= inputData.getValue(i)/(D-1);
         }
-        data.x /= 4*area;
+        data.x /= 2*area;
         return data;
     }
 
@@ -61,4 +81,5 @@ public class DataProcessorInterfacialTension extends DataProcessor {
     protected final ISpace space;
     protected IBox box;
     protected final DataDouble data;
+    protected int surfaceDim;
 }
