e5c0950bb4e242926f18e57598882cda9b98999d
Nancy Cribbin <cribbin@buffalo.edu>
2008-06-25 20:49:12 +0000

    Stub of 1 dimension hard rod system for Nancy

---
+++ etomica.apps/etomica/models/oneDHardRods/Sim1DHR.java
@@ -0,0 +1,23 @@
+package etomica.models.oneDHardRods;
+
+import etomica.simulation.Simulation;
+import etomica.space.Space;
+
+public class Sim1DHR extends Simulation {
+
+
+	private static final String APP_NAME = "Sim 1DHR";
+
+	public Sim1DHR(Space _space){
+        super(_space, true);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
