8e47dfbeb41c62a93d4ab9c125dad14a8d154419
Nancy Cribbin <cribbin@buffalo.edu>
2008-06-26 21:29:06 +0000

    building the code

--- etomica.apps/etomica/models/oneDHardRods/Sim1DHR.java
+++ etomica.apps/etomica/models/oneDHardRods/Sim1DHR.java
@@ -1,5 +1,6 @@
 package etomica.models.oneDHardRods;
 
+import etomica.normalmode.SimTarget;
 import etomica.simulation.Simulation;
 import etomica.space.Space;
 
@@ -16,8 +17,47 @@ public class Sim1DHR extends Simulation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        //set up simulation parameters
+        int D = 1;
+        int numMolecules = 10;
+        double density = 0.5;
+        double harmonicFudge = 1.0;
+        double simTime = 1000;
+        double temperature = 1;
 
+        String filename = "normal_modes_1DHR_"+density+"_"+simTime+"_"+numMolecules+"_"+harmonicFudge;
+        if (args.length > 0) {
+            filename = args[0];
+        }
+        if (args.length > 1) {
+            density = Double.parseDouble(args[1]);
+        }
+        if (args.length > 2) {
+            simTime = Double.parseDouble(args[2]);
+        }
+        if (args.length > 3) {
+            numMolecules = Integer.parseInt(args[3]);
+        }
+        if (args.length > 4) {
+            harmonicFudge = Double.parseDouble(args[4]);
+        }
+        
+        System.out.println("Running 1D hard rod simulation");
+        System.out.println(numMolecules+" atoms at density "+density);
+        System.out.println("harmonic fudge: "+harmonicFudge);
+        System.out.println(simTime+" time units");
+        System.out.println("output data to "+filename);
+
+        //instantiate simulation
+        SimTarget sim = new SimTarget(Space.getInstance(D), numMolecules, density);
+        
+        
+        
+        
+        
+        
+        
+        System.out.println("Fini.");
 	}
 
 }
