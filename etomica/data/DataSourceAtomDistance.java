bac4f2864b61b2055efc25dcf7eb6d769eb75a5e
Kate Shaul <schadel2@buffalo.edu>
2008-06-27 22:07:31 +0000

    Brand new DataSource class for collecting/storing distance between two atoms.  It's better than Christmas!  with your in-laws!  when the power is out!  And you feel cold...  So cold...

---
+++ etomica/data/DataSourceAtomDistance.java
@@ -0,0 +1,41 @@
+package etomica.data;
+
+import etomica.api.IAtomPositioned;
+import etomica.api.IVector;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataDouble.DataInfoDouble;
+import etomica.space.ISpace;
+import etomica.units.Length;
+
+public class DataSourceAtomDistance extends DataSourceScalar {
+	
+	public DataSourceAtomDistance(ISpace space) {
+		super("interatomic distance", Length.DIMENSION);
+		
+		this.space = space;
+	
+		vector = space.makeVector(); // to avoid making the vector each time getData() is called
+
+	}
+
+
+	public double getDataAsScalar() {
+
+        vector.Ev1Mv2(atom1.getPosition(), atom2.getPosition());
+        
+		return Math.sqrt(vector.squared());
+	}
+
+	
+	public void setAtoms(IAtomPositioned atom1, IAtomPositioned atom2) {
+		this.atom1 = atom1;
+		this.atom2 = atom2;
+	}
+	
+
+	protected final ISpace space;
+	protected final IVector vector;
+	protected IAtomPositioned atom1;
+	protected IAtomPositioned atom2;
+
+}
