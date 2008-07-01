b64cfc673b0646a2df0867f60dd94251a52299fa
Andrew Schultz <ajs42@buffalo.edu>
2008-07-01 21:57:47 +0000

    allow meter to track conversion of sites on specific atom types

--- etomica.modules/etomica/modules/chainequilibrium/MeterConversion.java
+++ etomica.modules/etomica/modules/chainequilibrium/MeterConversion.java
@@ -1,6 +1,10 @@
 package etomica.modules.chainequilibrium;
 
 import etomica.api.IAtomLeaf;
+import etomica.api.IAtomSet;
+import etomica.api.IBox;
+import etomica.api.IMolecule;
+import etomica.api.ISpecies;
 import etomica.atom.AtomLeafAgentManager;
 import etomica.data.DataSourceScalar;
 import etomica.units.Fraction;
@@ -12,28 +16,44 @@ import etomica.units.Fraction;
  */
 public class MeterConversion extends DataSourceScalar {
 
-    public MeterConversion(AtomLeafAgentManager agentManager) {
+    public MeterConversion(IBox box, AtomLeafAgentManager agentManager) {
         super("Conversion", Fraction.DIMENSION);
+        this.box = box;
         this.agentManager = agentManager;
-        agentIterator = agentManager.makeIterator();
+    }
+
+    public void setSpecies(ISpecies[] newSpecies) {
+        species = newSpecies;
     }
 
     public double getDataAsScalar() {
-        agentIterator.reset();
-        int nReacted = 0;
-        int total = 0;
-        while (agentIterator.hasNext()) {
-            IAtomLeaf[] bonds = (IAtomLeaf[])agentIterator.next();
-            total += bonds.length;
-            for (int i=0; i<bonds.length; i++) {
-                if (bonds[i] != null) {
-                    nReacted++;
-                }
+        total = 0;
+        nReacted = 0;
+        if (species == null) {
+            calcConversion(box.getMoleculeList());
+        }
+        else {
+            for (int i=0; i<species.length; i++) {
+                calcConversion(box.getMoleculeList(species[i]));
             }
         }
         return ((double)(nReacted))/total;
     }
+    
+    protected void calcConversion(IAtomSet monomerList) {
+        for (int i=0; i<monomerList.getAtomCount(); i++) {
+            IAtomLeaf[] bonds = (IAtomLeaf[])agentManager.getAgent(((IMolecule)monomerList.getAtom(i)).getChildList().getAtom(0));
+            total += bonds.length;
+            for (int j=0; j<bonds.length; j++) {
+                if (bonds[j] != null) {
+                    nReacted++;
+                }
+            }
+        }
+    }
 
     protected final AtomLeafAgentManager agentManager;
-    protected final AtomLeafAgentManager.AgentIterator agentIterator;
+    protected ISpecies[] species;
+    protected final IBox box;
+    protected int total, nReacted;
 }
