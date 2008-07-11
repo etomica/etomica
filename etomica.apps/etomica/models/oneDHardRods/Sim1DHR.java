c937403054d5e53dc5b973605c73970482442638
Nancy Cribbin <cribbin@buffalo.edu>
2008-07-11 02:37:51 +0000

    ease of transfer from home to school!

--- etomica.apps/etomica/models/oneDHardRods/Sim1DHR.java
+++ etomica.apps/etomica/models/oneDHardRods/Sim1DHR.java
@@ -1,6 +1,7 @@
 package etomica.models.oneDHardRods;
 
 import etomica.action.activity.ActivityIntegrate;
+import etomica.api.IAtomTypeLeaf;
 import etomica.api.IBox;
 import etomica.box.Box;
 import etomica.data.DataPump;
@@ -8,14 +9,24 @@ import etomica.data.DataSource;
 import etomica.integrator.Integrator;
 import etomica.integrator.IntegratorBox;
 import etomica.integrator.IntegratorHard;
+import etomica.integrator.IntegratorMC;
 import etomica.lattice.crystal.Basis;
 import etomica.lattice.crystal.Primitive;
+import etomica.lattice.crystal.PrimitiveCubic;
 import etomica.nbr.list.PotentialMasterList;
 import etomica.normalmode.CoordinateDefinition;
+import etomica.normalmode.CoordinateDefinitionLeaf;
 import etomica.normalmode.NormalModes1DHR;
+import etomica.normalmode.P2XOrder;
 import etomica.normalmode.SimTarget;
+import etomica.normalmode.WaveVectorFactory;
+import etomica.potential.P1HardPeriodic;
+import etomica.potential.P2HardSphere;
+import etomica.potential.Potential2;
+import etomica.potential.Potential2HardSpherical;
 import etomica.simulation.Simulation;
 import etomica.space.Boundary;
+import etomica.space.BoundaryRectangularPeriodic;
 import etomica.space.Space;
 import etomica.species.SpeciesSpheresMono;
 import etomica.util.ParameterBase;
@@ -67,18 +78,75 @@ public class Sim1DHR extends Simulation {
         SpeciesSpheresMono species = new SpeciesSpheresMono(this, space);
         getSpeciesManager().addSpecies(species);
         
-        //TARGET - system with a normal mode turned on
+        //TARGET - 1DHR system
         PotentialMasterList potentialMasterTarget = new PotentialMasterList(this, space);
         boxTarget = new Box(this, space);
         boxTarget.setNMolecules(species, numAtoms);
         
         IntegratorHard integratorTarget = new IntegratorHard(this, potentialMasterTarget, space);
+        integratorTarget.setTimeStep(4);
+        integratorTarget.setTemperature(temperature);
+        integratorTarget.setIsothermal(false);
+        integrators[1] = integratorTarget;
+        
+        Potential2 p2 = new P2HardSphere(space, 1.0, true);
+        p2 = new P2XOrder(space, (Potential2HardSpherical)p2);
+        potentialMasterTarget.addPotential(p2, new IAtomTypeLeaf[]
+                {species.getLeafType(), species.getLeafType()});
+        
+        primitive = new PrimitiveCubic(space, 1.0/density);
+        boundaryTarget = new BoundaryRectangularPeriodic(space, getRandom(),
+                numAtoms/density);
+        integratorTarget.setNullPotential(new P1HardPeriodic(space), species.getLeafType());
+        nCells = new int[]{numAtoms};
+        boxTarget.setBoundary(boundaryTarget);
+        
+        CoordinateDefinitionLeaf coordinateDefinitionTarget = new 
+                CoordinateDefinitionLeaf(this, boxTarget, primitive, space);
+        coordinateDefinitionTarget.initializeCoordinates(nCells);
+        
+        double neighborRange = 1.01/density;
+        potentialMasterTarget.setRange(neighborRange);
+        //find neighbors now.  Don't hook up NeighborListManager since the
+        //  neighbors won't change
+        potentialMasterTarget.getNeighborManager(boxTarget).reset();
+        integratorTarget.setBox(boxTarget);
         
         
-        //ORIGINAL - 1DHR system
+        //ORIGINAL
+        boundaryOriginal = new BoundaryRectangularPeriodic(space, getRandom(), 
+                numAtoms/density);
+        boxOriginal = new Box(boundaryOriginal, space);
+        addBox(boxOriginal);
+        boxOriginal.setNMolecules(species,numAtoms);
+        IntegratorMC integratorOriginal = new IntegratorMC(null, random, temperature);
+        integratorOriginal.setBox(boxOriginal);
+        
+        MCMoveConvertMode convert = new MCMoveConvertMode(getRandom());
+        integratorOriginal.getMoveManager().addMCMove(convert);
+        integrators[0] = integratorOriginal;
+        
+        CoordinateDefinitionLeaf coordinateDefinitionOriginal = new 
+                CoordinateDefinitionLeaf(this, boxOriginal, primitive, space);
+        
+        nm = new NormalModes1DHR(space.D());
+        nm.setHarmonicFudge(harmonicFudge);
+        nm.setTemperature(temperature);
+        
+        WaveVectorFactory waveVectorFactory = nm.getWaveVectorFactory();
+        waveVectorFactory.makeWaveVectors(boxOriginal);
+        
+        //nan WAVE VECTOR FACTORY STUFF GOES HERE
+        
+        convert.setCoordinateDefinition(coordinateDefinitionOriginal);
+        convert.setTemperature(temperature);
+        convert.setBox(boxOriginal);
+        
+        integratorOriginal.setBox(boxOriginal);
+        potentialMasterTarget.getNeighborManager(boxOriginal).reset();
         
         
-         
+        //OVERLAP   
         
         
         
@@ -119,18 +187,18 @@ public class Sim1DHR extends Simulation {
         }
         double density = params.density;
         double numSteps = params.numSteps;
-        int numMolecules = params.numMolecules;
+        int numAtoms = params.numAtoms;
         double harmonicFudge = params.harmonicFudge;
         double temperature = params.temperature;
         int D = params.D;
         String filename = params.filename;
         if(filename.length() ==0){
-        	filename = "normal_modes_1DHR _" + numMolecules;
+        	filename = "normal_modes_1DHR _" + numAtoms;
         }
         String refFileName = args.length>0 ? filename+"_ref" : null;
         
         System.out.println("Running 1D hard rod simulation");
-        System.out.println(numMolecules+" atoms at density "+density);
+        System.out.println(numAtoms+" atoms at density "+density);
         System.out.println("harmonic fudge: "+harmonicFudge);
         System.out.println((numSteps/1000)+ " total steps of 1000");
         System.out.println("output data to "+filename);
@@ -139,7 +207,7 @@ public class Sim1DHR extends Simulation {
         
         
         //instantiate simulation
-        SimTarget sim = new SimTarget(Space.getInstance(D), numMolecules, density);
+        SimTarget sim = new SimTarget(Space.getInstance(D), numAtoms, density);
         
         
         
@@ -155,7 +223,7 @@ public class Sim1DHR extends Simulation {
      * Inner class for parameters understood by the Sim1DHR constructor
      */
     public static class Sim1DHRParams extends ParameterBase {
-        public int numMolecules = 32;
+        public int numAtoms = 32;
         public double density = 0.5;
         public int D = 1;
         public long numSteps = 100000;
