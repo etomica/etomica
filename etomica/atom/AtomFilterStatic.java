cf1e8f5349d7681853f03bee0479963603360b4a
Andrew Schultz <ajs42@buffalo.edu>
2008-07-19 20:24:29 +0000

    fix comments / whitespace

--- etomica/atom/AtomFilterStatic.java
+++ etomica/atom/AtomFilterStatic.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package etomica.atom;
 
 import java.io.Serializable;
@@ -13,31 +10,32 @@ public class AtomFilterStatic implements AtomFilter, Serializable {
     private AtomFilterStatic(boolean accept) {
         rv = accept;
     }
-    
+
     public boolean accept(IAtom a) {return rv;}
-    /**
-     * Required to guarantee singleton when deserializing.
-     * @return the singleton INSTANCE
-     */
+
     /**
      * Static instance of a filter that accepts all atoms.
      * Returns true for null atom also.
      */
     public static final AtomFilterStatic ACCEPT_ALL = new AtomFilterStatic(true);
-    
+
     /**
      * Static instance of a filter that rejects all atoms.
      * Returns false for null atom also.
      */
     public static final AtomFilterStatic ACCEPT_NONE = new AtomFilterStatic(false);
-    
+
+    /**
+     * Required to guarantee singleton when deserializing.
+     * @return the singleton INSTANCE
+     */
     private Object readResolve() {
         if (this.accept(null)) {
             return ACCEPT_ALL;
         }
         return ACCEPT_NONE;
     }
-    
+
     private static final long serialVersionUID = 1L;
     private final boolean rv;
 }
