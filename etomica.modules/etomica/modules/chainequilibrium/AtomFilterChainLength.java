d9132f4b073a718466b4abe0d96438a91b4ba058
Andrew Schultz <ajs42@buffalo.edu>
2008-07-19 21:56:09 +0000

    add filter to show only longest chain

---
+++ etomica.modules/etomica/modules/chainequilibrium/AtomFilterChainLength.java
@@ -0,0 +1,102 @@
+package etomica.modules.chainequilibrium;
+
+import etomica.api.IAtom;
+import etomica.api.IAtomLeaf;
+import etomica.api.IAtomSet;
+import etomica.api.IBox;
+import etomica.atom.AtomFilter;
+import etomica.atom.AtomLeafAgentManager;
+
+/**
+ */
+public class AtomFilterChainLength implements AtomFilter, AtomLeafAgentManager.AgentSource {
+
+    public AtomFilterChainLength(AtomLeafAgentManager aam) {
+        agentManager = aam;
+    }
+
+    public Class getAgentClass() {
+        return LengthAgent.class;
+    }
+
+    public Object makeAgent(IAtomLeaf a) {
+        return new LengthAgent();
+    }
+
+    public void releaseAgent(Object agent, IAtomLeaf atom) {}
+
+    protected void tagAll() {
+        maxChainLength = 0;
+        // untag all the Atoms
+        IAtomSet leafList = box.getLeafList();
+        int nLeaf = leafList.getAtomCount();
+        for (int i=0; i<nLeaf; i++) {
+            ((LengthAgent)chainLengthManager.getAgent(leafList.getAtom(i))).chainLength = 0;
+        }
+
+        for (int i=0; i<nLeaf; i++) {
+            IAtomLeaf a = (IAtomLeaf)leafList.getAtom(i);
+            // if an Atom has a chain length, it was already counted as part of 
+            // another chain
+            if (((LengthAgent)chainLengthManager.getAgent(a)).chainLength > 0) continue;
+
+            int chainLength = recursiveTag(a, -1);
+            // re-tag with the actual chain length
+            recursiveTag(a, chainLength);
+            if (chainLength > maxChainLength) {
+                maxChainLength = chainLength;
+            }
+        }
+    }
+
+    protected int recursiveTag(IAtomLeaf a, int chainLength) {
+        ((LengthAgent)chainLengthManager.getAgent(a)).chainLength = chainLength;
+
+        IAtomLeaf[] nbrs = (IAtomLeaf[])agentManager.getAgent(a);
+
+        int ctr = 1;
+        
+        // count all the bonded partners
+        for(int i=0; i<nbrs.length; i++) {
+            if(nbrs[i] == null) continue;
+            if(((LengthAgent)chainLengthManager.getAgent(nbrs[i])).chainLength == chainLength) {
+                // this Atom was already counted as being within this chain
+                // so skip it
+                continue;
+            }
+            // count this Atom and all of its bonded partners
+            ctr += recursiveTag(nbrs[i], chainLength);
+        }
+        return ctr;
+    }
+
+    public IBox getBox() {
+        return box;
+    }
+
+    public void setBox(IBox box) {
+        this.box = box;
+        if (chainLengthManager != null) {
+            // allow old agentManager to de-register itself as a BoxListener
+            chainLengthManager.dispose();
+        }
+        chainLengthManager = new AtomLeafAgentManager(this,box);
+    }
+
+    public boolean accept(IAtom a) {
+        if (box.getLeafIndex(a) == 0) {
+            tagAll();
+        }
+        return ((LengthAgent)chainLengthManager.getAgent(a)).chainLength == maxChainLength;
+    }
+
+    private static final long serialVersionUID = 1L;
+    protected IBox box;
+    protected AtomLeafAgentManager chainLengthManager;
+    protected AtomLeafAgentManager agentManager;
+    protected int maxChainLength = 0;
+
+    public static class LengthAgent {
+        public int chainLength;
+    }
+}
