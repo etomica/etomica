65b5dc04a8616af997df1fb546abdd45f55cfa59
Kate Shaul <schadel2@buffalo.edu>
2008-08-01 20:12:56 +0000

    *** empty log message ***

--- etomica/action/IntegratorDimerApproach.java
+++ etomica/action/IntegratorDimerApproach.java
@@ -58,14 +58,26 @@ public class IntegratorDimerApproach extends IntegratorBox {
 		 * *****************************************************************
 		 */
         
-        double yaw   =   routeParams[0][0]*Math.PI/180;
-		double pitch = - routeParams[1][0]*Math.PI/180;
+        double yaw;
+        if (routeParams[0].length == 1) {
+        	yaw = routeParams[0][0]*Math.PI/180;
+        	
+        } else if (routeParams[0].length == 2) {
+        	double a = routeParams[0][0];
+        	double b = routeParams[0][1];
+        	yaw = ( a + b*r ) * Math.PI/180;
+        } else {
+        	throw new IllegalArgumentException("Incorrect number of elements in yaw array");
+        }
+        
+		double pitch =   routeParams[1][0]*Math.PI/180;
 		double roll  =   routeParams[2][0]*Math.PI/180;
-		
+	
         atomActionRotateBy.setAngles(roll,pitch,yaw);
         atomGroupActionRotateBy = new AtomGroupAction(atomActionRotateBy);
 		atomGroupActionRotateBy.actionPerformed(monomerB);
 		
+		printKeyAtomPositions();
 		
         /*System.out.println();
         System.out.println("After adjusting orientation: ");
@@ -77,14 +89,25 @@ public class IntegratorDimerApproach extends IntegratorBox {
 		 * *****************************************************************
 		 * *****************************************************************
 		 */
-        
+		double r0 = r;
+        delta = r0/60;
+		translationVector();
 		translateMonomerB(); 
 		
-		/*printKeyAtomPositions();
+		printKeyAtomPositions();
 		
 		if (route == 15) {
 			checkOHBondLength();
-		}*/
+		}
+		
+		/* *****************************************************************
+		 * *****************************************************************
+		 * set translation Vector that will be used during each call of doStepInternal
+		 * *****************************************************************
+		 * *****************************************************************
+		 */
+		 
+		translationVector();
 	
 	}
 	
@@ -93,26 +116,24 @@ public class IntegratorDimerApproach extends IntegratorBox {
 		// inverse sine function used to compute phi becomes undefined at small r
         // Using this if statement causes last few steps to affect no change,
 		// but prevents one from having to calculate precise number of steps
-		// to avoid rescaling PE plot and bothering with NaNs
+		// to avoid NaNs
 		
-		// The value of r at which the potential energy becomes order 10.
-		double liftOff = routeParams[5][0];
-		
-        if (r > liftOff) {
+      //  if (r > 0.0) {
         	
         	translateMonomerB();
         	
-        	/*printKeyAtomPositions();
+        	//printKeyAtomPositions();
+
         	
         	if (route == 15) {
         		checkOHBondLength();
-        	}*/
+        	}
         	
-        }
+       // }
 		
 	}
 	
-	public void translateMonomerB() {	
+	public void translationVector() {	
 		
 		/* *****************************************************************
 		 * *****************************************************************
@@ -159,7 +180,7 @@ public class IntegratorDimerApproach extends IntegratorBox {
 			double b = routeParams[4][1];
 			double c = routeParams[4][2];
 			
-			phi = Math.asin( (a/(r*r)) + (b/r) + c ); 
+			phi = Math.asin( (a/(r*r)) + (b/r) + c ); // already in radians
 			
 		} else {
 			
@@ -167,12 +188,13 @@ public class IntegratorDimerApproach extends IntegratorBox {
 			
 		}
 		
-		//System.out.println("phi' = " + phi);
+		// System.out.println("phi' = " + phi);
 		
 		// Rowley et al definition of phi (above) a little different...
 		phi = 0.5*Math.PI - phi;
+		//phi = -phi;
 		
-		//System.out.println("phi = " + phi);
+		// System.out.println("phi = " + phi);
 		
 		// Cartesian coordinates of new alpha-carbon position
 		double x = r*Math.cos(theta)*Math.sin(phi);
@@ -182,6 +204,10 @@ public class IntegratorDimerApproach extends IntegratorBox {
 		newOriginB.E( new double [] {x, y, z});
 		
 		translationVector.Ev1Mv2(newOriginB, atom_aC_B.getPosition());
+	
+	}
+	
+	public void translateMonomerB() {
 		
 		atomActionTranslateBy.setTranslationVector(translationVector);
 		atomGroupActionTranslateBy = new AtomGroupAction(atomActionTranslateBy);
@@ -189,7 +215,7 @@ public class IntegratorDimerApproach extends IntegratorBox {
         
         // Update r for next call 
 		r = r-delta;
-		
+	
 	}
 	
 	public void setRoute(int route) {
@@ -212,12 +238,12 @@ public class IntegratorDimerApproach extends IntegratorBox {
 	    
 	    atom_O_A  = (IAtomPositioned) atomSetA.getAtom(0);
 	    atom_aC_A = (IAtomPositioned) atomSetA.getAtom(1);
-	    atom_aH_A = (IAtomPositioned) atomSetA.getAtom(3);
+	    atom_aH_A = (IAtomPositioned) atomSetA.getAtom(2);
 	    atom_H1_A = (IAtomPositioned) atomSetA.getAtom(4);
 	    
 	    atom_O_B  = (IAtomPositioned) atomSetB.getAtom(0);
 	    atom_aC_B = (IAtomPositioned) atomSetB.getAtom(1);
-	    atom_aH_B = (IAtomPositioned) atomSetB.getAtom(3);
+	    atom_aH_B = (IAtomPositioned) atomSetB.getAtom(2);
 	    
 	}
 	
@@ -241,8 +267,15 @@ public class IntegratorDimerApproach extends IntegratorBox {
 	public void checkOHBondLength() {
 		IVector work = space.makeVector();
         
+        work.Ev1Mv2(atom_aC_A.getPosition(), atom_aC_B.getPosition());
+        double raCaC = Math.sqrt(work.squared());
+        
+        System.out.println("aC-aC distance" + raCaC);
+        
         work.Ev1Mv2(atom_O_B.getPosition(), atom_O_A.getPosition());
         double rOO = Math.sqrt(work.squared());
+        
+        System.out.println("O-O distance" + rOO);
         
         work.Ev1Mv2(atom_O_B.getPosition(), atom_aH_A.getPosition());
         double rOH1 = Math.sqrt(work.squared());
@@ -253,10 +286,10 @@ public class IntegratorDimerApproach extends IntegratorBox {
         double rOH2 = Math.sqrt(work.squared());
         double bondOHB = rOO - rOH2;
         
-        System.out.println();
+        /*System.out.println();
         System.out.println("bondOH (version A)" + bondOHA);
         System.out.println("bondOH (version B)" + bondOHB);
-        System.out.println();
+        System.out.println();*/
 	}
 	
 	private final ISpace space;
@@ -294,10 +327,10 @@ public class IntegratorDimerApproach extends IntegratorBox {
     protected static IAtomPositioned atom_aH_B;
     
     // The distance between the alpha carbons of the two monomers
-    protected double r = 14.0;  
+    protected double r = 50.0;  
     
     // The size of the change in r for each translation of monomer B
-	protected double delta = 0.2;
+	protected double delta;
 	
 	// The number of the approach route
 	protected int route;
