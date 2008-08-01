00bd217909dd88d34fdd995a5f1f27b6ec19aac1
Kate Shaul <schadel2@buffalo.edu>
2008-08-01 20:23:22 +0000

    A class not for really creating a histogram, but rather averaging the dependent-variable values within bins of the independent variable

---
+++ etomica/util/HistogramNotSoSimple.java
@@ -0,0 +1,131 @@
+package etomica.util;
+
+import java.io.Serializable;
+
+
+/* History
+ * 09/08/02 (DAK) added set/get methods for xMin, xMax, nBins
+ * 08/04/04 (DAK,AJS,NRC) deleted DataSource.X methods; de-implemented DataSource.X.  Dimension-related material removed
+ */
+
+/**
+ * Simple Histogram implementation with a static x range and number of bins.
+ * If an x value is given that falls outside the histogram's x range, the 
+ * value is dropped.  The x range and number of bins can be changed explicitly,
+ * but doing so will reset the histogram (losing all previously collected data).
+ */
+public class HistogramNotSoSimple implements Histogram, java.io.Serializable {
+	protected double deltaX;
+	private long sum;
+	protected int[] counts;
+	protected double[] sums;
+	protected double[] histogram;
+    protected double xValues[];
+    protected double xMin;
+    protected double xMax;
+    protected int nBins;
+
+    /**
+     * Makes a new histogram instance, with the range of x values given by
+     * xRange.
+     */
+    public HistogramNotSoSimple(DoubleRange xRange) {
+        this(100, xRange);
+	}
+	
+    /**
+     * Makes a new histogram instance, with the range of x values given by
+     * xRange and n bins.
+     */
+    public HistogramNotSoSimple(int n, DoubleRange xRange) {
+        nBins = n;
+        counts = new int[n];
+        sums = new double[n];
+        histogram = new double[n];
+        xValues = new double[n];
+        setXRange(xRange);
+    }
+	
+	public void reset() {
+        //resets all histogram values and counts to zero
+	    sum = 0;
+	    for(int i=0; i<nBins; i++) {
+	        counts[i] = 0;
+	        sums[i] = 0;
+            xValues[i] = xMin + deltaX * (i+0.5);
+	    }
+	}
+	
+    public void addValue(double x, double y) {
+        //takes new value and updates histogram
+        if(x >= xMin && x <= xMax) {
+	        int i = (int)Math.floor(((x-xMin)/deltaX));
+	        if(i == nBins){i--;}
+            counts[i]++;
+            sums[i] += y;
+	    }
+	    sum++;
+    }
+    
+    public void addValue(double x) {
+    	// This is just to trick Histogram!!! 
+    	// Using this method will not cause class to behave like HistogramSimple!
+    	addValue(x,1);
+    }
+    
+    public void setXRange(DoubleRange xRange) {
+        xMin = xRange.minimum();
+        xMax = xRange.maximum();
+        deltaX = (xMax-xMin)/nBins;
+        reset();
+    }
+    
+    public DoubleRange getXRange() {
+        return new DoubleRange(xMin, xMax);
+    }
+      
+    public int getNBins() {
+        return nBins;
+    }
+    
+    public void setNBins(int n) {
+        this.nBins = n;
+        counts = new int[n];
+        histogram = new double[n];
+        xValues = new double[n];
+        deltaX = (xMax-xMin)/nBins;
+        reset();
+    }
+    
+    public double[] getHistogram() {
+        //returns an array representing the present histogram
+        if (sum != 0) {
+		    for(int i=0; i<nBins; i++) {
+		        histogram[i] = (double)sums[i]/counts[i];
+		    }
+        }
+	    return histogram;
+    }
+    
+    public long getCount() {
+        return sum;
+    }
+ 
+    public double[] xValues() {
+        return xValues;
+    }
+    
+    public static class Factory implements Histogram.Factory, Serializable {
+        public Factory(int n, DoubleRange xRange) {
+            nBins = n;
+            this.xRange = xRange;
+        }
+        
+        public Histogram makeHistogram() {
+            return new HistogramNotSoSimple(nBins, xRange);
+        }
+        
+        private int nBins;
+        private DoubleRange xRange;
+    }
+}
