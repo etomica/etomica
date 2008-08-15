effda75c381e6ef9929df7be5d824b5204732881
Andrew Schultz <ajs42@buffalo.edu>
2008-08-15 18:11:43 +0000

    turn DataLogger into a DataProcessor.  remove obsolete priority functionality

--- etomica/data/DataLogger.java
+++ etomica/data/DataLogger.java
@@ -12,72 +12,47 @@ import etomica.action.activity.ControllerListener;
  * DataSink that manages a FileWriter and also listens to non-interval 
  * integrator events and sends appropriate data to a DataWriter.
  */
-public class DataLogger implements DataPipe, ControllerListener, java.io.Serializable {
+public class DataLogger extends DataProcessor implements ControllerListener, java.io.Serializable {
     
     public DataLogger(){
+        super();
         setWriteInterval(100);
-        setPriority(300);
-        tag = new DataTag();
+    }
+    
+    public void setDataSink(DataSink dataSink) {
+        if (!(dataSink instanceof DataWriter)) {
+            throw new IllegalArgumentException("data sink must be a DataWriter");
+        }
+        super.setDataSink(dataSink);
     }
     
     /**
      * Gives data to DataSink for writing
      */
-    public void putData(Data data) {
+    public Data processData(Data data) {
         if (--count == 0) {
             count = writeInterval;
             doWrite(data);
         }
+        return data;
     }
     
-    public void putDataInfo(IDataInfo newDataInfo) {
+    public IDataInfo processDataInfo(IDataInfo newDataInfo) {
         dataInfo = newDataInfo.getFactory().makeDataInfo();
         dataInfo.addTag(tag);
-        insertTransformerIfNeeded();
-        if (dataSink != null) {
-            dataSink.putDataInfo(dataInfo);
-        }
+        return dataInfo;
     }
     
-    public DataTag getTag() {
-        return tag;
-    }
-    
-    protected void insertTransformerIfNeeded() {
-        if(dataWriter == null || dataInfo == null) return;
-        //remove transformer if one was previously inserted
-        dataSink = dataWriter;
-        DataPipe caster = dataWriter.getDataCaster(dataInfo);
-        if(caster != null) {
-            caster.setDataSink(dataWriter);
-            dataSink = caster;
-        }
-        dataSink.putDataInfo(dataInfo);
-    }
-
     public DataPipe getDataCaster(IDataInfo incomingDataInfo) {
         // we don't care about the type although the DataWriter might
         return null;
     }
     
     /**
-     * Sets the data writer to the given DataSink.  It must be an instance of
-     * DataLogger.DataWriter
-     */
-    public void setDataSink(DataSink dataWriter) {
-        this.dataWriter = (DataWriter)dataWriter;
-        insertTransformerIfNeeded();
-    }
-    
-    public DataSink getDataSink() {
-        return dataWriter;
-    }
-    
-    /**
      * Close file when integrator is done.
      */
     public void actionPerformed(ControllerEvent evt){
-        if(dataWriter != null && (evt.getType() == ControllerEvent.NO_MORE_ACTIONS ||
+        if(dataSink != null && (evt.getType() == ControllerEvent.NO_MORE_ACTIONS ||
             evt.getType() == ControllerEvent.HALTED)) {
             if (writeOnFinishSource != null) {
                 doWrite(writeOnFinishSource.getData());
@@ -93,7 +68,7 @@ public class DataLogger implements DataPipe, ControllerListener, java.io.Seriali
      */
     private void doWrite(Data data) {
         openFile();
-        dataWriter.setFileWriter(fileWriter);
+        ((DataWriter)dataSink).setFileWriter(fileWriter);
         dataSink.putData(data);
         if(closeFileEachTime || !appending) {
             closeFile();
@@ -120,7 +95,7 @@ public class DataLogger implements DataPipe, ControllerListener, java.io.Seriali
             if(fileName == "") fileName = defaultFileName(); //if fileName is not defined yet, use the current date to be the fileName.
             fileWriter = new FileWriter(fileName + fileNameSuffix, appending);
             if (!appending) {
-                dataWriter.reset();
+                ((DataWriter)dataSink).reset();
             }
             fileIsOpen = true;
         }
@@ -222,23 +197,6 @@ public class DataLogger implements DataPipe, ControllerListener, java.io.Seriali
         this.writeOnInterval = writeOnInterval;
     }
 
-    /**
-     * @return Returns the interval-listener priority.
-     */
-    public int getPriority() {
-        return priority;
-    }
-    
-    /**
-     * Sets the interval-listener priority.  Default value is 300, which
-     * puts this after central-image enforcement and accumulator updates.
-     * @param priority The priority to set.
-     */
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-
     private static final long serialVersionUID = 2L;
     private int count; //counts number of events received before calling doAction().
     private int writeInterval; //number of times specified by the user between two actions.
@@ -251,13 +209,8 @@ public class DataLogger implements DataPipe, ControllerListener, java.io.Seriali
     private boolean sameFileEachTime = true; //whether to write to the same file at each INTERVAL.
     private boolean closeFileEachTime = false; //whether to close the file and open a new one at each INTERVAL.
     private transient boolean fileIsOpen = false; //at the beginning, it is false.
-    private int priority;
     private DataSource writeOnFinishSource = null;
     private boolean writeOnInterval = true;
-    private DataWriter dataWriter;
-    private DataSink dataSink;
-    private IDataInfo dataInfo;
-    protected final DataTag tag;
 
     private void readObject(java.io.ObjectInputStream in)
     throws IOException, ClassNotFoundException
