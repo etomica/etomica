1bfc015d18e0387f54b45424ffde7923dc3f71d4
Hye Min Kim <hmkim3@buffalo.edu>
2008-11-14 21:03:14 +0000

    *** empty log message ***

--- etomica.apps/etomica/virial/SpeciesFactorySpheres2.java
+++ etomica.apps/etomica/virial/SpeciesFactorySpheres2.java
@@ -3,21 +3,25 @@ package etomica.virial;
 import etomica.api.ISimulation;
 import etomica.api.ISpecies;
 import etomica.api.IVector;
-import etomica.config.ConformationChainZigZag;
+import etomica.chem.elements.ElementSimple;
+import etomica.config.ConformationChainZigZag2;
 import etomica.space.ISpace;
+
 
 public class SpeciesFactorySpheres2 implements SpeciesFactory, java.io.Serializable {
 
-    public SpeciesFactorySpheres2(ISpace space, int nA) {
-        this(space, nA, nominalBondL, nominalBondTheta);
+    public SpeciesFactorySpheres2(ISpace space, int nA, ElementSimple CH3element, ElementSimple CH2element) {
+        this(space, nA, CH3element,CH2element, nominalBondL, nominalBondTheta);
     }
     
-    public SpeciesFactorySpheres2(ISpace space, int nA, double bondL, double bondTheta) {
+    public SpeciesFactorySpheres2(ISpace space, int nA, ElementSimple CH3element, ElementSimple CH2element, double bondL, double bondTheta) {
         this.nA = nA;
+        this.CH3element = CH3element;
+        this.CH2element = CH2element;
         this.bondL = bondL;
         this.bondTheta = bondTheta;
         this.space = space;
-        init();
+                init();
     }
     
     public void setBondL(double newBondL) {
@@ -44,11 +48,11 @@ public class SpeciesFactorySpheres2 implements SpeciesFactory, java.io.Serializa
         IVector vector2 = space.makeVector();
         vector2.setX(0, bondL*Math.cos(bondTheta));
         vector2.setX(1, bondL*Math.sin(bondTheta));
-        conformation = new ConformationChainZigZag(space, vector1, vector2);
+        conformation = new ConformationChainZigZag2(space, vector1, vector2);
     }
     
     public ISpecies makeSpecies(ISimulation sim, ISpace _space) {
-        SpeciesAlkane species = new SpeciesAlkane(sim, _space, nA);
+        SpeciesAlkane species = new SpeciesAlkane(sim, _space, nA,CH3element, CH2element);
         species.setConformation(conformation);
         return species;
     }
@@ -60,7 +64,10 @@ public class SpeciesFactorySpheres2 implements SpeciesFactory, java.io.Serializa
     protected double bondL;
     protected double bondTheta;
     private final int nA;
-    private ConformationChainZigZag conformation;
+    private final ElementSimple CH3element;
+    private final ElementSimple CH2element;
+    //private final int element2;
+    private ConformationChainZigZag2 conformation;
 
 }
 
