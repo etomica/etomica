1f0e04833d3cbe7cee712023b947e00da4f1109c
Nancy Cribbin <cribbin@buffalo.edu>
2008-11-19 14:22:30 +0000

    finished, and tested.

--- etomica.apps/etomica/models/oneDHardRods/AffectedWaveVectors.java
+++ etomica.apps/etomica/models/oneDHardRods/AffectedWaveVectors.java
@@ -22,7 +22,7 @@ public class AffectedWaveVectors {
      * @param values
      */
     public void setWVs(int[] values){
-        if(values.length < length){
+        if(values.length > length){
             throw new IllegalArgumentException("Trying to compare more " +
                     "wavevectors than the system has (in AffectedWaveVectors!");
         }
@@ -30,6 +30,10 @@ public class AffectedWaveVectors {
             useme[i] = false;
         }
         for(int j = 0; j < values.length; j++){
+            if(values[j] < 1){
+                throw new IllegalArgumentException ("AffectedWaveVectors " +
+                        "cannot cope with a negative or zero wavevector");} 
+            useme[values[j]] = true;
         }
     }
 
@@ -40,4 +44,36 @@ public class AffectedWaveVectors {
     public boolean[] getWVs(){
         return useme;
     }
+    
+    
+    
+//    public static void main(String[] args){
+//        int dork = 10;
+//        AffectedWaveVectors wvs = new AffectedWaveVectors(dork);
+//        
+//        System.out.println("1, 6, 5");
+//        wvs.setWVs(new int[] {1, 6, 5});
+//        for(int j = 0; j < dork; j++){
+//            System.out.println("j:  "+j+ "  "+ wvs.getWV(j));
+//        }
+//        
+//        System.out.println("1, 6, 5, 10");
+//        wvs.setWVs(new int[] {1, 6, 5, 10});
+//        for(int j = 0; j < dork; j++){
+//            System.out.println("j:  "+j+ "  "+ wvs.getWV(j));
+//        }
+//        
+//        System.out.println("1, 0");   
+//        wvs.setWVs(new int[] {1, 0});
+//        for(int j = 0; j < dork; j++){
+//            System.out.println("j:  "+j+ "  "+ wvs.getWV(j));
+//        }
+//        
+//        System.out.println("2, -1");
+//        wvs.setWVs(new int[] {2, -1});
+//        for(int j = 0; j < dork; j++){
+//            System.out.println("j:  "+j+ "  "+ wvs.getWV(j));
+//        }            
+//    }  
 }
+
