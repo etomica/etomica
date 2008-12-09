4f83900ff79d4d48ed8ec4e77ee186984a8d0fdd
Tai Boon Tan <taitan@buffalo.edu>
2008-12-09 22:51:30 +0000

    NormalMode 1D Module

--- etomica.apps/etomica/normalmode/NormalModeAnalysisDisplay1D.java
+++ etomica.apps/etomica/normalmode/NormalModeAnalysisDisplay1D.java
@@ -23,32 +23,34 @@ public class NormalModeAnalysisDisplay1D extends Simulation {
     private static final long serialVersionUID = 1L;
 	private static final String APP_NAME = "1-D Harmonic Oscillator";
 
-	public NormalModeAnalysisDisplay1D(Space _space, int numAtoms, double density, double 
-			temperature, String filename){
-        super(_space, true);
+	public NormalModeAnalysisDisplay1D(){
+        super(Space.getInstance(1), true);
      
-        SpeciesSpheresMono species = new SpeciesSpheresMono(this, space);
+        //Default
+        double temperature = 0.1;
+        int numAtoms = 20;
+        SpeciesSpheresMono species = new SpeciesSpheresMono(this, getSpace());
         getSpeciesManager().addSpecies(species);
         
-        box = new Box(this, space);
+        box = new Box(this, getSpace());
         addBox(box);
         box.setNMolecules(species, numAtoms);
         
-        primitive = new PrimitiveCubic(space, 1.0/density);
-        boundary = new BoundaryRectangularPeriodic(space, getRandom(), numAtoms/density);
+        primitive = new PrimitiveCubic(getSpace(), 1.0/density);
+        boundary = new BoundaryRectangularPeriodic(getSpace(), getRandom(), numAtoms/density);
         nCells = new int[]{numAtoms};
         box.setBoundary(boundary);
 
-        coordinateDefinition = new CoordinateDefinitionLeaf(this, box, primitive, space);
+        coordinateDefinition = new CoordinateDefinitionLeaf(this, box, primitive, getSpace());
         coordinateDefinition.initializeCoordinates(nCells);
         
-        nm = new NormalModes1DHR(space.D());
+        nm = new NormalModes1DHR(getSpace().D());
         nm.setTemperature(temperature);
         
         WaveVectorFactory waveVectorFactory = nm.getWaveVectorFactory();
         waveVectorFactory.makeWaveVectors(box);
         
-        integrator = new IntegratorHarmonic(random, 0.001, temperature, space);
+        integrator = new IntegratorHarmonic(random, 0.001, temperature, getSpace());
         integrator.setCoordinateDefinition(coordinateDefinition);
         integrator.setWaveVectors(waveVectorFactory.getWaveVectors());
         integrator.setWaveVectorCoefficients(waveVectorFactory.getCoefficients());
@@ -63,34 +65,33 @@ public class NormalModeAnalysisDisplay1D extends Simulation {
         integrator.setBox(box);
         
 	}
+
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
  
-        int numAtoms =20;
-        double density = 0.4;
-        double temperature = 0.1;
         //instantiate simulation
-        NormalModeAnalysisDisplay1D sim = new NormalModeAnalysisDisplay1D(Space.getInstance(1), 
-        		numAtoms, density, temperature, APP_NAME);
+        NormalModeAnalysisDisplay1D sim = new NormalModeAnalysisDisplay1D();
         
-        SimulationGraphic simGraphic = new SimulationGraphic(sim, Space.getInstance(1), sim.getController());
+        SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, Space.getInstance(1), sim.getController());
         simGraphic.makeAndDisplayFrame(APP_NAME);
         
 	}
 	
 	public static class Applet extends javax.swing.JApplet{
 		
-        int numAtoms =20;
-        double density = 0.4;
+        int numAtoms =30;
+        double density = 1.0;
         double temperature = 0.1;
 		
 		public void init(){
 	
 		}
 	}
+	
+	
 	
 	protected IntegratorHarmonic integrator;
 	protected ActivityIntegrate activityIntegrate;
@@ -102,5 +103,9 @@ public class NormalModeAnalysisDisplay1D extends Simulation {
 	protected SpeciesSpheresMono species;
 	protected NormalModes1DHR nm;
 	protected CoordinateDefinitionLeaf coordinateDefinition;
+	protected double density = 0.5;
+
+
+
 	
 }
---
+++ etomica.apps/etomica/normalmode/NormalModeAnalysisDisplay1DGraphic.java
@@ -0,0 +1,122 @@
+package etomica.normalmode;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import etomica.api.IAction;
+import etomica.exception.ConfigurationOverlapException;
+import etomica.graphics.DeviceNSelector;
+import etomica.graphics.DeviceThermoSlider;
+import etomica.graphics.SimulationGraphic;
+import etomica.space.Space;
+import etomica.units.Temperature;
+
+/**
+ * 
+ * 
+ * @author Tai Boon Tan
+ */
+public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
+
+
+	public NormalModeAnalysisDisplay1DGraphic(final NormalModeAnalysisDisplay1D simulation, Space space) {
+		
+		super(simulation, TABBED_PANE, APP_NAME,REPAINT_INTERVAL, space, simulation.getController());
+		this.sim = simulation;
+		
+		resetAction = getController().getSimRestart().getDataResetAction();
+		
+		final DeviceNSelector nSlider = new DeviceNSelector(sim.getController());
+        nSlider.setSpecies(sim.species);
+        nSlider.setBox(sim.box);
+        nSlider.setMinimum(0);
+        nSlider.setMaximum(40);
+        nSlider.setLabel("Number of Atoms");
+        nSlider.setShowBorder(true);
+        nSlider.setShowValues(true);
+        
+        nSlider.setPostAction(new IAction() {
+            public void actionPerformed() {
+            	int n = (int)nSlider.getValue();
+                if(n == 0) {
+                	sim.integrator.setThermostatInterval(50);
+                }
+                else {
+                  sim.integrator.setThermostatInterval(50/n);
+                }
+                
+                if (oldN < n) {
+                	 coordinateDefinition.initializeCoordinates(new int[]{(int)nSlider.getValue()});
+                }
+                oldN = n;
+                try {
+                    sim.integrator.reset();
+                }
+                catch (ConfigurationOverlapException e) {
+                    throw new RuntimeException(e);
+                }
+                resetAction.actionPerformed();
+                getDisplayBox(sim.box).repaint();
+            }
+            
+           // Boundary boundary = new BoundaryRectangularPeriodic(sim.getSpace(), sim.getRandom(), n/sim.density);
+        	//IBox box = new Box(sim, sim.getSpace());
+            //sim.box.setBoundary(boundary);
+        	CoordinateDefinition coordinateDefinition = sim.coordinateDefinition;
+               
+            int oldN = sim.box.getMoleculeList().getAtomCount();
+           
+        });
+        
+        add(nSlider);
+       
+		/*
+		 * Temperature Slider
+		 */
+		temperatureSetter = new DeviceThermoSlider(sim.getController());
+		temperatureSetter.setPrecision(1);
+		temperatureSetter.setIsothermal();
+		temperatureSetter.setMinimum(0.0);
+		temperatureSetter.setMaximum(5.0);
+		temperatureSetter.setTemperature(0.0);
+		temperatureSetter.setSliderMajorValues(4);
+		temperatureSetter.setUnit(Temperature.SIM_UNIT);
+		temperatureSetter.setIntegrator(sim.integrator);
+		
+        final IAction temperatureAction = new IAction() {
+            public void actionPerformed() {
+                resetAction.actionPerformed();
+		    }
+		};
+		ActionListener isothermalListener = new ActionListener() {
+		    public void actionPerformed(ActionEvent event) {
+		        temperatureAction.actionPerformed();
+		    }
+		};
+
+		temperatureSetter.setSliderPostAction(temperatureAction);
+        temperatureSetter.addRadioGroupActionListener(isothermalListener);
+		
+		add(temperatureSetter);
+		// end of Temperature Slider
+	}
+	
+	public static void main(String[] args){
+		
+		NormalModeAnalysisDisplay1D sim = new NormalModeAnalysisDisplay1D();
+		NormalModeAnalysisDisplay1DGraphic simGraphic = new NormalModeAnalysisDisplay1DGraphic(sim, Space.getInstance(1));
+		SimulationGraphic.makeAndDisplayFrame(simGraphic.getPanel(), APP_NAME);
+		
+	}
+	
+
+
+	
+	private DeviceThermoSlider temperatureSetter; 
+	private static final long serialVersionUID = 1L;
+	private static final String APP_NAME = "1-D Harmonic Oscillator";
+	private static final int REPAINT_INTERVAL = 20;
+	protected NormalModeAnalysisDisplay1D sim;
+	protected final IAction resetAction;
+
+}
