65bebdb29ed8b4b0b5f29e0d52dfd8b2cc6eeee0
Tai Boon Tan <taitan@buffalo.edu>
2008-12-12 21:02:52 +0000

    with Harmonic energy textbox

--- etomica.apps/etomica/normalmode/NormalModeAnalysisDisplay1DGraphic.java
+++ etomica.apps/etomica/normalmode/NormalModeAnalysisDisplay1DGraphic.java
@@ -1,10 +1,13 @@
 package etomica.normalmode;
 
 import etomica.api.IAction;
+import etomica.api.IData;
 import etomica.data.DataInfo;
 import etomica.data.IDataSink;
+import etomica.data.types.DataDouble;
 import etomica.data.types.DataDoubleArray;
 import etomica.data.types.DataTable;
+import etomica.data.types.DataDouble.DataInfoDouble;
 import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
 import etomica.data.types.DataTable.DataInfoTable;
 import etomica.exception.ConfigurationOverlapException;
@@ -12,10 +15,12 @@ import etomica.graphics.ColorSchemeRandom;
 import etomica.graphics.DeviceNSelector;
 import etomica.graphics.DeviceThermoSlider;
 import etomica.graphics.DisplayTable;
+import etomica.graphics.DisplayTextBox;
 import etomica.graphics.SimulationGraphic;
 import etomica.space.Boundary;
 import etomica.space.BoundaryRectangularPeriodic;
 import etomica.space.Space;
+import etomica.units.Energy;
 import etomica.units.Null;
 
 /**
@@ -30,6 +35,7 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
 		
 		super(simulation, TABBED_PANE, APP_NAME,REPAINT_INTERVAL, space, simulation.getController());
 		this.sim = simulation;
+		
 		
 		resetAction = getController().getSimRestart().getDataResetAction();
 		
@@ -48,8 +54,44 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
 		temperatureSetter.setSliderPostAction(new IAction() {
             public void actionPerformed() {
             	
-            	sim.nm.setTemperature(sim.temperature);
-            	resetAction.actionPerformed();
+            	sim.nm.setTemperature(sim.integrator.temperature);
+                
+                int m = sim.nm.getOmegaSquared(sim.box).length;
+                double[] omega2 = new double[m];
+                
+                for (int i=0; i<m; i++){
+                	omega2[i] = sim.nm.getOmegaSquared(sim.box)[i][0];
+                	stringWV[i]=String.valueOf(sim.waveVectorFactory.getWaveVectors()[i].x(0));
+                }
+                
+                data[0] = new DataDoubleArray(new int[]{m},omega2);
+                omega2Table = new DataTable(data);
+               
+                /*
+                 * Harmonic Energy
+                 */
+                AHarm = new DataDouble();
+                double AHarmonic =0;
+                
+                coeffs = sim.nm.getWaveVectorFactory().getCoefficients();
+                for(int i=0; i<omega2.length; i++) {
+                        if (!Double.isInfinite(omega2[i])) {
+                            AHarmonic += coeffs[i] * Math.log(omega2[i]*coeffs[i] /
+                                    (sim.integrator.temperature*Math.PI));
+                    }
+                }
+                AHarm.E(AHarmonic);
+                displayAHarmonic.putDataInfo(dataInfoA);
+                displayAHarmonic.putData(AHarm);
+                displayAHarmonic.repaint();
+                
+                DataInfoDoubleArray columnInfo = new DataInfoDoubleArray("Omega^2", Null.DIMENSION, new int[]{m});
+                DataInfo dataInfo = new DataInfoTable("Omega^2", new DataInfoDoubleArray[]{columnInfo}, m, stringWV);
+                sink.putDataInfo(dataInfo);
+                sink.putData(omega2Table);
+            	                
+                resetAction.actionPerformed();
+            	
 		    }
 		});
        
@@ -87,6 +129,22 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
         data[0] = new DataDoubleArray(new int[]{m},omega2);
         omega2Table = new DataTable(data);
         
+        /*
+         * Harmonic Energy
+         */
+        
+        AHarm = new DataDouble();
+        double AHarmonic =0;
+        
+        coeffs = sim.nm.getWaveVectorFactory().getCoefficients();
+        
+        for(int i=0; i<omega2.length; i++) {
+                if (!Double.isInfinite(omega2[i])) {
+                    AHarmonic += coeffs[i] * Math.log(omega2[i]*coeffs[i] /
+                            (sim.integrator.temperature*Math.PI));
+            }
+        }
+        AHarm.E(AHarmonic);
         
         nSlider.setPostAction(new IAction() {
         	
@@ -103,7 +161,8 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
                 oldN = n;
                 try {
                 	sim.integrator.reset();
-                	sim.nm.setTemperature(sim.temperature);
+                	
+                	sim.nm.setTemperature(sim.integrator.temperature);
                 	sim.integrator.setWaveVectors(sim.waveVectorFactory.getWaveVectors());
                     sim.integrator.setWaveVectorCoefficients(sim.waveVectorFactory.getCoefficients());
                     sim.integrator.setOmegaSquared(sim.nm.getOmegaSquared(sim.box), sim.waveVectorFactory.getCoefficients());
@@ -119,12 +178,33 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
                     
                     data[0] = new DataDoubleArray(new int[]{m},omega2);
                     omega2Table = new DataTable(data);
-                                       
+                   
+                    
+                    /*
+                     * Harmonic Energy
+                     */
+                    AHarm = new DataDouble();
+                    
+                    double AHarmonic =0;
+                    coeffs = sim.nm.getWaveVectorFactory().getCoefficients();
+                    
+                    for(int i=0; i<omega2.length; i++) {
+                            if (!Double.isInfinite(omega2[i])) {
+                                AHarmonic += coeffs[i] * Math.log(omega2[i]*coeffs[i] /
+                                        (sim.integrator.temperature*Math.PI));
+                        }
+                    }
+                    AHarm.E(AHarmonic);
+                    DataInfoDouble dataInfoA = new DataInfoDouble("AHarmonic", Energy.DIMENSION);
+                    displayAHarmonic.putDataInfo(dataInfoA);
+                    displayAHarmonic.putData(AHarm);
+                    displayAHarmonic.repaint();
+                    
                     DataInfoDoubleArray columnInfo = new DataInfoDoubleArray("Omega^2", Null.DIMENSION, new int[]{m});
                     DataInfo dataInfo = new DataInfoTable("Omega^2", new DataInfoDoubleArray[]{columnInfo}, m, stringWV);
                     sink.putDataInfo(dataInfo);
                     sink.putData(omega2Table);
-                 
+                                                        
                 }
                 
                 catch (ConfigurationOverlapException e) {
@@ -139,16 +219,18 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
         
         //end of N Slider
         
-        
-        
-        
-        
+
+        displayAHarmonic = new DisplayTextBox();
+        displayAHarmonic.setPrecision(10);
+        dataInfoA = new DataInfoDouble("AHarmonic", Energy.DIMENSION);
+        displayAHarmonic.putDataInfo(dataInfoA);
+        displayAHarmonic.putData(AHarm);
+        displayAHarmonic.setLabel("Harmonic Energy");
+      
         add(nSlider);
         add(temperatureSetter);
+    
 		
-        
-        
-        
         
         getDisplayBox(sim.box).setColorScheme(new ColorSchemeRandom(sim, sim.box,sim.getRandom()));
 			
@@ -162,15 +244,17 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
         displayTable.setTransposed(false);
         
         DataInfoDoubleArray columnInfo = new DataInfoDoubleArray("Omega^2", Null.DIMENSION, new int[]{m});
-        DataInfo dataInfo = new DataInfoTable("Omega^2", new DataInfoDoubleArray[]{columnInfo}, m, stringWV);
-        sink.putDataInfo(dataInfo);
+        DataInfo dataInfoTable = new DataInfoTable("Omega^2", new DataInfoDoubleArray[]{columnInfo}, m, stringWV);
+        sink.putDataInfo(dataInfoTable);
         sink.putData(omega2Table);
         
         getPanel().tabbedPane.add("Omega^2", displayTable.graphic());
         
         //
-		
+        add(displayAHarmonic);
 	}
+	
+	
 	
 	public static void main(String[] args){
 		Space sp = Space.getInstance(1);
@@ -204,6 +288,9 @@ public class NormalModeAnalysisDisplay1DGraphic extends SimulationGraphic {
 	protected final DisplayTable displayTable;
 	protected IDataSink sink;
 	protected String[] stringWV;
-	
+	protected double[] coeffs;
+	protected IData AHarm;
+	protected DisplayTextBox displayAHarmonic;
+	protected DataInfoDouble dataInfoA; 
 
 }
