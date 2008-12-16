6d6ba5858e0506f47e6fb69fe498a0a1ece5f69b
Nancy Cribbin <cribbin@buffalo.edu>
2008-12-16 21:59:14 +0000

    *** empty log message ***

--- etomica.apps/etomica/models/oneDHardRods/MeterCompareMultipleModesBrute.java
+++ etomica.apps/etomica/models/oneDHardRods/MeterCompareMultipleModesBrute.java
@@ -54,18 +54,87 @@ public class MeterCompareMultipleModesBrute extends DataSourceScalar {
         BasisCell cell = cells[0];
         uOld = new double[cells.length][coordinateDim];
         double normalization = 1/Math.sqrt(cells.length);
+        int numWV = comparedWVs.length;
+        double[] realCoord = new double[numWV];
+        double[] imagCoord = new double[numWV];
         energyHardRod = 0.0;
         energyHarmonic = 0.0;
         
         
+        //Get the normal mode coordinates of the compared waveVectors, and
+        // store them in realCoord and imagCoord for further use.
+        for(int wvcount = 0; wvcount < numWV; wvcount++){
+            coordinateDefinition.calcT(waveVectors[wvcount], realT, imagT);
+            realCoord[wvcount] = 0.0;
+            imagCoord[wvcount] = 0.0;
+            for(int i = 0; i < coordinateDim; i++){  //Loop would go away
+                for(int j = 0; j < coordinateDim; j++){
+                    realCoord[wvcount] += eigenVectors[wvcount][i][j] * realT[j];
+                    imagCoord[wvcount] += eigenVectors[wvcount][i][j] * imagT[j];
+                }
+            }
+        }
         
+        //Store the original positions
+        for(int i = 0; i < cells.length; i++){
+            uNow = coordinateDefinition.calcU(cells[i].molecules);
+            System.arraycopy(uNow, 0, uOld[i], 0, coordinateDim);
+        }
         
+        //Remove the effects of the compared modes.
+        for(int wvcount = 0; wvcount < numWV; wvcount++){
+            for(int iCell = 0; iCell < cells.length; iCell++){
+                cell = cells[iCell];
+                for(int j = 0; j < coordinateDim; j++){
+                    deltaU[j] = 0.0;
+                }
+                
+                //Calculate the contributions to the current position of the 
+                //zeroed mode, and subtract it from the overall position.
+                double kR = waveVectors[wvcount].dot(cell.cellPosition);
+                double coskR = Math.cos(kR);
+                double sinkR = Math.sin(kR);
+                for(int i = 0; i < coordinateDim; i++){  //Loop would go away
+                    //Calculate the current coordinates.
+                    for(int j = 0; j < coordinateDim; j++){
+                        deltaU[j] -= waveVectorCoefficients[wvcount] * 
+                            eigenVectors[wvcount][i][j] * 2.0 * (realCoord[wvcount] *
+                            coskR - imagCoord[wvcount]*sinkR);
+                    }
+                }
+
+                for(int i = 0; i < coordinateDim; i++){
+                    deltaU[i] *= normalization;
+                }
+                
+                for(int i = 0; i < coordinateDim; i++) {
+                    uNow[i] += deltaU[i];
+                }
+                coordinateDefinition.setToU(cells[iCell].molecules, uNow);
+            }
+        }
+        energyHardRod = meterPE.getDataAsScalar();
         
+        //Calculate the energy due to the compared modes
+        for(int wvcount = 0; wvcount < numWV; wvcount++){
+            for(int i = 0; i < coordinateDim; i++){  //Loop would go away
+                if(Double.isInfinite(omegaSquared[wvcount][i])){
+                    continue;
+                }
+                double normalCoord = realCoord[wvcount]*realCoord[wvcount] + 
+                    imagCoord[wvcount] * imagCoord[wvcount];
+                energyHarmonic += waveVectorCoefficients[wvcount] * normalCoord 
+                    * omegaSquared[wvcount][i];
+            }
+        }
         
+     // Set all the atoms back to the old values of u
+        for (int iCell = 0; iCell<cells.length; iCell++) {
+            cell = cells[iCell];
+            coordinateDefinition.setToU(cell.molecules, uOld[iCell]);
+        }
         
-        
-        
-        return 0.0;
+        return energyHardRod + energyHarmonic;
     }
     
     
