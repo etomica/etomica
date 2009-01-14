7e9a57eaeef8adf1d155f33e35757112b6a4f2a8
Andrew Schultz <ajs42@buffalo.edu>
2009-01-14 13:23:09 +0000

    drop reflection for formatter.  use templates

--- etomica/action/PDBWriter.java
+++ etomica/action/PDBWriter.java
@@ -1,11 +1,11 @@
 package etomica.action;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Formatter;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -65,39 +65,21 @@ public class PDBWriter implements IAction, Serializable {
         if (file == null) {
             throw new IllegalStateException("must call setFile or setFileName before actionPerformed");
         }
-        Class formatterClass;
+        Formatter formatter;
         try {
-            formatterClass = Class.forName("java.util.Formatter");
+            formatter = new Formatter(file);
         }
-        catch (ClassNotFoundException e) {
-            throw new RuntimeException("JRE 1.5 is required for PDBWriter.  Try XYZWriter instead");
-        }
-        Object formatter;
-        Method formatMethod;
-        try {
-            formatMethod = formatterClass.getDeclaredMethod("format",new Class[]{String.class,new Object[0].getClass()});
-            formatter = formatterClass.getDeclaredConstructor(new Class[]{File.class}).newInstance(new Object[]{file});
-        }
-        catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-        catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        }
-        catch (IllegalAccessException e) {
+        catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         int atomCount = 0;
         int nLeaf = leafList.getAtomCount();
         for (int iLeaf=0; iLeaf<nLeaf; iLeaf++) {
             IAtomPositioned atom = (IAtomPositioned)leafList.getAtom(iLeaf);
-            Iterator elementIterator = elementAtomType.iterator();
+            Iterator<ElementLinker> elementIterator = elementAtomType.iterator();
             int elementIndex = -1;
             while (elementIterator.hasNext()) {
-                ElementLinker thisElement = (ElementLinker)elementIterator.next();
+                ElementLinker thisElement = elementIterator.next();
                 if (thisElement.type == ((IAtomLeaf)atom).getType()) {
                     elementIndex = thisElement.elementIndex;
                 }
@@ -108,30 +90,11 @@ public class PDBWriter implements IAction, Serializable {
                 elementCount++;
                 elementAtomType.add(thisElement);
             }
-            try {
-                formatMethod.invoke(formatter,new Object[]{"ATOM%7d%3s                %8.3f%8.3f%8.3f\n", new Object[]{new Integer(atomCount), new Character(elements[elementIndex]), 
-                        new Double(atom.getPosition().x(0)), new Double(atom.getPosition().x(1)), new Double(atom.getPosition().x(2))}});
-            }
-            catch (InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-            catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
+            formatter.format("ATOM%7d%3s                %8.3f%8.3f%8.3f\n", new Object[]{new Integer(atomCount), new Character(elements[elementIndex]), 
+                    new Double(atom.getPosition().x(0)), new Double(atom.getPosition().x(1)), new Double(atom.getPosition().x(2))});
             atomCount++;
         }
-        try {
-            formatterClass.getDeclaredMethod("close",new Class[]{}).invoke(formatter,null);
-        }
-        catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-        catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        formatter.close();
     }
 
     /**
@@ -151,9 +114,9 @@ public class PDBWriter implements IAction, Serializable {
             throw new RuntimeException(e);
         }
         try {
-            Iterator elementIterator = elementAtomType.iterator();
+            Iterator<ElementLinker> elementIterator = elementAtomType.iterator();
             while (elementIterator.hasNext()) {
-                ElementLinker thisElement = (ElementLinker)elementIterator.next();
+                ElementLinker thisElement = elementIterator.next();
                 fileWriter.write("select elemno="+elementNum[thisElement.elementIndex]+"\n");
                 fileWriter.write("spacefill "+((IAtomTypeSphere)thisElement.type).getDiameter()*0.5);
             }
@@ -168,7 +131,7 @@ public class PDBWriter implements IAction, Serializable {
     private static final char[] elements = new char[] {'H', 'O', 'F', 'N', 'C', 'P', 'S'};
     private static final int[] elementNum = new int[] {1, 8, 9, 7, 6, 15, 16};
     private int elementCount = 0;
-    private final LinkedList elementAtomType = new LinkedList();
+    private final LinkedList<ElementLinker> elementAtomType = new LinkedList<ElementLinker>();
     private IAtomList leafList;
     
     private static final class ElementLinker implements Serializable {
