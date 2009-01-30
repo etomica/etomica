6c03aef697574dc14ac0bc0d35a5832dd0a1a675
Andrew Schultz <ajs42@buffalo.edu>
2009-01-30 15:49:46 +0000

    add some comments

--- etomica/api/IEventManager.java
+++ etomica/api/IEventManager.java
@@ -2,12 +2,21 @@ package etomica.api;
 
 public interface IEventManager {
 
+    /**
+     * Notifies listeners held by this event manager of the given event.
+     */
     public void fireEvent(IEvent event);
 
+    /**
+     * Adds the given listener to this event manager.
+     */
 	public void addListener(IListener listener);
 
 	public void addListener(IListener listener, boolean doSerialize);
 
+	/**
+	 * Removes the given listener from this event manager.
+	 */
 	public void removeListener(IListener listener);
 
 }
