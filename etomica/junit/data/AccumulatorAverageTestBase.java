91c63851a6250b746ab0eae1b56f9c0d952c4cd2
Andrew Schultz <ajs42@buffalo.edu>
2009-03-23 13:39:31 +0000

    add test that just dumps in a single value

--- etomica/junit/data/AccumulatorAverageTestBase.java
+++ etomica/junit/data/AccumulatorAverageTestBase.java
@@ -18,19 +18,19 @@ public abstract class AccumulatorAverageTestBase extends TestCase {
 
         assertTrue(accumulator.getData().isNaN());
         assertEquals(accumulator.getBlockCount(), 0);
-        
+
         simpleTest();
-        
+
         accumulator.reset();
         assertTrue(accumulator.getData().isNaN());
         assertEquals(accumulator.getBlockCount(), 0);
-        
+
         simpleTest();
-        
+
         accumulator.putDataInfo(new DataDouble.DataInfoDouble("test", Null.DIMENSION));
         assertTrue(accumulator.getData().isNaN());
         assertEquals(accumulator.getBlockCount(), 0);
-        
+
         simpleTest();
     }
 
@@ -43,21 +43,44 @@ public abstract class AccumulatorAverageTestBase extends TestCase {
             rawData.x = rng.nextDouble();
             accumulator.putData(rawData);
         }
-        
+
         IData accData = accumulator.getData();
         double avg = accData.getValue(AccumulatorAverage.StatType.AVERAGE.index);
         assertTrue(Math.abs(avg-0.5) < 0.01);
-        
+
         double blockCorrelation = accData.getValue(AccumulatorAverage.StatType.BLOCK_CORRELATION.index);
         // block correlation should be ~0, but actual value will depend on # of blocks 
         assertTrue(Math.abs(blockCorrelation) < 3.0/Math.sqrt(accumulator.getBlockCount()));
-        
+
         double stdev = accData.getValue(AccumulatorAverage.StatType.STANDARD_DEVIATION.index);
         assertTrue(Math.abs(stdev-Math.sqrt(1.0/12.0)) < 5.e-4);
 
         double error = accData.getValue(AccumulatorAverage.StatType.ERROR.index);
         assertTrue(error/2.9e-4 + 2.9e-4/error - 2 < 0.02);
     }
-    
+
+    public void testSingleValue() {
+        accumulator.putDataInfo(new DataDouble.DataInfoDouble("test", Null.DIMENSION));
+
+        DataDouble rawData = new DataDouble();
+        rawData.x = 5.6;
+        for (int i=0; i<12345; i++) {
+            accumulator.putData(rawData);
+        }
+        
+        IData accData = accumulator.getData();
+        double avg = accData.getValue(AccumulatorAverage.StatType.AVERAGE.index);
+        assertTrue(Math.abs(avg-rawData.x) < 1.e-10);
+        
+        accData.getValue(AccumulatorAverage.StatType.BLOCK_CORRELATION.index);
+        // block correlation should be 0/0, actual value might be 0, some number, Infinity or NaN 
+        
+        double stdev = accData.getValue(AccumulatorAverage.StatType.STANDARD_DEVIATION.index);
+        assertTrue(stdev < 1.e-7);
+
+        double error = accData.getValue(AccumulatorAverage.StatType.ERROR.index);
+        assertTrue(error < 1.e-6);
+    }
+
     protected final AccumulatorAverage accumulator;
 }
