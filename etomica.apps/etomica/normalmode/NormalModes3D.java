e3bd5920216e68efa067ac3207ac0b8b775ddcee
Tai Boon Tan <taitan@buffalo.edu>
2009-04-02 23:45:27 +0000

    *** empty log message ***

--- etomica.apps/etomica/normalmode/NormalModes3D.java
+++ etomica.apps/etomica/normalmode/NormalModes3D.java
@@ -34,47 +34,12 @@ public class NormalModes3D implements NormalModes {
     			omega2[i][j] = 1.0/eigenvalues[i][j];
                }
     	}
-    	
-    	/*
-    	 *  set all the eigenvalues to 0.001
-    	 *  omega2 = 1/eigenvalue
-    	 */
-    	/*
-        for (int i=0; i<omega2.length; i++) {
-            for (int j=0; j<omega2[i].length; j++) {
-            	if (i==0 && j<12){
-            		omega2[i][j] = Double.POSITIVE_INFINITY;
-            		
-            	} else {
-            		omega2[i][j] = 1000*temperature;
-            		
-            	}
-            }
-        }
-        */
-    	
-    	
+
         return omega2;
     }
 
     public double[][][] getEigenvectors(IBox box) {
     	eigenvectors = ArrayReader2D.getFromFile("DB_FCC_n12_N"+getNCellNum()+".vec");
-    	/*
-    	for(int i=0; i<numWV; i++){
-    		for(int j=0; j<numEigen; j++){
-    			for(int k=0; k<numEigen; k++){
-    				if(j==k){
-    					eigenvectors[i][j][k] = 0.8;
-    					
-    				} else {
-    					eigenvectors[i][j][k] = 0.0;
-    					
-    				}
-    			}
-    		}
-    	}
-    	*/
-    	
         return eigenvectors;
     }
                       
