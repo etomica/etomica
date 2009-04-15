e502ccb8e98dbcf1d48a82a5633939254c3d6aac
Tai Boon Tan <taitan@buffalo.edu>
2009-04-15 17:20:30 +0000

    determine the normal mode coordinate

--- etomica.apps/etomica/normalmode/MeterHarmonicCoordinate.java
+++ etomica.apps/etomica/normalmode/MeterHarmonicCoordinate.java
@@ -1,30 +1,29 @@
 package etomica.normalmode;
 
-import etomica.api.IBox;
+import java.util.Arrays;
+
 import etomica.api.IData;
-import etomica.api.IMoleculeList;
+import etomica.api.IVector;
+import etomica.data.DataSourceScalar;
 import etomica.data.DataTag;
 import etomica.data.IEtomicaDataInfo;
-import etomica.data.IEtomicaDataSource;
-import etomica.data.types.DataDoubleArray;
-import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
-import etomica.normalmode.CoordinateDefinition.BasisCell;
-import etomica.units.Energy;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataDouble.DataInfoDouble;
+import etomica.units.Length;
 
 /**
- * Meter that calculates the Boltzmann-factored harmonic energy of each normal mode for a 
- * configuration given eigenvectors and omegas corresponding to wave vectors.
  * 
- * @author Andrew Schultz
  */
-public class MeterHarmonicCoordinate implements IEtomicaDataSource {
+ 
+public class MeterHarmonicCoordinate extends DataSourceScalar {
 
-    public MeterHarmonicCoordinate(CoordinateDefinition coordinateDefinition, NormalModes normalModes, int[] modes) {
-        this.coordinateDefinition = coordinateDefinition;
-        this.normalModes = normalModes;
-        dataInfo = new DataInfoDoubleArray("Harmonic single energy", Energy.DIMENSION, new int[]{0});
-        tag = new DataTag();
-        this.modes = modes;
+    public MeterHarmonicCoordinate(CoordinateDefinition coordinateDefinition) {
+    	super("Normal Mode Coordinate", Length.DIMENSION);
+    	
+        this.coordinateDefinition = coordinateDefinition;        
+        realT = new double[coordinateDefinition.getCoordinateDim()];
+        imaginaryT = new double[coordinateDefinition.getCoordinateDim()];
+        
     }
     
     public DataTag getTag() {
@@ -40,57 +39,38 @@ public class MeterHarmonicCoordinate implements IEtomicaDataSource {
     }
     
 
-    public IData getData() {
-        BasisCell cell = coordinateDefinition.getBasisCells()[0];
-        int coordinateDim = coordinateDefinition.getCoordinateDim();
-
-        IMoleculeList molecules = cell.molecules;
-        double[] u = coordinateDefinition.calcU(molecules);
-        double sqrtCells = Math.sqrt(coordinateDefinition.getBasisCells().length);
-
-        double[] x = data.getData();
+    public double getDataAsScalar() {
+    	
+    	int coordinateDim = coordinateDefinition.getCoordinateDim();
+    	
+    	double realCoord =0;
+    	double imaginaryCoord =0;
         
-        for (int i=0; i<modes.length; i++) {
-            double realCoord = 0;
-            for (int j=0; j<coordinateDim; j++) {
-                realCoord += u[j] * eigenvectors[0][modes[i]][j] * sqrtCells;
-            }
-            x[i] = realCoord;
-        }
-        return data;
-    }
-    
-    public IBox getBox() {
-        return coordinateDefinition.getBox();
+    	coordinateDefinition.calcT(waveVector, realT, imaginaryT);
+    	
+    	for (int i=0; i<coordinateDim; i++) {
+    		realCoord += realT[i] * eigenvectors[i];
+    		imaginaryCoord += imaginaryT[i] * eigenvectors[i];
+    	}
+    	double QCoord = Math.sqrt(realCoord*realCoord + imaginaryCoord*imaginaryCoord);
+    	return realCoord > 0 ? QCoord : -QCoord; 
+       
     }
 
-    public void setBox(IBox newBox) {
-        normalModes.getWaveVectorFactory().makeWaveVectors(newBox);
-        setEigenvectors(normalModes.getEigenvectors(newBox));
+    
+    public void setEigenvectors(double[] eigenVectors) {
+        this.eigenvectors = eigenVectors;
+    }
+    
+    public void setWaveVector(IVector waveVector){
+    	this.waveVector = waveVector;
+    } 
 
-        dataInfo = new DataInfoDoubleArray("Harmonic Coordinates", Energy.DIMENSION, new int[]{modes.length});
-        data = new DataDoubleArray(new int[]{modes.length});
-    }
     
-    public void setEigenvectors(double[][][] eigenVectors) {
-        this.eigenvectors = (double[][][])eigenVectors.clone();
-    }
-    
-    public void setName(String newName) {
-        name = newName;
-    }
-    
-    public String getName() {
-        return name;
-    }
     
     private static final long serialVersionUID = 1L;
     protected final CoordinateDefinition coordinateDefinition;
-    protected DataInfoDoubleArray dataInfo;
-    protected DataDoubleArray data;
-    private final DataTag tag;
-    protected double[][][] eigenvectors;
-    protected String name;
-    protected NormalModes normalModes;
-    protected int[] modes;
+    protected double[] eigenvectors;
+    protected double[] realT, imaginaryT;
+    protected IVector waveVector;
 }
