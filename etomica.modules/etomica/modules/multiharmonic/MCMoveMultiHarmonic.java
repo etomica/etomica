f096f5ffe68f5f7fcf35b5674f9a93db7b1ccfce
Andrew Schultz <ajs42@buffalo.edu>
2009-05-08 18:00:23 +0000

    add MCMove designed for multiharmonic system

---
+++ etomica.modules/etomica/modules/multiharmonic/MCMoveMultiHarmonic.java
@@ -0,0 +1,82 @@
+package etomica.modules.multiharmonic;
+
+import etomica.api.IAtomList;
+import etomica.api.IAtomPositioned;
+import etomica.api.IBox;
+import etomica.api.IRandom;
+import etomica.api.IVector;
+import etomica.api.IVectorMutable;
+import etomica.atom.iterator.AtomIterator;
+import etomica.atom.iterator.AtomIteratorLeafAtoms;
+import etomica.integrator.mcmove.MCMoveBox;
+import etomica.potential.P1Harmonic;
+
+/**
+ * MCMove which moves all atoms.  New coordinates are taken from a Gaussian
+ * distribution with width and center taken to match the P1Harmonic that
+ * governs the atom's potential.  All moves are accepted.
+ * 
+ * @author Andrew Schultz
+ */
+public class MCMoveMultiHarmonic extends MCMoveBox {
+
+    public MCMoveMultiHarmonic(P1Harmonic p1, IRandom random) {
+        super(null);
+        this.p1 = p1;
+        iterator = new AtomIteratorLeafAtoms();
+        this.random = random;
+    }
+    
+    public void setBox(IBox newBox) {
+        super.setBox(newBox);
+        iterator.setBox(box);
+    }
+
+    public AtomIterator affectedAtoms() {
+        return iterator;
+    }
+
+    public double energyChange() {
+        return uNew-uOld;
+    }
+
+    public void acceptNotify() {
+    }
+
+    public boolean doTrial() {
+        uOld = uNew;
+        double s = p1.getSpringConstant();
+        IVector x0 = p1.getX0();
+        IAtomList atoms = box.getLeafList();
+        uNew = 0;
+        double sqrtS = Math.sqrt(s);
+        for (int i=0; i<atoms.getAtomCount(); i++) {
+            IVectorMutable p = ((IAtomPositioned)atoms.getAtom(i)).getPosition();
+            for (int j=0; j<p.getD(); j++) {
+                double r = random.nextGaussian()/sqrtS;
+                uNew += 0.5*r*r*s;
+                p.setX(j, r);
+            }
+            p.PE(x0);
+        }
+        return true;
+    }
+
+    public double getA() {
+        return 1;
+    }
+
+    public double getB() {
+        return 0;
+    }
+
+    public void rejectNotify() {
+        throw new RuntimeException("oops");
+    }
+
+    private static final long serialVersionUID = 1L;
+    protected final P1Harmonic p1;
+    protected final AtomIteratorLeafAtoms iterator;
+    protected final IRandom random;
+    protected double uOld, uNew;
+}
