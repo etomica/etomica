918e45ff00d094e0222ea167f4c384ac136f74fb
Andrew Schultz <ajs42@buffalo.edu>
2009-06-10 23:01:31 +0000

    "oops"

--- etomica/exception/ConfigurationOverlapException.java
+++ etomica/exception/ConfigurationOverlapException.java
@@ -1,8 +1,6 @@
 package etomica.exception;
 
-import etomica.api.IAtomList;
 import etomica.api.IBox;
-import etomica.atom.Atom;
 
 /**
  * Exception thrown when an overlap is detected in the configuration of
@@ -18,15 +16,6 @@ public class ConfigurationOverlapException extends RuntimeException {
     public ConfigurationOverlapException(IBox box) {
         super("Overlap in configuration "+box);
         this.box = box;
-
-        IAtomList list = box.getLeafList();
-        for(int i = 0; i < 32; i++){
-            System.out.println("atom " + i);
-            for(int j = 0; j < 3; j++){
-                System.out.println(       ((Atom)list.getAtom(i)).getPosition().x(j)     );
-            }
-        }
-        throw new IllegalStateException("Dude!");
     }
 
     private static final long serialVersionUID = 1L;
