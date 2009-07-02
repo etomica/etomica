b6b460e4a40463c13dcdd9a1b0b45c74224c59c5
Nancy Cribbin <cribbin@buffalo.edu>
2009-07-02 23:00:53 +0000

    Dave's new idea!

---
+++ etomica.apps/etomica/models/oneDHardRods/SimDegreeFreedom.java
@@ -0,0 +1,170 @@
+package etomica.models.oneDHardRods;
+
+import etomica.action.activity.ActivityIntegrate;
+import etomica.api.IAtomType;
+import etomica.api.IBox;
+import etomica.atom.AtomTypeSphere;
+import etomica.box.Box;
+import etomica.data.DataSourceScalar;
+import etomica.integrator.IntegratorHard;
+import etomica.integrator.IntegratorMD;
+import etomica.lattice.crystal.Basis;
+import etomica.lattice.crystal.BasisCubicFcc;
+import etomica.lattice.crystal.BasisMonatomic;
+import etomica.lattice.crystal.Primitive;
+import etomica.lattice.crystal.PrimitiveCubic;
+import etomica.normalmode.CoordinateDefinition;
+import etomica.normalmode.CoordinateDefinitionLeaf;
+import etomica.normalmode.WaveVectorFactory;
+import etomica.normalmode.WaveVectorFactory1D;
+import etomica.normalmode.WaveVectorFactorySimple;
+import etomica.normalmode.WriteS;
+import etomica.potential.P1HardPeriodic;
+import etomica.potential.P2HardSphere;
+import etomica.potential.Potential;
+import etomica.potential.PotentialMaster;
+import etomica.potential.PotentialMasterMonatomic;
+import etomica.simulation.Simulation;
+import etomica.space.Boundary;
+import etomica.space.BoundaryDeformableLattice;
+import etomica.space.BoundaryRectangularPeriodic;
+import etomica.space.Space;
+import etomica.species.SpeciesSpheresMono;
+import etomica.util.HistogramSimple;
+
+/**
+ * MD simulation of hard spheres in 1D or 3D with tabulation of the
+ * collective-coordinate S-matrix. No graphic display of simulation.
+ */
+public class SimDegreeFreedom extends Simulation {
+
+    public SimDegreeFreedom(Space _space, int numAtoms, double density) {
+        super(_space, true);
+        PotentialMaster potentialMaster = new PotentialMasterMonatomic(this);
+
+        SpeciesSpheresMono species = new SpeciesSpheresMono(this, space);
+        getSpeciesManager().addSpecies(species);
+
+        box = new Box(space);
+        addBox(box);
+        box.setNMolecules(species, numAtoms);
+
+        integrator = new IntegratorHard(this, potentialMaster, space);
+        integrator.setTimeStep(0.04);
+        integrator.setTemperature(1.0);
+
+        integrator.setIsothermal(false);
+        activityIntegrate = new ActivityIntegrate(integrator);
+        getController().addAction(activityIntegrate);
+        // activityIntegrate.setMaxSteps(nSteps);
+
+        Potential potential = new P2HardSphere(space, 1.0, false);
+        AtomTypeSphere sphereType = (AtomTypeSphere)species.getLeafType();
+        potentialMaster.addPotential(potential, new IAtomType[] { sphereType,
+                sphereType });
+
+        int nCells;
+        Basis basis;
+        if (space.D() == 1) {
+            primitive = new PrimitiveCubic(space, 1.0/density);
+            nCells = numAtoms;
+            bdry = new BoundaryRectangularPeriodic(space, numAtoms/density);
+            ((IntegratorHard) integrator).setNullPotential(new P1HardPeriodic(space), sphereType);
+            basis = new BasisMonatomic(space);
+        } else {
+            primitive = new PrimitiveCubic(space, 1);
+            double v = primitive.unitCell().getVolume();
+            primitive.scaleSize(Math.pow(v*density/4,-1.0/3.0));
+            nCells = (int)Math.round(Math.pow(numAtoms/4, 1.0/3.0));
+            bdry = new BoundaryDeformableLattice(primitive, new int[]{nCells,nCells,nCells});
+            basis = new BasisCubicFcc();
+        }
+        box.setBoundary(bdry);
+
+        coordinateDefinition = new CoordinateDefinitionLeaf(this, box, primitive, basis, space);
+        coordinateDefinition.initializeCoordinates(new int[]{nCells, nCells, nCells});
+        
+        integrator.setBox(box);
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // defaults
+        int D = 1;
+        int nA = 32;
+        double density = 1.3;
+        if (D == 1) {
+            nA = 3;
+            density = 0.5;
+        }
+
+        double simTime = 10000;
+
+        // parse arguments
+        if (args.length > 1) {
+            density = Double.parseDouble(args[1]);
+        }
+        if (args.length > 2) {
+            simTime = Double.parseDouble(args[2]);
+        }
+        if (args.length > 3) {
+            nA = Integer.parseInt(args[3]);
+        }
+        String filename = "normal_modes" + D + "D_"+nA+"_"+((int)(density*100))+"_cubic";
+        if (args.length > 0) {
+            filename = args[0];
+        }
+
+        System.out.println("Running "
+                + (D == 1 ? "1D" : (D == 3 ? "FCC" : "2D hexagonal"))
+                + " hard sphere simulation");
+        System.out.println(nA + " atoms at density " + density);
+        System.out.println(simTime + " time units");
+        System.out.println("output data to " + filename);
+
+        // construct simulation
+        SimDegreeFreedom sim = new SimDegreeFreedom(Space.getInstance(D), nA, density);
+
+        Primitive primitive = sim.primitive;
+
+        WaveVectorFactory waveVectorFactory;
+        if (D == 1) {
+            waveVectorFactory = new WaveVectorFactory1D();
+        } else if (D == 2) {
+            waveVectorFactory = null;
+        } else {
+            waveVectorFactory = new WaveVectorFactorySimple(primitive, sim.space);
+        }
+
+        // start simulation
+        int nSteps = (int) (simTime / sim.integrator.getTimeStep());
+        sim.activityIntegrate.setMaxSteps(nSteps/10);
+        sim.getController().actionPerformed();
+        System.out.println("equilibration finished");
+        sim.getController().reset();
+        
+        sim.activityIntegrate.setMaxSteps(nSteps);
+        sim.getController().actionPerformed();
+
+        WriteS sWriter = new WriteS(sim.space);
+        sWriter.setFilename(filename);
+        sWriter.setWaveVectorFactory(waveVectorFactory);
+        sWriter.setOverwrite(true);
+        sWriter.actionPerformed();
+
+    }
+
+    private static final long serialVersionUID = 1L;
+    public IntegratorMD integrator;
+    public ActivityIntegrate activityIntegrate;
+    public IBox box;
+    public Boundary bdry;
+    public Primitive primitive;
+    public CoordinateDefinition coordinateDefinition;
+    HistogramSimple histogramRealCoord, histogramImagCoord;
+//    MeterNormalModeCoordinate meternmc;
+    
+}
