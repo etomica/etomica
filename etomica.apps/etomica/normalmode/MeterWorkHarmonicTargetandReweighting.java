9e0a6916366108897bba13581d86d42ccb6c3d62
Tai Boon Tan <taitan@buffalo.edu>
2009-08-03 21:27:22 +0000

    Reweighted Sampling

---
+++ etomica.apps/etomica/normalmode/MeterWorkHarmonicTargetandReweighting.java
@@ -0,0 +1,171 @@
+package etomica.normalmode;
+
+import etomica.data.DataSourceIndependentSimple;
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataFunction;
+import etomica.data.types.DataDouble.DataInfoDouble;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.data.types.DataFunction.DataInfoFunction;
+import etomica.potential.PotentialMaster;
+import etomica.units.Energy;
+import etomica.units.Null;
+import etomica.util.DoubleRange;
+import etomica.util.HistogramReweightedData;
+import etomica.util.HistogramSimple;
+
+/**
+ * Meter used for overlap sampling in the target-sampled system.  The meter
+ * measures the energy difference for the harmonic and target
+ * potentials.
+ * 
+ * the sampling of <beta*U_BA>B
+ * gives us the information of
+ * 	a. < beta*U_AW >A : notation ---- [ betaUBAf_avg ]
+ * 	b. < beta*U_AW >W : notation ---- [ betaUBAr_avg ] 
+ *  ---------- U_AW = U_A - U_W
+ *  
+ * output the probability distribution of the histogram for a. and b.
+ * 
+ * Key:
+ * 	a. eA = e^(-beta*U_A)
+ * 	b. eB = e^(-beta*U_B)
+ * 	c. eW = e^(-beta*U_W) 
+ * 
+ * @author Tai Boon Tan
+ */
+public class MeterWorkHarmonicTargetandReweighting implements IEtomicaDataSource {
+    
+    public MeterWorkHarmonicTargetandReweighting(MCMoveHarmonic mcMoveHarmonic, PotentialMaster potentialMaster, double ref) {
+    	this.mcMoveHarmonic = mcMoveHarmonic;
+        meterEnergy = new MeterPotentialEnergy(potentialMaster);
+        meterEnergy.setBox(mcMoveHarmonic.getBox());
+        this.refPref = ref;
+        data = new DataDouble();
+        dataInfo = new DataInfoDouble("Scaled Harmonic and soft sphere Energies", Null.DIMENSION);
+        histogramUAWf = new HistogramSimple(4000, new DoubleRange(-200, 200));
+        histogramUAWr = new HistogramReweightedData(4000, new DoubleRange(-200, 200));
+        nSamples = 0;
+        numSum = 0;
+        denomSum = 0;
+        betaUAWf = 0;        
+        
+        tag = new DataTag();
+    }
+
+    public IData getData() {
+    	betaUAB =	((meterEnergy.getDataAsScalar() - latticeEnergy) -
+        		mcMoveHarmonic.getLastTotalEnergy()) / temperature;
+    	double eA = Math.exp(-mcMoveHarmonic.getLastTotalEnergy()/temperature);
+    	double eB = Math.exp(-(meterEnergy.getDataAsScalar() - latticeEnergy) / temperature);
+        double eW = (eA*eB)/(eB+refPref*eA);
+    	
+    	double eAB = Math.exp(betaUAB);	
+    	
+        /*
+         * Histogram Transformation
+         */
+        
+        // < beta*U_AW >A
+        betaUAW = Math.log(1 + refPref*eAB);
+        
+        betaUAWf += betaUAW;
+        nSamples++;
+        
+    	histogramUAWf.addValue(betaUAW);
+    	
+    	// < beta*U_AW >W
+    	if (eA < 1e-100){
+    		numSum += eB/(refPref*eA)*100;
+    	} else {
+    		numSum += betaUAW*(eW / eA);    // ( beta*U_AW )*(eW/eA)
+    	}
+    	
+    	denomSum += (eW/eA); // eW/eA
+    	
+    	histogramUAWr.addValue(betaUAW, (eW/eA));
+    	
+    	data.x = betaUAB;
+    	return data;
+    }
+
+    public void setLatticeEnergy(double newLatticeEnergy) {
+        latticeEnergy = newLatticeEnergy;
+    }
+    
+    public void setTemperature(double newTemperature) {
+        temperature = newTemperature;
+    }
+    
+    public double getTemperature() {
+        return temperature;
+    }
+    
+    public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+
+    public DataTag getTag() {
+        return tag;
+    }
+    
+    
+    public IData getDataHistogramBetaUAWf(){
+    	dataHistogram = new DataFunction(new int[]{histogramUAWf.getNBins()}, histogramUAWf.getHistogram());
+    	return dataHistogram;
+    }
+    
+    public IEtomicaDataInfo getDataInfoHistogramBetaUAWf(){
+    	DataInfoDoubleArray independentInfo = new DataInfoDoubleArray("Energy", Energy.DIMENSION, new int[]{histogramUAWf.getNBins()});
+    	xDataSource = new DataSourceIndependentSimple(histogramUAWf.xValues(), independentInfo);
+    	dataInfoHistogram = new DataInfoFunction("Energy Histogram", Null.DIMENSION,xDataSource);
+    	return dataInfoHistogram;
+    }
+    
+    public IData getDataHistogramBetaUAWr(){
+    	dataHistogram = new DataFunction(new int[]{histogramUAWr.getNBins()}, histogramUAWr.getHistogram());
+    	return dataHistogram;
+    }
+    
+    public IEtomicaDataInfo getDataInfoHistogramBetaUAWr(){
+    	DataInfoDoubleArray independentInfo = new DataInfoDoubleArray("Energy", Energy.DIMENSION, new int[]{histogramUAWr.getNBins()});
+    	xDataSource = new DataSourceIndependentSimple(histogramUAWr.xValues(), independentInfo);
+    	dataInfoHistogram = new DataInfoFunction("Energy Histogram", Null.DIMENSION,xDataSource);
+    	return dataInfoHistogram;
+    }
+    
+    public double getBetaUAWf(){
+    	return betaUAWf/nSamples;
+    	
+    }
+    
+    public double getBetaUAWr(){
+    	return numSum/denomSum;
+    	
+    }
+    
+    
+    protected double temperature;
+    protected final MeterPotentialEnergy meterEnergy;
+    protected final MCMoveHarmonic mcMoveHarmonic;
+    protected final DataDouble data;
+    protected final DataInfoDouble dataInfo;
+    protected final DataTag tag;
+    protected double latticeEnergy;
+    protected double betaUAW, betaUAB; 
+    protected double betaUAWf;
+    protected final double refPref;
+    protected HistogramSimple histogramUAWf;
+    protected HistogramReweightedData histogramUAWr;
+    protected double nSamples;
+    protected double numSum, denomSum;
+    
+    protected DataSourceIndependentSimple xDataSource;
+    private DataFunction dataHistogram;
+    private DataInfoFunction dataInfoHistogram;
+    
+}
---
+++ etomica.apps/etomica/normalmode/MeterWorkTargetHarmonicandReweighting.java
@@ -0,0 +1,163 @@
+package etomica.normalmode;
+
+import etomica.data.DataSourceIndependentSimple;
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.meter.MeterPotentialEnergyFromIntegrator;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataFunction;
+import etomica.data.types.DataDouble.DataInfoDouble;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.data.types.DataFunction.DataInfoFunction;
+import etomica.integrator.IntegratorBox;
+import etomica.units.Energy;
+import etomica.units.Null;
+import etomica.util.DoubleRange;
+import etomica.util.HistogramReweightedData;
+import etomica.util.HistogramSimple;
+
+/**
+ * Meter used for overlap sampling in the target-sampled system.  The meter
+ * measures the energy difference for the harmonic and target
+ * potentials.
+ * 
+ * the sampling of <beta*U_BA>B
+ * gives us the information of
+ * 	a. < beta*U_BW >B : notation ---- [ betaUBWf_avg ]
+ * 	b. < beta*U_BW >W : notation ---- [ betaUBWr_avg ] 
+ *  ---------- U_BW = U_B - U_W
+ *  
+ * output the probability distribution of the histogram for a. and b.
+ * 
+ * Key:
+ * 	a. eA = e^(-beta*U_A)
+ * 	b. eB = e^(-beta*U_B)
+ * 	c. eW = e^(-beta*U_W) 
+ * 
+ * @author Tai Boon Tan
+ */
+public class MeterWorkTargetHarmonicandReweighting implements IEtomicaDataSource {
+    
+    public MeterWorkTargetHarmonicandReweighting(IntegratorBox integrator, MeterHarmonicEnergy meterHarmonicEnergy, double ref) {
+        meterEnergy = new MeterPotentialEnergyFromIntegrator(integrator);
+        this.integrator = integrator;
+        this.meterHarmonicEnergy = meterHarmonicEnergy;
+        this.refPref = ref;
+        data = new DataDouble();
+        dataInfo = new DataInfoDouble("Scaled Harmonic and hard sphere Energies", Null.DIMENSION);
+        histogramUBWf = new HistogramSimple(4000, new DoubleRange(-200, 200));
+        histogramUBWr = new HistogramReweightedData(4000, new DoubleRange(-200, 200));
+        nSamples = 0;
+        numSum = 0;
+        denomSum = 0;
+        betaUBWf = 0;
+        
+        tag = new DataTag();
+    }
+
+    public IData getData() {
+    	betaUBA =	(meterHarmonicEnergy.getDataAsScalar() -
+                	(meterEnergy.getDataAsScalar() - latticeEnergy)) / integrator.getTemperature();
+    	double eA = Math.exp(-meterHarmonicEnergy.getDataAsScalar()/integrator.getTemperature());
+    	double eB = Math.exp(-(meterEnergy.getDataAsScalar() - latticeEnergy) / integrator.getTemperature());
+        double eW = eA/(1+refPref*(eA/eB));
+    	
+    	double eBA = Math.exp(betaUBA);	
+    	
+        /*
+         * Histogram Transformation
+         */
+        
+        // < beta*U_BW >B
+        betaUBW = Math.log(eBA+ refPref);
+        
+        betaUBWf += betaUBW;
+        nSamples++;
+        
+    	histogramUBWf.addValue(betaUBW);
+    	
+    	// < beta*U_BW >W
+    	if (eA < 1e-60){
+    		numSum += (eA/eB)*100;
+    	} else {
+    		numSum += betaUBW*(eA / (eB + refPref*eA));    // ( beta*U_AW )*(eW/eB)
+    	}
+    	
+    	denomSum += eA /(eB + refPref*eA); // eW/eB
+    	
+    	histogramUBWr.addValue(betaUBW, (eW/eB));
+    	
+    	data.x = betaUBA;
+    	return data;
+    }
+
+    public void setLatticeEnergy(double newLatticeEnergy) {
+        latticeEnergy = newLatticeEnergy;
+    }
+    
+    public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+
+    public DataTag getTag() {
+        return tag;
+    }
+    
+    
+    public IData getDataHistogramBetaUBWf(){
+    	dataHistogram = new DataFunction(new int[]{histogramUBWf.getNBins()}, histogramUBWf.getHistogram());
+    	return dataHistogram;
+    }
+    
+    public IEtomicaDataInfo getDataInfoHistogramBetaUBWf(){
+    	DataInfoDoubleArray independentInfo = new DataInfoDoubleArray("Energy", Energy.DIMENSION, new int[]{histogramUBWf.getNBins()});
+    	xDataSource = new DataSourceIndependentSimple(histogramUBWf.xValues(), independentInfo);
+    	dataInfoHistogram = new DataInfoFunction("Energy Histogram", Null.DIMENSION,xDataSource);
+    	return dataInfoHistogram;
+    }
+    
+    public IData getDataHistogramBetaUBWr(){
+    	dataHistogram = new DataFunction(new int[]{histogramUBWr.getNBins()}, histogramUBWr.getHistogram());
+    	return dataHistogram;
+    }
+    
+    public IEtomicaDataInfo getDataInfoHistogramBetaUBWr(){
+    	DataInfoDoubleArray independentInfo = new DataInfoDoubleArray("Energy", Energy.DIMENSION, new int[]{histogramUBWr.getNBins()});
+    	xDataSource = new DataSourceIndependentSimple(histogramUBWr.xValues(), independentInfo);
+    	dataInfoHistogram = new DataInfoFunction("Energy Histogram", Null.DIMENSION,xDataSource);
+    	return dataInfoHistogram;
+    }
+    
+    public double getBetaUBWf(){
+    	return betaUBWf/nSamples;
+    	
+    }
+    
+    public double getBetaUBWr(){
+    	return numSum/denomSum;
+    	
+    }
+    
+    
+    protected final MeterPotentialEnergyFromIntegrator meterEnergy;
+    protected final MeterHarmonicEnergy meterHarmonicEnergy;
+    protected final IntegratorBox integrator;
+    protected final DataDouble data;
+    protected final DataInfoDouble dataInfo;
+    protected final DataTag tag;
+    protected double latticeEnergy;
+    protected double betaUBW, betaUBA; 
+    protected double betaUBWf;
+    protected final double refPref;
+    protected HistogramSimple histogramUBWf;
+    protected HistogramReweightedData histogramUBWr;
+    protected double nSamples;
+    protected double numSum, denomSum;
+    
+    protected DataSourceIndependentSimple xDataSource;
+    private DataFunction dataHistogram;
+    private DataInfoFunction dataInfoHistogram;
+    
+}
