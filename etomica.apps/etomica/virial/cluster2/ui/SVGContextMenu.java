1e1ac546c4d5b102423d9cabb6388c397b1b8e2a
Demien Lessa <dlessa@buffalo.edu>
2009-08-03 13:04:08 +0000

    completed the support for SVG drawing, interaction, and exporting using the Batik library.

---
+++ etomica.apps/etomica/virial/cluster2/ui/SVGContextMenu.java
@@ -0,0 +1,295 @@
+package etomica.virial.cluster2.ui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+
+import org.apache.batik.apps.rasterizer.DestinationType;
+import org.apache.batik.swing.JSVGCanvas;
+import org.apache.batik.swing.JSVGScrollPane;
+import org.apache.batik.transcoder.Transcoder;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.batik.transcoder.image.TIFFTranscoder;
+import org.apache.batik.transcoder.svg2svg.SVGTranscoder;
+import org.apache.fop.svg.PDFTranscoder;
+import org.w3c.dom.Document;
+import org.w3c.dom.svg.SVGDocument;
+
+public class SVGContextMenu extends JPopupMenu {
+
+  private static final long serialVersionUID = -5605994099188052122L;
+  public static final float EXPORT_WIDTH = 1024f;
+  private JMenuItem menuSaveAs;
+  private Component owner;
+// private JPopupMenu self;
+  private SVGDraw svgDraw;
+
+  public SVGContextMenu(Component parent, SVGDraw d) {
+
+    super();
+    owner = parent;
+// self = this;
+    svgDraw = d;
+    menuSaveAs = new JMenuItem("Save as...");
+    add(menuSaveAs);
+    MouseListener popupListener = new SaveAsListener();
+    menuSaveAs.addMouseListener(popupListener);
+  }
+
+  class SaveAsListener extends MouseAdapter {
+
+    private void save(File file) throws Exception {
+
+      String ext = SVGFileFilter.getExtension(file);
+      if (ext == null) {
+        return;
+      }
+      Transcoder t = getTranscoder(ext);
+      if (t == null) {
+        System.out.println("no transcoder found for " + file + " ("
+            + SVGFileFilter.getExtension(file) + ")");
+        return;
+      }
+      if (ext.equals(SVGFileFilter.EXT_svg)) {
+        writeOut(file, t);
+      }
+      else if (ext.equals(SVGFileFilter.EXT_gif) || ext.equals(SVGFileFilter.EXT_bmp)) {
+        File f = File.createTempFile("tmp_", ".png");
+        svgDraw.imageOut(f.getAbsolutePath());
+        //read the temporary PNG file into an image
+        BufferedImage image = ImageIO.read(f);
+        // write the image to disk
+        ImageIO.write(image, ext, file);
+        f.delete();
+      }
+      else {
+        svgDraw.imageOut(file.getAbsolutePath());
+      }
+// if ((t instanceof PDFTranscoder) || (t instanceof TIFFTranscoder)
+// || ext.equals(SVGFileFilter.EXT_png)) {
+// ByteArrayOutputStream os = streamOut(t);
+// FileOutputStream fos = new FileOutputStream(file);
+// fos.write(os.toByteArray());
+// fos.flush();
+// fos.close();
+// }
+// else {
+// ByteArrayOutputStream os = streamOut(t);
+// // create a PNG image from the raw XML source
+// BufferedImage image = ImageIO.read(new ByteArrayInputStream(os
+// .toByteArray()));
+// // write the image to disk
+// ImageIO.write(image, ext, file);
+// }
+    }
+
+    private ByteArrayOutputStream streamOut(Transcoder t) throws Exception {
+
+      // Set the transcoder input and output.
+      TranscoderInput input = new TranscoderInput(svgDraw.state.document);
+      ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+      TranscoderOutput output = new TranscoderOutput(ostream);
+      t.transcode(input, output);
+      ostream.flush();
+      ostream.close();
+      return ostream;
+    }
+
+    // for SVG, ...
+    private void writeOut(File file, Transcoder t) throws Exception {
+
+      // Set the transcoder input and output.
+      TranscoderInput input = new TranscoderInput(svgDraw.state.document);
+      OutputStream ostream = new FileOutputStream(file);
+      Writer w;
+      if (t instanceof SVGTranscoder) {
+        w = new OutputStreamWriter(ostream, "UTF-8");
+      }
+      else {
+        w = new OutputStreamWriter(ostream);
+      }
+      TranscoderOutput output = new TranscoderOutput(w);
+      // Perform the transcoding.
+      t.transcode(input, output);
+      ostream.flush();
+      ostream.close();
+    }
+
+    private Transcoder getTranscoder(String ext) {
+
+      Transcoder t = null;
+      if (ext.equals(SVGFileFilter.EXT_pdf)) {
+        PDFTranscoder pdf = new PDFTranscoder();
+        pdf.addTranscodingHint(PDFTranscoder.KEY_WIDTH, EXPORT_WIDTH);
+        t = pdf;
+      }
+      else if (ext.equals(SVGFileFilter.EXT_svg)) {
+        SVGTranscoder svg = new SVGTranscoder();
+        t = svg;
+      }
+      else if (ext.equals(SVGFileFilter.EXT_tif)) {
+        TIFFTranscoder tiff = new TIFFTranscoder();
+        tiff.addTranscodingHint(TIFFTranscoder.KEY_WIDTH, EXPORT_WIDTH);
+        tiff.addTranscodingHint(TIFFTranscoder.KEY_BACKGROUND_COLOR,
+            Color.white);
+        t = tiff;
+      }
+      else {
+        PNGTranscoder png = new PNGTranscoder();
+        png.addTranscodingHint(PNGTranscoder.KEY_BACKGROUND_COLOR, Color.white);
+        png.addTranscodingHint(PNGTranscoder.KEY_WIDTH, EXPORT_WIDTH);
+        t = png;
+      }
+      return t;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+
+      svgDraw.canvas.setEnabled(false);
+      svgDraw.prepareSave();
+      setVisible(false);
+      JFileChooser fc = new SVGFileChooser();
+      if (fc.showSaveDialog(owner) == JFileChooser.APPROVE_OPTION) {
+        File f = fc.getSelectedFile().getAbsoluteFile();
+        if (f.exists()) {
+          int actionDialog = JOptionPane.showConfirmDialog(owner,
+              "Replace existing file?");
+          if (actionDialog != JOptionPane.YES_OPTION) {
+            return;
+          }
+        }
+        try {
+          save(fc.getSelectedFile().getAbsoluteFile());
+          System.out.println("Saved file: "
+              + fc.getSelectedFile().getAbsoluteFile());
+        }
+        catch (Exception e1) {
+          // cowardly ignore
+          e1.printStackTrace();
+        }
+      }
+      svgDraw.canvas.setEnabled(true);
+    }
+  }
+}
+
+class SVGFileFilter extends javax.swing.filechooser.FileFilter {
+
+  public final static String EXT_bmp = "bmp";
+  public final static String EXT_gif = "gif";
+  public final static String EXT_jpg = "jpg";
+  public final static String EXT_pdf = "pdf";
+  public final static String EXT_png = "png";
+  public final static String EXT_svg = "svg";
+  public final static String EXT_tif = "tif";
+  private final static String DESC_bmp = "BMP bitmap (*.bmp)";
+  private final static String DESC_gif = "GIF bitmap (*.gif)";
+  private final static String DESC_jpg = "JPEG bitmap (*.jpeg)";
+  private final static String DESC_pdf = "PDF document (*.pdf)";
+  private final static String DESC_png = "PNG bitmap (*.png)";
+  private final static String DESC_svg = "SVG vector graphic (*.svg)";
+  private final static String DESC_tif = "TIFF bitmap (*.tif)";
+  final static SVGFileFilter FILTER_BMP = new SVGFileFilter(EXT_bmp, DESC_bmp);
+  final static SVGFileFilter FILTER_GIF = new SVGFileFilter(EXT_gif, DESC_gif);
+  final static SVGFileFilter FILTER_JPG = new SVGFileFilter(EXT_jpg, DESC_jpg);
+  final static SVGFileFilter FILTER_PDF = new SVGFileFilter(EXT_pdf, DESC_pdf);
+  final static SVGFileFilter FILTER_PNG = new SVGFileFilter(EXT_png, DESC_png);
+  final static SVGFileFilter FILTER_SVG = new SVGFileFilter(EXT_svg, DESC_svg);
+  final static SVGFileFilter FILTER_TIFF = new SVGFileFilter(EXT_tif, DESC_tif);
+  private String extension;
+  private String description;
+
+  public SVGFileFilter(String ext, String desc) {
+
+    super();
+    extension = ext;
+    description = desc;
+  }
+
+  public static String getExtension(File f) {
+
+    String ext = null;
+    String s = f.getName();
+    int i = s.lastIndexOf('.');
+    if (i > 0 && i < s.length() - 1) {
+      ext = s.substring(i + 1).toLowerCase();
+    }
+    return ext;
+  }
+
+  @Override
+  public boolean accept(File f) {
+
+    if (f.isDirectory()) {
+      return true;
+    }
+    String ext = getExtension(f);
+    return (ext != null) && (ext.equals(extension));
+  }
+
+  @Override
+  public String getDescription() {
+
+    return description;
+  }
+
+  public String getExtension() {
+
+    return extension;
+  }
+}
+
+class SVGFileChooser extends JFileChooser {
+
+  private static final long serialVersionUID = -9020881522405108590L;
+
+  public SVGFileChooser() {
+
+    super();
+    setDialogTitle("Export Cluster Diagrams");
+    setAcceptAllFileFilterUsed(false);
+    addChoosableFileFilter(SVGFileFilter.FILTER_BMP);
+    addChoosableFileFilter(SVGFileFilter.FILTER_GIF);
+    addChoosableFileFilter(SVGFileFilter.FILTER_JPG);
+    addChoosableFileFilter(SVGFileFilter.FILTER_PDF);
+    addChoosableFileFilter(SVGFileFilter.FILTER_PNG);
+    addChoosableFileFilter(SVGFileFilter.FILTER_SVG);
+    addChoosableFileFilter(SVGFileFilter.FILTER_TIFF);
+    setFileFilter(SVGFileFilter.FILTER_SVG);
+  }
+
+  @Override
+  public int showSaveDialog(Component component) throws HeadlessException {
+
+    int result = super.showSaveDialog(component);
+    if (result == JFileChooser.APPROVE_OPTION) {
+      File sf = getSelectedFile();
+      String ext = SVGFileFilter.getExtension(sf);
+      String fext = ((SVGFileFilter) getFileFilter()).getExtension();
+      if ((ext == null) || (!ext.equals(fext))) {
+        File nsf = new File(sf.getAbsolutePath() + "." + fext);
+        setSelectedFile(nsf);
+      }
+    }
+    return result;
+  }
+}
--- etomica.apps/etomica/virial/cluster2/ui/SVGDraw.java
+++ etomica.apps/etomica/virial/cluster2/ui/SVGDraw.java
@@ -3,15 +3,38 @@ package etomica.virial.cluster2.ui;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 import javax.swing.*;
 
+import org.apache.batik.apps.rasterizer.DestinationType;
+import org.apache.batik.apps.rasterizer.SVGConverterController;
+import org.apache.batik.apps.rasterizer.Main;
+import org.apache.batik.apps.rasterizer.SVGConverter;
+import org.apache.batik.apps.rasterizer.SVGConverterFileSource;
+import org.apache.batik.bridge.UpdateManager;
 import org.apache.batik.swing.*;
+import org.apache.batik.swing.svg.JSVGComponent;
 import org.apache.batik.svggen.*;
 import org.apache.batik.transcoder.Transcoder;
 import org.apache.batik.transcoder.TranscoderException;
@@ -25,77 +48,100 @@ import org.w3c.dom.svg.*;
 
 public class SVGDraw {
 
-  private static Node SELECTED_RECTANGLE = null;
-  private static final double INITIAL_ANGLE = Math.PI;
-  private static final int GUTTER = 10;
-  private static final int GRAPH_RADIUS = 60;
-  private static final int NODE_RADIUS = 12;
-  private static final boolean STREAM_OUT = true;
-  private static List<Rectangle2D> tiles = new ArrayList<Rectangle2D>();
+  SVGState state;
+  JSVGCanvas canvas;
+  SVGContextMenu menu;
 
   public static void main(String[] args) {
 
-    // Create an SVG document.
-    DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
-    String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
-    SVGDocument doc = (SVGDocument) impl.createDocument(svgNS, "svg", null);
-    Element root = doc.getDocumentElement();
-    root.setAttributeNS(svgNS, "width", "300");
-    root.setAttributeNS(svgNS, "height", "140");
-    root
-        .setAttributeNS(svgNS, "style",
-            "fill:white;fill-opacity:1;stroke:black;stroke-width:0.5;stroke-opacity:1");
-    drawGraph(doc, Color.red, 20, 20);
-    drawGraph(doc, Color.blue, 160, 20);
-//    drawRectangle(doc, 0);
-//    drawRectangle(doc, 1);
-    if (STREAM_OUT) {
-      streamOut(doc);
+    SVGDraw d = new SVGDraw();
+    d.display();
+  }
+
+  public SVGDraw() {
+
+    state = SVGDocumentBuilder.getState();
+  }
+
+  private String getExtension(String fileName) {
+
+    String ext = null;
+    String s = fileName;
+    int i = s.lastIndexOf('.');
+    if (i > 0 && i < s.length() - 1) {
+      ext = s.substring(i).toLowerCase();
     }
-    display(doc);
+    return ext;
   }
 
-  private static void display(final SVGDocument doc) {
+  private String getMimeType(String fileName) {
 
-    SVGGraphics2D g2 = new SVGGraphics2D(doc);
-    // Populate the document root with the generated SVG content.
-    g2.getRoot(doc.getDocumentElement());
-    // Display the document.
-    final JSVGScrollPane spane = new JSVGScrollPane(new JSVGCanvas());
-    spane.getCanvas().setSVGDocument(doc);
-    spane.getCanvas().addMouseListener(new MouseAdapter() {
-
-      @Override
-      public void mouseClicked(MouseEvent e) {
-
-        System.out.println("Mouse clicked @" + e.getPoint());
-        for (int i = 0; i < tiles.size(); i++) {
-          if (tiles.get(i).contains(e.getPoint())) {
-            System.out.println("Belongs to rectangle #" + i);
-            if (SELECTED_RECTANGLE != null) {
-              Element root = doc.getDocumentElement();
-              root.removeChild(SELECTED_RECTANGLE);
-            }
-            SELECTED_RECTANGLE = drawRectangle(doc, i);
-            spane.getCanvas().setSVGDocument(doc);
-          }
-        }
-      }
-    });
-    JFrame f = new JFrame();
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.getContentPane().add(spane);
-    f.setVisible(true);
-    f.setBounds(new Rectangle(0, 0, 300, 180));
+    String ext = getExtension(fileName);
+    if (ext.equals(DestinationType.JPEG_EXTENSION)) {
+      return DestinationType.JPEG_STR;
+    }
+    if (ext.equals(DestinationType.PDF_EXTENSION)) {
+      return DestinationType.PDF_STR;
+    }
+    if (ext.equals(DestinationType.PNG_EXTENSION)) {
+      return DestinationType.PNG_STR;
+    }
+    if (ext.equals(DestinationType.TIFF_EXTENSION)) {
+      return DestinationType.TIFF_STR;
+    }
+    return null;
   }
 
-  private static void streamOut(SVGDocument doc) {
+  public void imageOut(String fileName) {
+
+    File f = null;
+    try {
+      f = File.createTempFile("tmp_", "svg");
+      FileOutputStream fs = new FileOutputStream(f);
+      streamOut(fs);
+      fs.flush();
+      fs.close();
+      String mimeType = getMimeType(fileName);
+      String[] options = new String[13];
+      // destination
+      options[0] = Main.CL_OPTION_OUTPUT;
+      options[1] = fileName;
+      // destination type
+      options[2] = Main.CL_OPTION_MIME_TYPE;
+      options[3] = mimeType;
+      // quality (for JPEG)
+      options[4] = Main.CL_OPTION_QUALITY;
+      options[5] = "0.8";
+      // resolution 
+      options[6] = Main.CL_OPTION_DPI;
+      options[7] = "300";
+      // resolution 
+      options[8] = Main.CL_OPTION_WIDTH;
+      options[9] = "1024";
+      // resolution 
+      options[10] = Main.CL_OPTION_BACKGROUND_COLOR;
+      options[11] = "255.255.255.255";
+      // input file
+      options[12] = f.getAbsolutePath();
+      // convert
+      Main c = new Main(options);
+      c.execute();
+      // remove temporary file
+      f.delete();
+    }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  public void streamOut(OutputStream svg) {
 
     // Stream out SVG to the standard output using UTF-8 encoding.
     try {
-      TranscoderInput input = new TranscoderInput(doc);
-      TranscoderOutput output = new TranscoderOutput(new OutputStreamWriter(
-          System.out, "UTF-8"));
+      TranscoderInput input = new TranscoderInput(state.document);
+      TranscoderOutput output = new TranscoderOutput(new BufferedWriter(
+          new OutputStreamWriter(svg, "UTF-8")));
       Transcoder t = new SVGTranscoder();
       t.transcode(input, output);
     }
@@ -109,29 +155,189 @@ public class SVGDraw {
     }
   }
 
-  private static Node drawRectangle(Document doc, int i) {
+  public void display() {
 
-    // draw the cluster on an SVG canvas
-    SVGGraphics2D g = new SVGGraphics2D(doc);
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-        RenderingHints.VALUE_ANTIALIAS_ON);
-    g.setRenderingHint(RenderingHints.KEY_RENDERING,
-        RenderingHints.VALUE_RENDER_QUALITY);
-    Stroke stroke = new BasicStroke(1.5f, BasicStroke.CAP_BUTT,
-        BasicStroke.JOIN_MITER, 1.0f, new float[] {4, 4}, 8);
-    g.setStroke(stroke);
-    g.setPaint(Color.lightGray);
-    g.draw(tiles.get(i));
-    Element root = doc.getDocumentElement();
-    Node child = g.getTopLevelGroup().getFirstChild();
-    return root.appendChild(child);
+    SVGGraphics2D g2 = new SVGGraphics2D(state.document);
+    g2.getRoot(state.document.getDocumentElement());
+    canvas = new JSVGCanvas();
+    canvas.setDocumentState(JSVGComponent.ALWAYS_DYNAMIC);
+    JSVGScrollPane spane = new JSVGScrollPane(canvas);
+    menu = new SVGContextMenu(spane, this);
+    canvas.setSVGDocument(state.document);
+    canvas.addMouseMotionListener(state.getMouseMotionListener(canvas));
+    canvas.addMouseListener(state.getMouseClickedListener(menu, canvas));
+    JFrame f = new JFrame();
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// f.setLayout(new GridLayout());
+    f.setContentPane(new JSVGScrollPane(canvas));
+    f.setBounds(new Rectangle(0, 0, SVGProperties.MAP_WIDTH,
+        SVGProperties.MAP_HEIGHT + 20));
+    f.setVisible(true);
+//    f.pack();
+// f.setBounds(new Rectangle(0, 0, 300, 180));
   }
 
-  private static Node drawGraph(Document doc, Color color, int dX, int dY) {
+  public void prepareSave() {
+
+    state.HOVERING_RECTANGLE_ID = -1;
+    state.SELECTED_RECTANGLE_ID = -1;
+    final Runnable selected = new SelectedRectangle(state);
+    UpdateManager um = canvas.getUpdateManager();
+    try {
+      um.getUpdateRunnableQueue().invokeAndWait(selected);
+    }
+    catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+}
+
+class SVGState {
+
+  final boolean CONTROL_HOVER = true;
+  SVGDocument document = null;
+  Node HOVERING_RECTANGLE = null;
+  int HOVERING_RECTANGLE_ID = -1;
+  Node SELECTED_RECTANGLE = null;
+  int SELECTED_RECTANGLE_ID = -1;
+  final List<Rectangle2D> tiles;
+  private Runnable hovering;
+  private Runnable selected;
+
+  public SVGState(final SVGDocument doc, final List<Rectangle2D> rectangles) {
+
+    document = doc;
+    tiles = rectangles;
+    hovering = new HoveringRectangle(this);
+    selected = new SelectedRectangle(this);
+  }
+
+  private int getTile(Point p) {
+
+    return (p.x / SVGProperties.CLUSTER_WIDTH)
+        + (p.y / SVGProperties.CLUSTER_HEIGHT) * SVGProperties.CLUSTERS_ACROSS;
+  }
+
+  public MouseListener getMouseClickedListener(final SVGContextMenu menu,
+      final JSVGCanvas canvas) {
+
+    return new MouseAdapter() {
+
+      @Override
+      public void mouseClicked(MouseEvent e) {
+
+        if (e.getButton() == MouseEvent.BUTTON3) {
+          menu.setLocation(e.getXOnScreen(), e.getYOnScreen());
+          menu.setVisible(true);
+        }
+        else if (e.getButton() == MouseEvent.BUTTON1) {
+          int newSelected = getTile(e.getPoint());
+          if (newSelected >= tiles.size()) {
+            return;
+          }
+          if (SELECTED_RECTANGLE_ID == newSelected) {
+            SELECTED_RECTANGLE_ID = -1;
+          }
+          else {
+            SELECTED_RECTANGLE_ID = newSelected;
+          }
+          HOVERING_RECTANGLE_ID = -1;
+          canvas.getUpdateManager().getUpdateRunnableQueue().invokeLater(
+              selected);
+        }
+      }
+    };
+  }
+
+  public MouseMotionListener getMouseMotionListener(final JSVGCanvas canvas) {
+
+    return new MouseAdapter() {
+
+      @Override
+      public void mouseMoved(MouseEvent e) {
+
+        if (CONTROL_HOVER && (e.getButton() == MouseEvent.NOBUTTON)) {
+          int newHover = getTile(e.getPoint());
+          if (newHover >= tiles.size()) {
+            newHover = -1;
+          }
+          if (SELECTED_RECTANGLE_ID == newHover) {
+            newHover = -1;
+          }
+          if (newHover != HOVERING_RECTANGLE_ID) {
+            if (newHover == -1) {
+              HOVERING_RECTANGLE_ID = -1;
+            }
+            else {
+              HOVERING_RECTANGLE_ID = newHover;
+            }
+            canvas.getUpdateManager().getUpdateRunnableQueue().invokeLater(
+                hovering);
+          }
+        }
+      }
+    };
+  }
+}
+
+interface SVGProperties {
+
+  static final int GUTTER = 10;
+  static final int NODE_RADIUS = 12;
+  static final int GRAPH_RADIUS = 60;
+  static final int CLUSTERS_ACROSS = 2;
+  static final int MAP_WIDTH = 300;
+  static final int MAP_HEIGHT = 140;
+  static final int CLUSTER_WIDTH = MAP_WIDTH / CLUSTERS_ACROSS;
+  static final int CLUSTER_HEIGHT = CLUSTER_WIDTH;
+  static final boolean STREAM_OUT = false;
+  static final double INITIAL_ANGLE = Math.PI;
+  static final Stroke SELECTED_STROKE = new BasicStroke(1.5f,
+      BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 1.0f,
+      new float[] { 3, 3 }, 6);
+  static final Color SELECTED_COLOR = Color.DARK_GRAY;
+  static final Stroke HOVERING_STROKE = new BasicStroke(1.5f);
+  static final Color HOVERING_COLOR = Color.MAGENTA;
+}
+
+class SVGDocumentBuilder {
+
+  SVGDocument doc;
+  List<Rectangle2D> tiles = new ArrayList<Rectangle2D>();
+
+  private SVGDocumentBuilder() {
+
+    // Create an SVG document.
+    DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+    String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
+    doc = (SVGDocument) impl.createDocument(svgNS, "svg", null);
+    SVGGeneratorContext ctx = SVGGeneratorContext.createDefault(doc);
+    ctx.setComment("Generated by Etomica Cluster Viewer using Apache Batik.");
+    ctx.setEmbeddedFontsOn(true);
+    Element root = doc.getDocumentElement();
+    root.setAttributeNS(null, "width", String.valueOf(SVGProperties.MAP_WIDTH));
+    root.setAttributeNS(null, "height", String
+        .valueOf(SVGProperties.MAP_HEIGHT));
+    root
+        .setAttributeNS(null, "style",
+            "fill:white;fill-opacity:1;stroke:black;stroke-width:0.5;stroke-opacity:1");
+    drawGraph(doc, Color.red, 20, 20);
+    drawGraph(doc, Color.blue, 160, 20);
+  }
+
+  public static SVGState getState() {
+
+    SVGDocumentBuilder b = new SVGDocumentBuilder();
+    return new SVGState(b.doc, b.tiles);
+  }
+
+  private Node drawGraph(Document doc, Color color, int dX, int dY) {
 
     int size = 6;
-    int r = NODE_RADIUS;
-    int R = GRAPH_RADIUS;
+    int r = SVGProperties.NODE_RADIUS;
+    int R = SVGProperties.GRAPH_RADIUS;
+    int gutter = SVGProperties.GUTTER;
     double[] x = new double[size];
     double[] y = new double[size];
     double minX = -1, minY = -1, maxX = -1, maxY = -1;
@@ -145,8 +351,10 @@ public class SVGDraw {
         RenderingHints.VALUE_RENDER_QUALITY);
     // compute node centers
     for (int i = 0; i < size; i++) {
-      x[i] = (R - r) * Math.cos(INITIAL_ANGLE + i * angle) + (R - r + dX);
-      y[i] = (R - r) * Math.sin(INITIAL_ANGLE + i * angle) + (R - r + dY);
+      x[i] = (R - r) * Math.cos(SVGProperties.INITIAL_ANGLE + i * angle)
+          + (R - r + dX);
+      y[i] = (R - r) * Math.sin(SVGProperties.INITIAL_ANGLE + i * angle)
+          + (R - r + dY);
       if (minX == -1) {
         minX = x[i];
         maxX = x[i];
@@ -167,10 +375,8 @@ public class SVGDraw {
       }
     }
     // compute the bounding rectangle
-    tiles.add(new Rectangle2D.Double(minX - GUTTER, minY - GUTTER, maxX - minX
-        + r + 2 * GUTTER, maxY - minY + r + 2 * GUTTER));
-    System.out.println("Rectangle #" + (tiles.size() - 1) + " = "
-        + tiles.get(tiles.size() - 1));
+    tiles.add(new Rectangle2D.Double(minX - gutter, minY - gutter, maxX - minX
+        + r + 2 * gutter, maxY - minY + r + 2 * gutter));
     // draw edges
     for (int i = 0; i < size; i++) {
       for (int j = i + 1; j < size; j++) {
@@ -192,5 +398,90 @@ public class SVGDraw {
     Element root = doc.getDocumentElement();
     Node child = g.getTopLevelGroup().getFirstChild();
     return root.appendChild(child);
+  }
+}
+
+abstract class PaintRunnable implements Runnable {
+
+  protected Map<Integer, Node> rectangles = new HashMap<Integer, Node>();
+  protected Color color;
+  protected Stroke stroke;
+  protected SVGState state;
+
+  PaintRunnable(SVGState state, Stroke stroke, Color color) {
+
+    this.state = state;
+    this.color = color;
+    this.stroke = stroke;
+  }
+
+  protected void clearSelection() {
+
+    if (state.SELECTED_RECTANGLE != null) {
+      Element root = state.document.getDocumentElement();
+      root.removeChild(state.SELECTED_RECTANGLE);
+      state.SELECTED_RECTANGLE = null;
+    }
+  }
+
+  protected void clearHovering() {
+
+    if (state.HOVERING_RECTANGLE != null) {
+      Element root = state.document.getDocumentElement();
+      root.removeChild(state.HOVERING_RECTANGLE);
+      state.HOVERING_RECTANGLE = null;
+    }
+  }
+
+  protected Node drawRectangle(int i) {
+
+    if (!rectangles.containsKey(i)) {
+      // draw the cluster on an SVG canvas
+      SVGGraphics2D g = new SVGGraphics2D(state.document);
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+          RenderingHints.VALUE_ANTIALIAS_ON);
+      g.setRenderingHint(RenderingHints.KEY_RENDERING,
+          RenderingHints.VALUE_RENDER_QUALITY);
+      g.setStroke(stroke);
+      g.setPaint(color);
+      g.draw(state.tiles.get(i));
+      Node child = g.getTopLevelGroup().getFirstChild();
+      rectangles.put(i, child);
+    }
+    Element root = state.document.getDocumentElement();
+    return root.appendChild(rectangles.get(i));
+  }
+}
+
+class SelectedRectangle extends PaintRunnable {
+
+  SelectedRectangle(SVGState state) {
+
+    super(state, SVGProperties.SELECTED_STROKE, SVGProperties.SELECTED_COLOR);
+  }
+
+  public void run() {
+
+    clearHovering();
+    clearSelection();
+    if (state.SELECTED_RECTANGLE_ID != -1) {
+      state.SELECTED_RECTANGLE = drawRectangle(state.SELECTED_RECTANGLE_ID);
+    }
+  }
+}
+
+class HoveringRectangle extends PaintRunnable {
+
+  HoveringRectangle(SVGState state) {
+
+    super(state, SVGProperties.HOVERING_STROKE, SVGProperties.HOVERING_COLOR);
+  }
+
+  public void run() {
+
+    clearHovering();
+    if (state.HOVERING_RECTANGLE_ID != -1) {
+      state.HOVERING_RECTANGLE = drawRectangle(state.HOVERING_RECTANGLE_ID);
+    }
   }
 }
