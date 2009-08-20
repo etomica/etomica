24297850b499daf9faa37036bca83916428c15eb
Demien Lessa <dlessa@buffalo.edu>
2009-08-20 17:52:56 +0000

    non-static class used to readIcon

--- etomica.apps/etomica/virial/cluster2/graph/GraphSetFactory.java
+++ etomica.apps/etomica/virial/cluster2/graph/GraphSetFactory.java
@@ -7,8 +7,14 @@ public class GraphSetFactory {
   
   public static GraphSet completeGraphSet(char[] fieldColors, char[] rootColors) {
 
+    return completeGraphSet(fieldColors, rootColors, false);
+
+  }
+
+  public static GraphSet completeGraphSet(char[] fieldColors, char[] rootColors, boolean filterIsomorphs) {
+
     Nodes nodes = new SimpleNodes(fieldColors, rootColors);
-    return GraphFactory.storedGraphSet(nodes, null, false);
+    return GraphFactory.naiveGraphSet(nodes, null, filterIsomorphs);
 
   }
 
