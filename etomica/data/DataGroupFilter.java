8b4c0309a2b8c14560baf6cd85af083b4206855c
Andrew Schultz <ajs42@buffalo.edu>
2009-09-09 13:28:04 +0000

    copy tags from inputDataInfo

--- etomica/data/DataGroupFilter.java
+++ etomica/data/DataGroupFilter.java
@@ -83,7 +83,9 @@ public class DataGroupFilter extends DataProcessor {
         } catch(ArrayIndexOutOfBoundsException ex) {
             throw new ArrayIndexOutOfBoundsException("DataFilter was constructed to extract a Data element with an index that is larger than the number of Data elements wrapped in the DataGroup. Number of elements: "+nData+"; index array: "+Arrays.toString(indexes));
         }
-        return new DataInfoGroup(inputDataInfo.getLabel(), inputDataInfo.getDimension(), pushedDataInfo);
+        DataInfoGroup myDataInfo = new DataInfoGroup(inputDataInfo.getLabel(), inputDataInfo.getDimension(), pushedDataInfo);
+        myDataInfo.addTags(inputDataInfo.getTags());
+        return myDataInfo;
     }
 
     /**
