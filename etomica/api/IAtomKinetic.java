8245b53c9774f915eb34b80c6c5b19b8e79a8bb9
Andrew Schultz <ajs42@buffalo.edu>
2009-10-05 15:04:42 +0000

    add a comment

--- etomica/api/IAtomKinetic.java
+++ etomica/api/IAtomKinetic.java
@@ -6,6 +6,10 @@ package etomica.api;
  */
 public interface IAtomKinetic extends IAtom {
 
+    /**
+     * Returns the velocity of the IAtom.  Modifying the returned IVector will
+     * alter the IAtom's velocity.
+     */
     public IVectorMutable getVelocity();
  
 }
