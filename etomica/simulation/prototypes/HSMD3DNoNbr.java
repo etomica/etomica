97d932edada063bfda9ec6e76e03e588480138d7
Andrew Schultz <ajs42@buffalo.edu>
2010-01-21 14:50:16 +0000

    serialization was fun, but not really relevant anymore

--- etomica/simulation/prototypes/HSMD3DNoNbr.java
+++ etomica/simulation/prototypes/HSMD3DNoNbr.java
@@ -2,12 +2,6 @@
 
 package etomica.simulation.prototypes;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 import etomica.action.BoxImposePbc;
 import etomica.action.activity.ActivityIntegrate;
 import etomica.api.IAtomType;
@@ -61,59 +55,12 @@ public class HSMD3DNoNbr extends Simulation {
         integrator.setBox(box);
         integrator.getEventManager().addListener(new IntegratorListenerAction(new BoxImposePbc(box, space)));
         new ConfigurationLattice(new LatticeCubicFcc(space), space).initializeCoordinates(box);
-        
-        //ColorSchemeByType.setColor(speciesSpheres0, java.awt.Color.blue);
-
- //       MeterPressureHard meterPressure = new MeterPressureHard(integrator);
- //       DataAccumulator accumulatorManager = new DataAccumulator(meterPressure);
-        // 	DisplayTextBox box = new DisplayBox();
-        // 	box.setDatumSource(meterPressure);
- //       box.setDensity(0.7);
-    } //end of constructor
+    }
 
     public static void main( String[] args )
     {
-    	String filename = "test.bin";
-		
-    	try
-    	{
-    	    FileOutputStream fos = null;
-    	    ObjectOutputStream out = null;
-    	    HSMD3DNoNbr simulation = new HSMD3DNoNbr();
-    	    fos = new FileOutputStream( filename);
-			out = new ObjectOutputStream(fos);
-			out.writeObject( simulation );
-			out.close();
-			fos.close();
-			System.out.println( "Serialization of class HSMD3DNoNbr succeeded.");
-    	}
-    	catch(IOException ex)
-    	{
-    	    System.err.println( "Exception:" + ex.getMessage() );
-    	    ex.printStackTrace();
-    	}
-    	
-    	// Serialize back
-    	Simulation simulation = null;
-    	try
-    	{
-    	    FileInputStream fis = null;
-    	    fis = new FileInputStream(filename);
-    	    ObjectInputStream in = new ObjectInputStream(fis);
-    	    simulation = (Simulation) in.readObject();
-    	    in.close();
-    	    fis.close();
-    	    
-    	    System.out.println( "DeSerialization of class HSMD3DNoNbr succeeded.");
-
-    	}
-    	catch( Exception ex ) {
-    	    System.err.println( "Could not read simulation from file " + filename + ". Cause: " + ex.getMessage() );
-    	    ex.printStackTrace();
-    	}
-		
-	    // go daddy
-	    ((Simulation)simulation).getController().actionPerformed();
+        HSMD3DNoNbr simulation = new HSMD3DNoNbr();
+	    simulation.getController().actionPerformed();
 	    System.out.println( "Simulation run ok");
 
 		
