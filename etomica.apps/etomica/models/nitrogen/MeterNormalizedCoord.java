e31399c9dd17d1c5fdb630373215d66bffaa4c90
Tai Boon Tan <taitan@buffalo.edu>
2010-01-26 21:49:46 +0000

    Meter to quantify the normalized coordinate

---
+++ etomica.apps/etomica/models/nitrogen/MeterNormalizedCoord.java
@@ -0,0 +1,143 @@
+package etomica.models.nitrogen;
+
+import java.io.Serializable;
+
+import etomica.action.IAction;
+import etomica.api.IBox;
+import etomica.api.IMoleculeList;
+import etomica.api.ISpecies;
+import etomica.data.AccumulatorHistogram;
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataGroup;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.data.types.DataGroup.DataInfoGroup;
+import etomica.normalmode.CoordinateDefinition;
+import etomica.normalmode.CoordinateDefinition.BasisCell;
+import etomica.units.CompoundDimension;
+import etomica.units.Dimension;
+import etomica.units.Length;
+import etomica.units.Null;
+import etomica.util.DoubleRange;
+import etomica.util.HistogramExpanding;
+import etomica.util.HistogramSimple;
+
+/**
+ * Sample the distribution of the normalized coordinate for the atoms/ molecules
+ *  from coordinate definition, calcU method. They are the quantities of deviation
+ *  in translation and rotation from the atomic/molecular nominal position and
+ *  orientation
+ *  
+ *  Eg. N2 molecule with 5 D.O.F, this class will return 5 distributions of 
+ *  	3 translation and 2 rotation motion respectively.
+ */
+public class MeterNormalizedCoord implements IEtomicaDataSource, IAction, Serializable {
+
+    public MeterNormalizedCoord(IBox newBox, CoordinateDefinition coordDef, ISpecies species) {
+        tag = new DataTag();
+        this.coordinateDefinition = coordDef;
+        
+        if (species==null){
+        	throw new RuntimeException("Need to set species in MeterNormalizedCoord class");
+        }
+        dof = coordDef.getCoordinateDim() /newBox.getNMolecules(species);
+        
+        uDistributions = new DataDoubleArray[dof];
+        for (int i=0; i<uDistributions.length; i++) {
+        	/*
+        	 * Store in the x and y columns from Histogram
+        	 * x: value and y: probability
+        	 */
+            uDistributions[i] = new DataDoubleArray(new int[]{2, 2000});
+        }
+        data = new DataGroup(uDistributions);
+        
+        DataInfoDoubleArray[] uDistributionsInfo = new DataInfoDoubleArray[dof];
+        CompoundDimension length = new CompoundDimension(new Dimension[]{Length.DIMENSION}, new double[]{1});
+        for (int i=0; i<uDistributionsInfo.length; i++) {
+            uDistributionsInfo[i] = new DataInfoDoubleArray("u", length, new int[]{2,2000});
+        }
+        dataInfo = new DataInfoGroup("all uDistributions", Null.DIMENSION, uDistributionsInfo);
+        
+        histogramU = new HistogramExpanding[dof];
+        for (int i=0; i<dof; i++){
+        	histogramU[i] = new HistogramExpanding(0.005, new DoubleRange(-5, 5));
+        }
+    }
+
+    public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+    
+    /**
+     * Assigning the histogram to data
+     */
+    public void actionPerformed() {
+
+        BasisCell[] cells = coordinateDefinition.getBasisCells();
+                
+        for (int iCell = 0; iCell<cells.length; iCell++) {
+        	
+            BasisCell cell = cells[iCell];
+            IMoleculeList molecules = cell.molecules;
+            int numMolecules = molecules.getMoleculeCount();
+            
+            double[] u = coordinateDefinition.calcU(molecules);
+                                  
+	        for (int i=0; i<dof; i++){ 
+	        	for (int j=0; j<numMolecules; j++){
+	            	histogramU[i].addValue(u[i+dof*j]);
+	            }
+           
+	        }           
+        } //end of cell; there is only 1 cell
+    }
+
+    /**
+     * Returns the DataGroup of uDistribtion[i]
+     */
+    public IData getData() {
+    	
+    	for(int i=0; i<dof; i++){
+    		uDistributions[i].assignColumnFrom(0, histogramU[i].xValues());
+    		uDistributions[i].assignColumnFrom(1, histogramU[i].getHistogram());
+    		
+    	}
+    	
+    	return data;
+    }
+
+    /**
+     * Sets the tensor summation to 0.
+     */
+    public void reset() {
+        data.E(0);
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public DataTag getTag() {
+        return tag;
+    }
+    
+    private static final long serialVersionUID = 1L;
+    protected CoordinateDefinition coordinateDefinition;
+    private String name;
+    private final DataTag tag;
+    private IEtomicaDataInfo dataInfo;
+    private DataGroup data;
+    private HistogramExpanding[] histogramU;
+    private DataDoubleArray[] uDistributions;
+    private int dof;
+
+    
+}
