c8ccd72f37ff5256fee16a196565672acba75116
Nancy Cribbin <cribbin@buffalo.edu>
2010-02-18 21:32:27 +0000

    Tested and proofed.  Happy code!

--- etomica.apps/etomica/models/oneDHardRods/DoubleIntegral.java
+++ etomica.apps/etomica/models/oneDHardRods/DoubleIntegral.java
@@ -2,37 +2,84 @@ package etomica.models.oneDHardRods;
 
 public class DoubleIntegral {
 
-    double xStart, yStart, xEnd, yEnd; 
-    int xN, yN;
-    
-    public DoubleIntegral(double xstart, double xend, double ystart, double yend,
-            int xn, int yn){
-        xStart = xstart;
-        yStart = ystart;
-        xEnd = xend;
-        yEnd = yend;
-        xN = xn;
-        yN = yn;
+    public DoubleIntegral(double xStart, double xEnd, double yStart, double yEnd,
+            int xN, int yN){
+        double total = 0.0;
+        double xValue = 0.0;
+        double yValue = 0.0;
         
-        double xPrefix = (xEnd - xStart) / xN;
-        double yPrefix = (yEnd - yStart) / yN;
+        //we skip i = 0 because the endpoints do not double.
+        for(int i = 1; i < xN; i++) {
+            xValue = xStart + i *(xEnd - xStart) / xN;
+            for (int j = 1; j < yN; j++) {
+                yValue = yStart + j *(yEnd - yStart) / yN;
+                total += 4 * function(xValue, yValue);
+            }
+        }
         
-        double total = 0;
+        //Now do the "edge" stuff, where either the x or y value, but not both,
+        //  are the start or end value
+        xValue = xStart;
+        for (int j = 1; j < yN; j++) {
+            yValue = yStart + j *(yEnd - yStart) / yN;
+            total += 2 * function(xValue, yValue);
+        }
+        xValue = xEnd;
+        for (int j = 1; j < yN; j++) {
+            yValue = yStart + j *(yEnd - yStart) / yN;
+            total += 2 * function(xValue, yValue);
+        }
+        yValue = yStart;
+        for (int i = 1; i < yN; i++) {
+            xValue = xStart + i *(xEnd - xStart) / xN;
+            total += 2 * function(xValue, yValue);
+        }
+        yValue = yEnd;
+        for (int i = 1; i < yN; i++) {
+            xValue = xStart + i *(xEnd - xStart) / xN;
+            total += 2 * function(xValue, yValue);
+        }
         
+        //Now the four values for which both the x and y values are
+        //  the start or end value
+        xValue = xStart;
+        yValue = yStart;
+        total += function(xValue, yValue);
         
+        yValue = yEnd;
+        total += function(xValue, yValue);
         
+        xValue = xEnd;
+        yValue = yStart;
+        total += function(xValue, yValue);
         
+        yValue = yEnd;
+        total += function(xValue, yValue);
         
+        //Now we do the prefix thing
+        double prefix = (xEnd - xStart)*(yEnd - yStart) / (4* xN * yN);
         
+        total *= prefix;
+        
+        System.out.println("Integral = " + total);
     }
     
         
     private double function(double x, double y){
-        double value = x + y ;
+        double value = x * y ;
         return value;
     }
     
-    public static void main() {
+    public static void main(String[] args) {
+      double xStart = 0.0;
+      double yStart = 0.0;
+      double xEnd = 2.0;
+      double yEnd = 2.0;
+      int xN = 1000;
+      int yN = 1000;
+        
+
+      DoubleIntegral di = new DoubleIntegral(xStart, xEnd, yStart, yEnd, xN,yN);
         
     }
 }
