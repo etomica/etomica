04a38a9d1b8d1176bb9a87a34a479e02c6841bff
Hye Min Kim <hmkim3@buffalo.edu>
2010-02-24 20:37:31 +0000

    *** empty log message ***

---
+++ etomica.graph/etomica/graph/operations/ExcludeParameters.java
@@ -0,0 +1,8 @@
+package etomica.graph.operations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExcludeParameters implements Parameters {
+	public Map<Character, char[]> bondMap = new HashMap<Character, char[]>();
+}
