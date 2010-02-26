6aee7f3615b194ff9cdef03e9d1d4f19bf6bf48c
Hye Min Kim <hmkim3@buffalo.edu>
2010-02-26 22:43:02 +0000

    *** empty log message ***

---
+++ etomica.apps/etomica/virial/simulations/SingleAssociationSiteFluidGraph.java
@@ -0,0 +1,205 @@
+package etomica.virial.simulations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import etomica.action.IAction;
+import etomica.api.IAtomType;
+import etomica.api.IAtomTypeSphere;
+import etomica.data.AccumulatorAverage;
+import etomica.data.AccumulatorRatioAverage;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataGroup;
+import etomica.graph.model.Graph;
+import etomica.graphics.SimulationGraphic;
+import etomica.listener.IntegratorListenerAction;
+import etomica.potential.P2HardAssociationConeOneSite;
+import etomica.potential.P2LennardJones;
+import etomica.potential.P2MoleculeMonatomic;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.Species;
+import etomica.util.Arrays;
+import etomica.util.ParameterBase;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.ClusterBonds;
+import etomica.virial.ClusterSum;
+import etomica.virial.ConfigurationClusterMove;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerESpherical;
+import etomica.virial.MayerFunction;
+import etomica.virial.MayerFunctionSumGeneral;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerGeneralSpherical;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesFactoryOrientedSpheres;
+import etomica.virial.cluster.ClusterAssociation;
+import etomica.virial.cluster.Standard;
+
+/**
+ * repulsive potential: Lennard-Jones potential
+ * attractive potential: short-handed square well potential 
+ * Wertheim's single attraction-site model
+ *
+ * @author Hye Min Kim
+ */
+
+public class SingleAssociationSiteFluidGraph {
+
+	public static void main(String[] args) {
+		VirialAssociatingFluidParam params = new VirialAssociatingFluidParam();
+		Space space = Space3D.getInstance();
+		
+		int nPoint = params.nPoint;
+		int whichGraph = params.whichGraph;
+		final int nBody = nPoint;
+		double temperature=params.temperature;
+		double sigmaHSRef = params.sigmaHSRef;
+		double wellConstant = params.wellConstant;
+		long numSteps = params.numSteps;
+		
+		if (args.length == 5) {
+			nPoint = Integer.parseInt(args[0]);
+			whichGraph = Integer.parseInt(args[1]);
+        	temperature = Double.parseDouble(args[2]);
+            sigmaHSRef = Double.parseDouble(args[3]);
+            numSteps = Integer.parseInt(args[4]);
+            wellConstant = Double.parseDouble(args[5]);
+            
+        } else if (args.length != 0){
+        	throw new IllegalArgumentException("Wrong number of arguments");
+        }
+		double sigma=1;
+		double epsilon=1;
+		final double[] HSB = new double[9];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B2HS: "+HSB[2]);
+        System.out.println("B3HS: "+HSB[3]+" = "+(HSB[3]/(HSB[2]*HSB[2]))+" B2HS^2");
+        System.out.println("Square-Well epsilon =" +wellConstant+ "*epsilon");
+        System.out.println("repulsive potential : Lennard-Jones potential");
+        System.out.println("attractive potential : short-handed square well potential");
+		P2HardAssociationConeOneSite p = new P2HardAssociationConeOneSite(space, sigma, epsilon, Double.POSITIVE_INFINITY, wellConstant); //Lennard-Jones potential+square-well site-site attraction potential, 1.0=cutoffFactor
+		P2MoleculeMonatomic pMolecule = new P2MoleculeMonatomic(p);
+		P2LennardJones pR = new P2LennardJones(space, sigma, epsilon);//L-J potential
+		MayerGeneralSpherical fR = new MayerGeneralSpherical(pR);//LJ Mayer fR function
+		MayerESpherical eR = new MayerESpherical(pR);//LJ eR function
+		MayerGeneral fTotal = new MayerGeneral(pMolecule);//Mayer f function
+		MayerFunctionSumGeneral F = new MayerFunctionSumGeneral(space, new MayerFunction[]{fTotal,fR}, new double[]{1,-1});//association F=f-fR
+		
+		MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(sigmaHSRef);
+		
+        
+      ClusterAbstract targetCluster = ClusterAssociation.virialCluster(whichGraph, fR, F, (byte)nPoint);		
+		
+      ClusterAbstract refCluster = Standard.virialCluster(nBody, fRef, nBody>3, eRef, true);
+        refCluster.setTemperature(temperature);
+        targetCluster.setTemperature(temperature);
+		final SimulationVirialOverlap sim = new SimulationVirialOverlap(space, new SpeciesFactoryOrientedSpheres(), temperature, refCluster, targetCluster);
+		ConfigurationClusterMove configuration = new ConfigurationClusterMove(space, sim.getRandom());
+		configuration.initializeCoordinates(sim.box[1]);
+		sim.setAccumulatorBlockSize((int)numSteps*10);
+		sim.integratorOS.setNumSubSteps(1000);	
+
+		if (false) {
+            sim.box[0].getBoundary().setBoxSize(space.makeVector(new double[]{10,10,10}));
+            sim.box[1].getBoundary().setBoxSize(space.makeVector(new double[]{10,10,10}));
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, space, sim.getController());
+            simGraphic.getDisplayBox(sim.box[0]).setShowBoundary(false);
+            simGraphic.getDisplayBox(sim.box[1]).setShowBoundary(false);
+            Species species = (Species)sim.species;
+            IAtomType typeLJ = species.getAtomType(0);
+            ((IAtomTypeSphere)typeLJ).setDiameter(sigma);
+            simGraphic.makeAndDisplayFrame();
+    
+            sim.integratorOS.setNumSubSteps(1000);
+            sim.setAccumulatorBlockSize(1000);
+                
+            // if running interactively, set filename to null so that it doens't read
+            // (or write) to a refpref file
+            sim.getController().removeAction(sim.ai);
+            sim.getController().addAction(new IAction() {
+                public void actionPerformed() {
+                    sim.initRefPref(null, 100);
+                    sim.equilibrate(null,200);
+                    sim.ai.setMaxSteps(Long.MAX_VALUE);
+                }
+            });
+            sim.getController().addAction(sim.ai);
+            if ((Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref) || sim.refPref == 0)) {
+                throw new RuntimeException("Oops");
+            }
+            
+            return;
+        }
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nBody+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+//        sim.setRefPref(1.0082398078547523);
+        sim.initRefPref(refFileName, numSteps/100);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, numSteps/40);
+                
+        System.out.println("equilibration finished");
+
+        IAction progressReport = new IAction() {
+            public void actionPerformed() {
+                System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                double ratio = sim.dsvo.getDataAsScalar();
+                double error = sim.dsvo.getError();
+                System.out.println("abs average: "+ratio*HSB[nBody]+", error: "+error*HSB[nBody]);
+            }
+        };
+        IntegratorListenerAction progressReportListener = new IntegratorListenerAction(progressReport);
+        progressReportListener.setInterval((int)(numSteps/10));
+        sim.integratorOS.getEventManager().addListener(progressReportListener);
+        
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(numSteps);
+        for (int i=0; i<2; i++) {
+            System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize());
+        }
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
+        
+        double ratio = sim.dsvo.getDataAsScalar();
+        double error = sim.dsvo.getError();
+        System.out.println("ratio average: "+ratio+", error: "+error);
+        System.out.println("abs average: "+ratio*HSB[nBody]+", error: "+error*HSB[nBody]);
+        DataGroup allYourBase = (DataGroup)sim.accumulators[0].getData(sim.dsvo.minDiffLocation());
+        System.out.println("hard sphere ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("hard sphere   average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("hard sphere overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+        
+        allYourBase = (DataGroup)sim.accumulators[1].getData(sim.accumulators[1].getNBennetPoints()-sim.dsvo.minDiffLocation()-1);
+        System.out.println("lennard jones ratio average: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index)).getData()[1]);
+        System.out.println("lennard jones average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[0]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[0]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[0]);
+        System.out.println("lennard jones overlap average: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.AVERAGE.index)).getData()[1]
+                          +" stdev: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.STANDARD_DEVIATION.index)).getData()[1]
+                          +" error: "+((DataDoubleArray)allYourBase.getData(AccumulatorAverage.StatType.ERROR.index)).getData()[1]);
+    }
+	
+    
+	
+	public static class VirialAssociatingFluidParam extends ParameterBase {
+		public int nPoint = 2;
+		public int whichGraph = 1;
+		public double temperature = 1.5;//reduced temperature
+		public double sigmaHSRef = 1.5;
+		public long numSteps = 100000;
+		public double wellConstant = 16.0;
+	}
+
+}
