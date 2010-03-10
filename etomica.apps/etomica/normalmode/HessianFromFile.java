be83a9a252542876b06fc2f1d0374bb399e85984
Andrew Schultz <ajs42@buffalo.edu>
2010-03-10 15:21:52 +0000

    add a class whose purpose in life is to recalculate harmonic free energies (reading normal mode info from a file) for many temperatures

---
+++ etomica.apps/etomica/normalmode/HessianFromFile.java
@@ -0,0 +1,27 @@
+package etomica.normalmode;
+
+
+/**
+ * MC simulation of FCC soft-sphere model in 3D with tabulation of the
+ * collective-coordinate S-matrix. No graphic display of simulation.
+ * 
+ * @author Tai Boon Tan
+ */
+public class HessianFromFile {
+
+    public static void main(String[] args) {
+    	
+        // defaults
+        int nA =500;
+        String filename = "inputSSDB"+nA;//+"_d0962";
+        NormalModesFromFile nm = new NormalModesFromFile(filename, 3);
+        CalcHarmonicA.doit(nm, 3, 0.001, nA);
+        CalcHarmonicA.doit(nm, 3, 1.2147, nA);
+        
+        for (int i =1; i<17; i++){
+        	double temp = i*0.1;
+        	CalcHarmonicA.doit(nm, 3, temp, nA);
+        }
+    }
+    
+}
