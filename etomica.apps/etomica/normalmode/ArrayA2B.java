7a9cd0368bf7638ae69c007ce7ed7b2eb8b2909f
Andrew Schultz <ajs42@buffalo.edu>
2010-03-21 18:47:08 +0000

    add class to convert huge files to binary (serialized) format.  helpful for .S, .h and .vec

---
+++ etomica.apps/etomica/normalmode/ArrayA2B.java
@@ -0,0 +1,36 @@
+package etomica.normalmode;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+
+/**
+ * Reads a file containing n mxm matrices as ASCII and serializes them to
+ * another file.  The resulting file should be much smaller than the original
+ * ASCII file.
+ * 
+ * This class relies on ArrayReader2D to read in the original ASCII file.
+ * 
+ * @author Andrew Schultz
+ */
+public class ArrayA2B {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new RuntimeException("usage: A2B filename");
+        }
+        double[][][] x = ArrayReader2D.getFromFile(args[1]);
+        try {
+            FileOutputStream fos = null;
+            ObjectOutputStream out = null;
+            fos = new FileOutputStream(args[1]+".bin");
+            out = new ObjectOutputStream(fos); 
+            out.writeObject(x);
+            out.close();
+            fos.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
