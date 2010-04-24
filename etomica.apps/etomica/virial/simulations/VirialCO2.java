30e5edf0fb8b9dc8700ce656fe56b4e05fe8a1f8
Andrew Schultz <ajs42@buffalo.edu>
2010-04-24 03:38:03 +0000

    add class for CO2 Mayer sampling for EPM2 model

---
+++ etomica.apps/etomica/virial/simulations/VirialCO2.java
@@ -0,0 +1,216 @@
+package etomica.virial.simulations;
+
+import etomica.action.IAction;
+import etomica.api.IAtomList;
+import etomica.api.IElement;
+import etomica.api.IIntegratorEvent;
+import etomica.api.IIntegratorListener;
+import etomica.api.ISpecies;
+import etomica.chem.elements.Carbon;
+import etomica.chem.elements.Oxygen;
+import etomica.config.IConformation;
+import etomica.data.AccumulatorRatioAverage;
+import etomica.data.IData;
+import etomica.data.types.DataGroup;
+import etomica.graphics.SimulationGraphic;
+import etomica.potential.P2CO2EMP2;
+import etomica.space.ISpace;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresHetero;
+import etomica.units.Kelvin;
+import etomica.units.Pixel;
+import etomica.util.ParameterBase;
+import etomica.util.ReadParameters;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.MayerEGeneral;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesFactory;
+import etomica.virial.SpeciesFactoryTangentSpheres;
+import etomica.virial.cluster.Standard;
+
+/**
+ * LJ simulation using Mayer sampling to evaluate cluster integrals
+ */
+public class VirialCO2 {
+
+
+    public static void main(String[] args) {
+        VirialCO2Param params = new VirialCO2Param();
+        if (args.length > 0) {
+            ReadParameters readParameters = new ReadParameters(args[0], params);
+            readParameters.readParameters();
+        }
+        final int nPoints = params.nPoints;
+        double temperature = params.temperature;
+        long steps = params.numSteps;
+        double sigmaHSRef = params.sigmaHSRef;
+
+        System.out.println("CO2 overlap sampling B"+nPoints+" at T="+temperature+" Kelvin");
+        temperature = Kelvin.UNIT.toSim(temperature);
+
+        final double[] HSB = new double[9];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        HSB[8] = Standard.B8HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B"+nPoints+"HS: "+HSB[nPoints]);
+		
+        Space space = Space3D.getInstance();
+        
+        MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(sigmaHSRef);
+        P2CO2EMP2 pTarget = new P2CO2EMP2(space);
+        MayerGeneral fTarget = new MayerGeneral(pTarget);
+        MayerEGeneral eTarget = new MayerEGeneral(pTarget);
+        ClusterAbstract targetCluster = Standard.virialCluster(nPoints, fTarget, nPoints>3, eTarget, true);
+        targetCluster.setTemperature(temperature);
+        ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
+        refCluster.setTemperature(temperature);
+
+        System.out.println((steps*1000)+" steps ("+steps+" blocks of 1000)");
+		
+        final IConformation conformation = new IConformation() {
+            
+            public void initializePositions(IAtomList atomList) {
+                // atoms are C, O and O, so we arrange them as 1-0-2
+                double bondL = 1.1491;
+                atomList.getAtom(0).getPosition().E(0);
+                atomList.getAtom(1).getPosition().E(0);
+                atomList.getAtom(1).getPosition().setX(0, -bondL);
+                atomList.getAtom(2).getPosition().E(0);
+                atomList.getAtom(2).getPosition().setX(0, +bondL);
+            }
+        };
+        SpeciesFactory factory = new SpeciesFactory() {
+            public ISpecies makeSpecies(ISpace space) {
+                SpeciesSpheresHetero species = new SpeciesSpheresHetero(space, new IElement[]{Carbon.INSTANCE, Oxygen.INSTANCE});
+                species.setChildCount(new int[]{1,2});
+                species.setConformation(conformation);
+                return species;
+            }
+        };
+        final SimulationVirialOverlap sim = new SimulationVirialOverlap(space,factory, temperature,refCluster,targetCluster,false);
+        sim.box[1].getSampleCluster().value(sim.box[1]);
+        sim.integratorOS.setNumSubSteps(1000);
+
+        if (false) {
+            double size = 10;
+            sim.box[0].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            sim.box[1].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, space, sim.getController());
+            simGraphic.getDisplayBox(sim.box[0]).setPixelUnit(new Pixel(300.0/size));
+            simGraphic.getDisplayBox(sim.box[1]).setPixelUnit(new Pixel(300.0/size));
+            simGraphic.getDisplayBox(sim.box[0]).setShowBoundary(false);
+            simGraphic.getDisplayBox(sim.box[1]).setShowBoundary(false);
+            
+            simGraphic.makeAndDisplayFrame();
+
+            sim.integratorOS.setNumSubSteps(1000);
+            sim.setAccumulatorBlockSize(1000);
+                
+            // if running interactively, set filename to null so that it doens't read
+            // (or write) to a refpref file
+            sim.getController().removeAction(sim.ai);
+            sim.getController().addAction(new IAction() {
+                public void actionPerformed() {
+                    sim.initRefPref(null, 10);
+                    sim.equilibrate(null, 20);
+                    sim.ai.setMaxSteps(Long.MAX_VALUE);
+                }
+            });
+            sim.getController().addAction(sim.ai);
+            if (Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref) || sim.refPref == 0) {
+                throw new RuntimeException("Oops");
+            }
+
+            return;
+        }
+        
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+        sim.initRefPref(refFileName, steps/100);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, steps/40);
+        if (sim.refPref == 0 || Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref)) {
+            throw new RuntimeException("oops");
+        }
+
+        System.out.println("equilibration finished");
+        sim.setAccumulatorBlockSize((int)steps);
+
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(steps);
+        for (int i=0; i<2; i++) {
+            System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize()+" "+sim.mcMoveRotate[i].getStepSize());
+        }
+
+        if (false) {
+            IIntegratorListener progressReport = new IIntegratorListener() {
+                public void integratorInitialized(IIntegratorEvent e) {}
+                public void integratorStepStarted(IIntegratorEvent e) {}
+                public void integratorStepFinished(IIntegratorEvent e) {
+                    if ((sim.integratorOS.getStepCount()*10) % sim.ai.getMaxSteps() != 0) return;
+                    System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                    double ratio = sim.dsvo.getDataAsScalar();
+                    double error = sim.dsvo.getError();
+                    System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+                }
+            };
+            sim.integratorOS.getEventManager().addListener(progressReport);
+        }
+
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
+        System.out.println("actual reference step frequency "+sim.integratorOS.getActualStepFreq0());
+
+        double ratio = sim.dsvo.getDataAsScalar();
+        double error = sim.dsvo.getError();
+        System.out.println("ratio average: "+ratio+", error: "+error);
+        System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+        IData ratioData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.RATIO.index);
+        IData ratioErrorData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index);
+        IData averageData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.AVERAGE.index);
+        IData stdevData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.STANDARD_DEVIATION.index);
+        IData errorData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.ERROR.index);
+        System.out.println("reference ratio average: "+ratioData.getValue(1)+" error: "+ratioErrorData.getValue(1));
+        System.out.println("reference   average: "+averageData.getValue(0)
+                          +" stdev: "+stdevData.getValue(0)
+                          +" error: "+errorData.getValue(0));
+        System.out.println("reference overlap average: "+averageData.getValue(1)
+                          +" stdev: "+stdevData.getValue(1)
+                          +" error: "+errorData.getValue(1));
+        
+        ratioData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.RATIO.index);
+        ratioErrorData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index);
+        averageData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.AVERAGE.index);
+        stdevData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.STANDARD_DEVIATION.index);
+        errorData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.ERROR.index);
+        System.out.println("target ratio average: "+ratioData.getValue(1)+" error: "+ratioErrorData.getValue(1));
+        System.out.println("target average: "+averageData.getValue(0)
+                          +" stdev: "+stdevData.getValue(0)
+                          +" error: "+errorData.getValue(0));
+        System.out.println("target overlap average: "+averageData.getValue(1)
+                          +" stdev: "+stdevData.getValue(1)
+                          +" error: "+errorData.getValue(1));
+	}
+
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialCO2Param extends ParameterBase {
+        public int nPoints = 2;
+        public double temperature = 300;
+        public long numSteps = 100000;
+        public double sigmaHSRef = 5*1.1491;
+    }
+}
