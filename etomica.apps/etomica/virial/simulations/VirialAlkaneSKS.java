43b497898c5e0657c52dc409b087d6162da17e53
Andrew Schultz <ajs42@buffalo.edu>
2010-04-25 00:38:32 +0000

    Add classes for SKS alkanes and CO2/SKS mixtures.

---
+++ etomica.apps/etomica/virial/simulations/VirialAlkaneSKS.java
@@ -0,0 +1,361 @@
+package etomica.virial.simulations;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import etomica.action.IAction;
+import etomica.api.IAtomType;
+import etomica.api.IIntegratorEvent;
+import etomica.api.IIntegratorListener;
+import etomica.api.ISpecies;
+import etomica.atom.DiameterHashByType;
+import etomica.atom.iterator.ApiBuilder;
+import etomica.atom.iterator.ApiIndexList;
+import etomica.atom.iterator.Atomset3IteratorIndexList;
+import etomica.atom.iterator.Atomset4IteratorIndexList;
+import etomica.data.AccumulatorRatioAverage;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.types.DataDouble;
+import etomica.data.types.DataGroup;
+import etomica.graphics.ColorSchemeRandomByMolecule;
+import etomica.graphics.DisplayBox;
+import etomica.graphics.DisplayBoxCanvasG3DSys;
+import etomica.graphics.DisplayTextBox;
+import etomica.graphics.SimulationGraphic;
+import etomica.graphics.SimulationPanel;
+import etomica.listener.IntegratorListenerAction;
+import etomica.potential.P2LennardJones;
+import etomica.potential.P3BondAngle;
+import etomica.potential.P4BondTorsion;
+import etomica.potential.PotentialGroup;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.units.CompoundDimension;
+import etomica.units.CompoundUnit;
+import etomica.units.Dimension;
+import etomica.units.DimensionRatio;
+import etomica.units.Kelvin;
+import etomica.units.Liter;
+import etomica.units.Mole;
+import etomica.units.Pixel;
+import etomica.units.Quantity;
+import etomica.units.Unit;
+import etomica.units.UnitRatio;
+import etomica.units.Volume;
+import etomica.util.ParameterBase;
+import etomica.util.ReadParameters;
+import etomica.util.Constants.CompassDirection;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.MCMoveClusterTorsionMulti;
+import etomica.virial.MayerEGeneral;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesAlkane;
+import etomica.virial.SpeciesFactorySiepmannSpheres;
+import etomica.virial.cluster.Standard;
+
+/**
+ * Mayer sampling simulation for alkanes using the SKS force field.
+ */
+public class VirialAlkaneSKS {
+
+
+    public static void main(String[] args) {
+        VirialSiepmannSpheresParam params = new VirialSiepmannSpheresParam();
+        if (args.length > 0) {
+            ReadParameters paramReader = new ReadParameters(args[0], params);
+            paramReader.readParameters();
+        }
+        final int nPoints = params.nPoints;
+        int nSpheres = params.nSpheres;
+        double temperature = params.temperature;
+        long steps = params.numSteps;
+        double refFreq = params.refFreq;
+        double sigmaCH2 = 3.93;
+        double sigmaCH3 = 3.93;
+        double sigmaHSRef = sigmaCH3 + 0.5*nSpheres;
+        final double[] HSB = new double[8];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B"+nPoints+"HS: "+HSB[nPoints]);
+		
+        Space space = Space3D.getInstance();
+        
+        MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(sigmaHSRef);
+        PotentialGroup pTargetGroup = new PotentialGroup(2);
+        System.out.println("SKS "+nSpheres+"-mer chains B"+nPoints+" at "+temperature+"K");
+        temperature = Kelvin.UNIT.toSim(temperature);
+        double epsilonCH2 = Kelvin.UNIT.toSim(47.0);
+        double epsilonCH3 = Kelvin.UNIT.toSim(114.0);
+        double epsilonCH2CH3 = Math.sqrt(epsilonCH2*epsilonCH3);
+        P2LennardJones p2CH2 = new P2LennardJones(space, sigmaCH2, epsilonCH2);
+        P2LennardJones p2CH3 = new P2LennardJones(space, sigmaCH3, epsilonCH3);
+        P2LennardJones p2CH2CH3 = new P2LennardJones(space, 0.5*(sigmaCH2+sigmaCH3), epsilonCH2CH3);
+        
+        MayerGeneral fTarget = new MayerGeneral(pTargetGroup);
+        MayerEGeneral eTarget = new MayerEGeneral(pTargetGroup);
+        ClusterAbstract targetCluster = Standard.virialCluster(nPoints, fTarget, nPoints>3, eTarget, true);
+        targetCluster.setTemperature(temperature);
+        
+        ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
+        refCluster.setTemperature(temperature);
+
+        System.out.println((steps*1000)+" steps ("+steps+" blocks of 1000)");
+        final SimulationVirialOverlapRejected sim = new SimulationVirialOverlapRejected(space,new SpeciesFactorySiepmannSpheres(space, nSpheres),
+                          temperature,refCluster,targetCluster, nSpheres > 2);
+//        ((MCMoveStepTracker)sim.mcMoveTranslate[0].getTracker()).setNoisyAdjustment(true);
+//        ((MCMoveStepTracker)sim.mcMoveTranslate[1].getTracker()).setNoisyAdjustment(true);
+        if (refFreq >= 0) {
+            sim.integratorOS.setAdjustStepFreq(false);
+            sim.integratorOS.setStepFreq0(refFreq);
+        }
+
+        SpeciesAlkane species = (SpeciesAlkane)sim.species;
+        IAtomType typeCH3 = species.getAtomType(0);
+        IAtomType typeCH2 = species.getAtomType(1);
+        pTargetGroup.addPotential(p2CH2, ApiBuilder.makeIntergroupTypeIterator(new IAtomType[]{typeCH2, typeCH2}));
+        // CH2 on molecule1 to CH3 on molecule2
+        pTargetGroup.addPotential(p2CH2CH3, ApiBuilder.makeIntergroupTypeIterator(new IAtomType[]{typeCH2, typeCH3}));
+        pTargetGroup.addPotential(p2CH2CH3, ApiBuilder.makeIntergroupTypeIterator(new IAtomType[]{typeCH3, typeCH2}));
+        pTargetGroup.addPotential(p2CH3, ApiBuilder.makeIntergroupTypeIterator(new IAtomType[]{typeCH3, typeCH3}));
+        
+        sim.integratorOS.setNumSubSteps(1000);
+
+        // create the intramolecular potential here, add to it and add it to
+        // the potential master if needed
+        PotentialGroup pIntra = sim.integrators[1].getPotentialMaster().makePotentialGroup(1);
+        if (nSpheres > 2) {
+            P3BondAngle p3 = new P3BondAngle(space);
+            p3.setAngle(Math.PI*114.0/180.0);
+            p3.setEpsilon(Kelvin.UNIT.toSim(62500));
+            int[][] triplets = new int[nSpheres-2][3];
+            for (int i=0; i<nSpheres-2; i++) {
+                triplets[i][0] = i;
+                triplets[i][1] = i+1;
+                triplets[i][2] = i+2;
+            }
+            pIntra.addPotential(p3, new Atomset3IteratorIndexList(triplets));
+            // integrators share a common potentialMaster.  so just add to one
+            sim.integrators[1].getPotentialMaster().addPotential(pIntra,new ISpecies[]{sim.species});
+        }
+        MCMoveClusterTorsionMulti[] torsionMoves = null;
+        if (nSpheres > 3) {
+            P4BondTorsion p4 = new P4BondTorsion(space, 0, Kelvin.UNIT.toSim(355.03), Kelvin.UNIT.toSim(-68.19), Kelvin.UNIT.toSim(791.32));
+            int[][] quads = new int[nSpheres-3][4];
+            for (int i=0; i<nSpheres-3; i++) {
+                quads[i][0] = i;
+                quads[i][1] = i+1;
+                quads[i][2] = i+2;
+                quads[i][3] = i+3;
+            }
+            pIntra.addPotential(p4, new Atomset4IteratorIndexList(quads));
+            torsionMoves = new MCMoveClusterTorsionMulti[2];
+            torsionMoves[0] = new MCMoveClusterTorsionMulti(sim.integrators[1].getPotentialMaster(), space, sim.getRandom(), 1.0, p4, 40);
+            torsionMoves[0].setTemperature(temperature);
+            sim.integrators[0].getMoveManager().addMCMove(torsionMoves[0]);
+            torsionMoves[1] = new MCMoveClusterTorsionMulti(sim.integrators[1].getPotentialMaster(), space, sim.getRandom(), 1.0, p4, 40);
+            torsionMoves[1].setTemperature(temperature);
+            sim.integrators[1].getMoveManager().addMCMove(torsionMoves[1]);
+        }
+        if (nSpheres > 4) {
+            pIntra.addPotential(p2CH3,new ApiIndexList(new int[][]{{0,nSpheres-1}}));
+        }
+        if (nSpheres > 5) {
+            int[][] pairs = new int[2*(nSpheres-5)][2];
+            for (int i=0; i<nSpheres-5; i++) {
+                pairs[2*i][0] = 0;
+                pairs[2*i][1] = nSpheres-2-i;
+                pairs[2*i+1][0] = nSpheres-1;
+                pairs[2*i+1][1] = i+1;
+            }
+            pIntra.addPotential(p2CH2CH3,new ApiIndexList(pairs));
+        }
+        if (nSpheres > 6) {
+            int[][] pairs = new int[(nSpheres-6)*(nSpheres-5)/2][2];
+            int k = 0;
+            for (int i=1; i<nSpheres-5; i++) {
+                for (int j=i+4; j<nSpheres-1; j++) {
+                    pairs[k][0] = i;
+                    pairs[k][1] = j;
+                    k++;
+                }
+            }
+            pIntra.addPotential(p2CH2,new ApiIndexList(pairs));
+        }
+
+        if (true) {
+            double size = (nSpheres+5)*1.5;
+            sim.box[0].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            sim.box[1].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, space, sim.getController());
+            DisplayBox displayBox0 = simGraphic.getDisplayBox(sim.box[0]); 
+            DisplayBox displayBox1 = simGraphic.getDisplayBox(sim.box[1]);
+            displayBox0.setPixelUnit(new Pixel(300.0/size));
+            displayBox1.setPixelUnit(new Pixel(300.0/size));
+            displayBox0.setShowBoundary(false);
+            displayBox1.setShowBoundary(false);
+            ((DisplayBoxCanvasG3DSys)displayBox0.canvas).setBackgroundColor(Color.WHITE);
+            ((DisplayBoxCanvasG3DSys)displayBox1.canvas).setBackgroundColor(Color.WHITE);
+            
+            
+            DiameterHashByType diameterManager = (DiameterHashByType)displayBox0.getDiameterHash();
+            diameterManager.setDiameter(typeCH2, 0.8*sigmaCH2);
+            diameterManager.setDiameter(typeCH3, 0.8*sigmaCH3);
+            displayBox1.setDiameterHash(diameterManager);
+            ColorSchemeRandomByMolecule colorScheme = new ColorSchemeRandomByMolecule(sim, sim.box[0], sim.getRandom());
+            displayBox0.setColorScheme(colorScheme);
+            colorScheme = new ColorSchemeRandomByMolecule(sim, sim.box[1], sim.getRandom());
+            displayBox1.setColorScheme(colorScheme);
+            simGraphic.makeAndDisplayFrame();
+
+            sim.integratorOS.setNumSubSteps(1000);
+            sim.setAccumulatorBlockSize(1000);
+                
+            // if running interactively, set filename to null so that it doens't read
+            // (or write) to a refpref file
+            sim.getController().removeAction(sim.ai);
+            sim.getController().addAction(new IAction() {
+                public void actionPerformed() {
+                    sim.initRefPref(null, 10);
+                    sim.equilibrate(null, 20);
+                    sim.ai.setMaxSteps(Long.MAX_VALUE);
+                }
+            });
+            sim.getController().addAction(sim.ai);
+            if ((Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref) || sim.refPref == 0)) {
+                throw new RuntimeException("Oops");
+            }
+            
+            final DisplayTextBox averageBox = new DisplayTextBox();
+            averageBox.setLabel("Average");
+            final DisplayTextBox errorBox = new DisplayTextBox();
+            errorBox.setLabel("Error");
+            JLabel jLabelPanelParentGroup = new JLabel("B"+nPoints+" (L/mol)^"+(nPoints-1));
+            final JPanel panelParentGroup = new JPanel(new java.awt.BorderLayout());
+            panelParentGroup.add(jLabelPanelParentGroup,CompassDirection.NORTH.toString());
+            panelParentGroup.add(averageBox.graphic(), java.awt.BorderLayout.WEST);
+            panelParentGroup.add(errorBox.graphic(), java.awt.BorderLayout.EAST);
+            simGraphic.getPanel().controlPanel.add(panelParentGroup, SimulationPanel.getVertGBC());
+            
+            IAction pushAnswer = new IAction() {
+                public void actionPerformed() {
+                    double ratio = sim.dsvo.getDataAsScalar() * HSB[nPoints];
+                    double error = sim.dsvo.getError() * HSB[nPoints];
+                    data.x = ratio;
+                    averageBox.putData(data);
+                    data.x = error;
+                    errorBox.putData(data);
+                }
+                
+                DataDouble data = new DataDouble();
+            };
+            IEtomicaDataInfo dataInfo = new DataDouble.DataInfoDouble("B"+nPoints, new CompoundDimension(new Dimension[]{new DimensionRatio(Volume.DIMENSION, Quantity.DIMENSION)}, new double[]{nPoints-1}));
+            Unit unit = new CompoundUnit(new Unit[]{new UnitRatio(Liter.UNIT, Mole.UNIT)}, new double[]{nPoints-1});
+            averageBox.putDataInfo(dataInfo);
+            averageBox.setLabel("average");
+            averageBox.setUnit(unit);
+            errorBox.putDataInfo(dataInfo);
+            errorBox.setLabel("error");
+            errorBox.setPrecision(2);
+            errorBox.setUnit(unit);
+            sim.integratorOS.getEventManager().addListener(new IntegratorListenerAction(pushAnswer));
+            
+            return;
+        }
+        
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+        sim.initRefPref(refFileName, steps/40);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, steps/20);
+        
+        sim.setAccumulatorBlockSize((int)steps);
+        
+        System.out.println("equilibration finished");
+        System.out.println("MC Move step sizes (ref)    "+sim.mcMoveTranslate[0].getStepSize()+" "
+                +sim.mcMoveRotate[0].getStepSize()+" "
+                +(sim.mcMoveWiggle==null ? "" : (""+sim.mcMoveWiggle[0].getStepSize())));
+        System.out.println("MC Move step sizes (target) "+sim.mcMoveTranslate[1].getStepSize()+" "
+                +sim.mcMoveRotate[1].getStepSize()+" "
+                +(sim.mcMoveWiggle==null ? "" : (""+sim.mcMoveWiggle[1].getStepSize())));
+        if (nSpheres > 3) {
+            System.out.println("Torsion move acceptance "+torsionMoves[0].getTracker().acceptanceRatio()+" "+
+                    torsionMoves[1].getTracker().acceptanceRatio());
+        }
+
+        if (false) {
+            IIntegratorListener progressReport = new IIntegratorListener() {
+                public void integratorInitialized(IIntegratorEvent e) {}
+                public void integratorStepStarted(IIntegratorEvent e) {}
+                public void integratorStepFinished(IIntegratorEvent e) {
+                    if ((sim.integratorOS.getStepCount()*10) % sim.ai.getMaxSteps() != 0) return;
+                    System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                    double ratio = sim.dsvo.getDataAsScalar();
+                    double error = sim.dsvo.getError();
+                    System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+                }
+            };
+            sim.integratorOS.getEventManager().addListener(progressReport);
+        }
+
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(steps);
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
+        System.out.println("actual reference step frequency "+sim.integratorOS.getActualStepFreq0());
+        
+        double ratio = sim.dsvo.getDataAsScalar();
+        double error = sim.dsvo.getError();
+        System.out.println("ratio average: "+ratio+", error: "+error);
+        System.out.println("abs average: "+ratio*HSB[nPoints]+", error: "+error*HSB[nPoints]);
+        IData ratioData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.RATIO.index);
+        IData ratioErrorData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index);
+        IData averageData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.AVERAGE.index);
+        IData stdevData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.STANDARD_DEVIATION.index);
+        IData errorData = ((DataGroup)sim.accumulators[0].getData()).getData(AccumulatorRatioAverage.StatType.ERROR.index);
+        System.out.println("reference ratio average: "+ratioData.getValue(1)+" error: "+ratioErrorData.getValue(1));
+        System.out.println("reference   average: "+averageData.getValue(0)
+                          +" stdev: "+stdevData.getValue(0)
+                          +" error: "+errorData.getValue(0));
+        System.out.println("reference overlap average: "+averageData.getValue(1)
+                          +" stdev: "+stdevData.getValue(1)
+                          +" error: "+errorData.getValue(1));
+        
+        ratioData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.RATIO.index);
+        ratioErrorData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.RATIO_ERROR.index);
+        averageData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.AVERAGE.index);
+        stdevData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.STANDARD_DEVIATION.index);
+        errorData = ((DataGroup)sim.accumulators[1].getData()).getData(AccumulatorRatioAverage.StatType.ERROR.index);
+        System.out.println("target ratio average: "+ratioData.getValue(1)+" error: "+ratioErrorData.getValue(1));
+        System.out.println("target average: "+averageData.getValue(0)
+                          +" stdev: "+stdevData.getValue(0)
+                          +" error: "+errorData.getValue(0));
+        System.out.println("target overlap average: "+averageData.getValue(1)
+                          +" stdev: "+stdevData.getValue(1)
+                          +" error: "+errorData.getValue(1));
+	}
+
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialSiepmannSpheresParam extends ParameterBase {
+        public int nPoints = 2;
+        public int nSpheres = 6;
+        public double temperature = 300.0;   // Kelvin
+        public long numSteps = 10000;
+        public double refFreq = -1;
+    }
+}
