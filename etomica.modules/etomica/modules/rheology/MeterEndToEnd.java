f739410422fd0542d915471de28e1812ea1742e2
Andrew Schultz <ajs42@buffalo.edu>
2010-04-30 16:49:21 +0000

    Add end-to-end distance meter

---
+++ etomica.modules/etomica/modules/rheology/MeterEndToEnd.java
@@ -0,0 +1,44 @@
+package etomica.modules.rheology;
+
+import etomica.api.IAtomList;
+import etomica.api.IBox;
+import etomica.api.IMoleculeList;
+import etomica.api.IVectorMutable;
+import etomica.data.DataSourceScalar;
+import etomica.space.ISpace;
+import etomica.units.CompoundDimension;
+import etomica.units.Dimension;
+import etomica.units.Length;
+
+/**
+ * Meter that measures that end to end distance of the molecules
+ *
+ * @author Andrew Schultz
+ */
+public class MeterEndToEnd extends DataSourceScalar {
+
+    public MeterEndToEnd(ISpace space) {
+        super("end-to-end distance^2", new CompoundDimension(new Dimension[]{Length.DIMENSION}, new double[]{2}));
+        dr = space.makeVector();
+    }
+
+    public void setBox(IBox newBox) {
+        box = newBox;
+    }
+    
+    public double getDataAsScalar() {
+        IMoleculeList molecules = box.getMoleculeList();
+        double ee_tot = 0;
+        for (int i=0; i<molecules.getMoleculeCount(); i++) {
+            IAtomList atoms = molecules.getMolecule(i).getChildList();
+            dr.E(atoms.getAtom(atoms.getAtomCount()-1).getPosition());
+            dr.ME(atoms.getAtom(0).getPosition());
+            box.getBoundary().nearestImage(dr);
+            ee_tot += dr.squared();
+        }
+        return ee_tot/molecules.getMoleculeCount();
+    }
+
+    protected IBox box;
+    protected final IVectorMutable dr;
+}
