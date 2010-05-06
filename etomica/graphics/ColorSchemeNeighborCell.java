6ee0abd8ba8b47fc890824d9113bfa8a254fa2f7
Andrew Schultz <ajs42@buffalo.edu>
2010-05-06 13:29:40 +0000

    rename Neighbor=>NeighborCell since that's actually what it does

---
+++ etomica/graphics/ColorSchemeNeighborCell.java
@@ -0,0 +1,65 @@
+package etomica.graphics;
+
+import java.awt.Color;
+
+import etomica.api.IAtom;
+import etomica.api.IAtomList;
+import etomica.api.IBox;
+import etomica.api.ISimulation;
+import etomica.nbr.PotentialMasterNbr;
+import etomica.nbr.cell.Api1ACell;
+
+/**
+ * Colors atoms based on being in a neighboring cell to a reference atom.
+ */
+public class ColorSchemeNeighborCell extends ColorSchemeCollectiveAgent {
+    
+    public ColorSchemeNeighborCell(ISimulation sim, PotentialMasterNbr potentialMaster, IBox box, int dim) {
+        super(box);
+        typeColorScheme = new ColorSchemeByType(sim);
+        leafList = box.getLeafList();
+        nbrIterator = new Api1ACell(dim, 1.0, potentialMaster.getCellAgentManager());
+        nbrIterator.setDirection(null);
+        nbrIterator.setBox(box);
+    }
+    
+    public void colorAllAtoms() {
+		//color all atoms according to their type
+        int nLeaf = leafList.getAtomCount();
+        for (int iLeaf=0; iLeaf<nLeaf; iLeaf++) {
+            IAtom atom = leafList.getAtom(iLeaf);
+            agentManager.setAgent(atom, typeColorScheme.getAtomColor(atom));
+        }
+        if (referenceAtom == null) {
+            return;
+        }
+        //color blue the neighbor atoms in same group
+        nbrIterator.reset();
+        for (IAtomList pair = nbrIterator.next(); pair != null;
+             pair = nbrIterator.next()) {
+            IAtom atom = pair.getAtom(1);
+            if(atom.getType() == referenceAtom.getType()) {
+                agentManager.setAgent(atom, Color.blue);
+            } else {
+                agentManager.setAgent(atom, Color.yellow);
+            }
+        }
+        //color green the target atom 
+        agentManager.setAgent(referenceAtom, Color.green);
+    }
+    
+    public void setAtom(IAtom a) {
+        referenceAtom = a;
+        nbrIterator.setTarget(a);
+    }
+
+    public IAtom getAtom() {
+        return referenceAtom;
+    }
+    
+    private static final long serialVersionUID = 1L;
+    private IAtom referenceAtom;
+    private final Api1ACell nbrIterator;
+    private final IAtomList leafList;
+    private final ColorSchemeByType typeColorScheme;
+}
