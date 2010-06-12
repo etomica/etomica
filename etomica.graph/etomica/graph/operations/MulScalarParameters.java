c170248d44f8852a6f370df07b01fcd598c1b07e
Andrew Schultz <ajs42@buffalo.edu>
2010-06-12 23:28:20 +0000

    properly handle negative coefficients

--- etomica.graph/etomica/graph/operations/MulScalarParameters.java
+++ etomica.graph/etomica/graph/operations/MulScalarParameters.java
@@ -8,7 +8,17 @@ public class MulScalarParameters implements Parameters {
     private final Coefficient factor;
 
     public MulScalarParameters(int numerator, int denominator) {
-        factor = new CoefficientImpl(numerator, denominator);
+      int sign = 1;
+      if (numerator*denominator < 0) {
+        sign = -1;
+      }
+      if (numerator < 0) {
+        numerator = -numerator;
+      }
+      if (denominator < 0) {
+        denominator = -denominator;
+      }
+      factor = new CoefficientImpl(numerator, denominator, sign);
     }
 
     public MulScalarParameters(Coefficient factor) {
