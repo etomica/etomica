45fd32ce383f45d1ba11e6cfc55f5fef7193f7bd
Andrew Schultz <ajs42@buffalo.edu>
2010-06-12 05:17:03 +0000

    add a convenience constructor

--- etomica.graph/etomica/graph/operations/MulScalarParameters.java
+++ etomica.graph/etomica/graph/operations/MulScalarParameters.java
@@ -1,10 +1,15 @@
 package etomica.graph.operations;
 
 import etomica.graph.model.Coefficient;
+import etomica.graph.model.impl.CoefficientImpl;
 
 public class MulScalarParameters implements Parameters {
 
     private final Coefficient factor;
+
+    public MulScalarParameters(int numerator, int denominator) {
+        factor = new CoefficientImpl(numerator, denominator);
+    }
 
     public MulScalarParameters(Coefficient factor) {
         this.factor = factor;
