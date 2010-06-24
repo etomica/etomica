d513557b9f2d5ed2b16045255916489f8f9f9557
Andrew Schultz <ajs42@buffalo.edu>
2010-06-24 16:10:46 +0000

    allow decoration operation to be specified in terms of factors rather than colors

--- etomica.graph/etomica/graph/operations/Decorate.java
+++ etomica.graph/etomica/graph/operations/Decorate.java
@@ -1,6 +1,8 @@
 package etomica.graph.operations;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import etomica.graph.model.Graph;
@@ -18,49 +20,48 @@ public class Decorate implements Binary {
 
     assert (params instanceof DecorateParameters);
     char color = ((DecorateParameters)params).color;
+    int factor = ((DecorateParameters)params).factor;
     MulFlexibleParameters mfp = ((DecorateParameters)params).mfp;
     Set<Graph> result = new HashSet<Graph>();
-    int maxPow = 0;
+    List<Set<Graph>> allSet1 = new ArrayList<Set<Graph>>();
     for (Graph g : argument) {
       int colorCount = 0;
-      for (Node node : g.nodes()) {
-        if (node.getColor() == color) {
-          colorCount++;
+      if (factor == -1) {
+        for (Node node : g.nodes()) {
+          if (node.getColor() == color) {
+            colorCount++;
+          }
         }
       }
-      if (colorCount > maxPow) {
-        maxPow = colorCount;
-      }
-    }
-    Set<Graph>[] allSet1 = new Set[maxPow+1];
-    for (int i=1; i<allSet1.length; i++) {
-      allSet1[i] = new HashSet<Graph>();
-    }
-    for (Graph g : argument) {
-      int colorCount = 0;
-      for (Node node : g.nodes()) {
-        if (node.getColor() == color) {
-          colorCount++;
-        }
+      else {
+        colorCount = g.factors()[factor];
       }
       if (colorCount == 0) {
         result.add(g);
+        continue;
       }
-      else {
-        allSet1[colorCount].add(g);
+      while (colorCount > allSet1.size()-1) {
+        allSet1.add(new HashSet<Graph>());
       }
+      allSet1.get(colorCount).add(g);
     }
     // set2Pow holds (set2)^i
-    Set<Graph> set2Pow = argument2;
+    Set<Graph> set2Pow = new HashSet<Graph>();
+    set2Pow.addAll(argument2);
     MulFlexible mulFlex = new MulFlexible();
     IsoFree isoFree = new IsoFree();
-    for (int i=1; i<allSet1.length; i++) {
-      result.addAll(mulFlex.apply(allSet1[i], set2Pow, mfp));
+    for (int i=1; i<allSet1.size(); i++) {
+      result.addAll(mulFlex.apply(allSet1.get(i), set2Pow, mfp));
       result = isoFree.apply(result, null);
 
-      if (i+1<allSet1.length) {
+      if (i+1<allSet1.size()) {
         // we're going to make another pass.  calculate (set2)^(i+1)
         set2Pow = isoFree.apply(mulFlex.apply(set2Pow, argument2, mfp), null);
+      }
+    }
+    if (factor != -1) {
+      for (Graph g : result) {
+        g.factors()[factor] = 0;
       }
     }
     return result;
@@ -68,9 +69,16 @@ public class Decorate implements Binary {
 
   public static class DecorateParameters implements Parameters {
     public final char color;
+    public final int factor;
     public final MulFlexibleParameters mfp;
     public DecorateParameters(char color, MulFlexibleParameters mfp) {
       this.color = color;
+      this.mfp = mfp;
+      factor = -1;
+    }
+    public DecorateParameters(int factor, MulFlexibleParameters mfp) {
+      this.factor = factor;
+      this.color = '-';
       this.mfp = mfp;
     }
   }
