0cdd3b4664b5dcde10ecc7fe63c72774be4844e8
Andrew Schultz <ajs42@buffalo.edu>
2010-06-29 03:02:08 +0000

    add an identical graph filter

---
+++ etomica.graph/etomica/graph/iterators/filters/IdenticalGraphFilter.java
@@ -0,0 +1,38 @@
+package etomica.graph.iterators.filters;
+
+import java.util.Set;
+
+import etomica.graph.model.Graph;
+import etomica.graph.model.GraphIterator;
+
+/**
+ * Filter that combines graphs that are identical
+ *
+ * @author Andrew Schultz
+ */
+public class IdenticalGraphFilter extends GlobalFilter {
+
+  public IdenticalGraphFilter(GraphIterator iterator) {
+
+    super(iterator);
+  }
+
+  protected boolean accept(Graph g1, Set<Graph> set) {
+
+    boolean result = true;
+    if (!set.isEmpty()) {
+      for (Graph isoGraph : set) {
+        // test for identical graphs and, if they do match, update the graph
+        // coefficient and reject the new graph
+        if (isoGraph.compareTo(g1) == 0) {
+          isoGraph.coefficient().add(g1.coefficient());
+          result = false;
+          if (isoGraph.coefficient().getNumerator() == 0) {
+            set.remove(isoGraph);
+          }
+        }
+      }
+    }
+    return result;
+  }
+}
