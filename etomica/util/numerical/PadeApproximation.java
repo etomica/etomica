87a632947ad9b87ad28fdf94814f0157c1bfd4b8
Tai Boon Tan <taitan@buffalo.edu>
2010-07-01 18:18:13 +0000

    update

--- etomica/util/numerical/PadeApproximation.java
+++ etomica/util/numerical/PadeApproximation.java
@@ -22,7 +22,16 @@ import Jama.Matrix;
  */
 public class PadeApproximation {
 	public PadeApproximation(double[] b, int K, int L){
-		if((K+L) > b.length){
+		/*
+		 * checking the order and we do not allow K+L > M
+		 * K and L is the order of the power series of the numerator and denominator
+		 *  or the rational function respectively
+		 *  
+		 * M is the order of the examined polynomial
+		 */
+		int M = (b.length -1);
+		
+		if((K+L) > M){
 			throw new RuntimeException("K plus L should not exceed the M-order");
 		}
 		
@@ -30,8 +39,8 @@ public class PadeApproximation {
 		a = new double[K+1];
 		c = new double[L+1];
 		
-		double[][] y = new double[L][L];
-		double[] z = new double [L];
+		double[][] y = new double[M-K][M-K];
+		double[] z = new double [M-K];
 		
 		for (int icol=0; icol<L; icol++){
 			z[icol] = -b[K+1+icol];
@@ -40,25 +49,23 @@ public class PadeApproximation {
 		
 		for (int irow=0; irow<L; irow++){
 			for (int icol=0; icol<L; icol++ ){
-				y[irow][icol] = b[K+irow-icol]; 
-				
+				if((K+irow-icol)< 0.0) {
+					y[irow][icol] = 0.0;
+				} else {
+					y[irow][icol] = b[K+irow-icol]; 
+				}
 			}
 		}
 		
-		Y = new Matrix(y);
-		System.out.println("yMAtrix Col: " + Y.getColumnDimension());
-		System.out.println("yMAtrix Row: " + Y.getRowDimension());
+		Y = new Matrix(y);		
 		Z = new Matrix(z, z.length);
-		System.out.println("zMAtrix Col: " + Z.getColumnDimension());
-		System.out.println("zMAtrix Row: " + Z.getRowDimension());
+		
 	}
 	
 	public void solveCoefficients(){
 		
 		Matrix cMatrix = Y.solve(Z);
-		System.out.println("cMAtrix Col: " + cMatrix.getColumnDimension());
-		System.out.println("cMAtrix Row: " + cMatrix.getRowDimension());
-		
+	
 		/*
 		 * determine "c" coefficients
 		 */
@@ -71,11 +78,13 @@ public class PadeApproximation {
 		 * determine "a" coefficients
 		 */
 		for (int i=0; i<a.length; i++){
-			for (int j=i; j>=0; j--){
-				a[i] += b[j]*c[i-j];
+			for (int j=0; j<c.length; j++){
+				if((i-j)< 0.0 ) break;
+				
+				a[i] += b[i-j]*c[j];
+				
 			}
 		}
-		
 	}
 	
 	public double[] getA(){
@@ -88,9 +97,9 @@ public class PadeApproximation {
 	
 	public static void main(String[] args){
 		
-		double[] b = new double []{1.0, 4.0, 4.0, 5.0, 6.0};
-		int K = 2;
-		int L = 2;
+		double[] b = new double []{1.0, 2.0, 3.0, 4.0, 5.0,2.0};
+		int K = 4;
+		int L = 1;
 		PadeApproximation pade = new PadeApproximation(b, K, L);
 		pade.solveCoefficients();
 		
