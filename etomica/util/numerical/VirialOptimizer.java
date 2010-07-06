e1a19b84de5809f066e7dd5244e0a2a3d7749b63
Tai Boon Tan <taitan@buffalo.edu>
2010-07-06 20:40:54 +0000

    update

--- etomica/util/numerical/VirialOptimizer.java
+++ etomica/util/numerical/VirialOptimizer.java
@@ -57,21 +57,55 @@ public class VirialOptimizer {
 		}
 	}
 	
-	public void optimizeHigherbVirial(int K, int L, double bGuess){
+	public void optimizeHigherbVirial(int K, int L, double initbGuess){
 		
-		//double bGuess = 3.1;
-		
-		calcpPade(K, L, bGuess);
+		double interval = 0.0001;
+		bGuess = initbGuess;
+		calcpPade(K, L, initbGuess);
 		
 		double TSS = 0.0;
 		
 		for (int i=0; i<pPade.length; i++){
 			diff = (pPade[i]-pSimData[i][1]);
-		//	System.out.println(i +" " + diff);
 			TSS += diff*diff;
 			
 		}
-		System.out.print( TSS);
+				
+		while(TSS > tol){
+			double TSSleft = 0.0;
+			double TSSright = 0.0;
+			double bGuessleft  = bGuess - interval;
+			double bGuessright = bGuess + interval;
+			
+			calcpPade(K, L, bGuessleft);
+			for (int i=0; i<pPade.length; i++){
+				diff = (pPade[i]-pSimData[i][1]);
+				TSSleft += diff*diff;	
+			}
+			
+			calcpPade(K, L, bGuessright);
+			for (int i=0; i<pPade.length; i++){
+				diff = (pPade[i]-pSimData[i][1]);
+				TSSright += diff*diff;	
+			}
+
+			if(Math.abs(TSSright) <= Math.abs(TSSleft)){
+				TSS = TSSright;
+				bGuess = bGuessright;
+			} else {
+				TSS = TSSleft;
+				bGuess = bGuessleft;
+			}
+			/*
+			 * extra check
+			 * break out from the while loop if the difference is small
+			 *  although TSS does not meet the tolerance condition
+			 */
+			if (Math.abs(TSSright-TSSleft) < 1e-9) break;
+		}
+		
+		
+		System.out.println(bGuess +" " + TSS);
 	}
 	
 	
@@ -81,14 +115,14 @@ public class VirialOptimizer {
 		
 		VirialOptimizer vOpt = new VirialOptimizer(filenameP, filenameB);
 		
-		for (double i = 3.0; i < 3.1; i+=0.001){
-			System.out.print("\n" + i +" ");
-			vOpt.optimizeHigherbVirial(5, 3, i);		
-		}
+		vOpt.optimizeHigherbVirial(5, 3, 2.0);		
 	
 	}
 	
 	protected double diff;
 	protected double[] bVirial, rho, pPade;
 	protected double[][] pSimData, bVirialFromFile;
+	protected double tol = 1e-4;
+	protected double bGuess;
+	
 }
