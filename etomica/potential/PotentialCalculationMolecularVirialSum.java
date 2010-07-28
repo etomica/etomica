b5892da92430c0d3937400d8b9b286d8997dcd1d
Tai Boon Tan <taitan@buffalo.edu>
2010-07-28 22:15:59 +0000

    PotentialCalculation class that does virial sum for molecular model

---
+++ etomica/potential/PotentialCalculationMolecularVirialSum.java
@@ -0,0 +1,49 @@
+package etomica.potential;
+
+import etomica.api.IAtomList;
+import etomica.api.IMoleculeList;
+import etomica.api.IPotentialAtomic;
+import etomica.api.IPotentialMolecular;
+
+/**
+ * Evaluates the virial summed over all iterated molecules.
+ *
+ * @author Tai Boon Tan
+ */
+public class PotentialCalculationMolecularVirialSum implements PotentialCalculationMolecular {
+		
+	
+	public void doCalculation(IAtomList atoms, IPotentialAtomic potential) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void doCalculation(IMoleculeList molecules, IPotentialMolecular potential) {
+		if (!(potential instanceof PotentialMolecularSoft)) {
+			return;
+		}	
+		sum += ((PotentialMolecularSoft)potential).virial(molecules);
+	}
+	
+	/**
+	 * Sets the virial sum to zero, typically to begin a new virial-sum calculation.
+	 * @return this instance, so the method can be called in-line as the instance is
+	 * passed to the PotentialMaster.
+	 */
+	public PotentialCalculationMolecularVirialSum zeroSum() {
+		sum = 0.0;
+		return this;
+	}
+
+    private static final long serialVersionUID = 1L;
+
+	/**
+	 * Returns the current value of the energy sum.
+	 */
+	public double getSum() {return sum;}
+	
+	private double sum = 0.0;
+
+
+
+ }//end VirialSum
