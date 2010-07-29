2c9464ceefb67663fbf4bc3be86b2958055df2d9
Andrew Schultz <ajs42@buffalo.edu>
2010-07-29 19:56:44 +0000

    add ability to compute dy/dx avoid recomputing spline intermediates when nothing changes

--- etomica/util/numerical/AkimaSpline.java
+++ etomica/util/numerical/AkimaSpline.java
@@ -22,6 +22,7 @@ public class AkimaSpline {
         x = new double[0];
         y = new double[0];
         interpolatedY = new double[0];
+        interpolatedDy = new double[0];
     }
 
     public double[] doInterpolation(double[] interpolatedX) {
@@ -34,38 +35,42 @@ public class AkimaSpline {
             interpolatedY = new double[interpolatedX.length];
         }
 
-        for (int i=0; i<N-1; i++) {
-            m[i] = (y[i+1]-y[i])/(x[i+1]-x[i]);
+        if (dirty) {
+            for (int i=0; i<N-1; i++) {
+                m[i] = (y[i+1]-y[i])/(x[i+1]-x[i]);
+            }
+    
+            // special-case first t
+            t[0] = 0.5 * (3*m[0] - m[1]);
+            for (int i=1; i<N-1; i++) {
+                double am2m1;
+                if (i>1) {
+                    // special-case second t
+                    am2m1 = Math.abs(m[i-1]-m[i-2]);
+                }
+                else {
+                    am2m1 = Math.abs(m[1] - m[0]);
+                }
+                double am4m3;
+                if (i<N-2) {
+                    am4m3 = Math.abs(m[i+1]-m[i]);
+                }
+                else {
+                    // special-case next-to-last t
+                    am4m3 = Math.abs(m[N-2] - m[N-3]);
+                }
+                if (am2m1 != am4m3) {
+                    t[i] = (am4m3*m[i-1] + am2m1*m[i])/(am4m3 + am2m1);
+                }
+                else {
+                    t[i] = 0.5 * (m[i-1] + m[i]);
+                }
+            }
+            // special-case last t
+            t[N-1] = 0.5 * (3*m[N-2]-m[N-3]);
         }
-
-        // special-case first t
-        t[0] = 0.5 * (3*m[0] - m[1]);
-        for (int i=1; i<N-1; i++) {
-            double am2m1;
-            if (i>1) {
-                // special-case second t
-                am2m1 = Math.abs(m[i-1]-m[i-2]);
-            }
-            else {
-                am2m1 = Math.abs(m[1] - m[0]);
-            }
-            double am4m3;
-            if (i<N-2) {
-                am4m3 = Math.abs(m[i+1]-m[i]);
-            }
-            else {
-                // special-case next-to-last t
-                am4m3 = Math.abs(m[N-2] - m[N-3]);
-            }
-            if (am2m1 != am4m3) {
-                t[i] = (am4m3*m[i-1] + am2m1*m[i])/(am4m3 + am2m1);
-            }
-            else {
-                t[i] = 0.5 * (m[i-1] + m[i]);
-            }
-        }
-        // special-case last t
-        t[N-1] = 0.5 * (3*m[N-2]-m[N-3]);
+        
+        dirty = false;
 
         int j = 0;
         for (int i=0; j<interpolatedX.length && i<N-1; i++) {
@@ -84,7 +89,69 @@ public class AkimaSpline {
         }
         return interpolatedY;
     }
-    
+
+    public double[] doInterpolationDy(double[] interpolatedX) {
+        int N = x.length;
+        if (t.length != N) {
+            m = new double[N-1];
+            t = new double[N];
+        }
+        if (interpolatedDy == null || interpolatedDy.length != interpolatedX.length) {
+            interpolatedDy = new double[interpolatedX.length];
+        }
+
+        if (dirty) {
+            for (int i=0; i<N-1; i++) {
+                m[i] = (y[i+1]-y[i])/(x[i+1]-x[i]);
+            }
+
+            // special-case first t
+            t[0] = 0.5 * (3*m[0] - m[1]);
+            for (int i=1; i<N-1; i++) {
+                double am2m1;
+                if (i>1) {
+                    // special-case second t
+                    am2m1 = Math.abs(m[i-1]-m[i-2]);
+                }
+                else {
+                    am2m1 = Math.abs(m[1] - m[0]);
+                }
+                double am4m3;
+                if (i<N-2) {
+                    am4m3 = Math.abs(m[i+1]-m[i]);
+                }
+                else {
+                    // special-case next-to-last t
+                    am4m3 = Math.abs(m[N-2] - m[N-3]);
+                }
+                if (am2m1 != am4m3) {
+                    t[i] = (am4m3*m[i-1] + am2m1*m[i])/(am4m3 + am2m1);
+                }
+                else {
+                    t[i] = 0.5 * (m[i-1] + m[i]);
+                }
+            }
+            // special-case last t
+            t[N-1] = 0.5 * (3*m[N-2]-m[N-3]);
+        }
+
+        int j = 0;
+        for (int i=0; j<interpolatedX.length && i<N-1; i++) {
+            if (i < N-2 && interpolatedX[j] > x[i+1]) continue;
+            double p1 = t[i];
+            double dx = (x[i+1]-x[i]);
+            double p2 = (3*m[i] - 2*t[i] - t[i+1])/dx;
+            double p3 = (-2*m[i] + t[i] + t[i+1])/(dx*dx);
+
+            while (j < interpolatedX.length && (i == N-2 || interpolatedX[j] <= x[i+1])) {
+                dx = interpolatedX[j] - x[i];
+                interpolatedDy[j] = p1 + 2*p2*dx + 3*p3*dx*dx;
+                j++;
+            }
+        }
+        return interpolatedDy;
+    }
+
     public static void main(String[] args) {
         FileReader fileReader;
         String infile = "B7.dat.scaled";
@@ -135,11 +202,16 @@ public class AkimaSpline {
         }
     }
 
-    // hack for main method to prep for doInterpolation
+    public void markDirty() {
+        dirty = true;
+    }
+
     public void setInputData(double[] x, double[] y) {
         this.x = x;
         this.y = y;
+        dirty = true;
     }        
 
-    protected double[] m, t, x, y, interpolatedY;
+    protected double[] m, t, x, y, interpolatedY, interpolatedDy;
+    protected boolean dirty = false;
 }
