8c0764310c31aff21124f68e859dd6f37727d25f
Andrew Schultz <ajs42@buffalo.edu>
2010-09-09 17:37:05 +0000

    we don't look at neighbors use a better value for radiusInner

--- etomica.apps/etomica/normalmode/P1Constraint.java
+++ etomica.apps/etomica/normalmode/P1Constraint.java
@@ -9,87 +9,41 @@ import etomica.space.ISpace;
 
 public class P1Constraint extends Potential1{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public P1Constraint(ISpace space, Primitive primitive, IBox box, CoordinateDefinition coordinateDefinition) {
-		super(space);
-		
-		siteIndex = box.getLeafList().getAtomCount();
-		latticeSite = space.makeVectorArray(siteIndex);
-				
-		double neighborRadius = primitive.getSize()[0];
-		
-		if(box.getLeafList().getAtomCount()==32){
-			radiusInner = neighborRadius*neighborRadius/32;
-			
-		} else {
-			radiusInner = neighborRadius*neighborRadius/8;
-			
-		}
-		
-		
-		//Lattice Site Assignment
-		for(int i=0; i<siteIndex; i++){
-			latticeSite[i] = coordinateDefinition.getLatticePosition(box.getLeafList().getAtom(i));
-		}
-		
-		//Check for neighboring sites
-		IVectorMutable distance = space.makeVector();
-		neighborSite = new IVectorMutable[siteIndex][siteIndex];
-		
-		for (int i=0; i<siteIndex; i++){
-			for (int j=0; j<siteIndex; j++){
-				neighborSite[i][j]= space.makeVector();
+	public P1Constraint(ISpace space, double neighborRadius, IBox box, CoordinateDefinition coordinateDefinition) {
+	    super(space);
 
-			}
-		}
-		//Determine which are the neighboring sites
-		for (int i=0; i<siteIndex; i++){
-			
-			int k=0;
-			for(int j=0; j<siteIndex; j++){
-			
-				distance.Ev1Mv2(latticeSite[j], latticeSite[i]);
-				box.getBoundary().nearestImage(distance);
-				double distCheck = Math.sqrt(distance.squared());
-				
-				if (distCheck >0.00000001 && distCheck < neighborRadius-0.000001){
-					
-					neighborSite[i][k].E(latticeSite[j]);
-					k++;
-				}
-			}
-		}
-		
-		
-	}//End of Constructor
+	    siteIndex = box.getLeafList().getAtomCount();
+	    latticeSite = space.makeVectorArray(siteIndex);
+
+	    radiusInner = neighborRadius*neighborRadius/4;
+
+	    //Lattice Site Assignment
+	    for(int i=0; i<siteIndex; i++){
+	        latticeSite[i] = coordinateDefinition.getLatticePosition(box.getLeafList().getAtom(i));
+	    }
+	}
 	
 	public double getInnerRadius(){
-		return radiusInner;
+	    return radiusInner;
 	}
 	
 	@Override
 	public double energy(IAtomList atoms) {
-		
-		IVectorMutable posAtom = atoms.getAtom(0).getPosition();
-		
-		int atomIndex = atoms.getAtom(0).getLeafIndex();
-		double d = posAtom.Mv1Squared(latticeSite[atomIndex]);
-				
-		if (d < radiusInner){
-			return 0;
-		} 
-		
-		return Double.POSITIVE_INFINITY;
+
+	    IVectorMutable posAtom = atoms.getAtom(0).getPosition();
+
+	    int atomIndex = atoms.getAtom(0).getLeafIndex();
+	    double d = posAtom.Mv1Squared(latticeSite[atomIndex]);
+
+	    if (d < radiusInner){
+	        return 0;
+	    }
+
+	    return Double.POSITIVE_INFINITY;
 	}
 
-	
-	private IVectorMutable[] latticeSite; 
-	private IVectorMutable[][] neighborSite;
-	private int siteIndex;
-	private double radiusInner;
-	
+    private static final long serialVersionUID = 1L;
+    private IVectorMutable[] latticeSite; 
+    private int siteIndex;
+    private double radiusInner;
 }
