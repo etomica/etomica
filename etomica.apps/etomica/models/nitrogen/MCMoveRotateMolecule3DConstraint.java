fbe239a3d58fb2d9cad7d49c5f9e16db21af887d
Tai Boon Tan <taitan@buffalo.edu>
2010-10-25 19:20:24 +0000

    pick random rotation axis on the molecular plane

--- etomica.apps/etomica/models/nitrogen/MCMoveRotateMolecule3DConstraint.java
+++ etomica.apps/etomica/models/nitrogen/MCMoveRotateMolecule3DConstraint.java
@@ -14,6 +14,7 @@ import etomica.space.ISpace;
 import etomica.space.RotationTensor;
 import etomica.space3d.RotationTensor3D;
 import etomica.space3d.Tensor3D;
+import etomica.units.Degree;
 
 
 /**
@@ -73,8 +74,7 @@ public class MCMoveRotateMolecule3DConstraint extends MCMoveMolecule {
     }
      
     public boolean doTrial() {
-//        System.out.println("doTrial MCMoveRotateMolecule called");
-        
+
         if(box.getMoleculeList().getMoleculeCount()==0) {molecule = null; return false;}
         int iMolecule = random.nextInt(box.getMoleculeList().getMoleculeCount());
         
@@ -111,13 +111,25 @@ public class MCMoveRotateMolecule3DConstraint extends MCMoveMolecule {
         double randomValue = (2*random.nextDouble() - 1.0);
         double constant = (constraintAngle/180.0) *Math.PI;
         double dTheta = randomValue*constant;
-        //System.out.println("{MCMOVE} angle: " +Degree.UNIT.fromSim(dTheta));
-        rotationTensor.setAxial(r0.getD() == 3 ? random.nextInt(3) : 2,dTheta);
+        //rotationTensor.setAxial(r0.getD() == 3 ? random.nextInt(3) : 2,dTheta);
 
         r0.E(positionDefinition.position(molecule));
-		doTransform();
+        //doTransform();
+		doTransformMolec(iMolecule, dTheta);
         
+		molecOrientation.Ev1Mv2(leaftPos1, leafPos0);
+		molecOrientation.normalize();
+		workVector.Ev1Mv2(molecOrientation, initMolecOrientation[iMolecule][0]);
+		angleMol = Math.acos(molecOrientation.dot(initMolecOrientation[iMolecule][0]));
+		
         energyMeter.setTarget(molecule);
+        
+        if(Double.isNaN(energyMeter.getDataAsScalar())){
+        	double energy = energyMeter.getDataAsScalar();
+        	System.out.println("energy"+ energy);
+        	System.out.println("dTheta: " + dTheta + " angleMol: "+ Degree.UNIT.fromSim(angleMol));
+        	throw new RuntimeException("<MCMoveRotate> energy is NaN!!!!!!!!!!!!");
+        }
         return true;
     }
     
@@ -131,6 +143,43 @@ public class MCMoveRotateMolecule3DConstraint extends MCMoveMolecule {
             rotationTensor.transform(r);
             r.PE(r0);
         }
+    }
+    
+    protected void doTransformMolec(int iMolecule, double dTheta) {
+        IAtomList childList = molecule.getChildList();
+        for (int iChild = 0; iChild<childList.getAtomCount(); iChild++) {
+            IAtom a = childList.getAtom(iChild);
+            IVectorMutable r = a.getPosition();
+            r.ME(r0);
+        }
+        double s = (2*random.nextDouble() - 1.0);
+        double t = (2*random.nextDouble() - 1.0);
+        
+        rotationAxis.E(new double[]{0.0, 0.0, 0.0});
+		rotationAxis.PEa1Tv1(s, initMolecOrientation[iMolecule][1]);
+		rotationAxis.PEa1Tv1(t, initMolecOrientation[iMolecule][2]);
+		rotationAxis.normalize();
+		
+		IVectorMutable vec = space.makeVector();
+		vec.E(initMolecOrientation[iMolecule][0]);
+
+		double check = vec.dot(rotationAxis);
+		if(check > 1e-7){
+			System.out.println("check: " + check);
+			throw new RuntimeException("<MCMoveRotateMolecule3DConstraint> The rotation is NOT perpendicular to" +
+					" the nominal molecular axis");
+		}
+		
+		rotation.setRotationAxis(rotationAxis, dTheta);
+        ((AtomActionTransformed)atomGroupAction.getAtomAction()).setTransformationTensor(rotation);
+        atomGroupAction.actionPerformed(molecule);
+        
+        for (int iChild = 0; iChild<childList.getAtomCount(); iChild++) {
+            IAtom a = childList.getAtom(iChild);
+            IVectorMutable r = a.getPosition();
+            r.PE(r0);
+        }
+        
     }
     
     protected void doTransformToInitial(int iMolecule, double angleMol) {
