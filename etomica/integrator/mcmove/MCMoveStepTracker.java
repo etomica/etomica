052f8f4489779e7744da22cb1f068cfb3a187f73
Andrew Schultz <ajs42@buffalo.edu>
2010-11-01 14:08:02 +0000

    print box when printing step size adjustments

--- etomica/integrator/mcmove/MCMoveStepTracker.java
+++ etomica/integrator/mcmove/MCMoveStepTracker.java
@@ -49,7 +49,11 @@ public class MCMoveStepTracker extends MCMoveTracker {
                 stepSize *= adjustStep;
                 stepSize = Math.min(stepSize, mcMove.getStepSizeMax());
                 if (noisyAdjustment) {
-                    System.out.println(mcMove.getClass()+" increasing step size to "+stepSize+" (acceptance="+(double)nAccept/nTrials+")");
+                    System.out.print(mcMove.getClass());
+                    if (mcMove instanceof MCMoveBox) {
+                        System.out.print(" "+((MCMoveBox)mcMove).getBox());
+                    }
+                    System.out.println(" increasing step size to "+stepSize+" (acceptance="+(double)nAccept/nTrials+")");
                 }
                 if (lastAdjust < 1) {
                     lastAdjust = 1;
@@ -76,7 +80,11 @@ public class MCMoveStepTracker extends MCMoveTracker {
                 stepSize /= adjustStep;
                 stepSize = Math.max(stepSize, mcMove.getStepSizeMin());
                 if (noisyAdjustment) {
-                    System.out.println(mcMove.getClass()+" decreasing step size to "+stepSize+" (acceptance="+(double)nAccept/nTrials+")");
+                    System.out.print(mcMove.getClass());
+                    if (mcMove instanceof MCMoveBox) {
+                        System.out.print(" "+((MCMoveBox)mcMove).getBox());
+                    }
+                    System.out.println(" decreasing step size to "+stepSize+" (acceptance="+(double)nAccept/nTrials+")");
                 }
                 if (lastAdjust > -1) {
                     lastAdjust = -1;
