0f4bc90bea70b1d98621391571c51ce39267e977
Tai Boon Tan <taitan@buffalo.edu>
2010-11-16 23:15:03 +0000

    changes: 1. change TOL value 2. ax and bx values 3. added print statement

--- etomica/util/numerical/LineMinimization.java
+++ etomica/util/numerical/LineMinimization.java
@@ -31,40 +31,62 @@ public class LineMinimization {
 	
 
 	public double dLineMinimization(double[] p, double[] xi, FunctionMultiDimensionalDifferentiable function){
-		double TOL = 0.0002;
+		double TOL = 2.1e-9;
+		/*
+		 * xi is the first derivative
+		 */
 		
-		int j;
-		double xx, xmin, bx, ax;
+		double xmin;
 		int n = p.length;
 		ncom = n;
 		pcom = new double[n];
 		xicom = new double[n];
-		
-		
-		for(j=0; j<n; j++){
+
+		for(int j=0; j<n; j++){
 			pcom[j] = p[j];
 			xicom[j] = xi[j];
+			System.out.println("<lineMinimize>: pcom: " + pcom[j] + " ;xicom: " + xicom[j]);
 		}
 		
-		ax = 0.0;
-		xx = 1.0;
-		bx = Double.NaN;
+		// Values for bracketing
+		double ax = -1.5e-8;
+		double xx =  1.5e-8;
+		double bx = Double.NaN;
 		
 		f1dim.setFunction(function);
 		f1dim.setPcom(pcom);
 		f1dim.setXicom(xicom);
 
 		double[] x = new double[] {ax, xx, bx}; 
-		bisectionMethodMinimizationBracket.mnbrak(x, f1dim);
 		
+		System.out.println("\n<lineMinimize> BEGIN ******BRACKETING****");
+		x=bisectionMethodMinimizationBracket.mnbrak(x, f1dim);
+		System.out.println("<lineMinimize> END ******BRACKETING****");
+		System.out.println("bracketing the minimum: " +x[0]+" "+x[1]+" "+x[2]);
+//		System.out.println("exit in LineMinimization");
+//		System.exit(1);
+		System.out.println("\n<lineMinimize> BEGIN *******dbrent******");
 		double[] dbrentValue = brentMethodwDerivative.dbrent(x, f1dim, TOL);
+		System.out.println("<lineMinimize> END *******dbrent******");
+		System.out.println("dbrentvalue: " + dbrentValue[0] +" "+dbrentValue[1]);
+	
+
 		xmin = dbrentValue[0];
-		
-		for (j=0; j<n; j++){
+		System.out.println("\n<lineMinimize>*******for loop*******");
+		System.out.println("<lineMinimize> xmin: " + xmin);
+		for (int j=0; j<n; j++){
 			xi[j] *= xmin;
 			p[j] += xi[j];
+			//System.out.println("<lineMinimize>#########LineMinimization: xi: " + xi[j] + " ;p[j]: " + p[j]);
 		}
 	
+		for (int j=0; j<n; j++){
+			System.out.print(p[j] + ", ");
+			if (j>0&&(j+1)%5==0){
+				System.out.println("");
+			}
+		}
+		System.out.println("Lowest Energy found: "+ dbrentValue[1]);
 		return dbrentValue[1];
 	}
 	
