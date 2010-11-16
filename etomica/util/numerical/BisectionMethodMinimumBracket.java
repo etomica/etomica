2adf217945135e5cdd1cd9daf5c2d9cf5db39645
Tai Boon Tan <taitan@buffalo.edu>
2010-11-16 03:36:45 +0000

    mnbrak return double[] instead of void newX[] is the 3 points that brackets the minimun

--- etomica/util/numerical/BisectionMethodMinimumBracket.java
+++ etomica/util/numerical/BisectionMethodMinimumBracket.java
@@ -23,31 +23,34 @@ public class BisectionMethodMinimumBracket {
 	 * Author: Tai Boon Tan
 	 */
 	
-	public void mnbrak(double[] x, FunctionDifferentiable function){
+	public double[] mnbrak(double[] x, FunctionDifferentiable function){
 		double GOLD = 1.618034; 
 		double GLIMIT = 100.0;
 		double TINY = Double.MIN_VALUE;
 		double denom = 0.0;
 		
-		double ax = x[0];
-		double bx = x[1];
-		double cx = x[2];
+		double[] newX = x;
+		
+		newX[0] = x[0];
+		newX[1] = x[1];
+		newX[2] = x[2]; 
 		
 		double dum=0, u;
 		double fu, q, r, ulimit;
 		
-		double fa = function.f(ax);
-		double fb = function.f(bx);
+		double fa = function.f(newX[0]);
+		double fb = function.f(newX[1]);
 		
+
 		/*
 		 *  Switching the roles of a and b so that able to go
 		 *  downhill in the direction from a to b 
 		 */
-		if (fb >= fa){
+		if (fb > fa){
 			
-			dum = ax;		
-			ax = bx;
-			bx = dum;
+			dum = newX[0];		
+			newX[0] = newX[1];
+			newX[1] = dum;
 			
 			dum = fb;
 			fb = fa;
@@ -55,16 +58,16 @@ public class BisectionMethodMinimumBracket {
 		}
 		
 		/*
-		 * Initial guess for cx
+		 * Initial guess for newX[2]
 		 * GOLD is 1 plus the fraction of the so-called Golden Mean or Golden Section 
 		 * 	(0.38197 and 0.61893 respectively, if considered from both different ends)
 		 */
-		cx = bx + GOLD*(bx-ax);
-		double fc = function.f(cx);
-		
-		while (fb > fc){
-			r = (bx - ax)*(fb - fc);
-			q = (bx - cx)*(fb - fa);
+		newX[2] = newX[1] + GOLD*(newX[1]-newX[0]);
+		double fc = function.f(newX[2]);
+
+		while (fb >= fc){
+			r = (newX[1] - newX[0])*(fb - fc);
+			q = (newX[1] - newX[2])*(fb - fa);
 			
 			if ((q-r) >=0){
 				denom = 2*Math.abs(Math.max(Math.abs(q-r), TINY));
@@ -73,24 +76,24 @@ public class BisectionMethodMinimumBracket {
 			}
 			
 			/*
-			 * Computing u by parabolic extrapolation from ax, bx, and cx
+			 * Computing u by parabolic extrapolation from newX[0], newX[1], and newX[2]
 			 * TINY is used to avoid division by zero
 			 */
-			u = bx - ((bx-cx)*q - (bx-ax)*r)/denom;
-			ulimit = bx + GLIMIT*(cx-bx);
+			u = newX[1] - ((newX[1]-newX[2])*q - (newX[1]-newX[0])*r)/denom;
+			ulimit = newX[1] + GLIMIT*(newX[2]-newX[1]);
 			
-			if((bx-u)*(u-cx) > 0.0){ // If Parabolic u is between b and c
+			if((newX[1]-u)*(u-newX[2]) > 0.0){ // If Parabolic u is between b and c
 				fu = function.f(u);
 				if (fu < fc){        // Got a minimum between b and c
-					ax = bx;
-					bx = u;
+					newX[0] = newX[1];
+					newX[1] = u;
 					fa = fb;
 					fb = fu;
-					break;
+					return newX;
 				} else if (fu > fb){ // Got a minimum between a and u
-					cx = u;
+					newX[2] = u;
 					fc = fu;
-					break;
+					return newX;
 				}
 				
 				/*
@@ -98,28 +101,28 @@ public class BisectionMethodMinimumBracket {
 				 * Use default magnification
 				 */
 				
-				u = cx + GOLD*(cx-bx);
+				u = newX[2] + GOLD*(newX[2]-newX[1]);
 				fu = function.f(u);
 			
-			} else if ((cx-u)*(u-ulimit) > 0.0){        // If Parabolic fit is between c and its allowed limit
+			} else if ((newX[2]-u)*(u-ulimit) > 0.0){        // If Parabolic fit is between c and its allowed limit
 				fu = function.f(u);
 				if (fu < fc){
 					
-					bx = cx;
-					cx = u;
-					u  = cx+GOLD*(cx-bx);
+					newX[1] = newX[2];
+					newX[2] = u;
+					u  = newX[2]+GOLD*(newX[2]-newX[1]);
 					
 					fb = fc;
 					fc = fu;
 					fu = function.f(u);
 				}
 				
-			} else if ((u-ulimit)*(ulimit-cx) >= 0.0){   // Limitting parabolic u to the maximum allowed value
+			} else if ((u-ulimit)*(ulimit-newX[2]) >= 0.0){   // Limitting parabolic u to the maximum allowed value
 				u = ulimit;
 				fu = function.f(u);
 				
 			} else {									 // Rejecting parabolic u, use default magnification									
-				u = cx+ GOLD*(cx-bx);
+				u = newX[2]+ GOLD*(newX[2]-newX[1]);
 				fu = function.f(u);
 			}
 			
@@ -127,9 +130,9 @@ public class BisectionMethodMinimumBracket {
 			 *  Eliminating oldest point and continue
 			 */
 			
-			ax = bx;
-			bx = cx;
-			cx = u;
+			newX[0] = newX[1];
+			newX[1] = newX[2];
+			newX[2] = u;
 			
 			fa = fb;
 			fb = fc;
@@ -137,9 +140,11 @@ public class BisectionMethodMinimumBracket {
 			
 		}
 		
-		x[0] = ax;
-		x[1] = bx;
-		x[2] = cx;
+		x[0] = newX[0];
+		x[1] = newX[1];
+		x[2] = newX[2];
+
+		return newX;
 	}
 	
 }
