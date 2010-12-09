c0ffebfe8157f63de2f2fe6f75d08a11090cac13
Kate Shaul <schadel2@buffalo.edu>
2010-12-09 19:53:18 +0000

    Computes B4HNC(C), B4PY(C), and B4PY(V) in addition to B4HNC(V)

--- etomica.apps/etomica/virial/B4FFTLJ.java
+++ etomica.apps/etomica/virial/B4FFTLJ.java
@@ -7,7 +7,21 @@ import etomica.util.SineTransform;
 
 /**
  * 
- * Computes "simple" part of the f-bond-only formulation of B4.
+ * Computes four approximations of B4 by FFT: HNC(C), HNC(V), PY(C), and PY(V).
+ * 
+ * HNC: hypernetted-chain integral-equation theory
+ * PY: Percus-Yevick integral-equation theory
+ * (C):compressibility route 
+ * (V):virial (pressure) route 
+ *  
+ * The HNC(V) approximation is equivalent to the "simple part" of the f-bond-only formulation for (at least) B4, B5, and B6.
+ * 
+ * Values agree with those computed through the recursive formalism (the Ornstein-Zernike formulation) in BnHNCLJ.java and BnPYLJ.java.
+ * 
+ * Diagrams are labeled using the nomenclature of Barker, Leonard, and Pompe (1966): 
+ * "Fifth Virial Coefficients", JCP 44(11); e.g., D4 is the four-membered ring.
+ * 
+ * X is added to the diagram name to denote if an x=r*df/dr bond is present.  This our own nomenclature...
  * 
  * @author Kate Shaul
  *
@@ -27,33 +41,15 @@ public static void main(String[] args) {
 		int power = 13; // Defines discretization
 		double r_max = 100; // Defines range of separation distance, r = [0 rmax]
 		int N = 1<<power;  // Number of grid points in the discretizations of r- and k-space
-		
-		
-		//N = N -1;
-		SineTransform dst = new SineTransform();
-        
+
 		double del_r = r_max/(N-1);
 		
 		double[] fr = getfr( N, del_r,reducedTemp);
-		
 
+		
+		SineTransform dst = new SineTransform();
+		
 		double[] fk = dst.forward(fr, del_r);
-		
-		double[] fr2 = dst.reverse(fk, del_r);
-		 
-		
-		
-		if (t == -1 ) {
-			for (int i = 0;i<10; i=i+1) {
-				System.out.println((del_r*i) + "   " + fr[i] + "   " + fr2[i] + "   " + (fr2[i]-fr[i]) );
-			}
-			
-			for (int i = 10;i<N; i=i+50) {
-				System.out.println((del_r*i) + "   " + fr[i] + "   " + fr2[i] + "   " + (fr2[i]-fr[i]) );
-			}
-			//System.exit(0);
-		}
-		
 		
 		double[] ffk = new double[N];
 		double[] fffk = new double[N];
@@ -63,121 +59,56 @@ public static void main(String[] args) {
 		}
 		
 		double[] ffr = dst.reverse(ffk, del_r);
-		
-		double[] ffk2 = dst.forward(ffr, del_r);
-		
-		double[] fffk2 = new double [N];
-		for (int i = 0;i<N; i++) {
-			fffk2[i] = ffk2[i]*fk[i]; 
-		}
-		
-		/* Using fffk2 to compute d1r is more accurate than using fffk: the result converges *to the same answer* more quickly.
-		 * This indicates that error in the transform is compounded in the fffk computation, and that some of this error is somehow 
-		 * removed when taking the reverse and and forward transforms of ffk.  It seems likely 
-		 * not all of the error is removed by this ah hoc procedure, which would indicate that even faster convergence is possible.
-		 */
-		
 		double[] d1r = dst.reverse(fffk, del_r);
+
 		
-		double[] d2b1r = new double[N];
-		for (int i = 0;i<N; i++) {
-			
-			d2b1r[i] = fr[i]*ffr[i];
-		}
+		Space space = Space3D.getInstance();	
+		P2LennardJones p2 = new P2LennardJones(space, 1, 1);
 		
-		double[] d2b1k = dst.forward(d2b1r, del_r);
-		
-		double[] d2bk = new double[N];
-		for (int i = 0;i<N; i++) {
-			
-			d2bk[i] = fk[i]*d2b1k[i];
-		}
-		
-		double[] d2br = dst.reverse(d2bk, del_r);
-		
-		
-		
-		double B3 = 0;
-		double D1 = 0;
-		double D2A = 0;
-		double D2B = 0;
-		
-		double[] cB3 = new double[N];
-		double[] cB4Approx = new double[N];
-		cB3[0] = fr[0]*ffr[0];
-		cB4Approx[0] = fr[0]*( d1r[0]+ (ffr[0]*ffr[0]) + d2br[0] );
-		
+		double C3 = 0;
+		double D4 = 0;
+		double D5 = 0;
+		double D5X = 0;
 		
 		for (int i = 1;i<(N-1); i++) {
 			
-			double c = (i*del_r)*(i*del_r)*del_r;
+			double c = 4.0*Math.PI*(i*del_r)*(i*del_r)*del_r;
 			
-			B3 = B3 + fr[i]*ffr[i]*c;
+			C3 = C3 + fr[i]*ffr[i]*c;
 			 
-			D1 = D1 + fr[i]*d1r[i]*c;
+			D4 = D4 + fr[i]*d1r[i]*c;
 			
-			D2A = D2A + fr[i]*(ffr[i]*ffr[i])*c;
+			D5 = D5 + fr[i]*(ffr[i]*ffr[i])*c;
 			
-			D2B = D2B + fr[i]*(d2br[i] )*c;
-			
-			cB3[i] = fr[i]*ffr[i];
-			    
-			cB4Approx[i] = fr[i]*( d1r[i] + (ffr[i]*ffr[i]) + d2br[i] );
+			double r = del_r*i;
+			double x = (fr[i]+1)*(-1.0/reducedTemp)*p2.du(r*r);
+			D5X = D5X + x*(ffr[i]*ffr[i])*c;
 			
 		}
 		
-		double c = r_max*r_max*del_r;
+		double c = 4.0*Math.PI*r_max*r_max*del_r;
 		
-	    B3 = B3 + 0.5*fr[N-1]*ffr[N-1]*c;
+	    C3 = C3 + 0.5*fr[N-1]*ffr[N-1]*c;
 		
-		D1 = D1 + 0.5*fr[N-1]*d1r[N-1]*c; 
+		D4 = D4 + 0.5*fr[N-1]*d1r[N-1]*c; 
 		
-		D2A = D2A + 0.5*fr[N-1]*( ffr[N-1]*ffr[N-1] )*c; 
+		D5 = D5 + 0.5*fr[N-1]*( ffr[N-1]*ffr[N-1] )*c; 
 		
-		D2B = D2B + 0.5*fr[N-1]*( d2br[N-1] )*c; 
+		double r = del_r*(N-1);
+		double x = (fr[N-1]+1)*(-1.0/reducedTemp)*p2.du(r*r);
+		D5X = D5X + x*(ffr[N-1]*ffr[N-1])*c;
 		
-		cB3[N-1] = fr[N-1]*ffr[N-1];
-	    
-		cB4Approx[N-1] = fr[N-1]*( d1r[N-1] + (ffr[N-1]*ffr[N-1]) + d2br[N-1]);
+		double B3 = -1.0/(3.0)*C3;
 		
+		double B4HNCV = -3.0/8.0*( D4 + 2.0*D5);
 		
+		double B4HNCC = -3.0/8.0*D4 + -5.0/8.0*D5;
+		
+		double B4PYV  = -3.0/8.0*( D4 + 2.0*D5)-1.0/12.0*D5X;
 			
+		double B4PYC  = -1.0/4.0*( D4 + 2.0*D5);
 		
-
-		B3 = -1.0/(3.0)*4.0*Math.PI*B3;
-		
-		D1 = -1.0/(8.0)*4.0*Math.PI*D1;
-		
-		D2A = -1.0/(8.0)*4.0*Math.PI*D2A;
-		
-		D2B = -1.0/(8.0)*4.0*Math.PI*D2B;
-		
-		
-		double [] c1k = dst.forward(cB3, del_r);
-		
-		double [] c2k = dst.forward(cB4Approx, del_r);
-			
-		double B3F = -1.0/(3.0)*(c1k[0]); // B3 for m = 1
-		
-		double B4PY = -1.0/(4.0)*(c2k[0]); // B3 for m = 1
-		
-		//double b = Standard.B2HS(1.0);
-		//System.out.println("B3  (T* = "+ reducedTemp + ") = "+ B3);
-		//System.out.println("D1* (T* = "+ reducedTemp + ") = "+ D1/(b*b*b));
-		//System.out.println("D2A*(T* = "+ reducedTemp + ") = "+ D2A/(b*b*b));
-		//System.out.println("D2B*(T* = "+ reducedTemp + ") = "+ D2B/(b*b*b));
-		//System.out.println("D2* (T* = "+ reducedTemp + ") = "+ (D2A+D2B)/(b*b*b));
-		
-		//System.out.println(reducedTemp + "   " + B3 );
-		//System.out.println(reducedTemp + "   " + B3F );
-		
-		
-		//B4-B4Bridge
-		//System.out.println(reducedTemp + "   " + 3*(D1 + (D2A+D2B)) );
-		
-		//B4PY
-		System.out.println(reducedTemp + "   " + 2*(D1 + (D2A+D2B)) );
-		//System.out.println(reducedTemp + "   " + B4PY );
+		System.out.println(reducedTemp + "   "  + (B4HNCC)+ "   "  + (B4HNCV)+ "   "  + (B4PYC)+ "   "  + (B4PYV));
 		
 	}
 	}
