b24d73c3d03fb903e74eb059c1db0b6cf73e19e7
Tai Boon Tan <taitan@buffalo.edu>
2010-12-15 22:17:16 +0000

    BasisHcpBaseCentered

---
+++ etomica/lattice/crystal/BasisHcpBaseCentered.java
@@ -0,0 +1,23 @@
+package etomica.lattice.crystal;
+import etomica.space3d.Vector3D;
+
+/**
+ * A 2-atom basis for an hcp crystal 
+ *   (base-centered or side-centered)
+ *  
+ *
+ * @author Tai Boon Tan
+ */
+public class BasisHcpBaseCentered extends Basis {
+    
+    public BasisHcpBaseCentered() {
+        super(scaledPositions);
+    }
+    
+    private static final Vector3D[] scaledPositions = new Vector3D[] {
+        new Vector3D(0.0, 0.0, 0.0),
+        new Vector3D(0.5, 0.5, 0.0),
+    };
+
+    private static final long serialVersionUID = 1L;
+}
