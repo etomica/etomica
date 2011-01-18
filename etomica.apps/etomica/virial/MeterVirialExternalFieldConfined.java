6950c57aacc1b5f1705b9d9ec2705b6b116c6a05
Jung Ho Yang <junghoya@buffalo.edu>
2011-01-18 23:30:23 +0000

    for 2walls and 1wall cases

---
+++ etomica.apps/etomica/virial/MeterVirialExternalFieldConfined.java
@@ -0,0 +1,103 @@
+package etomica.virial;
+
+import etomica.api.IAtomList;
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.units.Null;
+
+/**
+ * Measures value of clusters in a box and returns the values
+ * divided by the sampling bias from the sampling cluster.
+ */
+public class MeterVirialExternalFieldConfined implements IEtomicaDataSource, java.io.Serializable {
+
+    /**
+	 * Constructor for MeterVirial.
+	 */
+	public MeterVirialExternalFieldConfined(ClusterAbstract aCluster, double[] wallposition, double [] walldistance) {
+		cluster = aCluster;
+		this.wallPosition = wallposition;
+		wallDistance = walldistance;
+        data = new DataDoubleArray(new int[]{walldistance.length+1,wallposition.length+1});
+        dataInfo = new DataInfoDoubleArray("Cluster Value",Null.DIMENSION, new int[]{walldistance.length+1,wallposition.length+1});
+        tag = new DataTag();
+        dataInfo.addTag(tag);
+	}
+
+	public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+    
+    public DataTag getTag() {
+        return tag;
+    }
+    
+    public IData getData() {
+        double pi = box.getSampleCluster().value(box);
+        double x[] = data.getData();
+        IAtomList atoms = box.getLeafList();
+        double lowestatom = 0;
+        double highestatom = 0;
+        for (int i=1; i<atoms.getAtomCount();i++){
+        	double z = atoms.getAtom(i).getPosition().getX(2);
+        	if (z<lowestatom){
+        		lowestatom = z;      		
+        	}
+        	else if (z>highestatom){
+        		highestatom = z;       		
+        	}
+        }
+        
+        double v=cluster.value(box)/pi;
+        int n=wallDistance.length+1;
+        for (int i=0; i<wallPosition.length; i++) {
+        	x[i*n]=v;
+            if (lowestatom-0.5 < wallPosition[i]){
+            	x[i*n] -= 0.5*v;
+            }
+            for (int j=0; j<wallDistance.length; j++){
+            	x[i*n+j+1]=x[i*n];
+            	if (highestatom+0.5 > wallPosition[i]+wallDistance[j]){
+            		x[i*n+j+1] -= 0.5*v;
+            	}
+            }
+            if (-highestatom-0.5 < wallPosition[i]){
+            	x[i*n] -= 0.5*v;
+            }
+        }
+        
+        for (int j=0; j<wallDistance.length; j++){
+        	x[n*wallPosition.length+j]=((wallDistance[j]-1)-(-lowestatom+highestatom)*v);
+        	if (x[n*wallPosition.length+j]<0){
+        		x[n*wallPosition.length+j]=0;
+        	
+        	}
+        }
+        return data;
+    }
+    
+    public ClusterAbstract getCluster() {
+        return cluster;
+    }
+    
+    public BoxCluster getBox() {
+        return box;
+    }
+    
+    public void setBox(BoxCluster newBox) {
+        box = newBox;
+    }
+
+    protected final ClusterAbstract cluster;
+	private final DataDoubleArray data;
+	private final IEtomicaDataInfo dataInfo;
+    private final DataTag tag;
+    private final double [] wallPosition;
+    private final double [] wallDistance;
+    private BoxCluster box;
+    private static final long serialVersionUID = 1L;
+}
