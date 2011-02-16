3e4a277a75d44a316867280cac3dfc031676a626
Andrew Schultz <ajs42@buffalo.edu>
2011-02-16 19:46:17 +0000

    allow node count to be specified

--- etomica.graph/etomica/graph/util/GraphNumber.java
+++ etomica.graph/etomica/graph/util/GraphNumber.java
@@ -9,13 +9,20 @@ import etomica.graph.model.GraphFactory;
 public class GraphNumber {
 
   public static void main(String[] args) {
-    String usage = "usage: GraphNumber n";
+    String usage = "usage: GraphNumber num [nodeCount]";
     if (args.length == 0) {
       System.out.println(usage);
       System.exit(1);
     }
-    long graphNum = Integer.parseInt(args[0]);
-    Graph g = makeGraph(graphNum);
+    long graphNum = Long.parseLong(args[0]);
+    Graph g;
+    if (args.length == 2) {
+      byte nodeCount = Byte.parseByte(args[1]);
+      g = makeGraph(graphNum, nodeCount);
+    }
+    else {
+      g = makeGraph(graphNum);
+    }
     System.out.println(g.nodeCount()+" "+g.edgesToString());
   }
 
@@ -32,6 +39,11 @@ public class GraphNumber {
     if (!success) {
       throw new RuntimeException("invalid number");
     }
+    return makeGraph(graphNum, nodeCount);
+  }
+    
+  public static Graph makeGraph(long graphNum, byte nodeCount) {
+    int numBits = 64-Long.numberOfLeadingZeros(graphNum);
     int nLeadingZeros = nodeCount*(nodeCount-1)/2 - numBits;
     String bitmapString = "";
     for (int i=0; i<nLeadingZeros; i++) {
