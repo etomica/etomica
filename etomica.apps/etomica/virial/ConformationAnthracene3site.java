9185ae476d3240ee1b1a9ad5505c90e034dbdcf4
Shu Yang <shuyang@buffalo.edu>
2011-04-27 18:50:59 +0000

    conformaton anthracene 3 site model from Iwai April 2011

---
+++ etomica.apps/etomica/virial/ConformationAnthracene3site.java
@@ -0,0 +1,44 @@
+package etomica.virial;
+
+import etomica.api.IAtom;
+import etomica.api.IAtomList;
+import etomica.api.IVectorMutable;
+import etomica.config.IConformation;
+import etomica.space.ISpace;
+
+ /**
+  *  Conformation for Anthracene
+  *  Reference paper: Iwai
+  * 3 site linear, group H to the two side benzene rings 
+ * @author Shu Yang
+ * date: March,7,2011
+ */
+public class ConformationAnthracene3site implements IConformation, java.io.Serializable{
+	
+	public ConformationAnthracene3site(ISpace space){
+		this.space = space;
+		vector = space.makeVector();
+	}
+
+	public void initializePositions(IAtomList atomList) {
+			
+		IAtom n1 = atomList.getAtom(SpeciesAnthracene3site545.indexC1);
+		n1.getPosition().E(new double[] {0, 0, 0});
+		
+		IAtom n2 = atomList.getAtom(SpeciesAnthracene3site545.indexCH1);
+		n2.getPosition().E(new double[] {-bondlength, 0, 0});
+		
+		IAtom n3 = atomList.getAtom(SpeciesAnthracene3site545.indexCH2);
+		n3.getPosition().E(new double[] {bondlength, 0, 0});
+		
+		
+	}
+		
+	protected final ISpace space;
+	protected static final double bondlength = 2.42;//converst nm to angstrom
+
+	
+	protected IVectorMutable vector;
+	
+	private static final long serialVersionUID = 1L;
+}
