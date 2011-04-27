d719eb1d3de3fedc667627256582c744edda45ac
Shu Yang <shuyang@buffalo.edu>
2011-04-27 18:36:52 +0000

    Species of phenanthrene TraPPE model 14 site model

---
+++ etomica.apps/etomica/virial/SpeciesTraPPEPhenanthrene.java
@@ -0,0 +1,100 @@
+package etomica.virial;
+
+import etomica.api.IAtomType;
+import etomica.api.IMolecule;
+import etomica.atom.Atom;
+import etomica.atom.AtomLeafDynamic;
+import etomica.atom.AtomTypeLeaf;
+import etomica.atom.Molecule;
+import etomica.chem.elements.Carbon;
+import etomica.chem.elements.ElementSimple;
+import etomica.space.ISpace;
+import etomica.species.Species;
+
+/**
+ *  
+ * Species Phenanthrene molecule
+ * this is for TraPPE, the Phenanthrene is rigid , LJ potential
+ * reference: TraPPE 4, UA description of linear and branched alkanes and alkylbenzenes, Siepmann
+ * modified from species of phenanthrene C14H10
+ * @author shu
+ * March.19.2011
+ *
+ */
+public class SpeciesTraPPEPhenanthrene extends Species {
+
+    public SpeciesTraPPEPhenanthrene(ISpace space) {
+        this(space, false);
+    }
+    
+    public SpeciesTraPPEPhenanthrene(ISpace space, boolean isDynamic) {
+        super();
+        this.space = space;
+        this.isDynamic = isDynamic;
+        
+        chType = new AtomTypeLeaf(new ElementSimple("CH", 13.0107));
+        cType = new AtomTypeLeaf(Carbon.INSTANCE);
+        ////should change because it is not united atom!!!
+        addChildType(chType);
+        addChildType(cType);
+
+        setConformation(new ConformationPhenanthreneTraPPE(space)); 
+     }
+
+     public IMolecule makeMolecule() {
+         Molecule Phenanthrene = new Molecule(this, 14);
+         // 4 Carbon without H, 8 Carbon with H
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, cType) : new Atom(space, cType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, cType) : new Atom(space, cType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, cType) : new Atom(space, cType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, cType) : new Atom(space, cType));
+         
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+
+         conformation.initializePositions(Phenanthrene.getChildList());
+         return Phenanthrene;
+     }
+
+     public IAtomType getCType() {
+         return cType;
+     }
+
+     public IAtomType getCHType() {
+         return chType;
+     }
+
+
+     public int getNumLeafAtoms() {
+         return 14;
+     }
+    
+    public final static int indexC1 = 0;
+    public final static int indexC2 = 1;
+    public final static int indexC3 = 2;
+    public final static int indexC4 = 3;
+    
+    public final static int indexCH1 = 4;
+    public final static int indexCH2 = 5;
+    public final static int indexCH3 = 6;
+    public final static int indexCH4 = 7;
+    public final static int indexCH5 = 8;
+    public final static int indexCH6 = 9;
+    public final static int indexCH7 = 10;
+    public final static int indexCH8 = 11;
+    public final static int indexCH9 = 12;
+    public final static int indexCH10 = 13;   
+    
+    private static final long serialVersionUID = 1L;
+    protected final ISpace space;
+    protected final boolean isDynamic;
+    protected final AtomTypeLeaf cType, chType;
+}
