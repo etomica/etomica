0d825c87b5993e601ac8294348a265dec016fa5f
Shu Yang <shuyang@buffalo.edu>
2011-04-27 18:34:46 +0000

    Species of phenanthrene 3 site model 545 type from Iwai model

---
+++ etomica.apps/etomica/virial/SpeciesPh3site545.java
@@ -0,0 +1,81 @@
+package etomica.virial;
+
+import etomica.api.IAtomType;
+import etomica.api.IMolecule;
+import etomica.atom.Atom;
+import etomica.atom.AtomLeafDynamic;
+import etomica.atom.AtomTypeLeaf;
+import etomica.atom.Molecule;
+import etomica.chem.elements.Carbon;
+import etomica.chem.elements.ElementSimple;
+import etomica.space.ISpace;
+import etomica.species.Species;
+
+/**
+ *  
+ * Species Phenanthrene molecule
+ * rigid , LJ potential, no charge, 3 site model.each benzene ring is a site
+ * reference: Iwai, monte carlo sim of Naphthalene, phenathlene,anthracene in SCF 1998
+ * @author shu
+ * March, 9, 2011
+ *
+ */
+public class SpeciesPh3site545 extends Species {
+
+    public SpeciesPh3site545(ISpace space) {
+        this(space, false);
+    }
+    
+    public SpeciesPh3site545(ISpace space, boolean isDynamic) {
+        super();
+        this.space = space;
+        this.isDynamic = isDynamic;
+        // the virial simulation  doesnt care this , but still I added this info here FYI
+        //CMass is the middle site, including 4 carbons and 0 hydrogens
+        //CHMass is the side site, including 5 carbons and 5 hydrogens
+        double CMass = 4 * 12.0107;
+        double CHMass = 5 * 12.0107 + 1 * 5;
+        chType = new AtomTypeLeaf(new ElementSimple("CH", CHMass));//"5" include hydrogen
+        cType = new AtomTypeLeaf(new ElementSimple("C", CMass));//"4" exclude hydrogen
+               
+        ////should change because it is not united atom!!!
+        addChildType(chType);
+        addChildType(cType);
+
+        setConformation(new ConformationPh3site(space)); 
+     }
+
+     public IMolecule makeMolecule() {
+         Molecule Phenanthrene = new Molecule(this, 10);
+         // 2 Carbon without H, 8 Carbon with H
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, cType) : new Atom(space, cType));
+         
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         Phenanthrene.addChildAtom(isDynamic ? new AtomLeafDynamic(space, chType) : new Atom(space, chType));
+         conformation.initializePositions(Phenanthrene.getChildList());
+         return Phenanthrene;
+     }
+
+     public IAtomType getCType() {
+         return cType;
+     }
+
+     public IAtomType getCHType() {
+         return chType;
+     }
+
+        // return the total number of the atoms
+     public int getNumLeafAtoms() {
+         return 3;
+     }
+    
+    public final static int indexC1 = 0;
+    public final static int indexCH1 = 1;
+    public final static int indexCH2 = 2;
+   
+    
+    private static final long serialVersionUID = 1L;
+    protected final ISpace space;
+    protected final boolean isDynamic;
+    protected final AtomTypeLeaf cType, chType;
+}
