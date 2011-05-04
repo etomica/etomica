02469c7789258e8a1a53df8a7c04f0f8fb0de60b
Kate Shaul <schadel2@buffalo.edu>
2011-05-04 21:34:20 +0000

    bond type required for virial calculations of corrections to PY(v) approximations

---
+++ etomica.apps/etomica/virial/MayerXSpherical.java
@@ -0,0 +1,44 @@
+package etomica.virial;
+
+import etomica.api.IBox;
+import etomica.api.IMoleculeList;
+import etomica.api.IPotential;
+import etomica.potential.P2LennardJones;
+import etomica.potential.Potential2SoftSpherical;
+/**
+ * @author kate
+ *
+ * -beta*e12*r12*du12/dr12 bond
+ */
+public class MayerXSpherical implements MayerFunction {
+
+	/**
+	 * Constructor for MayerESpherical.
+	 */
+	public MayerXSpherical(Potential2SoftSpherical potential) {
+		this.potential = potential;
+	}
+
+	/**
+	 * @see etomica.virial.MayerFunctionSpherical#f(etomica.AtomPair, double, double)
+	 */
+	public double f(IMoleculeList pair, double r2, double beta) {
+
+		if (r2 == 0) {
+			return 0;
+		} else {
+			return Math.exp(-beta*potential.u(r2))*(-beta*potential.du(r2));
+		}
+	}
+	
+	public void setBox(IBox newBox) {
+	    potential.setBox(newBox);
+	}
+
+	private final Potential2SoftSpherical potential;
+
+	public IPotential getPotential() {
+		return potential;
+	}
+
+}
