4c274caf702fdd67af06a25218aa01844c5f8b67
Kate Shaul <schadel2@buffalo.edu>
2011-05-21 21:27:02 +0000

    required for differentiation of virial coefficient diagrams by temperature

---
+++ etomica.graph/etomica/graph/operations/DifByConstant.java
@@ -0,0 +1,67 @@
+package etomica.graph.operations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import etomica.graph.model.Edge;
+import etomica.graph.model.Graph;
+
+public class DifByConstant implements Unary {
+
+  public Set<Graph> apply(Set<Graph> argument, Parameters params) {
+
+    assert (params instanceof DifByConstantParameters);
+    Set<Graph> result = new HashSet<Graph>();
+    for (Graph g : argument) {
+      Set<Graph> newSet = apply(g, (DifByConstantParameters) params);
+      if (newSet != null) {
+        result.addAll(newSet);
+      }
+    }
+    Unary isoFree = new IsoFree();
+    return isoFree.apply(result, params);
+  }
+
+  public Set<Graph> apply(Graph g, DifByConstantParameters params) {
+
+    Set<Graph> result = new HashSet<Graph>();
+    boolean deriv = false;
+    for (Edge edge : g.edges()) {
+      if (edge.getColor() == params.color()) {
+        Graph newGraph = g.copy();
+        newGraph.getEdge(edge.getId()).setColor(params.colorNew);
+        result.add(newGraph);
+        deriv = true;
+      }
+    }
+    if (deriv) {
+    	return result; 
+    } else {
+    	result.add(g);
+    	return result;
+    }
+  }
+  
+  public static class DifByConstantParameters implements Parameters {
+
+	  private char color;
+	  private char colorNew;
+
+	  public DifByConstantParameters(char color, char colorNew) {
+
+	    this.color = color;
+	    this.colorNew = colorNew;
+	  }
+
+	  public char color() {
+
+	    return color;
+	  }
+	  
+	  public char colorNew() {
+
+		    return colorNew;
+	  }
+	}
+
+}
