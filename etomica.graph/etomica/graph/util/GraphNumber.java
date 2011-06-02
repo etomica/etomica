8075123431e605d71cfb60210e6afc0a437b6274
Andrew Schultz <ajs42@buffalo.edu>
2011-06-02 18:53:31 +0000

    optionally display the graph

--- etomica.graph/etomica/graph/util/GraphNumber.java
+++ etomica.graph/etomica/graph/util/GraphNumber.java
@@ -4,26 +4,39 @@ import etomica.graph.model.Bitmap;
 import etomica.graph.model.BitmapFactory;
 import etomica.graph.model.Graph;
 import etomica.graph.model.GraphFactory;
+import etomica.graph.model.GraphList;
+import etomica.graph.viewer.ClusterViewer;
 
 
 public class GraphNumber {
 
   public static void main(String[] args) {
-    String usage = "usage: GraphNumber num [nodeCount]";
+    String usage = "usage: GraphNumber [-display] num [nodeCount]";
     if (args.length == 0) {
       System.out.println(usage);
       System.exit(1);
     }
-    long graphNum = Long.parseLong(args[0]);
+    String graphNumStr = args[0];
+    boolean display = false;
+    if (args.length > 1 && args[0].equals("-display")) {
+      display = true;
+      graphNumStr = args[1];
+    }
+    long graphNum = Long.parseLong(graphNumStr);
     Graph g;
-    if (args.length == 2) {
-      byte nodeCount = Byte.parseByte(args[1]);
+    if ((!display && args.length == 2) || (display && args.length == 3)) {
+      byte nodeCount = Byte.parseByte(display ? args[2] : args[1]);
       g = makeGraph(graphNum, nodeCount);
     }
     else {
       g = makeGraph(graphNum);
     }
     System.out.println(g.nodeCount()+" "+g.edgesToString());
+    if (display) {
+      GraphList<Graph> list = new GraphList<Graph>();
+      list.add(g);
+      ClusterViewer.createView("Graph "+graphNumStr, list);
+    }
   }
 
   public static Graph makeGraph(long graphNum) {
