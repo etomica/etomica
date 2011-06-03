873ad711492aa4240afd1bea94694e867263d833
Andrew Schultz <ajs42@buffalo.edu>
2011-06-03 19:34:08 +0000

    use conformation for TraPPE by default fix atom type get methods

--- etomica.apps/etomica/virial/SpeciesAlkane.java
+++ etomica.apps/etomica/virial/SpeciesAlkane.java
@@ -2,9 +2,11 @@ package etomica.virial;
 
 import etomica.api.IAtomType;
 import etomica.api.IMolecule;
+import etomica.api.IVectorMutable;
 import etomica.atom.Molecule;
 import etomica.chem.elements.Element;
 import etomica.chem.elements.ElementSimple;
+import etomica.config.ConformationChainZigZag2;
 import etomica.space.ISpace;
 import etomica.species.SpeciesSpheresHetero;
 
@@ -12,11 +14,20 @@ public class SpeciesAlkane extends SpeciesSpheresHetero {
 
     public SpeciesAlkane(ISpace _space, int numCarbons) {
         this(_space,numCarbons, new ElementSimple("CH3", 15), new ElementSimple("CH2", 14));
-        setTotalChildren(numCarbons);
     }
     public SpeciesAlkane(ISpace _space, int numCarbons, ElementSimple CH3element, ElementSimple CH2element) {
-    	 super(_space, makeAtomTypes(new Element[]{CH3element, CH2element}));
+    	super(_space, makeAtomTypes(new Element[]{CH3element, CH2element}));
         setTotalChildren(numCarbons);
+        setConformationParameters(nominalBondL, nominalBondTheta);
+    }
+    
+    public void setConformationParameters(double bondL, double bondTheta) {
+        IVectorMutable vector1 = space.makeVector();
+        vector1.setX(0, bondL);
+        IVectorMutable vector2 = space.makeVector();
+        vector2.setX(0, bondL*Math.cos(bondTheta));
+        vector2.setX(1, bondL*Math.sin(bondTheta));
+        conformation = new ConformationChainZigZag2(space, vector1, vector2);
     }
 
     public IMolecule makeMolecule() {
@@ -33,11 +44,11 @@ public class SpeciesAlkane extends SpeciesSpheresHetero {
         return group;
     }
     
-    public IAtomType getCH2Type() {
+    public IAtomType getCH3Type() {
         return childTypes[0];
     }
     
-    public IAtomType getCH3Type() {
+    public IAtomType getCH2Type() {
         return childTypes[1];
     }
     
@@ -52,4 +63,7 @@ public class SpeciesAlkane extends SpeciesSpheresHetero {
         }
     }
     private static final long serialVersionUID = 1L;
+    // parameters for TraPPE
+    protected static final double nominalBondL = 1.54;
+    protected static final double nominalBondTheta = Math.PI*114/180;
 }
