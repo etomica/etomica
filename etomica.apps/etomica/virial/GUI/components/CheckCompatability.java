dcd0671de287b5ce5671df1ee31046a2ffe74d56
Saranya Harikrishnan <saranyah@buffalo.edu>
2011-07-26 22:46:46 +0000

    Version1.0 - Created to separate code from CreateSimulationClass

---
+++ etomica.apps/etomica/virial/GUI/components/CheckCompatability.java
@@ -0,0 +1,211 @@
+package etomica.virial.GUI.components;
+
+import etomica.potential.PotentialGroup;
+import etomica.virial.GUI.containers.DialogBoxPanel;
+
+public class CheckCompatability {
+
+	private DialogBoxPanel messageAlert;
+	
+	private boolean potentialCheckFlag = false;
+	
+	private boolean Species1AtomicFlag;
+	private boolean Species2AtomicFlag;
+	
+	
+	private boolean Species1ChargeFlag;
+	private boolean Species2ChargeFlag;
+	
+	private boolean Species1MomentFlag;
+	private boolean Species2MomentFlag;
+	
+	private boolean InterNonBondedPotentialFlag;
+	private boolean Species1IntraBondedPotentialFlag;
+	private boolean Species2IntraBondedPotentialFlag;
+	
+	private PotentialGroup pIntra1TargetGroup;
+	private PotentialGroup pIntra2TargetGroup;
+	
+	
+	private SimulationEnvironment SimEnv;
+	private ParameterMapping[] potential;
+	
+	public void checkIfCompatible(ParameterMapping potential1,ParameterMapping potential2,SimulationEnvironment SimENV){
+		SimEnv = SimENV;
+		potential = new ParameterMapping[2];
+		potential[0] = potential1;
+		potential[1] = potential2;
+
+		
+		//Are we having a mixture? 
+		if(potential2 != null){
+			//Yes, We have a mixture of species!!!
+			InterNonBondedPotentialFlag = true;
+		}
+		else{
+			InterNonBondedPotentialFlag = false;
+			Species2IntraBondedPotentialFlag = false;
+		}
+		
+		if(InterNonBondedPotentialFlag == true){
+			for(int i = 0;i < 2;i++){
+				//We figure details abt each of the potential
+				
+				//If molecular or atomic
+				if (etomica.api.IPotentialMolecular.class.isAssignableFrom(potential[i].getPotential())){
+
+					//If potentialsites existing are greater than 1, although we dont have a intrabonded or intra non-bonded potential, but we have 
+					//cross potentials
+					
+					if(i==0){
+						Species1AtomicFlag = false;
+						Species1IntraBondedPotentialFlag = false;
+						pIntra1TargetGroup = null;
+					}
+					if(i==1){
+						Species2AtomicFlag = false;
+						Species2IntraBondedPotentialFlag = false;
+						pIntra2TargetGroup = null;
+					}
+				}
+				if(etomica.potential.Potential2SoftSpherical.class.isAssignableFrom(potential[i].getPotential())){
+					
+						//Inter-bonded potentials is to be gathered especially for alkanes, alcohol, etc
+						if(i==0){
+							Species1AtomicFlag = true;
+							if(SimEnv.getAlkane1Spheres() > 1){
+								Species1IntraBondedPotentialFlag = true;
+							}
+							else{
+								Species1IntraBondedPotentialFlag = false;
+								pIntra1TargetGroup = null;
+							}
+						}
+						if(i==1){
+							Species2AtomicFlag = true;
+							if(SimEnv.getAlkane2Spheres() > 1){
+								Species2IntraBondedPotentialFlag = true;
+							}
+							else{
+								Species2IntraBondedPotentialFlag = false;
+								pIntra2TargetGroup = null;
+							}
+						}
+				}
+				
+				
+				//If potential describing the interaction incluses charges or moments
+				String[] tempArray = potential[i].getParametersArray();
+				for(int j=0; j< tempArray.length;j++){
+					if(tempArray[j].equals("CHARGE")){
+						if(i == 0){
+							Species1ChargeFlag = true;}
+						if(i == 1){
+							Species2ChargeFlag = true;}
+						
+					}
+					if(tempArray[j].equals("MOMENT")||tempArray[j].equals("MOMENTSQR")){
+						//Dipole /Quadrapole moment exists!
+						if(i == 0){
+							Species1MomentFlag = true;}
+						if(i == 1){
+							Species2MomentFlag = true;}
+
+					}
+				}
+			
+				
+				
+				
+			//end of for loop for potentials
+			}
+			
+			if(Species1AtomicFlag && Species2AtomicFlag){
+				if(potential1.getPotential().equals(potential2.getPotential())){
+					potentialCheckFlag = true;
+				}
+				else{
+					potentialCheckFlag = false;
+				}
+			}
+			else if(!Species1AtomicFlag && Species2AtomicFlag){
+				if(potential1.getPotential().equals(potential2.getPotential())){
+					potentialCheckFlag = true;
+				}
+				else if( potential1.getPotential().toString().contains("LJ") && potential2.getPotential().toString().contains("LJ") ){
+					potentialCheckFlag = true;
+				}
+				else{
+					potentialCheckFlag = false;
+				}
+			}
+			else if(Species1AtomicFlag && !Species2AtomicFlag){
+				if(potential1.getPotential().equals(potential2.getPotential())){
+					potentialCheckFlag = true;
+				}
+				else if( potential1.getPotential().toString().contains("LJ") && potential2.getPotential().toString().contains("LJ") ){
+					potentialCheckFlag = true;
+				}
+				else{
+					potentialCheckFlag = false;
+				}
+			}
+			else if(!Species1AtomicFlag && !Species2AtomicFlag){
+				if(potential1.getPotential().equals(potential2.getPotential())){
+					potentialCheckFlag = true;
+				}
+				else if( potential1.getPotential().toString().contains("LJ") && potential2.getPotential().toString().contains("LJ") ){
+					potentialCheckFlag = true;
+				}
+				else{
+					potentialCheckFlag = false;
+				}
+			}
+			
+			
+
+			if(potentialCheckFlag){
+				//Condition for electrostatic interaction included
+				if((Species1ChargeFlag && Species2ChargeFlag)){
+					//run simulation
+					System.out.println("Run");
+				}
+				else if(Species1MomentFlag && Species2MomentFlag){
+					//run simulation
+					System.out.println("Run");
+				}
+				
+				else if(!Species1ChargeFlag && Species2MomentFlag){
+					//run simulation
+					System.out.println("Run");
+				}
+				else if(!Species1MomentFlag && Species2ChargeFlag ){
+					//run simulation
+					System.out.println("Run");
+				}
+				else if(Species1MomentFlag && !Species2ChargeFlag){
+					//run simulation
+					System.out.println("Run");
+				}
+				else if(Species1ChargeFlag && !Species2MomentFlag){
+					//run simulation
+					System.out.println("Run");
+					
+				}
+				else if(Species1ChargeFlag && Species2MomentFlag){
+					//stop simulation
+					System.out.println("Stop");
+				}
+				else if(Species1MomentFlag && Species2ChargeFlag){
+
+					//stop simulation
+					System.out.println("Stop");
+				}
+			}
+
+		//end of if statement for potential2 not equal to null	
+		}
+		
+		
+	}
+}
