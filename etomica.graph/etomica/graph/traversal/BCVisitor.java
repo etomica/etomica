12ac5f636948727d5225a26e5c7e3fb913fc66f7
Andrew Schultz <ajs42@buffalo.edu>
2011-09-21 22:51:27 +0000

    add traversal classes capable of returning the biconnected or singly connected components.

---
+++ etomica.graph/etomica/graph/traversal/BCVisitor.java
@@ -0,0 +1,62 @@
+package etomica.graph.traversal;
+
+import static etomica.graph.traversal.Traversal.STATUS_ARTICULATION_POINT;
+import static etomica.graph.traversal.Traversal.STATUS_START_BICOMPONENT;
+import static etomica.graph.traversal.Traversal.STATUS_VISITED_BICOMPONENT;
+import static etomica.graph.traversal.Traversal.STATUS_VISITED_NODE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import etomica.graph.model.Graph;
+
+/**
+ * This TraversalVisitor visits nodes and keeps track of biconnected
+ * components.
+ */
+public class BCVisitor implements TraversalVisitor {
+
+  private List<List<Byte>> biComponents;
+  private List<Byte> biComponent;
+  private boolean isArticulation = false;
+
+  public BCVisitor(List<List<Byte>> biComponents) {
+    this.biComponents = biComponents;
+  }
+
+  public boolean visit(byte nodeID, byte status) {
+
+    // the next node is an articulation point and should not be processed
+    if (status == STATUS_START_BICOMPONENT) {
+      biComponent = new ArrayList<Byte>();
+      biComponents.add(biComponent);
+    }
+    else if (status == STATUS_VISITED_BICOMPONENT) {
+      biComponent = null;
+    }
+    else if  (status == STATUS_ARTICULATION_POINT) {
+      isArticulation = true;
+    }
+    // visiting a node in the current biconnected component
+    else if (status == STATUS_VISITED_NODE) {
+      // if it is an articulation point, ignore it
+      if (isArticulation) {
+        isArticulation = false;
+      }
+      else {
+        biComponent.add(nodeID);
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Returns the biconnected components of the given graph.
+   */
+  public static List<List<Byte>> getBiComponents(Graph g) {
+    List<List<Byte>> biComponents = new ArrayList<List<Byte>>();
+    BCVisitor v = new BCVisitor(biComponents);
+    new Biconnected().traverseAll(g, v);
+    return biComponents;
+  }
+}
---
+++ etomica.graph/etomica/graph/traversal/CVisitor.java
@@ -0,0 +1,62 @@
+package etomica.graph.traversal;
+
+import static etomica.graph.traversal.Traversal.STATUS_ARTICULATION_POINT;
+import static etomica.graph.traversal.Traversal.STATUS_START_COMPONENT;
+import static etomica.graph.traversal.Traversal.STATUS_VISITED_COMPONENT;
+import static etomica.graph.traversal.Traversal.STATUS_VISITED_NODE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import etomica.graph.model.Graph;
+
+/**
+ * This TraversalVisitor visits nodes and keeps track of biconnected
+ * components.
+ */
+public class CVisitor implements TraversalVisitor {
+
+  private List<List<Byte>> components;
+  private List<Byte> component;
+  private boolean isArticulation = false;
+
+  public CVisitor(List<List<Byte>> components) {
+    this.components = components;
+  }
+
+  public boolean visit(byte nodeID, byte status) {
+
+    // the next node is an articulation point and should not be processed
+    if (status == STATUS_START_COMPONENT) {
+      component = new ArrayList<Byte>();
+      components.add(component);
+    }
+    else if (status == STATUS_VISITED_COMPONENT) {
+      component = null;
+    }
+    else if  (status == STATUS_ARTICULATION_POINT) {
+      isArticulation = true;
+    }
+    // visiting a node in the current biconnected component
+    else if (status == STATUS_VISITED_NODE) {
+      // if it is an articulation point, ignore it
+      if (isArticulation) {
+        isArticulation = false;
+      }
+      else {
+        component.add(nodeID);
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Returns the biconnected components of the given graph.
+   */
+  public static List<List<Byte>> getComponents(Graph g) {
+    List<List<Byte>> components = new ArrayList<List<Byte>>();
+    CVisitor v = new CVisitor(components);
+    new DepthFirst().traverseAll(g, v);
+    return components;
+  }
+}
