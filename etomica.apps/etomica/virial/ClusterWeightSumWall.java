a5c0ce07cb85e2960f69c0d480fbcd98aa3a5571
Jung Ho Yang <junghoya@buffalo.edu>
2011-11-08 15:46:30 +0000

    cluster weight sum of absolute value for single wall

---
+++ etomica.apps/etomica/virial/ClusterWeightSumWall.java
@@ -0,0 +1,40 @@
+package etomica.virial;
+
+import etomica.data.IData;
+
+public class ClusterWeightSumWall implements ClusterWeight {
+	
+	public ClusterWeightSumWall(MeterVirialExternalFieldRho meter, int pointCount ) {
+		this.meter = meter; 
+		this.pointCount = pointCount;
+		
+	}
+	
+	public ClusterAbstract makeCopy() {
+		
+		return new ClusterWeightSumWall(meter, pointCount);
+	}
+
+	public int pointCount() {
+		
+		return pointCount;
+	}
+
+	public double value(BoxCluster box) {
+		meter.setBox(box);
+		IData idata = meter.getData();
+		double sumwall = 0;
+		for (int i=1; i<idata.getLength()-1; i++) {
+			sumwall +=Math.abs(idata.getValue(i));
+		}
+		
+		return sumwall;
+	}
+
+	public void setTemperature(double temperature) {
+		
+		
+	}
+	private final MeterVirialExternalFieldRho meter;
+	private final int pointCount;
+}
