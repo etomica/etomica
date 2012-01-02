7714b6b11a224f8050ccb41869ba604c0e1961db
Saranya Harikrishnan <saranyah@buffalo.edu>
2012-01-02 21:16:25 +0000

    Changes

---
+++ etomica.apps/etomica/virial/GUI/components/CollectionBondedPotentialAlcohols.java
@@ -0,0 +1,38 @@
+package etomica.virial.GUI.components;
+
+
+import java.util.HashMap;
+
+import etomica.api.IPotential;
+import etomica.atom.iterator.Atomset3IteratorIndexList;
+import etomica.atom.iterator.AtomsetIteratorBasisDependent;
+
+import etomica.potential.P3BondAngle;
+
+import etomica.space.ISpace;
+import etomica.units.Kelvin;
+import etomica.virial.GUI.components2.CollectionAtomicPotential;
+import etomica.virial.GUI.components2.CollectionAtomicPotentials;
+
+
+public class CollectionBondedPotentialAlcohols extends ACollectionBondedPotential{
+	
+	
+	
+	@Override
+	public void addBondedPotentialSets(CollectionPotentialAtomicLike pObject,
+			ISpace space, int speciesIndex) {
+		// TODO Auto-generated method stub
+		
+		pObject.setHashMapPotentialIntraBonded(new HashMap<String[],IPotential>());
+		pObject.setHashMapAtomsetIterator(new HashMap<String[],AtomsetIteratorBasisDependent>());
+		
+		P3BondAngle P3 = new P3BondAngle(space);
+        P3.setAngle(108.5*Math.PI/180);
+        P3.setEpsilon(Kelvin.UNIT.toSim(55400));
+        int[][] triplets = new int[][] {{0,1,2}};
+        pObject.getHashMapPotentialIntraBonded().put(new String[]{"P3","P3"}, P3);
+        pObject.getHashmapAtomsetIterator().put(new String[]{"P3","P3"}, new Atomset3IteratorIndexList(triplets));
+	}
+
+}
