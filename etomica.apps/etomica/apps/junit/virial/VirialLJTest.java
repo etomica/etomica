3a8ce6104aacd72f437e0704a337dcbf88d673a3
Andrew Schultz <ajs42@buffalo.edu>
2012-01-05 19:04:36 +0000

    use fancier SimulationVirialOverlap2 error estimate is (a bit) lower, apparently due to more accurate error estimation (covariance perhaps)

--- etomica.apps/etomica/apps/junit/virial/VirialLJTest.java
+++ etomica.apps/etomica/apps/junit/virial/VirialLJTest.java
@@ -1,18 +1,19 @@
 package etomica.apps.junit.virial;
 
 import junit.framework.TestCase;
+import etomica.chem.elements.ElementSimple;
 import etomica.potential.P2LennardJones;
 import etomica.potential.Potential2Spherical;
 import etomica.space.Space;
 import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresMono;
 import etomica.virial.ClusterAbstract;
 import etomica.virial.MayerEHardSphere;
 import etomica.virial.MayerESpherical;
 import etomica.virial.MayerGeneralSpherical;
 import etomica.virial.MayerHardSphere;
-import etomica.virial.SpeciesFactorySpheres;
 import etomica.virial.cluster.Standard;
-import etomica.virial.simulations.SimulationVirialOverlap;
+import etomica.virial.simulations.SimulationVirialOverlap2;
 
 /**
  * Virial junit test.  This just runs a simple simulation and checks that the
@@ -45,7 +46,8 @@ public class VirialLJTest extends TestCase {
         ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
         refCluster.setTemperature(temperature);
 
-        final SimulationVirialOverlap sim = new SimulationVirialOverlap(space,new SpeciesFactorySpheres(), temperature,refCluster,targetCluster);
+        final SimulationVirialOverlap2 sim = new SimulationVirialOverlap2(space,new SpeciesSpheresMono(space, new ElementSimple("LJ")), temperature,refCluster,targetCluster);
+        sim.integratorOS.setAgressiveAdjustStepFraction(true);
         sim.integratorOS.setNumSubSteps(1000);
         // this will run a short simulation to find it
         sim.initRefPref(null, steps/100);
@@ -57,13 +59,15 @@ public class VirialLJTest extends TestCase {
         sim.ai.setMaxSteps(steps);
         sim.getController().actionPerformed();
 
-        double[] ratioAndError = sim.dsvo.getOverlapAverageAndError();
+        double[] ratioAndError = sim.dvo.getAverageAndError();
         double ratio = ratioAndError[0];
         double error = ratioAndError[1];
+        System.out.println("ratio: "+ratioAndError[0]+" "+ratioAndError[1]);
+
         // check against expected values, 0.0604 +/- 0.0036
         assertTrue("Final ratio within expected limits: "+ratio, Math.abs(ratio - 0.0604) < 0.011);
         // improvements to the algorithm might lower this.  be wary of changes that raise it.
         // improvements to uncertainty estimation might alter this up or down, but it shouldn't change by much.
-        assertTrue("Ratio uncertainty within expected limits: "+error, Math.abs(error - 0.0036) < 0.0004);
+        assertTrue("Ratio uncertainty within expected limits: "+error, Math.abs(error - 0.0034) < 0.0003);
     }
 }
