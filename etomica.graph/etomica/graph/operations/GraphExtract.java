9c5ae42cdcf27063cc1e32d0652c714d4e4544c2
Andrew Schultz <ajs42@buffalo.edu>
2012-04-11 18:57:14 +0000

    commit class to extract a subgraph

---
+++ etomica.graph/etomica/graph/operations/GraphExtract.java
@@ -0,0 +1,31 @@
+package etomica.graph.operations;
+
+import java.util.List;
+
+import etomica.graph.model.Graph;
+import etomica.graph.model.GraphFactory;
+
+/**
+ * This class takes the given nodes from the given graph and returns a new
+ * graph consisting only of those nodes (and the bonds between them).
+ */
+public class GraphExtract {
+
+  public Graph apply(Graph argument, List<Byte> nodeIDs) {
+    byte n = (byte)nodeIDs.size();
+    Graph g = GraphFactory.createGraph(n);
+    for (byte i=0; i<n; i++) {
+      byte ii = nodeIDs.get(i);
+      g.getNode(i).setColor(argument.getNode(ii).getColor());
+      g.getNode(i).setType(argument.getNode(ii).getType());
+      for (byte j=0; j<i; j++) {
+        byte jj = nodeIDs.get(j);
+        if (argument.hasEdge(jj,ii)) {
+          g.putEdge(j,i);
+          g.getEdge(j,i).setColor(argument.getEdge(jj,ii).getColor());
+        }
+      }
+    }
+    return g;
+  }
+}
