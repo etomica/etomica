9e3fdabf41adbc15e5d1776f07587732d034dbc1
Shu Yang <shuyang@buffalo.edu>
2013-01-19 21:39:54 +0000

    TraPPE N2 virial calculation MSMC

---
+++ etomica.apps/etomica/virial/simulations/VirialN2TraPPE.java
@@ -0,0 +1,234 @@
+package etomica.virial.simulations;
+
+import java.awt.Color;
+
+import etomica.action.IAction;
+import etomica.api.IAtomList;
+import etomica.api.IElement;
+import etomica.api.IIntegratorEvent;
+import etomica.api.IIntegratorListener;
+import etomica.api.ISpecies;
+import etomica.atom.DiameterHashByType;
+import etomica.chem.elements.ElementSimple;
+import etomica.chem.elements.Nitrogen;
+import etomica.chem.elements.Oxygen;
+import etomica.config.IConformation;
+import etomica.data.IData;
+import etomica.data.types.DataGroup;
+import etomica.graphics.ColorSchemeByType;
+import etomica.graphics.SimulationGraphic;
+import etomica.potential.P2CO2EMP;
+import etomica.space.ISpace;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresHetero;
+import etomica.units.Electron;
+import etomica.units.Kelvin;
+import etomica.units.Pixel;
+import etomica.util.Constants;
+import etomica.util.Function.Constant;
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+import etomica.virial.ClusterAbstract;
+import etomica.virial.MayerEGeneral;
+import etomica.virial.MayerEHardSphere;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.SpeciesFactory;
+import etomica.virial.cluster.Standard;
+
+/**
+ * TraPPE N2 model 
+ * Potoff and Siepmann. Vapor-Liquid Eq of Mixtures Containing Alkanes, Carbon Dioxide and Nitrogen, 1999
+ * N site:LJ+ negative partial charge, COM:positive charge
+ * Mayer sampling to evaluate cluster integrals
+ * 
+ * @author shu
+ * 01-18-2013
+ */
+public class VirialN2TraPPE {
+
+	public static void main(String[] args) {
+       
+		VirialN2Param params = new VirialN2Param();
+		if (args.length > 0) {
+			ParseArgs.doParseArgs(params, args);
+		} else {
+			
+		}
+        final int nPoints = params.nPoints;
+        double temperature = params.temperature;
+        long steps = params.numSteps;
+        double sigmaHSRef = params.sigmaHSRef;
+        double refFrac = params.refFrac;
+        
+        double sigmaA = params.sigmaA;
+        double sigmaN = params.sigmaN;
+        double epsilonN = params.epsilonN;
+        double sigmaAN = (sigmaA + sigmaN ) / 2.0;//LB rule
+        double charge = params.charge;
+        System.out.println("N2(TraPPE) overlap sampling B"+nPoints+" at T="+temperature+" Kelvin");
+        temperature = Kelvin.UNIT.toSim(temperature);
+
+        final double[] HSB = new double[9];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        HSB[8] = Standard.B8HS(sigmaHSRef);
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        System.out.println("B"+nPoints+"HS: "+HSB[nPoints]);
+        System.out.println(" twice B2HS:"+  2 * HSB[2]);
+        System.out.println(" three times B3HS:"+  3 * HSB[3]);
+        System.out.println("B4HS:"+  HSB[4]);
+        System.out.println("four times the B4HS:"+ 4* HSB[4]);
+        System.out.println("five times the B5HS:"+ 5* HSB[5]);
+        System.out.println("B6HS:"+  HSB[6]);
+        
+        Space space = Space3D.getInstance();
+        MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
+        MayerEHardSphere eRef = new MayerEHardSphere(sigmaHSRef);
+        ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);        
+        
+        P2CO2EMP pTarget = new P2CO2EMP(space, sigmaA, sigmaAN, sigmaN, 0.0, 0.0, Kelvin.UNIT.toSim(epsilonN), Electron.UNIT.toSim(charge));
+        MayerGeneral fTarget = new MayerGeneral(pTarget);
+        MayerEGeneral eTarget = new MayerEGeneral(pTarget);
+        ClusterAbstract targetCluster = Standard.virialCluster(nPoints, fTarget, nPoints>3, eTarget, true);
+        
+        refCluster.setTemperature(temperature);
+        targetCluster.setTemperature(temperature);
+
+        System.out.println((steps*1000)+" steps ("+steps+" blocks of 1000)");
+       
+        final IConformation conformation = new IConformation() {
+            
+        	public void initializePositions(IAtomList atomList) {
+        		// atoms are N-COM("A")-N, 1-0-2 
+        		double bondL = 1.10;
+                atomList.getAtom(0).getPosition().E(0);// COM("A")
+                atomList.getAtom(1).getPosition().E(0);// Nitrogen
+                atomList.getAtom(1).getPosition().setX(0, -0.5 * bondL);
+                atomList.getAtom(2).getPosition().E(0);// Nitrogen
+                atomList.getAtom(2).getPosition().setX(0, +0.5 * bondL);
+            }
+        };
+        
+        SpeciesSpheresHetero species = new SpeciesSpheresHetero(space, new IElement[]{new ElementSimple("A"), Nitrogen.INSTANCE});
+        species.setChildCount(new int[]{1,2});
+        species.setConformation(conformation);
+        
+        //simulation
+        final SimulationVirialOverlap2 sim = new SimulationVirialOverlap2(space,species, temperature,refCluster,targetCluster, false);
+        sim.box[1].getSampleCluster().value(sim.box[1]);
+        sim.integratorOS.setNumSubSteps(1000);
+        
+        if (false) {
+            double size = 5;
+            sim.box[0].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            sim.box[1].getBoundary().setBoxSize(space.makeVector(new double[]{size,size,size}));
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, space, sim.getController());
+            simGraphic.getDisplayBox(sim.box[0]).setPixelUnit(new Pixel(300.0/size));
+            simGraphic.getDisplayBox(sim.box[1]).setPixelUnit(new Pixel(300.0/size));
+            simGraphic.getDisplayBox(sim.box[0]).setShowBoundary(false);
+            simGraphic.getDisplayBox(sim.box[1]).setShowBoundary(false);
+            //set diameters
+            DiameterHashByType diameter = new DiameterHashByType(sim); 
+            diameter.setDiameter(species.getAtomType(0),0.5);
+            diameter.setDiameter(species.getAtomType(1),0.2);
+            
+            
+            simGraphic.getDisplayBox(sim.box[1]).setDiameterHash(diameter);
+            
+            ColorSchemeByType colorScheme = (ColorSchemeByType)simGraphic.getDisplayBox(sim.box[1]).getColorScheme();
+            colorScheme.setColor(sim.getSpecies(0).getAtomType(0), Color.gray);
+            colorScheme.setColor(sim.getSpecies(0).getAtomType(1), Color.red);
+            
+            simGraphic.makeAndDisplayFrame();
+
+            sim.integratorOS.setNumSubSteps(1000);
+            sim.setAccumulatorBlockSize(1000);
+                
+            // if running interactively, set filename to null so that it doens't read
+            // (or write) to a refpref file
+            sim.getController().removeAction(sim.ai);
+            sim.getController().addAction(new IAction() {
+                public void actionPerformed() {
+                    sim.initRefPref(null, 10);
+                    sim.equilibrate(null, 20);
+                    sim.ai.setMaxSteps(Long.MAX_VALUE);
+                }
+            });
+            sim.getController().addAction(sim.ai);
+            if (Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref) || sim.refPref == 0) {
+                throw new RuntimeException("Oops");
+            }
+
+            return;
+        }
+        
+        // if running interactively, don't use the file
+        String refFileName = args.length > 0 ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+        sim.initRefPref(refFileName, steps/100);
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, steps/40);
+        if (sim.refPref == 0 || Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref)) {
+            throw new RuntimeException("oops");
+        }
+
+        System.out.println("equilibration finished");
+        sim.setAccumulatorBlockSize((int)steps);
+
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.ai.setMaxSteps(steps);
+        for (int i=0; i<2; i++) {
+            System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize()+" "+sim.mcMoveRotate[i].getStepSize());
+        }
+        if (refFrac >= 0) {
+        	 sim.integratorOS.setRefStepFraction(refFrac);
+             sim.integratorOS.setAdjustStepFraction(false);
+        }
+
+        if (true) {
+            IIntegratorListener progressReport = new IIntegratorListener() {
+                public void integratorInitialized(IIntegratorEvent e) {}
+                public void integratorStepStarted(IIntegratorEvent e) {}
+                public void integratorStepFinished(IIntegratorEvent e) {
+                    if ((sim.integratorOS.getStepCount()*10) % sim.ai.getMaxSteps() != 0) return;
+                    System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                    double[] ratioAndError = sim.dvo.getAverageAndError();
+                    System.out.println("abs average: "+ratioAndError[0]*HSB[nPoints]+", error: "+ratioAndError[1]*HSB[nPoints]);
+                }
+            };
+            sim.integratorOS.getEventManager().addListener(progressReport);
+        }
+
+        sim.getController().actionPerformed();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getIdealRefStepFraction());
+        System.out.println("actual reference step frequency "+sim.integratorOS.getRefStepFraction());
+        sim.printResults(HSB[nPoints]);
+       System.out.println("reference B2 at T = 290K :" + -6.4600 / 10 * 6.0221415);
+	}
+
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialN2Param extends ParameterBase {
+        public int nPoints = 2;
+        public double temperature = 290;
+        public long numSteps = 10000;
+        public double sigmaHSRef = 2.0;
+        public double sigmaA = 1.0;
+        public double sigmaN = 3.31;
+        public double epsilonN = 36.0;
+        public double charge = 0.964;
+        public double refFrac = -1;
+    }
+    
+ 
+    
+}
