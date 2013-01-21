d1cc8c4adfeffb2a0982816156dca8bd950c8e53
Shu Yang <shuyang@buffalo.edu>
2013-01-21 14:13:13 +0000

    small change by the author

--- etomica/chem/elements/Fluoride.java
+++ etomica/chem/elements/Fluoride.java
@@ -1,14 +1,16 @@
 package etomica.chem.elements;
 
 /**
- * Class for the Sulfur element.
- *
- * @author Andrew
+ * Class for the Fluoride element.
+ * reference: Atomic weights of the elements 2009 (IUPAC Technical Report)
+ * http://iupac.org/publications/pac/83/2/0359/
+ * 
+ * @author Shu
  */
 public class Fluoride extends ElementChemical {
-    
+
     protected Fluoride(String symbol) {
-        this(symbol, 18.998404);
+        this(symbol, 18.9984);
     }
     
     public Fluoride(String symbol, double mass) {
