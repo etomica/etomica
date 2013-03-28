cb578987ca7bff1731146c8bf55c8b90a8a817e1
David Kofke <kofke@buffalo.edu>
2013-03-28 04:16:01 +0000

    First attempt at computing value of chain diagram, considering all permutations.  Algorithm is straightforward but not efficient.

--- etomica.apps/etomica/virial/ClusterChain.java
+++ etomica.apps/etomica/virial/ClusterChain.java
@@ -75,25 +75,70 @@ public class ClusterChain implements ClusterAbstract {
         
         //Compute the fL and fN's
         // fL[j][k][i] is sum of all permutations of partition i in which j and k are at the ends
-        for(int m=1; m<n; m++) {//structure as nested loops so we know what high bit is
-            final int iH = 1<<m; //high bit
-                                              
-            for(int i=1; i<iH; i++) {
-                for(int j=1; j<m; j++) {
-                    if()
-                    fL[m][j][iH|i] = bond[iH|i]*(fL[i]+fN[i]);
-                }
-                final int iLowBit = i & -i;
-                for(int iS=1; iS<i; iS++) {
-                    if ((iS & iLowBit) == 0) continue;
-                    final int iSComp = i & ~iS;
-                    if ((iSComp | iS) != i) continue;
-                    fN[iH|i] += fL[iH|iS]*(fL[iH|iSComp] + fN[iH|iSComp]);
+        for(int i=1; i<nf; i++) {//sum over subsets of points
+            for(int iS=1; iS<i; iS++) {//sum over partitions of i
+                int iSComp = i & ~iS;
+                if ((iSComp | iS) != i) continue;
+
+                for(int jL=0; jL<n; jL++) {//leaf on one partition
+                    int iL = 1<<jL;
+                    if((iL & iS) == 0) continue;
+                    for(int jR=0; jR<n; jR++) {//leaf on the other partition
+                        int iR = 1<<jR;
+                        if((iR & iSComp) == 0) continue;
+                        fL[jL][jR][i] = 0.0;
+                        for(int jM=0; jM<n; jM++) {//leaf where chains are spliced
+                            int iM = 1<<jM;
+                            if(jM==jL || jM==jR || (iM&i)==0) continue;
+                            fL[jL][jR][i] += fL[jL][jM][iS|iM]*fL[jM][jR][iSComp|iM];
+                        }
+                        fL[jR][jL][i] = fL[jL][jR][i];
+                    }
                 }
             }
         }
+//        for(int m=1; m<n; m++) {//structure as nested loops so we know what high bit is
+//            final int iH = 1<<m; //high bit
+//                                              
+//            for(int i=1; i<iH; i++) {//loop over partitions of j<m
+//                final int iLowBit = i & -i;
+//                final int inc = iLowBit<<1;
+//                for(int iS=iLowBit; iS<i; iS+=inc) {
+//                    //if ((iS & iLowBit) == 0) continue;
+//                    final int iSComp = i & ~iS;
+//                    if ((iSComp | iS) != i) continue;
+//
+//                    for(int jL=1; jL<m; jL++) {
+//                        int iL = 1<<jL;
+//                        if((iL & iS) == 0) continue;
+//                        for(int jR=0; jR<jL; jR++) {
+//                            int iR = 1<<jR;
+//                            if((iR & iSComp) == 0) continue;
+//                            for(int jM=1; jM<m; jM++) {
+//                                int iM = 1<<jM;
+//                                if(jM==jL || jM==jR || (iM&i)==0) continue;
+//                                fL[jL][jR][iH|i] = fL[jL][jM][iH|]*fL[jL][jR][i];
+//                                fL[jL][jR][iH|i] = fL[jL][]
+//                            }
+//                        }
+//                    }
+//                    final int iLowBit = i & -i;
+//                for(int iS=1; iS<i; iS++) {
+//                    if ((iS & iLowBit) == 0) continue;
+//                    final int iSComp = i & ~iS;
+//                    if ((iSComp | iS) != i) continue;
+//                    fN[iH|i] += fL[iH|iS]*(fL[iH|iSComp] + fN[iH|iSComp]);
+//                }
+//            }
+//        }
 
-        value = fL[nf-1] + fN[nf-1];
+        //sum over all leaf pairs for full-length chain
+        value = 0.0;
+        for(int jR=1; jR<n; jR++) {
+            for(int jL=0; jL<jR; jL++) {
+                value += fL[jR][jL][nf-1];
+            }
+        }
     }
 
     protected void updateF(BoxCluster box) {
@@ -104,7 +149,9 @@ public class ClusterChain implements ClusterAbstract {
         // recalculate all f values for all pairs
         for(int i=0; i<n-1; i++) {
             for(int j=i+1; j<n; j++) {
-                bond[(1<<i)|(1<<j)] = f.f(aPairs.getAPair(i,j),cPairs.getr2(i,j), beta)+1;
+                int index = (1<<i)|(1<<j);
+                fL[j][i][index] = f.f(aPairs.getAPair(i,j),cPairs.getr2(i,j), beta)+1;
+                fL[i][j][index] = fL[j][i][index];
             }
         }
     }
