172f24e62015a9c1ec4f35a4d49e24e839aeca9d
Andrew Schultz <ajs42@buffalo.edu>
2013-04-04 17:12:51 +0000

    Commit a class capable of doing multiplication of two graphs with all distinguishable points

---
+++ etomica.graph/etomica/graph/operations/MulUnion.java
@@ -0,0 +1,46 @@
+package etomica.graph.operations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import etomica.graph.model.Edge;
+import etomica.graph.model.Graph;
+
+/**
+ * This operation multiplies two graphs (or each pair of graphs from two sets)
+ * by superimposing all points (with the idea that all nodes are
+ * distinguishable).  The result is a graph with all bonds from both graphs.
+ * 
+ * @author Andrew Schultz
+ */
+public class MulUnion implements Binary {
+
+  public Set<Graph> apply(Set<Graph> left, Set<Graph> right, Parameters params) {
+    assert(params == null);
+    Set<Graph> result = new HashSet<Graph>();
+    for (Graph lg : left) {
+      for (Graph rg : right) {
+        Graph graph = apply(lg, rg);
+        if (graph != null) {
+          result.add(graph);
+        }
+      }
+    }
+    return result;
+  }
+
+  public Graph apply(Graph left, Graph right) {
+    assert(left.nodeCount() == right.nodeCount());
+    Graph result = left.copy();
+    for (Edge e : right.edges()) {
+      byte eid = e.getId();
+      if (result.hasEdge(eid)) {
+        throw new RuntimeException("can't union-multiply two graphs with edges in the same position");
+      }
+      result.putEdge(eid);
+      result.getEdge(eid).setColor(e.getColor());
+    }
+    return result;
+  }
+
+}
