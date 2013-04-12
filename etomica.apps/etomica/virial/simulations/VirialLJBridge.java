ae14a92a33f428f5722d1ff86b241372477f8d75
Andrew Schultz <ajs42@buffalo.edu>
2013-04-12 00:13:16 +0000

    generate the diagrams

--- etomica.apps/etomica/virial/simulations/VirialLJBridge.java
+++ etomica.apps/etomica/virial/simulations/VirialLJBridge.java
@@ -1,27 +1,33 @@
 package etomica.virial.simulations;
 
-import etomica.action.IAction;
-import etomica.data.types.DataDoubleArray;
-import etomica.data.types.DataGroup;
+import java.util.List;
+import java.util.Set;
+
+import etomica.api.IIntegratorEvent;
+import etomica.api.IIntegratorListener;
+import etomica.chem.elements.ElementSimple;
+import etomica.graph.model.Graph;
+import etomica.graph.property.FFTDecomposition;
 import etomica.potential.P2LennardJones;
 import etomica.potential.Potential2Spherical;
 import etomica.space.Space;
 import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresMono;
 import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
 import etomica.virial.ClusterAbstract;
-import etomica.virial.ClusterBonds;
-import etomica.virial.ClusterSumEF;
-import etomica.virial.ConfigurationClusterMove;
+import etomica.virial.ClusterSum;
 import etomica.virial.MayerEHardSphere;
-import etomica.virial.MayerESpherical;
-import etomica.virial.MayerFunction;
 import etomica.virial.MayerGeneralSpherical;
 import etomica.virial.MayerHardSphere;
-import etomica.virial.SpeciesFactorySpheres;
 import etomica.virial.cluster.Standard;
+import etomica.virial.cluster.VirialDiagrams;
 
 /**
- * MSMC computations (via overlap sampling) of the bridge diagram(s) within the f-bond-only formulation of B4 and B5
+ * MSMC computations (via overlap sampling) of the bridge diagram(s) within the
+ * f-bond-only formulation.  Diagrams are filtered from the full set of
+ * biconnected diagrams.  It is also possible to select diagrams with
+ * un-FFT-able biconnected components of 
  * 
  * Kate Shaul
  */
@@ -31,51 +37,17 @@ public class VirialLJBridge {
     public static void main(String[] args) {
 
         VirialLJParam params = new VirialLJParam();
+        ParseArgs.doParseArgs(params, args);
+        boolean isCommandLine = args.length > 0;
+        double temperature = params.temperature;
+        final int nPoints = params.numMolecules;
+        final double sigmaHSRef = params.sigmaHSRef;
+        long steps = params.numSteps;
+        final int iGraph = params.iGraph;
 
-        double temperature; final int nPoints; double sigmaHSRef;
-        long steps;
-        if (args.length == 0) {
-        	
-        	nPoints = params.numMolecules;
-            temperature = params.temperature;
-            steps = params.numSteps;
-            sigmaHSRef = params.sigmaHSRef;
-            
-            // number of overlap sampling steps
-            // for each overlap sampling step, the simulation boxes are allotted
-            // 1000 attempts for MC moves, total
-            
-        } else if (args.length == 4) {
-            //ReadParameters paramReader = new ReadParameters(args[0], params);
-            //paramReader.readParameters();
-        	nPoints = Integer.parseInt(args[0]);
-        	temperature = Double.parseDouble(args[1]);
-            steps = Integer.parseInt(args[2]);
-            sigmaHSRef = Double.parseDouble(args[3]);
-            params.writeRefPref = true;
-        	
-        } else {
-        	throw new IllegalArgumentException("Incorrect number of arguments passed to VirialRowleyAlcohol.");
-        }
-
-        
-
-        final double[] HSB = new double[9];
-        HSB[2] = Standard.B2HS(sigmaHSRef);
-        HSB[3] = Standard.B3HS(sigmaHSRef);
-        HSB[4] = Standard.B4HS(sigmaHSRef);
-        HSB[5] = Standard.B5HS(sigmaHSRef);
-        HSB[6] = Standard.B6HS(sigmaHSRef);
-        HSB[7] = Standard.B7HS(sigmaHSRef);
-        HSB[8] = Standard.B8HS(sigmaHSRef);
         System.out.println("sigmaHSRef: "+sigmaHSRef);
-        System.out.println("B2HS: "+HSB[2]);
-        System.out.println("B3HS: "+HSB[3]+" = "+(HSB[3]/(HSB[2]*HSB[2]))+" B2HS^2");
-        System.out.println("B4HS: "+HSB[4]+" = "+(HSB[4]/(HSB[2]*HSB[2]*HSB[2]))+" B2HS^3");
-        System.out.println("B5HS: "+HSB[5]+" = 0.110252 B2HS^4");
-        System.out.println("B6HS: "+HSB[6]+" = 0.03881 B2HS^5");
-        System.out.println("B7HS: "+HSB[7]+" = 0.013046 B2HS^6");
-        System.out.println("B8HS: "+HSB[8]+" = 0.004164 B2HS^7");
+        final double refIntegral = Standard.BHS(nPoints, sigmaHSRef);
+        System.out.println("B"+nPoints+"HS: "+refIntegral);
         System.out.println("Lennard Jones overlap sampling f-bond decomposition of B"+nPoints+" at T= "+temperature);
 		
         Space space = Space3D.getInstance();
@@ -84,76 +56,82 @@ public class VirialLJBridge {
         MayerEHardSphere eRef = new MayerEHardSphere(sigmaHSRef);
         Potential2Spherical pTarget = new P2LennardJones(space,1.0,1.0);
         MayerGeneralSpherical fTarget = new MayerGeneralSpherical(pTarget);
-        MayerESpherical eTarget = new MayerESpherical(pTarget);
         
-        ClusterAbstract targetCluster;
-        
-        if (nPoints == 4) {
-       
-       
-        
-	        int[][][] bondList = {{{0,1},{1,2},{2,3},{3,0},{0,2},{1,3}}, {}};
-	        
-        	
-        	//ring
-	        //int[][][] bondList = {{{0,1},{1,2},{2,3},{3,0}}, {}};
-	        
-	        ClusterBonds cluster = new ClusterBonds(4, bondList, false);
-	        
-	        
-	
-	        double[] weights = {-1.0/8.0};
-	        
-	        //double[] weights = {-3.0/8.0};
-	  
-		    
-		    targetCluster =  new ClusterSumEF(new ClusterBonds[] {cluster},weights,new MayerFunction[]{eTarget});
-        } else if (nPoints == 5) {
-        	
-        	int[][][] bondList1 = {{{0,1},{0,2},{0,4},{1,2},{1,4},{2,3},{3,4}}, {}};
-        	
-        	int[][][] bondList2 = {{{0,1},{0,2},{0,4},{1,2},{1,4},{2,3},{3,4},{2,4}}, {}};
-        	
-        	int[][][] bondList3 = {{{0,1},{0,2},{0,4},{1,2},{1,4},{2,3},{3,4},{1,3}}, {}};
-        	
-        	int[][][] bondList4 = {{{0,1},{0,2},{0,4},{1,2},{1,4},{2,3},{3,4},{1,3},{0,3}}, {}};
-        	
-        	int[][][] bondList5 = {{{0,1},{0,2},{0,4},{1,2},{1,4},{2,3},{3,4},{1,3},{0,3},{2,4}}, {}};
-        	
-        	ClusterBonds cluster1 = new ClusterBonds(5, bondList1, false);
-        	ClusterBonds cluster2 = new ClusterBonds(5, bondList2, false);
-        	ClusterBonds cluster3 = new ClusterBonds(5, bondList3, false);
-        	ClusterBonds cluster4 = new ClusterBonds(5, bondList4, false);
-        	ClusterBonds cluster5 = new ClusterBonds(5, bondList5, false);
-        	
-        	double[] weights = {-1.0, -1.0, -0.5, -1.0/3.0, -1.0/30.0};
-        	
-        	targetCluster =  new ClusterSumEF(new ClusterBonds[] {cluster1, cluster2, cluster3, cluster4, cluster5},weights,new MayerFunction[]{eTarget});
-        
-        
-        } else {
-        	throw new IllegalArgumentException("Cannot yet compute correction to f-bond decomposition approximation for that order of virial coefficient");
+        if (iGraph == -1) {
+            System.out.println("all bridge diagrams");
+        }
+        else if (iGraph < 0) {
+            System.out.println("all bridge diagrams with segment size >= "+(-iGraph));
+        }
+        else {
+            System.out.println("bridge diagram "+iGraph);
+        }
+        VirialDiagrams diagrams = new VirialDiagrams(nPoints, false, false);
+        diagrams.setDoReeHoover(false);
+        diagrams.setDoShortcut(true);
+        Set<Graph> graphs = diagrams.getMSMCGraphs(true, false);
+        FFTDecomposition isFFT = new FFTDecomposition();
+        List<Integer> segments = isFFT.getSegments();
+        Set<Graph> bridgeGraphs = diagrams.makeGraphList();
+        // 4th order: 31
+        // 5th order: around 500
+        // 6th order: around 16,000
+        // 7th order: around 1,000,000
+        int[] segmentDividers = new int[]{0,0,0,30,32,1000,2000000};
+        int gCount = 0;
+        for (Graph g : graphs) {
+            isFFT.check(g);
+            if (segments.size() == 0) continue;
+            long maxSegment = segments.get(0);
+            segments.clear();
+            int segmentSize = -1;
+            for (int i=3; i<nPoints+1; i++) {
+                if (segmentDividers[i-1] < maxSegment && segmentDividers[i] > maxSegment) {
+                    segmentSize = i;
+                }
+            }
+//            System.out.println(maxSegment+" "+segmentSize);
+            if (iGraph < 0 && segmentSize >= -iGraph) {
+                bridgeGraphs.add(g);
+                System.out.println(g);
+            }
+            else if (gCount == iGraph) {
+                bridgeGraphs.add(g);
+                System.out.println(g);
+            }
+            gCount++;
         }
 
+        ClusterSum targetCluster = diagrams.makeVirialCluster(bridgeGraphs, fTarget, null);
+        System.out.println();
+
+        int[][] refBondList = new int[nPoints-1][2];
+        for (int i=0; i<nPoints-1; i++) {
+            int first = i;
+            int second = i+1;
+            if (first == 1) first++;
+            if (second == 2) first++;
+            if (second == nPoints) second = 1;
+            refBondList[i][0] = first;
+            refBondList[i][1] = second;
+        }
         
         
         targetCluster.setTemperature(temperature);
         ClusterAbstract refCluster = Standard.virialCluster(nPoints, fRef, nPoints>3, eRef, true);
         refCluster.setTemperature(temperature);
 
-        System.out.println((steps*1000)+" steps ("+steps+" blocks of 1000)");
+        System.out.println(steps+" steps (1000 blocks of "+(steps/1000)+")");
 		
-        final SimulationVirialOverlap sim = new SimulationVirialOverlap(space,new SpeciesFactorySpheres(), temperature,refCluster,targetCluster);
-        
-        
-        // The diagram which constitutes B4-B4PY is zero for an overlapped configuration.  Without ConfigurationClusterMove, the initial configuration will be overlapped, and gamma/pi will be zero.
-        // Such configurations will not be visited later, precisely because pi is zero.
-        ConfigurationClusterMove clusterMove = new ConfigurationClusterMove(space, sim.getRandom());
-        clusterMove.initializeCoordinates(sim.box[1]);
-        
+        final SimulationVirialOverlap2 sim = new SimulationVirialOverlap2(space,new SpeciesSpheresMono(space, new ElementSimple("LJ")), temperature,refCluster,targetCluster);
         sim.integratorOS.setNumSubSteps(1000);
+        sim.integratorOS.setAggressiveAdjustStepFraction(true);
+        
+        
+        steps /= 1000;
+        long t1 = System.currentTimeMillis();
         // if running interactively, don't use the file
-        String refFileName = params.writeRefPref ? "refpref"+nPoints+"_"+temperature : null;
+        String refFileName = isCommandLine ? "refpref"+nPoints+"_"+temperature : null;
         // this will either read the refpref in from a file or run a short simulation to find it
 //        sim.setRefPref(1.0082398078547523);
         sim.initRefPref(refFileName, steps/100);
@@ -163,47 +141,41 @@ public class VirialLJBridge {
         
         System.out.println("equilibration finished");
 
-        IAction progressReport = new IAction() {
-            public void actionPerformed() {
-                System.out.print(sim.integratorOS.getStepCount()+" steps: ");
-                double[] ratioAndError = sim.dsvo.getOverlapAverageAndError();
-                System.out.println("abs average: "+ratioAndError[0]*HSB[nPoints]+", error: "+ratioAndError[1]*HSB[nPoints]);
-            }
-        };
-       // sim.integratorOS.addIntervalAction(progressReport);
-       // sim.integratorOS.setActionInterval(progressReport, (int)(steps/10));
 
-        sim.ai.setMaxSteps(steps);
+        
+        if (!isCommandLine) {
+            IIntegratorListener progressReport = new IIntegratorListener() {
+                public void integratorInitialized(IIntegratorEvent e) {}
+                public void integratorStepStarted(IIntegratorEvent e) {}
+                public void integratorStepFinished(IIntegratorEvent e) {
+                    if ((sim.integratorOS.getStepCount()*10) % sim.ai.getMaxSteps() != 0) return;
+                    System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                    double[] ratioAndError = sim.dvo.getAverageAndError();
+                    double ratio = ratioAndError[0];
+                    double error = ratioAndError[1];
+                    System.out.println("abs average: "+ratio*refIntegral+" error: "+error*refIntegral);
+                    if (ratio == 0 || Double.isNaN(ratio)) {
+                        throw new RuntimeException("oops");
+                    }
+                }
+            };
+            sim.integratorOS.getEventManager().addListener(progressReport);
+        }
+
+        sim.integratorOS.setNumSubSteps((int)steps);
+        sim.setAccumulatorBlockSize(steps);
+        sim.ai.setMaxSteps(1000);
         for (int i=0; i<2; i++) {
             System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize());
         }
         sim.getController().actionPerformed();
+        long t2 = System.currentTimeMillis();
 
-        System.out.println("final reference step frequency "+sim.integratorOS.getStepFreq0());
-        System.out.println("actual reference step frequency "+sim.integratorOS.getActualStepFreq0());
+        System.out.println("ideal reference step fraction "+sim.integratorOS.getIdealRefStepFraction());
+        System.out.println("actual reference step fraction "+sim.integratorOS.getRefStepFraction());
         
-        double[] ratioAndError = sim.dsvo.getOverlapAverageAndError();
-        System.out.println("ratio average: "+ratioAndError[0]+", error: "+ratioAndError[1]);
-        System.out.println("abs average: "+ratioAndError[0]*HSB[nPoints]+", error: "+ratioAndError[1]*HSB[nPoints]);
-        DataGroup allYourBase = (DataGroup)sim.accumulators[0].getData(sim.dsvo.minDiffLocation());
-        System.out.println("hard sphere ratio average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].RATIO.index)).getData()[1]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].RATIO_ERROR.index)).getData()[1]);
-        System.out.println("hard sphere   average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].AVERAGE.index)).getData()[0]
-                          +" stdev: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].STANDARD_DEVIATION.index)).getData()[0]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].ERROR.index)).getData()[0]);
-        System.out.println("hard sphere overlap average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].AVERAGE.index)).getData()[1]
-                          +" stdev: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].STANDARD_DEVIATION.index)).getData()[1]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[0].ERROR.index)).getData()[1]);
-        
-        allYourBase = (DataGroup)sim.accumulators[1].getData(sim.accumulators[1].getNBennetPoints()-sim.dsvo.minDiffLocation()-1);
-        System.out.println("lennard jones ratio average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].RATIO.index)).getData()[1]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].RATIO_ERROR.index)).getData()[1]);
-        System.out.println("lennard jones average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].AVERAGE.index)).getData()[0]
-                          +" stdev: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].STANDARD_DEVIATION.index)).getData()[0]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].ERROR.index)).getData()[0]);
-        System.out.println("lennard jones overlap average: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].AVERAGE.index)).getData()[1]
-                          +" stdev: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].STANDARD_DEVIATION.index)).getData()[1]
-                          +" error: "+((DataDoubleArray)allYourBase.getData(sim.accumulators[1].ERROR.index)).getData()[1]);
+        sim.printResults(refIntegral);
+        System.out.println("time: "+(t2-t1)/1000.0);
 	}
 
     /**
@@ -213,12 +185,11 @@ public class VirialLJBridge {
     	
     	// number of molecules in simulation (e.g., 2 for B2 calculation)
 
- 
-        public int numMolecules = 5;
+        public int numMolecules = 6;
         public double temperature = 1.0;
-        public long numSteps = 10000;
+        public long numSteps = 1000000000;
         public double sigmaHSRef = 1.5;
-        public boolean writeRefPref = false;
+        public int iGraph = -6;
     }
     
 }
