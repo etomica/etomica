19d3c52dde99d2a6508a34a6f49145d22a93ee74
Andrew Schultz <ajs42@buffalo.edu>
2013-05-31 18:31:32 +0000

    commit HS chain with a tail growth move

---
+++ etomica.apps/etomica/virial/MCMoveClusterAtomChainHSTail.java
@@ -0,0 +1,68 @@
+package etomica.virial;
+
+import etomica.api.IAtomList;
+import etomica.api.IRandom;
+import etomica.integrator.mcmove.MCMoveAtom;
+import etomica.space.ISpace;
+import etomica.space.IVectorRandom;
+
+/**
+ * Grows configurations of chains with the pair probability distribution that
+ * is flat for some distance and then has a power-law decay tail.
+ */
+public class MCMoveClusterAtomChainHSTail extends MCMoveAtom {
+
+    public MCMoveClusterAtomChainHSTail(IRandom random, ISpace _space, double sigma, double pow) {
+        super(random, null, _space);
+        this.sigma = sigma;
+        dr = (IVectorRandom)space.makeVector();
+        this.pow = pow;
+        
+        totPCore = (pow-3) / pow;
+    }
+
+    public boolean doTrial() {
+        
+        IAtomList leafAtoms = box.getLeafList();
+        int n = leafAtoms.getAtomCount();
+
+        for (int i=1; i<n; i++) {
+            IVectorRandom pos = (IVectorRandom)leafAtoms.getAtom(i).getPosition();
+            double rand = random.nextDouble();
+            if (rand < totPCore) {
+                pos.setRandomInSphere(random);
+                pos.TE(sigma);
+            }
+            else {
+                double r = Math.pow(1- (rand - totPCore)/(1-totPCore), -1.0/(pow-3))*sigma;
+                pos.setRandomSphere(random);
+                pos.TE(r);
+            }
+            pos.PE(leafAtoms.getAtom(i-1).getPosition());
+        }
+
+		((BoxCluster)box).trialNotify();
+		return true;
+	}
+	
+    public double getA() {
+        return 1;
+    }
+
+    public double getB() {
+    	return 0.0;
+    }
+    
+    public void rejectNotify() {
+        throw new RuntimeException("nope");
+    }
+    
+    public void acceptNotify() {
+    	((BoxCluster)box).acceptNotify();
+    }
+
+    protected final double sigma;
+    protected final IVectorRandom dr;
+    protected final double pow;
+    protected final double totPCore;
+}
