d9db5503e6d7cf1460453b8bdba606bd9457e02f
Andrew Schultz <ajs42@buffalo.edu>
2013-05-31 18:25:36 +0000

    cleanup, reduce memory footprint a bit

--- etomica/util/SineTransform.java
+++ etomica/util/SineTransform.java
@@ -12,12 +12,8 @@ package etomica.util;
  */
 
 public class SineTransform {
-	
-	public SineTransform() {
-	}
-	
-	
-	public double[] forward(double[] f, double del_r) {
+
+	public static double[] forward(double[] f, double del_r) {
 	
 		
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,27 +21,16 @@ public class SineTransform {
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		
 		int N = f.length;
-		
-		double[] Fr = new double[N]; 
-	
-		
-		for (int n=0; n < N; n++) {
-			
-			Fr[n] = (n)*del_r*f[n];
-			
-		}
-		
+
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		// Make auxiliary vector, Fr2 = [0,Fr[1:N-1],*0*,reverse(-Fr[1:N-1])], to evaluate DST with DFT
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		
-		double[] Fr2 = new double[2*(N)];
+        double[] Fr2 = new double[2*N];
 		
-		// Fr2[0] = 0; Fr2[N] = 0;
-
 		for (int i = 1; i<N; i++) {
-			Fr2[i] = Fr[i];           
-			Fr2[N+i] = -Fr[N-i]; 
+			Fr2[i] = (i)*del_r*f[i];
+			Fr2[N+i] = -(N-i)*del_r*f[N-i];
 		}
 		
 		
@@ -56,31 +41,17 @@ public class SineTransform {
 
 		FastFourierTransform fourier = new FastFourierTransform();
 		
-		double[] Fr2i = new double[2*(N)]; // no imaginary part
+		double[] Fr2i = new double[2*N]; // no imaginary part
 		fourier.setData(Fr2, Fr2i);
 		fourier.transform();
 		
-		double[] Fk2 = new double[2*(N)];
-		Fk2 = fourier.getImaginary();
-	
-		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		//% Extract the DST, Fk, from Fk2 = -1/N*[#,Fk,#,#]
-		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	
-		double del_k = Math.PI/((N)*del_r);
-		
-		double[] Fk = new double[N];
-		
-		for (int i = 0; i<N; i++) {
-			
-			 Fk[i] = -(N)*Fk2[i]; 
-			
-		}
-		
+		double[] Fk2 = fourier.getImaginary();
+
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		//% Compute fk (including zeroth mode) from Fk: 
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
+        double del_k = Math.PI/((N)*del_r);
 	
 		double [] fk = new double [N];
 		
@@ -88,12 +59,12 @@ public class SineTransform {
 		    
 		    // Special consideration of zeroth mode, fk(k=0):
 	    	// Even if there were an Fk(k=0) we could not utilize it in the regular way
-	    	fk[0] = fk[0] + 4.0*Math.PI*(Fr[i]*(i)*del_r)*del_r;
+	    	fk[0] = fk[0] + 4.0*Math.PI*(i*del_r*f[i]*(i)*del_r)*del_r;
+
+
+	    	// Modes 1 through N-1: 
 	    	
-	    	
-		    // Modes 1 through N-1: 
-	    	
-		     fk[i] = 4.0*Math.PI*(Fk[i]/((i)*del_k))*del_r; 
+            fk[i] = 4.0*Math.PI*(-(N)*Fk2[i]/((i)*del_k))*del_r; 
 		    // Fk[0] corresponds to k=1.
 
 		}
@@ -102,7 +73,7 @@ public class SineTransform {
 		return fk;
 	}
 	
-	public double[] reverse(double[] fk, double del_r) {
+	public static double[] reverse(double[] fk, double del_r) {
 		
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		//% Make auxiliary vector, Fk = k*fk, ignoring the k=0 mode  
@@ -111,29 +82,19 @@ public class SineTransform {
 		int N = fk.length;
 		
 		double del_k = Math.PI/((N)*del_r);
-
-	    double[] Fk = new double[N];
-		
-		for (int i=0;i<N;i++) {
-		    
-		    Fk[i] = fk[i]*(i)*del_k; 
-			
-			//  Fk[i] = fk[i+1]*(i+1)*Math.PI/r_max;
-		    
-		}
 		
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		// Make auxiliary vector, Fk2 = [0,Fk[1:N-1],0,reverse(-Fk[1:N-1])], to evaluate DST with DFT
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		
-		double[] Fk2 = new double[2*(N)];
+		double[] Fk2 = new double[2*N];
 	
 		Fk2[0] = 0;
 		Fk2[N] = 0; 
 		
 		for (int i=1; i<N; i++) {
-			Fk2[i] = Fk[i]; 
-			Fk2[2*N-i] = -Fk[i];    
+			Fk2[i] = fk[i]*(i)*del_k;
+			Fk2[2*N-i] = -Fk2[i];    
 		}
 		
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -146,20 +107,7 @@ public class SineTransform {
 		fourier.setData(Fk2, Fk2i);
 		fourier.transform();
 		
-		double[] Fr2 = new double[2*(N)];
-		Fr2 = fourier.getImaginary();
-
-		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		//% Extract Fr from Fr2
-		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		
-		double[] Fr = new double[N];
-		
-		for (int i=0; i<N; i++) {
-
-			  Fr[i] = -(N)*Fr2[i]; 
-
-		}
+		double[] Fr2 = fourier.getImaginary(); // = Fk2i
 
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		//% Compute fr from Fr=r*fr;
@@ -170,11 +118,11 @@ public class SineTransform {
 		for (int i=1; i<N; i++) {
 
 		    //Special consideration for zeroth mode
-		    fr[0] = fr[0] + 1.0/(2.0*Math.PI*Math.PI)*(Fk[i]*(i)*del_k)*del_k;
+            fr[0] = fr[0] + 1.0/(2.0*Math.PI*Math.PI)*(fk[i]*(i)*del_k*(i)*del_k)*del_k;
 
-		    fr[i] = 1.0/(2.0*Math.PI*Math.PI)*(Fr[i]/(i*del_r))*del_k;
-			
-		    
+            fr[i] = 1.0/(2.0*Math.PI*Math.PI)*(-(N)*Fr2[i]/(i*del_r))*del_k;
+            
+            
 		}
 		
 		
