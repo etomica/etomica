b9984a3f2f566354fa58cd1c4d6995fb66eff2ea
David Kofke <kofke@buffalo.edu>
2013-07-11 13:58:40 +0000

    Wrapped potentials are PotentialHard; add methods to specify and query bonds

--- etomica/potential/P2HardBondedList.java
+++ etomica/potential/P2HardBondedList.java
@@ -3,6 +3,7 @@ package etomica.potential;
 import etomica.api.IAtom;
 import etomica.api.IAtomList;
 import etomica.api.IBox;
+import etomica.atom.Atom;
 import etomica.atom.AtomArrayList;
 import etomica.atom.AtomLeafAgentManager;
 import etomica.atom.AtomLeafAgentManager.AgentSource;
@@ -14,15 +15,15 @@ import etomica.space.Tensor;
 /**
  * Hard potential that wraps two others.  One potential applies to atoms that are found on each other's
  * list of bonded atoms, and the other applies for pairs not on each other's list. The list of bonded atoms must
- * be configured externally to this class.
+ * be configured externally to this class, using the setBonded method.
  * 
  * @author David Kofke
  *
  */
 public class P2HardBondedList extends Potential2 implements PotentialHard, AgentSource {
         
-    public P2HardBondedList(Simulation sim, Potential2HardSpherical bondedPotential, 
-            Potential2HardSpherical nonBondedPotential) {
+    public P2HardBondedList(Simulation sim, PotentialHard bondedPotential, 
+            PotentialHard nonBondedPotential) {
         super(sim.getSpace());
         this.bondedPotential = bondedPotential;
         this.nonBondedPotential = nonBondedPotential;
@@ -71,8 +72,42 @@ public class P2HardBondedList extends Potential2 implements PotentialHard, Agent
         nonBondedPotential.setBox(box);
     }
     
-    protected boolean isBonded(IAtomList pair) {
-        return ((AtomArrayList)agentManager.getAgent(pair.getAtom(0))).contains(pair.getAtom(1));
+    /**
+     * Returns true if the pair of atoms are on each others' bonded list; false otherwise.
+     */
+    public boolean isBonded(IAtomList pair) {
+        return getBondedList(pair.getAtom(0)).contains(pair.getAtom(1));
+    }
+    
+    /**
+     * Adds/removes the pair of atoms to/from each other's bonded list.
+     * Adding a bond to bonded atoms does not cause an error, and has no effect.
+     * Remove a bond from unbonded atoms causes an exception.
+     * 
+     * @param bonded true for adding to bonded list; false for removing
+     * @param pair pair of atoms to be bonded or unbonded
+     * @throws IndexOutOfBoundsException if specifying unbonding, and atoms are not bonded already
+     */
+    public void setBonded(boolean bonding, IAtomList pair) {
+        IAtom atom0 = pair.getAtom(0);
+        IAtom atom1 = pair.getAtom(1);
+        AtomArrayList list0 = getBondedList(atom0);
+        AtomArrayList list1 = getBondedList(atom1);
+        if(bonding) {
+            if(isBonded(pair)) return;
+            list0.add(atom1);
+            list1.add(atom0);
+        } else {
+            list0.remove(list0.indexOf(atom1));//throws exception if not in list
+            list1.remove(list1.indexOf(atom0));
+        }
+    }
+    
+    /**
+     * Returns the list containing the bonded atoms for the given atom.
+     */
+    private AtomArrayList getBondedList(IAtom atom) {
+        return (AtomArrayList)agentManager.getAgent(atom);
     }
     
     public Class getAgentClass() {
@@ -88,7 +123,7 @@ public class P2HardBondedList extends Potential2 implements PotentialHard, Agent
     } 
     
 
-    private final Potential2HardSpherical bondedPotential, nonBondedPotential;
+    private final PotentialHard bondedPotential, nonBondedPotential;
     private boolean lastCollisionIsBonded;
     private AtomLeafAgentManager agentManager;
     private final BoxAgentManager boxAgentManager;
