4e65ae3956530fa5ccf881ccf1908fcd6f6879e9
Andrew Schultz <ajs42@buffalo.edu>
2013-07-12 00:30:30 +0000

    add methods to AtomPair to make it hashable

--- etomica/atom/AtomPair.java
+++ etomica/atom/AtomPair.java
@@ -7,37 +7,40 @@ import etomica.api.IAtomList;
 /**
  * Data structure that contains two mutable atom instances.
  */
-public class AtomPair implements IAtomList, java.io.Serializable {
+public class AtomPair implements IAtomList {
 
     public AtomPair() {
     }
-    
+
     public AtomPair(IAtom atom0, IAtom atom1) {
         this.atom0 = atom0;
         this.atom1 = atom1;
     }
-    
-    /* (non-Javadoc)
-     * @see etomica.AtomSet#getAtom(int)
-     */
+
+    public boolean equals(Object obj) {
+        if (((AtomPair)obj).atom0 == atom0) {
+            return ((AtomPair)obj).atom1 == atom1;
+        }
+        return (((AtomPair)obj).atom0 == atom1 && ((AtomPair)obj).atom1 == atom0);
+    }
+
+    public int hashCode() {
+        return atom0.hashCode() + atom1.hashCode();
+    }
+
     public final IAtom getAtom(int i) {
         if(i == 0) return atom0;
         if(i == 1) return atom1;
         throw new IllegalArgumentException();
     }
 
-    /* (non-Javadoc)
-     * @see etomica.AtomSet#count()
-     */
     public final int getAtomCount() {
         return 2;
     }
-    
+
     public String toString() {
         return "["+atom0+","+atom1+"]";
     }
 
-    private static final long serialVersionUID = 1L;
     public IAtom atom0, atom1;
-
 }
