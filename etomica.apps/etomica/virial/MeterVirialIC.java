0c6bd3c40e8a83f110a243162f130e1d65a2ed43
Andrew Schultz <ajs42@buffalo.edu>
2014-03-01 20:16:30 +0000

    Meter for computing distribution functions

---
+++ etomica.apps/etomica/virial/MeterVirialIC.java
@@ -0,0 +1,67 @@
+package etomica.virial;
+
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.units.Null;
+
+/**
+ * Returns cluster and distribution function values from ClusterSumIC 
+ */
+public class MeterVirialIC implements IEtomicaDataSource, java.io.Serializable {
+    protected static int count;
+    protected final ClusterSumIC targetCluster;
+    private final DataDoubleArray data;
+    private final IEtomicaDataInfo dataInfo;
+    private final DataTag tag;
+    private BoxCluster box;
+    private static final long serialVersionUID = 1L;
+
+    /**
+	 * Constructor for MeterVirial.
+	 */
+	public MeterVirialIC(ClusterSumIC targetCluster) {
+	    this.targetCluster = targetCluster;
+	    int nv = targetCluster.getNumValues();
+        data = new DataDoubleArray(nv);
+        dataInfo = new DataInfoDoubleArray("Cluster Value",Null.DIMENSION, new int[]{nv});
+        tag = new DataTag();
+        dataInfo.addTag(tag);
+	}
+
+	public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+    
+    public DataTag getTag() {
+        return tag;
+    }
+    
+    public IData getData() {
+        double pi = box.getSampleCluster().value(box);
+        if (pi == 0 || pi == Double.POSITIVE_INFINITY || Double.isNaN(pi)) {
+            box.trialNotify();
+            box.getSampleCluster().value(box);
+            throw new RuntimeException("oops "+pi);
+        }
+        double x[] = data.getData();
+        int nv = targetCluster.getNumValues();
+        x[nv-1] = targetCluster.value(box)/pi;
+        for (int i=0; i<nv-1; i++) {
+            x[i] = targetCluster.value12(i)/pi;
+        }
+        return data;
+    }
+    
+    public BoxCluster getBox() {
+        return box;
+    }
+    
+    public void setBox(BoxCluster newBox) {
+        box = newBox;
+    }
+
+}
