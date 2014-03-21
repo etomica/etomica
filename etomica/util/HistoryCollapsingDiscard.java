e8e04756d3f6571b1d680dfa34feb42e331ecbfa
Andrew Schultz <ajs42@buffalo.edu>
2014-03-21 17:10:29 +0000

    Really make HistoryCollapsing a common parent of HistoryCollapsingDiscard

--- etomica/util/HistoryCollapsingDiscard.java
+++ etomica/util/HistoryCollapsingDiscard.java
@@ -10,94 +10,17 @@ package etomica.util;
  * 
  * @author Andrew Schultz
  */
-public class HistoryCollapsingDiscard implements History {
-    
+public class HistoryCollapsingDiscard extends HistoryCollapsing {
+
     public HistoryCollapsingDiscard() {this(100);}
     public HistoryCollapsingDiscard(int n) {
         this(n, 2);
     }
-    
+
     public HistoryCollapsingDiscard(int nBins, int nCollapseBins) {
-        setNumCollapseBins(nCollapseBins);
-        setHistoryLength(nBins);
-        reset();
-    }
-    
-    /**
-     * Sets the number of values kept in the history.  If more data
-     * is contained in the history than the set length, the existing 
-     * data is collapsed until it fits within the new length.  History
-     * length must be at least 2.
-     */
-    public void setHistoryLength(int n) {
-        if (n==history.length) return;
-        if (n < numCollapseBins) {
-            throw new IllegalArgumentException("You have GOT to be kidding.  History length must be greater than "+numCollapseBins);
-        }
-        if (n % numCollapseBins != 0) {
-            throw new IllegalArgumentException("History length ("+n+") must be an integer multiple of the # of collapse bins ("+numCollapseBins+")");
-        }
-        while (n < cursor) {
-            collapseData();
-        }
-
-        double[] temp = new double[n];
-        System.arraycopy(history,0,temp,0,cursor);
-        history = temp;
-        for (int i = cursor; i<n; i++) {
-            history[i] = Double.NaN;
-        }
-
-        temp = new double[n];
-        System.arraycopy(xValues,0,temp,0,cursor);
-        xValues = temp;
-        for (int i = cursor; i<n; i++) {
-            xValues[i] = Double.NaN;
-        }
+        super(nBins, nCollapseBins);
     }
 
-    public int getHistoryLength() {
-        return history.length;
-    }
-	
-    /**
-     * Sets the number of "collapse" bins.  When the data is collapsed, one
-     * value will be taken from each consecutive n bins, where n is the number
-     * of bins set by this method.  The value will be taken from the middle bin
-     * or (for an even number of bins), taken from the bin to the left of the
-     * middle.
-     */
-    public void setNumCollapseBins(int newNumCollapseBins) {
-        numCollapseBins = newNumCollapseBins;
-        if (xValues.length % numCollapseBins != 0) {
-            System.err.println("History length ("+xValues.length+") must be an integer multiple of the # of collapse bins ("+numCollapseBins+")");
-        }
-        reset();
-    }
-    
-    public int getNumCollapseBins() {
-        return numCollapseBins;
-    }
-    
-    /**
-     * Removes entire history, setting all values to NaN.
-     * After calling reset, data will be recorded at every interval.
-     */
-    public void reset() {
-        int nValues = getHistoryLength();
-        for(int i=0; i<nValues; i++) {
-            xValues[i] = Double.NaN;
-            history[i] = Double.NaN;
-        }
-        cursor = 0;
-        interval = 1;
-        intervalCount = 0;
-    }
-    
-    public double[] getXValues() {
-        return xValues;
-    }
-	
     /**
      * adds data to the history.  If insufficient space exists
      * to store the data, existing data is collapsed by 1/2 and 
@@ -131,18 +54,4 @@ public class HistoryCollapsingDiscard implements History {
         cursor /= numCollapseBins;
         interval *= numCollapseBins;
     }
-    
-    /**
-     * Returns the history
-     */
-    public double[] getHistory() {
-        return history;
-    }
-
-    protected double[] history = new double[0];
-    protected int cursor;
-    protected double[] xValues = new double[0];
-    protected int interval;
-    protected int intervalCount;
-    protected int numCollapseBins;
 }
