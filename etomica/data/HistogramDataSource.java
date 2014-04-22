e5b2a8bdf5f5aa203a26ca98f032d2b43aaf92fa
Andrew Schultz <ajs42@buffalo.edu>
2014-04-22 01:29:15 +0000

    commit HistogramDataSource

---
+++ etomica/data/HistogramDataSource.java
@@ -0,0 +1,68 @@
+package etomica.data;
+
+import etomica.data.types.DataFunction;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.data.types.DataFunction.DataInfoFunction;
+import etomica.units.Null;
+import etomica.util.Histogram;
+
+/**
+ * DataSource that simply exposes a Histogram as a DataFunction.
+ */
+public class HistogramDataSource implements IEtomicaDataSource {
+
+    protected final Histogram histogram;
+    protected DataSourceIndependentSimple xDataSource;
+    private DataFunction data;
+    private IEtomicaDataInfo dataInfo;
+    protected final DataTag tag;
+
+    public HistogramDataSource(Histogram histogram) {
+        this.histogram = histogram;
+        tag = new DataTag();
+        setupData();
+    }
+
+    /**
+     * Returns the set of histograms.
+     */
+    public IData getData() {
+        // check to see if current data functions are the right length
+        // histogram might change the number of bins on its own.
+        // covertly call getHistogram() here.  We have to call it anyway
+        // so that the array data gets updated.
+        if (data.getData() != histogram.getHistogram() ||
+                xDataSource.getIndependentData(0).getData() != histogram.xValues()) {
+            setupData();
+        }
+        
+        return data;
+    }
+
+    /**
+     * Constructs the Data objects used by this class.
+     */
+    private void setupData() {
+        DataInfoDoubleArray independentInfo = new DataInfoDoubleArray("x",Null.DIMENSION,new int[]{histogram.getNBins()});
+        data = new DataFunction(new int[]{histogram.getNBins()}, histogram.getHistogram());
+        if (xDataSource != null) {
+            xDataSource.update(histogram.xValues(), independentInfo);
+        }
+        else {
+            xDataSource = new DataSourceIndependentSimple(histogram.xValues(), independentInfo);
+        }
+        dataInfo = new DataInfoFunction("histogram", Null.DIMENSION, xDataSource);
+        dataInfo.addTag(tag);
+    }
+
+    public DataTag getTag() {
+        return tag;
+    }
+
+    /**
+     * Returns the DataInfo for the output Data.
+     */
+    public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+}
