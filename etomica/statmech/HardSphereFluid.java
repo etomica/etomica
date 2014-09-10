5821ce14c1f329801d2c2b82d4fd228c47deb7a4
Andrew Schultz <ajs42@buffalo.edu>
2014-09-10 18:36:18 +0000

    commit more accurate EOS for HS

---
+++ etomica/statmech/HardSphereFluid.java
@@ -0,0 +1,56 @@
+package etomica.statmech;
+
+/**
+ * Computes EOS for HS fluid.  Uses exponential of a polynomial with low order
+ * terms take from virial coefficients and high order terms fit to simulation
+ * data.  The fitted terms take a different form that allows them to more
+ * efficiently describe the high-density behavior.
+ * 
+ * @author Andrew Schultz
+ */
+public class HardSphereFluid {
+    
+    public final static double[] NexpVirial = {2.094395102393195,0.5483113556160761,-0.04333676720888957,0.05751876349502738,0.07502830097299396,
+        -0.014474727265714415,-0.003183349327605778,0.017074836705042406,-0.0026047002163831975,-0.005498931252679095};
+    public final static double[] NexpStretchFit = {6.960601433965988e-03,-2.263564675011192e-06,5.464112392968496e-10,-3.004093460638223e-14};
+    public final static double a = 7123.0875;
+    
+    // Z = exp([Ni * rho^(i-1)] + rho^11 [Mi x^i])
+    //    x = a^rho
+    public static double zFluid(double rho) {
+        double expSum = 0;
+        double rhoi = rho;
+        for (int i=0; i<NexpVirial.length; i++) {
+            expSum += NexpVirial[i]*rhoi;
+            rhoi *= rho;
+        }
+        double rhon = rhoi;
+        double x = Math.pow(a, rho);
+        double xi = 1;
+        for (int i=0; i<NexpStretchFit.length; i++) {
+            expSum += rhon*NexpStretchFit[i]*xi;
+            xi *= x;
+        }
+        return Math.exp(expSum);
+    }
+    
+    /**
+     * Returns the difference between absolute free energies between the given
+     * densities for solid (FCC) hard spheres.  The result is computed using
+     * numeric integration with n points.
+     */
+    public static double Asolid(double rho){
+        return Asolid(rho, 10000);
+    }
+    
+    public static double Asolid(double rho, int n) {
+        // use trapezoid rule to integrate the correction
+        double h = rho/n;
+        double sum = 0.0;
+        for (int k=1; k<n; k++) {
+            sum += (zFluid(k*h)-1)/(k*h);
+        }
+        sum += 0.5*(zFluid(rho)-1)/rho;
+        return Math.log(rho)-1 + h*sum;
+    }
+}
---
+++ etomica/statmech/HardSphereSolid.java
@@ -0,0 +1,244 @@
+package etomica.statmech;
+
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+
+/**
+ * Hard sphere solid EOS.  Free energy at one density taken from
+ * http://dx.doi.org/10.1063/1.4818990
+ * 
+ * Z fit to simulation data for perfect FCC crystal.
+ * 
+ * @author Andrew Schultz
+ */
+public class HardSphereSolid {
+
+    protected static final double sqrt2 = Math.sqrt(2);
+    // fitting parameters for Z vs. x
+    protected static final double[] c = new double[]{-4.338711486444215e-01,3.908120499110030e-01,5.954516832360230e-01,1.859059648880398e+01,-2.881675745716088e+02,3.206943027086472e+03,-2.103760905875148e+04,8.317792071175583e+04,-1.806368017944632e+05,1.702057910137580e+05};
+    protected static final double rho0 = 1.0409;
+    protected static final double A0 = 5.91901 + idFreeEnergy(rho0);  // Helmholtz free energy at rho=1.0409, A=5.91901(3)
+
+    public HardSphereSolid(){
+    }
+
+    public static double calcdMudP(double rho, double drho) {
+        double rhop = rho + drho;
+        double rhom = rho - drho;
+        double Zp = zSolid(rhop);
+        double Pp = Zp*rhop;
+        double Ap = Asolid(rhop, 100000);
+        double mup = Ap + Zp;
+        double Zm = zSolid(rhom);
+        double Pm = Zm*rhom;
+        double Am = Asolid(rhom, 100000);
+        double mum = Am + Zm;
+        return (mup - mum)/(Pp - Pm);
+    }
+
+    public static double calcdPdrho(double rho, double drho) {
+        double rhop = rho + drho;
+        double rhom = rho - drho;
+        double Zp = zSolid(rhop);
+        double Pp = Zp*rhop;
+        double Zm = zSolid(rhom);
+        double Pm = Zm*rhom;
+        return (Pp - Pm)/(rhop - rhom);
+    }
+
+    
+    public static double calcdmudrho(double rho, double drho) {
+        double rhop = rho + drho;
+        double rhom = rho - drho;
+        double Zp = zSolid(rhop);
+        double Ap = Asolid(rhop);
+        double mup = Zp + Ap;
+        double Zm = zSolid(rhom);
+        double Am = Asolid(rhom);
+        double mum = Zm + Am;
+        return (mup - mum)/(rhop - rhom);
+    }
+
+    /**
+     * Returns the compressibility factor for solid (FCC) hard spheres at the
+     * given density.
+     */
+    public static double zSolid(double rho) {
+        double x = (1-rho/sqrt2);
+        return 3/x + zSolidCorrection(rho);
+    }
+
+    /**
+     * Returns the correction to high density expression for the
+     * compressibility factor for solid (FCC) hard spheres at the given
+     * density.
+     */
+    public static double zSolidCorrection(double rho) {
+        double x = (1-rho/sqrt2);
+        double z = 0;
+        double xp = 1;
+        for  (int i=0; i<c.length; i++) {
+            z += c[i]*xp;
+            xp *= x;
+        }
+        return z;
+    }
+    
+    public static double densityForPressure(double pressure) {
+        if (pressure < 10.24) throw new RuntimeException("looks like your pressure corresponds to density<1");
+        double rho1 = 2*pressure/(6+sqrt2*pressure);
+        double p1 = zSolid(rho1)*rho1;
+        if (p1 == pressure) return rho1;
+        double p2 = pressure-(p1-pressure)*3;
+        double rho2 = 2*p2/(6+sqrt2*p2);
+        if (rho2 == rho1) throw new RuntimeException("oops");
+        p2 = zSolid(rho2)*rho2;
+        if (p2 == pressure) return rho2;
+        if ((p1-pressure)*(p2-pressure) > 0) {
+            System.out.println("target: "+pressure);
+            System.out.println(rho1+" "+p1);
+            System.out.println(rho2+" "+p2);
+            throw new RuntimeException("oops");
+        }
+        if (rho2 < rho1) {
+            double t = rho1;
+            rho1 = rho2;
+            rho2 = t;
+            t = p1;
+            p1 = p2;
+            p2 = t;
+        }
+        while (true) {
+            double rho3 = (rho1+rho2)/2;
+            if (rho3 == rho1) {
+                return rho1;
+            }
+            if (rho3 == rho2) {
+                return rho2;
+            }
+            double p3 = zSolid(rho3)*rho3;
+            if (p3 == pressure) return rho3;
+            if (p3 == p1 || p3 == p2) return rho3;
+            if (p3 < p1 || p3 > p2) throw new RuntimeException("oops");
+            if (p3 > pressure) {
+                rho2 = rho3;
+                p2 = p3;
+            }
+            else {
+                rho1 = rho3;
+                p1 = p3;
+            }
+        }
+    }
+
+    public static double densityForMu(double mu) {
+        if (mu < 14.77) throw new RuntimeException("looks like your mu corresponds to density<1");
+        double rho1 = 1.00;
+        double rho2 = Math.sqrt(2);
+        double mu1 = zSolid(rho1) + Asolid(rho1, 100000);
+        double mu2 = Double.POSITIVE_INFINITY;
+        while (true) {
+            double rho3 = (rho1+rho2)/2;
+            if (rho3 == rho1) {
+                return rho1;
+            }
+            if (rho3 == rho2) {
+                return rho2;
+            }
+            double mu3 = zSolid(rho3)*rho3;
+            if (mu3 == mu) return rho3;
+            if (mu3 == mu1 || mu3 == mu2) return rho3;
+            if (mu3 < mu1 || mu3 > mu2) throw new RuntimeException("oops");
+            if (mu3 > mu) {
+                rho1 = rho3;
+                mu1 = mu3;
+            }
+            else {
+                rho2 = rho3;
+                mu2 = mu3;
+            }
+        }
+    }
+
+    /**
+     * Returns the difference between absolute free energies between the given
+     * densities for solid (FCC) hard spheres.  The result is computed using
+     * numeric integration with n points.
+     */
+    public static double Asolid(double rho){
+        return Asolid(rho, 10000);
+    }
+    
+    public static double Asolid(double rho, int n) {
+        // integral from rho0 to rho of 3/((1-rho/sqrt2)*rho)
+        // this is the high-density expression for the change in the free energy
+        double dA0 = -3*(Math.log((1-rho/sqrt2)/(1-rho0/sqrt2)*rho0/rho));
+        // now use trapezoid rule to integrate the correction
+        double h = (rho - rho0)/n;
+        double sum = 0.0;
+        sum += 0.5*zSolidCorrection(rho0)/rho0;
+        for (int k=1; k<n; k++) {
+            sum += zSolidCorrection(rho0+k*h)/(rho0+k*h);
+        }
+        sum += 0.5*zSolidCorrection(rho)/rho;
+        return A0 + dA0 + h*sum;
+    }
+
+    /**
+     * Returns the ideal gas free energy for the given density.
+     */
+    public static double idFreeEnergy(double rho){return (Math.log(rho)-1.0);}
+
+    public static void main(String[] args) {
+        HardSphereSolid hs = new HardSphereSolid();
+
+        if (args.length>0) {
+            EOSParams params = new EOSParams();
+            ParseArgs.doParseArgs(params, args);
+            if (params.density > 0) {
+                double rho = params.density;
+                double Z = hs.zSolid(rho);
+                double A = hs.Asolid(rho, 100000);
+                double mu = A + Z;
+                double dPdrho = calcdPdrho(rho, params.drho);
+                System.out.print(String.format("%20.15f %20.15f %21.14f %21.15f %21.15f\n", rho, Z*rho, A, mu, dPdrho));
+            }
+            else if (params.pressure > 0) {
+                double rho = hs.densityForPressure(params.pressure);
+                double Z = hs.zSolid(rho);
+                double A = hs.Asolid(rho, 100000);
+                double mu = A + Z;
+                double dPdrho = calcdPdrho(rho, params.drho);
+                System.out.print(String.format("%20.15f %20.15f %21.14f %21.15f %21.15f\n", rho, Z*rho, A, mu, dPdrho));
+            }
+            else if (params.mu > 0) {
+                double rho = hs.densityForMu(params.pressure);
+                double Z = hs.zSolid(rho);
+                double A = hs.Asolid(rho, 100000);
+                double mu = A + Z;
+                double dPdrho = calcdPdrho(rho, params.drho);
+                System.out.print(String.format("%20.15f %20.15f %21.14f %21.15f %21.15f\n", rho, Z*rho, A, mu, dPdrho));
+            }
+            return;
+        }
+
+        double dr = 0.000001;
+        for (int i=100; i<=141; i++) {
+            double rho = i*0.01;
+            
+            double Z = hs.zSolid(rho);
+            double A = hs.Asolid(rho, 100000);
+            double mu = A + Z;
+            System.out.print(String.format("%4.2f %20.15f %21.14f %21.15f %20.15e %20.15e %20.15e %20.15e %20.15e %20.15e\n", rho, Z, A, mu, hs.calcdMudP(rho, dr), (1/rho), (1/rho)*(1 - Z/(hs.calcdPdrho(rho, dr))), hs.calcdmudrho(rho, dr), (1/rho)*(hs.calcdPdrho(rho, dr)), hs.calcdmudrho(rho,dr)/hs.calcdPdrho(rho,dr)));
+            
+//            System.out.print(String.format("%1.2f %1.2f %20.15f\n", rho1, rho2, -(hs.deltaA(rho1, rho2, 10000)/(1/rho2-1/rho1))));
+        }
+    }
+    
+    public static class EOSParams extends ParameterBase {
+        public double density = 0;
+        public double pressure = 0;
+        public double mu = 0;
+        public double drho = 0.000001;
+    }
+}
