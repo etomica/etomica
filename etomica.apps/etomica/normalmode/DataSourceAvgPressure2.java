4e0afe7d718d60df9b6d0b8335d029c5b4bab27e
Andrew Schultz <ajs42@buffalo.edu>
2014-09-10 23:00:48 +0000

    another way to calculate the pressure (grand canonical route)

---
+++ etomica.apps/etomica/normalmode/DataSourceAvgPressure2.java
@@ -0,0 +1,45 @@
+package etomica.normalmode;
+
+import etomica.data.DataSourceScalar;
+import etomica.integrator.mcmove.MCMoveOverlapListener;
+import etomica.units.Pressure;
+
+/**
+ * Computes pressure from the grand canonical route based on measured free
+ * energy differences and simulation parameters.
+ * 
+ * @author Andrew Schultz
+ */
+public class DataSourceAvgPressure2 extends DataSourceScalar {
+    protected final MCMoveOverlapListener mcMoveOverlapMeter;
+    protected double bmu;
+    protected double bALattice;
+    protected double latticeDensity;
+    protected double volume;
+
+    public DataSourceAvgPressure2(MCMoveOverlapListener mcMoveOverlapMeter, double betaMu, double betaALattice, double latticeDensity, double volume) {
+        super("pressure", Pressure.DIMENSION);
+        this.mcMoveOverlapMeter = mcMoveOverlapMeter;
+        this.bmu = betaMu;
+        this.bALattice = betaALattice;
+        this.latticeDensity = latticeDensity;
+        this.volume = volume;
+    }
+
+    public void setBetaMu(double newBetaMu) {
+        bmu = newBetaMu;
+    }
+
+    public double getDataAsScalar() {
+        double[] ratios = mcMoveOverlapMeter.getRatios();
+        if (ratios == null) return Double.NaN;
+        double q = 1;
+        double sum = 1;
+        double l = Math.exp(bmu);
+        for (int i=ratios.length-1; i>=0; i--) {
+            q *= ratios[i]/l;
+            sum += q;
+        }
+        return (bmu-bALattice)*latticeDensity+Math.log(sum)/volume;
+    }
+}
