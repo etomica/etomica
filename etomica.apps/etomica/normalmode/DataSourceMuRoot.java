07f540b23fbba939773e44ee3a826a6764bd2643
Andrew Schultz <ajs42@buffalo.edu>
2014-09-10 21:21:14 +0000

    commit classes that determine chemical potential from defect free energy and pressure measurements

---
+++ etomica.apps/etomica/normalmode/DataSourceMuRoot.java
@@ -0,0 +1,136 @@
+package etomica.normalmode;
+
+import etomica.data.AccumulatorAverageBlockless;
+import etomica.data.DataDistributer;
+import etomica.data.DataSourceScalar;
+import etomica.integrator.mcmove.MCMoveOverlapListener;
+import etomica.units.Null;
+import etomica.units.Pressure;
+
+/**
+ * Datasource that returns an estimate of mu based on thermodynamic
+ * self-consistency of pressure measurements and defect free energies.
+ * 
+ * @author Andrew Schultz
+ */
+public class DataSourceMuRoot extends DataSourceScalar {
+
+    protected final MCMoveOverlapListener mcMoveOverlapMeter;
+    protected final DataDistributer pSplitter;
+    protected double mu;
+    protected double bALattice, volume, latticeDensity;
+    protected double lastP, lastVacancyConcentration;
+    
+    public DataSourceMuRoot(MCMoveOverlapListener mcMoveOverlapMeter, double mu, DataDistributer pSplitter, double bALattice, double latticeDensity, double volume) {
+        super("mu", Null.DIMENSION);
+        this.mcMoveOverlapMeter = mcMoveOverlapMeter;
+        this.pSplitter = pSplitter;
+        this.mu = mu;
+        this.latticeDensity = latticeDensity;
+        this.bALattice = bALattice;
+        this.volume = volume;
+    }
+    
+    public synchronized double getDataAsScalar() {
+        double[] ratios = mcMoveOverlapMeter.getRatios();
+        if (ratios == null || ratios.length == 0 || pSplitter.getNumDataSinks() < 2) return Double.NaN;
+        double myMu = mu;
+        double lastMu = mu;
+        double maxMu = Double.POSITIVE_INFINITY;
+        double minMu = Double.NEGATIVE_INFINITY;
+        while (true) {
+
+            double p = 1;
+            double tot = 0;
+            double l = Math.exp(myMu);
+            double vAvg = 0;
+            for (int i=ratios.length-1; i>=0; i--) {
+                tot += p;
+                if (Double.isNaN(ratios[i])) {
+                    if (i==ratios.length-1) return Double.NaN;
+                    break;
+                }
+                p /= l*ratios[i];
+            }
+            tot += p;
+            p = 1;
+            double pressure1 = 0;
+            for (int i=0; i<pSplitter.getNumDataSinks() && i<=ratios.length; i++) {
+                double pi = p/tot;
+                AccumulatorAverageBlockless acc = (AccumulatorAverageBlockless)pSplitter.getDataSink(i);
+                if (acc == null || acc.getSampleCount() == 0) {
+                    if (i<2) return Double.NaN;
+                    break;
+                }
+                pressure1 += pi*acc.getData().getValue(acc.AVERAGE.index);
+                vAvg += pi*i;
+                if (ratios.length-1-i >= 0) {
+                    if (Double.isNaN(ratios[ratios.length-1-i])) {
+                        break;
+                    }
+                    p /= l*ratios[ratios.length-1-i];
+                }
+            }
+            lastVacancyConcentration = vAvg / (volume*latticeDensity);
+
+            // we could take pLattice from the data collected when nVacancy=0,
+            // but for a large system, that might rarely happen
+            double pressure2 = (myMu-bALattice)*latticeDensity+Math.log(tot)/volume;
+            lastP = pressure2;
+            if (Double.isNaN(pressure1) || Double.isNaN(pressure2)) {
+                throw new RuntimeException("oops");
+            }
+            double newMu = myMu - (pressure2-pressure1)/latticeDensity;
+//            if (newMu < 15) {
+//                System.out.println("oops");
+//            }
+            if (newMu == myMu || newMu == lastMu || newMu > maxMu || newMu < minMu) {
+                return newMu;
+            }
+            if (((newMu-myMu)*(lastMu-myMu) > 0 && (Math.abs(newMu-myMu) >= Math.abs(lastMu-myMu))) || newMu > maxMu || newMu < minMu) {
+                // getting worse
+                return myMu;
+            }
+            if (newMu < myMu) {
+                // myMu was too large
+                if (maxMu > myMu) maxMu = myMu;
+            }
+            else {
+                // myMu was too small
+                if (minMu < myMu) minMu = myMu;
+            }
+            lastMu = myMu;
+            myMu = newMu;
+        }
+    }
+    
+    public double getLastPressure() {
+        return lastP;
+    }
+    
+    public double getLastVacancyConcentration() {
+        return lastVacancyConcentration;
+    }
+    
+    public class DataSourceMuRootPressure extends DataSourceScalar {
+        public DataSourceMuRootPressure() {
+            super("P", Pressure.DIMENSION);
+        }
+        
+        public double getDataAsScalar() {
+            DataSourceMuRoot.this.getDataAsScalar();
+            return DataSourceMuRoot.this.getLastPressure();
+        }
+    }
+
+    public class DataSourceMuRootVacancyConcentration extends DataSourceScalar {
+        public DataSourceMuRootVacancyConcentration() {
+            super("vc", Null.DIMENSION);
+        }
+        
+        public double getDataAsScalar() {
+            DataSourceMuRoot.this.getDataAsScalar();
+            return DataSourceMuRoot.this.getLastVacancyConcentration();
+        }
+    }
+}
---
+++ etomica.apps/etomica/normalmode/DataSourceMuRoot1.java
@@ -0,0 +1,144 @@
+package etomica.normalmode;
+
+import etomica.data.AccumulatorAverageBlockless;
+import etomica.data.DataDistributer;
+import etomica.data.DataSourceScalar;
+import etomica.integrator.mcmove.MCMoveOverlapListener;
+import etomica.units.Null;
+import etomica.units.Pressure;
+
+/**
+ * Datasource that returns an estimate of mu based on thermodynamic
+ * self-consistency of pressure measurements and defect free energies. This
+ * class only considers the free energy of the first defect (it assumes that
+ * all additional defects do not interact).  The pressure is also only computed
+ * for the perfect and single-vacancy crystal and extrapolated from there.
+ * 
+ * @author Andrew Schultz
+ */
+public class DataSourceMuRoot1 extends DataSourceScalar {
+
+    protected final MCMoveOverlapListener mcMoveOverlapMeter;
+    protected final DataDistributer pSplitter;
+    protected double mu;
+    protected double bALattice, volume, latticeDensity;
+    protected double lastP, lastVacancyConcentration;
+    
+    public DataSourceMuRoot1(MCMoveOverlapListener mcMoveOverlapMeter, double mu, DataDistributer pSplitter, double bALattice, double latticeDensity, double volume) {
+        super("mu", Null.DIMENSION);
+        this.mcMoveOverlapMeter = mcMoveOverlapMeter;
+        this.pSplitter = pSplitter;
+        this.mu = mu;
+        this.latticeDensity = latticeDensity;
+        this.bALattice = bALattice;
+        this.volume = volume;
+    }
+    
+    public synchronized double getDataAsScalar() {
+        double[] ratios = mcMoveOverlapMeter.getRatios();
+        if (ratios == null || ratios.length == 0 || pSplitter.getNumDataSinks() < 2) return Double.NaN;
+        double daDef = Math.log(ratios[ratios.length-1]*(volume/latticeDensity));
+        if (Double.isNaN(daDef)) return Double.NaN;
+        double myMu = mu;
+        double lastMu = mu;
+        double maxMu = Double.POSITIVE_INFINITY;
+        double minMu = Double.NEGATIVE_INFINITY;
+        int n0 = mcMoveOverlapMeter.getMinNumAtoms();
+        int nLatticeAtoms = n0 + ratios.length;
+        while (true) {
+
+            double p = 1;
+            double tot = 0;
+            double vAvg = 0;
+            for (int i=0; p/tot > 1e-14; i++) {
+                tot += p;
+                p /= Math.exp(daDef+myMu)/(nLatticeAtoms-i)*(i+1);
+                if (i>nLatticeAtoms/10) return Double.NaN;
+            }
+            tot += p;
+            p = 1;
+            double pressure1 = 0;
+            AccumulatorAverageBlockless acc0 = (AccumulatorAverageBlockless)pSplitter.getDataSink(0);
+            AccumulatorAverageBlockless acc1 = (AccumulatorAverageBlockless)pSplitter.getDataSink(1);
+            if (acc0 == null || acc1 == null || acc0.getSampleCount() == 0 || acc1.getSampleCount() == 0) return Double.NaN;
+            double p0 = acc0.getData().getValue(acc0.AVERAGE.index);
+            double p1 = acc1.getData().getValue(acc1.AVERAGE.index);
+            double dp = p1-p0;
+            if (dp > 0) dp = 0;
+            for (int i=0; p/tot > 1e-14; i++) {
+                double pi = p/tot;
+                double iPressure = p0 + dp*i;
+                pressure1 += pi*iPressure;
+                if (Double.isNaN(pressure1)) {
+                    throw new RuntimeException("oops "+pi+" "+iPressure);
+                }
+                vAvg += pi*i;
+                p /= Math.exp(daDef+myMu)/(nLatticeAtoms-i)*(i+1);
+            }
+            lastVacancyConcentration = vAvg / (volume*latticeDensity);
+
+            // we could take pLattice from the data collected when nVacancy=0,
+            // but for a large system, that might rarely happen
+            double pressure2 = (myMu-bALattice)*latticeDensity+Math.log(tot)/volume;
+            lastP = pressure2;
+            if (Double.isNaN(pressure2) || Double.isInfinite(pressure2)) {
+                throw new RuntimeException("oops "+myMu+" "+bALattice+" "+latticeDensity+" "+tot+" "+volume);
+            }
+            // calculate a new estimate of mu
+            double newMu = myMu - (pressure2-pressure1)/latticeDensity;
+            if (Double.isNaN(newMu) || Double.isInfinite(newMu)) {
+                throw new RuntimeException("oops "+myMu+" "+pressure1+" "+pressure2+" "+latticeDensity);
+            }
+//            if (newMu < 15) {
+//                System.out.println("oops");
+//            }
+            if (newMu == myMu || newMu == lastMu || newMu > maxMu || newMu < minMu) {
+                return newMu;
+            }
+            if (((newMu-myMu)*(lastMu-myMu) > 0 && (Math.abs(newMu-myMu) >= Math.abs(lastMu-myMu))) || newMu > maxMu || newMu < minMu) {
+                // getting worse
+                return myMu;
+            }
+            if (newMu < myMu) {
+                // myMu was too large
+                if (maxMu > myMu) maxMu = myMu;
+            }
+            else {
+                // myMu was too small
+                if (minMu < myMu) minMu = myMu;
+            }
+            lastMu = myMu;
+            myMu = newMu;
+        }
+    }
+    
+    public double getLastPressure() {
+        return lastP;
+    }
+    
+    public double getLastVacancyConcentration() {
+        return lastVacancyConcentration;
+    }
+    
+    public class DataSourceMuRootPressure extends DataSourceScalar {
+        public DataSourceMuRootPressure() {
+            super("P", Pressure.DIMENSION);
+        }
+        
+        public double getDataAsScalar() {
+            DataSourceMuRoot1.this.getDataAsScalar();
+            return DataSourceMuRoot1.this.getLastPressure();
+        }
+    }
+
+    public class DataSourceMuRootVacancyConcentration extends DataSourceScalar {
+        public DataSourceMuRootVacancyConcentration() {
+            super("vc", Null.DIMENSION);
+        }
+        
+        public double getDataAsScalar() {
+            DataSourceMuRoot1.this.getDataAsScalar();
+            return DataSourceMuRoot1.this.getLastVacancyConcentration();
+        }
+    }
+}
