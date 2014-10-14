f588ad88a000cb51c4cbb24e0dbe2e247b01df19
Ramachandran Subramanian <rsubrama@buffalo.edu>
2014-10-14 17:49:31 +0000

    Cluster for including exchange contributions

---
+++ etomica.apps/etomica/virial/ClusterExchange.java
@@ -0,0 +1,41 @@
+package etomica.virial;
+
+import etomica.api.IPotentialAtomic;
+import etomica.atom.AtomSetSinglet;
+
+
+public class ClusterExchange implements ClusterAbstract {
+	protected double beta;
+	protected IPotentialAtomic p1;
+	public ClusterExchange (IPotentialAtomic p1) {
+		this.p1 = p1;		
+	}
+
+	public ClusterAbstract makeCopy() {
+		ClusterExchange cEx = new ClusterExchange(p1);
+		cEx.setTemperature(1/beta);
+		return cEx;
+	}
+
+	public int pointCount() {
+		return 1;
+	}
+
+	public double value(BoxCluster box) {		
+		int beads = box.getLeafList().getAtomCount();		
+		double sum = 0;
+		AtomSetSinglet atoms = new AtomSetSinglet();
+		for (int i=0; i<beads; i++) {
+			atoms.atom = box.getLeafList().getAtom(i);
+			sum += p1.energy(atoms);
+		}
+		sum /= beads;
+		sum = Math.exp(-beta*sum);
+		return sum;
+	}
+
+	public void setTemperature(double temperature) {
+		beta = 1/temperature;
+	}
+
+}
