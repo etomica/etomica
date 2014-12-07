cb6c0dceb0789e0a5e9abb5e1c5ca47de1311752
Andrew Schultz <ajs42@buffalo.edu>
2014-12-07 19:51:20 +0000

    meter that measures how much HS diameter can increase

---
+++ etomica.apps/etomica/normalmode/MeterMaxExpansion.java
@@ -0,0 +1,49 @@
+package etomica.normalmode;
+
+import etomica.api.IAtom;
+import etomica.api.IAtomList;
+import etomica.api.IBoundary;
+import etomica.api.IBox;
+import etomica.api.IVectorMutable;
+import etomica.data.DataSourceScalar;
+import etomica.nbr.list.NeighborListManager;
+import etomica.space.ISpace;
+import etomica.units.Length;
+
+/**
+ * measures the maximum amount by which by which the HS diameter could increase
+ * without causing overlap
+ * 
+ * @author Andrew Schultz
+ */
+public class MeterMaxExpansion extends DataSourceScalar {
+
+    protected final IVectorMutable dr;
+    protected final NeighborListManager neighborManager;
+    protected final IBox box;
+    
+    public MeterMaxExpansion(ISpace space, IBox box, NeighborListManager neighborManager) {
+        super("displacement", Length.DIMENSION);
+        this.neighborManager = neighborManager;
+        this.box = box;
+        dr = space.makeVector();
+    }
+    
+    public double getDataAsScalar() {
+        IBoundary boundary = box.getBoundary();
+        IAtomList leafList = box.getLeafList();
+        double min = 1e10;
+        for (int i=0; i<leafList.getAtomCount(); i++) {
+            IAtom atomi = leafList.getAtom(i);
+            IAtomList nbrs = neighborManager.getUpList(atomi)[0];
+            for (int j=0; j<nbrs.getAtomCount(); j++) {
+                IAtom atomj = nbrs.getAtom(j);
+                dr.Ev1Mv2(atomi.getPosition(), atomj.getPosition());
+                boundary.nearestImage(dr);
+                double r2 = dr.squared();
+                if (r2 < min) min = r2;
+            }
+        }
+        return Math.sqrt(min)-1;
+    }
+}
