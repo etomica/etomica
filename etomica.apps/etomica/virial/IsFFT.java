a582e597c66cc7e186ff258fe6b112dc65ee87bd
Andrew Schultz <ajs42@buffalo.edu>
2014-12-08 18:33:01 +0000

    cleanup

--- etomica.apps/etomica/virial/IsFFT.java
+++ etomica.apps/etomica/virial/IsFFT.java
@@ -4,66 +4,39 @@
 
  package etomica.virial;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.Set;
 
 import etomica.graph.model.Graph;
 import etomica.graph.model.GraphList;
 import etomica.graph.property.FactorFFT;
 import etomica.graph.property.IsArticulationPair;
-import etomica.graph.property.IsArticulationPoint;
 import etomica.graph.property.Property;
-import etomica.graph.util.GraphNumber;
-//import etomica.graph.viewer.ClusterViewer;
 import etomica.virial.cluster.VirialDiagrams;
 
 public class IsFFT implements Property {
 	
-	IsArticulationPoint p = new IsArticulationPoint();
 	IsArticulationPair q = new IsArticulationPair();
 	FactorFFT r = new FactorFFT();
 	
 	public boolean check(Graph g){
-		
-		int flag=0;
-		Graph copy = g.copy();
-		for (int i =0;((i<g.nodeCount())&&(flag==0));i++){  // Generate a list of Articulation Pairs
-			for (int j=i+1;((j<g.nodeCount())&&(flag==0));j++){
-				if((q.isArticulationPair(g,i,j))&&(flag==0)){
-					//System.out.println("HELLO");
-					g = copy.copy();
-					flag=1;
-				}
-			}
-		}
-		
-		if(flag==0) return false;
-		flag=0;
-		for (int i =0;((i<g.nodeCount())&&(flag==0));i++){  // Generate a list of Articulation Pairs
-			for (int j=i+1;((j<g.nodeCount())&&(flag==0));j++){
-				if((q.isArticulationPair(g,i,j))&&(flag==0)){
-					g = copy.copy();
-					if(r.factor(copy,(byte)i,(byte)j)) {
-					//	System.out.println("FFT possible with this AP");
-						continue;
+
+		for (int i =0;i<g.nodeCount();i++){  // Generate a list of Articulation Pairs
+			for (int j=i+1;j<g.nodeCount();j++){
+				if(q.isArticulationPair(g,i,j)){
+					if(r.factor(g,(byte)i,(byte)j)) {
+					    //	System.out.println("FFT possible with this AP");
+						return true;
 					}
-					else flag=1;
 				}
-		
 			}
 		}
-		if( flag==0) return true;
-		else return false;
+		return false;
 	}
 		
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) {
 		
 		IsFFT m = new IsFFT();
 		final int n =5;
-		int Count=0,FFTCount=0;
         boolean multibody = false;
         boolean flex = false;
         Set<Graph> FFT,MSMC;
@@ -83,13 +56,11 @@ public class IsFFT implements Property {
         			FFT.add(g);
         			//System.out.println("FFT");
         			//System.out.println(g.getStore().toNumberString()+" "+g);
-        			FFTCount++;
         		}
         		else {
         			//System.out.println("MSMC");
         			//System.out.println(g.getStore().toNumberString()+" "+g);
         			MSMC.add(g);
-        			Count++;
         		}
         	}
         }
