eb9d9f0c7cabed608ac939d6073e0cac1493613c
Andrew Schultz <ajs42@buffalo.edu>
2014-12-08 18:44:00 +0000

    less broken

--- etomica.apps/etomica/virial/MeterVirialBDBin.java
+++ etomica.apps/etomica/virial/MeterVirialBDBin.java
@@ -4,13 +4,18 @@
 
 package etomica.virial;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
 import etomica.action.IAction;
 import etomica.api.IRandom;
 import etomica.data.AccumulatorAverageBlockless;
+import etomica.data.AccumulatorAverageBlockless.AllData;
 import etomica.data.DataTag;
 import etomica.data.IEtomicaDataInfo;
 import etomica.data.types.DataDoubleBDArray;
@@ -24,9 +29,21 @@ import etomica.units.Null;
  */
 public class MeterVirialBDBin implements IAction {
 
+    protected final IRandom random;
+    protected final ClusterWheatley targetCluster;
+    private final DataDoubleBDArray data;
+    private final MathContext mc;
+    private final IEtomicaDataInfo dataInfo;
+    private final DataTag tag;
+    private BoxCluster box;
+    protected final AccumulatorAverageBlockless[] accumulators;
+    protected final long[] unscreenedCount;
+    protected final double[] weight;
+    protected double maxSampleValue;
+    protected double tRatio;
     protected static final BigDecimal BDZERO = new BigDecimal(0);
     protected static final BigDecimal BDONE = new BigDecimal(1);
-    protected long nextReweightStep = 10000;
+    protected long nextReweightStep = 100000L;
     protected long totalCount = 0;
 
     /**
@@ -45,8 +62,7 @@ public class MeterVirialBDBin implements IAction {
         File vpi = new File("vpi.dat");
         if (vpi.exists()) vpi.delete();
         int nPairs = targetCluster.pointCount()*(targetCluster.pointCount()-1)/2;
-        count = new long[nPairs+1];
-        screenedCount = new long[nPairs+1];
+        unscreenedCount = new long[nPairs+1];
         weight = new double[nPairs+1];
         accumulators = new AccumulatorAverageBlockless[nPairs+1];
         for (int i = 0; i<accumulators.length; i++) {
@@ -55,6 +71,13 @@ public class MeterVirialBDBin implements IAction {
             accumulators[i].setPushInterval(100000000);
             weight[i] = 1;
         }
+	}
+
+	public void setWeight(double newWeight) {
+	    for (int i=0; i<weight.length; i++) {
+	        weight[i] = newWeight;
+	    }
+	    nextReweightStep = Long.MAX_VALUE;
 	}
 
 	public IEtomicaDataInfo getDataInfo() {
@@ -72,21 +95,19 @@ public class MeterVirialBDBin implements IAction {
     public void actionPerformed() {
         
         totalCount++;
-        BigDecimal[] x = data.getData();
         boolean hasNonZeroValue = targetCluster.checkConfig(box);
-        int edgeCount = targetCluster.getEdgeCount();
-        count[edgeCount]++;
         if (!hasNonZeroValue) {
-            screenedCount[edgeCount]++;
             return;
         }
+        int edgeCount = targetCluster.getEdgeCount();
+        unscreenedCount[edgeCount]++;
         if (weight[edgeCount] < 1 && weight[edgeCount] < random.nextDouble()) {
             return;
         }
+        BigDecimal[] x = data.getData();
         double v = targetCluster.calcValue(box);
         if (v != 0) {
             double pi = box.getSampleCluster().value(box);
-            if (Math.abs(v/pi) > maxSampleValue) maxSampleValue = Math.abs(v/pi);
             x[0] = new BigDecimal(v, mc).divide(new BigDecimal(pi, mc), mc);
 
 //          try {
@@ -103,97 +124,111 @@ public class MeterVirialBDBin implements IAction {
         
         accumulators[edgeCount].addData(data);
         
-        if (totalCount >= nextReweightStep && maxSampleValue > 0) {
-            // tRatio is the ratio of the time needed to compute the biconnected
-            // value (and reference value) for one configuration to the time needed
-            // to generate a configuration and screen it (and any other overhead).
-            long totalSampleCount = 0;
-            for (int i=0; i<count.length; i++) {
-                totalSampleCount += accumulators[i].getSampleCount();
-            }
-            double t0 = totalCount;
-            double t1 = totalSampleCount*tRatio;
-            double E0 = 0;
-            double E1 = 0;
-            // E0 = sum(sci*(steps-sci)/steps * ai^2)
-            // E1 = sum(sci*sci*stdev*stdev/sampci)
-
-            double maxw = 0;
-            double[] localWeight = new double[count.length];
-            double maxAvg = 0;
-            for (int i=0; i<count.length; i++) {
-                long c = count[i] - screenedCount[i];
-                long sampleCount = accumulators[i].getSampleCount();
-                if (sampleCount == 0) sampleCount = 1;
-                localWeight[i] = maxSampleValue*maxSampleValue/sampleCount;
-                if (sampleCount==1) {
-                    // we have never seen i bonds, or the configuration was always screened
-                    // or we just have no statistics
-                    continue;
-                }
-                DataGroup allYourBase = (DataGroup)accumulators[i].getData();
-                DataDoubleBDArray averageData = (DataDoubleBDArray)allYourBase.getData(accumulators[i].AVERAGE.index);
-                DataDoubleBDArray stdevData = (DataDoubleBDArray)allYourBase.getData(accumulators[i].STANDARD_DEVIATION.index);
-
-                BigDecimal avg = averageData.getData()[0];
-                if (avg.doubleValue() == 0) {
-                    // we have seen i bonds, but the cluster value is zero
-                    continue;
-                }
-
-                // E0 = sum(sci*(steps-sci)/steps * ai^2)
-                E0 += c*((double)(totalCount-c))/totalCount * avg.doubleValue()*avg.doubleValue();
-
-                if (Math.abs(avg.doubleValue()) > maxAvg) maxAvg = Math.abs(avg.doubleValue());
-                BigDecimal stdev = stdevData.getData()[0];
-
-                if (sampleCount > 1) {
-                    BigDecimal lw = stdev.multiply(stdev, mc);
-                    localWeight[i] += lw.doubleValue();
-                    if (sampleCount > 10 && localWeight[i] > maxw) maxw = localWeight[i];
-                }
-
-                // E1 = sum(sci*sci*stdev*stdev/sampci)
-                E1 += c*((double)c)/accumulators[i].getSampleCount() * stdev.doubleValue()*stdev.doubleValue();
-            }
-            if (E0 == 0 || E1 == 0) {
-                nextReweightStep *= 2;
-                return;
-            }
-            E0 /= (totalCount*totalCount);
-            E1 /= (totalCount*totalCount);
-            if (E1 == 0) {
-                // no value fluctuations, perhaps B4?
-                E1 = 0.1*E0;
-            }
-//            System.out.println("weights");
-            double newT1 = 0;
-            for (int i=0; i<count.length; i++) {
-                if (localWeight[i] > maxw) {
-                    weight[i] = 1;
-                }
-                else {
-                    weight[i] = localWeight[i] / maxw;
-                }
-                newT1 += tRatio * (count[i]-screenedCount[i]) * weight[i] / totalCount;
-            }
-            double y = 1.0/(1.0 + Math.sqrt(E0*t0/(E1*t1)));
-            System.out.print(String.format("var0 frac %8.5f  t0 frac %8.5f   y %5.3f   new T0 frac %5.3f\n", E0/(E0+E1), t0/(t0+t1), 1-y, 1/(1+newT1)));
-            // newT1 is the new fraction of time we would spend calculating cluster values
-            // y is the optimal fraction of time.
-            // if y<newT, then we scale everything down (happy)
-            // if y>newT1, then we scale up, but some weights>1.
-            //    we'll still calculate all of them, but can't actually cause it to visit more than 100%
-            y /= newT1/(newT1+1);
-            for (int i=0; i<count.length; i++) {
-                double w = weight[i]*y;
-                if (w > 1) w = 1;
-                else if (accumulators[i].getSampleCount() < 10) w = 1;
-                if (i>=targetCluster.pointCount()) System.out.println(String.format("%2d %12d  %6.4f  %6.4f  %6.4f", i, (count[i]-screenedCount[i]), weight[i], w, tRatio * (count[i]-screenedCount[i]) * w / totalCount));
-                weight[i] = w;
-            }
+        if (totalCount >= nextReweightStep) {
+            recomputeWeights();
             nextReweightStep = totalCount*2;
         }
+    }
+    
+    public void recomputeWeights() {
+        // tRatio is the ratio of the time needed to compute the biconnected
+        // value (and reference value) for one configuration to the time needed
+        // to generate a configuration and screen it (and any other overhead).
+        long totalSampleCount = 0;
+        double totalSqValue = 0;
+        for (int i=0; i<accumulators.length; i++) {
+            AccumulatorAverageBlockless ai = accumulators[i];
+            long sc = ai.getSampleCount();
+            totalSampleCount += sc;
+
+            DataGroup allYourBase = (DataGroup)ai.getData();
+            double average = ((DataDoubleBDArray)allYourBase.getData(ai.AVERAGE.index)).getData()[0].doubleValue();
+            double stdev = ((DataDoubleBDArray)allYourBase.getData(ai.STANDARD_DEVIATION.index)).getData()[0].doubleValue();
+            totalSqValue += sc * (stdev*stdev + average*average);
+        }
+        double avgSqValue = totalSqValue / totalSampleCount;
+        double t0 = totalCount;
+        double t1 = totalSampleCount*tRatio;
+        double E0 = 0;
+        double E1 = 0;
+        // E0 = sum(sci*(steps-sci)/steps * ai^2)
+        // E1 = sum(sci*sci*stdev*stdev/sampci)
+
+        double maxw = 0;
+        double[] localWeight = new double[unscreenedCount.length];
+        for (int i=0; i<unscreenedCount.length; i++) {
+            long c = unscreenedCount[i];
+            long sampleCount = accumulators[i].getSampleCount();
+            if (sampleCount == 0) {
+                // we have never seen i bonds, or the configuration was always screened
+                localWeight[i] = avgSqValue;
+                continue;
+            }
+            localWeight[i] = avgSqValue/sampleCount;
+            DataGroup allYourBase = (DataGroup)accumulators[i].getData();
+            DataDoubleBDArray averageData = (DataDoubleBDArray)allYourBase.getData(accumulators[i].AVERAGE.index);
+            DataDoubleBDArray stdevData = (DataDoubleBDArray)allYourBase.getData(accumulators[i].STANDARD_DEVIATION.index);
+
+            BigDecimal avg = averageData.getData()[0];
+            if (avg.doubleValue() == 0) {
+                // we have seen i bonds, but the cluster value is zero
+                continue;
+            }
+
+            // E0 = sum(sci*(steps-sci)/steps * ai^2)
+            E0 += c*((double)(totalCount-c))/totalCount * avg.doubleValue()*avg.doubleValue();
+
+            if (sampleCount<2) {
+                continue;
+            }
+            
+            BigDecimal stdev = stdevData.getData()[0];
+
+            if (sampleCount > 1) {
+                BigDecimal lw = stdev.multiply(stdev, mc);
+                localWeight[i] += lw.doubleValue();
+                if (sampleCount > 10 && localWeight[i] > maxw) maxw = localWeight[i];
+            }
+
+            // E1 = sum(sci*sci*stdev*stdev/sampci)
+            E1 += c*((double)c)/accumulators[i].getSampleCount() * stdev.doubleValue()*stdev.doubleValue();
+        }
+        if (E0 == 0) {
+            nextReweightStep *= 2;
+            return;
+        }
+        if (E1 == 0) {
+            // no value fluctuations, perhaps B4?
+            E1 = avgSqValue/totalSampleCount;
+        }
+        E0 /= (totalCount*totalCount);
+        E1 /= (totalCount*totalCount);
+//        System.out.println("weights");
+        double newT1 = 0;
+        for (int i=0; i<unscreenedCount.length; i++) {
+            weight[i] = localWeight[i]/maxw;
+            newT1 += tRatio * unscreenedCount[i] * weight[i] / totalCount;
+        }
+        double y1 = Math.sqrt(E1*t1/(E0*t0));
+        // newT1 is the new fraction of time we would spend calculating cluster values
+        // y is the optimal fraction of time.
+        // if y<newT, then we scale everything down (happy)
+        // if y>newT1, then we scale up, but some weights>1.
+        //    we'll still calculate all of them, but can't actually cause it to visit more than 100%
+        double x1 = y1 / newT1;
+        newT1 = 0;
+        long totalUnscreened = 0;
+        for (int i=0; i<unscreenedCount.length; i++) {
+            double w = weight[i]*x1;
+            if (w > 1) w = 1;
+            else if (accumulators[i].getSampleCount() < 2) w = 1;
+            long c = unscreenedCount[i];
+//            if (i>=targetCluster.pointCount()) System.out.println(String.format("%2d %12d  %6.4f  %6.4f  %6.4f", i, (count[i]-screenedCount[i]), weight[i], w, tRatio * (count[i]-screenedCount[i]) * w / totalCount));
+            weight[i] = w;
+            newT1 += tRatio * c * w / totalCount;
+            totalUnscreened += c;
+        }
+        System.out.print(String.format("var0 frac %8.5f   t0 frac %8.5f  ideal t0 frac %5.3f  new t0 frac %5.3f   measure frac %5.3f\n", E0/(E0+E1), t0/(t0+t1), 1/(1+y1), 1/(1+newT1), newT1*(totalCount/tRatio/totalUnscreened)));
     }
     
     public ClusterWheatley getTargetCluster() {
@@ -208,12 +243,8 @@ public class MeterVirialBDBin implements IAction {
         box = newBox;
     }
     
-    public long[] getCount() {
-        return count;
-    }
-    
-    public long[] getScreenedCount() {
-        return screenedCount;
+    public long[] getUnscreenedCount() {
+        return unscreenedCount;
     }
     
     public AccumulatorAverageBlockless[] getAccumulators() {
@@ -223,17 +254,121 @@ public class MeterVirialBDBin implements IAction {
     public MathContext getMathContext() {
         return mc;
     }
+    
+    public void writeData(String filename) {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            fw.write(""+totalCount+"\n");
+            for (int i=0; i<unscreenedCount.length; i++) {
+                long sampleCount = accumulators[i].getSampleCount();
+                if (sampleCount == 0) continue;
+                long usc = unscreenedCount[i];
+                AllData ad = accumulators[i].getRawData();
+                fw.write(i+" "+usc+" "+ad.count+" "+((DataDoubleBDArray)ad.sum).getData()[0].toString()+" "+((DataDoubleBDArray)ad.sumSquare).getData()[0].toString()+"\n");
+            }
+            fw.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-    protected final IRandom random;
-    protected final ClusterWheatley targetCluster;
-	private final DataDoubleBDArray data;
-	private final MathContext mc;
-	private final IEtomicaDataInfo dataInfo;
-    private final DataTag tag;
-    private BoxCluster box;
-    protected final AccumulatorAverageBlockless[] accumulators;
-    protected final long[] count, screenedCount;
-    protected final double[] weight;
-    protected double maxSampleValue;
-    protected double tRatio;
+    public void writeWeights(String filename) {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            for (int i=0; i<unscreenedCount.length; i++) {
+                long sampleCount = accumulators[i].getSampleCount();
+                if (sampleCount == 0) continue;
+                fw.write(i+" "+weight[i]+"\n");
+            }
+            fw.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readData(String[] filenames) {
+        int nPairs = targetCluster.pointCount()*(targetCluster.pointCount()-1)/2;
+        BigDecimal[] sums = new BigDecimal[nPairs+1];
+        BigDecimal[] sumSquares = new BigDecimal[nPairs+1];
+        long[] sampleCounts = new long[nPairs+1];
+        try {
+            for (String filename : filenames) {
+                File f = new File(filename);
+                if (!f.exists()) continue;
+                FileReader fr = new FileReader(filename);
+                BufferedReader bufReader = new BufferedReader(fr);
+                String line = bufReader.readLine();
+                totalCount += Long.parseLong(line);
+                while ((line=bufReader.readLine()) != null) {
+                    String iStr = line.replaceAll(" .*", "");
+                    int i = Integer.parseInt(iStr);
+                    String[] values = line.replaceAll("^[^ ]* ", "").split(" +");
+                    long usc = Long.parseLong(values[0]);
+                    long sampleCount = Long.parseLong(values[1]);
+                    BigDecimal sum = new BigDecimal(values[2], mc);
+                    BigDecimal sumSquare = new BigDecimal(values[3], mc);
+                    if (unscreenedCount[i] > 0) {
+                        unscreenedCount[i] += usc;
+                        sampleCounts[i] += sampleCount;
+                        sums[i] = sums[i].add(sum, mc);
+                        sumSquares[i] = sumSquares[i].add(sumSquare, mc);
+                    }
+                    else {
+                        unscreenedCount[i] = usc;
+                        sampleCounts[i] = sampleCount;
+                        weight[i] = 1.0;
+                        sums[i] = sum;
+                        sumSquares[i] = sumSquare;
+                    }
+                }
+                bufReader.close();
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i=0; i<unscreenedCount.length; i++) {
+            long usc = unscreenedCount[i];
+            if (usc == 0) continue;
+            DataDoubleBDArray sumData = new DataDoubleBDArray(1, 40);
+            DataDoubleBDArray sumSquareData = new DataDoubleBDArray(1, 40);
+            sumData.E(new BigDecimal[]{sums[i]});
+            sumSquareData.E(new BigDecimal[]{sumSquares[i]});
+            AccumulatorAverageBlockless acc = new AccumulatorAverageBlockless();
+            acc.putDataInfo(dataInfo);
+            acc.setPushInterval(100000000);
+            accumulators[i] = acc;
+            acc.setRawData(new AllData(sampleCounts[i], sumData, sumSquareData));
+        }
+        recomputeWeights();
+    }
+
+    public void readWeights(String filename) {
+        File f = new File(filename);
+        if (!f.exists()) return;
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader bufReader = new BufferedReader(fr);
+            String line = null;
+            while ((line=bufReader.readLine()) != null) {
+                String iStr = line.replaceAll(" .*", "");
+                int i = Integer.parseInt(iStr);
+                String weightStr = line.replaceAll("^[^ ]* ", "");
+                weight[i] = Double.parseDouble(weightStr);
+                unscreenedCount[i] = 0;
+                AccumulatorAverageBlockless acc = new AccumulatorAverageBlockless();
+                acc.putDataInfo(dataInfo);
+                acc.setPushInterval(100000000);
+                accumulators[i] = acc;
+            }
+            bufReader.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        nextReweightStep = Long.MAX_VALUE;
+    }
+
 }
