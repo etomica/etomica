e546d5687864eee862709471a0aa33ee00c3bfd8
Andrew Schultz <ajs42@buffalo.edu>
2014-12-08 18:37:13 +0000

    insert atoms in a random sequence

--- etomica.apps/etomica/virial/MCMoveClusterAtomHSChain.java
+++ etomica.apps/etomica/virial/MCMoveClusterAtomHSChain.java
@@ -22,13 +22,26 @@ public class MCMoveClusterAtomHSChain extends MCMoveAtom {
         
         IAtomList leafAtoms = box.getLeafList();
         int n = leafAtoms.getAtomCount();
+        if (seq == null) {
+            seq = new int[n];
+        }
+        for (int i=0; i<n; i++) {
+            seq[i] = i;
+        }
+        for (int i=0; i<n; i++) {
+            int j = i+random.nextInt(n-i);
+            int k = seq[j];
+            seq[j] = seq[i];
+            seq[i] = k;
+        }
+        leafAtoms.getAtom(seq[0]).getPosition().E(0);
 
         for (int i=1; i<n; i++) {
-            IVectorRandom pos = (IVectorRandom)leafAtoms.getAtom(i).getPosition();
+            IVectorRandom pos = (IVectorRandom)leafAtoms.getAtom(seq[i]).getPosition();
 
             pos.setRandomInSphere(random);
             pos.TE(sigma);
-            pos.PE(leafAtoms.getAtom(i-1).getPosition());
+            pos.PE(leafAtoms.getAtom(seq[i-1]).getPosition());
         }
 
 		((BoxCluster)box).trialNotify();
@@ -53,4 +66,5 @@ public class MCMoveClusterAtomHSChain extends MCMoveAtom {
 
     protected final double sigma;
     protected final IVectorRandom dr;
+    protected int[] seq;
 }
