bb7d1777949f4e8a5ec050d38990703ffe4d211a
Andrew Schultz <ajs42@buffalo.edu>
2014-12-28 22:17:26 +0000

    molecular potential wrapper

---
+++ etomica/potential/PotentialMolecularMonatomic.java
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package etomica.potential;
+
+import etomica.api.IBox;
+import etomica.api.IMoleculeList;
+import etomica.api.IPotentialAtomic;
+import etomica.atom.AtomArrayList;
+import etomica.space.ISpace;
+
+/**
+ * Intermolecular potential that wraps an atomic potential.  The potential is
+ * assumed to be monatomic.
+ * 
+ * @author Andrew Schultz
+ */
+public class PotentialMolecularMonatomic extends PotentialMolecular {
+    
+    protected final IPotentialAtomic potentialAtomic;
+    protected final AtomArrayList atoms;
+
+    public PotentialMolecularMonatomic(ISpace space, IPotentialAtomic potentialAtomic) {
+        super(potentialAtomic.nBody(), space);
+        this.potentialAtomic = potentialAtomic;
+        atoms = new AtomArrayList(nBody);
+    }
+
+    public double getRange() {
+        return potentialAtomic.getRange();
+    }
+
+    public double energy(IMoleculeList molecules) {
+        atoms.clear();
+        for (int i=0; i<nBody; i++) {
+            atoms.add(molecules.getMolecule(i).getChildList().getAtom(0));
+        }
+        return potentialAtomic.energy(atoms);
+    }
+
+    public void setBox(IBox box) {
+        potentialAtomic.setBox(box);
+    }
+}
