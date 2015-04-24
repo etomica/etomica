a971d4ea90e27b61f3cf5cdee346eeb4e5d56ea4
Andrew Schultz <ajs42@buffalo.edu>
2015-04-24 18:15:14 +0000

    commit atomic sum potential class

---
+++ etomica/potential/PotentialAtomicSum.java
@@ -0,0 +1,47 @@
+package etomica.potential;
+
+import etomica.api.IAtomList;
+import etomica.api.IBox;
+import etomica.api.IPotentialAtomic;
+
+/**
+ * Atomic potential class that simply sums up contributions from multiple
+ * (atomic) potentials.
+ * 
+ * @author Andrew Schultz
+ */
+public class PotentialAtomicSum implements IPotentialAtomic {
+
+    protected final IPotentialAtomic[] p;
+    
+    public PotentialAtomicSum(IPotentialAtomic[] p) {
+        this.p = p;
+    }
+
+    public double getRange() {
+        double r = 0;
+        for (int i=0; i<p.length; i++) {
+            if (r < p[i].getRange()) r = p[i].getRange();
+        }
+        return r;
+    }
+
+    public void setBox(IBox box) {
+        for (int i=0; i<p.length; i++) {
+            p[i].setBox(box);
+        }
+    }
+
+    public int nBody() {
+        return p[0].nBody();
+    }
+
+    public double energy(IAtomList atoms) {
+        double sum = 0;
+        for (int i=0; i<p.length; i++) {
+            sum += p[i].energy(atoms);
+        }
+        return sum;
+    }
+
+}
