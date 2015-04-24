32aac6d243abac8814e03f23f26af06ad7a74314
Andrew Schultz <ajs42@buffalo.edu>
2015-04-24 18:37:47 +0000

    commit molecular sum potential class

---
+++ etomica/potential/PotentialMolecularSum.java
@@ -0,0 +1,47 @@
+package etomica.potential;
+
+import etomica.api.IBox;
+import etomica.api.IMoleculeList;
+import etomica.api.IPotentialMolecular;
+
+/**
+ * Molecular potential class that simply sums up contributions from multiple
+ * (molecular) potentials.
+ * 
+ * @author Andrew Schultz
+ */
+public class PotentialMolecularSum implements IPotentialMolecular {
+
+    protected final IPotentialMolecular[] p;
+    
+    public PotentialMolecularSum(IPotentialMolecular[] p) {
+        this.p = p;
+    }
+
+    public double getRange() {
+        double r = 0;
+        for (int i=0; i<p.length; i++) {
+            if (r < p[i].getRange()) r = p[i].getRange();
+        }
+        return r;
+    }
+
+    public void setBox(IBox box) {
+        for (int i=0; i<p.length; i++) {
+            p[i].setBox(box);
+        }
+    }
+
+    public int nBody() {
+        return p[0].nBody();
+    }
+
+    public double energy(IMoleculeList molecules) {
+        double sum = 0;
+        for (int i=0; i<p.length; i++) {
+            sum += p[i].energy(molecules);
+        }
+        return sum;
+    }
+
+}
