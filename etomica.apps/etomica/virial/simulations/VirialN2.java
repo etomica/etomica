aa47b098e8d898bdf5ed8e3ee7deaa60c2e2b435
Ramachandran Subramanian <rsubrama@buffalo.edu>
2015-05-04 22:38:47 +0000

    simulation class to compute classical and semiclassical virial coefficients of nitrogen using Hellmann (2013) potential

---
+++ etomica.apps/etomica/virial/simulations/VirialN2.java
@@ -0,0 +1,207 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package etomica.virial.simulations;
+
+import java.util.List;
+
+import etomica.api.IIntegratorEvent;
+import etomica.api.IIntegratorListener;
+import etomica.api.IPotentialAtomic;
+import etomica.chem.elements.ElementSimple;
+import etomica.integrator.mcmove.MCMove;
+import etomica.potential.P2NitrogenHellmann;
+import etomica.potential.PotentialMolecularMonatomic;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresRotating;
+import etomica.units.Kelvin;
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+import etomica.virial.ClusterWheatleyHS;
+import etomica.virial.ClusterWheatleySoft;
+import etomica.virial.MayerGeneral;
+import etomica.virial.MayerHardSphere;
+import etomica.virial.cluster.Standard;
+
+public class VirialN2 {
+    public static void main(String[] args) {
+        VirialN2Param params = new VirialN2Param();
+        boolean isCommandline = args.length > 0;
+        if (isCommandline) {
+            ParseArgs parseArgs = new ParseArgs(params);
+            parseArgs.parseArgs(args, true);
+        }
+        else {
+            // default options - choose these before committing to CVS
+            params.potentialLevel = level.classical;
+            params.temperatureK = 500;
+            params.numSteps = (long)1E8;
+            params.pN2HellmannA = false;
+
+            // runtime options - make changes in these and not the default options above           
+//            params.potentialLevel = level.semiClassical;
+//            params.temperatureK = 500;
+//            params.numSteps = (long)1E8;
+        }
+        final int nPoints = params.nPoints;
+        final double temperatureK = params.temperatureK;
+        final double temperature = Kelvin.UNIT.toSim(temperatureK);
+        final boolean pairOnly = params.pairOnly;
+        long steps = params.numSteps;
+        double sigmaHSRef = params.sigmaHSRef;
+        double refFrac = params.refFrac;
+        final boolean pN2HellmannA = params.pN2HellmannA;        
+        
+        final level pLevel = params.potentialLevel;
+        final double[] HSB = new double[8];
+        HSB[2] = Standard.B2HS(sigmaHSRef);
+        HSB[3] = Standard.B3HS(sigmaHSRef);
+        HSB[4] = Standard.B4HS(sigmaHSRef);
+        HSB[5] = Standard.B5HS(sigmaHSRef);
+        HSB[6] = Standard.B6HS(sigmaHSRef);
+        HSB[7] = Standard.B7HS(sigmaHSRef);
+        
+        System.out.println("Overlap sampling for N2 pair potential of Hellmann (2013) at " + temperatureK + " K");
+        if (pLevel == level.semiClassical) System.out.println("Quadratic Feymann-Hibbs effective potential employed.");
+        
+        System.out.println("Reference diagram: B"+nPoints+" for hard spheres with diameter " + sigmaHSRef + " Angstroms");
+        
+        System.out.println("B"+nPoints+"HS: "+HSB[nPoints]);
+        if (steps%1000 != 0) {
+            throw new RuntimeException("steps should be a multiple of 1000");
+        }
+        System.out.println(steps+" steps (1000 IntegratorOverlap steps of "+(steps/1000)+")");
+        
+        Space space = Space3D.getInstance();
+        // make ref and tar clusters
+        MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
+        ClusterWheatleyHS refCluster = new ClusterWheatleyHS(nPoints, fRef);
+        refCluster.setTemperature(temperature);
+        
+        final IPotentialAtomic p2 = pLevel == level.classical ? new P2NitrogenHellmann(space) : new P2NitrogenHellmann(space).makeSemiclassical(temperature);
+        if (pN2HellmannA) ((P2NitrogenHellmann)p2).parametersB = false;
+        PotentialMolecularMonatomic p2N2 = new PotentialMolecularMonatomic(space, p2);                
+        MayerGeneral fTar = new MayerGeneral(p2N2);
+        ClusterWheatleySoft tarCluster = new ClusterWheatleySoft(nPoints, fTar, 1e-12);
+        tarCluster.setTemperature(temperature);
+        
+        // make species
+        SpeciesSpheresRotating speciesUranium = new SpeciesSpheresRotating(space,new ElementSimple("U",238.02891));
+        
+        // make simulation
+        final SimulationVirialOverlap2 sim = new SimulationVirialOverlap2(space, speciesUranium, temperature, refCluster, tarCluster);
+//        sim.init();
+        sim.integratorOS.setNumSubSteps(1000);
+        steps /= 1000;
+        
+        // add additional moves here, simulation already has translation and rotation moves
+        
+        System.out.println();
+        String refFileName = null;
+        if (isCommandline) {
+            String tempString = ""+temperatureK;
+            if (temperatureK == (int)temperatureK) {
+                // temperature is an integer, use "200" instead of "200.0"
+                tempString = ""+(int)temperatureK;
+            }
+            refFileName = "refpref"+nPoints;
+            refFileName += pairOnly ? "_2b" : "_3b";
+            refFileName += "_"+tempString+"_";            
+            if (pLevel == level.semiClassical) refFileName += "SC";
+            if (pLevel == level.classical) refFileName += "C";            
+        }
+        long t1 = System.currentTimeMillis();
+        
+        sim.initRefPref(refFileName, steps/40);
+        if (refFrac >= 0) {
+            sim.integratorOS.setRefStepFraction(refFrac);
+            sim.integratorOS.setAdjustStepFraction(false);
+        }
+        sim.equilibrate(refFileName, steps/10);
+        System.out.println("equilibration finished");
+        
+        sim.integratorOS.setNumSubSteps((int)steps);
+        sim.setAccumulatorBlockSize(steps);
+        sim.integratorOS.setAggressiveAdjustStepFraction(true);
+        sim.ai.setMaxSteps(1000);
+        for (int i=0; i<2; i++) {
+            System.out.println("MC Move step sizes "+sim.mcMoveTranslate[i].getStepSize()+" "+sim.mcMoveRotate[i].getStepSize());
+        }
+        final double refIntegralF = HSB[nPoints];
+        IIntegratorListener progressReport = new IIntegratorListener() {
+            public void integratorInitialized(IIntegratorEvent e) {}
+            public void integratorStepStarted(IIntegratorEvent e) {}
+            public void integratorStepFinished(IIntegratorEvent e) {
+                if ((sim.integratorOS.getStepCount()*10) % sim.ai.getMaxSteps() != 0) return;
+                System.out.print(sim.integratorOS.getStepCount()+" steps: ");
+                double[] ratioAndError = sim.dvo.getAverageAndError();
+                double ratio = ratioAndError[0];
+                double error = ratioAndError[1];
+                //                    System.out.println("abs average: "+ratio*refIntegralF+" error: "+error*refIntegralF);
+                System.out.println("abs average: "+ratio*refIntegralF+" error: "+error*refIntegralF);
+                if (ratio == 0 || Double.isNaN(ratio)) {
+                    throw new RuntimeException("oops");
+                }
+            }
+        };
+        // this is where the simulation takes place
+        sim.getController().actionPerformed();
+        //end of simulation
+        long t2 = System.currentTimeMillis();
+        
+        
+        System.out.println(" ");
+        System.out.println("final reference step fraction "+sim.integratorOS.getIdealRefStepFraction());
+        System.out.println("actual reference step fraction "+sim.integratorOS.getRefStepFraction());
+        System.out.println(" ");
+        
+        System.out.println("Reference system: ");
+        List<MCMove> refMoves = sim.integrators[0].getMoveManager().getMCMoves();
+        for (MCMove m : refMoves) {
+            double acc = m.getTracker().acceptanceRatio();
+            System.out.println(m.toString()+" acceptance ratio: "+acc);            
+        }
+        System.out.println("Target system: ");
+        List<MCMove> tarMoves = sim.integrators[1].getMoveManager().getMCMoves();
+        for (MCMove m : tarMoves) {
+            double acc = m.getTracker().acceptanceRatio();
+
+//            if (acc == 1) {
+//                throw new RuntimeException("something seems fishy");
+//            }
+            System.out.println(m.toString()+" acceptance ratio: "+acc);
+        }
+        sim.printResults(HSB[nPoints]);        
+        
+        if ((t2-t1)/1000.0 > 24*3600) {
+            System.out.println("time: "+(t2-t1)/(24*3600*1000.0)+" days");
+        }
+        else if ((t2-t1)/1000.0 > 3600) {
+            System.out.println("time: "+(t2-t1)/(3600*1000.0)+" hrs");
+        }
+        else if ((t2-t1)/1000.0 > 60) {
+            System.out.println("time: "+(t2-t1)/(60*1000.0)+" mins");
+        }
+        else {
+            System.out.println("time: "+(t2-t1)/1000.0+" secs");
+        }
+    }
+    enum level {
+        classical,semiClassical;
+    }
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialN2Param extends ParameterBase {
+        public int nPoints = 2;        
+        public double temperatureK = 500.0;   // Kelvin
+        public long numSteps = 1000000;
+        public double refFrac = -1;        
+        public double sigmaHSRef = 3.0; // -1 means use equation for sigmaHSRef        
+        public level potentialLevel = level.classical;
+        public boolean pairOnly = true;
+        public boolean pN2HellmannA = true;
+    }
+
+}
