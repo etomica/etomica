757b328e157c25bdfab33912dcd5a5e266d0e371
Andrew Schultz <ajs42@buffalo.edu>
2015-07-22 05:16:30 +0000

    add more bits to graphics

--- etomica.apps/etomica/surfacetension/LJMC.java
+++ etomica.apps/etomica/surfacetension/LJMC.java
@@ -7,18 +7,23 @@ import java.util.List;
 
 import etomica.action.IAction;
 import etomica.action.activity.ActivityIntegrate;
+import etomica.api.IAtomList;
 import etomica.api.IAtomType;
 import etomica.api.IBox;
+import etomica.api.IVector;
 import etomica.api.IVectorMutable;
 import etomica.box.Box;
 import etomica.config.ConfigurationLattice;
 import etomica.data.AccumulatorAverage;
 import etomica.data.AccumulatorAverageCollapsing;
 import etomica.data.AccumulatorAverageFixed;
+import etomica.data.AccumulatorHistory;
 import etomica.data.DataDump;
+import etomica.data.DataFork;
 import etomica.data.DataProcessor;
 import etomica.data.DataPump;
 import etomica.data.DataPumpListener;
+import etomica.data.DataSourceCountSteps;
 import etomica.data.DataTag;
 import etomica.data.meter.MeterNMolecules;
 import etomica.data.meter.MeterPressureTensor;
@@ -27,7 +32,8 @@ import etomica.graphics.DisplayPlot;
 import etomica.graphics.DisplayTextBoxesCAE;
 import etomica.graphics.SimulationGraphic;
 import etomica.integrator.IntegratorMC;
-import etomica.integrator.mcmove.MCMoveAtom;
+import etomica.integrator.mcmove.MCMoveAtomInRegion;
+import etomica.integrator.mcmove.MCMoveStepTracker;
 import etomica.lattice.LatticeCubicFcc;
 import etomica.listener.IntegratorListenerAction;
 import etomica.nbr.cell.PotentialMasterCell;
@@ -37,6 +43,7 @@ import etomica.simulation.Simulation;
 import etomica.space.ISpace;
 import etomica.space3d.Space3D;
 import etomica.species.SpeciesSpheresMono;
+import etomica.util.HistoryCollapsingAverage;
 import etomica.util.ParameterBase;
 import etomica.util.ParseArgs;
 
@@ -47,7 +54,7 @@ public class LJMC extends Simulation {
     public final IBox box;
     public final ActivityIntegrate activityIntegrate;
     public final IntegratorMC integrator;
-    public final MCMoveAtom mcMoveAtom, mcMoveAtomBigStep;
+    public final MCMoveAtomInRegion mcMoveAtom, mcMoveAtomBigStep;
 
     public LJMC(ISpace _space, int numAtoms, double temperature, double aspectRatio) {
         super(_space);
@@ -76,15 +83,13 @@ public class LJMC extends Simulation {
         addBox(box);
         IVectorMutable dim = space.makeVector();
         box.getBoundary().setBoxSize(dim);
-        box.setNMolecules(species, numAtoms);
         integrator.setBox(box);
-        potentialMaster.getNbrCellManager(box).assignCellAll();
         
-        mcMoveAtom = new MCMoveAtom(random, potentialMaster, space);
+        mcMoveAtom = new MCMoveAtomInRegion(random, potentialMaster, space);
         integrator.getMoveManager().addMCMove(mcMoveAtom);
-        mcMoveAtomBigStep = new MCMoveAtom(random, potentialMaster, space);
-        mcMoveAtomBigStep.setStepSize(6);
-        mcMoveAtomBigStep.setStepSizeMin(5);
+        mcMoveAtomBigStep = new MCMoveAtomInRegion(random, potentialMaster, space);
+//        mcMoveAtomBigStep.setStepSize(6);
+//        mcMoveAtomBigStep.setStepSizeMin(5);
         integrator.getMoveManager().addMCMove(mcMoveAtomBigStep);
         integrator.getMoveManager().setFrequency(mcMoveAtomBigStep, 0.2);
         
@@ -111,10 +116,41 @@ public class LJMC extends Simulation {
         }
         double Lx = Lyz*aspectRatio;
         
+        mcMoveAtom.setXRange(-0.6*Lx*xL, 0.6*Lx*xL, 5);
+        mcMoveAtomBigStep.setXRange(0.4*Lx*xL, -0.4*Lx*xL, 50);
+        mcMoveAtomBigStep.setStepSize(6);
+        mcMoveAtomBigStep.setStepSizeMin(5);
+        ((MCMoveStepTracker)mcMoveAtomBigStep.getTracker()).setAcceptanceTarget(0.2);
+//        ((MCMoveStepTracker)mcMoveAtom.getTracker()).setNoisyAdjustment(true);
+//        ((MCMoveStepTracker)mcMoveAtomBigStep.getTracker()).setNoisyAdjustment(true);
+
+        
         //initialize box to just liquid size, put atoms on FCC lattice
+        int nL = (int)((Lx*xL*Lyz*Lyz)*rhoL);
+        box.setNMolecules(species, nL);
         box.getBoundary().setBoxSize(space.makeVector(new double[]{Lx*xL,Lyz,Lyz}));
         new ConfigurationLattice(new LatticeCubicFcc(space), space).initializeCoordinates(box);
         // then expand box size
+        
+        Box boxV = new Box(space);
+        addBox(boxV);
+        boxV.setNMolecules(species, numAtoms-nL);
+        boxV.getBoundary().setBoxSize(space.makeVector(new double[]{Lx*(1-xL),Lyz,Lyz}));
+        new ConfigurationLattice(new LatticeCubicFcc(space), space).initializeCoordinates(boxV);
+        
+        box.setNMolecules(species, numAtoms);
+        IAtomList atoms = box.getLeafList();
+        for (int i=0; i<numAtoms-nL; i++) {
+            IVector vx = boxV.getLeafList().getAtom(i).getPosition();
+            IVectorMutable x = atoms.getAtom(nL+i).getPosition();
+            x.E(vx);
+            if (vx.getX(0) > 0) {
+                x.setX(0, vx.getX(0) + 0.5*Lx*xL);
+            }
+            else {
+                x.setX(0, vx.getX(0) - 0.5*Lx*xL);
+            }
+        }
         
         box.getBoundary().setBoxSize(space.makeVector(new double[]{Lx,Lyz,Lyz}));
 
@@ -154,17 +190,20 @@ public class LJMC extends Simulation {
             
             MeterProfileByVolume densityProfileMeter = new MeterProfileByVolume(space);
             densityProfileMeter.setBox(sim.box);
+            densityProfileMeter.getXDataSource().setNValues(400);
             MeterNMolecules meterNMolecules = new MeterNMolecules();
             meterNMolecules.setSpecies(sim.species);
             densityProfileMeter.setDataSource(meterNMolecules);
+            DataFork profileFork = new DataFork();
             AccumulatorAverageFixed densityProfileAvg = new AccumulatorAverageFixed(10);
             densityProfileAvg.setPushInterval(10);
-            DataPump profilePump = new DataPump(densityProfileMeter, densityProfileAvg);
+            DataPump profilePump = new DataPump(densityProfileMeter, profileFork);
+            profileFork.addDataSink(densityProfileAvg);
             DataDump profileDump = new DataDump();
             densityProfileAvg.addDataSink(profileDump, new AccumulatorAverage.StatType[]{densityProfileAvg.AVERAGE});
             IntegratorListenerAction profilePumpListener = new IntegratorListenerAction(profilePump);
             sim.integrator.getEventManager().addListener(profilePumpListener);
-            profilePumpListener.setInterval(10);
+            profilePumpListener.setInterval(numAtoms);
             dataStreamPumps.add(profilePump);
 
             final FitTanh fitTanh = new FitTanh();
@@ -179,11 +218,29 @@ public class LJMC extends Simulation {
             profilePlot.setLabel("Density");
             ljmcGraphic.add(profilePlot);
             
+            
+            final FitTanh fitTanh0 = new FitTanh();
+            profileFork.addDataSink(fitTanh0);
+
+            DisplayPlot profile0Plot = new DisplayPlot();
+            profileFork.addDataSink(profile0Plot.getDataSet().makeDataSink());
+            fitTanh0.setDataSink(profile0Plot.getDataSet().makeDataSink());
+            profile0Plot.setLegend(new DataTag[]{densityProfileMeter.getTag()}, "density");
+            profile0Plot.setLegend(new DataTag[]{fitTanh0.getTag()}, "fit");
+            profile0Plot.setDoLegend(true);
+            profile0Plot.setLabel("Density0");
+            ljmcGraphic.add(profile0Plot);
+                        
+            
+            
+            
             DataProcessor surfaceTension = new DataProcessorSurfaceTension();
             
             DataPumpListener tensionPump = new DataPumpListener(meterPTensor, surfaceTension, numAtoms);
+            DataFork tensionFork = new DataFork();
+            surfaceTension.setDataSink(tensionFork);
             AccumulatorAverageCollapsing surfaceTensionAvg = new AccumulatorAverageCollapsing();
-            surfaceTension.setDataSink(surfaceTensionAvg);
+            tensionFork.addDataSink(surfaceTensionAvg);
             DisplayTextBoxesCAE tensionBox = new DisplayTextBoxesCAE();
             tensionBox.setAccumulator(surfaceTensionAvg);
             tensionBox.setLabel("Surface Tension");
@@ -192,8 +249,33 @@ public class LJMC extends Simulation {
             sim.integrator.getEventManager().addListener(tensionPump);
             ljmcGraphic.add(tensionBox);
             
+            SurfaceTensionMapped stMap = new SurfaceTensionMapped(space, sim.box, sim.species, sim.potentialMaster);
+            tensionFork.addDataSink(stMap);
+            DataFork mappedFork = new DataFork();
+            stMap.setDataSink(mappedFork);
+            AccumulatorAverageCollapsing surfaceTensionMappedAvg = new AccumulatorAverageCollapsing();
+            mappedFork.addDataSink(surfaceTensionMappedAvg);
+            DisplayTextBoxesCAE mappedTensionBox = new DisplayTextBoxesCAE();
+            mappedTensionBox.setAccumulator(surfaceTensionMappedAvg);
+            mappedTensionBox.setLabel("Mapped Surface Tension");
+            mappedTensionBox.setPrecision(4);
+            ljmcGraphic.add(mappedTensionBox);
             
+            AccumulatorHistory stHistory = new AccumulatorHistory(new HistoryCollapsingAverage());
+            DataSourceCountSteps stepCounter = new DataSourceCountSteps(sim.integrator);
+            stHistory.setTimeDataSource(stepCounter);
+            tensionFork.addDataSink(stHistory);
+            AccumulatorHistory stMappedHistory = new AccumulatorHistory(new HistoryCollapsingAverage());
+            stMappedHistory.setTimeDataSource(stepCounter);
+            mappedFork.addDataSink(stMappedHistory);
             
+            DisplayPlot stPlot = new DisplayPlot();
+            stHistory.setDataSink(stPlot.getDataSet().makeDataSink());
+            stPlot.setLegend(new DataTag[]{stHistory.getTag()}, "conv");
+            stMappedHistory.setDataSink(stPlot.getDataSet().makeDataSink());
+            stPlot.setLegend(new DataTag[]{stMappedHistory.getTag()}, "mapped");
+            stPlot.setLabel("History");
+            ljmcGraphic.add(stPlot);
             
             return;
         }
