3822dedf20634854894d80122497d886b04cbcd5
Ramachandran Subramanian <rsubrama@buffalo.edu>
2015-10-08 22:19:35 +0000

    makeTI() method had a typo which was leading to wrong results. fixed it

--- etomica/potential/P2HeSimplified.java
+++ etomica/potential/P2HeSimplified.java
@@ -145,6 +145,62 @@ public class P2HeSimplified extends Potential2SoftSpherical {
             return u;
         }
     }
+    
+    public P2HeTI makeTI(double temperature) {
+        return this.new P2HeTI(temperature);
+    }
+    public class P2HeTI implements Potential2Spherical {
+
+        protected final double temperature;
+        protected final double mass = 4.002602;
+        protected double fac;
+
+        public P2HeTI(double temperature) {
+            this.temperature = temperature;
+            double hbar = Constants.PLANCK_H/(2*Math.PI);
+            fac = hbar*hbar/(24*mass/2)/(temperature*temperature);
+        }
+
+        public double energy(IAtomList atoms) {
+            dr.Ev1Mv2(atoms.getAtom(1).getPosition(),atoms.getAtom(0).getPosition());
+            boundary.nearestImage(dr);
+            return u(dr.squared());
+        }
+
+        public double getRange() {
+            return P2HeSimplified.this.getRange();
+        }
+
+        public void setBox(IBox box) {
+            P2HeSimplified.this.setBox(box);
+        }
+
+        public int nBody() {
+            return 2;
+        }
+
+        public double u(double r2) {
+            if (r2 < sigmaHC2) {
+                return Double.POSITIVE_INFINITY;
+            }
+
+            double r = Math.sqrt(r2);
+
+            double A1r = A1*r;
+            double expA1r = Math.exp(-A1r);
+
+            double s2 = 1.0/r2;
+            double s4 = s2*s2;
+            double s6 = s4*s2;
+            double s8 = s4*s4;
+            double s10 = s6*s4;
+          
+            double uc = A0*expA1r - A2*s6  - A3*(useC10 ? s10 : s8);
+            double duc = -A0*A1*r*expA1r + A2*6*s6 + A3*(useC10 ? 10*s10 : 8*s8);
+            double usc = uc + fac/r2*(duc*duc);
+            return usc;
+        }
+    }
 
     /**
      * Sets the ith parameter value
