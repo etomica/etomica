368a34df70edc5bf8845c68b167f6f7ec1caa509
Andrew Schultz <ajs42@buffalo.edu>
2015-12-04 23:13:31 +0000

    commit classes capable of using arbitrary v(r) functions

---
+++ etomica.apps/etomica/mappedvirial/MeterMappedVirialV.java
@@ -0,0 +1,60 @@
+package etomica.mappedvirial;
+
+ import etomica.api.IAtom;
+import etomica.api.IBox;
+import etomica.api.IPotentialMaster;
+import etomica.atom.AtomLeafAgentManager;
+import etomica.atom.AtomLeafAgentManager.AgentSource;
+import etomica.atom.iterator.IteratorDirective;
+import etomica.data.DataSourceScalar;
+import etomica.integrator.IntegratorVelocityVerlet;
+import etomica.integrator.IntegratorVelocityVerlet.MyAgent;
+import etomica.potential.PotentialCalculationForceSum;
+import etomica.space.ISpace;
+import etomica.units.Pressure;
+
+public class MeterMappedVirialV extends DataSourceScalar implements  AgentSource<IntegratorVelocityVerlet.MyAgent> {
+
+    protected final ISpace space;
+    protected final IPotentialMaster potentialMaster;
+    protected final PotentialCalculationForceSum pcForce;
+    protected final IBox box;
+    protected final IteratorDirective allAtoms;
+    protected final AtomLeafAgentManager<MyAgent> forceManager;
+    protected final PotentialCalculationMappedVirialV pc;
+    
+    public MeterMappedVirialV(ISpace space, IPotentialMaster potentialMaster, IBox box, int nbins) {
+        super("pma",Pressure.DIMENSION);
+        this.space = space;
+        this.box = box;
+        this.potentialMaster = potentialMaster;
+        pcForce = new PotentialCalculationForceSum();
+        if (box != null) {
+            forceManager = new AtomLeafAgentManager<MyAgent>(this, box, MyAgent.class);
+            pcForce.setAgentManager(forceManager);
+        }
+        else {
+            forceManager = null;
+        }
+        pc = new PotentialCalculationMappedVirialV(space, box, nbins, forceManager);
+        allAtoms = new IteratorDirective();
+    }
+    
+    public MyAgent makeAgent(IAtom a) {
+        return new MyAgent(space);
+    }
+    
+    public void releaseAgent(MyAgent agent, IAtom atom) {}
+
+    public PotentialCalculationMappedVirialV getPotentialCalculation() {
+        return pc;
+    }
+
+    public double getDataAsScalar() {
+        pcForce.reset();
+        potentialMaster.calculate(box, allAtoms, pcForce);
+        pc.reset();
+        potentialMaster.calculate(box, allAtoms, pc);
+        return pc.getPressure();
+    }
+}
