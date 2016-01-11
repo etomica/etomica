3769f38267629ef932a28792967dc86678bf5e8f
Weisong Lin <weisongl@buffalo.edu>
2016-01-11 22:22:14 +0000

    Dipole potential

--- etomica/potential/P2LJDipole.java
+++ etomica/potential/P2LJDipole.java
@@ -1,12 +1,5 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package etomica.potential;
 
-import org.apache.commons.math3.special.Erf;
-
-import etomica.api.IAtom;
 import etomica.api.IAtomList;
 import etomica.api.IBoundary;
 import etomica.api.IBox;
@@ -20,30 +13,21 @@ import etomica.space.ISpace;
 import etomica.space.Tensor;
 
 /**
- * Hard sphere molecule with a dipole sitting at the center.
+ * Lennard Jones molecule with a point dipole sitting in the center.
  *
  * @author Weisong & Shu
- * date:May 2015
+ * date:July 2015
  * 
  */
-public class P2HSDipole extends PotentialMolecular implements IPotentialMolecularSecondDerivative {
-//public class P2HSDipole extends PotentialMolecular implements IPotentialMolecularTorque  {
-	public P2HSDipole(ISpace space) {
-		this(space, 1, 1);
-	}
+public class P2LJDipole extends PotentialMolecular implements IPotentialMolecularSecondDerivative{  
 
-	public P2HSDipole(ISpace space, double dipole) {
-		this(space, 1,dipole);
-	}
-
-	public P2HSDipole(ISpace space, double sigma,double dipole) {
-		this(space, 1,dipole,Double.POSITIVE_INFINITY);
-	}
-
-	public P2HSDipole(ISpace space, double sigma,double dipole,double rCut) {
-		super(2, space);
-		setSigma(sigma);
-		gradient = new IVectorMutable[2];
+    public P2LJDipole(ISpace space, double sigma, double epsilon, double dipole,double rCut) {
+        super(2, space);
+        setSigma(sigma);
+        setEpsilon(epsilon);
+        setDipole(dipole);
+        dr = space.makeVector();
+        gradient = new IVectorMutable[2];
 		gradient[0] = space.makeVector();
 		gradient[1] = space.makeVector();
 		torque = new IVectorMutable[2];
@@ -58,7 +42,6 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 		a[1] = space.makeVector();
 		a[2] = space.makeVector();
 		
-		dr = space.makeVector();
 		drunit = space.makeVector();
 		runit = space.makeVector();
 		work = space.makeVector();
@@ -66,68 +49,17 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 
 		setDipole(dipole);
 		this.rCut = rCut;
-	}
+        
+    }
+    
+    public void setBox(IBox box) {
+        boundary = box.getBoundary();
+    }
 
-
-	public void setBox(IBox box) {
-		boundary = box.getBoundary();
-	}
-
-	public double getRange() {
-		return Double.POSITIVE_INFINITY;
-	}
-
-	public double energy(IMoleculeList pair){
-		IMolecule molecule1 = pair.getMolecule(0);
-		IMolecule molecule2 = pair.getMolecule(1);
-		IAtomList atomList1 = molecule1.getChildList();
-		IAtomList atomList2 = molecule2.getChildList();
-
-		IAtomOriented atom1 = (IAtomOriented)atomList1.getAtom(0);
-		IAtomOriented atom2 = (IAtomOriented)atomList2.getAtom(0);
-		// dr is a r1-r2
-		dr.Ev1Mv2(atom1.getPosition(), atom2.getPosition());
-		boundary.nearestImage(dr);
-		double r2 = dr.squared();
-
-		if(r2 > rCut*rCut){
-			return 0;
-		}
-
-		if(r2 < sigma2) { // hard core
-			return Double.POSITIVE_INFINITY;
-		}
-		// normalize dr, the vector between the molecules
-		dr.normalize();
-
-		// v1 (unit vector) is the orientation of molecule 1: dipole1 direction
-		IVector v1 = atom1.getOrientation().getDirection();
-		// v2 (unit vector) is the orientation of molecule 2: dipole1 direction
-		IVector v2 = atom2.getOrientation().getDirection();
-
-		// cos(dipole 1 and dipole 2)=cos(v1 and v2)
-		double cos_D1_D2 = v1.dot(v2);
-		//cos(dipole 1 and r12)
-		double cos_D1_r = v1.dot(dr);
-		//cos(r12 and dipole 2)
-		double cos_r_D2=dr.dot(v2);
-		double r12Magnitude = Math.sqrt(r2);
-		double ener = dipole * dipole *  (cos_D1_D2 - 3.0  * cos_D1_r* cos_r_D2);
-		ener = ener/r12Magnitude /r2 ;
-		return ener;
-	}
-
-	public double getSigma() {return sigma;}
-
-	public final void setSigma(double s) {
-		sigma = s;
-		sigma2 = s * s;
-	}
-
-	public void setDipole(double moment){
-		dipole=moment;
-	}
-	
+    public double getRange() {
+        return Double.POSITIVE_INFINITY;
+    }
+    
     public IVector[] gradient(IMoleculeList pair, Tensor pressureTensor) {
         return gradient(pair);
     }
@@ -158,10 +90,54 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
         }
         return gradient[1].dot(dr);
     }
+    
 
-	public IVector[][] gradientAndTorque(IMoleculeList atoms) {
-    	IMolecule molecule1 = atoms.getMolecule(0);
-		IMolecule molecule2 = atoms.getMolecule(1);
+    public double energy(IMoleculeList pair){
+    	IMolecule molecule1 = pair.getMolecule(0);
+		IMolecule molecule2 = pair.getMolecule(1);
+		IAtomList atomList1 = molecule1.getChildList();
+		IAtomList atomList2 = molecule2.getChildList();
+        IAtomOriented atom1 = (IAtomOriented)atomList1.getAtom(0);
+        IAtomOriented atom2 = (IAtomOriented)atomList2.getAtom(0);
+
+        // LJ contribution
+        dr.Ev1Mv2(atom1.getPosition(), atom2.getPosition());//r1-r2
+        boundary.nearestImage(dr);// added it on 13 jan 2015
+
+        double r2 = dr.squared();
+		if (r2 > rCut*rCut) {
+			return 0;
+		}
+        
+        
+        double s2 = sigma2/(r2);
+        double s6 = s2*s2*s2;
+        double enerLJ = epsilon4*s6*(s6 - 1.0);
+       // System.out.println("distance between two molecules:"+Math.sqrt(r2));
+        if (enerLJ == Double.POSITIVE_INFINITY){// if the molecules are superimposed 
+        	return Double.POSITIVE_INFINITY;
+        }
+        dr.normalize();
+        // v1 (unit vector) is the orientation of molecule 1: dipole1 direction
+        IVector v1 = atom1.getOrientation().getDirection();
+        // v2 (unit vector) is the orientation of molecule 2: dipole2 direction
+        IVector v2 = atom2.getOrientation().getDirection();
+        // cos(dipole 1 and dipole 2)=cos(v1 and v2)
+        double cos_D1_D2 = v1.dot(v2);
+        //cos(dipole 1 and r12)
+        double cos_D1_r = v1.dot(dr);
+        //cos(r12 and dipole 2)
+        double cos_r_D2=dr.dot(v2);
+        double r12Magnitude = Math.sqrt(r2);
+        double enerDipole = dipole * dipole *  (cos_D1_D2 - 3.0  * cos_D1_r * cos_r_D2)/ r12Magnitude / r2;
+        return enerLJ + enerDipole;
+    }
+    
+    
+
+	public IVector[][] gradientAndTorque(IMoleculeList molecules) {
+    	IMolecule molecule1 = molecules.getMolecule(0);
+		IMolecule molecule2 = molecules.getMolecule(1);
 		IAtomList atomList1 = molecule1.getChildList();
 		IAtomList atomList2 = molecule2.getChildList();
 
@@ -211,19 +187,12 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 			torque[0].TE(3.0*v2.dot(drunit));
 			torque[0].ME(work);
 			torque[0].TE(fac);
-
+			
 			torque[1].E(v2);
 			torque[1].XE(drunit);
 			torque[1].TE(3.0*v1.dot(drunit));
 			torque[1].PE(work);
 			torque[1].TE(fac);
-			
-			
-//			System.out.println("dr = " + dr);
-//			System.out.println("vi = " + v1);
-//			System.out.println("vj = " + v2);
-//			System.out.println("torque[0] = " + torque[0]);   //TODO debug only
-//			System.out.println("torque[1] = " + torque[1]);   //TODO debug only
 		}
 
 		// pairwise additive, so
@@ -280,6 +249,10 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 		double r = Math.sqrt(r2);
 		runit.Ea1Tv1(1.0/r, dr);
 		double coeff = dipole*dipole/r2/r;
+		
+		
+//		System.out.println("rij_P2 = " + dr);// debug only
+		
 		
 		//ei cross dejdxj - 3.0*(dejdxj.runit)*(ei cross runite)
 		dr.E(ei);
@@ -345,7 +318,8 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 		secondDerivative[1].E(a);
 		secondDerivative[1].TE(coeff);//ii
 		
-//		System.out.println("dudii = \n" + secondDerivative[1]);//TODO debug only
+		
+//		System.out.println("dudij = \n" + secondDerivative[1]);// debug only
 		
 		//dejdxj cross ei -3.0*(ei.runit)*(dejdxj cross runit)    
 		dr.E(dejdxj);
@@ -373,23 +347,54 @@ public class P2HSDipole extends PotentialMolecular implements IPotentialMolecula
 		secondDerivative[2].TE(coeff);//jj
 		
 		
-//		System.out.println("dudjj = \n" + secondDerivative[2]);//TODO debug only
+//		System.out.println("dudjj = \n" + secondDerivative[2]);// debug only
 //		System.exit(2);// TODO bebug only
         return secondDerivative;
         
     }
-	
+    
+    public double getSigma() {return sigma;}
+    public final void setSigma(double s) {
+        sigma = s;
+        sigma2 = s*s;
+    }
 
-	private static final long serialVersionUID = 1L;
-	private double sigma , sigma2;
-	private IBoundary boundary;
-	private final IVectorMutable dr,drunit,work,runit;
-	private double dipole;
-	private double rCut;
+    public double getEpsilon() {return epsilon;}
+    
+    public void setEpsilon(double eps) {
+        epsilon = eps;
+        epsilon4 = 4*epsilon;
+    }
+    public void setDipole(double s) {
+        dipole = s;
+    }
+    /**
+     * Sets the temperature used for Boltzmann-weighting of the orientational
+     * average energy used in u(double) and integral(double)
+     */
+    public void setTemperature(double newTemperature) {
+        temperature = newTemperature;
+    }
+    
+    public double getTemperature() {
+        return temperature;
+    }
+
+    private static final long serialVersionUID = 1L;
+    private double sigma, sigma2;
+    private double epsilon, epsilon4;
+    private double dipole;
+    private IBoundary boundary;
+    private final IVectorMutable dr,drunit,work,runit;
+    protected double temperature;
+    private double rCut;
 	private final IVectorMutable[] gradient;
 	protected final IVectorMutable[] torque;
-	protected final IVectorMutable[][] gradientAndTorque;
 	protected final IVectorMutable [] a;
-	protected final Tensor[] secondDerivative;		
+	protected final IVectorMutable[][] gradientAndTorque;
+	protected final Tensor[] secondDerivative;
 
+ 
+
+    
 }
