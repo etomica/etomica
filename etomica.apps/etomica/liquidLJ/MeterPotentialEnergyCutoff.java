b8ecd7eda4becadc8c40ad48a7bd86f2d614d0d7
Andrew Schultz <ajs42@buffalo.edu>
2016-01-31 19:57:33 +0000

    add classes for liquidLJ package simulation is MD with hybrid MC cutoff is short, but computes long range corrections to free energy

---
+++ etomica.apps/etomica/liquidLJ/MeterPotentialEnergyCutoff.java
@@ -0,0 +1,75 @@
+package etomica.liquidLJ;
+import etomica.api.IBox;
+import etomica.api.IPotentialMaster;
+import etomica.atom.iterator.IteratorDirective;
+import etomica.data.DataTag;
+import etomica.data.IData;
+import etomica.data.IEtomicaDataInfo;
+import etomica.data.IEtomicaDataSource;
+import etomica.data.types.DataDoubleArray;
+import etomica.data.types.DataDoubleArray.DataInfoDoubleArray;
+import etomica.space.ISpace;
+import etomica.units.Energy;
+
+/**
+ * Meter for evaluation of the potential energy in a box.
+ * Includes several related methods for computing the potential energy of a single
+ * atom or molecule with all neighboring atoms
+ *
+ * @author David Kofke
+ */
+ 
+public class MeterPotentialEnergyCutoff implements IEtomicaDataSource {
+    
+    public MeterPotentialEnergyCutoff(IPotentialMaster potentialMaster, ISpace space, double[] cutoffs) {
+        dataInfo = new DataDoubleArray.DataInfoDoubleArray("energy", Energy.DIMENSION, new int[]{cutoffs.length});
+        tag = new DataTag();
+        data = new DataDoubleArray(cutoffs.length);
+        dataInfo.addTag(tag);
+        iteratorDirective.includeLrc = false;
+        potential = potentialMaster;
+        iteratorDirective.setDirection(IteratorDirective.Direction.UP);
+        energy = new PotentialCalculationEnergySumCutoff(space, cutoffs);
+    }
+    
+    public IEtomicaDataInfo getDataInfo() {
+        return dataInfo;
+    }
+
+    public DataTag getTag() {
+        return tag;
+    }
+
+   /**
+    * Computes total potential energy for box.
+    * Currently, does not include long-range correction to truncation of energy
+    */
+    public IData getData() {
+        if (box == null) throw new IllegalStateException("must call setBox before using meter");
+        energy.setBox(box);
+    	energy.zeroSum();
+        potential.calculate(box, iteratorDirective, energy);
+        System.arraycopy(energy.getSums(), 0, data.getData(), 0, dataInfo.getLength());
+        return data;
+    }
+    /**
+     * @return Returns the box.
+     */
+    public IBox getBox() {
+        return box;
+    }
+    /**
+     * @param box The box to set.
+     */
+    public void setBox(IBox box) {
+        this.box = box;
+    }
+
+    protected IBox box;
+    protected final DataInfoDoubleArray dataInfo;
+    protected final DataTag tag;
+    protected final DataDoubleArray data;
+    protected final IteratorDirective iteratorDirective = new IteratorDirective();
+    protected final PotentialCalculationEnergySumCutoff energy;
+    protected final IPotentialMaster potential;
+}
---
+++ etomica.apps/etomica/liquidLJ/PotentialCalculationEnergySumCutoff.java
@@ -0,0 +1,73 @@
+package etomica.liquidLJ;
+
+import etomica.api.IAtomList;
+import etomica.api.IBoundary;
+import etomica.api.IBox;
+import etomica.api.IPotentialAtomic;
+import etomica.api.IVectorMutable;
+import etomica.potential.Potential2SoftSpherical;
+import etomica.potential.PotentialCalculation;
+import etomica.space.ISpace;
+
+/**
+ * Evaluates the energy summed over all iterated atoms. Each call to doCalculate
+ * accumulates the energy of the given potential applied to the atoms produced
+ * by the given iterator. Each such call accumulates a single sum, which can
+ * be accessed via the getSum() method.  Sum is re-zeroed only upon a call to the
+ * zeroSum() method. 
+ *
+ * @author David Kofke
+ */
+public class PotentialCalculationEnergySumCutoff implements PotentialCalculation {
+
+    public PotentialCalculationEnergySumCutoff(ISpace space, double[] cutoffs) {
+        dr = space.makeVector();
+        r2Cuts = new double[cutoffs.length];
+        for (int i=0; i<cutoffs.length; i++) {
+            r2Cuts[i] = cutoffs[i]*cutoffs[i];
+        }
+        sums = new double[cutoffs.length];
+    }
+    
+    public void setBox(IBox box) {
+        this.box = box;
+        boundary = box.getBoundary();
+    }
+
+    /**
+	 * Adds to the energy sum the energy values obtained from application of the given potential to the
+	 * atoms produced by the given iterator.  Iterator is reset by method before beginning calculation.
+	 */
+	public void doCalculation(IAtomList atoms, IPotentialAtomic potential) {
+        dr.Ev1Mv2(atoms.getAtom(1).getPosition(),atoms.getAtom(0).getPosition());
+        boundary.nearestImage(dr);
+        double r2 = dr.squared();
+        double u = ((Potential2SoftSpherical)potential).u(r2);
+        for (int i=sums.length-1; i>=0; i--) {
+            if (r2 > r2Cuts[i]) break;
+            sums[i] += u;
+        }
+	}
+	
+	/**
+	 * Sets the energy sum to zero, typically to begin a new energy-sum calculation.
+	 */
+	public void zeroSum() {
+	    for (int i=0; i<sums.length; i++) {
+	        sums[i] = 0.0;
+	    }
+		boundary = box.getBoundary();
+	}
+
+	/**
+	 * Returns the current value of the energy sum.
+	 */
+	public double[] getSums() {
+        return sums;
+    }
+	
+	protected double[] sums, r2Cuts;
+	protected final IVectorMutable dr;
+	protected IBox box;
+	protected IBoundary boundary;
+}
