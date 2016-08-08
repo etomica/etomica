4b30ee513c06f7f900677809bd7e956557bae499
tboxcli <>
2016-08-08 17:09:13 +0000

    mapped potential energy

---
+++ etomica.apps/etomica/mappedvirial/MappedU.java
@@ -0,0 +1,260 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package etomica.mappedvirial;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import etomica.action.BoxInflate;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.api.IAtomType;
+import etomica.api.IBox;
+import etomica.box.Box;
+import etomica.config.ConfigurationLattice;
+import etomica.data.AccumulatorAverageFixed;
+import etomica.data.DataPump;
+import etomica.data.DataPumpListener;
+import etomica.data.IData;
+import etomica.data.meter.MeterPotentialEnergy;
+import etomica.data.meter.MeterRDFPC;
+import etomica.graphics.DisplayPlot;
+import etomica.graphics.SimulationGraphic;
+import etomica.integrator.IntegratorMC;
+import etomica.integrator.mcmove.MCMoveAtom;
+import etomica.lattice.LatticeCubicFcc;
+import etomica.listener.IntegratorListenerAction;
+import etomica.nbr.cell.PotentialMasterCell;
+import etomica.potential.P2LennardJones;
+import etomica.potential.P2SoftSphericalTruncated;
+import etomica.simulation.Simulation;
+import etomica.space.ISpace;
+import etomica.space.Space;
+import etomica.species.SpeciesSpheresMono;
+import etomica.util.Histogram;
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+
+public class MappedU extends Simulation {
+    
+    public SpeciesSpheresMono species;
+    public IBox box;
+    public IntegratorMC integrator;
+    public MCMoveAtom move;
+    public ActivityIntegrate activityIntegrate;
+    public P2SoftSphericalTruncated p2Truncated;
+    
+    public MappedU(ISpace _space, int numAtoms, double temperature, double density, double rc) {
+        super(_space);
+
+	    //species and potentials
+	    species = new SpeciesSpheresMono(this, space);
+        addSpecies(species);
+        
+        //construct box
+	    box = new Box(space);
+        addBox(box);
+        box.setNMolecules(species, numAtoms);
+        
+        BoxInflate inflater = new BoxInflate(box, space);
+        inflater.setTargetDensity(density);
+        inflater.actionPerformed();
+
+        PotentialMasterCell potentialMaster = new PotentialMasterCell(this, rc, space);
+        potentialMaster.lrcMaster().setEnabled(false);
+        
+        //controller and integrator
+	    integrator = new IntegratorMC(potentialMaster, random, temperature);
+        activityIntegrate = new ActivityIntegrate(integrator);
+        getController().addAction(activityIntegrate);
+        move = new MCMoveAtom(random, potentialMaster, space);
+        integrator.getMoveManager().addMCMove(move);
+
+	    P2LennardJones potential = new P2LennardJones(space);
+        p2Truncated = new P2SoftSphericalTruncated(space, potential, rc);
+	    potentialMaster.addPotential(p2Truncated, new IAtomType[]{species.getLeafType(), species.getLeafType()});
+	    
+        new ConfigurationLattice(new LatticeCubicFcc(space), space).initializeCoordinates(box);
+        integrator.setBox(box);
+        potentialMaster.setCellRange(2);
+        
+        potentialMaster.getNbrCellManager(box).assignCellAll();
+        
+        integrator.getMoveEventManager().addListener(potentialMaster.getNbrCellManager(box).makeMCMoveListener());
+    }
+    
+    public static void main(String[] args) throws IOException {
+        
+        LJMDParams params = new LJMDParams();
+        if (args.length > 0) {
+            ParseArgs.doParseArgs(params, args);
+        }
+        else {
+            params.temperature = 2;
+            params.density = 0.10;
+            params.numSteps = 10000000;
+            params.rc = 5;
+            params.numAtoms = 200;
+            params.functionsFile = "0.10";
+        }
+                          
+        int numAtoms = params.numAtoms;
+        double temperature = params.temperature;
+        double density = params.density;
+        long numSteps = params.numSteps;
+        double rc = params.rc;
+        String functionsFile = params.functionsFile;
+        boolean computeU = params.computeU;
+        boolean computeUMA = params.computeUMA;
+        boolean graphics = params.graphics;
+        int nBlocks = params.nBlocks;
+
+        System.out.println("Virial mapped average");
+        System.out.println(numAtoms+" atoms, "+numSteps+" steps");
+        System.out.println("density: "+density);
+        System.out.println("temperature: "+temperature);
+        System.out.println("cutoff: "+rc);
+        System.out.println(nBlocks+" blocks");
+
+        ISpace space = Space.getInstance(3);
+
+        MappedU sim = new MappedU(space, numAtoms, temperature, density, rc);
+        
+        if (graphics) {
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE, space, sim.getController());
+            simGraphic.makeAndDisplayFrame();
+
+            ArrayList<DataPump> dataStreamPumps = simGraphic.getController().getDataStreamPumps();
+            
+            MeterRDFPC meterRDF = new MeterRDFPC(space, sim.integrator.getPotentialMaster(), sim.box);
+            meterRDF.setBox(sim.box);
+            int nbins = (int)Math.round(rc/0.01);
+            meterRDF.getXDataSource().setNValues(nbins);
+            meterRDF.getXDataSource().setXMax(rc);
+            sim.integrator.getEventManager().addListener(new IntegratorListenerAction(meterRDF, numAtoms));
+
+            DisplayPlot rdfPlot = new DisplayPlot();
+            DataPump rdfPump = new DataPump(meterRDF,rdfPlot.getDataSet().makeDataSink());
+            IntegratorListenerAction rdfPumpListener = new IntegratorListenerAction(rdfPump);
+            sim.integrator.getEventManager().addListener(rdfPumpListener);
+            rdfPumpListener.setInterval(10);
+            dataStreamPumps.add(rdfPump);
+            
+            rdfPlot.setDoLegend(false);
+           // rdfPlot.getPlot().setTitle("Radial Distribution Function");
+            rdfPlot.setLabel("RDF");
+            simGraphic.add(rdfPlot);
+
+            return;
+        }
+        
+        long t1 = System.currentTimeMillis();
+        
+        sim.activityIntegrate.setMaxSteps(numSteps/10);
+        sim.getController().actionPerformed();
+        sim.getController().reset();
+        sim.activityIntegrate.setMaxSteps(numSteps);
+        sim.integrator.getMoveManager().setEquilibrating(false);
+        
+        int nBins = 1000000;
+        long numSamples = numSteps/numAtoms;
+        long samplesPerBlock = numSamples/nBlocks;
+        if (samplesPerBlock == 0) samplesPerBlock = 1;
+ 
+        final MeterMappedU meterMappedU = new MeterMappedU(space, sim.integrator.getPotentialMaster(), sim.box, nBins);
+        meterMappedU.getPotentialCalculation().setTemperature(sim.integrator.getTemperature(), sim.p2Truncated);
+        final AccumulatorAverageFixed accMappedVirial = new AccumulatorAverageFixed(samplesPerBlock);
+        DataPumpListener pumpMappedU = new DataPumpListener(meterMappedU, accMappedVirial, numAtoms);
+        if (computeUMA) sim.integrator.getEventManager().addListener(pumpMappedU);
+        System.out.println("x0: "+meterMappedU.getPotentialCalculation().getX0());
+        
+        double qp_q = meterMappedU.getPotentialCalculation().getQP_Q();
+        System.out.println("qp/q: "+qp_q);
+        System.out.println("rho*db2/dbeta: "+qp_q*numAtoms/2);
+        double qp = meterMappedU.getPotentialCalculation().getqp();
+        System.out.println("qp: "+qp); 
+        
+        final MeterPotentialEnergy meterU = new MeterPotentialEnergy(sim.integrator.getPotentialMaster());
+        meterU.setBox(sim.box);
+        final AccumulatorAverageFixed accU = new AccumulatorAverageFixed(samplesPerBlock);
+        DataPumpListener pumpU = new DataPumpListener(meterU, accU, numAtoms);
+        if (computeU) sim.integrator.getEventManager().addListener(pumpU);
+
+        MeterMeanForce meterF = null;
+        MeterRDFPC meterRDF = null;
+        if (functionsFile != null) {
+            int nbins = (int)Math.round(rc/0.01);
+            meterF = new MeterMeanForce(space, sim.integrator.getPotentialMaster(), sim.p2Truncated, sim.box, nbins);
+            if (computeU && computeUMA) sim.integrator.getEventManager().addListener(new IntegratorListenerAction(meterF, numAtoms));
+    
+            meterRDF = new MeterRDFPC(space, sim.integrator.getPotentialMaster(), sim.box);
+            meterRDF.setBox(sim.box);
+            meterRDF.getXDataSource().setNValues(nbins);
+            meterRDF.getXDataSource().setXMax(rc);
+            sim.integrator.getEventManager().addListener(new IntegratorListenerAction(meterRDF, numAtoms));
+        }
+
+        sim.getController().actionPerformed();
+        
+        IData mappedAvg = accMappedVirial.getData(accMappedVirial.AVERAGE);
+        IData mappedErr = accMappedVirial.getData(accMappedVirial.ERROR);
+        IData mappedCor = accMappedVirial.getData(accMappedVirial.BLOCK_CORRELATION);
+        double avg = mappedAvg.getValue(0);
+        double err = mappedErr.getValue(0);
+        double cor = mappedCor.getValue(0);
+        if (computeUMA) System.out.print(String.format("avg: %13.6e   err: %11.4e   cor: % 4.2f\n", avg, err, cor));
+        System.out.println("Uavg: "+ ((-1*qp_q*numAtoms/2)-(avg/numAtoms)));
+        
+        double UAvg = accU.getData(accU.AVERAGE).getValue(0);
+        double UErr = accU.getData(accU.ERROR).getValue(0);
+        double UCor = accU.getData(accU.BLOCK_CORRELATION).getValue(0);
+        if (computeU) System.out.print(String.format("Potential     avg: %13.6e   err: %11.4e   cor: % 4.2f\n", UAvg, UErr, UCor));
+
+        if (functionsFile != null) {
+            FileWriter fw = new FileWriter(functionsFile+"_gr.dat");
+            IData rdata = meterRDF.getIndependentData(0);
+            IData gdata = meterRDF.getData();
+            for (int i=0; i<rdata.getLength(); i++) {
+                double r = rdata.getValue(i);
+                double g = gdata.getValue(i);
+                double e = Math.exp(-sim.p2Truncated.u(r*r)/temperature);
+                fw.write(String.format("%5.3f %22.15e %22.15e\n", r, g, e));
+            }
+            fw.close();
+            
+            if (computeU && computeUMA) {
+                fw = new FileWriter(functionsFile+"_mf.dat");
+                rdata = meterF.getIndependentData(0);
+                IData fdata = meterF.getData();
+                Histogram f2hist = meterF.getHistogram2();
+                double[] f2 = f2hist.getHistogram();
+                for (int i=0; i<rdata.getLength(); i++) {
+                    double r = rdata.getValue(i);
+                    double mf = fdata.getValue(i);
+                    if (Double.isNaN(mf)) continue;
+                    double sdf = Math.sqrt(f2[i]-mf*mf);
+                    double pf = -sim.p2Truncated.du(r*r)/r;
+                    fw.write(String.format("%5.3f %22.15e %22.15e %22.15e\n", r, mf, sdf, pf));
+                }
+                fw.close();
+            }
+        }
+
+        long t2 = System.currentTimeMillis();
+        System.out.println("time: "+(t2-t1)*0.001);
+    }
+    
+    public static class LJMDParams extends ParameterBase {
+        public int numAtoms = 100;
+        public double temperature = 1.0;
+        public double density = 0.01;
+        public long numSteps = 1000000;
+        public double rc = 4;
+        public String functionsFile = null;
+        public boolean computeU = true;
+        public boolean computeUMA = true;
+        public boolean graphics = false;
+        public int nBlocks = 100;
+    }
+}
---
+++ etomica.apps/etomica/mappedvirial/MappedUpotential.java
@@ -0,0 +1,197 @@
+package etomica.mappedvirial;
+
+import etomica.api.IAtom;
+import etomica.api.IAtomList;
+import etomica.api.IBox;
+import etomica.api.IPotentialAtomic;
+import etomica.api.IVector;
+import etomica.api.IVectorMutable;
+import etomica.atom.AtomLeafAgentManager;
+import etomica.atom.AtomPair;
+import etomica.atom.iterator.IteratorDirective;
+import etomica.box.Box;
+import etomica.integrator.IntegratorVelocityVerlet.MyAgent;
+import etomica.potential.P2LennardJones;
+import etomica.potential.P2SoftSphericalTruncated;
+import etomica.potential.P2SoftSphericalTruncatedForceShifted;
+import etomica.potential.Potential2SoftSpherical;
+import etomica.potential.PotentialCalculation;
+import etomica.simulation.Simulation;
+import etomica.space.ISpace;
+import etomica.space3d.Space3D;
+
+/**
+ * PotentialCalculation that implements mapped-averaged virial (to get the
+ * pressure).
+ * 
+ * @author Andrew Schultz
+ */
+public class MappedUpotential implements PotentialCalculation {
+
+    protected final IBox box;
+    protected final IteratorDirective allAtoms;
+    protected final AtomLeafAgentManager<MyAgent> forceManager;
+    protected final ISpace space;
+    protected double beta;
+    protected final IVectorMutable dr;
+    protected double c1;
+    protected final double[] cumint;
+    protected final AtomPair pair;
+    protected double vol;
+    protected double q;
+    protected double qp;
+    protected double qp_q;
+    protected double vShift;
+    protected final int nbins;
+    protected double sum;
+    protected double x0, vCut;
+
+    public MappedUpotential(ISpace space, IBox box, int nbins, AtomLeafAgentManager<MyAgent> forceManager) {
+        this.space = space;
+        this.box = box;
+        this.nbins = nbins;
+        pair = new AtomPair();
+        this.forceManager = forceManager;
+        allAtoms = new IteratorDirective();
+        dr = space.makeVector();
+        cumint = new double[nbins+1];
+        vol = box.getBoundary().volume();
+    }
+
+    public static void main(String[] args) {
+        Simulation sim = new Simulation(Space3D.getInstance());
+        IBox box = new Box(sim.getSpace());
+        MappedUpotential pc = new MappedUpotential(sim.getSpace(),box, 1000000, null);
+        P2LennardJones potential = new P2LennardJones(sim.getSpace());
+        P2SoftSphericalTruncated p2Truncated = new P2SoftSphericalTruncatedForceShifted(sim.getSpace(), potential, 2.5);
+        pc.setTemperature(1, p2Truncated);
+        for (int i=10; i<30; i++) {
+            double r = i*0.1;
+            if (r>=2.5) r = 2.499999;
+            System.out.println(r+" "+pc.calcXs(r, p2Truncated.u(r*r)));
+        }
+    }
+    
+    public double getX0() {
+        return x0;
+    }
+    
+    public void setVCut(double newVCut) {
+        vCut = newVCut;
+    }
+
+    /**
+     * Sets volume to an arbitrary value (instead of the box volume).  This is
+     * used for 1/(1+q/V) terms.
+     */
+    public void setVolume(double newVol) {
+        vol = newVol;
+    }
+
+    public void setTemperature(double T, Potential2SoftSpherical p2) {
+        beta = 1/T;
+        double rc = p2.getRange();
+        x0 = rc*0.95;
+        if (vCut==0) vCut = x0;
+        c1 = Math.log(rc+1)/nbins;
+        int D = space.D();
+        vShift = -p2.u(vCut*vCut);
+        for (int i=1; i<=nbins; i++) {
+            double r = Math.exp(c1*i)-1;
+            double r2 = r*r;
+            if (r >= p2.getRange()) {
+                if (i<nbins) throw new RuntimeException("oops "+i+" "+nbins+" "+r+" "+p2.getRange());
+                r = Math.exp(c1*i*0.9999)-1;
+                r2 = r*r;
+            }
+            double u = p2.u(r2);
+            double evm1 = 0;
+            double v = u + vShift;
+            if (r>vCut) v = 0;
+            evm1 = Math.exp(-beta*v);
+            qp += (D==2 ? r : r2)*evm1*v*c1*(r+1);
+            q += (D==2 ? r : r2)*(evm1-1)*c1*(r+1);
+            
+        }
+        qp *= -1*(D==2?2:4)*Math.PI;
+        q *= (D==2?2:4)*Math.PI;
+        q += vol;
+        qp_q = qp/q;
+
+        for (int i=1; i<=nbins; i++) {
+            double r = Math.exp(c1*i)-1;
+            double r2 = r*r;
+            if (r >= p2.getRange()) {
+                if (i<nbins) throw new RuntimeException("oops "+i+" "+nbins+" "+r+" "+p2.getRange());
+                r = Math.exp(c1*i*0.9999)-1;
+                r2 = r*r;
+            }
+            double u = p2.u(r2);
+            double evm1 = 0;
+            double v = u + vShift;
+            if (r>vCut) v = 0;
+            evm1 = Math.exp(-beta*v);
+            cumint[i] = cumint[i-1] + (D==2 ? r : r2)*(evm1)*(v+qp_q)*c1*(r+1);
+        }
+    }
+    
+    protected double calcXs(double r, double u) {
+        double y = cumint(r);
+        double v = u + vShift;
+        if (r > vCut) v = 0;
+        double evm1 = Math.exp(-beta*v);
+        return y/(r*r*evm1);
+    }
+
+    protected double cumint( double r) {
+        // r = (exp(c1*i) - 1)
+        double i = Math.log(r+1)/c1;
+        int ii = (int)i;
+        double y = cumint[ii] + (cumint[ii+1]-cumint[ii])*(i-ii);
+        return y;
+    }
+
+    public double getQP_Q() {
+        return qp_q;
+    }
+    
+    public double getqp() {
+        return qp;
+    }
+    
+    public void reset() {
+        sum = 0;
+    }
+    
+    public void doCalculation(IAtomList atoms, IPotentialAtomic potential) {
+        if (!(potential instanceof Potential2SoftSpherical)) return;
+        Potential2SoftSpherical p2 = (Potential2SoftSpherical)potential;
+        IAtom a = atoms.getAtom(0);
+        IAtom b = atoms.getAtom(1);
+//        System.out.println("volume "+vol);
+        dr.Ev1Mv2(b.getPosition(),a.getPosition());
+        box.getBoundary().nearestImage(dr);
+        double r2 = dr.squared();
+        double r = Math.sqrt(r2);
+        if (r > p2.getRange()) return;
+        double fij = p2.du(r2);
+        double up = fij/r;
+        double vp = up;
+        if (r>vCut) vp = 0;
+        if (r<x0) {
+            IVector fi = forceManager.getAgent(a).force;
+            IVector fj = forceManager.getAgent(b).force;
+            double u = p2.u(r2);
+            sum += u*(beta-1);
+            double fifj = (fi.dot(dr) - fj.dot(dr))/r;
+            double xs = calcXs(r, u);
+            double wp = 0.5*fifj;
+            sum += xs*beta*(vp-wp);
+        }
+    } 
+
+    public double getPressure() {
+        int D = space.D(); 
+        return sum;
+    }
+}
