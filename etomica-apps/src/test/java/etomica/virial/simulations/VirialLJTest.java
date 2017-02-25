36613b5c686a6a4f0a37154bc7f9088df5acf52d
Alex Kofke <akofke@gmail.com>
2017-02-25 00:26:38 -0500

    Move apps test classes to separate source set
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- etomica-apps/src/test/java/etomica/virial/simulations/VirialLJTest.java
+++ etomica-apps/src/test/java/etomica/virial/simulations/VirialLJTest.java
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package etomica.apps.junit.virial;
+package etomica.virial.simulations;
 
 import junit.framework.TestCase;
 import etomica.chem.elements.ElementSimple;
