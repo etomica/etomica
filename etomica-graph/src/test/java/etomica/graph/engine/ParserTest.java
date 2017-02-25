f98c843be8f7a150226b4212c942b37d95860f81
Alex Kofke <akofke@gmail.com>
2017-02-25 01:32:42 -0500

    move another etomica.graph test
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- etomica-graph/src/test/java/etomica/graph/engine/ParserTest.java
+++ etomica-graph/src/test/java/etomica/graph/engine/ParserTest.java
@@ -2,9 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package etomica.graph.engine.test;
+package etomica.graph.engine;
 
-import etomica.graph.engine.Parser;
 import etomica.graph.engine.Parser.ParserException;
 import etomica.graph.engine.impl.ParserImpl;
 import junit.framework.TestCase;
