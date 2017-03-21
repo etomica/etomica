1aaca50fbfe8cf4a5ee644146848f47a450f5140
Alex Kofke <akofke@gmail.com>
2017-03-20 23:13:24 -0400

    add to proof-of-concept parmed parser
    add test gromacs datafiles to resources directory, include
    jackson as dependency and try parsing the python program output
    
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- etomica-core/src/main/java/etomica/parser/ParmedParser.java
+++ etomica-core/src/main/java/etomica/parser/ParmedParser.java
@@ -1,23 +1,48 @@
 package etomica.parser;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import etomica.box.Box;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
  * Created by alex on 3/16/17.
  */
 public class ParmedParser {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static void main(String[] args) {
         try {
-            ProcessBuilder pb = new ProcessBuilder("venv/bin/parmed_json", "test.top", "test.gro");
-            System.out.println(pb.environment());
-            pb.inheritIO();
+            File topFile = getResourceFile("test.top");
+            File groFile = getResourceFile("test.gro");
+
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "venv/bin/parmed_json",
+                    topFile.getCanonicalPath(),
+                    groFile.getCanonicalPath()
+            );
+
 
             Process p = pb.start();
+
+            JsonNode tree = mapper.readTree(p.getInputStream());
+
             p.waitFor();
+
+            System.out.println(tree);
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static File getResourceFile(String filename) {
+        ClassLoader classLoader = ParmedParser.class.getClassLoader();
+        return new File(classLoader.getResource(filename).getFile());
     }
 }
