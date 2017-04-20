7eed1c49ee87043d1b186c4340d78cce0bcd88fb
Andrew Schultz <ajs42@buffalo.edu>
2017-04-20 10:10:42 -0400

    let's call it fluorine

--- etomica-core/src/main/java/etomica/chem/elements/Fluorine.java
+++ etomica-core/src/main/java/etomica/chem/elements/Fluorine.java
@@ -5,22 +5,22 @@
 package etomica.chem.elements;
 
 /**
- * Class for the Fluoride element.
+ * Class for the Fluorine element.
  * reference: Atomic weights of the elements 2009 (IUPAC Technical Report)
  * http://iupac.org/publications/pac/83/2/0359/
  * 
  * @author Shu
  */
-public class Fluoride extends ElementChemical {
+public class Fluorine extends ElementChemical {
 
-    protected Fluoride(String symbol) {
+    protected Fluorine(String symbol) {
         this(symbol, 18.9984);
     }
     
-    public Fluoride(String symbol, double mass) {
+    public Fluorine(String symbol, double mass) {
         super(symbol, mass, 9);
     }
 
     private static final long serialVersionUID = 1L;
-    public static final Fluoride INSTANCE = new Fluoride("F");
+    public static final Fluorine INSTANCE = new Fluorine("F");
 }
