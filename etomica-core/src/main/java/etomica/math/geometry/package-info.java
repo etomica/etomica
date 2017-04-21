892bf7d1c6eec322122e6ad72cb28bfc5916b7d0
Alex Kofke <akofke@gmail.com>
2017-04-20 23:52:08 -0400

    convert to package-info.java

---
+++ etomica-core/src/main/java/etomica/math/geometry/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Provides classes that define points, planes, and geometric shapes. The shapes are
+ * used in particular by the Boundary class.
+ * <!--
+ * <h2>Package Specification</h2>
+ * ##### FILL IN ANY SPECS NEEDED BY JAVA COMPATIBILITY KIT #####
+ * <ul>
+ * <li><a href="">##### REFER TO ANY FRAMEMAKER SPECIFICATION HERE #####</a>
+ * </ul>
+ * <h2>Related Documentation</h2>
+ * For overviews, tutorials, examples, guides, and tool documentation, please see:
+ * <ul>
+ * <li><a href="">##### REFER TO NON-SPEC DOCUMENTATION HERE #####</a>
+ * </ul>
+ * -->
+ * <!-- Put @see and @since tags down here. -->
+ */
+package etomica.math.geometry;
