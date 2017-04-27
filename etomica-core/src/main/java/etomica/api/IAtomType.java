984643b6485ca19a6365fb3eb0c78e718548d976
Andrew Schultz <ajs42@buffalo.edu>
2017-04-27 16:47:41 -0400

    IAtomType comment updates

--- etomica-core/src/main/java/etomica/api/IAtomType.java
+++ etomica-core/src/main/java/etomica/api/IAtomType.java
@@ -4,14 +4,21 @@
 
 package etomica.api;
 
-
+/**
+ * IAtomType identifies a set of atoms and defines properties of those atoms.
+ * Properties include indicies used for tracking, mass and element.
+ */
 public interface IAtomType {
 
+    /**
+     * Informs the atom type what species contains the atom types.  This should
+     * only be called by the species.
+     */
     public void setSpecies(ISpecies newParent);
 
     /**
-     * Informs the ISpecies what its index should be.  This should only be
-     * called by the SpeciesManager.
+     * Informs the IAtomType what its index should be.  This should only be
+     * called by the species.
      */
     public void setIndex(int newIndex);
 
@@ -21,11 +28,22 @@ public interface IAtomType {
      * atom types in the simulation.
      */
     public int getIndex();
-    
+
+    /**
+     * Informs the atom type what its child index is.  This should only be called
+     * by the species.
+     */
     public void setChildIndex(int newChildIndex);
 
+    /**
+     * Returns the child index.  This is the index of the atom type within the
+     * species.
+     */
     public int getChildIndex();
 
+    /**
+     * Returns the species that contains the atom type
+     */
     public ISpecies getSpecies();
 
     /**
@@ -38,6 +56,9 @@ public interface IAtomType {
      */
     public double rm();
 
+    /**
+     * Return the element for this atom type
+     */
     public IElement getElement();
 
 }
