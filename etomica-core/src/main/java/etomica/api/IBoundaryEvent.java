e1b7cba12f569541cd50de58e09d5c3a1d2035fc
Andrew Schultz <ajs42@buffalo.edu>
2017-04-27 16:53:09 -0400

    IBoundaryEvent comment updates

--- etomica-core/src/main/java/etomica/api/IBoundaryEvent.java
+++ etomica-core/src/main/java/etomica/api/IBoundaryEvent.java
@@ -4,7 +4,15 @@
 
 package etomica.api;
 
+/**
+ * Event that informs a listener about some change to a boundary.  The listener
+ * can determine the specific type of even from the class of the event object
+ * and can use event methods to determine what happened to the boundary.
+ */
 public interface IBoundaryEvent {
-
+    
+    /**
+     * Returns the boundary which has changed.
+     */
     public IBoundary getBoundary();
 }
