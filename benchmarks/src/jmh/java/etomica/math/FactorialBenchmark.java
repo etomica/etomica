96a6d4522622ff0f0fe9090e055e4770d9ae579f
Alex Kofke <akofke@gmail.com>
2017-04-29 23:40:18 -0400

    add attempt of benchmark for a simulation

--- benchmarks/src/jmh/java/etomica/math/FactorialBenchmark.java
+++ benchmarks/src/jmh/java/etomica/math/FactorialBenchmark.java
@@ -1,27 +1,29 @@
 package etomica.math;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * Created by alex on 4/29/17.
  */
 @State(Scope.Benchmark)
+@Measurement(time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class FactorialBenchmark {
 
     @Param({"1", "5", "20"})
     public int n;
 
     @Benchmark
-    public long benchEtomicaFactorial() {
+    public long etomicaFactorial() {
         return etomica.math.SpecialFunctions.factorial(n);
     }
 
     @Benchmark
-    public long benchApacheFactorial() {
+    public long apacheFactorial() {
         return org.apache.commons.math3.util.CombinatoricsUtils.factorial(n);
     }
 }
