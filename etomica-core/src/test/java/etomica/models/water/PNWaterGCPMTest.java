c0091bde62901a2bc93076a5b7d9261cd3837d09
akshara021 <aksharagoyal021@gmail.com>
2017-05-10 15:01:15 -0400

    Make unit test for P2WaterGCPM

--- etomica-core/src/test/java/etomica/models/water/PNWaterGCPMTest.java
+++ etomica-core/src/test/java/etomica/models/water/PNWaterGCPMTest.java
@@ -1,8 +1,41 @@
+package etomica.models.water;
+
+import etomica.action.MoleculeActionTranslateTo;
+import etomica.api.IMolecule;
+import etomica.api.IMoleculeList;
+import etomica.atom.MoleculePair;
+import etomica.box.Box;
+import etomica.space3d.Space3D;
+import etomica.space3d.Vector3D;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Lenovo on 05-10-2017.
+ * Created by akshara on 05-10-2017.
  */
 public class PNWaterGCPMTest {
+    private IMoleculeList molecules;
+    private static final double EPSILON = 1e-10;
+    @Before
+    public void setUp() throws Exception {
+        IMolecule mol1 = new SpeciesWater4PCOM(Space3D.getInstance()).makeMolecule();
+        IMolecule mol2 = new SpeciesWater4PCOM(Space3D.getInstance()).makeMolecule();
+
+        MoleculeActionTranslateTo act = new MoleculeActionTranslateTo(Space3D.getInstance());
+        act.setDestination(new Vector3D(4, 4, 4));
+        act.actionPerformed(mol2);
+
+        molecules = new MoleculePair(mol1, mol2);
+    }
+
+
+    @Test
+    public void testEnergy() throws Exception {
+        PNWaterGCPM potential = new PNWaterGCPM(Space3D.getInstance());
+        potential.setBox(new Box(Space3D.getInstance()));
+        assertEquals(-14.868664238785254, potential.energy(molecules), EPSILON);
+    }
 
 }
