b259179c0a6e1848f01af95343497b8be921edf2
akshara021 <aksharagoyal021@gmail.com>
2017-05-10 12:46:28 -0400

    Make unit test for P2WaterSPCE

---
+++ etomica-core/src/test/java/etomica/models/water/P2WaterSPCETest.java
@@ -0,0 +1,41 @@
+package etomica.models.water;
+
+import etomica.action.MoleculeActionTranslateTo;
+import etomica.api.IMolecule;
+import etomica.api.IMoleculeList;
+import etomica.atom.MoleculePair;
+import etomica.box.Box;
+import etomica.space3d.Space3D;
+import etomica.space3d.Vector3D;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by akshara on 05-10-2017.
+ */
+public class P2WaterSPCETest {
+    private IMoleculeList molecules;
+    private static final double EPSILON = 1e-10;
+    @Before
+    public void setUp() throws Exception {
+        IMolecule mol1 = new SpeciesWater4P(Space3D.getInstance()).makeMolecule();
+        IMolecule mol2 = new SpeciesWater4P(Space3D.getInstance()).makeMolecule();
+
+        MoleculeActionTranslateTo act = new MoleculeActionTranslateTo(Space3D.getInstance());
+        act.setDestination(new Vector3D(4, 4, 4));
+        act.actionPerformed(mol2);
+
+        molecules = new MoleculePair(mol1, mol2);
+    }
+
+
+    @Test
+    public void energy() throws Exception {
+        P2WaterSPCE potential = new P2WaterSPCE(Space3D.getInstance());
+        potential.setBox(new Box(Space3D.getInstance()));
+        assertEquals(-7.167975911365829, potential.energy(molecules), EPSILON);
+    }
+
+}
