9e5b61cbf3348671501f9343eb01604976d3bc69
Andrew J. Schultz <ajschult@verizon.net>
2017-05-29 08:37:24 -0400

    Fix basis atom position

--- etomica-core/src/main/java/etomica/lattice/crystal/BasisHcp4.java
+++ etomica-core/src/main/java/etomica/lattice/crystal/BasisHcp4.java
@@ -20,6 +20,6 @@ public class BasisHcp4 extends Basis {
         new Vector3D(0.0, 0.0, 0.0),
         new Vector3D(0.5, 0.5,0),
         new Vector3D(0.5,1.0/3.0,0.5),
-        new Vector3D(0,2.0/3.0,0.5)
+        new Vector3D(0,5.0/6.0,0.5)
     };
 }
