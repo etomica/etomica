7222afc197a68b38c6514b5501f6c100a68f169b
Alex Kofke <akofke@gmail.com>
2017-06-10 18:55:16 -0400

    Add Dimensioned annotation and reflections dependency

---
+++ etomica-core/src/main/java/etomica/units/Dimensioned.java
@@ -0,0 +1,12 @@
+package etomica.units;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Dimensioned {
+    Class<? extends Dimension> dimension();
+}
