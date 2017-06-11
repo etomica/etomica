4e10ec5dd72b09b687a47e68851d24a6f6d0de56
Andrew J. Schultz <ajschult@verizon.net>
2017-06-11 10:31:28 -0400

    handle deformable boundary add atom filter to exclude atoms with small displacements

--- etomica-apps/src/main/java/etomica/freeenergy/npath/ConfigFromFileLAMMPS.java
+++ etomica-apps/src/main/java/etomica/freeenergy/npath/ConfigFromFileLAMMPS.java
@@ -1,20 +1,20 @@
 package etomica.freeenergy.npath;
 
 import etomica.action.IAction;
+import etomica.atom.AtomFilter;
 import etomica.atom.IAtom;
 import etomica.atom.IAtomList;
 import etomica.box.Box;
 import etomica.graphics.*;
 import etomica.modifier.Modifier;
+import etomica.molecule.IMolecule;
 import etomica.simulation.Simulation;
-import etomica.space.Boundary;
-import etomica.space.BoundaryDeformablePeriodic;
-import etomica.space.Space;
-import etomica.space.Vector;
+import etomica.space.*;
 import etomica.space3d.Space3D;
 import etomica.species.Species;
 import etomica.species.SpeciesSpheresMono;
 import etomica.units.Dimension;
+import etomica.units.Null;
 import etomica.units.Quantity;
 import etomica.util.ParameterBase;
 import etomica.util.ParseArgs;
@@ -59,6 +59,7 @@ public class ConfigFromFileLAMMPS {
         int config = 1;
         List<Vector[]> allCoords = new ArrayList<>();
         Vector[] iCoords = null;
+        List<Vector[]> allEdges = new ArrayList<>();
         while ((line = reader.readLine()) != null) {
             if (line.matches("ITEM:.*")) {
                 read = "";
@@ -70,8 +71,11 @@ public class ConfigFromFileLAMMPS {
                 }
                 else if (line.matches("ITEM: ATOMS.*")) {
                     read = "atoms";
-                    Boundary boundary = new BoundaryDeformablePeriodic(space, edges);
-                    box.setBoundary(boundary);
+                    allEdges.add(edges);
+                    if (config == 1) {
+                        Boundary boundary = new BoundaryDeformablePeriodic(space, edges);
+                        box.setBoundary(boundary);
+                    }
                     iCoords = new Vector[numAtoms];
                 }
                 item = 0;
@@ -107,7 +111,6 @@ public class ConfigFromFileLAMMPS {
                         edges[0].E(new double[]{xhi - xlo, 0, 0});
                         edges[1].E(new double[]{xy, yhi - ylo, 0});
                         edges[2].E(new double[]{xz, yz, zhi - zlo});
-                        System.out.println("edges: " + edges[0] + " " + edges[1] + " " + edges[2]);
                     }
                 }
                 item++;
@@ -123,6 +126,7 @@ public class ConfigFromFileLAMMPS {
                 if (item == numAtoms) {
                     allCoords.add(iCoords);
                     config++;
+                    edges = space.makeVectorArray(3);
                 }
             }
         }
@@ -130,9 +134,46 @@ public class ConfigFromFileLAMMPS {
         final DisplayBox display = new DisplayBox(sim, box, space, sim.getController());
         graphic.add(display);
         if (colorDeviation) {
-            graphic.getDisplayBox(box).setColorScheme(new ColorSchemeDeviation(allCoords.get(0), box, space));
+            ColorSchemeDeviation colorScheme = new ColorSchemeDeviation(allCoords.get(0), box, space);
+            graphic.getDisplayBox(box).setColorScheme(colorScheme);
+            final AtomFilterDeviation atomFilter = new AtomFilterDeviation(colorScheme);
+            display.setAtomFilter(atomFilter);
+
+            DeviceSlider filterSlider = new DeviceSlider(sim.getController(), new Modifier() {
+                @Override
+                public double getValue() {
+                    return atomFilter.getThreshold();
+                }
+
+                @Override
+                public void setValue(double newValue) {
+                    atomFilter.setThreshold(newValue);
+                }
+
+                @Override
+                public Dimension getDimension() {
+                    return Null.DIMENSION;
+                }
+
+                @Override
+                public String getLabel() {
+                    return "Displacement Threshold";
+                }
+            });
+            filterSlider.setMinimum(0);
+            filterSlider.setMaximum(1);
+            filterSlider.setPrecision(2);
+            filterSlider.setNMajor(5);
+            filterSlider.setPostAction(new IAction() {
+
+                @Override
+                public void actionPerformed() {
+                    graphic.getDisplayBox(box).repaint();
+                }
+            });
+            graphic.add(filterSlider);
         }
-        ModifierConfiguration modifierConfig = new ModifierConfiguration(box, allCoords);
+        ModifierConfiguration modifierConfig = new ModifierConfiguration(box, allCoords, allEdges);
         DeviceSlider configSlider = new DeviceSlider(sim.getController(), modifierConfig);
         configSlider.setMaximum(config - 2);
         configSlider.setNMajor(config - 1);
@@ -147,8 +188,9 @@ public class ConfigFromFileLAMMPS {
         });
         graphic.add(configSlider);
 
-        graphic.makeAndDisplayFrame();
         modifierConfig.setValue(0);
+
+        graphic.makeAndDisplayFrame();
     }
 
     public static class ConfigFromFileLAMMPSParam extends ParameterBase {
@@ -157,54 +199,86 @@ public class ConfigFromFileLAMMPS {
     }
 
     public static class ColorSchemeDeviation extends ColorScheme implements ColorSchemeCollective {
-        protected final Vector[] latticeCoords;
+        protected final Vector[] scaledCoords0, rescaledCoords0;
         protected final Box box;
         protected final Vector dr;
         protected final Color[] colors;
+        protected final Vector[] edges0, edges;
+        protected final Tensor t0, t;
+        protected final Vector r0;
         protected double rMax;
 
         public ColorSchemeDeviation(Vector[] latticeCoords, Box box, Space space) {
-            this.latticeCoords = latticeCoords;
             this.box = box;
             dr = space.makeVector();
             colors = new Color[256];
             for (int i = 0; i < 256; i++) {
                 colors[i] = new Color(i, 0, 255 - i);
             }
+            edges0 = space.makeVectorArray(3);
+            for (int i = 0; i < 3; i++) {
+                edges0[i] = box.getBoundary().getEdgeVector(i);
+            }
+            t0 = space.makeTensor();
+            t0.E(edges0);
+            t0.invert();
+            edges = space.makeVectorArray(3);
+            t = space.makeTensor();
+            scaledCoords0 = space.makeVectorArray(latticeCoords.length);
+            for (int i = 0; i < scaledCoords0.length; i++) {
+                scaledCoords0[i].E(latticeCoords[i]);
+                t0.transform(scaledCoords0[i]);
+            }
+            rescaledCoords0 = space.makeVectorArray(latticeCoords.length);
+            r0 = space.makeVector();
         }
 
         @Override
         public void colorAllAtoms() {
             double maxR2 = 0;
             IAtomList atoms = box.getLeafList();
-            Boundary boundary = box.getBoundary();
+            BoundaryDeformablePeriodic boundary = (BoundaryDeformablePeriodic) box.getBoundary();
+            for (int i = 0; i < 3; i++) {
+                edges[i] = boundary.getEdgeVector(i);
+            }
+            t.E(edges);
+
             for (int i = 0; i < atoms.getAtomCount(); i++) {
-                dr.Ev1Mv2(atoms.getAtom(i).getPosition(), latticeCoords[i]);
+                r0.E(scaledCoords0[i]);
+                t.transform(r0);
+                rescaledCoords0[i].E(r0);
+                dr.Ev1Mv2(atoms.getAtom(i).getPosition(), r0);
                 boundary.nearestImage(dr);
                 double r2 = dr.squared();
                 if (r2 > maxR2) maxR2 = r2;
             }
-            rMax = maxR2 == 0 ? 1 : Math.sqrt(maxR2);
+            rMax = maxR2 < 1e-16 ? 1 : Math.sqrt(maxR2);
+        }
+
+        public double getRelativeDisplacement(IAtom a) {
+            dr.Ev1Mv2(a.getPosition(), rescaledCoords0[a.getLeafIndex()]);
+            box.getBoundary().nearestImage(dr);
+            return Math.sqrt(dr.squared()) / rMax;
         }
 
         @Override
         public Color getAtomColor(IAtom a) {
-            dr.Ev1Mv2(a.getPosition(), latticeCoords[a.getLeafIndex()]);
-            box.getBoundary().nearestImage(dr);
-            int i = (int) (255.9999 * (Math.sqrt(dr.squared()) / rMax));
+            int i = (int) (255.9999 * getRelativeDisplacement(a));
             return colors[i];
         }
     }
 
     public static class ModifierConfiguration implements Modifier {
 
+        protected final List<Vector[]> allCoords;
+        protected final List<Vector[]> allEdges;
         protected int configIndex = -1;
         protected Box box;
-        protected List<Vector[]> allCoords;
 
-        public ModifierConfiguration(Box box, List<Vector[]> allCoords) {
+        public ModifierConfiguration(Box box, List<Vector[]> allCoords, List<Vector[]> allEdges) {
             this.box = box;
             this.allCoords = allCoords;
+            this.allEdges = allEdges;
         }
 
         @Override
@@ -217,6 +291,9 @@ public class ConfigFromFileLAMMPS {
             int nv = (int) Math.round(newValue);
             if (nv == configIndex) return;
             configIndex = nv;
+            for (int i = 0; i < 3; i++) {
+                ((BoundaryDeformablePeriodic) box.getBoundary()).setEdgeVector(i, allEdges.get(configIndex)[i]);
+            }
             IAtomList atoms = box.getLeafList();
             Vector[] myCoords = allCoords.get(configIndex);
             for (int i = 0; i < atoms.getAtomCount(); i++) {
@@ -232,6 +309,35 @@ public class ConfigFromFileLAMMPS {
         @Override
         public String getLabel() {
             return "Configuration";
+        }
+    }
+
+    public static class AtomFilterDeviation implements AtomFilter {
+
+        protected final ColorSchemeDeviation colorScheme;
+        protected double threshold = 0;
+
+        public AtomFilterDeviation(ColorSchemeDeviation colorScheme) {
+            this.colorScheme = colorScheme;
+        }
+
+        public double getThreshold() {
+            return threshold;
+        }
+
+        public void setThreshold(double threshold) {
+            this.threshold = threshold;
+        }
+
+        @Override
+        public boolean accept(IAtom a) {
+            double x = colorScheme.getRelativeDisplacement(a);
+            return x >= threshold;
+        }
+
+        @Override
+        public boolean accept(IMolecule mole) {
+            return false;
         }
     }
 }
