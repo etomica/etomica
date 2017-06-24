13517a894e6b135d80678c1688446bdbfe36a9b6
Alex Kofke <akofke@gmail.com>
2017-06-24 15:09:38 -0400

    Add shell script to run benchmarks

---
+++ benchmark.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+[[ "${PWD}" == "$(git rev-parse --show-toplevel)" ]] || {
+    echo "You must run this script in the root of the repository";
+    exit 1
+}
+
+TIMESTAMP=$(date +%FT%T)
+COMMIT=$(git rev-parse --short HEAD)
+
+OUTDIR="${PWD}/benchmarks/build/tmp/results"
+
+while getopts "o:" opt; do
+    case "${opt}" in
+        o)
+            OUTDIR=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+./gradlew jmhJar
+
+java -Djmh.ignoreLock=true -jar ./benchmarks/build/libs/benchmarks-jmh.jar BenchSim -rf JSON -rff "${OUTDIR}/${TIMESTAMP}_${COMMIT}.json"
