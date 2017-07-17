9658552228cdaf9f2c8bc0180562e7625ccdf31f
Navneeth <navneeth@buffalo.edu>
2017-07-17 15:51:43 -0400

    NIST data added to Ewald Summation Test.

--- etomica-core/src/test/java/etomica/potential/EwaldSummationTest.java
+++ etomica-core/src/test/java/etomica/potential/EwaldSummationTest.java
@@ -5,8 +5,6 @@ import etomica.atom.IAtom;
 import etomica.box.Box;
 import etomica.config.Configuration;
 import etomica.config.ConfigurationResourceFile;
-import etomica.graphics.DisplayBox;
-import etomica.graphics.SimulationGraphic;
 import etomica.models.water.P2WaterSPCE;
 import etomica.models.water.SpeciesWater3P;
 import etomica.simulation.Simulation;
@@ -19,9 +17,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
+ * This test compares the computed values against the values provided by NIST at
+ * https://www.nist.gov/mml/csd/chemical-informatics-research-group/spce-water-reference-calculations-10%C3%A5-cutoff.
+ * NIST provides data for 4 configurations. This test checks only 1 of them. The file being compared can be changed by
+ * changing the field "filenum".
+ *
  * Created by Navneeth on 7/12/2017.
  */
 public class EwaldSummationTest {
+
+    private static int filenum;
+    private static int[] NIST_nmol = {100,200,300,750};
+    private static double[] NIST_boxl = {20,20,20,30};
+    private static double[] NIST_real = {-5.58889E+05,-1.19295E+06,-1.96297E+06,-3.57226E+06};
+    private static double[] NIST_fourier = {6.27009E+03, 6.03495E+03, 5.24461E+03, 7.58785E+03};
+    private static double[] NIST_self = {-2.84469E+06,-5.68938E+06,-8.53407E+06,-1.42235E+07};
+    private static double[] NIST_corr = {2.80999E+06, 5.61998E+06, 8.42998E+06, 1.41483E+07};
 
     EwaldSummation es;
     Box box;
@@ -49,8 +60,9 @@ public class EwaldSummationTest {
     }
     @Before
     public void setup(){
-        int numofmolecules = 750;
-        double boxlength = 30;
+        filenum = 4; // pick 1, 2, 3 or 4
+        int numofmolecules = NIST_nmol[filenum-1];
+        double boxlength = NIST_boxl[filenum -1];
         double kcut = Math.sqrt(26.999)*2*Math.PI/boxlength;
         double rCutRealES = 10;
         Space space = Space.getInstance(3);
@@ -69,7 +81,7 @@ public class EwaldSummationTest {
         es.setAlpha(5.6/boxlength);
 
         Configuration config = new ConfigurationResourceFile(
-                String.format("etomica/potential/spce4.pos"),
+                String.format("etomica/potential/spce"+String.valueOf(filenum)+".pos"),
                 EwaldSummationTest.class
         );
 
@@ -78,19 +90,21 @@ public class EwaldSummationTest {
 
 
     }
-    public static void main(String[] str) {
+    //Main method visualizes the box
+
+    /*public static void main(String[] str) {
         EwaldSummationTest foo = new EwaldSummationTest();
         foo.setup();
         SimulationGraphic simg = new SimulationGraphic(foo.sim, foo.sim.getSpace(),null);
         simg.add(new DisplayBox(foo.sim, foo.box, foo.sim.getSpace(),null));
         simg.makeAndDisplayFrame();
 
-    }
+    }*/
 
     @Test
     public void testUReal() throws Exception {
         double testval = es.uReal();
-        double shouldbe = Kelvin.UNIT.toSim(-3.57226E+06);
+        double shouldbe = Kelvin.UNIT.toSim(NIST_real[filenum-1]);
 
         assertEquals(shouldbe,testval,10);
 
@@ -98,7 +112,7 @@ public class EwaldSummationTest {
     @Test
     public void testUFourier() throws Exception {
         double testval = es.uFourier();
-        double shouldbe = Kelvin.UNIT.toSim(7.58785E+03);
+        double shouldbe = Kelvin.UNIT.toSim(NIST_fourier[filenum-1]);
 
         assertEquals(shouldbe,testval,0.01);
 
@@ -106,7 +120,7 @@ public class EwaldSummationTest {
     @Test
     public void testUSelf() throws Exception {
         double testval = es.uSelf();
-        double shouldbe = Kelvin.UNIT.toSim(-1.42235E+07);
+        double shouldbe = Kelvin.UNIT.toSim(NIST_self[filenum-1]);
 
         assertEquals(shouldbe,testval,100);
 
@@ -114,7 +128,7 @@ public class EwaldSummationTest {
     @Test
     public void testUBondCorr() throws Exception {
         double testval = es.uBondCorr();
-        double shouldbe = Kelvin.UNIT.toSim(1.41483E+07);
+        double shouldbe = Kelvin.UNIT.toSim(NIST_corr[filenum-1]);
 
         assertEquals(shouldbe,testval,100);
 
