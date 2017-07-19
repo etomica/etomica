a461bbd1f79fc1d8a2ccb740aa19eb4540002e29
weisongl <weisongl@buffalo.edu>
2017-07-19 16:10:51 -0400

    Add comments for P2ReactionFieldDipole. (#75)
    Revise secondDerivative method to avoid making arrays and vectors.

--- etomica-core/src/main/java/etomica/potential/P2ReactionFieldDipole.java
+++ etomica-core/src/main/java/etomica/potential/P2ReactionFieldDipole.java
@@ -14,78 +14,115 @@ import etomica.space.Boundary;
 import etomica.space.Space;
 import etomica.space.Tensor;
 import etomica.space.Vector;
+import etomica.space3d.Vector3D;
+
+/**
+ * Potential to implement the reaction field treatment of electrostatic interactions. In this approach, the long
+ * range electrostatic interactions are modeled via a dielectric continuum with dielectric constant epsilon.
+ * The net effect is that molecules within a specified cutoff distance interact with an effective pair potential
+ * that is otherwise independent of their separation distance.
+ * <p>
+ * In conjunction with this two-body potential, a zero-body potential must also be added to the PotentialMaster.
+ * The zero-body potential is provided by the method makeP0.
+ */
 
 public class P2ReactionFieldDipole extends PotentialMolecular implements PotentialMolecularSoft, IPotentialMolecularSecondDerivative {
+    protected final IMoleculePositionDefinition positionDefinition;
+    protected final Vector iDipole, cavityDipole;
+    protected final Vector dr;
+    protected final Vector[][] gradientAndTorque;
+    protected final Tensor[] secondDerivative;
+    protected final Vector3D[] a;
+    protected DipoleSource dipoleSource;
+    protected Boundary boundary;
+    protected double cutoff2, cutoff;
+    protected double epsilon;
+    protected double fac;
 
+
+    /**
+     * @param space              defines the dimensionality of the simulation
+     * @param positionDefinition defines the location of a molecule
+     */
     public P2ReactionFieldDipole(Space space, IMoleculePositionDefinition positionDefinition) {
         super(2, space);
         this.positionDefinition = positionDefinition;
         iDipole = space.makeVector();
         cavityDipole = space.makeVector();
         dr = space.makeVector();
-        dr1 = space.makeVector();
         gradientAndTorque = new Vector[2][2];
         gradientAndTorque[0][0] = space.makeVector();
         gradientAndTorque[0][1] = space.makeVector();
         gradientAndTorque[1][0] = space.makeVector();
         gradientAndTorque[1][1] = space.makeVector();
-        secondDerivative = new Tensor [3];
+        secondDerivative = new Tensor[3];
         secondDerivative[0] = space.makeTensor();
-		secondDerivative[1] = space.makeTensor();
-		secondDerivative[2] = space.makeTensor();
-		a = new Vector[3];
-		a[0] = space.makeVector();
-		a[1] = space.makeVector();
-		a[2] = space.makeVector();
+        secondDerivative[1] = space.makeTensor();
+        secondDerivative[2] = space.makeTensor();
+        a = new Vector3D[3];
+        a[0] = (Vector3D) space.makeVector();
+        a[1] = (Vector3D) space.makeVector();
+        a[2] = (Vector3D) space.makeVector();
     }
 
     /**
-     * Returns the dipole source used by this object.
+     * The DipoleSource returns the dipole vector of a given molecule.
+     *
+     * @return the dipole source used by this object.
      */
     public DipoleSource getDipoleSource() {
         return dipoleSource;
     }
 
     /**
-     * Sets the dipole source used by this object should use.
+     * @param newDipoleSource the DipoleSource to set
      */
     public void setDipoleSource(DipoleSource newDipoleSource) {
         dipoleSource = newDipoleSource;
     }
-    
+
+    /**
+     * The radius of the sphere within which molecules contribute to the reaction
+     * field of a given molecule. This value should match the truncation radius
+     * for electrostatic interactions.
+     *
+     * @return the cutoff radius of the reactionField.
+     */
     public double getRange() {
         return cutoff;
     }
-    
+
+    /**
+     * @param newRange the new cutoff radius of the reactionField.
+     */
     public void setRange(double newRange) {
         cutoff = newRange;
         cutoff2 = newRange * newRange;
         if (epsilon < Double.POSITIVE_INFINITY) {
-            fac = 2*(epsilon-1)/(2*epsilon+1)/(cutoff2*cutoff);
-        }
-        else {
-            fac = 1 / (cutoff2*cutoff);
+            fac = 2 * (epsilon - 1) / (2 * epsilon + 1) / (cutoff2 * cutoff);
+        } else {
+            fac = 1 / (cutoff2 * cutoff);
         }
     }
-    
+
     /**
-     * Returns the dielectric constant of the fluid surrounding the cavity.
+     * @return the dielectric constant of the medium surrounding the cavity.
      */
     public double getDielectric() {
         return epsilon;
     }
-    
+
     /**
-     * Sets the dielectric constant of the fluid surrounding the cavity.
+     * @param newDielectric the new value of the dielectric constant of
+     *                      the medium surrounding the cavity.
      */
     public void setDielectric(double newDielectric) {
         epsilon = newDielectric;
         if (cutoff > 0) {
             if (epsilon < Double.POSITIVE_INFINITY) {
-                fac = 2*(epsilon-1)/(2*epsilon+1)/(cutoff2*cutoff);
-            }
-            else {
-                fac = 1 / (cutoff2*cutoff);
+                fac = 2 * (epsilon - 1) / (2 * epsilon + 1) / (cutoff2 * cutoff);
+            } else {
+                fac = 1 / (cutoff2 * cutoff);
             }
         }
     }
@@ -95,153 +132,125 @@ public class P2ReactionFieldDipole extends PotentialMolecular implements Potenti
     }
 
     public double energy(IMoleculeList molecules) {
-    	IMolecule molecule0 = molecules.getMolecule(0);
-		IMolecule molecule1 = molecules.getMolecule(1);
-		dr.E(positionDefinition.position(molecule1));
-		dr.ME(positionDefinition.position(molecule0));
-		boundary.nearestImage(dr);//rij
-		double r2 = dr.squared();
-		if (r2 > cutoff*cutoff) return 0;
-		
+        IMolecule molecule0 = molecules.getMolecule(0);
+        IMolecule molecule1 = molecules.getMolecule(1);
+        dr.E(positionDefinition.position(molecule1));
+        dr.ME(positionDefinition.position(molecule0));
+        boundary.nearestImage(dr);//rij
+        double r2 = dr.squared();
+        if (r2 > cutoff * cutoff) return 0;
+
         iDipole.E(dipoleSource.getDipole(molecules.getMolecule(0)));
         double idotj = iDipole.dot(dipoleSource.getDipole(molecules.getMolecule(1)));
-        return -fac*idotj;
+        return -fac * idotj;
     }
 
     public Vector[][] gradientAndTorque(IMoleculeList molecules) {
-    	IMolecule molecule0 = molecules.getMolecule(0);
-		IMolecule molecule1 = molecules.getMolecule(1);
-		dr.E(positionDefinition.position(molecule1));
-		dr.ME(positionDefinition.position(molecule0));
-		boundary.nearestImage(dr);
-		gradientAndTorque[0][0].E(0);
-		gradientAndTorque[0][1].E(0);
-		double r2 = dr.squared();
-		if (r2 > cutoff*cutoff){
-			gradientAndTorque[1][0].E(0);
-			gradientAndTorque[1][1].E(0);
-			return gradientAndTorque;
-		}
-		
+        IMolecule molecule0 = molecules.getMolecule(0);
+        IMolecule molecule1 = molecules.getMolecule(1);
+        dr.E(positionDefinition.position(molecule1));
+        dr.ME(positionDefinition.position(molecule0));
+        boundary.nearestImage(dr);
+        gradientAndTorque[0][0].E(0);
+        gradientAndTorque[0][1].E(0);
+        double r2 = dr.squared();
+        if (r2 > cutoff * cutoff) {
+            gradientAndTorque[1][0].E(0);
+            gradientAndTorque[1][1].E(0);
+            return gradientAndTorque;
+        }
+
         iDipole.E(dipoleSource.getDipole(molecule0));
         iDipole.XE(dipoleSource.getDipole(molecule1));
         iDipole.TE(fac);
         gradientAndTorque[1][0].E(iDipole);
-        gradientAndTorque[1][1].Ea1Tv1(-1,iDipole);
-		
+        gradientAndTorque[1][1].Ea1Tv1(-1, iDipole);
+
         return gradientAndTorque;
     }
-    
-    public Tensor[] secondDerivative(IMoleculeList molecules){
-    	IMolecule molecule0 = molecules.getMolecule(0);
-		IMolecule molecule1 = molecules.getMolecule(1);
-		dr.E(positionDefinition.position(molecule1));
-		dr.ME(positionDefinition.position(molecule0));
-		boundary.nearestImage(dr);//rij
-		double r2 = dr.squared();
-		secondDerivative[0].E(0);
-		secondDerivative[1].E(0);
-		secondDerivative[2].E(0);
-		if (r2 > cutoff*cutoff) return secondDerivative;
-		iDipole.E(dipoleSource.getDipole(molecule0));
-		Vector jDipole = space.makeVector();
-		jDipole.E(dipoleSource.getDipole(molecule1));
-		
-		double exi = iDipole.getX(0);//ei and ej is the dipole orientation with mu
-		double eyi = iDipole.getX(1);
-		double ezi = iDipole.getX(2);
-		double exj = jDipole.getX(0);
-		double eyj = jDipole.getX(1);
-		double ezj = jDipole.getX(2);
-		
-		Vector deidxi = space.makeVector();
-		Vector deidyi = space.makeVector();
-		Vector deidzi = space.makeVector();
-		Vector dejdxj = space.makeVector();
-		Vector dejdyj = space.makeVector();
-		Vector dejdzj = space.makeVector();
-		
-		double [] deidxiD = {0,-ezi,eyi};
-		double [] deidyiD = {ezi,0,-exi};
-		double [] deidziD = {-eyi,exi,0};
-		deidxi.E(deidxiD);
-		deidyi.E(deidyiD);
-		deidzi.E(deidziD);
-		
-		double [] dejdxjD = {0,-ezj,eyj};
-		double [] dejdyjD = {ezj,0,-exj};
-		double [] dejdzjD = {-eyj,exj,0};
-		dejdxj.E(dejdxjD);
-		dejdyj.E(dejdyjD);
-		dejdzj.E(dejdzjD);
-		
-		a[0].E(iDipole);
-		a[0].XE(dejdxj);
-		a[1].E(iDipole);
-		a[1].XE(dejdyj);
-		a[2].E(iDipole);
-		a[2].XE(dejdzj);
-		secondDerivative[0].E(a);
-		
-		secondDerivative[0].TE(-fac);//ij and ji are the same
-		
-		a[0].E(deidxi);
-		a[0].XE(jDipole);
-		a[1].E(deidyi);
-		a[1].XE(jDipole);
-		a[2].E(deidzi);
-		a[2].XE(jDipole);
-		secondDerivative[1].E(a);
-		
-		secondDerivative[1].TE(-fac);//ii
-		
-		a[0].E(dejdxj);
-		a[0].XE(iDipole);
-		a[1].E(dejdyj);
-		a[1].XE(iDipole);
-		a[2].E(dejdzj);
-		a[2].XE(iDipole);
-		secondDerivative[2].E(a);
-		
-		
-		secondDerivative[2].TE(-fac);//jj 
-    	return secondDerivative; 
-    }
-    
-    
 
-    public Vector[] gradient(IMoleculeList atoms) {
+    public Tensor[] secondDerivative(IMoleculeList molecules) {
+        IMolecule molecule0 = molecules.getMolecule(0);
+        IMolecule molecule1 = molecules.getMolecule(1);
+        dr.E(positionDefinition.position(molecule1));
+        dr.ME(positionDefinition.position(molecule0));
+        boundary.nearestImage(dr);//rij
+        double r2 = dr.squared();
+        secondDerivative[0].E(0);
+        secondDerivative[1].E(0);
+        secondDerivative[2].E(0);
+        if (r2 > cutoff * cutoff) return secondDerivative;
+        iDipole.E(dipoleSource.getDipole(molecule0));
+        Vector jDipole = space.makeVector();
+        jDipole.E(dipoleSource.getDipole(molecule1));
+
+        double exi = iDipole.getX(0);//ei and ej is the dipole orientation with mu
+        double eyi = iDipole.getX(1);
+        double ezi = iDipole.getX(2);
+        double exj = jDipole.getX(0);
+        double eyj = jDipole.getX(1);
+        double ezj = jDipole.getX(2);
+
+
+        a[0].E(0, ezj, -eyj);
+        a[0].XE(iDipole);
+
+
+        a[1].E(-ezj, 0, exj);
+        a[1].XE(iDipole);
+
+        a[2].E(eyj, -exj, 0);
+        a[2].XE(iDipole);
+        secondDerivative[0].E(a);
+
+        secondDerivative[0].TE(-fac);//ij and ji are the same
+
+        a[0].E(0, -ezi, eyi);
+        a[0].XE(jDipole);
+
+        a[1].E(ezi, 0, -exi);
+        a[1].XE(jDipole);
+
+        a[2].E(-eyi, exi, 0);
+        a[2].XE(jDipole);
+        secondDerivative[1].E(a);
+
+        secondDerivative[1].TE(-fac);//ii
+
+        a[0].E(0, -ezj, eyj);
+        a[0].XE(iDipole);
+        a[1].E(ezj, 0, -exj);
+        a[1].XE(iDipole);
+        a[2].E(-eyj, exj, 0);
+        a[2].XE(iDipole);
+        secondDerivative[2].E(a);
+
+
+        secondDerivative[2].TE(-fac);//jj
+        return secondDerivative;
+    }
+
+    public Vector[] gradient(IMoleculeList molecules) {
         return gradientAndTorque[0];
     }
 
-    public Vector[] gradient(IMoleculeList atoms, Tensor pressureTensor) {
-        return gradient(atoms);
+    public Vector[] gradient(IMoleculeList molecules, Tensor pressureTensor) {
+        return gradient(molecules);
     }
 
-    public double virial(IMoleculeList atoms) {
+    //Reaction field is position independent if the cutoff radius scales with volume.
+    public double virial(IMoleculeList molecules) {
         return 0;
     }
 
     /**
-     * Returns a 0-body potential that should be added along with this
+     * @return a 0-body potential that should be added along with this
      * potential.
      */
     public IPotentialMolecular makeP0() {
         return new P0ReactionField(this.space, this);
     }
 
-    protected final IMoleculePositionDefinition positionDefinition;
-    protected final Vector iDipole, cavityDipole;
-    protected final Vector dr ,dr1;
-    protected DipoleSource dipoleSource;
-    protected Boundary boundary;
-    protected double cutoff2, cutoff;
-    protected double epsilon;
-    protected final Vector[][] gradientAndTorque;
-    protected final Tensor[] secondDerivative;
-    protected final Vector[] a;
-    protected double fac;
-    
     /**
      * A 0-body potential that should be added along with this potential.  The
      * 0-body potential includes the effective self-interaction of the
@@ -250,38 +259,42 @@ public class P2ReactionFieldDipole extends PotentialMolecular implements Potenti
      * potential does not result in a gradient or torque on the molecule and is
      * independent of position or orientation.
      */
-    public static class P0ReactionField extends PotentialMolecular implements IPotential0Lrc, PotentialMolecularSoft {
+    private static class P0ReactionField extends PotentialMolecular implements IPotential0Lrc, PotentialMolecularSoft {
 
-        public P0ReactionField(Space space, P2ReactionFieldDipole p) {
-            super(0,space);
+        protected final P2ReactionFieldDipole potential;
+        protected final Vector[] gradient;
+        protected IMolecule targetAtom;
+        protected Box box;
+
+        private P0ReactionField(Space space, P2ReactionFieldDipole p) {
+            super(0, space);
             this.potential = p;
             gradient = new Vector[0];
         }
-        
+
         public double energy(IMoleculeList atoms) {
             double epsilon = potential.getDielectric();
             double cutoff = potential.getRange();
             DipoleSource dipoleSource = potential.getDipoleSource();
-            double fac = 2*(epsilon-1)/(2*epsilon+1)/(cutoff*cutoff*cutoff);
+            double fac = 2 * (epsilon - 1) / (2 * epsilon + 1) / (cutoff * cutoff * cutoff);
             double u = 0;
             if (targetAtom != null) {
                 Vector iDipole = dipoleSource.getDipole(targetAtom);
                 u = -0.5 * fac * iDipole.squared();
-            }
-            else {
+            } else {
                 IMoleculeList moleculeList = box.getMoleculeList();
-                for (int i=0; i<moleculeList.getMoleculeCount(); i++) {
+                for (int i = 0; i < moleculeList.getMoleculeCount(); i++) {
                     Vector iDipole = dipoleSource.getDipole(moleculeList.getMolecule(i));
                     u += -0.5 * fac * iDipole.squared();
                 }
             }
             return u;
         }
-        
+
         public void setBox(Box newBox) {
             box = newBox;
         }
-        
+
         public void setTargetMolecule(IMolecule atom) {
             if (atom == null) {
                 targetAtom = null;
@@ -289,7 +302,7 @@ public class P2ReactionFieldDipole extends PotentialMolecular implements Potenti
             }
             targetAtom = atom;
         }
-        
+
         public void setTargetAtom(IAtom targetAtom) {
             throw new RuntimeException("Can't provide correction for an individual atom");
         }
@@ -301,19 +314,14 @@ public class P2ReactionFieldDipole extends PotentialMolecular implements Potenti
         public Vector[] gradient(IMoleculeList atoms) {
             return gradient;
         }
-        
+
         public Vector[] gradient(IMoleculeList atoms, Tensor pressureTensor) {
             return gradient(atoms);
         }
-        
+
         public double virial(IMoleculeList atoms) {
             return 0;
         }
-
-        protected final P2ReactionFieldDipole potential;
-        protected final Vector[] gradient;
-        protected IMolecule targetAtom;
-        protected Box box;
 
     }
 }
