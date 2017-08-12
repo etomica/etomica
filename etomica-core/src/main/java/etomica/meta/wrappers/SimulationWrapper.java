cd37fc8efbb1e72d7d98cc485d1de3919ef7d5ae
David Kofke <kofke@buffalo.edu>
2017-08-12 17:44:18 -0400

    Provide a method to obtain coordinates of all atoms in wrapped Simulation

--- etomica-core/src/main/java/etomica/meta/wrappers/SimulationWrapper.java
+++ etomica-core/src/main/java/etomica/meta/wrappers/SimulationWrapper.java
@@ -1,5 +1,7 @@
 package etomica.meta.wrappers;
 
+import etomica.atom.IAtomList;
+import etomica.box.Box;
 import etomica.integrator.Integrator;
 import etomica.integrator.IntegratorBox;
 import etomica.integrator.IntegratorManagerMC;
@@ -7,6 +9,7 @@ import etomica.meta.SimulationModel;
 import etomica.meta.properties.InstanceProperty;
 import etomica.potential.PotentialMaster;
 import etomica.simulation.Simulation;
+import etomica.simulation.prototypes.HSMD2D;
 
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
@@ -23,6 +26,8 @@ import java.util.Set;
  * and adds a "synthetic" method to return them to the list of the simulation's properties.
  */
 public class SimulationWrapper extends ObjectWrapper<Simulation> {
+
+    protected double allCoords[][][] = new double[0][][];
 
     public SimulationWrapper(Simulation wrapped, SimulationModel simModel, boolean doSerialize) {
         super(wrapped, simModel, doSerialize);
@@ -57,5 +62,36 @@ public class SimulationWrapper extends ObjectWrapper<Simulation> {
         }
     }
 
+    /**
+     * Array of coordinates of all atoms in all boxes in the wrapped Simulation.
+     * @return 3-dimensional array of coordinates. First index indicates the box; second index
+     * indicates the leaf atom in the box; third index indicates the coordinate (i.e., x, y, z)
+     */
+    public double[][][] getAllCoordinates() {
+        Simulation sim = simModel.getSimulation();
+        if(allCoords.length != sim.getBoxCount()) {
+            allCoords = new double[sim.getBoxCount()][0][];
+            for(int i=0; i< sim.getBoxCount(); i++) {
+                IAtomList leafList = sim.getBox(i).getLeafList();
+                if(allCoords[i].length != leafList.getAtomCount()) {
+                    allCoords[i] = new double[leafList.getAtomCount()][sim.getSpace().D()];
+                }
+                for(int j=0; j<leafList.getAtomCount(); j++) {
+                    leafList.getAtom(j).getPosition().assignTo(allCoords[i][j]);
+                }
+            }
+        }
+        return allCoords;
+    }
 
+
+/*
+    public static void main(String[] args) {
+        Simulation sim = new HSMD2D();
+        SimulationWrapper wrapper = new SimulationWrapper(sim, new SimulationModel(sim), true);
+        double[][][] coords = wrapper.getAllCoordinates();
+        System.out.println("done");
+
+    }
+*/
 }
