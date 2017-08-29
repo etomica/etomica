ce530612a0c89d269b8517bafc6a1ca96328def4
Alex Kofke <akofke@gmail.com>
2017-08-28 21:08:30 -0400

    Add CORS headers filter to server

--- etomica-server/src/main/java/etomica/server/EtomicaServer.java
+++ etomica-server/src/main/java/etomica/server/EtomicaServer.java
@@ -18,11 +18,15 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.websockets.WebsocketBundle;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.websocket.server.ServerEndpointConfig;
+import java.util.EnumSet;
 import java.util.Map;
 import java.util.Timer;
 import java.util.UUID;
@@ -72,6 +76,7 @@ public class EtomicaServer extends Application<EtomicaServerConfig> {
     @Override
     public void run(EtomicaServerConfig configuration, Environment environment) throws Exception {
         environment.healthChecks().register("basic", new BasicHealthCheck());
+        configureCors(environment);
 
 //        environment.jersey().register(new SimulationsIndexResource(new ComponentIndex<>(Simulation.class)));
 //        environment.jersey().register(new SimulationResource(simStore));
@@ -81,6 +86,18 @@ public class EtomicaServer extends Application<EtomicaServerConfig> {
 
     public static void main(String[] args) throws Exception {
         new EtomicaServer().run(args);
+    }
+
+    private static void configureCors(Environment env) {
+        final FilterRegistration.Dynamic cors = env.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
     private static class WSConfigurator extends ServerEndpointConfig.Configurator {
