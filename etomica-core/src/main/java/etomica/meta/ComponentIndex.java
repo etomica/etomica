27857aa4d117fe7a93031f3b05a4b094aa65f8c1
Alex Kofke <akofke@gmail.com>
2017-09-05 12:36:21 -0400

    Use common reflections instance to avoid scanning twice

--- etomica-core/src/main/java/etomica/meta/ComponentIndex.java
+++ etomica-core/src/main/java/etomica/meta/ComponentIndex.java
@@ -4,16 +4,17 @@ import org.reflections.Reflections;
 
 import java.util.Set;
 
+import static etomica.meta.Common.REFLECTIONS;
+
 public class ComponentIndex<T> {
     private final Class<T> componentClass;
 
-    private static final Reflections reflections = new Reflections("etomica");
 
     public ComponentIndex(Class<T> componentClass) {
         this.componentClass = componentClass;
     }
 
     public Set<Class<? extends T>> getComponentSet() {
-        return reflections.getSubTypesOf(componentClass);
+        return REFLECTIONS.getSubTypesOf(componentClass);
     }
 }
