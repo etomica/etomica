40a90d536c120889e827e08a79dfbae86f464b36
Andrew Schultz <ajs42@buffalo.edu>
2017-10-02 18:43:21 -0400

    Migrate DataTableWriter away from using a caster.  Set up internal stream instead.

--- etomica-core/src/main/java/etomica/data/DataTableWriter.java
+++ etomica-core/src/main/java/etomica/data/DataTableWriter.java
@@ -4,34 +4,33 @@
 
 package etomica.data;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 import etomica.data.DataLogger.DataWriter;
 import etomica.data.types.CastGroupOfTablesToDataTable;
-import etomica.data.types.CastGroupToDoubleArray;
 import etomica.data.types.CastToTable;
 import etomica.data.types.DataDoubleArray;
 import etomica.data.types.DataGroup.DataInfoGroup;
 import etomica.data.types.DataTable;
 import etomica.data.types.DataTable.DataInfoTable;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * A DataWriter that writes out data as a table with optional column headings
  */
-public class DataTableWriter implements DataWriter, java.io.Serializable {
+public class DataTableWriter implements DataWriter {
 
+    protected IDataSink internalDataSink;
+    // DataLogger will give the fileWriter back to us when it actually writes
+    private transient FileWriter fileWriter;
+    private boolean firstWrite;
+    private IDataInfo dataInfo;
+    private boolean includeHeader;
+    
     public DataTableWriter() {
         super();
         setIncludeHeader(true);
         reset();
-    }
-
-    /**
-     * Directs the writer to include column headers or not
-     */
-    public void setIncludeHeader(boolean newIncludeHeader) {
-        includeHeader = newIncludeHeader;
     }
     
     /**
@@ -40,40 +39,81 @@ public class DataTableWriter implements DataWriter, java.io.Serializable {
     public boolean getIncludeHeader() {
         return includeHeader;
     }
-    
-    public void putDataInfo(IDataInfo newDataInfo) {
-        dataInfo = newDataInfo;
+
+    /**
+     * Directs the writer to include column headers or not
+     */
+    public void setIncludeHeader(boolean newIncludeHeader) {
+        includeHeader = newIncludeHeader;
     }
 
-    public DataPipe getDataCaster(IDataInfo newDataInfo) {
+    public void putDataInfo(IDataInfo newDataInfo) {
         if (newDataInfo instanceof DataInfoTable) {
             // we like tables
-            return null;
+            dataInfo = newDataInfo;
+            return;
         }
-        else if (newDataInfo instanceof DataInfoGroup) {
-            if (((DataInfoGroup)newDataInfo).getNDataInfo() == 0) {
-                //it's empty, turn it into an empty array
-                return new CastGroupToDoubleArray();
-            }
-            IDataInfo dataInfo0 = ((DataInfoGroup)newDataInfo).getSubDataInfo(0);
-            for (int i = 1; i<((DataInfoGroup)newDataInfo).getNDataInfo(); i++) {
-                IDataInfo subDataInfo = ((DataInfoGroup)newDataInfo).getSubDataInfo(0);
-                if (subDataInfo.getClass() != dataInfo0.getClass()){
-                    throw new IllegalArgumentException("DataSinkTable can only handle homogeneous groups");
+        if (newDataInfo instanceof DataInfoGroup) {
+            IDataInfo dataInfo0 = null;
+            if (((DataInfoGroup) newDataInfo).getNDataInfo() > 1) {
+                dataInfo0 = ((DataInfoGroup) newDataInfo).getSubDataInfo(0);
+                for (int i = 1; i < ((DataInfoGroup) newDataInfo).getNDataInfo(); i++) {
+                    IDataInfo subDataInfo = ((DataInfoGroup) newDataInfo).getSubDataInfo(0);
+                    if (subDataInfo.getClass() != dataInfo0.getClass()) {
+                        throw new IllegalArgumentException("DataSinkTable can only handle homogeneous groups");
+                    }
                 }
             }
-            if (dataInfo0 instanceof DataInfoTable) {
+            if (dataInfo0 == null || dataInfo0 instanceof DataInfoTable) {
                 // group of tables, just flatten it
-                return new CastGroupOfTablesToDataTable();
+                internalDataSink = new CastGroupOfTablesToDataTable();
+                ((CastGroupOfTablesToDataTable) internalDataSink).setDataSink(makeTerminalSink());
+                internalDataSink.putDataInfo(newDataInfo);
+                return;
             }
             // turn group into a multi-dimensional array, which we'll cast to a table
             // with a CastToTable
-            return new CastGroupToDoubleArray();
+            internalDataSink = new CastGroupOfTablesToDataTable();
+            ((CastGroupOfTablesToDataTable) internalDataSink).setDataSink(makeTerminalSink());
+            internalDataSink.putDataInfo(newDataInfo);
+            return;
         }
-        return new CastToTable();
+        internalDataSink = new CastToTable();
+        ((CastToTable) internalDataSink).setDataSink(makeTerminalSink());
+        internalDataSink.putDataInfo(newDataInfo);
     }
-    
+
+    public DataPipe getDataCaster(IDataInfo newDataInfo) {
+        return null;
+    }
+
+    protected IDataSink makeTerminalSink() {
+        return new IDataSink() {
+            @Override
+            public void putData(IData data) {
+                DataTableWriter.this.putDataInternal(data);
+            }
+
+            @Override
+            public void putDataInfo(IDataInfo dataInfo) {
+                DataTableWriter.this.putDataInfoInternal(dataInfo);
+            }
+        };
+    }
+
+    protected void putDataInfoInternal(IDataInfo myDataInfo) {
+        dataInfo = myDataInfo;
+    }
+
     public void putData(IData data) {
+        if (internalDataSink != null) {
+            internalDataSink.putData(data);
+            return;
+        }
+        putDataInternal(data);
+    }
+
+    protected void putDataInternal(IData data) {
         DataTable table = (DataTable)data;
         try {
             int nColumns = table.getNData();
@@ -90,32 +130,24 @@ public class DataTableWriter implements DataWriter, java.io.Serializable {
                 fileWriter.write("\n");
             }
             firstWrite = false;
-            for (int i=0; i<table.getNRows(); i++) {
+            for (int i = 0; i<table.getNRows(); i++) {
                 fileWriter.write(Double.toString(((DataDoubleArray)table.getData(0)).getData()[i]));
-                for (int j=1; j<nColumns; j++) {
+                for (int j = 1; j<nColumns; j++) {
                     fileWriter.write("  "+Double.toString(((DataDoubleArray)table.getData(j)).getData()[i]));
                 }
                 fileWriter.write("\n");
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        
+
     }
-    
+
     public void setFileWriter(FileWriter newFileWriter) {
         fileWriter = newFileWriter;
     }
-    
+
     public void reset() {
         firstWrite = true;
     }
-
-    private static final long serialVersionUID = 2L;
-    // DataLogger will give the fileWriter back to us when it actually writes
-    private transient FileWriter fileWriter;
-    private boolean firstWrite;
-    private IDataInfo dataInfo;
-    private boolean includeHeader;
 }
