0db417d0a0c4c9e4330b4290accdd442978f0a2b
Andrew Schultz <ajs42@buffalo.edu>
2017-10-13 16:51:18 -0400

    unbreak benchmark

--- benchmarks/src/jmh/java/etomica/simulation/prototypes/HSMD3DBenchmark.java
+++ benchmarks/src/jmh/java/etomica/simulation/prototypes/HSMD3DBenchmark.java
@@ -4,23 +4,16 @@
 
 package etomica.simulation.prototypes;
 
-import etomica.integrator.Integrator;
-import etomica.integrator.IntegratorMD;
 import etomica.simulation.Simulation;
-import etomica.space3d.Space3D;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.profile.GCProfiler;
-import org.openjdk.jmh.profile.StackProfiler;
-import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-
-import java.util.Collection;
 
 /**
  * Created by alex on 4/29/17.
@@ -32,7 +25,7 @@ public class HSMD3DBenchmark {
 
     @Setup
     public void setUp() {
-        sim = new HSMD3D(Space3D.getInstance());
+        sim = new HSMD3D();
     }
 
     @Benchmark
