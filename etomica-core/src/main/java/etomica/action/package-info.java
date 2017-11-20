0b1aad65e1dd67f30dc483fd6cb23884bf46a8b8
David Kofke <kofke@buffalo.edu>
2017-11-20 16:43:19 -0500

    Update package info for etomica.action

--- etomica-core/src/main/java/etomica/action/package-info.java
+++ etomica-core/src/main/java/etomica/action/package-info.java
@@ -1,14 +1,16 @@
 /**
- * Interfaces and classes that define elementary actions that can be performed
- * on simulation elements. Examples include classes that move an atom, or that change the density of a box.
- * This package defines the {@link etomica.action.IAction Action} interface. This interface includes the <tt>actionPerformed</tt>
- * method, which a class implements to define its action.
- * <p>
- * Interfaces extending Action define actions for different <i>etomica</i> elements, such as
+ * Define elementary actions that can be performed on simulation elements.
+ * Examples include classes that move an Atom, or that change the density of a Box.
+ * Classes defining such actions implement the {@link etomica.action.IAction Action} interface.
+ * Interfaces extending IAction define actions for different simulation elements, such as
  * Box, Atom, and so on.
  * <p>
  * {@link etomica.action.Activity Activity} implements Action and is an abstract class appropriate
  * to encapsulate more complex, time-consuming tasks.  Classes extending Activity are collected
- * in the subpackage {@link etomica.action.activity etomica.action.activity}.
+ * in the subpackage {@link etomica.action.activity etomica.action.activity}. Whereas Actions must
+ * be completed before further processing continues, Activity instances generally operate on a different
+ * thread than the one that calls them, and can be paused, resumed or halted before they complete.
+ * Classes extending Activity are collected in the subpackage {@link etomica.action.activity etomica.action.activity}.
  */
 package etomica.action;
+
