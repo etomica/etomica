461b5bf794c75e1cdbe0f2b3e1d14fd5439bf383
Alex Kofke <akofke@gmail.com>
2017-12-28 19:32:36 -0500

    Change BoxEventManager to use CopyOnWriteArrayList
    Potentially speeds up cases where molecules are added or removed
    rapidly by removing synchronization on event manager methods.

--- etomica-core/src/main/java/etomica/box/BoxEventManager.java
+++ etomica-core/src/main/java/etomica/box/BoxEventManager.java
@@ -8,67 +8,67 @@ import etomica.atom.IAtom;
 import etomica.molecule.IMolecule;
 import etomica.species.ISpecies;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class BoxEventManager {
 
-    private final List<BoxEventListener> listeners = new ArrayList<>();
+    private final List<BoxEventListener> listeners = new CopyOnWriteArrayList<>();
     private final Box box;
 
     public BoxEventManager(Box box) {
         this.box = box;
     }
 
-    public synchronized void addListener(BoxEventListener newListener) {
+    public void addListener(BoxEventListener newListener) {
 
         if (newListener == null) throw new NullPointerException("Cannot add null as a listener to Box");
         if (listeners.contains(newListener)) {
             throw new RuntimeException(newListener + " is already an interval action");
         }
-        listeners.add(0, newListener);
+        listeners.add(newListener);
     }
 
-    public synchronized void removeListener(BoxEventListener listener) {
+    public void removeListener(BoxEventListener listener) {
         listeners.remove(listener);
     }
 
-    public synchronized void moleculeAdded(IMolecule molecule) {
+    public void moleculeAdded(IMolecule molecule) {
         BoxMoleculeEvent event = new BoxMoleculeEvent(box, molecule);
         for (BoxEventListener listener : listeners) {
             listener.boxMoleculeAdded(event);
         }
     }
 
-    public synchronized void moleculeRemoved(IMolecule molecule) {
+    public void moleculeRemoved(IMolecule molecule) {
         BoxMoleculeEvent event = new BoxMoleculeEvent(box, molecule);
         for (BoxEventListener listener : listeners) {
             listener.boxMoleculeRemoved(event);
         }
     }
 
-    public synchronized void globalAtomLeafIndexChanged(int index) {
+    public void globalAtomLeafIndexChanged(int index) {
         BoxIndexEvent event = new BoxIndexEvent(box, index);
         for (BoxEventListener listener : listeners) {
             listener.boxGlobalAtomLeafIndexChanged(event);
         }
     }
 
-    public synchronized void atomLeafIndexChanged(IAtom atom, int index) {
+    public void atomLeafIndexChanged(IAtom atom, int index) {
         BoxAtomIndexEvent event = new BoxAtomIndexEvent(box, atom, index);
         for (BoxEventListener listener : listeners) {
             listener.boxAtomLeafIndexChanged(event);
         }
     }
 
-    public synchronized void numberMolecules(ISpecies species, int count) {
+    public void numberMolecules(ISpecies species, int count) {
         BoxMoleculeCountEvent event = new BoxMoleculeCountEvent(box, species, count);
         for (BoxEventListener listener : listeners) {
             listener.boxNumberMolecules(event);
         }
     }
 
-    public synchronized void moleculeIndexChanged(IMolecule molecule, int index) {
+    public void moleculeIndexChanged(IMolecule molecule, int index) {
         BoxMoleculeIndexEvent event = new BoxMoleculeIndexEvent(box, molecule, index);
         for (BoxEventListener listener : listeners) {
             listener.boxMoleculeIndexChanged(event);
