812992cffe495b10df22672c6681ba1565d5b73e
Alex Kofke <akofke@gmail.com>
2018-01-04 17:14:33 -0500

    Have AtomPotentialList use a shared static empty boolean array in constructor
    Avoids potentially allocating a new redundant empty array for each atom.

--- etomica-core/src/main/java/etomica/nbr/list/AtomPotentialList.java
+++ etomica-core/src/main/java/etomica/nbr/list/AtomPotentialList.java
@@ -4,56 +4,54 @@
 
 package etomica.nbr.list;
 
-import java.io.Serializable;
 import java.util.Arrays;
 
 
 /**
- * Class used to maintain list of whether each 1-body potential that
- * applies to an AtomType is currently in effect or not for an Atom.
+ * Class used to maintain list of whether each 1-body potential that applies to an AtomType is currently in effect or
+ * not for an Atom.
  */
-public class AtomPotentialList implements Serializable {
+public final class AtomPotentialList {
 
-    private static final long serialVersionUID = 1L;
-    protected boolean[] isInteractingList;
-	
+    private static final boolean[] EMPTY_BOOLEAN_ARR = new boolean[0];
+    private boolean[] isInteractingList;
+
     /**
      * Constructs sequencer for the given atom.
      */
     public AtomPotentialList() {
-        isInteractingList = new boolean[0];
+        // use static empty array to avoid initially allocating a separate empty array for each atom
+        isInteractingList = EMPTY_BOOLEAN_ARR;
     }
-    
+
     /**
-     * Sets whether the atom is interacting or not with the potential
-     * corresponding to the given index.
+     * Sets whether the atom is interacting or not with the potential corresponding to the given index.
      */
     public void setIsInteracting(boolean flag, int index) {
         if (!(index < isInteractingList.length)) {
-            isInteractingList = Arrays.copyOf(isInteractingList,index+1);
+            isInteractingList = Arrays.copyOf(isInteractingList, index + 1);
         }
         isInteractingList[index] = flag;
     }
 
     /**
-     * Returns booleans indicating whether the Atom interacts with each 
-     * potential or not.
+     * Returns booleans indicating whether the Atom interacts with each potential or not.
      */
     public boolean[] getInteractingList() {
         return isInteractingList;
     }
-	
+
     protected void setCapacity(int index) {
         isInteractingList = Arrays.copyOf(isInteractingList, index);
     }
-	
+
     /**
      * Clears neighbor lists, resetting all flags to false (not interacting).
      */
-	public void clearNbrs() {
-		final int length = isInteractingList.length;
-		for (int i=0; i<length; i++) {
-			isInteractingList[i] = false;
-		}
-	}
+    public void clearNbrs() {
+        final int length = isInteractingList.length;
+        for (int i = 0; i < length; i++) {
+            isInteractingList[i] = false;
+        }
+    }
 }
