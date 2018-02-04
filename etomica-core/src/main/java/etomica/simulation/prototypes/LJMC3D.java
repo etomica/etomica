462fe47bcf2bcc206c17046a80ff62253f65a1de
Andrew J. Schultz <ajschult@verizon.net>
2018-02-03 20:34:43 -0500

    Prototype simulation for MC simulation of LJ particles

---
+++ etomica-core/src/main/java/etomica/simulation/prototypes/LJMC3D.java
@@ -0,0 +1,167 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package etomica.simulation.prototypes;
+
+import etomica.action.BoxInflate;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.atom.AtomType;
+import etomica.box.Box;
+import etomica.config.ConfigurationLattice;
+import etomica.data.AccumulatorAverageFixed;
+import etomica.data.AccumulatorHistory;
+import etomica.data.DataPumpListener;
+import etomica.data.history.HistoryCollapsingAverage;
+import etomica.data.meter.MeterPotentialEnergyFromIntegrator;
+import etomica.data.types.DataGroup;
+import etomica.graphics.DisplayPlot;
+import etomica.graphics.SimulationGraphic;
+import etomica.integrator.IntegratorMC;
+import etomica.integrator.mcmove.MCMoveAtom;
+import etomica.lattice.LatticeCubicFcc;
+import etomica.nbr.cell.PotentialMasterCell;
+import etomica.potential.P2LennardJones;
+import etomica.potential.P2SoftSphericalTruncated;
+import etomica.simulation.Simulation;
+import etomica.space3d.Space3D;
+import etomica.species.SpeciesSpheresMono;
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+
+/**
+ * Simple Simulation for Monte Carlo simulation of Lennard-Jonesium.  Interactions are
+ * tracked with cell lists.
+ *
+ * @author Andrew Schultz
+ */
+
+public class LJMC3D extends Simulation {
+
+    public final PotentialMasterCell potentialMaster;
+    public final ActivityIntegrate activityIntegrate;
+    public final IntegratorMC integrator;
+
+    /**
+     * Creates simulation with default parameters from {@link SimParams}
+     */
+    public LJMC3D() {
+        this(new SimParams());
+    }
+
+    /**
+     * Creates simulation with the given parameters
+     */
+    public LJMC3D(SimParams params) {
+        super(Space3D.getInstance());
+        double rc = 3;
+        potentialMaster = new PotentialMasterCell(this, rc, space);
+
+        integrator = new IntegratorMC(this, potentialMaster);
+        integrator.setTemperature(params.temperature);
+
+        activityIntegrate = new ActivityIntegrate(integrator);
+        getController().addAction(activityIntegrate);
+
+        SpeciesSpheresMono species = new SpeciesSpheresMono(this, space);
+        addSpecies(species);
+
+        Box box = new Box(space);
+        addBox(box);
+        box.setNMolecules(species, params.numAtoms);
+        BoxInflate inflater = new BoxInflate(box, space, params.density);
+        inflater.actionPerformed();
+
+        ConfigurationLattice config = new ConfigurationLattice(new LatticeCubicFcc(space), space);
+        config.initializeCoordinates(box);
+
+        P2LennardJones p2lj = new P2LennardJones(space);
+        P2SoftSphericalTruncated p2 = new P2SoftSphericalTruncated(space, p2lj, rc);
+        AtomType type1 = species.getLeafType();
+        potentialMaster.addPotential(p2, new AtomType[]{type1, type1});
+        potentialMaster.setCellRange(2);
+
+        integrator.setBox(box);
+        MCMoveAtom mcMoveAtom = new MCMoveAtom(random, potentialMaster, space);
+        integrator.getMoveManager().addMCMove(mcMoveAtom);
+        potentialMaster.getNbrCellManager(box).assignCellAll();
+        integrator.getMoveEventManager().addListener(potentialMaster.getNbrCellManager(box).makeMCMoveListener());
+    }
+
+    public static void main(String[] args) {
+
+        SimParams params = new SimParams();
+        if (args.length > 0) {
+            ParseArgs.doParseArgs(params, args);
+        } else {
+            // modify parameters here for interactive testing
+        }
+
+        LJMC3D sim = new LJMC3D(params);
+        long steps = params.steps;
+        int interval = 10;
+        int blocks = 100;
+        long blockSize = steps / (interval * blocks);
+
+        // equilibration
+        long t1 = System.currentTimeMillis();
+        sim.activityIntegrate.setMaxSteps(steps / 10);
+        sim.activityIntegrate.actionPerformed();
+        System.out.println("equilibration finished");
+
+        // data collection
+        MeterPotentialEnergyFromIntegrator meterPE = new MeterPotentialEnergyFromIntegrator(sim.integrator);
+        AccumulatorAverageFixed acc = new AccumulatorAverageFixed(blockSize);
+        DataPumpListener pump = new DataPumpListener(meterPE, acc, interval);
+        sim.getIntegrator().getEventManager().addListener(pump);
+
+        sim.activityIntegrate.setMaxSteps(steps);
+        sim.getIntegrator().resetStepCount();
+        sim.activityIntegrate.actionPerformed();
+
+        long t2 = System.currentTimeMillis();
+
+        DataGroup dataPE = (DataGroup) acc.getData();
+        int numAtoms = sim.getBox(0).getLeafList().getAtomCount();
+        double avg = dataPE.getValue(acc.AVERAGE.index) / numAtoms;
+        double err = dataPE.getValue(acc.ERROR.index) / numAtoms;
+        double cor = dataPE.getValue(acc.BLOCK_CORRELATION.index);
+
+        System.out.println("energy avg: " + avg + "  err: " + err + "  cor: " + cor);
+        System.out.println("time: " + (t2 - t1) * 0.001);
+    }
+
+    public static class Graphic {
+        public static void main(String[] args) {
+            SimParams params = new SimParams();
+            if (args.length > 0) {
+                ParseArgs.doParseArgs(params, args);
+            } else {
+                // modify parameters here for interactive testing
+            }
+
+            LJMC3D sim = new LJMC3D(params);
+            SimulationGraphic graphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE);
+
+            MeterPotentialEnergyFromIntegrator meterPE = new MeterPotentialEnergyFromIntegrator(sim.integrator);
+            AccumulatorHistory accPE = new AccumulatorHistory(new HistoryCollapsingAverage());
+            DataPumpListener pumpPE = new DataPumpListener(meterPE, accPE, 10);
+            sim.getIntegrator().getEventManager().addListener(pumpPE);
+
+            DisplayPlot historyPE = new DisplayPlot();
+            accPE.setDataSink(historyPE.getDataSet().makeDataSink());
+            historyPE.setLabel("PE");
+            graphic.add(historyPE);
+
+            graphic.makeAndDisplayFrame();
+
+        }
+    }
+
+    public static class SimParams extends ParameterBase {
+        public long steps = 1000000;
+        public double density = 0.6;
+        public double temperature = 1.2;
+        public int numAtoms = 1000;
+    }
+}
