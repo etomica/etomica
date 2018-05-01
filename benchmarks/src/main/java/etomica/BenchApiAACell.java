e610b67ea62eeb86a9af8dd55d54295babaa7cd3
Alex Kofke <akofke@gmail.com>
2018-05-01 11:03:02 -0400

    Move ApiAACell to private inner class of BenchApiAACell
    Since we don't need it anymore.

--- benchmarks/src/main/java/etomica/BenchApiAACell.java
+++ benchmarks/src/main/java/etomica/BenchApiAACell.java
@@ -1,15 +1,19 @@
 package etomica;
 
+import etomica.atom.AtomArrayList;
+import etomica.atom.IAtom;
 import etomica.atom.IAtomList;
+import etomica.atom.iterator.ApiInterArrayList;
+import etomica.atom.iterator.ApiIntraArrayList;
+import etomica.atom.iterator.AtomLeafsetIterator;
 import etomica.box.Box;
-import etomica.config.Configuration;
 import etomica.config.ConfigurationLattice;
 import etomica.lattice.CellLattice;
 import etomica.lattice.LatticeCubicFcc;
-import etomica.nbr.cell.ApiAACell;
-import etomica.nbr.cell.ApiAACellFast;
+import etomica.lattice.RectangularLattice;
 import etomica.nbr.cell.Cell;
 import etomica.nbr.cell.NeighborCellManager;
+import etomica.potential.IteratorDirective;
 import etomica.simulation.Simulation;
 import etomica.space.Vector;
 import etomica.space3d.Space3D;
@@ -24,6 +28,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.BiConsumer;
 import java.util.stream.IntStream;
 
 @State(Scope.Benchmark)
@@ -285,5 +290,189 @@ public class BenchApiAACell {
                 .build();
 
         new Runner(opts).run();
+    }
+
+    private static class ApiAACellFast {
+        private final Box box;
+        private CellLattice lattice;
+
+        public ApiAACellFast(Box box) {
+            this.box = box;
+        }
+
+        public void setLattice(CellLattice lattice) {
+            this.lattice = lattice;
+        }
+
+        public void forEachPair(AtomPairConsumer action) {
+            int[][] nbrCells = lattice.getUpNeighbors();
+            for (int centralCellIdx = 0; centralCellIdx < lattice.sites().length; centralCellIdx++) {
+
+                Cell centralCell = (Cell) lattice.sites()[centralCellIdx];
+                IAtomList centralCellAtoms = centralCell.occupants();
+
+                if (!centralCellAtoms.isEmpty()) {
+
+                    // loop over pairs within the cell
+                    for (int i = 0; i < centralCellAtoms.size(); i++) {
+                        for (int j = i + 1; j < centralCellAtoms.size(); j++) {
+                            action.accept(centralCellAtoms.get(i), centralCellAtoms.get(j));
+                        }
+                    }
+
+                    for (int nbrCellIdx : nbrCells[centralCellIdx]) {
+                        Cell nbrCell = (Cell) lattice.sites()[nbrCellIdx];
+                        IAtomList nbrCellAtoms = nbrCell.occupants();
+
+                        for (int i = 0; i < centralCellAtoms.size(); i++) {
+                            for (int j = 0; j < nbrCellAtoms.size(); j++) {
+                                action.accept(centralCellAtoms.get(i), nbrCellAtoms.get(j));
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        @FunctionalInterface
+        public interface AtomPairConsumer extends BiConsumer<IAtom, IAtom> {
+            @Override
+            void accept(IAtom atom1, IAtom atom2);
+        }
+    }
+
+    /**
+     * Returns iterates formed from all cell-based neighbor pairs.
+     */
+    private static class ApiAACell {
+
+        protected final boolean[] periodicity;
+        private final Box box;
+        private final ApiIntraArrayList intraListIterator;
+        private final ApiInterArrayList interListIterator;
+        private final CellLattice.NeighborIterator neighborIterator;
+        private final RectangularLattice.Iterator cellIterator;
+        private AtomLeafsetIterator listIterator;
+
+        /**
+         * Constructor makes iterator that must have box specified and then be
+         * reset() before iteration.
+         *
+         * @param D     the dimension of the space of the simulation (used to
+         *              construct cell iterators)
+         * @param range the distance within which pairs of atoms are considered
+         *              neighbors. Used to define neighbor cells; some iterates may
+         *              exceed this separation
+         */
+        public ApiAACell(int D, double range, Box box) {
+            cellIterator = new RectangularLattice.Iterator(D);
+            neighborIterator = new CellLattice.NeighborIterator(D, range);
+            neighborIterator.setDirection(IteratorDirective.Direction.UP);
+            interListIterator = new ApiInterArrayList(new AtomArrayList(), new AtomArrayList());
+            intraListIterator = new ApiIntraArrayList();
+            listIterator = intraListIterator;
+            periodicity = new boolean[D];
+            this.box = box;
+        }
+
+        public void setLattice(CellLattice lattice) {
+            cellIterator.setLattice(lattice);
+            neighborIterator.setLattice(lattice);
+            unset();
+        }
+
+        /**
+         * Returns the number of atom pairs the iterator will return if reset and
+         * iterated in its present state.
+         */
+        public int size() {
+            int count = 0;
+            reset();
+            for (Object a = nextPair(); a != null; a = nextPair()) {
+                count++;
+            }
+            return count;
+        }
+
+        public IAtomList nextPair() {
+            IAtomList nextPair = listIterator.next();
+            if (nextPair == null) {
+                return advanceLists();
+            }
+            return nextPair;
+        }
+
+        public void unset() {
+            listIterator.unset();
+        }
+
+        /**
+         * Returns 2, indicating that this is a pair iterator.
+         */
+        public int nBody() {
+            return 2;
+        }
+
+        public void reset() {
+            for (int i = 0; i < periodicity.length; i++) {
+                periodicity[i] = box.getBoundary().getPeriodicity(i);
+            }
+            neighborIterator.setPeriodicity(periodicity);
+            cellIterator.reset();
+            neighborIterator.checkDimensions();
+            neighborIterator.unset();
+            listIterator.unset();
+            //System.out.println("reset in ApiAACell");
+        }//end of reset
+
+        // Moves to next pair of lists that can provide an iterate
+        // This should be invoked only if listIterator.hasNext is false
+        private IAtomList advanceLists() {
+            do {
+                //advance neighbor cell
+                if (neighborIterator.hasNext()) {
+                    interListIterator.setInnerList(((Cell) neighborIterator.next()).occupants());
+                    listIterator = interListIterator;
+                    interListIterator.reset();
+                    IAtomList pair = listIterator.next();
+                    if (pair != null) {
+                        return pair;
+                    }
+
+                    //advance central cell and set up neighbor cell iterator if
+                    // central cell has some molecules
+                } else if (cellIterator.hasNext()) {
+                    AtomArrayList list = ((Cell) cellIterator.peek()).occupants();
+                    neighborIterator.setSite(cellIterator.nextIndex());
+
+                    if (!list.isEmpty()) {//central cell has molecules
+                        interListIterator.setOuterList(list); //for neighbor-cell looping
+                        intraListIterator.setList(list);//for intra-cell looping
+                        neighborIterator.reset();
+
+                        listIterator = intraListIterator;
+                        intraListIterator.reset();
+
+                        IAtomList pair = listIterator.next();
+                        if (pair != null) {
+                            return pair;
+                        }
+                    } else {//no molecules in central cell
+                        neighborIterator.unset();
+                        listIterator.unset();
+                    }
+                } else {//no more cells at all
+                    return null;
+                }
+            } while (true);
+        }//end of advanceCell
+
+        /**
+         * @return Returns the cellIterator.
+         */
+        public CellLattice.NeighborIterator getNbrCellIterator() {
+            return neighborIterator;
+        }
     }
 }
