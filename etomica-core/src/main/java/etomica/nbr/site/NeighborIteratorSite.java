c3e5ab0ef47051326e1e7f251b715ced92d6bdb3
Alex Kofke <akofke@gmail.com>
2018-05-25 17:14:47 -0400

    Implement NeighborIteratorSite

--- etomica-core/src/main/java/etomica/nbr/site/NeighborIteratorSite.java
+++ etomica-core/src/main/java/etomica/nbr/site/NeighborIteratorSite.java
@@ -11,6 +11,7 @@ public class NeighborIteratorSite implements NeighborIterator {
     private final NeighborSiteManager siteManager;
     private final CellLattice lattice;
     private final RectangularLatticeNbrIteratorAdjacent realIterator;
+    private final int[] latticeIndex;
 
     public NeighborIteratorSite(NeighborSiteManager siteManager, Box box) {
         this.siteManager = siteManager;
@@ -18,10 +19,33 @@ public class NeighborIteratorSite implements NeighborIterator {
         this.realIterator = new RectangularLatticeNbrIteratorAdjacent(lattice.D());
         realIterator.setLattice(lattice);
         realIterator.setPeriodicity(box.getBoundary().getPeriodicity());
+        this.latticeIndex = new int[box.getSpace().getD()];
     }
 
     @Override
     public void forEachNeighbor(IAtom targetAtom, IteratorDirective.Direction direction, AtomPairConsumer upAction, AtomPairConsumer downAction) {
-        // TODO
+        lattice.latticeIndex(siteManager.getSite(targetAtom).latticeArrayIndex, latticeIndex);
+        realIterator.setSite(this.latticeIndex);
+
+        if (direction != IteratorDirective.Direction.DOWN) {
+            realIterator.setDirection(IteratorDirective.Direction.UP);
+            realIterator.reset();
+
+            while (this.realIterator.hasNext()) {
+                AtomSite site = ((AtomSite) realIterator.next());
+                upAction.accept(targetAtom, site.getAtom());
+            }
+        }
+
+        if (direction != IteratorDirective.Direction.UP) {
+            realIterator.setDirection(IteratorDirective.Direction.DOWN);
+            realIterator.reset();
+
+            while (this.realIterator.hasNext()) {
+                AtomSite site = ((AtomSite) realIterator.next());
+                downAction.accept(site.getAtom(), targetAtom);
+            }
+
+        }
     }
 }
