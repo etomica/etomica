b639c4dc3dfca7302c43a1afd2b1d0f194149978
Andrew Schultz <ajs42@buffalo.edu>
2018-08-17 16:50:15 -0400

    Actually add the Function to handle uniform mapping

---
+++ etomica-apps/src/main/java/etomica/mappedDensity/FunctionUniform.java
@@ -0,0 +1,29 @@
+package etomica.mappedDensity;
+
+import etomica.math.function.FunctionDifferentiable;
+
+public class FunctionUniform implements FunctionDifferentiable {
+
+    private final double L;
+
+    public FunctionUniform(double L) {
+        this.L = L;
+    }
+
+    @Override
+    public double df(int nder, double x) {
+        switch (nder) {
+            case 0:
+                return x + L / 2;
+            case 1:
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public double f(double x) {
+        return df(0, x);
+    }
+}
