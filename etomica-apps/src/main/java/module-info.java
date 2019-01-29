a2590aa9156b6b44f2c35d35adaed1a8f48e4f5a
Alex Kofke <akofke@gmail.com>
2019-01-28 23:46:58 -0500

    Don't need java.net.http

--- etomica-apps/src/main/java/module-info.java
+++ etomica-apps/src/main/java/module-info.java
@@ -20,7 +20,6 @@ module etomica.apps {
     requires etomica.core;
     requires etomica.graph;
     requires java.desktop;
-    requires java.net.http;
 //    requires jackson.databind;
     requires Jama;
 }
