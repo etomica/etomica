8d1b0b0253157d2a6fe57eb8af648e0fa2c6ae95
Alex Kofke <akofke@gmail.com>
2020-05-27 14:21:52 -0400

    Uninline gradient in gradient-with-pressure but still check if it's zero

--- etomica-core/src/main/java/etomica/potential/Potential2SoftSpherical.java
+++ etomica-core/src/main/java/etomica/potential/Potential2SoftSpherical.java
@@ -99,18 +99,10 @@ public abstract class Potential2SoftSpherical extends Potential2 implements Pote
     }
     
     public Vector[] gradient(IAtomList atoms, Tensor pressureTensor) {
-        dr.Ev1Mv2(atoms.get(1).getPosition(),atoms.get(0).getPosition());
-        boundary.nearestImage(dr);
-        double r2 = dr.squared();
-        double du = du(r2);
-        if (du == 0) {
-            gradient[0].E(0.0);
-            gradient[1].E(0.0);
-            return gradient;
+        gradient(atoms);
+        if (!gradient[0].isZero()) {
+            pressureTensor.PEv1v2(gradient[0],dr);
         }
-        gradient[1].Ea1Tv1(du/r2,dr);
-        gradient[0].Ea1Tv1(-1,gradient[1]);
-        pressureTensor.PEv1v2(gradient[0], dr);
         return gradient;
     }
     
