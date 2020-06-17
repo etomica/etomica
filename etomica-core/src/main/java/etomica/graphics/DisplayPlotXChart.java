bf20ba8bfd1e94a5a93941c9ece752002c556e9c
Alex Kofke <akofke@gmail.com>
2020-06-17 14:16:34 -0400

    More XChart DisplayPlot features and bug fixes
    Trim NaNs from head and tail of the series but not in the middle, this allows
    line plot discontinuities where NaNs are present. Implement zooming with a LayerUI.

--- etomica-core/src/main/java/etomica/graphics/DisplayPlotXChart.java
+++ etomica-core/src/main/java/etomica/graphics/DisplayPlotXChart.java
@@ -3,17 +3,17 @@ package etomica.graphics;
 import etomica.data.*;
 import etomica.data.types.DataFunction;
 import etomica.units.Unit;
-import etomica.units.dimensions.Null;
 import etomica.units.systems.UnitSystem;
 import org.knowm.xchart.*;
-import org.knowm.xchart.internal.chartpart.SelectionZoom;
 
 import javax.swing.*;
+import javax.swing.plaf.LayerUI;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class DisplayPlotXChart extends Display implements DataSetListener {
     private final DataSet dataSet;
@@ -27,6 +27,9 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
     private final List<DataTagBag> unitList = new ArrayList<>();
     private final List<DataTagBag> drawLineList = new ArrayList<>();
 
+    private final ZoomUI zoomUI = new ZoomUI();
+    private final JLayer<XChartPanel<XYChart>> layer;
+
     public DisplayPlotXChart() {
         this(new DataSet());
     }
@@ -38,10 +41,15 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
                 .build();
         this.panel = new XChartPanel<>(this.plot);
         this.unitMap = new HashMap<>();
+        ZoomMouseAdapter zoomAdapter = new ZoomMouseAdapter();
+        this.panel.addMouseListener(zoomAdapter);
+        this.panel.addMouseMotionListener(zoomAdapter);
+
+        this.layer = new JLayer<>(this.panel, this.zoomUI);
     }
 
     public Component graphic() {
-        return this.panel;
+        return this.layer;
     }
 
     public DataSet getDataSet() {
@@ -51,7 +59,7 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
     public IDataSink makeSink(String name) {
         IDataSink sink = this.dataSet.makeDataSink(name);
         XYSeries series = this.plot.addSeries(name, new double[]{Double.NaN}, new double[]{Double.NaN});
-        this.plot.updateXYSeries(name, new double[0], new double[0], null);
+        series.setEnabled(false);
         return sink;
     }
 
@@ -100,6 +108,14 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
         this.plot.getStyler().setYAxisLogarithmic(log);
     }
 
+    public void setXRange(double min, double max) {
+        this.plot.getStyler().setXAxisMin(min).setXAxisMax(max);
+    }
+
+    public void setYRange(double min, double max) {
+        this.plot.getStyler().setYAxisMin(min).setYAxisMax(max);
+    }
+
     public void setDoLegend(boolean legend) {
         this.plot.getStyler().setLegendVisible(legend);
 
@@ -129,8 +145,12 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
         return this.plot;
     }
 
-    public XChartPanel<XYChart> getPanel() {
-        return this.panel;
+    public JComponent getPanel() {
+        return this.layer;
+    }
+
+    public void setSize(int w, int h) {
+        this.panel.setSize(w, h);
     }
 
     public DisplayPlotXChart getPlot() {
@@ -186,6 +206,12 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
     private void update() {
         int nSeries = this.dataSet.getDataCount();
 
+        ArrayList<Double>[] newXValues = new ArrayList[nSeries];
+        ArrayList<Double>[] newYValues = new ArrayList[nSeries];
+
+        boolean xAxisLog = this.plot.getStyler().isXAxisLogarithmic();
+        boolean yAxisLog = this.plot.getStyler().isYAxisLogarithmic();
+
         for (int i = 0; i < nSeries; i++) {
             if (this.dataSet.getDataInfo(i) instanceof DataFunction.DataInfoFunction) {
                 String seriesName = this.dataSet.getName(i);
@@ -198,28 +224,183 @@ public class DisplayPlotXChart extends Display implements DataSetListener {
 
                 ArrayList<Double> filteredData = new ArrayList<>(data.length);
                 ArrayList<Double> filteredXValues = new ArrayList<>(xValues.length);
-                IntStream.range(0, xValues.length)
-                        .filter(idx -> {
-                            boolean xLogBad = this.plot.getStyler().isXAxisLogarithmic() && xValues[idx] <= 0;
-                            boolean yLogBad = this.plot.getStyler().isYAxisLogarithmic() && data[idx] <= 0;
-                            return !Double.isNaN(yUnit.fromSim(data[idx])) && !xLogBad && !yLogBad;
-                        })
-                        .forEach(idx -> {
-                            filteredData.add(yUnit.fromSim(data[idx]));
-                            filteredXValues.add(xUnit.fromSim(xValues[idx]));
-                        });
+                int leadingNans = 0;
+                boolean leadingNansDone = false;
+                int trailingNans = 0;
+                for (int j = 0; j < xValues.length; j++) {
+                    double x = xValues[j];
+                    double y = data[j];
 
+                    boolean badValue = Double.isNaN(x) || Double.isNaN(y) || (xAxisLog && x <= 0) || (yAxisLog && y <= 0);
+                    if (!leadingNansDone && badValue) {
+                        leadingNans++;
+                    } else {
+                        leadingNansDone = true;
+                    }
 
+                    if (badValue) {
+                        trailingNans++;
+                    } else {
+                        trailingNans = 0;
+                    }
+                }
+                for (int j = leadingNans; j < xValues.length - trailingNans; j++) {
+                    double x = xUnit.fromSim(xValues[j]);
+                    double y = yUnit.fromSim(data[j]);
 
-                SwingUtilities.invokeLater(() -> {
-                    this.plot.getSeriesMap().get(seriesName).setEnabled(!filteredData.isEmpty());
-                    this.plot.updateXYSeries(seriesName, filteredXValues, filteredData, null);
-                });
+                    if (xAxisLog && x <= 0) {
+                        x = Double.NaN;
+                    }
+                    if (yAxisLog && y <= 0) {
+                        y = Double.NaN;
+                    }
+                    filteredXValues.add(x);
+                    filteredData.add(y);
+                }
+
+                newXValues[i] = filteredXValues;
+                newYValues[i] = filteredData;
             }
         }
-        if (this.panel.isShowing()) {
-            this.panel.revalidate();
-            this.panel.repaint();
+        SwingUtilities.invokeLater(() -> {
+            for (int i = 0; i < nSeries; i++) {
+                String name = dataSet.getName(i);
+                this.plot.getSeriesMap().get(name).setEnabled(!newYValues[i].isEmpty());
+                this.plot.updateXYSeries(name, newXValues[i], newYValues[i], null);
+            }
+            if (this.layer.isShowing()) {
+                this.layer.revalidate();
+                this.layer.repaint();
+            }
+        });
+    }
+
+    private static class ZoomUI extends LayerUI<XChartPanel<XYChart>> {
+        private final Rectangle2D rect = new Rectangle2D.Double(0, 0, 0, 0);
+        private boolean shown = false;
+
+        @Override
+        public void paint(Graphics g, JComponent c) {
+            super.paint(g, c);
+
+            if (shown) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.draw(this.rect);
+            }
+        }
+
+        /**
+         * Sets the dimensions of the rectangle to draw
+         *
+         * @param x x coordinate of the upper left hand corner (x grows to the right)
+         * @param y y coordinate of the upper left hand corner (y grows down)
+         * @param w the width of the rectangle
+         * @param h the height of the rectangle
+         */
+        public void setRect(int x, int y, int w, int h) {
+            rect.setRect(x, y, w, h);
+        }
+
+        /**
+         * Sets whether the zoom indicator should be shown.
+         * @param b true to show, false to hide.
+         */
+        public void setRectShown(boolean b) {
+            shown = b;
+        }
+
+        /**
+         * Reset the rectangle coordinates to all 0. Should
+         * usually be called right after the rectangle is hidden
+         * to prevent a flash of the old dimensions once it is shown
+         * again.
+         */
+        public void resetRect() {
+            rect.setRect(0, 0, 0, 0);
+        }
+    }
+
+    private class ZoomMouseAdapter extends MouseAdapter {
+        private int pressedX;
+        private int pressedY;
+        private int currentX;
+        private int currentY;
+        private boolean drawingRect = false;
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            super.mouseClicked(e);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                return;
+            }
+            this.pressedX = e.getX();
+            this.pressedY = e.getY();
+
+            zoomUI.setRect(pressedX, pressedY, 0, 0);
+            zoomUI.setRectShown(true);
+            this.drawingRect = true;
+            layer.repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if (e.isPopupTrigger() || !drawingRect) {
+                return;
+            }
+
+            zoomUI.setRectShown(false);
+            this.drawingRect = false;
+
+            if (zoomUI.rect.getWidth() == 0 || zoomUI.rect.getHeight() == 0) {
+                layer.repaint();
+                zoomUI.resetRect();
+                return;
+            }
+            zoomUI.resetRect();
+            int xMin = Math.min(pressedX, currentX);
+            int xMax = Math.max(pressedX, currentX);
+
+            int yTop = Math.min(pressedY, currentY);
+            int yBottom = Math.max(pressedY, currentY);
+
+            double newXMin = plot.getChartXFromCoordinate(xMin);
+            double newXMax = plot.getChartXFromCoordinate(xMax);
+
+            double newYMin = plot.getChartYFromCoordinate(yBottom);
+            double newYMax = plot.getChartYFromCoordinate(yTop);
+
+            plot.getStyler().setXAxisMin(newXMin);
+            plot.getStyler().setXAxisMax(newXMax);
+
+            plot.getStyler().setYAxisMin(newYMin);
+            plot.getStyler().setYAxisMax(newYMax);
+
+            layer.repaint();
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            if (!drawingRect) {
+                return;
+            }
+            currentX = e.getX();
+            currentY = e.getY();
+
+            int xDist = Math.abs(currentX - pressedX);
+            int yDist = Math.abs(currentY - pressedY);
+
+
+            zoomUI.setRect(
+                    Math.min(pressedX, currentX),
+                    Math.min(pressedY, currentY),
+                    xDist,
+                    yDist
+            );
+            layer.repaint();
         }
     }
 }
