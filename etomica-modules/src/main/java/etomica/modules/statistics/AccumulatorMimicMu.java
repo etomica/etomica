8bd126f6ff758151f08d025fcdfa094b86890b91
Andrew J. Schultz <ajs42@buffalo.edu>
2020-07-28 18:15:30 -0400

    make our own data so we don't mess with accumulator's data

--- etomica-modules/src/main/java/etomica/modules/statistics/AccumulatorMimicMu.java
+++ etomica-modules/src/main/java/etomica/modules/statistics/AccumulatorMimicMu.java
@@ -14,6 +14,7 @@ import etomica.integrator.IntegratorBox;
 public class AccumulatorMimicMu extends AccumulatorAverageFixed {
 
     protected final IntegratorBox integrator;
+    protected DataGroup data;
 
     public AccumulatorMimicMu(IntegratorBox integrator) {
         super();
@@ -22,17 +23,21 @@ public class AccumulatorMimicMu extends AccumulatorAverageFixed {
 
     public void putDataInfo(IDataInfo dataInfo) {
         dataSink.putDataInfo(dataInfo);
+        data = (DataGroup) dataInfo.makeData();
     }
 
-    public IData processData(IData data) {
+    public IData processData(IData inputData) {
         double temperature = integrator.getTemperature();
-        DataGroup g = (DataGroup) data;
+        DataGroup g = (DataGroup) inputData;
         DataDouble avg = (DataDouble) g.getData(AVERAGE.index);
         DataDouble err = (DataDouble) g.getData(ERROR.index);
         DataDouble stdev = (DataDouble) g.getData(STANDARD_DEVIATION.index);
-        err.x = temperature * err.x / avg.x;
-        stdev.x = temperature * stdev.x / avg.x;
-        avg.x = -temperature * Math.log(avg.x);
+        DataDouble avgOut = (DataDouble) data.getData(AVERAGE.index);
+        DataDouble errOut = (DataDouble) data.getData(ERROR.index);
+        DataDouble stdevOut = (DataDouble) data.getData(STANDARD_DEVIATION.index);
+        errOut.x = temperature * err.x / avg.x;
+        stdevOut.x = temperature * stdev.x / avg.x;
+        avgOut.x = -temperature * Math.log(avg.x);
         return data;
     }
 }
