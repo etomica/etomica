510f62588acfaf896ab984a4ffd50161b7a76d22
Alex Kofke <akofke@gmail.com>
2020-07-28 16:48:18 -0400

    Clean up AboutBoxWindow a bit
    - Use JTextArea to make text selectable and simplify building the
    components.
    - Make website link into a clickable URL
    - Allow the look and feel to set the font to get nice OS-defaults.
    - Use MigLayout to remove GridBag boilerplate.
    - Don't hard-code size.

--- etomica-core/src/main/java/etomica/graphics/AboutBoxWindow.java
+++ etomica-core/src/main/java/etomica/graphics/AboutBoxWindow.java
@@ -5,25 +5,18 @@
 package etomica.graphics;
 
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
+import net.miginfocom.swing.MigLayout;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.MatteBorder;
+import javax.swing.event.HyperlinkEvent;
 
 
 public class AboutBoxWindow extends JDialog {
@@ -42,17 +35,8 @@ public class AboutBoxWindow extends JDialog {
     private static final String NSF_TEXT = "Funding support provided by the National Science Foundation.";
     private static final String WWW_TEXT = "http://www.etomica.org";
 
-	private static final int GRAPHIC_WIDTH    = 200;
-	private static final int GRAPHIC_HEIGHT   = 40;
-	private static final int GRAPHIC_X        = 30;
-    private static final int GRAPHIC_Y        = 5;
-
-    private static final int ABOUT_WIDTH      = 400;
-    private static final int ABOUT_HEIGHT     = 340;
     private static final int ABOUT_X          = 10;
     private static final int ABOUT_Y          = 10;
-
-    private static final int CREDIT_FONT_SIZE       = 12;
 
 	public AboutBoxWindow(Component owner, String appName) {
 
@@ -80,137 +64,102 @@ public class AboutBoxWindow extends JDialog {
 
 	private void createGUI() {
 
-		this.getContentPane().setLayout(new GridBagLayout());
+		this.getContentPane().setLayout(new MigLayout("flowy, insets dialog", "[align center]"));
 		this.setResizable(false);
 		this.setLocation(ABOUT_X, ABOUT_Y);
 		this.setModal(true);
 
-		GridBagConstraints gbc = new GridBagConstraints();
-
-		Font creditFont = new Font("", Font.PLAIN, CREDIT_FONT_SIZE);
-
-    	setSize(ABOUT_WIDTH, ABOUT_HEIGHT);
     	getContentPane().setBackground(Color.WHITE);
 
-		JLabel[] creditLabels = null;
+		getContentPane().add(etomicaGraphic);
 
-		gbc.gridx = 0; gbc.gridy = 0;
-		getContentPane().add(etomicaGraphic, gbc);
-		gbc.gridy++;
-
-	    gbc.insets = new Insets(10, 0, 10, 0);
+		// Add link to homepage
+		JEditorPane wwwLabel = new JEditorPane();
+		wwwLabel.setEditable(false);
+		wwwLabel.setBackground(null);
+		wwwLabel.setContentType("text/html");
+		wwwLabel.setText("<a href=\"" + WWW_TEXT + "\">" + WWW_TEXT + "</a>");
+		wwwLabel.addHyperlinkListener(e -> {
+			if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+				if (e.getURL() != null) {
+					try {
+						Desktop.getDesktop().browse(e.getURL().toURI());
+					} catch (IOException | URISyntaxException ioException) {
+						ioException.printStackTrace();
+					}
+				}
+			}
+		});
+		getContentPane().add(wwwLabel);
 
 		// Add personal acknowledgements
 		if(creationCredits != null || softwareCredits != null) {
-		    JPanel acknowledgementPanel = new JPanel();			
+		    JPanel acknowledgementPanel = new JPanel(new MigLayout("gapx 30px"));
 		    acknowledgementPanel.setBackground(Color.WHITE);
 
 			// Add Created By Credits
 			if (creationCredits != null) {
+                JTextArea textArea = makeTextArea();
+                textArea.append("Created By:");
 
-		    	JPanel creditPanel = new JPanel();
-		    	 
-		    	creditPanel.setBorder(new MatteBorder(0, 0, 0, 15, Color.WHITE));
-		    	creditPanel.setBackground(Color.WHITE);
-		    	creditPanel.setLayout(new GridLayout(creationCredits.length+1, 1));
-
-		    	JLabel cb = new JLabel("Created By : ");
-		    	cb.setFont(creditFont);
-		    	creditPanel.add(cb);
-
-		    	creditLabels = new JLabel[creationCredits.length];
-
-		    	for(int i = 0; i < creationCredits.length; i++) {
-		    		creditLabels[i] = new JLabel(creationCredits[i]);
-		    	}
-
-		    	for(int i = 0; i < creditLabels.length; i++) {
-		    		creditLabels[i].setFont(creditFont);
-		    		creditPanel.add(creditLabels[i]);
-		    	}
-		    	acknowledgementPanel.add(creditPanel);
+				for (String creationCredit : creationCredits) {
+					textArea.append("\n" + creationCredit);
+				}
+		    	acknowledgementPanel.add(textArea);
 		    }
-
-		    creditLabels = null;
 
 		    // Add Software Support Credits
 		    if (softwareCredits != null) {
+		    	JTextArea textArea = makeTextArea();
+		    	textArea.append("Software Support:");
 
-		    	JPanel softwareCreditPanel = new JPanel();
-		    	softwareCreditPanel.setBackground(Color.WHITE);
-		    	softwareCreditPanel.setBorder(new MatteBorder(0, 15, 0, 0, Color.WHITE));
-		    	softwareCreditPanel.setLayout(new GridLayout(softwareCredits.length+1, 1));
-
-		    	creditLabels = new JLabel[softwareCredits.length];
-
-		    	for(int i = 0; i < softwareCredits.length; i++) {
-		    		creditLabels[i] = new JLabel(softwareCredits[i]);
-		    	}
-
-		    	JLabel ss = new JLabel("Software Support :");
-		    	ss.setFont(creditFont);
-		    	softwareCreditPanel.add(ss);
-
-		    	for(int i = 0; i < creditLabels.length; i++) {
-		    		creditLabels[i].setFont(creditFont);
-		    		softwareCreditPanel.add(creditLabels[i]);
-		    	}
-		    	acknowledgementPanel.add(softwareCreditPanel);
+				for (String softwareCredit : softwareCredits) {
+                    textArea.append("\n" + softwareCredit);
+				}
+		    	acknowledgementPanel.add(textArea);
 		    }
 
-		    getContentPane().add(acknowledgementPanel, gbc);
-			gbc.gridy++;
+		    getContentPane().add(acknowledgementPanel);
 		}
 
-		// Add SUNY and DE
-        JPanel sunyPanel = new JPanel();
-        sunyPanel.setBackground(Color.WHITE);
-    	sunyPanel.setLayout(new GridLayout(2, 0));
-        
-        JLabel sunyLabel = new JLabel(SUNY_TEXT);
-    	sunyLabel.setFont(creditFont);
-    	sunyPanel.add(sunyLabel);
-        JLabel ceLabel = new JLabel(CE_TEXT);
-    	ceLabel.setFont(creditFont);
-    	sunyPanel.add(ceLabel);
-        
-        getContentPane().add(sunyPanel, gbc);
-		gbc.gridy++;
+		// Add SUNY and CE
+        JTextArea sunyArea = makeTextArea();
+		sunyArea.append(SUNY_TEXT + "\n");
+		sunyArea.append(CE_TEXT);
+        getContentPane().add(sunyArea);
 
 		// Add NSF
-        JLabel nsfLabel = new JLabel(NSF_TEXT);
-        nsfLabel.setFont(creditFont);
-    	getContentPane().add(nsfLabel, gbc);
-		gbc.gridy++;
-
-		// Add link to homepage
-		// For now, this is just text.  Not sure how
-		// to add a link in any java version except for 6.
-		// In java 1.6, can use the java.awt.Desktop, but that
-		// is not available before java 6.
-        JLabel wwwLabel = new JLabel(WWW_TEXT);
-        wwwLabel.setFont(creditFont);
-    	getContentPane().add(wwwLabel, gbc);
-		gbc.gridy++;
+        JTextArea nsfArea = makeTextArea();
+        nsfArea.append(NSF_TEXT);
+    	getContentPane().add(nsfArea);
 
 	   	// Add Close button
 		JButton closeBtn = new JButton("OK");
 		closeBtn.addActionListener(event -> this.setVisible(false));
 		closeBtn.setBackground(Color.LIGHT_GRAY);
-    	getContentPane().add(closeBtn, gbc);
-		gbc.gridy++;
+    	getContentPane().add(closeBtn, "gaptop 15px");
 	}
 
 	public void setVisible(boolean b) {
-        this.setLocation(owner.getLocationOnScreen().x + 10,
-        		         owner.getLocationOnScreen().y + 10);
+	    this.pack();
+	    // prevent jumping to new location on close
+	    if (b) {
+			this.setLocation(owner.getLocationOnScreen().x + 10,
+					owner.getLocationOnScreen().y + 10);
+		}
 		super.setVisible(b);
+	}
+
+	private static JTextArea makeTextArea() {
+		JTextArea f = new JTextArea();
+		f.setEditable(false);
+		f.setBorder(null);
+		f.setBackground(null);
+		return f;
 	}
 
 	private static JPanel makeEtomicaGraphic() {
 		JPanel panel = new JPanel();
-		panel.setSize(new Dimension(GRAPHIC_WIDTH, GRAPHIC_HEIGHT));
-		panel.setLocation(GRAPHIC_X, GRAPHIC_Y);
 		panel.setBackground(Color.WHITE);
 
 		final Icon etomicaImage = new ImageIcon(AboutBoxWindow.class.getResource(ETOMICA_IMAGE));
