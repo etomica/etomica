de758af42fd59ab99583a0b33f6f915046eb8ba9
David Kofke <kofke@buffalo.edu>
2020-07-29 01:11:34 -0400

    Basic functionality of self-assembly module is in place.
    Further work needed on fixing bug with epsilon units, adding capability to adjust sigmas, lambdas, and number of segments in polymer.
    
    P2Tether is given infinite interaction range as kluge to get intramolecular potential to work.

--- etomica-modules/src/main/java/etomica/modules/selfassembly/SelfAssemblySim.java
+++ etomica-modules/src/main/java/etomica/modules/selfassembly/SelfAssemblySim.java
@@ -7,32 +7,32 @@ package etomica.modules.selfassembly;
 import etomica.action.activity.ActivityIntegrate;
 import etomica.action.controller.Controller;
 import etomica.atom.AtomLeafAgentManager;
-import etomica.atom.AtomLeafAgentManager.AgentSource;
 import etomica.atom.AtomType;
 import etomica.atom.IAtom;
-import etomica.atom.IAtomList;
+import etomica.atom.iterator.ApiBuilder;
 import etomica.box.Box;
+import etomica.config.ConformationLinear;
 import etomica.data.meter.MeterTemperature;
 import etomica.integrator.IntegratorHard;
 import etomica.integrator.IntegratorMD.ThermostatType;
 import etomica.lattice.LatticeCubicFcc;
 import etomica.lattice.LatticeOrthorhombicHexagonal;
-import etomica.molecule.IMolecule;
+import etomica.nbr.CriterionAll;
+import etomica.nbr.CriterionBondedSimple;
+import etomica.nbr.CriterionInterMolecular;
 import etomica.nbr.list.PotentialMasterList;
-import etomica.potential.P2HardSphere;
-import etomica.potential.P2SquareWell;
-import etomica.potential.P2Tether;
-import etomica.potential.PotentialMaster;
+import etomica.potential.*;
 import etomica.simulation.Simulation;
 import etomica.space.BoundaryRectangularPeriodic;
 import etomica.space.Space;
+import etomica.species.ISpecies;
 import etomica.species.SpeciesSpheresHetero;
 import etomica.species.SpeciesSpheresMono;
 import etomica.units.Kelvin;
 
 public class SelfAssemblySim extends Simulation {
 
-    public final PotentialMaster potentialMaster;
+    public final PotentialMasterList potentialMaster;
     public final ConfigurationLatticeRandom config;
     public Controller controller1;
 	public IntegratorHard integratorHard;
@@ -42,7 +42,7 @@ public class SelfAssemblySim extends Simulation {
 	public SpeciesSpheresMono speciesA;
 	public SpeciesSpheresHetero speciesB;
 	public final P2SquareWell p2AA, p2AB1, p2AB2, p2B1B1, p2B1B2, p2B2B2;
-	public final P2Tether ABbonded;
+	public final P2Tether p2Bond;
     public ActivityIntegrate activityIntegrate;
     public AtomLeafAgentManager<IAtom[]> agentManager = null;
     public int nA, nB;
@@ -57,9 +57,17 @@ public class SelfAssemblySim extends Simulation {
     public SelfAssemblySim(Space space) {
         super(space);
 
+        nA = 1000;
+        nB = 10;
+        nB1 = 20;
+        nB2 = 2;
+
         speciesA = new SpeciesSpheresMono(space, typeA);
         speciesA.setIsDynamic(true);
         speciesB = new SpeciesSpheresHetero(space, new AtomType[] {typeB1, typeB2});
+        speciesB.setConformation(new ConformationLinear(space,0.05));
+        speciesB.setChildCount(new int[] {nB1, nB2});
+
         speciesB.setIsDynamic(true);
         addSpecies(speciesA);
         addSpecies(speciesB);
@@ -69,17 +77,29 @@ public class SelfAssemblySim extends Simulation {
 
         controller1 = getController();
 
-        sigAA = sigAB1 = sigAB2 = sigB1B1 = sigB1B2 = sigB2B2 = 1.0;
-        epsAA = epsAB1 = epsAB2 = epsB1B1 =  epsB1B2 = epsB2B2 = 1.0;
-        lamAA = lamAB1 = lamAB2 = lamB1B1 =  lamB1B2 = lamB2B2 = 2.0;
+        sigAA = 1.0;
+        epsAA = 1.0;
+
+        sigAB1 = 1.0;
+        epsAB1 = 1.0;
+
+        sigAB2 = 1.0;
+        epsAB2 = 1.0;
+
+        sigB1B1 = 1.0;
+        epsB1B1 = 1.0;
+
+        sigB1B2 = 1.5;
+        epsB1B2 = 0.01;
+
+        sigB2B2 = 1.0;
+        epsB2B2 = 1.0;
+
+        lamAA = lamAB1 = lamAB2 = lamB1B1 = lamB1B2 = lamB2B2 = 2.0;
 
         box = this.makeBox(new BoundaryRectangularPeriodic(space, space.D() == 2 ? 60 : 20));
-        box.setNMolecules(speciesA, 50);
-        nA = 100;
-        box.setNMolecules(speciesB, 100);
-        nB = 10;
-        nB1 = 20;
-        nB2 = 20;
+        box.setNMolecules(speciesA, nA);
+        box.setNMolecules(speciesB, nB);
         config = new ConfigurationLatticeRandom(space.D() == 2 ? new LatticeOrthorhombicHexagonal(space) : new LatticeCubicFcc(space), space, random);
         config.initializeCoordinates(box);
 
@@ -92,13 +112,18 @@ public class SelfAssemblySim extends Simulation {
         integratorHard.getEventManager().addListener(((PotentialMasterList) potentialMaster).getNeighborManager(box));
 
         //potentials
-        p2AA = new P2SquareWell(space, sigAA, epsAA, lamAA, true);
-        p2AB1 = new P2SquareWell(space, sigAB1, epsAB1, lamAB1, true);
-        p2AB2 = new P2SquareWell(space, sigAB2, epsAB2, lamAB2, true);
-        p2B1B1 = new P2SquareWell(space, sigB1B1, epsB1B1, lamB1B1, true);
-        p2B1B2 = new P2SquareWell(space, sigB1B2, epsB1B2, lamB1B2, true);
-        p2B2B2 = new P2SquareWell(space, sigB2B2, epsB2B2, lamB2B2, true);
-        ABbonded = new P2Tether(space, sigAA * 0.8, true);
+        p2AA = new P2SquareWell(space, sigAA, lamAA, epsAA, true);
+        p2AB1 = new P2SquareWell(space, sigAB1, lamAB1, epsAB1, true);
+        p2AB2 = new P2SquareWell(space, sigAB2, lamAB2, epsAB2, true);
+        p2B1B1 = new P2SquareWell(space, sigB1B1, lamB1B1, epsB1B1, true);
+        p2B1B2 = new P2SquareWell(space, sigB1B2, lamB1B2, epsB1B2, true);
+        p2B2B2 = new P2SquareWell(space, sigB2B2, lamB2B2, epsB2B2, true);
+        p2Bond = new P2Tether(space, sigAA * 0.8, true);
+
+       // p2Bond = new P2HardBond(space, 0.8, 0.2, true);
+        PotentialGroup p1Surfactant = potentialMaster.makePotentialGroup(1);
+        p1Surfactant.addPotential(p2Bond, ApiBuilder.makeAdjacentPairIterator());
+        potentialMaster.addPotential(p1Surfactant, new ISpecies[]{speciesB});
 
         potentialMaster.addPotential(p2AA, new AtomType[]{typeA, typeA});
         potentialMaster.addPotential(p2AB1, new AtomType[]{typeA, typeB1});
@@ -106,6 +131,22 @@ public class SelfAssemblySim extends Simulation {
         potentialMaster.addPotential(p2B1B2, new AtomType[]{typeB1, typeB2});
         potentialMaster.addPotential(p2B1B1, new AtomType[]{typeB1, typeB1});
         potentialMaster.addPotential(p2B2B2, new AtomType[]{typeB2, typeB2});
+
+        CriterionInterMolecular sqwCriterion = (CriterionInterMolecular) potentialMaster.getCriterion(typeB1, typeB1);
+        CriterionBondedSimple nonBondedCriterion = new CriterionBondedSimple(new CriterionAll());
+        nonBondedCriterion.setBonded(false);
+        sqwCriterion.setIntraMolecularCriterion(nonBondedCriterion);
+
+        sqwCriterion = (CriterionInterMolecular) potentialMaster.getCriterion(typeB1, typeB2);
+        nonBondedCriterion = new CriterionBondedSimple(new CriterionAll());
+        nonBondedCriterion.setBonded(false);
+        sqwCriterion.setIntraMolecularCriterion(nonBondedCriterion);
+
+        sqwCriterion = (CriterionInterMolecular) potentialMaster.getCriterion(typeB2, typeB2);
+        nonBondedCriterion = new CriterionBondedSimple(new CriterionAll());
+        nonBondedCriterion.setBonded(false);
+        sqwCriterion.setIntraMolecularCriterion(nonBondedCriterion);
+
 
         // **** Setting Up the thermometer Meter *****
 
