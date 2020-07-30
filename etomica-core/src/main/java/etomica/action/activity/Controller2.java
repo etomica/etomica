1fa63be13325627a569b432be2b1392906cf83aa
Alex Kofke <akofke@gmail.com>
2020-07-30 12:57:10 -0400

    More adjustments to new Controller

--- etomica-core/src/main/java/etomica/action/activity/Controller2.java
+++ etomica-core/src/main/java/etomica/action/activity/Controller2.java
@@ -6,37 +6,141 @@ import etomica.util.Debug;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Controller2 {
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private ActivityTask currentTask;
-    private final ArrayDeque<ActivityTask> pendingActivities = new ArrayDeque<>(1);
+    private final ArrayDeque<ActivityHandle> pendingActivities = new ArrayDeque<>(1);
+    private final LinkedBlockingQueue<IAction> actionQueue = new LinkedBlockingQueue<>();
+    private boolean started = false;
+    private final Object lock = new Object();
+
+    private final AtomicBoolean pauseFlag = new AtomicBoolean(true);
+    private static final IAction WAKE_UP = () -> {};
+
 
     public Controller2() {
     }
 
-    public void setActivity(Activity2 activity, long maxSteps, double sleepPeriodMillis) {
-        ActivityTask task = new ActivityTask(activity, new LinkedBlockingQueue<>());
+    /**
+     * Adds an Activity to be executed by the Controller on a separate thread. Activities will be run in the
+     * order they are added, with the next one starting after the current one completes. The returned
+     * `CompletableFuture` will be completed when the activity finishes.
+     * @param activity the repeated action.
+     * @param maxSteps
+     * @param sleepPeriodMillis
+     */
+    public ActivityHandle addActivity(Activity2 activity, long maxSteps, double sleepPeriodMillis) {
+        ActivityTask task = new ActivityTask(activity, this.actionQueue, this.pauseFlag);
         task.maxSteps = maxSteps;
-        this.currentTask = task;
-        this.currentTask.sleepPeriodMillis = sleepPeriodMillis;
+        task.sleepPeriodMillis = sleepPeriodMillis;
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        ActivityHandle activityHandle = new ActivityHandle(future, task);
+        synchronized (this.lock) {
+            this.pendingActivities.add(activityHandle);
+            this.processActivities();
+        }
+        return activityHandle;
+    }
+
+    /**
+     * Runs an activity to completion on the current thread.
+     * @param activity
+     * @param maxSteps
+     */
+    public void runActivityBlocking(Activity2 activity, long maxSteps) {
+        ActivityTask task = new ActivityTask(activity, this.actionQueue, this.pauseFlag);
+        task.maxSteps = maxSteps;
+        task.run();
     }
 
     public void start() {
-        executor.submit(this.currentTask);
+        synchronized (this.lock) {
+            this.started = true;
+            this.processActivities();
+        }
     }
 
-    public void pause() {
-        this.currentTask.actionQueue.add(this.currentTask.pauseAction);
+    private void processActivities() {
+        if (this.started) {
+            for (ActivityHandle activity : pendingActivities) {
+                this.executor.submit(() -> {
+                    this.currentTask = activity.task;
+                    try {
+                        activity.task.run();
+                        activity.future.complete(null);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                        activity.future.completeExceptionally(e);
+                    }
+                });
+            }
+            this.pendingActivities.clear();
+        }
+
     }
 
+    public CompletableFuture<Void> pause() {
+        this.pauseFlag.set(true);
+        return submitActionInterrupt(WAKE_UP);
+    }
 
-    public void submitAction(IAction action) {
-        this.currentTask.submitAction(action);
+    public CompletableFuture<Void> unpause() {
+        this.pauseFlag.set(false);
+        return submitActionInterrupt(WAKE_UP);
+    }
+
+    public CompletableFuture<Void> toggle() {
+        this.pauseFlag.set(!this.pauseFlag.get());
+        return submitActionInterrupt(WAKE_UP);
+    }
+
+    public boolean isPaused() {
+        return this.pauseFlag.get();
+    }
+
+    public CompletableFuture<Void> submitActionInterrupt(IAction action) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        this.actionQueue.add(() -> {
+            if (future.isCancelled()) {
+                System.out.println("canceled");
+                return;
+            }
+            try {
+                action.actionPerformed();
+                future.complete(null);
+            } catch (Throwable ex) {
+                future.completeExceptionally(ex);
+            }
+            System.out.println(action);
+        });
+        return future;
+    }
+
+    public CompletableFuture<Void> addActionSequential(IAction action) {
+        Activity2 activity = new Activity2() {
+            @Override
+            public void preAction() {
+
+            }
+
+            @Override
+            public void postAction() {
+
+            }
+
+            @Override
+            public void actionPerformed() {
+                action.actionPerformed();
+            }
+        };
+        ActivityHandle handle = this.addActivity(activity, 1, 0);
+        return handle.future;
     }
 
     public double getSleepPeriod() {
@@ -55,31 +159,6 @@ public class Controller2 {
         this.currentTask.maxSteps = maxSteps;
     }
 
-    public void addAction(IAction action) {
-        if (action instanceof Activity2) {
-//            this.setActivity();
-        } else {
-            ActivityTask task = new ActivityTask(new Activity2() {
-                @Override
-                public void preAction() {
-
-                }
-
-                @Override
-                public void postAction() {
-
-                }
-
-                @Override
-                public void actionPerformed() {
-                    action.actionPerformed();
-                }
-            }, new LinkedBlockingQueue<>());
-        }
-
-    }
-
-
     private static class ActivityTask implements Runnable {
         final Activity2 activity;
         long maxSteps;
@@ -87,21 +166,15 @@ public class Controller2 {
         final LinkedBlockingQueue<IAction> actionQueue;
 
         private double sleepCarryover = 0.0;
-        private boolean pauseFlag;
         private final List<IAction> actionsToRun = new ArrayList<>();
+        private final AtomicBoolean pauseFlag;
 
-        final IAction pauseAction = () -> pauseFlag = true;
-        final IAction resumeAction = () -> pauseFlag = false;
-        final IAction toggleAction = () -> pauseFlag = !pauseFlag;
-
-        ActivityTask(Activity2 activity, LinkedBlockingQueue<IAction> actionQueue) {
+        ActivityTask(Activity2 activity, LinkedBlockingQueue<IAction> actionQueue, AtomicBoolean pauseFlag) {
             this.activity = activity;
-            this.pauseFlag = false;
+            this.pauseFlag = pauseFlag;
+            this.sleepPeriodMillis = 0.0;
+            this.maxSteps = Long.MAX_VALUE;
             this.actionQueue = actionQueue;
-        }
-
-        public void submitAction(IAction action) {
-            this.actionQueue.add(action);
         }
 
         @Override
@@ -109,7 +182,7 @@ public class Controller2 {
             activity.preAction();
 
             for (long currentStep = 0; currentStep < maxSteps; ) {
-                if (pauseFlag) {
+                if (pauseFlag.get()) {
                     // If the activity is paused, just block waiting for Action events in the queue but don't invoke
                     // the main activity. Since the only way to unpause the activity is to send an "unpause" action,
                     // this effectively suspends the thread until it is resumed while still handling action events.
@@ -128,7 +201,7 @@ public class Controller2 {
                 }
 
                 // An action event may have paused the activity so check again.
-                if (!pauseFlag) {
+                if (!pauseFlag.get()) {
                     if (Debug.ON) {
                         if (currentStep == Debug.START) { Debug.DEBUG_NOW = true; }
                         if (currentStep == Debug.STOP) { break; }
@@ -163,4 +236,37 @@ public class Controller2 {
             }
         }
     }
+
+    public static class ActivityHandle {
+        public final CompletableFuture<Void> future;
+        private final ActivityTask task;
+
+        private ActivityHandle(CompletableFuture<Void> future, ActivityTask task) {
+            this.future = future;
+            this.task = task;
+        }
+
+        public void setMaxSteps(long maxSteps) {
+            this.task.maxSteps = maxSteps;
+        }
+
+        public long getMaxSteps() {
+            return this.task.maxSteps;
+        }
+
+        public void setSleepPeriod(double sleepPeriodMillis) {
+            this.task.sleepPeriodMillis = sleepPeriodMillis;
+        }
+
+        public double getSleepPeriod() {
+            return this.task.sleepPeriodMillis;
+        }
+    }
+
+//    public static class ControllerFuture<T> extends CompletableFuture<T> {
+//        @Override
+//        public <U> CompletableFuture<U> newIncompleteFuture() {
+//            return super.newIncompleteFuture();
+//        }
+//    }
 }
--- etomica-core/src/test/java/etomica/action/activity/ControllerTest.java
+++ etomica-core/src/test/java/etomica/action/activity/ControllerTest.java
@@ -1,11 +1,94 @@
 package etomica.action.activity;
 
+import etomica.action.IAction;
+import etomica.action.activity.Controller2.ActivityHandle;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
-    public void testStuff() {
+    @Test
+    public void testActivityBasic() {
         Controller2 controller = new Controller2();
+
+        TestActivity act = new TestActivity();
+        ActivityHandle handle = controller.addActivity(act, 20, 1);
+
+        assertFalse(act.didPre);
+        assertFalse(act.didPost);
+        assertEquals(0, act.runCount);
+
+        controller.start();
+
+        try {
+            handle.future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e);
+        }
+
+        assertTrue(act.didPre);
+        assertTrue(act.didPost);
+        assertEquals(20, act.runCount);
     }
 
+    @Test
+    void testActions() {
+        Controller2 controller = new Controller2();
+
+        TestActivity act = new TestActivity();
+        act.time = 1;
+        ActivityHandle handle = controller.addActivity(act, 100, 1);
+        List<TestAction> actions = Stream.generate(TestAction::new).limit(50).collect(Collectors.toList());
+        controller.start();
+        List<CompletableFuture<Void>> actionFutures = actions.stream().map(controller::submitActionInterrupt).collect(Collectors.toList());
+        try {
+            handle.future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e);
+        }
+        actions.forEach(a -> assertTrue(a.ran));
+        actionFutures.forEach(f -> f.isDone());
+    }
+
+    static class TestActivity implements Activity2 {
+        boolean didPre = false;
+        boolean didPost = false;
+        int runCount = 0;
+        int time = 0;
+
+        @Override
+        public void preAction() {
+            didPre = true;
+        }
+
+        @Override
+        public void postAction() {
+            didPost = true;
+        }
+
+        @Override
+        public void actionPerformed() {
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            runCount++;
+        }
+    }
+
+    static class TestAction implements IAction {
+        boolean ran = false;
+        @Override
+        public void actionPerformed() {
+            ran = true;
+        }
+    }
 }
