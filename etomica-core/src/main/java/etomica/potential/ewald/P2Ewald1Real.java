c5160acc56ffeadeb46c66041bdc002f8dd271f3
Alex Kofke <akofke@gmail.com>
2020-11-06 19:18:08 -0500

    Add Ewald electrostatic potential

---
+++ etomica-core/src/main/java/etomica/potential/ewald/P2Ewald1Real.java
@@ -0,0 +1,50 @@
+package etomica.potential.ewald;
+
+import etomica.potential.Potential2SoftSpherical;
+import etomica.space.Space;
+import etomica.space3d.Space3D;
+
+import static etomica.math.SpecialFunctions.erfc;
+
+public class P2Ewald1Real extends Potential2SoftSpherical {
+    private final double qiqj;
+    private final double alpha;
+
+    private static final double SQRT_PI = Math.sqrt(Math.PI);
+
+    public P2Ewald1Real(double qiqj, double alpha) {
+        super(Space3D.getInstance());
+        this.qiqj = qiqj;
+        this.alpha = alpha;
+    }
+
+    @Override
+    public double du(double r2) {
+        double r = Math.sqrt(r2);
+        return -qiqj * (2 * SQRT_PI * Math.exp(-alpha*alpha*r2) *alpha + erfc(alpha*r)/r);
+    }
+
+    @Override
+    public void udu(double r2, double[] u, double[] du) {
+        double r = Math.sqrt(r2);
+        u[0] += qiqj*erfc(alpha*r)/r;
+        du[0] += -qiqj* 2 * SQRT_PI * Math.exp(-alpha*alpha*r2) * alpha;
+    }
+
+    @Override
+    public double d2u(double r2) {
+        double r = Math.sqrt(r2);
+        return -qiqj * (2 * SQRT_PI * Math.exp(-alpha*alpha*r2) *(alpha*(1 - alpha*alpha*2*r)) + erfc(alpha*r)/r);
+    }
+
+    @Override
+    public double uInt(double rC) {
+        return 0;
+    }
+
+    @Override
+    public double u(double r2) {
+        double r = Math.sqrt(r2);
+        return qiqj*erfc(alpha*r)/r;
+    }
+}
