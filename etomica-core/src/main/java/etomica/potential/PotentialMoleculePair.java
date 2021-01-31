c6b264a25ca5d5b070303c4b86cbe3f11d76d199
Andrew J. Schultz <ajs42@buffalo.edu>
2021-01-31 15:56:19 -0500

    take a SpeciesManager since that's all we need; handle i-j interactions properly

--- etomica-core/src/main/java/etomica/potential/PotentialMoleculePair.java
+++ etomica-core/src/main/java/etomica/potential/PotentialMoleculePair.java
@@ -9,27 +9,28 @@ import etomica.atom.IAtomList;
 import etomica.box.Box;
 import etomica.molecule.IMolecule;
 import etomica.molecule.IMoleculeList;
-import etomica.simulation.Simulation;
 import etomica.space.Boundary;
 import etomica.space.Space;
 import etomica.space.Vector;
 import etomica.species.ISpecies;
+import etomica.species.SpeciesManager;
 
 public class PotentialMoleculePair implements IPotentialMolecular {
     protected final Space space;
     protected Boundary boundary;
     protected final Potential2Soft[][] atomPotentials;
 
-    public PotentialMoleculePair(Simulation sim) {
-        this.space = sim.getSpace();
+    public PotentialMoleculePair(Space space, SpeciesManager sm) {
+        this.space = space;
         // we could try to store the potentials more compactly, but it doesn't really matter
-        ISpecies species = sim.getSpecies(sim.getSpeciesCount() - 1);
+        ISpecies species = sm.getSpecies(sm.getSpeciesCount() - 1);
         int lastTypeIndex = species.getAtomType(species.getUniqueAtomTypeCount() - 1).getIndex();
         atomPotentials = new Potential2Soft[lastTypeIndex + 1][lastTypeIndex + 1];
     }
 
     public void setAtomPotential(AtomType atomType1, AtomType atomType2, Potential2Soft p2) {
         atomPotentials[atomType1.getIndex()][atomType2.getIndex()] = p2;
+        atomPotentials[atomType2.getIndex()][atomType1.getIndex()] = p2;
     }
 
     @Override
