7d99bf736b64b0ea034eaf4f6ed65f0f2c5fff93
Andrew J. Schultz <ajs42@buffalo.edu>
2021-02-25 13:48:01 -0500

    split out zero-momentuming function as a separate class

--- etomica-core/src/main/java/etomica/integrator/IntegratorMDFasterer.java
+++ etomica-core/src/main/java/etomica/integrator/IntegratorMDFasterer.java
@@ -21,7 +21,6 @@ import etomica.space.Space;
 import etomica.space.Vector;
 import etomica.units.dimensions.Dimensioned;
 import etomica.units.dimensions.Time;
-import etomica.util.Debug;
 import etomica.util.random.IRandom;
 
 /**
@@ -523,46 +522,8 @@ public abstract class IntegratorMDFasterer extends IntegratorBoxFasterer impleme
      * the whole system is zero.
      */
     protected void shiftMomenta() {
-        momentum.E(0);
-        IAtomList leafList = box.getLeafList();
-        int nLeaf = leafList.size();
-        if (nLeaf == 0) return;
-        if (nLeaf > 1) {
-            double totalMass = 0;
-            for (int iLeaf = 0; iLeaf < nLeaf; iLeaf++) {
-                IAtom a = leafList.get(iLeaf);
-                double mass = a.getType().getMass();
-                if (mass != Double.POSITIVE_INFINITY) {
-                    momentum.PEa1Tv1(mass, ((IAtomKinetic) a).getVelocity());
-                    totalMass += mass;
-                }
-            }
-            if (totalMass == 0) return;
-            momentum.TE(1.0 / totalMass);
-            //momentum is now net velocity
-            //set net momentum to 0
-            for (int iLeaf = 0; iLeaf < nLeaf; iLeaf++) {
-                IAtomKinetic a = (IAtomKinetic) leafList.get(iLeaf);
-                double rm = a.getType().rm();
-                if (rm != 0 && rm != Double.POSITIVE_INFINITY) {
-                    a.getVelocity().ME(momentum);
-                }
-            }
-            if (Debug.ON) {
-                momentum.E(0);
-                for (int iLeaf = 0; iLeaf < nLeaf; iLeaf++) {
-                    IAtomKinetic a = (IAtomKinetic) leafList.get(iLeaf);
-                    double mass = a.getType().getMass();
-                    if (mass != Double.POSITIVE_INFINITY) {
-                        momentum.PEa1Tv1(mass, a.getVelocity());
-                    }
-                }
-                momentum.TE(1.0 / totalMass);
-                if (Math.sqrt(momentum.squared()) > 1.e-10) {
-                    System.out.println("Net momentum per leaf atom is " + momentum + " but I expected it to be 0");
-                }
-            }
-            momentum.E(0);
+        if (box.getLeafList().size() > 1) {
+            ActionZeroMomentum.zeroMomenta(box);
         }
     }
 
