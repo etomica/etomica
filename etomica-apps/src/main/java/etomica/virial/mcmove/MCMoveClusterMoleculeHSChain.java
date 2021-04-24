e8d6f5501ee322ab3134ef79f6175ac7d4b6fdc4
Andrew J. Schultz <ajschult784@gmail.com>
2021-04-24 10:15:48 -0400

    handle constraint map (for flexible molecules)

--- etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterMoleculeHSChain.java
+++ etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterMoleculeHSChain.java
@@ -4,6 +4,7 @@
 
 package etomica.virial.mcmove;
 
+import etomica.box.Box;
 import etomica.integrator.mcmove.MCMoveMolecule;
 import etomica.molecule.IMolecule;
 import etomica.molecule.IMoleculeList;
@@ -22,12 +23,28 @@ public class MCMoveClusterMoleculeHSChain extends MCMoveMolecule {
 
     protected final double sigma;
     protected int[] seq;
-    protected MoleculePositionCOM moleculeCOM ;
+    protected MoleculePositionCOM moleculeCOM;
+    protected int[] constraintMap;
 
     public MCMoveClusterMoleculeHSChain(IRandom random, Space _space, double sigma) {
         super(null,random, _space,1, 1);
         this.sigma = sigma;
         moleculeCOM = new MoleculePositionCOM(space);
+    }
+
+    public void setBox(Box p) {
+        super.setBox(p);
+        if (constraintMap == null) {
+            constraintMap = new int[box.getMoleculeList().size()];
+            for (int i=0; i<constraintMap.length; i++) {
+                constraintMap[i] = i;
+            }
+        }
+    }
+
+
+    public void setConstraintMap(int[] newConstraintMap) {
+        constraintMap = newConstraintMap;
     }
 
     public boolean doTrial() {
@@ -46,30 +63,57 @@ public class MCMoveClusterMoleculeHSChain extends MCMoveMolecule {
             seq[j] = seq[i];
             seq[i] = k;
         }
-        IMolecule molecule = moleculeList.get(seq[0]);
+
+        int prev = seq[0];
+        int start = 1;
+        if (constraintMap[prev] != prev) {
+            prev = seq[1];
+            start = 2;
+        }
+        IMolecule molecule = moleculeList.get(prev);
         groupTranslationVector.E(moleculeCOM.position(molecule));
 
         groupTranslationVector.TE(-1);
 
         moveMoleculeAction.actionPerformed(molecule);
 
-        for (int i = 1; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (constraintMap[j] == prev && j != prev) {
+                groupTranslationVector.E(0);
+                translateFrom(prev, j);
+            }
+        }
+
+        for (int i = start; i<n; i++) {
+            if (constraintMap[seq[i]] != seq[i]) continue;
             groupTranslationVector.setRandomInSphere(random);
 
-            double sig = getSigma(seq[i - 1], seq[i]);
+            double sig = getSigma(prev, seq[i]);
             groupTranslationVector.TE(sig);
+            translateFrom(prev, seq[i]);
 
-            IMolecule moleculePrevious = moleculeList.get(seq[i-1]);
-            groupTranslationVector.PE(moleculeCOM.position(moleculePrevious));
-
-            molecule = moleculeList.get(seq[i]);
-            groupTranslationVector.ME(moleculeCOM.position(molecule));
-
-            moveMoleculeAction.actionPerformed(molecule);
+            for (int j=0; j<n; j++) {
+                if (constraintMap[j] == seq[i] && j != seq[i]) {
+                    groupTranslationVector.E(0);
+                    translateFrom(seq[i], j);
+                }
+            }
+            prev = seq[i];
         }
 
         ((BoxCluster)box).trialNotify();
         return true;
+    }
+
+    protected void translateFrom(int prev, int current) {
+        IMoleculeList moleculeList = box.getMoleculeList();
+        IMolecule moleculePrevious = moleculeList.get(prev);
+        groupTranslationVector.PE(moleculeCOM.position(moleculePrevious));
+
+        molecule = moleculeList.get(current);
+        groupTranslationVector.ME(moleculeCOM.position(molecule));
+
+        moveMoleculeAction.actionPerformed(molecule);
     }
 
     protected double getSigma(int i, int j) {
