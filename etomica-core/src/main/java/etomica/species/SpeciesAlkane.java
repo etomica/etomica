6e9f0f9a568c055878f6a648e3d55376416d7fb1
Andrew J. Schultz <ajschult784@gmail.com>
2021-08-20 17:13:43 -0400

    allow builder to be returned

--- etomica-core/src/main/java/etomica/species/SpeciesAlkane.java
+++ etomica-core/src/main/java/etomica/species/SpeciesAlkane.java
@@ -6,7 +6,6 @@ package etomica.species;
 
 import etomica.atom.AtomType;
 import etomica.config.ConformationChainZigZag2;
-import etomica.space.Space;
 import etomica.space.Vector;
 import etomica.space3d.Space3D;
 
@@ -25,17 +24,35 @@ public class SpeciesAlkane {
     }
 
     public static SpeciesGeneral create(int numCarbons, AtomType ch3Type, AtomType ch2Type, double bondL, double bondTheta) {
-        Space space = Space3D.getInstance();
-        ConformationChainZigZag2 conf = new ConformationChainZigZag2(
-                space,
+        return makeBuilder(numCarbons, ch3Type, ch2Type, bondL, bondTheta).build();
+    }
+
+    public static ConformationChainZigZag2 makeConformation() {
+        return makeConformation(nominalBondL, nominalBondTheta);
+    }
+
+    public static ConformationChainZigZag2 makeConformation(double bondL, double bondTheta) {
+        return new ConformationChainZigZag2(
+                Space3D.getInstance(),
                 Vector.of(bondL, 0, 0),
                 Vector.of(-bondL * Math.cos(bondTheta), bondL * Math.sin(bondTheta), 0)
         );
+    }
+
+    public static SpeciesBuilder makeBuilder(int numCarbons) {
+        return makeBuilder(numCarbons, AtomType.simple("CH3", 15), AtomType.simple("CH2", 14));
+    }
+
+    public static SpeciesBuilder makeBuilder(int numCarbons, AtomType ch3Type, AtomType ch2Type) {
+        return makeBuilder(numCarbons, ch3Type, ch2Type, nominalBondL, nominalBondTheta);
+    }
+
+    public static SpeciesBuilder makeBuilder(int numCarbons, AtomType ch3Type, AtomType ch2Type, double bondL, double bondTheta) {
+        ConformationChainZigZag2 conf = makeConformation(bondL, bondTheta);
         return new SpeciesBuilder(Space3D.getInstance())
                 .addCount(ch3Type, 1)
                 .addCount(ch2Type, Math.max(numCarbons - 2, 0))
                 .addCount(ch3Type, Math.min(1, numCarbons-1))
-                .withConformation(conf)
-                .build();
+                .withConformation(conf);
     }
 }
