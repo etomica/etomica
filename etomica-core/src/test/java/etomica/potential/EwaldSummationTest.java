a1e8278afcb60677e6dbe4336ff130ad50d8a072
Andrew J. Schultz <ajschult784@gmail.com>
2021-11-14 14:04:02 -0500

    get rid of EwaldSummation

--- etomica-core/src/test/java/etomica/potential/EwaldSummationTest.java
+++ etomica-core/src/test/java/etomica/potential/EwaldSummationTest.java
@@ -4,9 +4,7 @@
 
 package etomica.potential;
 
-import etomica.atom.AtomLeafAgentManager;
 import etomica.atom.AtomType;
-import etomica.atom.IAtom;
 import etomica.box.Box;
 import etomica.config.Configuration;
 import etomica.config.ConfigurationResourceFile;
@@ -23,9 +21,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,7 +43,6 @@ class EwaldSummationTest {
     private static final double[] NIST_self = {-2.84469E+06, -5.68938E+06, -8.53407E+06, -1.42235E+07};
     private static final double[] NIST_corr = {2.80999E+06, 5.61998E+06, 8.42998E+06, 1.41483E+07};
 
-    private EwaldSummation es;
     private PotentialMaster pair;
     private PotentialComputeEwaldFourier fourier;
     private PotentialMasterBonding pmBonding;
@@ -65,8 +59,6 @@ class EwaldSummationTest {
         SpeciesGeneral species = SpeciesWater3P.create();
         sim.addSpecies(species);
         Box box = sim.makeBox();
-        ChargeAgentSourceSPCE agentSource = new ChargeAgentSourceSPCE(species);
-        AtomLeafAgentManager<EwaldSummation.MyCharge> atomAgentManager = new AtomLeafAgentManager<EwaldSummation.MyCharge>(agentSource, box);
 
         box.setNMolecules(species, numofmolecules);
         box.getBoundary().setBoxSize(new Vector3D(boxlength, boxlength, boxlength));
@@ -93,9 +85,6 @@ class EwaldSummationTest {
 
         pmBonding = fourier.makeIntramolecularCorrection();
 
-        es = new EwaldSummation(box, atomAgentManager, space, kcut, rCutRealES);
-        es.setAlpha(5.6 / boxlength);
-
         Configuration config = new ConfigurationResourceFile(
                 String.format("spce%d.pos", filenum),
                 EwaldSummationTest.class
@@ -110,12 +99,6 @@ class EwaldSummationTest {
     @DisplayName("Test Ewald Sum computed values against NIST values")
     void testEwaldSum(RepetitionInfo repetitionInfo) {
         int filenum = repetitionInfo.getCurrentRepetition();
-        assertAll(
-                () -> assertEquals(Kelvin.UNIT.toSim(NIST_real[filenum - 1]), es.uReal(), 10, "uReal"),
-                () -> assertEquals(Kelvin.UNIT.toSim(NIST_fourier[filenum - 1]), es.uFourier(), 0.01, "uFourier"),
-                () -> assertEquals(Kelvin.UNIT.toSim(NIST_self[filenum - 1]), es.uSelf(), 100, "uSelf"),
-                () -> assertEquals(Kelvin.UNIT.toSim(NIST_corr[filenum - 1]), es.uBondCorr(), 100, "uBondCorr")
-        );
 
         double fourierNIST = NIST_fourier[filenum - 1] + NIST_self[filenum - 1];
         assertAll(
@@ -123,32 +106,5 @@ class EwaldSummationTest {
                 () -> assertEquals(Kelvin.UNIT.toSim(fourierNIST), fourier.computeAll(false), 100, "uFourierFasterer"),
                 () -> assertEquals(Kelvin.UNIT.toSim(NIST_corr[filenum - 1]), pmBonding.computeAll(false), 100, "uFourierIntra")
         );
-
-        assertAll(
-                () -> assertEquals(es.uReal(), pair.computeAll(false), 0.001, "uRealFasterer"),
-                () -> assertEquals(es.uFourier() + es.uSelf(), fourier.computeAll(false), 0.001, "uFourierFasterer")
-        );
-    }
-
-    private static class ChargeAgentSourceSPCE implements AtomLeafAgentManager.AgentSource<EwaldSummation.MyCharge> {
-        private final Map<AtomType, EwaldSummation.MyCharge> myCharge;
-
-        public ChargeAgentSourceSPCE(SpeciesGeneral species) {
-            myCharge = new HashMap<>();
-            double chargeH = P2WaterSPCE.QH;
-            double chargeO = P2WaterSPCE.QO;
-            myCharge.put(species.getTypeByName("H"), new EwaldSummation.MyCharge(chargeH));
-            myCharge.put(species.getTypeByName("O"), new EwaldSummation.MyCharge(chargeO));
-        }
-
-        // *********************** set half(even # of particles ) as +ion, the other half -ion ***********************
-        public EwaldSummation.MyCharge makeAgent(IAtom a, Box agentBox) {
-            return myCharge.get(a.getType());
-        }
-
-        public void releaseAgent(EwaldSummation.MyCharge agent, IAtom atom, Box agentBox) {
-            // Do nothing
-        }
-
     }
 }
