5ac4dbbc16d136ef6825b720e4a8bb114dbead1c
arpit <arpitban@buffalo.edu>
2022-02-08 12:09:24 -0500

    Creates a MC Move for change in torsion angle

--- etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterTorsion.java
+++ etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterTorsion.java
@@ -1,20 +1,29 @@
 package etomica.virial.mcmove;
 
+import etomica.atom.IAtom;
 import etomica.atom.iterator.AtomIterator;
 import etomica.box.Box;
+import etomica.data.histogram.HistogramSimple;
 import etomica.integrator.mcmove.MCMoveBoxStep;
 import etomica.integrator.mcmove.MCMoveStep;
+import etomica.math.DoubleRange;
 import etomica.molecule.IMolecule;
 import etomica.molecule.IMoleculeList;
+import etomica.molecule.MoleculePositionCOMPBC;
+import etomica.potential.IPotentialBondTorsion;
 import etomica.potential.PotentialMaster;
 import etomica.potential.compute.PotentialCompute;
+import etomica.space.Boundary;
 import etomica.space.Space;
 import etomica.space.Vector;
 import etomica.space3d.RotationTensor3D;
+import etomica.space3d.Vector3D;
 import etomica.species.ISpecies;
 import etomica.util.collections.IntArrayList;
 import etomica.util.random.IRandom;
 import etomica.virial.BoxCluster;
+
+import java.util.List;
 
 public class MCMoveClusterTorsion extends MCMoveBoxStep {
 
@@ -33,7 +42,7 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
     int [] indices = new int[3];
     int [] modified;
     int modifiedIndex = 0;
-    //int com = 0;
+    HistogramSimple histogramSimple = new HistogramSimple(100, new DoubleRange(0, Math.PI));
 
     public MCMoveClusterTorsion(PotentialCompute potentialCompute, Space space, IntArrayList[] bonding, IRandom random, double stepSize) {
         super(null);
@@ -43,6 +52,7 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
         this.bonding = bonding;
         this.stepSize = stepSize;
         setStepSizeMin(stepSize);
+        setStepSizeMax(2*Math.PI);
         modified = new int[bonding.length];
     }
 
@@ -57,6 +67,7 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
         wOld = ((BoxCluster)box).getSampleCluster().value((BoxCluster)box);
         IMoleculeList moleculeList = box.getMoleculeList();
         for(int i = 0; i<moleculeList.size(); i++) {
+            Vector com = space.makeVector();
             if (species != null && moleculeList.get(i).getType() != species) {
                 continue;
             }
@@ -65,6 +76,7 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
                 position[i][j] = space.makeVector();
                 position[i][j].E(molecule.getChildList().getAtoms().get(j).getPosition());
             }
+            com.E(MoleculePositionCOMPBC.com(box.getBoundary(), molecule));
             modifiedIndex = 0;
             dt = stepSize * (random.nextDouble() - 0.5);
             do{
@@ -77,6 +89,16 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
             }while(bonding[b].size() < 2);
             modified[modifiedIndex] = b;
             ++modifiedIndex;
+            List<IAtom> atoms = molecule.getChildList().getAtoms();
+            for (int j = 0; j < bonding[a].size(); j++ ) {
+                if (bonding[bonding[a].getInt(j)].size() > 1 && bonding[a].getInt(j) != b) {
+                    for (int k = 0; k < bonding[b].size(); k++) {
+                        if (bonding[bonding[b].getInt(k)].size() > 1 && bonding[b].getInt(k) != a) {
+                            histogramSimple.addValue(computeTorsion(box.getBoundary(), atoms.get(bonding[a].getInt(j)), atoms.get(a), atoms.get(b), atoms.get(bonding[b].getInt(k))));
+                        }
+                    }
+                }
+            }
             Vector axis = space.makeVector();
             axis.Ev1Mv2(molecule.getChildList().getAtoms().get(a).getPosition(), molecule.getChildList().getAtoms().get(b).getPosition());
             axis.normalize();
@@ -85,6 +107,10 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
             transformBondedAtoms(rotationTensor, position[i][a], a, molecule);
             rotationTensor.invert();
             transformBondedAtoms(rotationTensor, position[i][a], b, molecule);
+            com.ME(MoleculePositionCOMPBC.com(box.getBoundary(), molecule));
+            for(int j = 0; j < molecule.getChildList().size(); j++) {
+                molecule.getChildList().getAtoms().get(j).getPosition().PE(com);
+            }
         }
         ((BoxCluster)box).trialNotify();
         uNew = potential.computeAll(false);
@@ -117,6 +143,52 @@ public class MCMoveClusterTorsion extends MCMoveBoxStep {
         rotationTensor3D.transform(r);
         r.PE(pointOnAxis);
         molecule.getChildList().getAtoms().get(index).getPosition().E(r);
+    }
+
+    private static double computeTorsion(Boundary boundary, IAtom iAtom, IAtom jAtom, IAtom kAtom, IAtom lAtom) {
+        Vector ri = iAtom.getPosition();
+        Vector rj = jAtom.getPosition();
+        Vector rk = kAtom.getPosition();
+        Vector rl = lAtom.getPosition();
+        Vector rji = new Vector3D();
+        Vector rjk = new Vector3D();
+        Vector rkl = new Vector3D();
+
+        rji.Ev1Mv2(ri, rj);
+        boundary.nearestImage(rji);
+        rjk.Ev1Mv2(rk, rj);
+        boundary.nearestImage(rjk);
+        double rjk2 = rjk.squared();
+        rkl.Ev1Mv2(rl, rk);
+        boundary.nearestImage(rkl);
+
+        Vector vji = new Vector3D();
+        vji.E(rji);
+        vji.PEa1Tv1(-rjk.dot(rji) / rjk2, rjk);
+        double vji2 = vji.squared();
+        Vector vkl = new Vector3D();
+        vkl.E(rkl);
+        vkl.PEa1Tv1(-rjk.dot(rkl) / rjk2, rjk);
+        double vkl2 = vkl.squared();
+        double rji2 = rji.squared();
+        double rkl2 = rkl.squared();
+
+        double vji2vkl2 = vji2 * vkl2;
+        if (vji2 < 1e-6 * rji2 || vkl2 < 1e-6 * rkl2) {
+            // one of the vectors (ji, kl) is nearly colinear with jk
+            return 0;
+        }
+        double vji_vkl = 1 / Math.sqrt(vji2vkl2);
+        double theta = Math.acos(vji.dot(vkl) * vji_vkl);
+
+        return theta;
+    }
+
+    public void printHistogram(){
+        double [] histogram = histogramSimple.getHistogram();
+        for (int i = 0; i < histogram.length; i++) {
+            System.out.println(histogramSimple.xValues()[i] + "\t" + histogram[i]);
+        }
     }
 
     @Override
