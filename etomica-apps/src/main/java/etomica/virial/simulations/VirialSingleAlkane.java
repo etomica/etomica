677f479ba8b41515cea06e873d3f2a82c0259e27
Arpit Bansal <arpitban@buffalo.edu>
2022-02-09 22:19:23 -0500

    Cleaned code and added print statements

--- etomica-apps/src/main/java/etomica/virial/simulations/VirialSingleAlkane.java
+++ etomica-apps/src/main/java/etomica/virial/simulations/VirialSingleAlkane.java
@@ -56,18 +56,13 @@ public class VirialSingleAlkane {
         if (args.length > 0) {
             ParseArgs.doParseArgs(params, args);
         } else {
-            params.nPoints = 2;
             params.temperature = 400.0;// Kelvin
             params.numSteps = 1000000;
-            params.refFreq = -1;
-            params.file = "C";
+            params.file = "CCCCC";
         }
 
-        final int nPoints = params.nPoints;
-        //int nSpheres = params.nSpheres;
         double temperature = params.temperature;
         long steps = params.numSteps;
-        double refFreq = params.refFreq;
         String file = params.file;
         ParserAMBER.Stuff stuff= ParserAMBER.makeStuff(file, new ParserAMBER.Options());
         SpeciesManager speciesManager = stuff.speciesManager;
@@ -91,16 +86,14 @@ public class VirialSingleAlkane {
             }
         }
 
-        double sigmaHSRef = 5;
-
         Space space = Space3D.getInstance();
 
-        MayerHardSphere fRef = new MayerHardSphere(sigmaHSRef);
         PotentialMoleculePair potentialMoleculePair = new PotentialMoleculePair(space, speciesManager);
         potentialMoleculePair.setAtomPotential(C, C, potential2Soft[0][0]);
         potentialMoleculePair.setAtomPotential(C, H, potential2Soft[0][1]);
         potentialMoleculePair.setAtomPotential(H, H, potential2Soft[1][1]);
 
+        System.out.println("numSteps " + steps);
         System.out.println("Shape Descriptors at " + temperature +"K");
         temperature = Kelvin.UNIT.toSim(temperature);
 
@@ -110,36 +103,30 @@ public class VirialSingleAlkane {
         Box box = new Box(new BoundaryRectangularNonperiodic(space), space);
         sim.addBox(box);
         box.setNMolecules(alkane, 1);
+
         PotentialComputeIntramolecular potentialComputeIntramolecular0 = new PotentialComputeIntramolecular(space, sim.box(), speciesManager, fullBondingInfo, potential2Soft);
         PotentialMasterBonding potentialMasterBonding0 = new PotentialMasterBonding(speciesManager, sim.box(), fullBondingInfo);
         PotentialComputeAggregate potentialComputeAggregate0 = new PotentialComputeAggregate(potentialComputeIntramolecular0, potentialMasterBonding0);
 
         MeterGyrationTensor meterGyrationTensor = new MeterGyrationTensor(space);
         meterGyrationTensor.setBox(sim.box());
+
         IntegratorMCFasterer integrator = new IntegratorMCFasterer(potentialComputeAggregate0, sim.getRandom(), temperature, sim.box());
-        sim.getController().addActivity(new ActivityIntegrate(integrator));
-
-        AccumulatorAverageFixed accumulator = new AccumulatorAverageFixed(steps/1000);
-        DataPumpListener pump = new DataPumpListener(meterGyrationTensor, accumulator);
-
-        integrator.getEventManager().addListener(pump);
-        sim.getController().runActivityBlocking(new ActivityIntegrate(integrator, steps));
 
         MCMoveAtomFasterer mcMoveAtomFasterer = new MCMoveAtomFasterer(sim.getRandom(), potentialComputeAggregate0, sim.box());
         integrator.getMoveManager().addMCMove(mcMoveAtomFasterer);
 
         MCMoveAngle mcMoveAngle0 = new MCMoveAngle(potentialComputeAggregate0, space, bonding, sim.getRandom(), 1);
-
         integrator.getMoveManager().addMCMove(mcMoveAngle0);
 
         MCMoveTorsion mcMoveTorsion0 = new MCMoveTorsion(potentialComputeAggregate0, space,bonding, sim.getRandom(), 2*Math.PI);
-
         if(fullBondingInfo.bondedQuads[0].size() > 0) {
             integrator.getMoveManager().addMCMove(mcMoveTorsion0);
         }
 
         if(false) {
             double size = (bonding.length/3 + 5) * 1.5;
+            sim.getController().addActivity(new ActivityIntegrate(integrator));
             sim.box().getBoundary().setBoxSize(Vector.of(new double[]{size, size, size}));
             SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE);
             DisplayBox displayBox0 = simGraphic.getDisplayBox(sim.box());
@@ -152,106 +139,47 @@ public class VirialSingleAlkane {
             diameterManager.setDiameter(speciesManager.getSpecies(0).getAtomType(1), 0.5);
             ColorSchemeRandomByMolecule colorScheme = new ColorSchemeRandomByMolecule(sim.getSpeciesManager(), sim.box(), sim.getRandom());
             displayBox0.setColorScheme(colorScheme);
-
             simGraphic.makeAndDisplayFrame();
-
-            sim.getController().addActivity(new ActivityIntegrate(integrator));
-
-            final DisplayTextBox averageBox = new DisplayTextBox();
-            averageBox.setLabel("Average");
-            final DisplayTextBox errorBox = new DisplayTextBox();
-            errorBox.setLabel("Error");
-            JLabel jLabelPanelParentGroup = new JLabel("B" + nPoints + " (L/mol)^" + (nPoints - 1));
-            final JPanel panelParentGroup = new JPanel(new BorderLayout());
-            panelParentGroup.add(jLabelPanelParentGroup, Constants.CompassDirection.NORTH.toString());
-            panelParentGroup.add(averageBox.graphic(), BorderLayout.WEST);
-            panelParentGroup.add(errorBox.graphic(), BorderLayout.EAST);
-            simGraphic.getPanel().controlPanel.add(panelParentGroup, SimulationPanel.getVertGBC());
-
-            IAction pushAnswer = new IAction() {
-                DataDouble data = new DataDouble();
-
-                public void actionPerformed() {
-                    //double[] ratioAndError = sim.dvo.getAverageAndError();
-                    //double ratio = ratioAndError[0];
-                    //double error = ratioAndError[1];
-                    //data.x = ratio;
-                    //averageBox.putData(data);
-                    //data.x = error;
-                    //errorBox.putData(data);
-                }
-            };
-            IDataInfo dataInfo = new DataDouble.DataInfoDouble("B" + nPoints, new CompoundDimension(new Dimension[]{new DimensionRatio(Volume.DIMENSION, Quantity.DIMENSION)}, new double[]{nPoints - 1}));
-            Unit unit = new CompoundUnit(new Unit[]{new UnitRatio(Liter.UNIT, Mole.UNIT)}, new double[]{nPoints - 1});
-            averageBox.putDataInfo(dataInfo);
-            averageBox.setLabel("average");
-            averageBox.setUnit(unit);
-            errorBox.putDataInfo(dataInfo);
-            errorBox.setLabel("error");
-            errorBox.setPrecision(2);
-            errorBox.setUnit(unit);
-            integrator.getEventManager().addListener(new IntegratorListenerAction(pushAnswer));
             return;
         }
 
-        // if running interactively, don't use the file
-        String refFileName = isCommandline ? "refpref"+nPoints+"_"+temperature : null;
-        // this will either read the refpref in from a file or run a short simulation to find it
-        //sim.initRefPref(refFileName, steps/40);
+        sim.getController().runActivityBlocking(new ActivityIntegrate(integrator, steps/10));
 
-
-        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
-        // if it does continue looking for a pref, it will write the value to the file
-        //sim.equilibrate(refFileName, steps/20);
-        ActivityIntegrate ai = new ActivityIntegrate(integrator, 1000);
-        //sim.setAccumulatorBlockSize(steps);
+        ActivityIntegrate ai = new ActivityIntegrate(integrator, steps);
 
         System.out.println("equilibration finished");
-        //sim.setAccumulatorBlockSize(steps);
-        //System.out.println("MC Move step sizes "+sim.mcMoveTranslate.getStepSize()+" "
-          //      +mcMoveRotate.getStepSize()+" "
-            //    +(sim.mcMoveWiggle==null ? "" : (""+sim.mcMoveWiggle.getStepSize())));
-        //System.out.println("Torsion move acceptance "+torsionMoves[0].getTracker().acceptanceRatio()+" "+
-        //           torsionMoves[1].getTracker().acceptanceRatio());
-
-
-        if (false) {
-            //final double refIntegralF = refIntegral;
-            IntegratorListener progressReport = new IntegratorListener() {
-                public void integratorInitialized(IntegratorEvent e) {}
-                public void integratorStepStarted(IntegratorEvent e) {}
-                public void integratorStepFinished(IntegratorEvent e) {
-                    if ((integrator.getStepCount()*10) % ai.getMaxSteps() != 0) return;
-                    System.out.print(integrator.getStepCount()+" steps: ");
-                    //double[] ratioAndError = sim.dvo.getAverageAndError();
-                    //double ratio = ratioAndError[0];
-                    //double error = ratioAndError[1];
-                    //System.out.println("abs average: "+ratio*refIntegralF+", error: "+error*refIntegralF);
-                }
-            };
-            integrator.getEventManager().addListener(progressReport);
-        }
-
         integrator.getMoveManager().setEquilibrating(false);
+        AccumulatorAverageFixed accumulator = new AccumulatorAverageFixed((steps/100)/speciesManager.getSpecies(0).getLeafAtomCount());
+        DataPumpListener pump = new DataPumpListener(meterGyrationTensor, accumulator, speciesManager.getSpecies(0).getLeafAtomCount());
+        integrator.getEventManager().addListener(pump);
         sim.getController().runActivityBlocking(ai);
 
         System.out.println("Acceptance of Torsion Move "+ mcMoveTorsion0.getTracker().acceptanceProbability());
 
-        double AsphericityAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(0);
-        double AsphericityErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(0);
-        double AsphericityCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(0);
+        double RGAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(0);
+        double RGErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(0);
+        double RGCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(0);
+        double RGSD = accumulator.getData(AccumulatorAverage.STANDARD_DEVIATION).getValue(0);
 
-        double AcylindricityAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(1);
-        double AcylindricityErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(1);
-        double AcylindricityCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(1);
+        double AsphericityAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(1);
+        double AsphericityErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(1);
+        double AsphericityCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(1);
+        double AsphericitySD = accumulator.getData(AccumulatorAverage.STANDARD_DEVIATION).getValue(1);
 
-        double AnisotropyAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(2);
-        double AnisotropyErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(2);
-        double AnisotropyCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(2);
+        double AcylindricityAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(2);
+        double AcylindricityErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(2);
+        double AcylindricityCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(2);
+        double AcylindricitySD = accumulator.getData(AccumulatorAverage.STANDARD_DEVIATION).getValue(2);
 
-        System.out.print(String.format("Asphericity avg: %13.6e  err: %11.4e   cor: % 4.2f\n", AsphericityAvg, AsphericityErr, AsphericityCor));
-        System.out.print(String.format("Acylindricity avg: %13.6e  err: %11.4e   cor: % 4.2f\n", AcylindricityAvg, AcylindricityErr, AcylindricityCor));
-        System.out.print(String.format("Anisotropy avg: %13.6e  err: %11.4e   cor: % 4.2f\n", AnisotropyAvg, AnisotropyErr, AnisotropyCor));
+        double AnisotropyAvg = accumulator.getData(AccumulatorAverage.AVERAGE).getValue(3);
+        double AnisotropyErr = accumulator.getData(AccumulatorAverage.ERROR).getValue(3);
+        double AnisotropyCor = accumulator.getData(AccumulatorAverage.BLOCK_CORRELATION).getValue(3);
+        double AnisotropySD = accumulator.getData(AccumulatorAverage.STANDARD_DEVIATION).getValue(3);
+
+        System.out.print(String.format("RadiusOfGyration avg: %13.6e  sd: %13.6e  err: %11.4e   cor: % 4.2f\n", RGAvg, RGSD, RGErr, RGCor));
+        System.out.print(String.format("Asphericity avg: %13.6e  sd: %13.6e  err: %11.4e   cor: % 4.2f\n", AsphericityAvg, AsphericitySD, AsphericityErr, AsphericityCor));
+        System.out.print(String.format("Acylindricity avg: %13.6e  sd: %13.6e  err: %11.4e   cor: % 4.2f\n", AcylindricityAvg, AcylindricitySD, AcylindricityErr, AcylindricityCor));
+        System.out.print(String.format("Anisotropy avg: %13.6e  sd: %13.6e  err: %11.4e   cor: % 4.2f\n", AnisotropyAvg, AnisotropySD, AnisotropyErr, AnisotropyCor));
 
         long t2 = System.currentTimeMillis();
         System.out.println("time: "+(t2-t1)*0.001);
@@ -261,11 +189,8 @@ public class VirialSingleAlkane {
      * Inner class for parameters
      */
     public static class VirialSingleAlkaneParam extends ParameterBase {
-        public int nPoints = 1;
-        //public int nSpheres = 3;
         public double temperature = 300.0;// Kelvin
         public long numSteps = 100000000;
-        public double refFreq = -1;
         public String file = "CCCCCC";
     }
 }
