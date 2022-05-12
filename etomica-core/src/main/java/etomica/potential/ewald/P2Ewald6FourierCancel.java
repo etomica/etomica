94772e6dd2aaf8e48377892b9c420808838b756c
Andrew J. Schultz <ajschult784@gmail.com>
2022-05-12 01:02:15 -0400

    fix f6 and usage of alpha66

--- etomica-core/src/main/java/etomica/potential/ewald/P2Ewald6FourierCancel.java
+++ etomica-core/src/main/java/etomica/potential/ewald/P2Ewald6FourierCancel.java
@@ -16,7 +16,6 @@ public class P2Ewald6FourierCancel implements IPotential2 {
     private final double Bij, f6;
 
     public P2Ewald6FourierCancel(double sigmaI, double epsilonI, double sigmaJ, double epsilonJ, double alpha6) {
-        super();
         this.alpha6Sq = alpha6 * alpha6;
         this.alpha66 = alpha6Sq * alpha6Sq * alpha6Sq;
         double localBij = 0;
@@ -27,14 +26,14 @@ public class P2Ewald6FourierCancel implements IPotential2 {
             localBij += bik * bjk;
         }
         this.Bij = localBij;
-        f6 = 4*0.5*(sigmaI+sigmaJ)*Math.sqrt(epsilonI*epsilonJ);
+        f6 = 4*Math.pow(0.5*(sigmaI+sigmaJ),6)*Math.sqrt(epsilonI*epsilonJ);
     }
 
     @Override
     public double u(double r2) {
         double a2 = r2 * alpha6Sq;
         double a4 = a2*a2;
-        return (-Bij*alpha66*(1+a2+a4/2)*Math.exp(-a2) + f6)/(a4*a2);
+        return alpha66*(-Bij*(1+a2+a4/2)*Math.exp(-a2) + f6)/(a4*a2);
     }
 
     @Override
@@ -44,7 +43,7 @@ public class P2Ewald6FourierCancel implements IPotential2 {
         double a6 = a4*a2;
         double e = Math.exp(-a2);
         // (2a + 2a3) e/a6 - 2(1 + a2 + a4/2) e/a5 - 6(1 + a2 + a4/2) e/a7
-        return (Bij * alpha66 *(6 + 6*a2 + 3*a4 + a6)*e - 6*f6)/a6;
+        return alpha66 * (Bij *(6 + 6*a2 + 3*a4 + a6)*e - 6*f6)/a6;
     }
 
     @Override
@@ -53,9 +52,9 @@ public class P2Ewald6FourierCancel implements IPotential2 {
         double a4 = a2 * a2;
         double a6 = a4 * a2;
         double e = Math.exp(-a2);
-        u012[0] += (-Bij * alpha66 * (1 + a2 + a4 / 2) * e + f6)/ a6;
-        u012[1] += (Bij * alpha66 * (6 + 6 * a2 + 3 * a4 + a6) * e - 6*f6) / a6;
-        u012[2] += (-Bij * alpha66 * (42 + 42 * a2 + 21 * a4 + (7 + 2 * a2) * a6) * e + 42*f6) / a6;
+        u012[0] += alpha66 * (-Bij * (1 + a2 + a4 / 2) * e + f6)/ a6;
+        u012[1] += alpha66 * (Bij * (6 + 6 * a2 + 3 * a4 + a6) * e - 6*f6) / a6;
+        u012[2] += alpha66 * (-Bij * (42 + 42 * a2 + 21 * a4 + (7 + 2 * a2) * a6) * e + 42*f6) / a6;
     }
 
     @Override
@@ -64,7 +63,7 @@ public class P2Ewald6FourierCancel implements IPotential2 {
         double a4 = a2*a2;
         double a6 = a4*a2;
         double e = Math.exp(-a2);
-        return (-Bij * alpha66 *(42 + 42*a2 + 21*a4 + (7+2*a2)*a6)*e + 42*f6)/a6;
+        return alpha66 * (-Bij *(42 + 42*a2 + 21*a4 + (7+2*a2)*a6)*e + 42*f6)/a6;
     }
 
 }
