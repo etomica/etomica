bf2e24c28ad89f1973279e2db9822cc19647d645
Andrew J. Schultz <ajschult784@gmail.com>
2022-09-07 15:47:58 -0400

    add ability to also transform real-space coordinates directly

--- etomica-apps/src/main/java/etomica/normalmode/PathIntegralHOGraphic.java
+++ etomica-apps/src/main/java/etomica/normalmode/PathIntegralHOGraphic.java
@@ -7,13 +7,10 @@ import etomica.action.IAction;
 import etomica.atom.DiameterHash;
 import etomica.atom.DiameterHashByType;
 import etomica.atom.IAtom;
-import etomica.atom.IAtomList;
 import etomica.box.Box;
-import etomica.graphics.ColorScheme;
-import etomica.graphics.DeviceButton;
-import etomica.graphics.DeviceSlider;
-import etomica.graphics.SimulationGraphic;
+import etomica.graphics.*;
 import etomica.modifier.Modifier;
+import etomica.modifier.ModifierBoolean;
 import etomica.space.Vector;
 import etomica.space2d.Space2D;
 import etomica.units.dimensions.Dimension;
@@ -47,26 +44,10 @@ public class PathIntegralHOGraphic {
         if (isTIA){
             omega2 = omega2*(1.0 + omega2/12.0/omegaN/omegaN);
         }
-        double beta = 1 / temperature;
-        double betaN = beta/nBeads;
 
         final SimQuantumAO sim = new SimQuantumAO(Space2D.getInstance(), nBeads, temperature, k2, k4, omega2, isTIA, true);
 
         SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE);
-
-        double[] lambdaN = new double[nBeads];
-        double[][] eigenvectors = new double[nBeads][nBeads];
-        double[] q = new double[2*nBeads];  // initial q should all be 0
-        for (int k=0; k<nBeads; k++) {
-            double s = Math.sin(Math.PI*k/nBeads);
-            lambdaN[k] = betaN * massssss*(4.0*omegaN*omegaN*s*s + omega2);
-            for (int i = 0; i < nBeads; i++) {
-                double arg = 2.0*Math.PI/nBeads*i*k;
-                double cs = k > nBeads / 2 ? Math.sin(arg) : Math.cos(arg);
-                double onetwo = k==0 || 2*k == nBeads ? 1 : 2;
-                eigenvectors[i][k] = cs * Math.sqrt(onetwo / nBeads);
-            }
-        }
 
         ColorScheme colorScheme = new ColorScheme() {
             protected Color[] allColors;
@@ -90,27 +71,16 @@ public class PathIntegralHOGraphic {
         };
         simGraphic.getDisplayBox(sim.box).setColorScheme(colorScheme);
         simGraphic.remove(simGraphic.getTrioControllerButton());
-        DeviceButton newConfigButton = new DeviceButton(sim.getController(), new IAction() {
-            @Override
-            public void actionPerformed() {
-                sim.atomMove.doTrial();
-                sim.atomMove.acceptNotify();
-                updateQ(sim.box.getLeafList(), q, eigenvectors);
-                simGraphic.getDisplayBox(sim.box).repaint();
-            }
-        });
+        TransformerNormalMode transformerNM = new TransformerNormalMode(sim.box, k2, temperature);
+        TransformerReal transformerReal = new TransformerReal(sim.box, (MCMoveHOReal) sim.atomMove);
+        NewConfigAction newConfigAction = new NewConfigAction(sim, simGraphic, transformerNM);
+        DeviceButton newConfigButton = new DeviceButton(sim.getController(), newConfigAction);
         newConfigButton.setLabel("New Config");
         simGraphic.getPanel().graphicsPanel.add(newConfigButton.graphic(), BorderLayout.SOUTH);
         DiameterHash diameterHash = simGraphic.getDisplayBox(sim.box).getDiameterHash();
         ((DiameterHashByType)diameterHash).setDiameter(sim.species().getLeafType(), 0.1);
 
-        IAction updateLambda = new IAction() {
-            @Override
-            public void actionPerformed() {
-                updateLambda(sim.box(), q, lambdaN, eigenvectors, sim.p1ah.getSpringConstants()[0], sim.integrator.getTemperature());
-                simGraphic.getDisplayBox(sim.box).repaint();
-            }
-        };
+        FullUpdateAction updateLambda = new FullUpdateAction(sim, simGraphic, transformerNM);
 
         DeviceSlider k2Slider = new DeviceSlider(sim.getController(), new ModifierK2(massssss, isTIA, omegaN, sim));
         k2Slider.setMinimum(-2);
@@ -134,56 +104,255 @@ public class PathIntegralHOGraphic {
         temperatureSlider.setPostAction(updateLambda);
         simGraphic.add(temperatureSlider);
 
+        DeviceCheckBox realCheckbox = new DeviceCheckBox(sim.getController(), "real space", new ModifierBoolean() {
+            @Override
+            public void setBoolean(boolean b) {
+                Transformer t = b ? transformerReal : transformerNM;
+                t.updateAndQ(sim.p1ah.getSpringConstants()[0], sim.integrator.getTemperature());
+                newConfigAction.setTransformer(t);
+                updateLambda.setTransformer(t);
+            }
+
+            @Override
+            public boolean getBoolean() {
+                return newConfigAction.transformer instanceof TransformerReal;
+            }
+        });
+        simGraphic.add(realCheckbox);
+
         simGraphic.makeAndDisplayFrame("Path Integral Demo");
 
     }
 
-    public static void updateLambda(Box box, double[] q, double[] lambda, double[][] eigenvectors, double k2, double temperature) {
-        IAtomList atoms = box.getLeafList();
-        int n = lambda.length;
-        double beta = 1.0/(temperature);
-        double betaN = beta/n;
-        double omegaN = 1.0/(SimQuantumAO.hbar*betaN);
-        double omega2 = k2/1;
-        for (int k=0; k<n; k++) {
-            double s = Math.sin(Math.PI * k / n);
-            double newLambda = betaN * 1 * (4.0 * omegaN * omegaN * s * s + omega2);
-            for (int d=0; d<q.length/n; d++) {
-                q[d*n+k] *= Math.sqrt(lambda[k]/newLambda);
-            }
-            lambda[k] = newLambda;
+    public static class TransformerReal implements Transformer {
+
+        public final double[] q;
+        public final MCMoveHOReal move;
+        public final Box box;
+        public final double[] chainSigmas;
+
+        public TransformerReal(Box box, MCMoveHOReal move) {
+            this.box = box;
+            this.move = move;
+            q = new double[box.getSpace().D()*box.getLeafList().size()];
+            chainSigmas = move.getChainSigmas().clone();
+            updateQ();
         }
 
-        updateR(atoms, q, eigenvectors);
+        public void updateR() {
+            int n = box.getLeafList().size();
+            int D = box.getSpace().D();
+            double[][] centerCoefficients = move.getCenterCoefficients();
+            double[] R11 = centerCoefficients[0], R1N = centerCoefficients[1];
+            IAtom atom0 = null, atomPrev = null;
+            for (IAtom a : box.getLeafList()) {
+                Vector ri = a.getPosition();
+                int i = a.getLeafIndex();
+                int N = n - i;
+                if (i == 0) {
+                    for (int d=0; d<D; d++) {
+                        ri.setX(d, q[d*n]);
+                    }
+                    atom0 = atomPrev = a;
+                    continue;
+                }
+                for (int d=0; d<D; d++) {
+                    ri.setX(d, q[d*n+i]);
+                }
+                ri.PEa1Tv1(R11[N], atomPrev.getPosition());
+                ri.PEa1Tv1(R1N[N], atom0.getPosition());
+                atomPrev = a;
+            }
+        }
+
+        @Override
+        public void updateQ() {
+            int n = box.getLeafList().size();
+            int D = box.getSpace().D();
+            double[][] centerCoefficients = move.getCenterCoefficients();
+            double[] R11 = centerCoefficients[0], R1N = centerCoefficients[1];
+            IAtom atom0 = null, atomPrev = null;
+            for (IAtom a : box.getLeafList()) {
+                int i = a.getLeafIndex();
+                Vector r = a.getPosition();
+                if (i == 0) {
+                    for (int d=0; d<D; d++) {
+                        q[d*n] = r.getX(d);
+                    }
+                    atom0 = atomPrev = a;
+                    continue;
+                }
+                int N = n - i;
+                Vector dr = box.getSpace().makeVector();;
+                dr.E(a.getPosition());
+                dr.PEa1Tv1(-R11[N], atomPrev.getPosition());
+                dr.PEa1Tv1(-R1N[N], atom0.getPosition());
+                for (int d=0; d<r.getD(); d++) {
+                    q[d*n + i] = dr.getX(d);
+                }
+                atomPrev = a;
+            }
+        }
+
+        @Override
+        public void update(double k2, double temperature) {
+            // do nothing.  we let MCMove get updated and then retrieve stuff from it.
+            double[] newChainSigmas = move.getChainSigmas();
+            int n = box.getLeafList().size();
+            for (int d=0; d<box.getSpace().D(); d++) {
+                for (int i=0; i<n; i++) {
+                    if (i == 0) {
+                        q[d*n] *= newChainSigmas[0] / chainSigmas[0];
+                    }
+                    else {
+                        int N = n - i;
+                        q[d*n + i] *= newChainSigmas[N] / chainSigmas[N];
+                    }
+                }
+            }
+            System.arraycopy(newChainSigmas, 0, chainSigmas, 0, chainSigmas.length);
+
+            updateR();
+        }
+
+        public void updateAndQ(double k2, double temperature) {
+            // do nothing.  we let MCMove get updated and then retrieve stuff from it.
+            double[] newChainSigmas = move.getChainSigmas();
+            System.arraycopy(newChainSigmas, 0, chainSigmas, 0, chainSigmas.length);
+
+            updateQ();
+        }
+
     }
 
-    public static void updateQ(IAtomList atoms, double[] q, double[][] eigenvectors) {
-        Arrays.fill(q, 0);
-        int n = eigenvectors.length;
-        for (IAtom a : atoms) {
-            int i = a.getLeafIndex();
-            Vector r = a.getPosition();
-            for (int d=0; d<r.getD(); d++) {
-                for (int k=d*n; k<(d+1)*n; k++) {
-                    q[k] += r.getX(d)*eigenvectors[i][k%n];
+    public static class TransformerNormalMode implements Transformer {
+
+        public final double[] q;
+        public final double[] lambda;
+        public final double[][] eigenvectors;
+        public final Box box;
+
+        public TransformerNormalMode(Box box, double k2, double temperature) {
+            this.box = box;
+            int nBeads = box.getLeafList().size();
+            double mass = 1;
+            double omegaN = nBeads*temperature/SimQuantumAO.hbar; // 1/hbar*betan
+
+            double omega2 = k2/mass;
+
+            double beta = 1 / temperature;
+            double betaN = beta/nBeads;
+            lambda = new double[nBeads];
+            eigenvectors = new double[nBeads][nBeads];
+            q = new double[box.getSpace().D()*nBeads];  // initial q should all be 0
+            for (int k=0; k<nBeads; k++) {
+                double s = Math.sin(Math.PI*k/nBeads);
+                lambda[k] = betaN * mass * (4.0*omegaN*omegaN*s*s + omega2);
+                for (int i = 0; i < nBeads; i++) {
+                    double arg = 2.0*Math.PI/nBeads*i*k;
+                    double cs = k > nBeads / 2 ? Math.sin(arg) : Math.cos(arg);
+                    double onetwo = k==0 || 2*k == nBeads ? 1 : 2;
+                    eigenvectors[i][k] = cs * Math.sqrt(onetwo / nBeads);
                 }
             }
         }
-    }
 
-    public static void updateR(IAtomList atoms, double[] q, double[][] eigenvectors) {
-        int n = eigenvectors.length;
-        for (IAtom a : atoms) {
-            int i = a.getLeafIndex();
-            Vector r = a.getPosition();
-            for (int d=0; d<r.getD(); d++) {
-                double x = 0;
-                for (int k=d*n; k<(d+1)*n; k++) {
-                    x += eigenvectors[i][k%n]*q[k];
+        public void updateR() {
+            int n = eigenvectors.length;
+            for (IAtom a : box.getLeafList()) {
+                int i = a.getLeafIndex();
+                Vector r = a.getPosition();
+                for (int d=0; d<r.getD(); d++) {
+                    double x = 0;
+                    for (int k=d*n; k<(d+1)*n; k++) {
+                        x += eigenvectors[i][k%n]*q[k];
+                    }
+                    r.setX(d, x);
                 }
-                r.setX(d, x);
             }
         }
+
+        @Override
+        public void updateQ() {
+            Arrays.fill(q, 0);
+            int n = eigenvectors.length;
+            for (IAtom a : box.getLeafList()) {
+                int i = a.getLeafIndex();
+                Vector r = a.getPosition();
+                for (int d=0; d<r.getD(); d++) {
+                    for (int k=d*n; k<(d+1)*n; k++) {
+                        q[k] += r.getX(d)*eigenvectors[i][k%n];
+                    }
+                }
+            }
+        }
+
+        protected void updateInternal(double k2, double temperature) {
+            int n = lambda.length;
+            double beta = 1.0/(temperature);
+            double betaN = beta/n;
+            double omegaN = 1.0/(SimQuantumAO.hbar*betaN);
+            double omega2 = k2/1;
+            for (int k=0; k<n; k++) {
+                double s = Math.sin(Math.PI * k / n);
+                double newLambda = betaN * 1 * (4.0 * omegaN * omegaN * s * s + omega2);
+                for (int d=0; d<q.length/n; d++) {
+                    q[d*n+k] *= Math.sqrt(lambda[k]/newLambda);
+                }
+                lambda[k] = newLambda;
+            }
+        }
+
+        @Override
+        public void update(double k2, double temperature) {
+            int n = lambda.length;
+            double beta = 1.0/(temperature);
+            double betaN = beta/n;
+            double omegaN = 1.0/(SimQuantumAO.hbar*betaN);
+            double omega2 = k2/1;
+            for (int k=0; k<n; k++) {
+                double s = Math.sin(Math.PI * k / n);
+                double newLambda = betaN * 1 * (4.0 * omegaN * omegaN * s * s + omega2);
+                for (int d=0; d<q.length/n; d++) {
+                    q[d*n+k] *= Math.sqrt(lambda[k]/newLambda);
+                }
+                lambda[k] = newLambda;
+            }
+            updateR();
+        }
+
+        @Override
+        public void updateAndQ(double k2, double temperature) {
+            int n = lambda.length;
+            double beta = 1.0/(temperature);
+            double betaN = beta/n;
+            double omegaN = 1.0/(SimQuantumAO.hbar*betaN);
+            double omega2 = k2/1;
+            for (int k=0; k<n; k++) {
+                double s = Math.sin(Math.PI * k / n);
+                lambda[k] = betaN * 1 * (4.0 * omegaN * omegaN * s * s + omega2);
+            }
+            updateQ();
+        }
+    }
+
+    public interface Transformer {
+
+        /**
+         * Uses the atom positions to update the given q
+         */
+        public void updateQ();
+
+        /**
+         * Updates internal data for the given temperature and k2, scales the given q values,
+         * then updates real atom positions
+         */
+        public void update(double k2, double temperature);
+
+        /**
+         * Updates internal data for the given temperature and recomputes q for the current config
+         */
+        public void updateAndQ(double k2, double temperature);
     }
 
 
@@ -265,6 +434,53 @@ public class PathIntegralHOGraphic {
         @Override
         public String getLabel() {
             return "k2";
+        }
+    }
+
+    private static class NewConfigAction implements IAction {
+
+        private final SimQuantumAO sim;
+        private final SimulationGraphic simGraphic;
+        public Transformer transformer;
+
+        public NewConfigAction(SimQuantumAO sim, SimulationGraphic simGraphic, Transformer transformer) {
+            this.sim = sim;
+            this.simGraphic = simGraphic;
+            this.transformer = transformer;
+        }
+
+        public void setTransformer(Transformer transformer) {
+            this.transformer = transformer;
+        }
+
+        @Override
+        public void actionPerformed() {
+            sim.atomMove.doTrial();
+            sim.atomMove.acceptNotify();
+            transformer.updateQ();
+            simGraphic.getDisplayBox(sim.box).repaint();
+        }
+    }
+
+    private static class FullUpdateAction implements IAction {
+        private Transformer transformer;
+        private final SimQuantumAO sim;
+        private final SimulationGraphic simGraphic;
+
+        public FullUpdateAction(SimQuantumAO sim, SimulationGraphic simGraphic, Transformer transformer) {
+            this.transformer = transformer;
+            this.sim = sim;
+            this.simGraphic = simGraphic;
+        }
+
+        public void setTransformer(Transformer transformer) {
+            this.transformer = transformer;
+        }
+
+        @Override
+        public void actionPerformed() {
+            transformer.update(sim.p1ah.getSpringConstants()[0], sim.integrator.getTemperature());
+            simGraphic.getDisplayBox(sim.box).repaint();
         }
     }
 }
