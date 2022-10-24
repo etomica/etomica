49635e7df4661ef2dd81370cf864a55cb53db83a
Andrew J. Schultz <ajschult784@gmail.com>
2022-10-24 15:42:23 -0400

    look at 1 molecule at a time

--- etomica-apps/src/main/java/etomica/normalmode/MeterMSDHO.java
+++ etomica-apps/src/main/java/etomica/normalmode/MeterMSDHO.java
@@ -1,27 +1,33 @@
 package etomica.normalmode;
 
+import etomica.atom.IAtom;
 import etomica.box.Box;
 import etomica.data.DataSourceScalar;
-import etomica.potential.PotentialMasterBonding;
-import etomica.potential.compute.PotentialComputeField;
+import etomica.molecule.CenterOfMass;
+import etomica.molecule.IMolecule;
+import etomica.space.Vector;
 import etomica.units.dimensions.Null;
 
 public class MeterMSDHO extends DataSourceScalar {
     protected Box box;
-    protected int nBeads;
 
     public MeterMSDHO(int nBeads, Box box) {
-        super("Stuff", Null.DIMENSION);
+        super("PI MSD", Null.DIMENSION);
         this.box = box;
-        this.nBeads = nBeads;
     }
 
     @Override
     public double getDataAsScalar() {
         double r2sum = 0;
-        for (int i=0; i < nBeads; i++){
-            r2sum += box.getLeafList().get(i).getPosition().squared();
+        Vector dr = box.getSpace().makeVector();
+        for (IMolecule molecule : box.getMoleculeList()) {
+            Vector com = CenterOfMass.position(box, molecule);
+            for (IAtom atom : molecule.getChildList()) {
+                dr.Ev1Mv2(atom.getPosition(), com);
+                box.getBoundary().nearestImage(dr);
+                r2sum += dr.squared();
+            }
         }
-        return r2sum/nBeads;
+        return r2sum/box.getLeafList().size();
     }
 }
