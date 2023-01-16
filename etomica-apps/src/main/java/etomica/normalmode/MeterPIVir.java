82670b767305c45a2bdff38f43b01f594e05d7ab
sabrygad <sabrygad@buffalo.edu>
2023-01-16 16:24:30 -0600

    all meters consistent now using d(N-1)n DOF

--- etomica-apps/src/main/java/etomica/normalmode/MeterPIVir.java
+++ etomica-apps/src/main/java/etomica/normalmode/MeterPIVir.java
@@ -64,7 +64,7 @@ public class MeterPIVir implements IDataSource, PotentialCallback {
             }
         }
 
-        x[0] =   dim/2.0/beta + pcP1.getLastEnergy() + 1.0/2.0*vir - EnShift;//En
+        x[0] =   dim*nBeads/beta + dim/2.0/beta + pcP1.getLastEnergy() + 1.0/2.0*vir - EnShift;//En
 //        x[1] = 1.0/4.0/beta*(-3.0*vir - rHr); //Cvn/kb^2, without Var
         return data;
     }
