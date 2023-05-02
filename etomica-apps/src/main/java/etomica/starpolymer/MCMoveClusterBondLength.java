etomica/etomica

commit d3a2ff7366862b577c3f7a6448269bbc57bac4e4

remove (almost) all classes obsoleted by fasterer classes

etomica-apps/src/main/java/etomica/starpolymer/MCMoveClusterBondLength.java
@@ -1,136 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-package etomica.starpolymer;
-
-import etomica.atom.IAtom;
-import etomica.atom.IAtomList;
-import etomica.box.Box;
-import etomica.data.meter.MeterPotentialEnergy;
-import etomica.molecule.MoleculeSourceRandomMolecule;
-import etomica.potential.PotentialCalculationEnergySum;
-import etomica.potential.PotentialMaster;
-import etomica.space.Space;
-import etomica.space.Vector;
-import etomica.species.ISpecies;
-import etomica.util.random.IRandom;
-import etomica.virial.BoxCluster;
-import etomica.virial.mcmove.MCMoveClusterMolecule;
-
-/**
- * An MC Move for cluster simulations that "wiggles" a chain molecule.  If the
- * first or last atom in the chain is chosen, it is moved to a new position
- * with the same bond length as before, but perturbed by some angle from its
- * original position.  If an Atom in the middle of the chain is chosen, a
- * crankshaft move is performed that maintains its distances with its
- * neighbors.  If a middle Atom has a bond angle too close to 180 degrees
- * (such that rotation does nothing) the Atom is not moved at all.
- * In each doTrial, wiggle moves are attempted on all molecules in the box.
- *
- * @author Andrew Schultz
- */
-public class MCMoveClusterBondLength extends MCMoveClusterMolecule {
-
-    public MCMoveClusterBondLength(IRandom random, PotentialMaster potentialMaster, int armLength, Space _space) {
-        this(potentialMaster, random, 1.0, armLength, _space);
-    }
-
-    public MCMoveClusterBondLength(PotentialMaster potentialMaster,
-                                   IRandom random, double stepSize, int armLength, Space _space) {
-        super(random, _space, stepSize);
-        moleculeSource = new MoleculeSourceRandomMolecule();
-        ((MoleculeSourceRandomMolecule) moleculeSource).setRandomNumberGenerator(random);
-        this.space = _space;
-        this.armLength = armLength;
-        translateVector = _space.makeVector();
-        energyMeter = new MeterPotentialEnergy(potentialMaster);
-    }
-
-    public void setBox(Box p) {
-        super.setBox(p);
-        energyMeter.setBox(p);
-    }
-
-    public void setSpecies(ISpecies newSpecies) {
-        species = newSpecies;
-    }
-
-    //note that total energy is calculated
-    public boolean doTrial() {
-        molecule = moleculeSource.getMolecule();
-        energyMeter.setTarget(molecule);
-        uOld = energyMeter.getDataAsScalar();
-        wOld = ((BoxCluster) box).getSampleCluster().value((BoxCluster) box);
-        if (uOld > 1e8) {
-            PotentialCalculationEnergySum.debug = true;
-            energyMeter.getDataAsScalar();
-            throw new RuntimeException("molecule " + molecule + " in box " + box + " has an overlap");
-        }
-        IAtomList childList = molecule.getChildList();
-        int numChildren = childList.size();
-
-        startAtom = random.nextInt(numChildren - 1) + 1;
-
-        IAtom fixedAtom = childList.get(startAtom - 1);
-        if (startAtom % armLength == 1) {
-//            System.out.println("star atom " + startAtom);
-            fixedAtom = childList.get(0);
-        }
-        r0 = fixedAtom.getPosition();
-//            System.out.println(selectedAtoms[i]+" "+j+" before "+selectedAtoms[i].coord.position());
-        dr = stepSize * 2 * (random.nextDouble() - 0.5);
-        doTransform();
-
-        ((BoxCluster) box).trialNotify();
-        return true;
-    }
-
-    protected void doTransform() {
-        IAtomList childList = molecule.getChildList();
-        int stopAtom = (((startAtom - 1) / armLength) + 1) * armLength;
-        Vector startPos = childList.get(startAtom).getPosition();
-        translateVector.Ev1Mv2(startPos, r0);
-        bl = Math.sqrt(translateVector.squared());
-        translateVector.TE(dr / bl);
-        double fac = -((double) (stopAtom - startAtom)) / childList.size();
-        for (int iChild = 0; iChild < childList.size(); iChild++) {
-            IAtom a = childList.get(iChild);
-            Vector r = a.getPosition();
-            boolean foo = startAtom < iChild && iChild <= stopAtom;
-            r.PEa1Tv1(foo ? (fac + 1) : fac, translateVector);
-        }
-    }
-
-    public void acceptNotify() {
-//        if(r0.isZero()) System.out.println("accepted! ");
-//        System.out.println("accepting bond length move");
-        super.acceptNotify();
-    }
-
-    public void rejectNotify() {
-        dr = -dr;
-//        System.out.println("rejecting bond length move");
-        doTransform();
-        super.rejectNotify();
-    }
-
-    public double getChi(double temperature) {
-        uNew = energyMeter.getDataAsScalar();
-        wNew = ((BoxCluster) box).getSampleCluster().value((BoxCluster) box);
-        double blNew = bl + dr;
-        double ratio = blNew / bl;
-        return (wOld == 0 ? 1 : wNew / wOld) * Math.exp(-(uNew - uOld) / temperature) * ratio * ratio;
-    }
-
-    protected final MeterPotentialEnergy energyMeter;
-    protected Vector r0;
-    protected double dr;
-    protected int startAtom;
-    protected final Space space;
-    protected double wOld, wNew;
-    protected ISpecies species;
-    protected final int armLength;
-    protected final Vector translateVector;
-    protected double bl;
-}

etomica-apps/src/main/java/etomica/starpolymer/MCMoveClusterRotateArm.java
@@ -1,149 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-package etomica.starpolymer;
-
-import etomica.atom.IAtom;
-import etomica.atom.IAtomList;
-import etomica.box.Box;
-import etomica.data.meter.MeterPotentialEnergy;
-import etomica.molecule.MoleculeSourceRandomMolecule;
-import etomica.potential.PotentialCalculationEnergySum;
-import etomica.potential.PotentialMaster;
-import etomica.space.Space;
-import etomica.space.Vector;
-import etomica.space3d.RotationTensor3D;
-import etomica.space3d.Vector3D;
-import etomica.species.ISpecies;
-import etomica.util.random.IRandom;
-import etomica.virial.BoxCluster;
-import etomica.virial.mcmove.MCMoveClusterMolecule;
-
-/**
- * An MC Move for cluster simulations that "wiggles" a chain molecule.  If the
- * first or last atom in the chain is chosen, it is moved to a new position
- * with the same bond length as before, but perturbed by some angle from its
- * original position.  If an Atom in the middle of the chain is chosen, a
- * crankshaft move is performed that maintains its distances with its
- * neighbors.  If a middle Atom has a bond angle too close to 180 degrees
- * (such that rotation does nothing) the Atom is not moved at all.
- * In each doTrial, wiggle moves are attempted on all molecules in the box.
- *
- * @author Andrew Schultz
- */
-public class MCMoveClusterRotateArm extends MCMoveClusterMolecule {
-
-    public PotentialMaster potentialMaster;
-
-    public MCMoveClusterRotateArm(IRandom random, PotentialMaster potentialMaster, int armLength, Space _space) {
-        this(potentialMaster, random, 1.0, armLength, _space);
-    }
-
-    public MCMoveClusterRotateArm(PotentialMaster potentialMaster,
-                                  IRandom random, double stepSize, int armLength, Space _space) {
-        super(random, _space, stepSize);
-        this.potentialMaster = potentialMaster;
-        moleculeSource = new MoleculeSourceRandomMolecule();
-        ((MoleculeSourceRandomMolecule) moleculeSource).setRandomNumberGenerator(random);
-        this.space = _space;
-        this.armLength = armLength;
-        setStepSizeMax(Math.PI);
-        energyMeter = new MeterPotentialEnergy(potentialMaster);
-        work1 = _space.makeVector();
-        work2 = _space.makeVector();
-        work3 = _space.makeVector();
-        rotationTensor = new RotationTensor3D();
-        axis = space.makeVector();
-    }
-
-    public void setBox(Box p) {
-        super.setBox(p);
-        energyMeter.setBox(p);
-    }
-
-    public void setSpecies(ISpecies newSpecies) {
-        species = newSpecies;
-    }
-
-    //note that total energy is calculated
-    public boolean doTrial() {
-        molecule = moleculeSource.getMolecule();
-        energyMeter.setTarget(molecule);
-        uOld = energyMeter.getDataAsScalar();
-        wOld = ((BoxCluster) box).getSampleCluster().value((BoxCluster) box);
-        if (uOld > 1e8) {
-            PotentialCalculationEnergySum.debug = true;
-            energyMeter.getDataAsScalar();
-            throw new RuntimeException("molecule " + molecule + " in box " + box + " has an overlap");
-        }
-        IAtomList childList = molecule.getChildList();
-        int numChildren = childList.size();
-
-        startAtom = random.nextInt(numChildren - 1) + 1;
-
-        IAtom fixedAtom = childList.get(startAtom - 1);
-        if (startAtom % armLength == 1) fixedAtom = childList.get(0);
-        r0 = fixedAtom.getPosition();
-//            System.out.println(selectedAtoms[i]+" "+j+" before "+selectedAtoms[i].coord.position());
-        axis.setRandomSphere(random);
-        double theta = random.nextDouble() * stepSize;
-//        System.out.println("rotate from "+startAtom+" by "+theta+" about "+axis);
-        rotationTensor.setRotationAxis(axis, theta);
-        doTransform();
-        ((BoxCluster) box).trialNotify();
-        return true;
-    }
-
-
-    protected void doTransform() {
-        IAtomList childList = molecule.getChildList();
-        int stopAtom = (((startAtom - 1) / armLength) + 1) * armLength;
-        Vector sum = new Vector3D();
-        for (int iChild = startAtom; iChild <= stopAtom; iChild++) {
-            IAtom a = childList.get(iChild);
-            Vector r = a.getPosition();
-            sum.ME(r);
-            r.ME(r0);
-            box.getBoundary().nearestImage(r);
-            rotationTensor.transform(r);
-            r.PE(r0);
-            sum.PE(r);
-        }
-        sum.TE(1.0 / childList.size());
-        for (int i = 0; i < childList.size(); i++) {
-            IAtom a = childList.get(i);
-            Vector r = a.getPosition();
-            r.ME(sum);
-        }
-    }
-
-    public void acceptNotify() {
-//        System.out.println("accepting rotate");
-        super.acceptNotify();
-    }
-
-    public void rejectNotify() {
-//        System.out.println("rejecting rotate");
-        rotationTensor.invert();
-        doTransform();
-        super.rejectNotify();
-    }
-
-    public double getChi(double temperature) {
-        uNew = energyMeter.getDataAsScalar();
-        wNew = ((BoxCluster) box).getSampleCluster().value((BoxCluster) box);
-        return (wOld == 0 ? 1 : wNew / wOld) * Math.exp(-(uNew - uOld) / temperature);
-    }
-
-    protected final MeterPotentialEnergy energyMeter;
-    protected Vector r0;
-    protected int startAtom;
-    protected final Vector work1, work2, work3;
-    protected final Space space;
-    protected ISpecies species;
-    protected double wOld, wNew;
-    protected final int armLength;
-    protected final RotationTensor3D rotationTensor;
-    protected final Vector axis;
-}

commit ce65c11503d1c3aeb9191f5bb513d80e1bfec793

get moves working again (broken by d3a2ff73)

etomica-apps/src/main/java/etomica/starpolymer/MCMoveClusterBondLength.java
@@ -6,6 +6,7 @@ package etomica.starpolymer;
 
 import etomica.atom.IAtom;
 import etomica.atom.IAtomList;
+import etomica.box.Box;
 import etomica.integrator.mcmove.MCMoveBoxStep;
 import etomica.molecule.IMolecule;
 import etomica.molecule.MoleculeSource;
@@ -52,12 +53,18 @@ public class MCMoveClusterBondLength extends MCMoveBoxStep {
     public MCMoveClusterBondLength(PotentialCompute potentialMaster,
                                    IRandom random, double stepSize, int armLength, Space _space) {
         super();
+        this.random = random;
         moleculeSource = new MoleculeSourceRandomMolecule();
         ((MoleculeSourceRandomMolecule) moleculeSource).setRandomNumberGenerator(random);
         this.space = _space;
         this.armLength = armLength;
         translateVector = _space.makeVector();
         this.potentialMaster = potentialMaster;
+    }
+
+    public void setBox(Box box) {
+        super.setBox(box);
+        moleculeSource.setBox(box);
     }
 
     public void setSpecies(ISpecies newSpecies) {
@@ -108,6 +115,7 @@ public class MCMoveClusterBondLength extends MCMoveBoxStep {
         dr = -dr;
 //        System.out.println("rejecting bond length move");
         doTransform();
+        ((BoxCluster) box).rejectNotify();
     }
 
     public double getChi(double temperature) {
@@ -120,7 +128,7 @@ public class MCMoveClusterBondLength extends MCMoveBoxStep {
 
     @Override
     public void acceptNotify() {
-
+        ((BoxCluster) box).acceptNotify();
     }
 
     @Override

etomica-apps/src/main/java/etomica/starpolymer/MCMoveClusterRotateArm.java
@@ -6,6 +6,7 @@ package etomica.starpolymer;
 
 import etomica.atom.IAtom;
 import etomica.atom.IAtomList;
+import etomica.box.Box;
 import etomica.integrator.mcmove.MCMoveBoxStep;
 import etomica.molecule.IMolecule;
 import etomica.molecule.MoleculeSource;
@@ -68,6 +69,11 @@ public class MCMoveClusterRotateArm extends MCMoveBoxStep {
         axis = space.makeVector();
     }
 
+    public void setBox(Box box) {
+        super.setBox(box);
+        moleculeSource.setBox(box);
+    }
+
     public void setSpecies(ISpecies newSpecies) {
         species = newSpecies;
     }
@@ -123,12 +129,14 @@ public class MCMoveClusterRotateArm extends MCMoveBoxStep {
 
     public void acceptNotify() {
 //        System.out.println("accepting rotate");
+        ((BoxCluster) box).acceptNotify();
     }
 
     public void rejectNotify() {
 //        System.out.println("rejecting rotate");
         rotationTensor.invert();
         doTransform();
+        ((BoxCluster) box).rejectNotify();
     }
 
     public double getChi(double temperature) {