0f459d95b732d0d62a51eadeb158fe6c47c5efc3
Andrew J. Schultz <ajschult784@gmail.com>
2024-05-03 11:56:07 -0400

    add sim to compute coefficients for simple polymer chains

---
+++ etomica-apps/src/main/java/etomica/virial/simulations/VirialChain.java
@@ -0,0 +1,405 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package etomica.virial.simulations;
+
+import etomica.action.IAction;
+import etomica.action.activity.ActivityIntegrate;
+import etomica.atom.AtomType;
+import etomica.box.Box;
+import etomica.config.ConformationLinear;
+import etomica.data.IDataInfo;
+import etomica.data.types.DataDouble;
+import etomica.graph.model.Graph;
+import etomica.graph.operations.DeleteEdge;
+import etomica.graph.operations.DeleteEdgeParameters;
+import etomica.graphics.*;
+import etomica.integrator.IntegratorListenerAction;
+import etomica.math.SpecialFunctions;
+import etomica.molecule.IMoleculeList;
+import etomica.potential.*;
+import etomica.space.Space;
+import etomica.space.Vector;
+import etomica.space3d.Space3D;
+import etomica.species.ISpecies;
+import etomica.species.SpeciesBuilder;
+import etomica.species.SpeciesManager;
+import etomica.units.Pixel;
+import etomica.units.dimensions.Dimension;
+import etomica.units.dimensions.*;
+import etomica.util.Constants.CompassDirection;
+import etomica.util.ParameterBase;
+import etomica.util.ParseArgs;
+import etomica.util.collections.IntArrayList;
+import etomica.util.random.RandomMersenneTwister;
+import etomica.virial.MayerFunction;
+import etomica.virial.MayerGeneral;
+import etomica.virial.cluster.ClusterChainHS;
+import etomica.virial.cluster.ClusterSum;
+import etomica.virial.cluster.ClusterSumShell;
+import etomica.virial.cluster.VirialDiagrams;
+import etomica.virial.mcmove.MCMoveClusterAngle;
+import etomica.virial.mcmove.MCMoveClusterMoleculeHSChain;
+import etomica.virial.mcmove.MCMoveClusterMoleculeMulti;
+import etomica.virial.mcmove.MCMoveClusterStretch;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Mayer sampling simulation for alkanes using the TraPPE-United Atoms force field.
+ *   M.G. Martin and J.I. Siepmann, "Transferable Potentials for Phase
+ *   Equilibria. 1. United-Atom Description of n-Alkanes," J. Phys. Chem. B
+ *   102, 2569-2577 (1998)
+ *   
+ *   
+ *   modified from VirialAlkaneFlex2 so that eovererr can be used
+ *   March 2013
+ */
+public class VirialChain {
+
+    public static String getSplitGraphString(Set<Graph> gSplit, VirialDiagrams flexDiagrams, boolean correction) {
+        DeleteEdge edgeDeleter = new DeleteEdge();
+        DeleteEdgeParameters ede = new DeleteEdgeParameters(flexDiagrams.eBond);
+        boolean first = true;
+        String str = "";
+        for (Graph gs : gSplit) {
+            if (VirialDiagrams.graphHasEdgeColor(gs, flexDiagrams.efbcBond)) {
+                str += " "+gs.nodeCount()+"bc";
+            }
+            else {
+                str += " "+gs.getStore().toNumberString();
+                if (VirialDiagrams.graphHasEdgeColor(gs, flexDiagrams.eBond)) {
+                    str += "p" + edgeDeleter.apply(gs, ede).getStore().toNumberString();
+                }
+            }
+            if (first && correction) str += "c";
+            first = false;
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        VirialSiepmannSpheresParam params = new VirialSiepmannSpheresParam();
+        boolean isCommandline = args.length > 0;
+        if (args.length > 0) {
+            ParseArgs.doParseArgs(params, args);
+        } else {
+            params.nPoints = 2;
+            params.nSpheres = 50;
+            params.temperature = 3;
+            params.eFENE = 10;
+        }
+        final int nPoints = params.nPoints;
+        int nSpheres = params.nSpheres;
+        double temperature = params.temperature;
+        long steps = params.numSteps;
+        double refFreq = params.refFreq;
+        double sigmaHSRef = 1.5 + 0.15*nSpheres;
+        double eFENE = params.eFENE;
+
+        Space space = Space3D.getInstance();
+
+        ConformationLinear conf = new ConformationLinear(Space3D.getInstance(), 1, new double[]{0,Math.PI/2});
+        AtomType type = AtomType.simple("A");
+        ISpecies species = new SpeciesBuilder(Space3D.getInstance())
+                .addCount(type, nSpheres)
+                .withConformation(conf).build();
+        SpeciesManager sm = new SpeciesManager.Builder().addSpecies(species).build();
+
+        PotentialMoleculePair pTarget = new PotentialMoleculePair(space, sm);
+        System.out.println(nSpheres+"-mer chain B"+nPoints+" at T = "+temperature);
+        P2LennardJones p2 = new P2LennardJones(1, 1);
+
+        MayerGeneral fTarget = new MayerGeneral(pTarget);
+        MayerFunction fRefPos = new MayerFunction() {
+            public void setBox(Box box) {
+            }
+
+            public double f(IMoleculeList pair, double r2, double beta) {
+                return r2 < sigmaHSRef * sigmaHSRef ? 1 : 0;
+            }
+        };
+
+        boolean flex = nSpheres > 2 && nPoints > 2;
+        VirialDiagrams alkaneDiagrams = new VirialDiagrams(nPoints, false, flex);
+        alkaneDiagrams.setDoReeHoover(false);
+        ClusterSum targetCluster = alkaneDiagrams.makeVirialCluster(fTarget);
+        ClusterChainHS refCluster = new ClusterChainHS(nPoints, fRefPos);
+        double vhs = (4.0 / 3.0) * Math.PI * sigmaHSRef * sigmaHSRef * sigmaHSRef;
+        final double refIntegral = SpecialFunctions.factorial(nPoints) / 2 * Math.pow(vhs, nPoints - 1);
+
+        ClusterSumShell[] targetDiagrams = new ClusterSumShell[0];
+        int[] targetDiagramNumbers = new int[0];
+        boolean[] diagramFlexCorrection = null;
+
+        if (nSpheres > 2) {
+            targetDiagrams = alkaneDiagrams.makeSingleVirialClusters(targetCluster, null, fTarget);
+            targetDiagramNumbers = new int[targetDiagrams.length];
+            System.out.println("individual clusters:");
+            Set<Graph> singleGraphs = alkaneDiagrams.getMSMCGraphs(true, false);
+            Map<Graph,Graph> cancelMap = alkaneDiagrams.getCancelMap();
+            int iGraph = 0;
+            diagramFlexCorrection = new boolean[targetDiagrams.length];
+            for (Graph g : singleGraphs) {
+            	System.out.print(iGraph+" ("+g.coefficient()+") "+g.getStore().toNumberString()); // toNumberString: its corresponding number
+            	targetDiagramNumbers[iGraph] = Integer.parseInt(g.getStore().toNumberString());
+
+                Graph cancelGraph = cancelMap.get(g);
+                if (cancelGraph != null) {
+                    diagramFlexCorrection[iGraph] = true;
+                    Set<Graph> gSplit = alkaneDiagrams.getSplitDisconnectedVirialGraphs(cancelGraph);
+
+                    System.out.print(" - "+getSplitGraphString(gSplit, alkaneDiagrams, false));
+
+                }
+            	System.out.println();
+            	iGraph++;
+            }
+            System.out.println();
+            Set<Graph> disconnectedGraphs = alkaneDiagrams.getExtraDisconnectedVirialGraphs();
+            if (disconnectedGraphs.size() > 0) {
+                System.out.println("extra clusters:");
+                
+                for (Graph g : disconnectedGraphs) {
+                	Set<Graph> gSplit = alkaneDiagrams.getSplitDisconnectedVirialGraphs(g);
+                	System.out.println(g.coefficient()+" "+getSplitGraphString(gSplit, alkaneDiagrams, true));
+                }
+                System.out.println();
+            }
+        }
+
+        targetCluster.setTemperature(temperature);
+        refCluster.setTemperature(temperature);
+        for (int i=0; i<targetDiagrams.length; i++) {
+            targetDiagrams[i].setTemperature(temperature);
+        }
+
+        System.out.println("sigmaHSRef: "+sigmaHSRef);
+        // eovererr expects this string, BnHS
+        System.out.println("B"+nPoints+"HS: "+refIntegral);
+
+        PotentialMasterBonding.FullBondingInfo bondingInfo = new PotentialMasterBonding.FullBondingInfo(sm);
+
+        if (nSpheres > 1 && eFENE > 0 && eFENE < Double.POSITIVE_INFINITY) {
+            P2Fene potentialFene = new P2Fene(2, eFENE);
+            P2LennardJones potentialLJ = new P2LennardJones();
+            P2SoftSphericalSum pBonding = new P2SoftSphericalSum(potentialFene, potentialLJ);
+            List<int[]> pairs = new ArrayList<>();
+            for (int i=0; i<nSpheres-1; i++) {
+                pairs.add(new int[]{i,i+1});
+            }
+            bondingInfo.setBondingPotentialPair(species, pBonding, pairs);
+        }
+        if (nSpheres > 2 && false) {
+            P3BondAngle p3 = null;
+            List<int[]> triplets = new ArrayList<>();
+            for (int i=0; i<nSpheres-2; i++) {
+                triplets.add(new int[]{i,i+1,i+2});
+            }
+            bondingInfo.setBondingPotentialTriplet(species, p3, triplets);
+        }
+
+        final SimulationVirialOverlap2 sim = new SimulationVirialOverlap2(space, sm,
+                new int[]{flex ? (nPoints+1) : nPoints},temperature, refCluster, targetCluster);
+        sim.setExtraTargetClusters(targetDiagrams);
+        sim.setDoWiggle(false);
+        sim.setBondingInfo(bondingInfo);
+        sim.setIntraPairPotentials(pTarget.getAtomPotentials());
+        sim.setRandom(new RandomMersenneTwister(2));
+        sim.init();
+
+        sim.integrators[0].getMoveManager().removeMCMove(sim.mcMoveTranslate[0]);
+        MCMoveClusterMoleculeHSChain mcMoveHSC = new MCMoveClusterMoleculeHSChain(sim.getRandom(), sim.box[0], sigmaHSRef);
+        sim.integrators[0].getMoveManager().addMCMove(mcMoveHSC);
+        sim.accumulators[0].setBlockSize(1);
+
+        if (flex) {
+            int[] constraintMap = new int[nPoints+1];
+            for (int i=0; i<nPoints; i++) {
+                constraintMap[i] = i;
+            }
+            constraintMap[nPoints] = 0;
+            mcMoveHSC.setConstraintMap(constraintMap);
+            ((MCMoveClusterMoleculeMulti)sim.mcMoveTranslate[1]).setConstraintMap(constraintMap);
+//            ((MCMoveClusterRotateMoleculeMulti)sim.mcMoveRotate[0]).setConstraintMap(constraintMap);
+//            ((MCMoveClusterRotateMoleculeMulti)sim.mcMoveRotate[1]).setConstraintMap(constraintMap);
+        }
+
+        if (refFreq >= 0) {
+            sim.integratorOS.setAdjustStepFraction(false);
+            sim.integratorOS.setRefStepFraction(refFreq);
+        }
+      
+        sim.integratorOS.setNumSubSteps(1000);
+        sim.integratorOS.setAggressiveAdjustStepFraction(true);
+        System.out.println(steps+" steps (1000 blocks of "+steps/1000+")");
+        steps /= 1000;
+
+        pTarget.setAtomPotential(type, type, p2);
+
+        sim.integratorOS.setNumSubSteps(1000);
+
+        MCMoveClusterAngle[] angleMoves = null;
+        MCMoveClusterStretch[] stretchMoves = null;
+
+        IntArrayList[] bonding = new IntArrayList[nSpheres];
+        bonding[0] = new IntArrayList(new int[]{1});
+        for (int i=1; i<nSpheres-1; i++) {
+            bonding[i] = new IntArrayList(new int[]{i-1,i+1});
+        }
+        bonding[nSpheres-1] = new IntArrayList(new int[]{nSpheres-2});
+
+        if (nSpheres > 1 && eFENE > 0 && eFENE < Double.POSITIVE_INFINITY) {
+            stretchMoves = new MCMoveClusterStretch[2];
+            stretchMoves[0] = new MCMoveClusterStretch(sim.integrators[0].getPotentialCompute(), space, bonding, sim.getRandom(), 1);
+            stretchMoves[0].setBox(sim.box[0]);
+            sim.integrators[0].getMoveManager().addMCMove(stretchMoves[0]);
+            stretchMoves[1] = new MCMoveClusterStretch(sim.integrators[1].getPotentialCompute(), space, bonding, sim.getRandom(), 1);
+            stretchMoves[1].setBox(sim.box[1]);
+            sim.integrators[1].getMoveManager().addMCMove(stretchMoves[1]);
+        }
+
+        if (nSpheres > 2) {
+            angleMoves = new MCMoveClusterAngle[2];
+            angleMoves[0] = new MCMoveClusterAngle(sim.integrators[0].getPotentialCompute(), space, bonding, sim.getRandom(), 1);
+            angleMoves[0].setBox(sim.box[0]);
+            sim.integrators[0].getMoveManager().addMCMove(angleMoves[0]);
+            angleMoves[1] = new MCMoveClusterAngle(sim.integrators[1].getPotentialCompute(), space, bonding, sim.getRandom(), 1);
+            angleMoves[1].setBox(sim.box[1]);
+            sim.integrators[1].getMoveManager().addMCMove(angleMoves[1]);
+        }
+
+        if(true) {
+            double size = (nSpheres + 5) * 1.5;
+            sim.box[0].getBoundary().setBoxSize(Vector.of(new double[]{size, size, size}));
+            sim.box[1].getBoundary().setBoxSize(Vector.of(new double[]{size, size, size}));
+            SimulationGraphic simGraphic = new SimulationGraphic(sim, SimulationGraphic.TABBED_PANE);
+            DisplayBox displayBox0 = simGraphic.getDisplayBox(sim.box[0]);
+            DisplayBox displayBox1 = simGraphic.getDisplayBox(sim.box[1]);
+            displayBox0.setPixelUnit(new Pixel(300.0 / size));
+            displayBox1.setPixelUnit(new Pixel(300.0 / size));
+            displayBox0.setShowBoundary(false);
+            displayBox1.setShowBoundary(false);
+            ((DisplayBoxCanvasG3DSys) displayBox0.canvas).setBackgroundColor(Color.WHITE);
+            ((DisplayBoxCanvasG3DSys) displayBox1.canvas).setBackgroundColor(Color.WHITE);
+
+
+            ColorSchemeRandomByMolecule colorScheme = new ColorSchemeRandomByMolecule(sim.getSpeciesManager(), sim.box[0], sim.getRandom());
+            displayBox0.setColorScheme(colorScheme);
+            colorScheme = new ColorSchemeRandomByMolecule(sim.getSpeciesManager(), sim.box[1], sim.getRandom());
+            displayBox1.setColorScheme(colorScheme);
+
+
+            simGraphic.makeAndDisplayFrame();
+
+            sim.integratorOS.setNumSubSteps(1000);
+            sim.setAccumulatorBlockSize(1000);
+
+            // if running interactively, set filename to null so that it doens't read
+            // (or write) to a refpref file
+            sim.initRefPref(null, 10, false);
+            sim.equilibrate(null, 20, false);
+            sim.getController().addActivity(new ActivityIntegrate(sim.integratorOS));
+            if ((Double.isNaN(sim.refPref) || Double.isInfinite(sim.refPref) || sim.refPref == 0)) {
+                throw new RuntimeException("Oops");
+            }
+
+            final DisplayTextBox averageBox = new DisplayTextBox();
+            averageBox.setLabel("Average");
+            final DisplayTextBox errorBox = new DisplayTextBox();
+            errorBox.setLabel("Error");
+            JLabel jLabelPanelParentGroup = new JLabel("B" + nPoints);
+            final JPanel panelParentGroup = new JPanel(new BorderLayout());
+            panelParentGroup.add(jLabelPanelParentGroup, CompassDirection.NORTH.toString());
+            panelParentGroup.add(averageBox.graphic(), BorderLayout.WEST);
+            panelParentGroup.add(errorBox.graphic(), BorderLayout.EAST);
+            simGraphic.getPanel().controlPanel.add(panelParentGroup, SimulationPanel.getVertGBC());
+
+            IAction pushAnswer = new IAction() {
+                DataDouble data = new DataDouble();
+
+                public void actionPerformed() {
+                    double[] ratioAndError = sim.dvo.getAverageAndError();
+                    double ratio = ratioAndError[0];
+                    double error = ratioAndError[1];
+                    data.x = ratio;
+                    averageBox.putData(data);
+                    data.x = error;
+                    errorBox.putData(data);
+                }
+            };
+            IDataInfo dataInfo = new DataDouble.DataInfoDouble("B" + nPoints, new CompoundDimension(new Dimension[]{new DimensionRatio(Volume.DIMENSION, Quantity.DIMENSION)}, new double[]{nPoints - 1}));
+            averageBox.putDataInfo(dataInfo);
+            averageBox.setLabel("average");
+            errorBox.putDataInfo(dataInfo);
+            errorBox.setLabel("error");
+            errorBox.setPrecision(2);
+            sim.integratorOS.getEventManager().addListener(new IntegratorListenerAction(pushAnswer));
+            return;
+        }
+
+        long t1 = System.nanoTime();
+        // if running interactively, don't use the file
+        String refFileName = isCommandline ? "refpref"+nPoints+"_"+temperature : null;
+        // this will either read the refpref in from a file or run a short simulation to find it
+        sim.initRefPref(refFileName, steps/40);
+
+                
+        // run another short simulation to find MC move step sizes and maybe narrow in more on the best ref pref
+        // if it does continue looking for a pref, it will write the value to the file
+        sim.equilibrate(refFileName, steps/20);
+        ActivityIntegrate ai = new ActivityIntegrate(sim.integratorOS, 1000);
+        sim.setAccumulatorBlockSize(steps);
+
+        System.out.println("equilibration finished");
+        sim.setAccumulatorBlockSize(steps);
+        sim.integratorOS.setNumSubSteps((int)steps);
+        System.out.println("MC Move step sizes (ref)    "
+                +(sim.mcMoveRotate==null ? "" : (""+sim.mcMoveRotate[0].getStepSize()))+" "
+                +(sim.mcMoveWiggle==null ? "" : (""+sim.mcMoveWiggle[0].getStepSize())));
+        System.out.println("MC Move step sizes (target) "+sim.mcMoveTranslate[1].getStepSize()+" "
+                +(sim.mcMoveRotate==null ? "" : (""+sim.mcMoveRotate[1].getStepSize()))+" "
+                +(sim.mcMoveWiggle==null ? "" : (""+sim.mcMoveWiggle[1].getStepSize())));
+
+        sim.integratorOS.getMoveManager().setEquilibrating(false);
+        sim.getController().runActivityBlocking(ai);
+        long t2 = System.nanoTime();
+
+        System.out.println("final reference step frequency "+sim.integratorOS.getIdealRefStepFraction());
+        System.out.println("actual reference step frequency "+sim.integratorOS.getRefStepFraction());
+        String[] extraNames = new String[targetDiagrams.length];
+        for (int i=0; i<targetDiagrams.length; i++) {
+            String n = "";
+            if (targetDiagramNumbers[i] < 0) {
+                n = "diagram " + (-targetDiagramNumbers[i]) + "bc";
+            } else {
+                n = "diagram " + targetDiagramNumbers[i];
+
+                if (diagramFlexCorrection[i]) {
+                    n += "c";
+                }
+            }
+            extraNames[i] = n;
+        }
+        sim.printResults(refIntegral, extraNames);
+        System.out.println("time: "+(t2-t1)/1e9);
+    }
+
+    /**
+     * Inner class for parameters
+     */
+    public static class VirialSiepmannSpheresParam extends ParameterBase {
+        public int nPoints = 2;
+        public int nSpheres = 3;
+        public double temperature = 1;
+        public long numSteps = 1000000;
+        public double refFreq = -1;
+        public double eFENE = 0;
+    }
+}
