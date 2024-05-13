7453184fa28746fa207ed380b85bc59603a25d6a
Andrew J. Schultz <ajschult784@gmail.com>
2024-05-13 10:37:24 -0400

    migrate intramolecular bits to direct sampling from overlap

--- etomica-apps/src/main/java/etomica/virial/simulations/SimulationVirial.java
+++ etomica-apps/src/main/java/etomica/virial/simulations/SimulationVirial.java
@@ -13,8 +13,12 @@ import etomica.data.types.DataGroup;
 import etomica.integrator.Integrator;
 import etomica.integrator.IntegratorMC;
 import etomica.integrator.mcmove.MCMoveBoxStep;
+import etomica.potential.IPotential2;
+import etomica.potential.PotentialMasterBonding;
+import etomica.potential.compute.NeighborManagerIntra;
 import etomica.potential.compute.PotentialCompute;
 import etomica.potential.compute.PotentialComputeAggregate;
+import etomica.potential.compute.PotentialComputePair;
 import etomica.simulation.Simulation;
 import etomica.space.Space;
 import etomica.species.ISpecies;
@@ -50,6 +54,9 @@ public class SimulationVirial extends Simulation {
     public int[] newSeeds;
     public int[] numMolecules;
     protected double boxLength;
+    protected PotentialMasterBonding.FullBondingInfo bondingInfo;
+    protected IPotential2[][] pairPotentials;
+    protected boolean initialized;
 
     public SimulationVirial(Space space, ISpecies[] species, int[] nMolecules, double temperature, ClusterWeight aSampleCluster, ClusterAbstract refCluster, ClusterAbstract[] targetClusters) {
         super(space);
@@ -74,7 +81,21 @@ public class SimulationVirial extends Simulation {
         boxLength = length;
     }
 
+    public void setIntraPairPotentials(IPotential2[][] pairPotentials) {
+        if (initialized) throw new RuntimeException("too late");
+        this.pairPotentials = pairPotentials;
+    }
+
+    public void setBondingInfo(PotentialMasterBonding.FullBondingInfo bondingInfo) {
+        this.bondingInfo = bondingInfo;
+    }
+
     public void init() {
+        if (initialized) throw new RuntimeException("you can only call me once");
+        // we aren't actually initialized yet, but we will be unless we crash.
+        // if we crash, we shouldn't get called again!
+        initialized = true;
+
         if (seeds != null) {
             setRandom(new RandomMersenneTwister(seeds));
         }
@@ -89,8 +110,18 @@ public class SimulationVirial extends Simulation {
             box.setNMolecules(species[i], numMolecules[i]);
         }
 
-        PotentialCompute pc = null;
-        pc = new PotentialComputeAggregate();
+        PotentialCompute pc;
+        if (pairPotentials != null) {
+            PotentialMasterBonding pmBonding = new PotentialMasterBonding(getSpeciesManager(), box, bondingInfo);
+            PotentialComputePair pcPair = new PotentialComputePair(getSpeciesManager(), box, new NeighborManagerIntra(box, bondingInfo), pairPotentials);
+            pc = new PotentialComputeAggregate(pmBonding, pcPair);
+        }
+        else if (bondingInfo != null){
+            pc = new PotentialMasterBonding(getSpeciesManager(), box, bondingInfo);
+        }
+        else {
+            pc = new PotentialComputeAggregate();
+        }
 
         // temperature isn't going to mean anything here, but pass it anyway
         integrator = new IntegratorMC(pc, random, temperature, box);
