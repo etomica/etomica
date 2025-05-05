601b5139f400be0686d33dffe60dc227d3eb94cb
Andrew J. Schultz <ajschult784@gmail.com>
2025-05-05 15:11:36 -0400

    make it possible to not rotate central molecule

--- etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterRotateMoleculeMulti.java
+++ etomica-apps/src/main/java/etomica/virial/mcmove/MCMoveClusterRotateMoleculeMulti.java
@@ -35,6 +35,7 @@ public class MCMoveClusterRotateMoleculeMulti extends MCMoveBoxStep {
     protected final RotationTensor rotationTensor;
     protected boolean doLattice;
     protected int[] rotationCenters;
+    public int startMolecule;
 
     public MCMoveClusterRotateMoleculeMulti(IRandom random, Box box) {
         super();
@@ -95,7 +96,7 @@ public class MCMoveClusterRotateMoleculeMulti extends MCMoveBoxStep {
             trialCount = relaxInterval;
         }
         IMoleculeList moleculeList = box.getMoleculeList();
-        for (int i = 0; i<moleculeList.size(); i++) {
+        for (int i = startMolecule; i<moleculeList.size(); i++) {
             IMolecule molecule = moleculeList.get(i);
             if (doLattice) {
                 if (i==0) continue;
