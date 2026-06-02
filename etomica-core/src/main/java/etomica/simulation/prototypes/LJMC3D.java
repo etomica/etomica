febef50b030b364d9c1f6d0332d6466a09ed0284
Andrew J. Schultz <ajschult784@gmail.com>
2026-06-02 18:53:26 -0400

    Update to collect pressure
    also equilibrate at high T if needed and report step size

--- etomica-core/src/main/java/etomica/simulation/prototypes/LJMC3D.java
+++ etomica-core/src/main/java/etomica/simulation/prototypes/LJMC3D.java
@@ -15,6 +15,7 @@ import etomica.data.DataPumpListener;
 import etomica.data.DataSourceCountSteps;
 import etomica.data.history.HistoryCollapsingAverage;
 import etomica.data.meter.MeterPotentialEnergyFromIntegrator;
+import etomica.data.meter.MeterPressure;
 import etomica.data.types.DataGroup;
 import etomica.graphics.DisplayPlot;
 import etomica.graphics.SimulationGraphic;
@@ -37,11 +38,13 @@ import etomica.util.ParseArgs;
  *
  * @author Andrew Schultz
  */
-
 public class LJMC3D extends Simulation {
 
     public final PotentialMasterCell potentialMaster;
     public final IntegratorMC integrator;
+    public final P2LennardJones p2lj;
+    public final Box box;
+    public final MCMoveAtom move;
 
     /**
      * Creates simulation with default parameters from {@link SimParams}
@@ -59,10 +62,11 @@ public class LJMC3D extends Simulation {
         SpeciesGeneral species = SpeciesGeneral.monatomic(space, AtomType.simpleFromSim(this));
         addSpecies(species);
 
-        double rc = 3;
-        Box box = new Box(space);
+        double rc = params.rc;
+        box = new Box(space);
         addBox(box);
         potentialMaster = new PotentialMasterCell(getSpeciesManager(), box, 2, BondingInfo.noBonding());
+        potentialMaster.doAllTruncationCorrection = true;
 
         box.setNMolecules(species, params.numAtoms);
         BoxInflate inflater = new BoxInflate(box, space, params.density);
@@ -71,25 +75,25 @@ public class LJMC3D extends Simulation {
         ConfigurationLattice config = new ConfigurationLattice(new LatticeCubicFcc(space), space);
         config.initializeCoordinates(box);
 
-        P2LennardJones p2lj = new P2LennardJones();
+        p2lj = new P2LennardJones();
         P2SoftSphericalTruncated p2 = new P2SoftSphericalTruncated(p2lj, rc);
         AtomType atomType = species.getLeafType();
         potentialMaster.setPairPotential(atomType, atomType, p2);
 
         integrator = new IntegratorMC(potentialMaster, random, params.temperature, box);
 
-        MCMoveAtom mcMoveAtom = new MCMoveAtom(random, potentialMaster, box);
-        integrator.getMoveManager().addMCMove(mcMoveAtom);
+        move = new MCMoveAtom(random, potentialMaster, box);
+        integrator.getMoveManager().addMCMove(move);
 
-        this.getController().addActivity(new ActivityIntegrate(integrator));
+        getController().addActivity(new ActivityIntegrate(integrator));
     }
 
     public static void main(String[] args) {
-
         SimParams params = new SimParams();
         if (args.length > 0) {
             ParseArgs.doParseArgs(params, args);
-        } else {
+        }
+        else {
             // modify parameters here for interactive testing
         }
 
@@ -103,11 +107,23 @@ public class LJMC3D extends Simulation {
         System.out.println("N: " + params.numAtoms);
         System.out.println("T: " + params.temperature);
         System.out.println("density: " + params.density);
+        System.out.println("rc: " + params.rc);
         System.out.println("steps: " + params.steps);
+        MeterPressure meterP = new MeterPressure(sim.box, sim.integrator.getPotentialCompute());
+        sim.integrator.reset();
+        meterP.setTemperature(params.temperature);
 
         // equilibration
         long t1 = System.currentTimeMillis();
+        if (params.eq1T > params.temperature) {
+            System.out.println("equilibrating at T " + params.eq1T);
+            sim.integrator.setTemperature(params.eq1T);
+            sim.getController().runActivityBlocking(new ActivityIntegrate(sim.integrator, steps / 10));
+            sim.integrator.setTemperature(params.temperature);
+        }
+
         sim.getController().runActivityBlocking(new ActivityIntegrate(sim.integrator, steps / 10));
+        sim.integrator.getMoveManager().setEquilibrating(false);
         System.out.println("equilibration finished");
 
         // data collection
@@ -116,19 +132,30 @@ public class LJMC3D extends Simulation {
         DataPumpListener pump = new DataPumpListener(meterPE, acc, interval);
         sim.integrator.getEventManager().addListener(pump);
 
+        int intervalP = params.numAtoms;
+        long blockSizeP = steps / (long)(intervalP * blocks);
+        AccumulatorAverageFixed accP = new AccumulatorAverageFixed(blockSizeP);
+        DataPumpListener pumpP = new DataPumpListener(meterP, accP, intervalP);
+        sim.integrator.getEventManager().addListener(pumpP);
         sim.integrator.resetStepCount();
         sim.integrator.getMoveManager().setEquilibrating(false);
         sim.getController().runActivityBlocking(new ActivityIntegrate(sim.integrator, steps));
 
+        System.out.println("MC step size: " + sim.move.getStepSize());
         long t2 = System.currentTimeMillis();
 
-        DataGroup dataPE = (DataGroup) acc.getData();
+        DataGroup dataPE = (DataGroup)acc.getData();
         int numAtoms = sim.getBox(0).getLeafList().size();
         double avg = dataPE.getValue(acc.AVERAGE.index) / numAtoms;
         double err = dataPE.getValue(acc.ERROR.index) / numAtoms;
         double cor = dataPE.getValue(acc.BLOCK_CORRELATION.index);
-
-        System.out.println("energy avg: " + avg + "  err: " + err + "  cor: " + cor);
+        System.out.println("U avg: " + avg + "  err: " + err + "  cor: " + cor);
+        DataGroup dataP = (DataGroup)accP.getData();
+        double avgP = dataP.getValue(acc.AVERAGE.index);
+        double errP = dataP.getValue(acc.ERROR.index);
+        double corP = dataP.getValue(acc.BLOCK_CORRELATION.index);
+        System.out.println("P avg: " + avgP + "  err: " + errP + "  cor: " + corP);
+        System.out.println("Z avg: " + avgP / params.density / params.temperature + "  err: " + errP / params.density / params.temperature + "  cor: " + corP);
         System.out.println("time: " + (t2 - t1) * 0.001);
     }
 
@@ -166,5 +193,7 @@ public class LJMC3D extends Simulation {
         public double density = 0.6;
         public double temperature = 1.2;
         public int numAtoms = 1000;
+        public double rc = 5.0;
+        public double eq1T = 0.0;
     }
 }
